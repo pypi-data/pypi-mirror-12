71b8b3599681b7194c70b48c2b1f82395906662e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:06 +0000

    Add package nester_lorraine/1.5.0/nester_lorraine-1.5.0.zip

---
+++ packages/nester_lorraine/nester_lorraine-1.5.0.zip/nester_lorraine-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name="nester_lorraine",
+        version="1.5.0",
+        py_modules=['nester'],
+        author="lorrainelau",
+        author_email="392866835@qq.com",
+        url="",
+        description="A simple printer of nested lists"
+    )
