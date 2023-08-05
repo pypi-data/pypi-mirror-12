d0eaa3e37b83cd464332cf980b41e736fb0e9313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:23 +0000

    Add package zx1995_nester/1.0.0/zx1995_nester-1.0.0.zip

---
+++ packages/zx1995_nester/zx1995_nester-1.0.0.zip/zx1995_nester-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+
+    name = 'zx1995_nester',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'hypython',
+    author_email = 'hfpython@headfirstlabs.com',
+    
+    
+
+    )
