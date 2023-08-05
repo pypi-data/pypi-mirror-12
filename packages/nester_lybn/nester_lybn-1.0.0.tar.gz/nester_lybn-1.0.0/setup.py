7a1ec1b59fd6422166c41290802dd1c6424b2eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:28 +0000

    Add package nester_lybn/1.0.0/nester_lybn-1.0.0.tar.gz

---
+++ packages/nester_lybn/nester_lybn-1.0.0.tar.gz/nester_lybn-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'nester_lybn', 
+        version      = '1.0.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['nester_lybn'],
+        author       = 'lybn',
+        author_email = 'lybn@vng.com.vn',
+        url          = 'http://www.lybn.com',
+        description  = 'A simple printer of nested lists',
+     )
