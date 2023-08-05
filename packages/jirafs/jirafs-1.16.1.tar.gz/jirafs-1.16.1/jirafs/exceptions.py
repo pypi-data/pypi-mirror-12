95edd6bec6dcfd32bab498673108a4a188f8d89b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:24 +0000

    Add package jirafs/1.16.1/jirafs-1.16.1.tar.gz

---
+++ packages/jirafs/jirafs-1.16.1.tar.gz/jirafs-1.16.1/jirafs/exceptions.py
@@ -0,0 +1,47 @@
+class JirafsError(Exception):
+    pass
+
+
+class NotTicketFolderException(JirafsError):
+    pass
+
+
+class LocalCopyOutOfDate(JirafsError):
+    pass
+
+
+class JiraInteractionFailed(JirafsError):
+    pass
+
+
+class GitCommandError(JirafsError):
+    def __init__(self, *args, **kwargs):
+        self._command = kwargs.pop('cmd', None)
+        self._returncode = kwargs.pop('returncode', None)
+        self._output = kwargs.pop('stdout', None)
+
+        super(GitCommandError, self).__init__(*args, **kwargs)
+
+    @property
+    def command(self):
+        return self._command
+
+    @property
+    def returncode(self):
+        return self._returncode
+
+    @property
+    def output(self):
+        return self._output
+
+
+class IssueValidationError(JirafsError):
+    pass
+
+
+class MacroError(JirafsError):
+    pass
+
+
+class MacroContentError(MacroError):
+    pass
