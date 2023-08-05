078c2c7cd465c9f9103d0d1ad67485de60f76d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:11 +0000

    Add package sitemap_python/0.1.2/sitemap_python-0.1.2.tar.gz

---
+++ packages/sitemap_python/sitemap_python-0.1.2.tar.gz/sitemap_python-0.1.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+del os.link
+
+sdict = {
+    'name': 'sitemap_python',
+    'version': "0.1.2",
+    'license': 'MIT',
+    'packages': ['sitemap'],
+    'zip_safe': False,
+    'install_requires': [],
+    'author': 'Lichun',
+    'url': 'https://github.com/socrateslee/sitemap_python',
+    'classifiers': [
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python']
+}
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(**sdict)
