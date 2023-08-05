3ba65b377b60e50ff42728e42902747eedd497f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:11 +0000

    Add package pas.plugins.memberpropertytogroup/1.0a6/pas.plugins.memberpropertytogroup-1.0a6.zip

---
+++ packages/pas.plugins.memberpropertytogroup/pas.plugins.memberpropertytogroup-1.0a6.zip/pas.plugins.memberpropertytogroup-1.0a6/src/pas/plugins/memberpropertytogroup/browser/mptg_helper.js
@@ -0,0 +1,62 @@
+jQuery('head').append( jQuery('<link rel="stylesheet" type="text/css" />').attr('href', '++resource++pas.plugins.memberpropertytogroup.helper.css') );
+
+(function() {
+     jQuery(document).ready(function() {
+
+        // Emulate initial markup of in Plone 4 where fields with empty values
+        // have a class 'empty' attached.
+        jQuery('#form div.field').each(function() {
+          var div = jQuery(this);
+          if (div.children('input').val() === '') {
+            div.addClass('empty');
+          } else if (div.children('textarea').val() === '') {
+            div.addClass('empty');
+          }
+        });
+
+        jQuery('.add_more_properties').bind('click', function (event) {
+            event.preventDefault();
+            jQuery('#form div.field').each(function () {
+                if (jQuery(this).hasClass('empty')) {
+                    jQuery(this).removeClass('empty');
+                    jQuery(this).next().removeClass('empty');
+                  return false;  // stop the each loop
+                }
+            return true;
+            });
+
+            jQuery('.remove_properties').attr("disabled", false);
+
+            if (jQuery('#form').children('.empty').length === 0) {
+                jQuery('.add_more_properties').attr("disabled", true);
+            }
+        });
+        jQuery('.remove_properties').bind('click', function () {
+            event.preventDefault();
+            if (jQuery('#form').has('.empty').length) {
+                jQuery('#form div.field').each(function () {
+                    if (jQuery(this).hasClass('empty')) {
+                        jQuery(this).prev().children('textarea').val('');
+                        jQuery(this).prev().prev().children('input').val('');
+                        jQuery(this).prev().addClass('empty');
+                        jQuery(this).prev().prev().addClass('empty');
+                        return false;  // stop the each loop
+                    }
+                  return true;
+                });
+
+            } else {
+                jQuery('#form div.field').last().children('textarea').val('');
+                jQuery('#form div.field').last().addClass('empty');
+                jQuery('#form div.field').last().prev().children('input').val('');
+                jQuery('#form div.field').last().prev().addClass('empty');
+            }
+
+            jQuery('.add_more_properties').attr("disabled", false);
+
+            if (jQuery('#form').children('.empty').length === 20) {
+                jQuery('.remove_properties').attr("disabled", true);
+            }
+        });
+     });
+})(jQuery);
