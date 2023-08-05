ef99d0ac5946a32c5bc73f764ca0cbb5bc032542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:44 +0000

    Add package awscurl/0.1/awscurl-0.1.tar.gz

---
+++ packages/awscurl/awscurl-0.1.tar.gz/awscurl-0.1/setup.py
@@ -0,0 +1,14 @@
+__author__ = 'iokulist'
+
+from distutils.core import setup
+
+setup(name='awscurl',
+      version='0.1',
+      description='Curl like tool with AWS request signing',
+      url='http://github.com/okigan/awscurl',
+      author='Igor Okulist',
+      author_email='okigan@gmail.com',
+      license='MIT',
+      packages=['awscurl'],
+      zip_safe=False
+      )
