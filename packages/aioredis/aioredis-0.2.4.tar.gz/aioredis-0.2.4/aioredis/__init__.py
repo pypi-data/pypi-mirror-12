1a61264366fd45c07282cc554728656e38d5c23d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:16 +0000

    Add package aioredis/0.2.4/aioredis-0.2.4.tar.gz

---
+++ packages/aioredis/aioredis-0.2.4.tar.gz/aioredis-0.2.4/aioredis/__init__.py
@@ -0,0 +1,21 @@
+from .connection import RedisConnection, create_connection
+from .commands import Redis, create_redis, create_reconnecting_redis
+from .pool import RedisPool, create_pool
+from .util import Channel
+from .errors import (
+    RedisError,
+    ProtocolError,
+    ReplyError,
+    PipelineError,
+    MultiExecError,
+    )
+
+
+__version__ = '0.2.4'
+
+# make pyflakes happy
+(create_connection, RedisConnection,
+ create_redis, create_reconnecting_redis, Redis,
+ create_pool, RedisPool, Channel,
+ RedisError, ProtocolError, ReplyError,
+ PipelineError, MultiExecError)
