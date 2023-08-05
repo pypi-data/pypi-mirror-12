dcd961b167f3a0d6e7a195b21f6d8fdd36278208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:50 +0000

    Add package testing1/1.2.0/testing1-1.2.0.tar.gz

---
+++ packages/testing1/testing1-1.2.0.tar.gz/testing1-1.2.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = 'testing1',
+    version = '1.2.0',
+    py_modules = ['print_lol'],
+    author = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    )
