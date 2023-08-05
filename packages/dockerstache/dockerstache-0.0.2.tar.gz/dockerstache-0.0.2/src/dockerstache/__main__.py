934567d07e28b748e26374792cb3e06a1ad12c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:07 +0000

    Add package dockerstache/0.0.2/dockerstache-0.0.2.tar.gz

---
+++ packages/dockerstache/dockerstache-0.0.2.tar.gz/dockerstache-0.0.2/src/dockerstache/__main__.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""
+_dockerstache_
+
+"""
+import os
+import sys
+import argparse
+import json
+
+from .templates import process_templates
+from . import get_logger
+
+LOGGER = get_logger()
+
+
+def build_parser():
+    """
+    _build_parser_
+
+    Set up CLI parser options, parse the
+    CLI options an return the parsed results
+
+    """
+    parser = argparse.ArgumentParser(
+        description='dockerstache templating util'
+    )
+    parser.add_argument(
+        '--output', '-o',
+        help='Working directory to render dockerfile and templates',
+        dest='output',
+        default=os.path.join(os.getcwd(), 'diesel_output')
+        )
+    parser.add_argument(
+        '--input', '-i',
+        help='Working directory containing dockerfile and script mustache templates',
+        dest='input',
+        default=os.getcwd()
+        )
+    parser.add_argument(
+        '--context', '-c',
+        help='JSON file containing context dictionary to render templates',
+        dest='context',
+        required=True
+    )
+
+    opts = parser.parse_args()
+    return opts
+
+
+def main():
+    """
+    _main_
+
+    Create a CLI parser and use that to run
+    the template rendering process
+
+    """
+    opts = build_parser()
+    if not os.path.exists(opts.context):
+        msg = "Context file {} not found".format(opts.context)
+        LOGGER.error(msg)
+        sys.exit(1)
+    LOGGER.info(
+        (
+            "{{dockerstache}}: In: {}\n"
+            "{{dockerstache}}: Out: {}\n"
+            "{{dockerstache}}: Context: {}\n"
+        ).format(opts.input, opts.output, opts.context)
+    )
+    with open(opts.context, 'r') as handle:
+        context = json.load(handle)
+
+    process_templates(
+        opts.input,
+        opts.output,
+        context
+        )
+
+
+if __name__ == '__main__':
+    main()
