eb01d4fd932684fa6cc0616e8aa4ccfb2c13c05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:51 +0000

    Add package wolverine/0.2.1/wolverine-0.2.1.tar.gz

---
+++ packages/wolverine/wolverine-0.2.1.tar.gz/wolverine-0.2.1/wolverine/__init__.py
@@ -0,0 +1,115 @@
+import asyncio
+from configparser import ConfigParser
+import logging
+import importlib
+import os
+import signal
+import functools
+import types
+
+from .discovery import MicroRegistry
+
+logger = logging.getLogger(__name__)
+
+
+class MicroApp(object):
+    SIG_NAMES = ('SIGINT', 'SIGTERM', 'SIGHUP')
+
+    _default_registry = MicroRegistry
+
+    def __init__(self, loop=None, config_file='settings.ini', mock=False):
+        self.name = ''
+        self.mock = mock
+        self.config_file = config_file
+        default_settings = os.path.join(__path__[0],
+                                        'settings.ini')
+        self.config = ConfigParser()
+        self.config.read(default_settings)
+
+        self.tasks = []
+        self.modules = {}
+        self.loop = loop
+
+        if not self.mock:
+            self.loop = loop or asyncio.get_event_loop()
+
+        def _exit(sig_name):
+            if not self.mock:
+                self.loop.create_task(self.stop(sig_name))
+
+        if not self.mock:
+            for sig in self.SIG_NAMES:
+                self.loop.add_signal_handler(getattr(signal, sig),
+                                             functools.partial(_exit,
+                                                               sig))
+
+    def register_module(self, module):
+        logger.info("registering module" + module.name)
+        module.register_app(self)
+        self.modules[module.name] = module
+
+    def _get_module_class(self, app_var):
+        _path = self.config['APP'][app_var]
+        module_name, class_name = _path.rsplit(".", 1)
+        _module = importlib.import_module(module_name)
+        return getattr(_module, class_name)()
+
+    def _load_registry(self):
+        registry = self._get_module_class('REGISTRY')
+        self.register_module(registry)
+
+    def _load_router(self):
+        router = self._get_module_class('ROUTER')
+        self.register_module(router)
+
+    def run(self):
+        print('')
+        self.config.read(self.config_file)
+        self.name = self.config['APP'].get('NAME', 'Spooky Ash')
+        print('-' * 20)
+        print('   --', self.name, '--')
+        print('-' * 20)
+        print('')
+
+        if self.mock:
+            for key, module in self.modules.items():
+                ret = module.run()
+
+        def _run():
+            self._load_registry()
+            self._load_router()
+
+            for key, module in self.modules.items():
+                ret = module.run()
+
+                if not self.mock:
+                    if isinstance(ret, types.GeneratorType):
+                        yield from ret
+
+        if not self.mock:
+            self.loop.create_task(_run())
+            self.loop.run_forever()
+            logger.info('closing loop')
+
+            try:
+                self.loop.close()
+            except Exception:
+                logger.error('boom', exc_info=True)
+
+    def stop(self, sig_name):
+        if sig_name in self.SIG_NAMES:
+            for key, module in self.modules.items():
+                yield from module.stop()
+            tasks = asyncio.Task.all_tasks(self.loop)
+            for task in tasks:
+                try:
+                    task.cancel()
+                except Exception:
+                    logger.error('failed to cancel task', exc_info=True)
+            self.loop.stop()
+
+    def __getattr__(self, item):
+        if item in self.modules.keys():
+            return self.modules[item]
+        else:
+            raise AttributeError('no module named "' + item + '" in the app')
---
+++ packages/wolverine/wolverine-0.2.1.tar.gz/wolverine-0.2.1/wolverine/discovery/__init__.py
@@ -0,0 +1,16 @@
+from wolverine.module import MicroModule
+
+
+class MicroRegistry(MicroModule):
+
+    def register(self, key, value, **options):
+        pass
+
+    def deregister(self, key, **options):
+        pass
+
+    def listen(self, name, **options):
+        pass
+
+    def unwrap(self, data, data_type=None):
+        pass
