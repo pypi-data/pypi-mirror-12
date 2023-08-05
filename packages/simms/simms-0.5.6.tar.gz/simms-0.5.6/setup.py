be417a7fc74b571ec0e6b7ccbc5846e172fc81c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:38 +0000

    Add package simms/0.5.6/simms-0.5.6.tar.gz

---
+++ packages/simms/simms-0.5.6.tar.gz/simms-0.5.6/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+import simms
+
+setup(name="simms",
+    version=simms.__version__,
+    description="Empty MS creation tool",
+    author="Sphesihle Makhathini",
+    author_email="Sphesihle Makhathini <sphemakh@gmail.com>",
+    url="https://github.com/sphemakh/simms",
+    packages=["simms"],
+    requires=["casapy","numpy"],
+    scripts=["simms/bin/" + i for i in os.listdir("simms/bin")], 
+    licence="This program should come with the GNU General Public Licence. "\
+            "If not, find it at http://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html",
+    classifiers=[],
+     )
