19002c1ba762afbca3eab35cf7ceb435b50da5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:26 +0000

    Add package pyvagrantfile/0.5.5/pyvagrantfile-0.5.5.tar.gz

---
+++ packages/pyvagrantfile/pyvagrantfile-0.5.5.tar.gz/pyvagrantfile-0.5.5/pyvagrantfile/__init__.py
@@ -0,0 +1,44 @@
+import sys
+
+__author__ = 'drews'
+
+PY_VERSION = sys.version_info[:2]
+PY2 = (PY_VERSION[0] == 2)
+PY3 = (PY_VERSION[0] == 3)
+
+class Vagrantfile(object):
+
+    def __init__(self):
+        self.struct = None
+
+    def __getattr__(self, name):
+        return getattr(self.struct, name)
+
+
+class VagrantfileVm(object): pass
+
+class VagrantfileProviderVb(object): pass
+
+class VagrantfileProvisionShell(object): pass
+
+class VagrantfileProvisionChef(object):
+    def __init__(self):
+        self.roles = []
+        self.recipes = []
+
+    def add_recipe(self, new_recipe):
+        self.recipes.append(new_recipe)
+
+    def add_role(self, new_role):
+        self.roles.append(new_role)
+
+class VagrantfileProvisionPuppet(object): pass
+
+class VagrantfileNetworkForwardedPort(object):
+    def __init__(self, guest, host):
+        self.guest = int(guest)
+        self.host = int(host)
+
+class VagrantfileNetworkPrivateNetwork(object):
+    def __init__(self, ip=None):
+        self.ip = ip
---
+++ packages/pyvagrantfile/pyvagrantfile-0.5.5.tar.gz/pyvagrantfile-0.5.5/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+import sys
+
+PY_VERSION = sys.version_info[:2]
+PY2 = (PY_VERSION[0] == 2)
+PY3 = (PY_VERSION[0] == 3)
+
+try:
+    from pypandoc import convert
+
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+requirements = []
+
+if sys.version_info[:2] == (2, 6):
+    requirements.append('ordereddict')
+
+package_version = '0.5.5'
+
+setup(
+    name='pyvagrantfile',
+    packages=find_packages(exclude=['tests*']),
+    setup_requires=['pypandoc'],
+    version=package_version,
+    description='Parser to extract data from a Vagrantfile into a data struct readable by python',
+    long_description=read_md('README.md'),
+    author='Drew J. Sonne',
+    author_email='drew.sonne@gmail.com',
+    url='https://github.com/drewsonne/pyvagrantfile',
+    download_url='https://github.com/drewsonne/pyvagrantfile/archive/{0}.tar.gz'.format(package_version),
+    include_package_data=True,
+    install_requires=requirements,
+    keywords=['vagrant', 'parser', 'ruby'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development',
+        'Topic :: System :: Installation/Setup'
+    ]
+)
