2f99f49665636382fd7004df9f6e4b9688cdfc17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:13 +0000

    Add package devourer/0.2.1/devourer-0.2.1.tar.gz

---
+++ packages/devourer/devourer-0.2.1.tar.gz/devourer-0.2.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='devourer',
+    packages=['devourer'],
+    version='0.2.1',
+    install_requires=[
+        'requests',
+        'six',
+    ],
+    description='Devourer is a generic API client.'
+                'It features an object-oriented, declarative approach to simplify the communication.',
+    author='Bonnier Business Polska / Krzysztof Bujniewicz',
+    author_email='racech@gmail.com',
+    url='https://github.com/bonnierpolska/devourer',
+    download_url='https://github.com/bonnierpolska/devourer/tarball/0.2',
+    keywords=['api', 'generic api', 'api client'],
+    classifiers=[]
+)
