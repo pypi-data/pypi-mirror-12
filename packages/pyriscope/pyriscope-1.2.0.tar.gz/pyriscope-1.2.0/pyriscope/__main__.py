beead18dcd7ae44a41775f2a9c48d109c9380edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:13 +0000

    Add package pyriscope/1.2.0/pyriscope-1.2.0.tar.gz

---
+++ packages/pyriscope/pyriscope-1.2.0.tar.gz/pyriscope-1.2.0/pyriscope/__main__.py
@@ -0,0 +1,15 @@
+import sys
+from pyriscope import processor
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    if len(args) == 1 and args[0] == "__magic__":
+        args = input("Enter args now: ").strip(' ').split(' ')
+
+    processor.process(args)
+
+if __name__ == "__main__":
+    main()
