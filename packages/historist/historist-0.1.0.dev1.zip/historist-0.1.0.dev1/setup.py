a18583e2d7292816bf6ad12d5da5ea571f256e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:50 +0000

    Add package historist/0.1.0.dev1/historist-0.1.0.dev1.zip

---
+++ packages/historist/historist-0.1.0.dev1.zip/historist-0.1.0.dev1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='historist',
+      version='0.1.0.dev1',
+      description='activity logger',
+      url='http://github.com/sina33/historist',
+      author='Sina Saeedi',
+      author_email='sina.saeedi@outlook.com',
+      license='MIT',
+      packages=['historist'],
+      zip_safe=False)
