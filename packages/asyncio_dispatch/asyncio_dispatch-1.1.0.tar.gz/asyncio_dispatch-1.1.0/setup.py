85c239ca659a9ebad12bc6fd99174642e845457b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:28 +0000

    Add package asyncio_dispatch/1.1.0/asyncio_dispatch-1.1.0.tar.gz

---
+++ packages/asyncio_dispatch/asyncio_dispatch-1.1.0.tar.gz/asyncio_dispatch-1.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name="asyncio_dispatch",
+    version="1.1.0",
+    packages=['asyncio_dispatch'],
+    package_data={
+        # Include readme and license
+        '': ['LICENSE.txt', 'README.rst'],
+    },
+
+    # Metadata
+    author="Mike Lenzen",
+    author_email="lenzenmi@gmail.com",
+    description="asyncio_dispatch is a is a signal dispatcher for the asyncio event loop found in Python versions 3.4 and up.",
+    long_description=long_description,
+    keywords="asyncio_dispatch asyncio dispatch signal event",
+    url="https://github.com/lenzenmi/asyncio_dispatch/",
+    classifiers=[
+                 'License :: OSI Approved :: MIT License',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Topic :: Software Development :: Libraries',
+                 'Development Status :: 4 - Beta'
+                 ]
+)
