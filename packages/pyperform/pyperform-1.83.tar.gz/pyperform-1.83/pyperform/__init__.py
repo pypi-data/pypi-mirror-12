0037f19496b813fe373e6eb350124a497a97f9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:23 +0000

    Add package pyperform/1.83/pyperform-1.83.tar.gz

---
+++ packages/pyperform/pyperform-1.83.tar.gz/pyperform-1.83/pyperform/__init__.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+
+__version__ = '1.83'
+
+
+from pyperform.benchmark import Benchmark
+from .comparisonbenchmark import ComparisonBenchmark
+from .benchmarkedclass import BenchmarkedClass
+from .benchmarkedfunction import BenchmarkedFunction
+from .thread import Thread
+from .timer import timer
+from .exceptions import ValidationError
+
+
+def enable():
+    """
+    Enable all benchmarking.
+    """
+    Benchmark.enable = True
+    ComparisonBenchmark.enable = True
+    BenchmarkedFunction.enable = True
+    BenchmarkedClass.enable = True
+
+
+def disable():
+    """
+    Disable all benchmarking.
+    """
+    Benchmark.enable = False
+    ComparisonBenchmark.enable = False
+    BenchmarkedFunction.enable = False
+    BenchmarkedClass.enable = False
+
+
+
+
+
+
+
+
+
+
+
