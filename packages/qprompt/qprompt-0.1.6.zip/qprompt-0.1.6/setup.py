022372546a053a0b1be55450af81513913309112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:02 +0000

    Add package qprompt/0.1.6/qprompt-0.1.6.zip

---
+++ packages/qprompt/qprompt-0.1.6.zip/qprompt-0.1.6/setup.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+from setuptools import setup, find_packages
+
+setup(
+    name = "qprompt",
+    version = "0.1.6",
+    author = "Jeff Rimko",
+    author_email = "jeffrimko@gmail.com",
+    description = "Library for quick CLI prompts.",
+    license = "MIT",
+    keywords = "cli",
+    url = "https://github.com/jeffrimko/Qprompt",
+    py_modules=["qprompt"],
+    long_description=open("../README.rst").read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
