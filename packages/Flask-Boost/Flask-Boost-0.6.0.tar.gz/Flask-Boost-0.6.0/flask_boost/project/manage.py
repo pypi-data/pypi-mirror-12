4ec3077a6ce71a33f2dd907fde20a091cf2b9079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:46 +0000

    Add package Flask-Boost/0.6.0/Flask-Boost-0.6.0.tar.gz

---
+++ packages/Flask-Boost/Flask-Boost-0.6.0.tar.gz/Flask-Boost-0.6.0/flask_boost/project/manage.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+import os
+import glob2
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+from application import create_app
+from application.models import db
+
+# Used by app debug & livereload
+PORT = 5000
+
+app = create_app()
+manager = Manager(app)
+
+# db migrate commands
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def run():
+    """Run app."""
+    app.run(port=PORT)
+
+
+@manager.command
+def live():
+    """Run livereload server"""
+    from livereload import Server
+
+    server = Server(app)
+
+    # html
+    for filepath in glob2.glob("application/templates/**/*.html"):
+        server.watch(filepath)
+    # css
+    for filepath in glob2.glob("application/static/css/**/*.css"):
+        server.watch(filepath)
+    # js
+    for filepath in glob2.glob("application/static/js/**/*.js"):
+        server.watch(filepath)
+    # image
+    for filepath in glob2.glob("application/static/image/**/*.*"):
+        server.watch(filepath)
+
+    server.serve(port=PORT)
+
+
+@manager.command
+def build():
+    """Use FIS to compile assets."""
+    os.chdir('application')
+    os.system('fis release -d ../output -opmD')
+
+
+@manager.command
+def createdb():
+    """Create database."""
+    db.create_all()
+
+
+if __name__ == "__main__":
+    manager.run()
