6663b7f703de69875127aae92e9a1a0f89bf5006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package cfblog/0.1.2/cfblog-0.1.2.tar.gz

---
+++ packages/cfblog/cfblog-0.1.2.tar.gz/cfblog-0.1.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except Exception:
+    long_description = ""
+
+setup(
+    name="cfblog",
+    description="Awesome CMS for Django",
+    long_description=long_description,
+
+    version="0.1.2",
+    author='Vinay Karanam',
+    author_email="vinayinvicible@gmail.com",
+
+    url='https://github.com/Coverfox/cfblog/',
+    license='BSD',
+
+    install_requires=[
+        "fhurl>=0.1.7", "smarturls", "six", "Django>=1.3",
+        "dj-database-url",
+    ],
+    packages=['cfblog', 'cfblog.migrations'],
+
+    zip_safe=True,
+)
