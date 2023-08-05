fbad1586a3e333dfb355cc82381f8181343ea46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:55 +0000

    Add package nesterjoeyzy/1.1.0/nesterjoeyzy-1.1.0.zip

---
+++ packages/nesterjoeyzy/nesterjoeyzy-1.1.0.zip/nesterjoeyzy-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name          = 'nesterjoeyzy',
+        version       = '1.1.0',
+        py_modules    =['nesterjoeyzy'],
+        author        = 'fuckie',
+        author_email  = 'windwhisperzy@gmail.com',
+        url           = '000',
+        description   = 'A simple printer of nested lists',
+      )
