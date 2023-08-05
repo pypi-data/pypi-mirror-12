9c1bf1d613e4d46654eb0f612ed29695817578a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:31 +0000

    Add package metanumber/1.0/metanumber-1.0.tar.gz

---
+++ packages/metanumber/metanumber-1.0.tar.gz/metanumber-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'metanumber',
+  packages = ['metanumber'],
+  version = '1.0',
+  description = 'A python library for accessing the Metanumber API',
+  author = 'Dylan Shields',
+  author_email = 'shielddt@email.sc.edu',
+  url = 'https://github.com/dshields1/metanumber',
+  download_url = 'https://github.com/dshields1/metanumber/tarball/0.1',
+  keywords = ['metanumber', 'api'],
+  classifiers = [],
+)
