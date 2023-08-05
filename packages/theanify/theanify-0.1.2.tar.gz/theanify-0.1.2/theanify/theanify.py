b5b673c8feff1d2131e7631d3cd04d6b7c923b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:04 +0000

    Add package theanify/0.1.2/theanify-0.1.2.tar.gz

---
+++ packages/theanify/theanify-0.1.2.tar.gz/theanify-0.1.2/theanify/theanify.py
@@ -0,0 +1,75 @@
+import logging
+
+import theano
+
+def theanify(*args, **kwargs):
+    def func(f):
+        return PreTheano(f, args, **kwargs)
+    return func
+
+class PreTheano(object):
+    def __init__(self, f, args, updates=None):
+        self.f = f
+        self.args = args
+        self.obj = None
+        self.updates = updates
+
+    def set_instance(self, obj):
+        self.obj = obj
+
+    def get_var(self):
+        return self(*self.args)
+
+    def __call__(self, *args):
+        assert self.obj is not None, "Make sure you call the Theanifiable constructor"
+        return self.f(self.obj, *args)
+
+class Theanifiable(object):
+
+    def __init__(self):
+        for name in dir(self):
+            obj = getattr(self, name)
+            if isinstance(obj, PreTheano):
+                obj.set_instance(self)
+
+    def compile_method(self, name, args=None):
+        obj = getattr(self, name)
+        args = args or obj.args
+        if isinstance(obj, PreTheano):
+            logging.debug("Compiling <%s> method..." % name)
+            updates = None
+            if obj.updates:
+                updates = getattr(self, obj.updates)(*obj.args)
+            var = obj(*args)
+            compiled = theano.function(
+                obj.args,
+                var,
+                updates=updates,
+                allow_input_downcast=True
+            )
+            setattr(self, name, compiled)
+            setattr(self, "_%s" % name, obj)
+
+    def compile(self):
+        logging.info("Compiling <%s> object..." % self.__class__.__name__)
+        attrs = {}
+        for name in dir(self):
+            obj = getattr(self, name)
+            if isinstance(obj, PreTheano):
+                logging.debug("Compiling <%s> method..." % name)
+                updates = None
+                if obj.updates:
+                    updates = getattr(self, obj.updates)(*obj.args)
+                var = obj(*obj.args)
+                compiled = theano.function(
+                    obj.args,
+                    var,
+                    updates=updates,
+                    allow_input_downcast=True
+                )
+                attrs[name] = compiled
+                attrs["_%s" % name] = obj
+        for attr, obj in attrs.items():
+            setattr(self, attr, obj)
+        logging.info("Done compiling <%s> object." % self.__class__.__name__)
+        return self
