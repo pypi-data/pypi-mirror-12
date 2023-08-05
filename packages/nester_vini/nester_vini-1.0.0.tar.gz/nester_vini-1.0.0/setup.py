5c944a882b556a4696ec430a6722e23812204322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:20 +0000

    Add package nester_vini/1.0.0/nester_vini-1.0.0.tar.gz

---
+++ packages/nester_vini/nester_vini-1.0.0.tar.gz/nester_vini-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_vini',
+    version = '1.0.0',
+    py_modules  = ['nester_vini'],
+    author = 'vinitha',
+    author_email = 'vinithabm.13@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
+    
