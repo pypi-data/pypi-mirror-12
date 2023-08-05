e8c27097945da13c430de66708f8ef770b9238f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package Neighborhoodize/0.9.2/Neighborhoodize-0.9.2.tar.gz

---
+++ packages/Neighborhoodize/Neighborhoodize-0.9.2.tar.gz/Neighborhoodize-0.9.2/neighborhoodize/__init__.py
@@ -0,0 +1,20 @@
+"""A module for taking latitude, longitude pairs and returning which
+neighborhood they're located in. Currently Chicago only."""
+
+# imports
+import sys
+
+import city_of_chicago
+import nyc
+import zillow
+
+from common import NeighborhoodMap, Neighborhood
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    STATUS_CODE = main()
+    sys.exit(STATUS_CODE)
