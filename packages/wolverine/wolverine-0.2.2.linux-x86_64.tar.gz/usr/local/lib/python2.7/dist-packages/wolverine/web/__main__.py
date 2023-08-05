4e8b1d3f73a6d8da2afd691a17f731050ae3d321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:31 +0000

    Add package wolverine/0.2.2/wolverine-0.2.2.linux-x86_64.tar.gz

---
+++ packages/wolverine/wolverine-0.2.2.linux-x86_64.tar.gz/usr/local/lib/python2.7/dist-packages/wolverine/web/__main__.py
@@ -0,0 +1,41 @@
+import asyncio
+import logging
+from optparse import OptionParser
+import os
+from wolverine import MicroApp
+from wolverine.gateway import GatewayModule
+from wolverine.web import WebModule
+
+LOG_FORMAT = "%(asctime)s %(levelname)s" \
+             " %(name)s:%(lineno)s %(message)s"
+
+
+def web():
+
+    log_level = logging.INFO
+    usage = "usage: %prog [options] arg"
+    parser = OptionParser(usage)
+    parser.add_option("-l", "--log-level", dest="log_level",
+                      help="log level one of "
+                           "(DEBUG, INFO, WARNING, ERROR, Critical)",
+                      default="ERROR")
+    (options, args) = parser.parse_args()
+    if options.log_level:
+        log_level = getattr(logging, options.log_level.upper())
+    logging.basicConfig(level=log_level,
+                        format=LOG_FORMAT)
+
+    loop = asyncio.get_event_loop()
+    import wolverine.web
+    default_settings = os.path.join(wolverine.web.__path__[0],
+                                    'settings.ini')
+    app = MicroApp(loop=loop, config_file=default_settings)
+    gateway = GatewayModule()
+    app.register_module(gateway)
+    web_console = WebModule()
+    app.register_module(web_console)
+    app.run()
+
+
+if __name__ == '__main__':
+    web()
