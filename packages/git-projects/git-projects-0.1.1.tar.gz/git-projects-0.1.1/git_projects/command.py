b9e2f48a52bfcc502cc12a13d031c7e7553ccaba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:49 +0000

    Add package git-projects/0.1.1/git-projects-0.1.1.tar.gz

---
+++ packages/git-projects/git-projects-0.1.1.tar.gz/git-projects-0.1.1/git_projects/command.py
@@ -0,0 +1,47 @@
+from argparse import ArgumentParser
+from subprocess import (Popen, PIPE)
+
+
+PROJECT_PREFIX = '@'
+
+
+class GitError(Exception):
+    pass
+
+
+def parse_args():
+    parser = ArgumentParser(description='git-projects')
+    parser.add_argument(PROJECT_PREFIX, nargs='+', help='project name')
+    return parser.parse_known_args()
+
+
+def parse_command():
+    args, ignored_args = parse_args()
+    args = args.__dict__.get(PROJECT_PREFIX)
+
+    projects = list()
+    git_args = list()
+
+    for arg in args:
+        if str.startswith(arg, PROJECT_PREFIX):
+            projects.append(arg)
+        else:
+            git_args.append(arg)
+
+    for i in range(len(projects)):
+        projects[i] = projects[i][1:]
+
+    return projects, git_args + ignored_args
+
+
+def git(target, *args):
+    """
+    Git command opener wrapper.
+    """
+    cmd = ['git'] + list(args)
+    popen = Popen(cmd, close_fds=True, cwd=target,
+                  stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    out, err = popen.communicate()
+    if popen.returncode != 0:
+        raise GitError(err.decode('utf-8'))
+    return out.decode('utf-8')
