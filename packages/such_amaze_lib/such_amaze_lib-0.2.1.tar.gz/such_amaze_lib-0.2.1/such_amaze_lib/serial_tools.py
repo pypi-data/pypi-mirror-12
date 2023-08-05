d13db20c9ee52bfe313c1d3301d232724055ddac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package such_amaze_lib/0.2.1/such_amaze_lib-0.2.1.tar.gz

---
+++ packages/such_amaze_lib/such_amaze_lib-0.2.1.tar.gz/such_amaze_lib-0.2.1/such_amaze_lib/serial_tools.py
@@ -0,0 +1,30 @@
+import glob
+import sys
+
+import serial
+
+
+def get_available_usb():
+    """
+        Return a list of available serial USB port, works on win, macosx and linux
+
+    :return: list of available serial USB port
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM{}'.format(n) for n in range(1, 256)]
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.u*')
+    elif sys.platform.startswith('linux'):
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    else:
+        msg = '{} is not a supported platform'.format(sys.platform)
+        raise EnvironmentError(msg)
+    available_usb = list()
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            available_usb.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return available_usb
