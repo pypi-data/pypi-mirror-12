39fbf4994b85dd911d0654d561b7f0408b107379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:29 +0000

    Add package rtv/1.6.1/rtv-1.6.1.tar.gz

---
+++ packages/rtv/rtv-1.6.1.tar.gz/rtv-1.6.1/rtv/__main__.py
@@ -0,0 +1,91 @@
+import sys
+import locale
+import logging
+
+import praw
+import praw.errors
+import tornado
+from requests import exceptions
+
+from . import config
+from .exceptions import RTVError
+from .curses_helpers import curses_session, LoadScreen
+from .submission import SubmissionPage
+from .subreddit import SubredditPage
+from .docs import AGENT
+from .oauth import OAuthTool
+from .__version__ import __version__
+
+__all__ = []
+_logger = logging.getLogger(__name__)
+
+# Pycharm debugging note:
+# You can use pycharm to debug a curses application by launching rtv in a
+# console window (python -m rtv) and using pycharm to attach to the remote
+# process. On Ubuntu, you may need to allow ptrace permissions by setting
+# ptrace_scope to 0 in /etc/sysctl.d/10-ptrace.conf.
+# http://blog.mellenthin.de/archives/2010/10/18/gdb-attach-fails
+
+
+def main():
+    "Main entry point"
+
+    # Squelch SSL warnings
+    logging.captureWarnings(True)
+    locale.setlocale(locale.LC_ALL, '')
+
+    # Set the terminal title
+    # TODO: Need to clear the title when the program exits
+    title = 'rtv {0}'.format(__version__)
+    sys.stdout.write("\x1b]2;{0}\x07".format(title))
+
+    # Fill in empty arguments with config file values. Parameters explicitly
+    # typed on the command line will take priority over config file params.
+    parser = config.build_parser()
+    args = parser.parse_args()
+
+    local_config = config.load_config()
+    for key, val in local_config.items():
+        if getattr(args, key, None) is None:
+            setattr(args, key, val)
+
+    if args.ascii:
+        config.unicode = False
+    if not args.persistent:
+        config.persistent = False
+    if args.clear_auth:
+        config.clear_refresh_token()
+
+    if args.log:
+        logging.basicConfig(level=logging.DEBUG, filename=args.log)
+    else:
+        logging.root.addHandler(logging.NullHandler())
+
+    try:
+        print('Connecting...')
+        reddit = praw.Reddit(user_agent=AGENT.format(version=__version__))
+        reddit.config.decode_html_entities = False
+        with curses_session() as stdscr:
+            oauth = OAuthTool(reddit, stdscr, LoadScreen(stdscr))
+            if oauth.refresh_token:
+                oauth.authorize()
+
+            if args.link:
+                page = SubmissionPage(stdscr, reddit, oauth, url=args.link)
+                page.loop()
+            subreddit = args.subreddit or 'front'
+            page = SubredditPage(stdscr, reddit, oauth, subreddit)
+            page.loop()
+    except (exceptions.RequestException, praw.errors.PRAWException,
+            RTVError) as e:
+        _logger.exception(e)
+        print('{}: {}'.format(type(e).__name__, e))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Ensure sockets are closed to prevent a ResourceWarning
+        reddit.handler.http.close()
+        # Explicitly close file descriptors opened by Tornado's IOLoop
+        tornado.ioloop.IOLoop.current().close(all_fds=True)
+
+sys.exit(main())
---
+++ packages/rtv/rtv-1.6.1.tar.gz/rtv-1.6.1/rtv/subreddit.py
@@ -0,0 +1,231 @@
+import curses
+import time
+import logging
+import atexit
+
+import requests
+
+from .exceptions import SubredditError, AccountError
+from .page import BasePage, Navigator, BaseController
+from .submission import SubmissionPage
+from .subscription import SubscriptionPage
+from .content import SubredditContent
+from .helpers import open_browser, open_editor
+from .docs import SUBMISSION_FILE
+from .history import load_history, save_history
+from .curses_helpers import (Color, LoadScreen, add_line, get_arrow, get_gold,
+                             show_notification, prompt_input)
+
+__all__ = ['history', 'SubredditController', 'SubredditPage']
+_logger = logging.getLogger(__name__)
+history = load_history()
+
+
+@atexit.register
+def save_links():
+    global history
+    save_history(history)
+
+
+class SubredditController(BaseController):
+    character_map = {}
+
+
+class SubredditPage(BasePage):
+
+    def __init__(self, stdscr, reddit, oauth, name):
+
+        self.controller = SubredditController(self)
+        self.loader = LoadScreen(stdscr)
+        self.oauth = oauth
+
+        content = SubredditContent.from_name(reddit, name, self.loader)
+        super(SubredditPage, self).__init__(stdscr, reddit, content, oauth)
+
+    def loop(self):
+        "Main control loop"
+
+        while True:
+            self.draw()
+            cmd = self.stdscr.getch()
+            self.controller.trigger(cmd)
+
+    @SubredditController.register(curses.KEY_F5, 'r')
+    def refresh_content(self, name=None, order=None):
+        "Re-download all submissions and reset the page index"
+
+        name = name or self.content.name
+        order = order or self.content.order
+
+        # Hack to allow an order specified in the name by prompt_subreddit() to
+        # override the current default
+        if order == 'ignore':
+            order = None
+
+        try:
+            self.content = SubredditContent.from_name(
+                self.reddit, name, self.loader, order=order)
+        except AccountError:
+            show_notification(self.stdscr, ['Not logged in'])
+        except SubredditError:
+            show_notification(self.stdscr, ['Invalid subreddit'])
+        except requests.HTTPError:
+            show_notification(self.stdscr, ['Could not reach subreddit'])
+        else:
+            self.nav = Navigator(self.content.get)
+
+    @SubredditController.register('f')
+    def search_subreddit(self, name=None):
+        "Open a prompt to search the given subreddit"
+
+        name = name or self.content.name
+        prompt = 'Search {}:'.format(name)
+        query = prompt_input(self.stdscr, prompt)
+        if query is None:
+            return
+
+        try:
+            self.content = SubredditContent.from_name(
+                self.reddit, name, self.loader, query=query)
+        except (IndexError, SubredditError):  # if there are no submissions
+            show_notification(self.stdscr, ['No results found'])
+        else:
+            self.nav = Navigator(self.content.get)
+
+    @SubredditController.register('/')
+    def prompt_subreddit(self):
+        "Open a prompt to navigate to a different subreddit"
+        prompt = 'Enter Subreddit: /r/'
+        name = prompt_input(self.stdscr, prompt)
+        if name is not None:
+            self.refresh_content(name=name, order='ignore')
+
+    @SubredditController.register(curses.KEY_RIGHT, 'l')
+    def open_submission(self):
+        "Select the current submission to view posts"
+
+        data = self.content.get(self.nav.absolute_index)
+        page = SubmissionPage(self.stdscr, self.reddit, self.oauth,
+                              url=data['permalink'])
+        page.loop()
+        if data['url_type'] == 'selfpost':
+            global history
+            history.add(data['url_full'])
+
+    @SubredditController.register(curses.KEY_ENTER, 10, 'o')
+    def open_link(self):
+        "Open a link with the webbrowser"
+        data = self.content.get(self.nav.absolute_index)
+
+        url = data['url_full']
+        global history
+        history.add(url)
+        if data['url_type'] in ['x-post', 'selfpost']:
+            page = SubmissionPage(self.stdscr, self.reddit, self.oauth,
+                                  url=url)
+            page.loop()
+        else:
+            open_browser(url)
+
+    @SubredditController.register('c')
+    def post_submission(self):
+        "Post a new submission to the given subreddit"
+
+        if not self.reddit.is_oauth_session():
+            show_notification(self.stdscr, ['Not logged in'])
+            return
+
+        # Strips the subreddit to just the name
+        # Make sure it is a valid subreddit for submission
+        subreddit = self.reddit.get_subreddit(self.content.name)
+        sub = str(subreddit).split('/')[2]
+        if '+' in sub or sub in ('all', 'front', 'me'):
+            show_notification(self.stdscr, ['Invalid subreddit'])
+            return
+
+        # Open the submission window
+        submission_info = SUBMISSION_FILE.format(name=subreddit, content='')
+        curses.endwin()
+        submission_text = open_editor(submission_info)
+        curses.doupdate()
+
+        # Validate the submission content
+        if not submission_text:
+            show_notification(self.stdscr, ['Aborted'])
+            return
+        if '\n' not in submission_text:
+            show_notification(self.stdscr, ['No content'])
+            return
+
+        title, content = submission_text.split('\n', 1)
+        with self.safe_call as s:
+            with self.loader(message='Posting', delay=0):
+                post = self.reddit.submit(sub, title, text=content)
+                time.sleep(2.0)
+            # Open the newly created post
+            s.catch = False
+            page = SubmissionPage(self.stdscr, self.reddit, self.oauth,
+                                  submission=post)
+            page.loop()
+            self.refresh_content()
+
+    @SubredditController.register('s')
+    def open_subscriptions(self):
+        "Open user subscriptions page"
+
+        if not self.reddit.is_oauth_session():
+            show_notification(self.stdscr, ['Not logged in'])
+            return
+
+        # Open subscriptions page
+        page = SubscriptionPage(self.stdscr, self.reddit, self.oauth)
+        page.loop()
+
+        # When user has chosen a subreddit in the subscriptions list,
+        # refresh content with the selected subreddit
+        if page.selected_subreddit_data is not None:
+            self.refresh_content(name=page.selected_subreddit_data['name'])
+
+    @staticmethod
+    def draw_item(win, data, inverted=False):
+
+        n_rows, n_cols = win.getmaxyx()
+        n_cols -= 1  # Leave space for the cursor in the first column
+
+        # Handle the case where the window is not large enough to fit the data.
+        valid_rows = range(0, n_rows)
+        offset = 0 if not inverted else -(data['n_rows'] - n_rows)
+
+        n_title = len(data['split_title'])
+        for row, text in enumerate(data['split_title'], start=offset):
+            if row in valid_rows:
+                add_line(win, text, row, 1, curses.A_BOLD)
+
+        row = n_title + offset
+        if row in valid_rows:
+            seen = (data['url_full'] in history)
+            link_color = Color.MAGENTA if seen else Color.BLUE
+            attr = curses.A_UNDERLINE | link_color
+            add_line(win, u'{url}'.format(**data), row, 1, attr)
+
+        row = n_title + offset + 1
+        if row in valid_rows:
+            add_line(win, u'{score} '.format(**data), row, 1)
+            text, attr = get_arrow(data['likes'])
+            add_line(win, text, attr=attr)
+            add_line(win, u' {created} {comments} '.format(**data))
+
+            if data['gold']:
+                text, attr = get_gold()
+                add_line(win, text, attr=attr)
+
+            if data['nsfw']:
+                text, attr = 'NSFW', (curses.A_BOLD | Color.RED)
+                add_line(win, text, attr=attr)
+
+        row = n_title + offset + 2
+        if row in valid_rows:
+            add_line(win, u'{author}'.format(**data), row, 1, curses.A_BOLD)
+            add_line(win, u' /r/{subreddit}'.format(**data), attr=Color.YELLOW)
+            if data['flair']:
+                add_line(win, u' {flair}'.format(**data), attr=Color.RED)
