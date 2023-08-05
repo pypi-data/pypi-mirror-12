bd2e312436518ed5d3b8e0dbdabee83e3cdbf6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:24 +0000

    Add package fio_prestashop/3.4.7.0/fio_prestashop-3.4.7.0.tar.gz

---
+++ packages/fio_prestashop/fio_prestashop-3.4.7.0.tar.gz/fio_prestashop-3.4.7.0/__init__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+"""
+
+from trytond.pool import Pool
+from channel import (
+    Channel, PrestashopExportOrdersWizardView, PrestashopConnectionWizardView,
+    PrestashopExportOrdersWizard, PrestashopConnectionWizard
+)
+from country import (
+    Country, Subdivision, CountryPrestashop, SubdivisionPrestashop
+)
+from currency import CurrencyPrestashop, Currency
+from party import Party, Address, ContactMechanism
+from product import Template, TemplatePrestashop, Product, ProductPrestashop
+from sale import Sale, SaleLine, SiteOrderState
+from lang import Language, SiteLanguage
+
+
+def register():
+    "Register classes with pool"
+    Pool.register(
+        Channel,
+        PrestashopExportOrdersWizardView,
+        PrestashopConnectionWizardView,
+        Country,
+        Subdivision,
+        CountryPrestashop,
+        SubdivisionPrestashop,
+        Currency,
+        CurrencyPrestashop,
+        Language,
+        SiteLanguage,
+        Party,
+        Address,
+        ContactMechanism,
+        Template,
+        TemplatePrestashop,
+        Product,
+        ProductPrestashop,
+        Sale,
+        SaleLine,
+        SiteOrderState,
+        module='prestashop', type_='model')
+    Pool.register(
+        PrestashopExportOrdersWizard,
+        PrestashopConnectionWizard,
+        module='prestashop', type_='wizard')
