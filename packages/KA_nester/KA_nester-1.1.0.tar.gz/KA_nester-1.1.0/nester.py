80c5688b6953a707ec012747ad584220f29b47a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:36 +0000

    Add package KA_nester/1.1.0/KA_nester-1.1.0.tar.gz

---
+++ packages/KA_nester/KA_nester-1.1.0.tar.gz/KA_nester-1.1.0/nester.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+
+### Prints nested lists into lists
+def print_lol(the_list, level):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end=' ')
+            print(each_item)
+
