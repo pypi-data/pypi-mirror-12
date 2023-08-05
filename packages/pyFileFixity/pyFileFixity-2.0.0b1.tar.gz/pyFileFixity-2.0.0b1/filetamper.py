a28881799c6a6c3e95fbe868d7466a6d00615b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:48 +0000

    Add package pyFileFixity/2.0.0b1/pyFileFixity-2.0.0b1.tar.gz

---
+++ packages/pyFileFixity/pyFileFixity-2.0.0b1.tar.gz/pyFileFixity-2.0.0b1/filetamper.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+#
+# Random file characters tamperer
+# Randomly tampers characters in a file in order to test for integrity after.
+# Copyright (C) 2015 Larroque Stephen
+#
+# Licensed under the MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#------------------------------
+#
+# This script is similar to what has been done in a paper, albeit in a dumbed down version (since they could automatically check for corruption measures based on file format): Heydegger, Volker. "Analysing the impact of file formats on data integrity." Archiving Conference. Vol. 2008. No. 1. Society for Imaging Science and Technology, 2008.
+# And another interesting paper by the same author: Heydegger, Volker. "Just one bit in a million: On the effects of data corruption in files." Research and Advanced Technology for Digital Libraries. Springer Berlin Heidelberg, 2009. 315-326.
+# Errors are not evenly spread but rather block level (thus concentrated). Addis, Matthew, et al. "Reliable Audiovisual Archiving Using Unreliable Storage Technology and Services." (2009).
+#
+
+# Import necessary libraries
+import lib.argparse as argparse
+import os, sys, random
+import shlex # for string parsing as argv argument to main(), unnecessary otherwise
+
+# Relative path to absolute
+def fullpath(relpath):
+    if (type(relpath) is object or type(relpath) is file):
+        relpath = relpath.name
+    return os.path.abspath(os.path.expanduser(relpath))
+
+
+def main(argv=None):
+    if argv is None: # if argv is empty, fetch from the commandline
+        argv = sys.argv[1:]
+    elif isinstance(argv, basestring): # else if argv is supplied but it's a simple string, we need to parse it to a list of arguments before handing to argparse or any other argument parser
+        argv = shlex.split(argv) # Parse string just like argv using shlex
+
+    #==== COMMANDLINE PARSER ====
+
+    #== Commandline description
+    desc = '''Random file characters tamperer in Python
+Description: Randomly tampers characters in a file in order to test for integrity after.
+    '''
+    ep = '''NOTE: this script tampers at the character (byte) level, not the bits! Thus the measures you will get here may be different from those you will find in papers (you must divide your probability by 8).'''
+
+    #== Commandline arguments
+    #-- Constructing the parser
+    main_parser = argparse.ArgumentParser(add_help=True, description=desc, epilog=ep, formatter_class=argparse.RawTextHelpFormatter)
+    # Required arguments
+    main_parser.add_argument('-i', '--input', metavar='filetotamper.ext', type=str, nargs=1, required=True,
+                        help='Path to the file to tamper.')
+    main_parser.add_argument('-m', '--mode', metavar='e, erasure, n, noise', type=str, nargs=1, required=True,
+                        help='Tampering mode: erasure or noise?')
+    main_parser.add_argument('-p', '--probability', type=float, nargs=1, required=True,
+                        help='Probability of corruption (float between 0.0 and 1.0)')
+    # Optional arguments
+    main_parser.add_argument('--block_probability', type=float, nargs=1, required=False,
+                        help='Probability of block tampering (between 0.0 and 1.0, do not set it if you want to spread errors evenly, but researchs have shown that errors are rather at block level and not evenly distributed)')
+    main_parser.add_argument('-b', '--burst_length', metavar="startint|endint", type=str, required=False,
+                        help='If specified, this will define the number of consecutive characters that will be corrupted when the corruption probability (--probability) is triggered. Specify a range startint|endint, the burst length will be uniformly sampled over this range.')
+    main_parser.add_argument('--header', type=int, required=False,
+                        help='Only tamper the header of the file')
+
+    #== Parsing the arguments
+    args = main_parser.parse_args(argv) # Storing all arguments to args
+
+    #-- Set variables from arguments
+    filepath = fullpath(args.input[0])
+    mode = args.mode[0]
+    proba = float(args.probability[0])
+    burst_length = args.burst_length
+    if burst_length: burst_length = [int(r) for r in burst_length.split('|')] # split range and convert to int
+
+    block_proba = None
+    if args.block_probability:
+        block_proba = float(args.block_probability[0])
+
+    blocksize = 65536
+    header = args.header
+    if header and header > 0:
+        blocksize = header
+
+    print('Tampering the file %s, please wait...' % os.path.basename(filepath))
+    if not os.path.isfile(filepath):
+        print("File does not exist: %s" % filepath)
+    else:
+        count = 0
+        with open(filepath, "r+b") as fh: # 'r+' allows to read AND overwrite characters. Else any other option won't allow both ('a+' read and append, 'w+' erases the file first then allow to read and write), and 'b' is just for binary because we can open any filetype.
+            if proba >= 1: proba = 1.0/os.fstat(fh.fileno()).st_size * proba # normalizing probability if it's an integer (ie: the number of characters to flip on average)
+            buf = fh.read(blocksize) # We process blocks by blocks because it's a lot faster (IO is still the slowest operation in any computing system)
+            while len(buf) > 0:
+                if not block_proba or (random.random() < block_proba): # If block tampering is enabled, process only if this block is selected by probability
+                    pos2tamper = []
+                    burst_remain = 0 # if burst is enabled and corruption probability is triggered, then we will here store the remaining number of characters to corrupt (the length is uniformly sampled over the range specified in arguments)
+                    # Create the list of bits to tamper (it's a lot more efficient to precompute the list of characters to corrupt, and then modify in the file the characters all at once)
+                    for i in xrange(len(buf)):
+                        if burst_remain > 0 or (random.random() < proba): # Corruption probability: corrupt only if below the bit-flip proba
+                            pos2tamper.append(i) # keep this character's position in the to-be-corrupted list
+                            if burst_remain > 0: # if we're already in a burst, we minus one and continue onto the next character
+                                burst_remain -= 1
+                            elif burst_length: # else we're not in a burst, we create one (triggered by corruption probability: as soon as one character triggers the corruption probability, then we do a burst)
+                                burst_remain = random.randint(burst_length[0], burst_length[1]) - 1 # if burst is enabled, then we randomly (uniformly) pick a random length for the burst between the range specified, and since we already tampered one character, we minus 1
+                    # If there's any character to tamper in the list, we tamper the string
+                    if pos2tamper:
+                        count = count + len(pos2tamper)
+                        #print("Before: %s" % buf)
+                        buf = bytearray(buf) # Strings in Python are immutable, thus we need to convert to a bytearray
+                        for pos in pos2tamper:
+                            if mode == 'e' or mode == 'erasure': # Erase the character (set a null byte)
+                                buf[pos] = 0
+                            elif mode == 'n' or mode == 'noise': # Noising the character (set a random ASCII character)
+                                buf[pos] = random.randint(0,255)
+                        #print("After: %s" % buf)
+                        # Overwriting the string into the file
+                        prevpos = fh.tell() # need to store and place back the seek cursor because after the write, if it's the end of the file, the next read may be buggy (getting characters that are not part of the file)
+                        fh.seek(fh.tell()-len(buf)) # Move the cursor at the beginning of the string we just read
+                        fh.write(buf) # Overwrite it
+                        fh.seek(prevpos) # Restore the previous position after the string
+                # If we only tamper the header, we stop here by setting the buffer to an empty string
+                if header and header > 0:
+                    buf = ''
+                # Else we continue to the next data block
+                else:
+                    # Load the next characters from file
+                    buf = fh.read(blocksize)
+        print("Tampering done: %i characters tampered." % count)
+
+
+# Calling main function if the script is directly called (not imported as a library in another program)
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ packages/pyFileFixity/pyFileFixity-2.0.0b1.tar.gz/pyFileFixity-2.0.0b1/setup.py
@@ -0,0 +1,229 @@
+# See:
+# https://docs.python.org/2/distutils/setupscript.html
+# http://docs.cython.org/src/reference/compilation.html
+# https://docs.python.org/2/extending/building.html
+# http://docs.cython.org/src/userguide/source_files_and_compilation.html
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
+
+import os
+
+# For Makefile parsing
+try:    # pragma: no cover
+    import ConfigParser
+    import StringIO
+except ImportError:    # pragma: no cover
+    # Python 3 compatibility
+    import configparser as ConfigParser
+    import io as StringIO
+import sys, subprocess
+
+
+### Makefile auxiliary functions ###
+
+
+def parse_makefile_aliases(filepath):
+    '''Parse a makefile to find commands and substitute variables.
+    Note that this function is not a total replacement of make, it only
+    parse aliases.
+    Expects a makefile with only aliases and ALWAYS a line return between
+    each command, eg:
+
+    ```
+    all:
+        test
+        install
+    test:
+        nosetest
+    install:
+        python setup.py install
+    ```
+
+    Returns a dict, with a list of commands for each alias.
+    '''
+
+    # -- Parsing the Makefile using ConfigParser
+    # Adding a fake section to make the Makefile a valid Ini file
+    ini_str = '[root]\n' + open(filepath, 'r').read()
+    ini_fp = StringIO.StringIO(ini_str)
+    # Parse it using ConfigParser
+    config = ConfigParser.RawConfigParser()
+    config.readfp(ini_fp)
+    # Fetch the list of aliases
+    aliases = config.options('root')
+
+    # -- Extracting commands for each alias
+    commands = {}
+    for alias in aliases:
+        # strip the first line return, and then split by any line return
+        commands[alias] = config.get('root', alias).lstrip('\n').split('\n')
+
+    # -- Commands substitution
+    # We loop until we can substitute all aliases by their commands
+    # What we do is that we check each command of each alias, and
+    # if there is one command that is to be substituted by an alias,
+    # we try to do it right away, but if it's not possible because
+    # this alias himself points to other aliases, then we stop
+    # and put the current alias back in the queue, which we
+    # will process again later when we have substituted the
+    # other aliases.
+
+    # Create the queue of aliases to process
+    aliases_todo = commands.keys()
+    # Create the dict that will hold the substituted aliases by their full commands
+    commands_new = {}
+    # Loop until we have processed all aliases
+    while aliases_todo:
+        # Pick the first alias in the queue
+        alias = aliases_todo.pop(0)
+        # Create a new entry in the resulting dict
+        commands_new[alias] = []
+        # For each command of this alias
+        for cmd in commands[alias]:
+            # If the alias points to itself, we pass
+            if cmd == alias:
+                pass
+            # If the alias points to a full command, we substitute
+            elif cmd in aliases and cmd in commands_new:
+                # Append all the commands referenced by the alias
+                commands_new[alias].extend(commands_new[cmd])
+            # If the alias points to another alias, we delay,
+            # waiting for the other alias to be substituted first
+            elif cmd in aliases and cmd not in commands_new:
+                # Delete the current entry to avoid other aliases
+                # to reference this one wrongly (as it is empty)
+                del commands_new[alias]
+                # Put back into the queue
+                aliases_todo.append(alias)
+                # Break the loop for the current alias
+                break
+            # Else this is just a full command (no reference to an alias)
+            # so we just append it
+            else:
+                commands_new[alias].append(cmd)
+    commands = commands_new
+    del commands_new
+    # -- Prepending prefix to avoid conflicts with standard setup.py commands
+    #for alias in commands.keys():
+        #commands['make_'+alias] = commands[alias]
+        #del commands[alias]
+    return commands
+
+def execute_makefile_commands(commands, alias, verbose=False):
+    cmds = commands[alias]
+    for cmd in cmds:
+        if verbose: print("Running command: %s" % cmd)
+        subprocess.check_call(cmd.split())
+
+
+### Cython extensions ###
+
+
+try:
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
+
+ext = '.pyx' if USE_CYTHON else '.c'
+
+extensions = [
+                        Extension('lib.brownanrs.cff', [os.path.join('lib', 'brownanrs', 'cff'+ext)]),
+                        Extension('lib.brownanrs.cpolynomial', [os.path.join('lib', 'brownanrs', 'cpolynomial'+ext)]),
+                        Extension('lib.reedsolomon.creedsolo', [os.path.join('lib', 'reedsolomon', 'creedsolo'+ext)]),
+                    ]
+
+if USE_CYTHON: extensions = cythonize(extensions)
+
+
+### Main setup.py config ###
+
+
+# Get version from __init__.py
+__version__ = None
+version_file = os.path.join(os.path.dirname(__file__), '_infos.py')
+for line in open(version_file).readlines():
+    if (line.startswith('version_info') or line.startswith('__version__') or line.startswith('__author__') or line.startswith('__email__')):
+        exec(line.strip())
+
+
+# Executing makefile commands if specified
+if sys.argv[1].lower().strip() == 'make':
+    # Filename of the makefile
+    fpath = 'Makefile'
+    # Parse the makefile, substitute the aliases and extract the commands
+    commands = parse_makefile_aliases(fpath)
+
+    # If no alias (only `python setup.py make`), print the list of aliases
+    if len(sys.argv) < 3 or sys.argv[-1] == '--help':
+        print("Shortcut to use commands via aliases. List of aliases:")
+        for alias in sorted(commands.keys()):
+            print("- "+alias)
+
+    # Else process the commands for this alias
+    else:
+        arg = sys.argv[-1]
+        if arg == 'none': # unit testing, we do nothing (we just checked the makefile parsing)
+            sys.exit(0)
+        elif arg in commands.keys(): # else if the alias exists, we execute its commands
+            execute_makefile_commands(commands, arg, verbose=True)
+        else: # else the alias cannot be found
+            raise Exception("Provided alias cannot be found: make %s" % (arg))
+    # Stop the processing of setup.py here
+    sys.exit(0) # Important to avoid setup.py to spit an error because of the command not being standard
+
+
+# Python module configuration
+setup(
+    name = "pyFileFixity",
+    version=__version__,
+    description='Helping file fixity (long term storage of data) via redundant error correcting codes and hash auditing.',
+    license='MIT License',
+    author=__author__,
+    author_email=__email__,
+    url='https://github.com/lrq3000/pyFileFixity',
+    maintainer=__author__,
+    maintainer_email=__email__,
+    platforms = ["any"],
+    py_modules = [os.path.splitext(f)[0] for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.py')], # list all python files in current folder
+    long_description = open("README.rst", "r").read(),
+    classifiers=[  # Trove classifiers, see https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Archiving',
+        'Topic :: System :: Archiving :: Backup',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Recovery Tools',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+    ],
+    keywords = 'file repair monitor change reed-solomon error correction',
+    ext_modules = extensions,
+    #test_suite='nose.collector',
+    #tests_require=['nose', 'coverage'],
+)
+
+# Use pypandoc to convert the Markdown readme into ReStructuredText for PyPi package generation
+#import pypandoc
+#converts markdown to reStructured
+#z = pypandoc.convert('README','rst',format='markdown')
+#writes converted file
+#with open('README.rst','w') as outfile:
+#    outfile.write(z)
