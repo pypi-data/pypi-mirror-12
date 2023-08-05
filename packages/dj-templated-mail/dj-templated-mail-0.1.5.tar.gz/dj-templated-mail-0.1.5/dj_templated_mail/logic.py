7ef48d021a0e654f007e1a820c10ae826707128e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:09 +0000

    Add package dj-templated-mail/0.1.5/dj-templated-mail-0.1.5.tar.gz

---
+++ packages/dj-templated-mail/dj-templated-mail-0.1.5.tar.gz/dj-templated-mail-0.1.5/dj_templated_mail/logic.py
@@ -0,0 +1,96 @@
+# coding: utf-8
+
+import logging
+
+from django.core.mail import EmailMessage, EmailMultiAlternatives
+from django.template import Context, Template
+
+from .models import EmailTemplate
+
+
+log = logging.getLogger('dj_templated_email')
+
+
+class NoTemplateError(Exception):
+    pass
+
+
+def _render_from_text(text, **context):
+    return Template(text).render(Context(context))
+
+
+def build_templated_mail(
+    template_name,
+    recipients,
+    context=None,
+    sender=None,
+    bcc=None,
+    cc=None,
+    files=None,
+):
+    context = context or {}
+    files = files or []
+
+    t = EmailTemplate.objects.filter(
+        template_name__iexact=template_name
+    ).first()
+    if not t:
+        raise NoTemplateError('template "{}" not found'.format(template_name))
+    subject = _render_from_text(t.subject, **context)
+    text = _render_from_text(t.plain_text, **context)
+
+    if t.html:
+        msg = EmailMultiAlternatives(
+            subject,
+            text,
+            sender,
+            list(recipients),
+            bcc=bcc,
+            cc=cc,
+        )
+        html = _render_from_text(t.html, **context)
+        msg.attach_alternative(html, 'text/html')
+    else:
+        msg = EmailMessage(
+            subject,
+            text,
+            sender,
+            list(recipients),
+            bcc=bcc,
+            cc=cc,
+        )
+
+    for file_data in files:
+        msg.attach(*file_data)
+
+    return msg
+
+
+def send_templated_mail(
+    template_name,
+    recipients,
+    context=None,
+    sender=None,
+    bcc=None,
+    cc=None,
+    files=None,
+    fail_silently=False
+):
+    # remove duplicates from bcc
+    if not bcc:
+        bcc = []
+    if cc:
+        bcc = set(bcc) - set(cc)
+    bcc = list(set(bcc) - set(recipients))
+
+    log.info('sending {} to {} bcc {}'.format(template_name, recipients, bcc))
+    mail = build_templated_mail(
+        template_name=template_name,
+        recipients=recipients,
+        context=context,
+        sender=sender,
+        bcc=bcc,
+        cc=cc,
+        files=files,
+    )
+    mail.send(fail_silently=fail_silently)
