56e64179f665c01d567dfb43341c081eec027dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:02 +0000

    Add package nester_lorraine/1.2.0/nester_lorraine-1.2.0.zip

---
+++ packages/nester_lorraine/nester_lorraine-1.2.0.zip/nester_lorraine-1.2.0/nester.py
@@ -0,0 +1,26 @@
+"""This is the "movies.py" module and it provides one function called printEach()
+    which prints lists that may or may not include nested lists."""
+
+def printEach(movies):
+    
+    """This function takes one positional argument called "movies", which
+        is any Python list (of - posiibly -nested lists). Each data item in the
+        provided list is (recursively) printed to the screen on it's own line."""
+    
+    for each_item in movies:
+        if isinstance(each_item, list):
+            printEach(each_item)
+        else:
+            print(each_item)
+
+    
+def printEach(movies, indent, level=0):
+    for each_item in movies:
+        if isinstance(each_item, list):
+            printEach(each_item,level+1)
+        else:
+            for tab in range(level):
+                if indent:
+                    print("\t", end="")
+            print(each_item)
+
