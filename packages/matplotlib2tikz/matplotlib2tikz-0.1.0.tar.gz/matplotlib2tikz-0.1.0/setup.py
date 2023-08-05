41ed2c98ff76e7a4977121dbd4ddbe1b40c1575e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:19 +0000

    Add package matplotlib2tikz/0.1.0/matplotlib2tikz-0.1.0.tar.gz

---
+++ packages/matplotlib2tikz/matplotlib2tikz-0.1.0.tar.gz/matplotlib2tikz-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+from distutils.core import setup
+import os
+import pypandoc
+
+from matplotlib2tikz import __version__
+
+longdesc = pypandoc.convert(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), 'README.md'),
+    'rst'
+    )
+
+setup(
+    name='matplotlib2tikz',
+    version=__version__,
+    py_modules=['matplotlib2tikz'],
+    url='https://github.com/nschloe/matplotlib2tikz',
+    download_url='https://github.com/nschloe/matplotlib2tikz/downloads',
+    author='Nico Schlömer',
+    author_email='nico.schloemer@gmail.com',
+    requires=['matplotlib (>=1.4.0)', 'numpy'],
+    description='convert matplotlib figures into TikZ/PGFPlots',
+    long_description=longdesc,
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion'
+        ]
+    )
