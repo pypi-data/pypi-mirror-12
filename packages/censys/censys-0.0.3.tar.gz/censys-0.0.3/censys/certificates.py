95e329492b53a039fa09bef5059747870faeb2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:26 +0000

    Add package censys/0.0.3/censys-0.0.3.tar.gz

---
+++ packages/censys/censys-0.0.3.tar.gz/censys-0.0.3/censys/certificates.py
@@ -0,0 +1,42 @@
+import json
+from censys import *
+
+class CensysCertificates(CensysAPIBase):
+
+    def search(self, query, page=1, fields=[]):
+        data = {
+            "query":query,
+            "page":page,
+            "fields":fields
+        }
+        return self._post("search/certificates", data=data)
+
+    def view(self, sha256fp):
+        return self._get("/".join(("view", "certificates", sha256fp)))
+
+    def report(self, query, field, buckets=50):
+        data = {
+            "query":query,
+            "field":field,
+            "buckets":int(buckets)
+        }
+        return self._post("report/certificates", data=data)
+
+
+class CensysCertificatesTests(unittest.TestCase):
+
+    def setUp(self):
+        self._api = CensysCertificates()
+
+    def testGet(self):
+        print json.dumps(self._api.view("fce621c0dc1c666d03d660472f636ce91e66e96460545f0da7eb1a24873e2f70"))
+
+    def testSearch(self):
+        print self._api.search("*")
+
+    def testReport(self):
+        print self._api.report("*", "parsed.subject_key_info.key_algorithm.name")
+
+
+if __name__ == "__main__":
+    unittest.main()
