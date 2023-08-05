a15ac5ae6d7a987d48a4e3ae9390933891461869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:58 +0000

    Add package schema-transformer/0.0.2/schema-transformer-0.0.2.tar.gz

---
+++ packages/schema-transformer/schema-transformer-0.0.2.tar.gz/schema-transformer-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+VERSION = '0.0.2'
+
+install_reqs = [
+    'jsonpointer',
+    'jsonschema',
+    'six'
+]
+
+setup(
+    name='schema-transformer',
+    packages=['schema_transformer'],
+    version=VERSION,
+    description='A library for doing schema transformations',
+    author='Fabian von Feilitzsch',
+    author_email='fabian@cos.io',
+    url='https://github.com/fabianvf/schema-transformer',
+    download_url='https://github.com/fabianvf/schema-transformer/tarball/{}'.format(VERSION),
+    install_requires=install_reqs
+)
