29383be26d6c28409cd5ec5414700592484b639c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:53 +0000

    Add package corpkit/1.62/corpkit-1.62-py2-none-any.whl

---
+++ packages/corpkit/corpkit-1.62-py2-none-any.whl/dictionaries/taxonomies.py
@@ -0,0 +1,23 @@
+# in here is a list of taxonomies to augment those provided by wordnet.
+
+# add the output name as the dictionary key, 
+# and components in the taxonomy as a list of vals
+
+taxonomies = {'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2'],
+              'concept': ['realisation', 'realisation2']}
