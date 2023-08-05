f5b883b8a0534ae66df992aa018ca0856a6805a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:10 +0000

    Add package payeezy/1.0.8/payeezy-1.0.8.tar.gz

---
+++ packages/payeezy/payeezy-1.0.8.tar.gz/payeezy-1.0.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='payeezy',
+    version='1.0.8',
+    description='Unofficial Python 3 module to process basic(purchase and authorize) transactions with Payeezy',
+    url='https://github.com/lemmetry/payeezy',
+    author='Artem Kisel',
+    author_email='artem.a.kisel@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Office/Business :: Financial :: Point-Of-Sale',
+    ],
+    keywords='Payeezy purchase authorize transaction FirstData',
+    packages=find_packages(),
+    install_requires=[],
+)
