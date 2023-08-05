60f83a8c2d784094437733a0a9174241f1534a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:10 +0000

    Add package gevent_openssl/1.2/gevent_openssl-1.2.zip

---
+++ packages/gevent_openssl/gevent_openssl-1.2.zip/gevent_openssl-1.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='gevent_openssl',
+    version='1.2',
+    description='A gevent wrapper for pyOpenSSL',
+    author='Menno Finlay-Smits',
+    author_email='menno@freshfoo.com',
+    url='https://github.com/mjs/gevent_openssl',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    license='New BSD',
+    platforms='any',
+    install_requires=['pyOpenSSL>=0.11', 'gevent'],
+)
