fe5f201a4f5ef424e562f026942c5b44896be81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:31 +0000

    Add package libtree/1.0.0/libtree-1.0.0.tar.gz

---
+++ packages/libtree/libtree-1.0.0.tar.gz/libtree-1.0.0/libtree/config.sample.py
@@ -0,0 +1,7 @@
+config = {
+    'postgres': {
+        'details': 'dbname=tree user=fkochem',
+        'benchmark_details': 'dbname=benchmark_tree user=fkochem',
+        'test_details': 'dbname=test_tree user=fkochem'
+    }
+}
