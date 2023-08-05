4eed9010f30866b10cba1638365af063264e4252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package distutilazy/0.4.0/distutilazy-0.4.0.tar.gz

---
+++ packages/distutilazy/distutilazy-0.4.0.tar.gz/distutilazy-0.4.0/distutilazy/__init__.py
@@ -0,0 +1,11 @@
+"""
+distutilazy
+-----------
+
+Extra distutils command classes.
+
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "0.4.0"
+__all__ = ("clean", "pyinstaller", "command")
