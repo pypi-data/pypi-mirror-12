c8f18bb459e02513cc9dd3a8958f32c5f7b2afb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:10 +0000

    Add package jadi/1.0.3/jadi-1.0.3.tar.gz

---
+++ packages/jadi/jadi-1.0.3.tar.gz/jadi-1.0.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(
+    name='jadi',
+    version='1.0.3',
+    install_requires=[],
+    description='Minimalistic IoC for Python',
+    author='Eugeny Pankov',
+    author_email='e@ajenti.org',
+    url='https://github.com/ajenti/jadi',
+    packages=['jadi'],
+)
