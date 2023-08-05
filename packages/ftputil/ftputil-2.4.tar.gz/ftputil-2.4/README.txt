23260fb40a5329b69b7104a0ccbaa3059587049f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:06 +0000

    Add package ftputil/2.4/ftputil-2.4.tar.gz

---
+++ packages/ftputil/ftputil-2.4.tar.gz/ftputil-2.4/README.txt
@@ -0,0 +1,113 @@
+ftputil
+=======
+
+Purpose
+-------
+
+ftputil is a high-level FTP client library for the Python programming
+language. ftputil implements a virtual file system for accessing FTP
+servers, that is, it can generate file-like objects for remote files.
+The library supports many functions similar to those in the os,
+os.path and shutil modules. ftputil has convenience functions for
+conditional uploads and downloads, and handles FTP clients and servers
+in different timezones.
+
+What's new?
+-----------
+
+ftputil 2.4 adds a ``chmod`` method to the ``FTPHost`` class, similar
+to ``os.chmod``.
+
+There's a new exception ``CommandNotImplementedError``, derived from
+``PermanentError``, to denote commands not implemented by the FTP
+server or disabled by its administrator.
+
+Using the ``xreadlines`` method of FTP file objects causes a warning
+through Python's warnings framework.
+
+Upgrading is recommended.
+
+Incompatibility notice
+----------------------
+
+Both the ``xreadlines`` method and the long-deprecated direct access
+of exceptions via the ``ftputil`` module (as in
+``ftputil.PermanentError``) will be removed in ftputil *2.5*. Starting
+with that version, exception classes will only be accessible via the
+``ftp_error`` module.
+
+The distribution contains a small tool ``find_deprecated_code.py`` to
+scan a directory for the deprecated uses. Invoke the program with the
+``--help`` option to see a description.
+
+Documentation
+-------------
+
+The documentation for ftputil can be found in the file ftputil.txt
+(reStructuredText format) or ftputil.html (recommended, generated
+from ftputil.txt).
+
+Prerequisites
+-------------
+
+To use ftputil, you need Python, at least version 2.3. Python is a
+programming language, available from http://www.python.org for free.
+
+Installation
+------------
+
+- *If you have an older version of ftputil installed, delete it or move
+  it somewhere else, so that it doesn't conflict with the new version!*
+
+- Unpack the archive file containing the distribution files. If you
+  had an hypothetical ftputil version 1.2, you would type at the shell
+  prompt:
+
+    tar xzf ftputil-1.2.tar.gz
+
+  However, if you read this, you probably unpacked the archive already. ;-)
+
+- Make the directory to where the files were unpacked your current directory.
+  Consider that after unpacking, you have a directory ftputil-1.2. Make it
+  the current directory with
+
+    cd ftputil-1.2
+
+- Type
+
+    python setup.py install
+
+  at the shell prompt. On Unix/Linux, you have to be root to perform the
+  installation. Likewise, you have to be logged in as administrator if you
+  install on Windows.
+
+  If you want to customize the installation paths, please read
+  http://docs.python.org/inst/inst.html .
+
+If you have easy_install installed, you can install the current
+version of ftputil directly from the Python Package Index (PyPI)
+without downloading the package explicitly.
+
+- Just type
+
+    easy_install ftputil
+
+  on the command line. You'll probably need root/administrator
+  privileges to do that (see above).
+
+License
+-------
+
+ftputil is Open Source Software. It is distributed under the
+new/modified/revised BSD license (see
+http://www.opensource.org/licenses/bsd-license.html ).
+
+Authors
+-------
+
+Stefan Schwarzer <sschwarzer@sschwarzer.net>
+
+Evan Prodromou <evan@bad.dynu.ca> (lrucache module)
+
+Please provide feedback! It's surely appreciated. :-)
+
---
+++ packages/ftputil/ftputil-2.4.tar.gz/ftputil-2.4/ftp_error.py
@@ -0,0 +1,147 @@
+# Copyright (C) 2003-2008, Stefan Schwarzer <sschwarzer@sschwarzer.net>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# - Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# - Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# - Neither the name of the above author nor the names of the
+#   contributors to the software may be used to endorse or promote
+#   products derived from this software without specific prior written
+#   permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+ftp_error.py - exception classes and wrappers
+"""
+
+# $Id: ftp_error.py 754 2008-12-16 16:03:39Z schwa $
+
+# "Too many ancestors"
+# pylint: disable-msg = R0901
+
+import ftplib
+import sys
+
+import ftputil_version
+
+
+class FTPError(Exception):
+    """General error class."""
+
+    def __init__(self, ftp_exception):
+        super(FTPError, self).__init__(ftp_exception)
+        # `message` is set by the base class
+        self.strerror = self.message
+        try:
+            self.errno = int(self.strerror[:3])
+        except (TypeError, IndexError, ValueError):
+            self.errno = None
+        self.filename = None
+
+    def __str__(self):
+        return "%s\nDebugging info: %s" % \
+               (self.strerror, ftputil_version.version_info)
+
+# internal errors are those that have more to do with the inner
+#  workings of ftputil than with errors on the server side
+class InternalError(FTPError):
+    """Internal error."""
+    pass
+
+class RootDirError(InternalError):
+    """Raised for generic stat calls on the remote root directory."""
+    pass
+
+class InaccessibleLoginDirError(InternalError):
+    """May be raised if the login directory isn't accessible."""
+    pass
+
+class TimeShiftError(InternalError):
+    """Raised for invalid time shift values."""
+    pass
+
+class ParserError(InternalError):
+    """Raised if a line of a remote directory can't be parsed."""
+    pass
+
+# currently not used
+class KeepAliveError(InternalError):
+    """Raised if the keep-alive feature failed."""
+    pass
+
+class FTPOSError(FTPError, OSError):
+    """Generic FTP error related to `OSError`."""
+    pass
+
+class TemporaryError(FTPOSError):
+    """Raised for temporary FTP errors (4xx)."""
+    pass
+
+class PermanentError(FTPOSError):
+    """Raised for permanent FTP errors (5xx)."""
+    pass
+
+class CommandNotImplementedError(PermanentError):
+    """Raised if the server doesn't implement a certain feature (502)."""
+    pass
+
+# currently not used
+class SyncError(PermanentError):
+    """Raised for problems specific to syncing directories."""
+    pass
+
+#XXX Do you know better names for `_try_with_oserror` and
+#    `_try_with_ioerror`?
+def _try_with_oserror(callee, *args, **kwargs):
+    """
+    Try the callee with the given arguments and map resulting
+    exceptions from `ftplib.all_errors` to `FTPOSError` and its
+    derived classes.
+    """
+    try:
+        return callee(*args, **kwargs)
+    except ftplib.error_temp, obj:
+        raise TemporaryError(obj)
+    except ftplib.error_perm, obj:
+        if str(obj).startswith("502"):
+            raise CommandNotImplementedError(obj)
+        else:
+            raise PermanentError(obj)
+    except ftplib.all_errors:
+        ftp_error = sys.exc_info()[1]
+        raise FTPOSError(ftp_error)
+
+class FTPIOError(FTPError, IOError):
+    """Generic FTP error related to `IOError`."""
+    pass
+
+def _try_with_ioerror(callee, *args, **kwargs):
+    """
+    Try the callee with the given arguments and map resulting
+    exceptions from `ftplib.all_errors` to `FTPIOError`.
+    """
+    try:
+        return callee(*args, **kwargs)
+    except ftplib.all_errors:
+        ftp_error = sys.exc_info()[1]
+        raise FTPIOError(ftp_error)
+
