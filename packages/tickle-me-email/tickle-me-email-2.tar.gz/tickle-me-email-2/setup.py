783828d0c57f2e60f1b8bf8a9b09e37629b3318c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:30 +0000

    Add package tickle-me-email/2/tickle-me-email-2.tar.gz

---
+++ packages/tickle-me-email/tickle-me-email-2.tar.gz/tickle-me-email-2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tickle-me-email',
+    version=2,
+    url='https://chris-lamb.co.uk/projects/tickle-me-email',
+    author='Chris Lamb',
+    author_email='chris@chris-lamb.co.uk',
+    scripts=(
+        'tickle-me-email',
+    ),
+)
