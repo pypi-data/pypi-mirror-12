447896f4ceaaba99c6b3edff416ce1cf5453c538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:48 +0000

    Add package thermostat/0.2.0/thermostat-0.2.0-py2.py3-none-any.whl

---
+++ packages/thermostat/thermostat-0.2.0-py2.py3-none-any.whl/thermostat/regression.py
@@ -0,0 +1,30 @@
+import numpy as np
+from numpy.linalg import lstsq
+
+def runtime_regression(daily_runtime, daily_demand):
+    """
+    Least squares regession of runtime against a measure of demand.
+
+    Parameters
+    ----------
+    hourly_runtime : pd.Series with pd.DatetimeIndex
+        Runtimes for a particular heating or cooling season.
+    daily_demand : pd.Series with pd.DatetimeIndex
+        A daily demand measure for each day in the heating or cooling season.
+
+    Returns
+    -------
+    slope : float
+        The slope parameter found by the regression to minimize sq error
+    intercept : float
+        The intercept parameter found by the regression to minimize sq error
+    mean_sq_err : float
+        The mean squared error of the regession.
+    """
+    x = daily_demand.values[:,np.newaxis]
+    y = daily_runtime
+    results = lstsq(x, y) # model: y = a*x
+    slope = results[0][0]
+    mean_sq_err = results[1][0] / y.shape[0] # convert from sum sq err
+    return slope, mean_sq_err
+
