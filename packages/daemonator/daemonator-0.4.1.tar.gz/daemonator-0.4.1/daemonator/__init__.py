dfa6e72c30e0a133bfbc2906468f9512b89ed60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:58 +0000

    Add package daemonator/0.4.1/daemonator-0.4.1.tar.gz

---
+++ packages/daemonator/daemonator-0.4.1.tar.gz/daemonator-0.4.1/daemonator/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'Flávio Cardoso Pontes'
+__author_email__ = '<flaviopontes@acerp.org.br>'
+__copyright__ = 'Copyright © 2015 Associação de Comunicação Educativa Roquette Pinto - ACERP'
+
+__version_info__ = (0, 4, 1)
+__version__ = '.'.join(map(str, __version_info__))
+__package__ = 'daemonator'
