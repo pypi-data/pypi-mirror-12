99155900b9800c67b1ca9961a9385978bd289570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:20 +0000

    Add package incuna-groups/1.0.3/incuna-groups-1.0.3.tar.gz

---
+++ packages/incuna-groups/incuna-groups-1.0.3.tar.gz/incuna-groups-1.0.3/groups/apps.py
@@ -0,0 +1,17 @@
+from django.apps import AppConfig
+
+
+class GroupsConfig(AppConfig):
+    """
+    Configuration for `groups`.
+
+    Provides:
+    * default_within_days - a default parameter for the `within_days` methods on some
+      of the model managers, which return items that were posted or posted to within
+      that time period.
+    """
+    name = 'groups'
+    default_within_days = 7
+
+    new_comment_subject = 'New comment on {discussion}'
+    new_discussion_subject = 'New discussion in {group}'
