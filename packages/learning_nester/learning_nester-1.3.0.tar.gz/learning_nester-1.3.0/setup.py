ba9174d68f6416d079e11f0d068c2e267f3fd90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:06 +0000

    Add package learning_nester/1.3.0/learning_nester-1.3.0.tar.gz

---
+++ packages/learning_nester/learning_nester-1.3.0.tar.gz/learning_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'learning_nester',
+	version = '1.3.0',
+	py_modules = ['learning_nester'],
+	author = 'floyd',
+	author_email = 'jefferson@conz.com.br',
+	url = 'http://www.conz.com.br',
+	description = 'simple printer of nested lists'
+)
