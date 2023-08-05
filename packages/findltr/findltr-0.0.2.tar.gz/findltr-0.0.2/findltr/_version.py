595a24540520761af64050b12fdc3cb10c13f329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:26 +0000

    Add package findltr/0.0.2/findltr-0.0.2.tar.gz

---
+++ packages/findltr/findltr-0.0.2.tar.gz/findltr-0.0.2/findltr/_version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: maybelinot
+# @Email: edik.trott@yandex.ru
+# @Date:   2015-09-12 14:46:59
+# @Last Modified by:   maybelinot
+# @Last Modified time: 2015-10-26 12:24:38
+
+from __future__ import unicode_literals, absolute_import
+
+version_info = ('0', '0', '2')
+__version__ = '.'.join(version_info[0:3])  # + '-' + version_info[3]
