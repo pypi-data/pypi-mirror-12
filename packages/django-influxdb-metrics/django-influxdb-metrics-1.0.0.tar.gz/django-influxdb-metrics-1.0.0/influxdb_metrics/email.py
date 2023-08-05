e39ca67bbb075898c83bd78e6e2c97cee42d6f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:26 +0000

    Add package django-influxdb-metrics/1.0.0/django-influxdb-metrics-1.0.0.tar.gz

---
+++ packages/django-influxdb-metrics/django-influxdb-metrics-1.0.0.tar.gz/django-influxdb-metrics-1.0.0/influxdb_metrics/email.py
@@ -0,0 +1,23 @@
+"""Custom email backends for the influxdb_metrics app."""
+from django.core.mail.backends.smtp import EmailBackend
+from django.conf import settings
+
+from .utils import write_points
+
+
+class InfluxDbEmailBackend(EmailBackend):
+    """
+    Custom email backend that sends the number of sent emails to InfluxDB.
+
+    """
+    def send_messages(self, email_messages):
+        num_sent = super(InfluxDbEmailBackend, self).send_messages(
+            email_messages)
+        if num_sent:
+            data = [{
+                'measurement': 'django_email_sent',
+                'tags': {'host': settings.INFLUXDB_TAGS_HOST, },
+                'fields': {'value': num_sent, },
+            }]
+            write_points(data)
+        return num_sent
