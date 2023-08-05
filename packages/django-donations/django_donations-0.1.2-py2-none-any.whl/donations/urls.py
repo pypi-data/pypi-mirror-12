665a6f678d429a75afd0099ecfa66ebc5cbde592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:16 +0000

    Add package django-donations/0.1.2/django_donations-0.1.2-py2-none-any.whl

---
+++ packages/django-donations/django_donations-0.1.2-py2-none-any.whl/donations/urls.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf.urls import include, url, patterns
+from .views import DonateAPI, VerifyAPI
+
+api_urls = patterns(
+    '',
+    url(r'^donate/$', DonateAPI.as_view(), name="donate"),
+    url(r'^verify/(?P<id>[0-9]+)$', VerifyAPI.as_view(), name="verify"),
+)
+
+view_urls = patterns(
+    '',
+    # url(r'^donate/$', DonateView.as_view(), name="donate"),
+)
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^api/', include(api_urls, namespace='api')),
+    url(r'^', include(view_urls)),
+)
