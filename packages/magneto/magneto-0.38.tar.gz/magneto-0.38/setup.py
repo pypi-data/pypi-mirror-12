c5b6066eb1698ed5e6ad46906d6dc846ce132b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:28 +0000

    Add package magneto/0.38/magneto-0.38.tar.gz

---
+++ packages/magneto/magneto-0.38.tar.gz/magneto-0.38/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='magneto',
+    version='0.38',
+    description='Magneto - Test Automation for Android',
+    author='EverythingMe',
+    author_email='automation@everything.me',
+    url='http://github.com/EverythingMe/magneto',
+    packages=find_packages(),
+    install_requires=[
+        'pytest==2.7.2',
+        'uiautomator==0.1.35',
+        'futures==3.0.3',
+        'IPython==3.2.1',
+        'Click==4.1',
+        'pytest-disable==0.2'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'magneto = magneto.main:main',
+            'imagneto = magneto.imagneto:main'
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing'
+    ]
+)
