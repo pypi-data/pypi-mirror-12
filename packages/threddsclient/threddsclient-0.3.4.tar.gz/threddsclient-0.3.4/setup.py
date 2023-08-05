1d3c1a619b710ff86f5632e62aa9e08731586b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:17 +0000

    Add package threddsclient/0.3.4/threddsclient-0.3.4.tar.gz

---
+++ packages/threddsclient/threddsclient-0.3.4.tar.gz/threddsclient-0.3.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+def changes():
+    with open('CHANGES.rst') as f:
+        return f.read()
+
+reqs = [line.strip() for line in open('requirements.txt')]
+
+setup(name             = 'threddsclient',
+      version          = '0.3.4',
+      description      = 'Thredds catalog client',
+      long_description = readme() + '/n/n' + changes(),
+      author	       = 'Birdhouse',
+      email            = '',
+      license          = 'Apache 2.0',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe         = False,
+      install_requires = reqs,
+      )
