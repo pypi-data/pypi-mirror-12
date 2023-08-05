8f56ed0b51d285ca8bc55a994064b323266ef101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:46 +0000

    Add package payeezy/1.0.7/payeezy-1.0.7.tar.gz

---
+++ packages/payeezy/payeezy-1.0.7.tar.gz/payeezy-1.0.7/payeezy/api.py
@@ -0,0 +1,39 @@
+import json
+from payeezy import transactions
+from payeezy.__make_request_bodies import *
+
+
+def process_authorization(transaction_total,
+                          card_type,
+                          card_number,
+                          card_expiry,
+                          card_cvv,
+                          cardholder_name,
+                          merchant_reference=''):
+
+    request_body = make_request_body_for_authorization(transaction_total, card_type, card_number, card_expiry, card_cvv,
+                                                       cardholder_name, merchant_reference)
+    payload = json.dumps(request_body)
+
+    authorization = transactions.Transaction(payload)
+    authorization.run_transaction()
+
+    return authorization
+
+
+def process_purchase(transaction_total,
+                     card_type,
+                     card_number,
+                     card_expiry,
+                     card_cvv,
+                     cardholder_name,
+                     merchant_reference=''):
+
+    request_body = make_request_body_for_purchase(transaction_total, card_type, card_number, card_expiry, card_cvv,
+                                                  cardholder_name, merchant_reference)
+    payload = json.dumps(request_body)
+
+    purchase = transactions.Transaction(payload)
+    purchase.run_transaction()
+
+    return purchase
