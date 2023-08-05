8724a1baa056f66639155d4391090d4fd3a9c2a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:25 +0000

    Add package pybrowserstack-screenshots/0.1/pybrowserstack-screenshots-0.1.tar.gz

---
+++ packages/pybrowserstack-screenshots/pybrowserstack-screenshots-0.1.tar.gz/pybrowserstack-screenshots-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='pybrowserstack-screenshots',
+      version='0.1',
+      description="api wrapper and python client for Browserstack Screenshots, including phantomCSS support",
+      long_description=""" """,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='browserstack screenshots phantomCSS',
+      author='Clark Mckenzie',
+      author_email='clarkmckenzie@googlemail.com',
+      url='http://github.com/cmck/pybrowserstack-screenshots',
+      download_url='https://github.com/cmck/pybrowserstack-screenshots/tarball/0.1',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+            "simplejson>=3.3.2",
+            "requests>=2.2.0",
+            "Pillow>=2.3.0",
+      ],
+)
