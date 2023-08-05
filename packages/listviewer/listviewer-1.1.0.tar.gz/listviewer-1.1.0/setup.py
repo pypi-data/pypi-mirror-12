c8ad8463952747ab2a4a834c630c8b86221e14a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:17 +0000

    Add package listviewer/1.1.0/listviewer-1.1.0.tar.gz

---
+++ packages/listviewer/listviewer-1.1.0.tar.gz/listviewer-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'listviewer',
+	version = '1.1.0',
+	py_modules = ['listviewer'],
+	author = 'saif',
+	author_email = 'saifsmailbox98@yahoo.in',
+	url = 'http://saifurrahman.tk',
+	description = 'A simple printer of nested lists'
+)
