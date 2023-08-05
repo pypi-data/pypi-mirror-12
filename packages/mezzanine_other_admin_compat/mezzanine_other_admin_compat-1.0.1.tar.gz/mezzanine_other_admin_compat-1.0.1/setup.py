92d05630fbc66045295019e98c551b84a1aa7d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:05 +0000

    Add package mezzanine_other_admin_compat/1.0.1/mezzanine_other_admin_compat-1.0.1.tar.gz

---
+++ packages/mezzanine_other_admin_compat/mezzanine_other_admin_compat-1.0.1.tar.gz/mezzanine_other_admin_compat-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mezzanine_other_admin_compat',
+    version='1.0.1',
+    license='BSD',
+    author='Sam Kingston',
+    author_email='sam@sjkwi.com.au',
+    description='Django app to provide compatibility with other admin skins/apps when using Mezzanine',
+    url='https://github.com/sjkingo/mezzanine_other_admin_compat',
+    packages=find_packages(),
+)
