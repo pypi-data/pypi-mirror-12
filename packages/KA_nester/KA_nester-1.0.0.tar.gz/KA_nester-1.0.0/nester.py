b568cb7fa0bec1356f2b6caae828a9e91c3fbdd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:34 +0000

    Add package KA_nester/1.0.0/KA_nester-1.0.0.tar.gz

---
+++ packages/KA_nester/KA_nester-1.0.0.tar.gz/KA_nester-1.0.0/nester.py
@@ -0,0 +1,8 @@
+### Prints nested lists into lists
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
