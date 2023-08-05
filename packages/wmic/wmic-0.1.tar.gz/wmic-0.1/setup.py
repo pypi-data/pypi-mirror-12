99dad5a64ed5ac4bf3fa18fa77901fb60d32a433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:03 +0000

    Add package wmic/0.1/wmic-0.1.tar.gz

---
+++ packages/wmic/wmic-0.1.tar.gz/wmic-0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+
+# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
+# update it when the contents of directories change.
+if os.path.exists('MANIFEST'): os.remove('MANIFEST')
+
+from distutils.core import setup
+import setuptools
+
+setup(name='wmic',
+      version='0.1',
+      description='WMI client',
+      license="MIT",
+      author="David Lundgren",
+      author_email="dlundgren@syberisle.net",
+      url="https://www.github.com/dlundgren/py-wmi-client",
+      include_package_data=True,
+      install_requires=[
+          'natsort',
+          'pyasn1',
+          'pycrypto',
+          'impacket',
+      ],
+      scripts=[os.path.join('scripts', 'wmic')]
+      )
