ebef46cee541e300fa3104c3588389c6182c2d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:02 +0000

    Add package dockerstache/0.0.1/dockerstache-0.0.1.tar.gz

---
+++ packages/dockerstache/dockerstache-0.0.1.tar.gz/dockerstache-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='dockerstache',
+      version='0.0.1',
+      description='Dockerfile mustache templating tools',
+      author='Dave Evans',
+      author_email='evansde77.github@gmail.com',
+      url='https://github.com/evansde77/dockerstache',
+      packages = find_packages('src'),
+      package_dir={'': 'src'},
+      entry_points = {
+        'console_scripts': ['dockerstache=dockerstache.__main__:main'],
+      }
+     )
---
+++ packages/dockerstache/dockerstache-0.0.1.tar.gz/dockerstache-0.0.1/src/dockerstache/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""
+_dockerstache_
+
+util package for rendering docker files from mustache templates
+
+"""
+import sys
+import logging
+import logging.handlers
+
+
+_LOGGER = {
+    "logger": None
+}
+
+
+def get_logger():
+    """
+    _get_logger_
+
+    Get package logger instance
+
+    """
+    if _LOGGER['logger'] is None:
+        _LOGGER['logger']  = setup_logger()
+    return _LOGGER['logger']
+
+
+def setup_logger():
+    logger = logging.getLogger('dockerstache')
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    return logger
