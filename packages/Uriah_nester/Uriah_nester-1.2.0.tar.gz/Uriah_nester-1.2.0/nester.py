ce718ac97d99ed41437708be778c11d8e74ca3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:39 +0000

    Add package Uriah_nester/1.2.0/Uriah_nester-1.2.0.tar.gz

---
+++ packages/Uriah_nester/Uriah_nester-1.2.0.tar.gz/Uriah_nester-1.2.0/nester.py
@@ -0,0 +1,8 @@
+def print_list(test, level=0):
+    for element in test:
+        if isinstance(element, list):
+            print_list(element, level + 1)
+        else:
+            for num in range(level):
+                print("\t", end='');
+            print(element)
---
+++ packages/Uriah_nester/Uriah_nester-1.2.0.tar.gz/Uriah_nester-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+      name = 'Uriah_nester',
+      version = '1.2.0',
+      py_modules = ['nester'],
+      author = 'Jack',
+      author_email = 'gy4567@gmail.com',
+      description = 'test distribution',
+     )
