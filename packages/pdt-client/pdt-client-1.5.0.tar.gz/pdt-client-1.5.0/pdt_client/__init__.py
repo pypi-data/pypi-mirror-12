fe04cfd17d9d779abc5a3b706bf689edcf88deb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:11 +0000

    Add package pdt-client/1.5.0/pdt-client-1.5.0.tar.gz

---
+++ packages/pdt-client/pdt-client-1.5.0.tar.gz/pdt-client-1.5.0/pdt_client/__init__.py
@@ -0,0 +1,9 @@
+"""pdt-client public interface."""
+__version__ = '1.5.0'
+
+try:
+    from .commands import migrate
+
+    __all__ = [migrate.__name__]
+except ImportError:  # pragma: no cover
+    pass
