bd2a6e3f3693af3ee68700a6a22aaf19900c790c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package twitter_bot_utils/0.9.5.1/twitter_bot_utils-0.9.5.1.tar.gz

---
+++ packages/twitter_bot_utils/twitter_bot_utils-0.9.5.1.tar.gz/twitter_bot_utils-0.9.5.1/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup
+
+try:
+    readme = open('./README.rst', 'r').read()
+
+except IOError:
+    try:
+        readme = open('./README.md', 'r').read()
+    except IOError:
+        readme = ''
+
+setup(
+    name='twitter_bot_utils',
+
+    version='0.9.5.1',
+
+    description='Python utilities for twitter bots',
+
+    long_description=readme,
+
+    url='http://github.com/fitnr/twitter_bot_utils',
+
+    author='Neil Freeman',
+
+    author_email='contact@fakeisthenewreal.org',
+
+    license='GPL-3.0',
+
+    packages=['twitter_bot_utils'],
+
+    entry_points={
+        'console_scripts': [
+            'twitter-fave-mentions=twitter_bot_utils.cli:fave_mentions',
+            'twitter-auto-follow=twitter_bot_utils.cli:auto_follow'
+        ],
+    },
+
+    use_2to3=True,
+
+    install_requires=[
+        'tweepy >=3.1.0, <4',
+        'PyYAML==3.11',
+        'argparse>=1.1',
+    ],
+
+    test_suite="tests",
+
+    tests_require=[
+        'setuptools>=17.1',
+        'pbr>=0.11,<1.0',
+        'coverage',
+        'mock'
+    ],
+)
