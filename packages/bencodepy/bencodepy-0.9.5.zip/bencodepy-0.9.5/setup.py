054e0b94f4c4bd8603e17ce8daf2c444aee5dc7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:23 +0000

    Add package bencodepy/0.9.5/bencodepy-0.9.5.zip

---
+++ packages/bencodepy/bencodepy-0.9.5.zip/bencodepy-0.9.5/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='bencodepy',
+    version='0.9.5',
+    packages=['bencodepy'],
+    url='https://github.com/eweast/bencodepy_opti',
+    license='GPLv2',
+    author='Eric Weast',
+    author_email='eweast@hotmail.com',
+    description='Bencode encoder/decoder written in Python 3 under the GPLv2.'
+)
