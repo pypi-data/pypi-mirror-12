8960a9d3389bd18077f11ae3e1a5983cdd71ace6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:42 +0000

    Add package markdown-magic/1.1/markdown-magic-1.1.tar.gz

---
+++ packages/markdown-magic/markdown-magic-1.1.tar.gz/markdown-magic-1.1/setup.py
@@ -0,0 +1,30 @@
+# from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='markdown-magic',
+    version='1.1',
+    maintainer='Radhika Ghosal',
+    maintainer_email='radhikaghosal@gmail.com',
+    url='https://github.com/RadhikaG/markdown-magic',
+    packages=['magic'],
+    install_requires=['markdown>=2.5', 'requests'],
+    license='MIT',
+
+    description='A Python-Markdown plugin for creating memes and finding the best-fitting gifs for your posts.',
+
+    keywords=['markdown','markup','memes','gifs','plugin'],
+
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Markup",
+        ],
+
+    long_description=open('README.rst').read(),
+)
