39f7d56b4232238da5320ad972254802ce704543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:29 +0000

    Add package pycvodes/0.2.0/pycvodes-0.2.0.tar.gz

---
+++ packages/pycvodes/pycvodes-0.2.0.tar.gz/pycvodes-0.2.0/pycvodes/__init__.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, absolute_import
+
+from ._cvodes_numpy import adaptive, predefined, requires_jac, steppers
+from ._util import _check_callable, _check_indexing
+from ._release import __version__
+assert (__version__, requires_jac, steppers)  # silence pyflakes
+
+
+def integrate_adaptive(rhs, jac, y0, x0, xend, dx0, atol, rtol,
+                       check_callable=False, check_indexing=False, **kwargs):
+    """
+    Integrates a system of ordinary differential equations.
+
+    Parameters
+    ----------
+    rhs: callable
+        Function with signature f(t, y, fout) which modifies fout *inplace*.
+    jac: callable
+        Function with signature j(t, y, jmat_out, dfdx_out) which modifies
+        jmat_out and dfdx_out *inplace*.
+    y0: array_like
+        initial values of the dependent variables
+    x0: float
+        initial value of the independent variable
+    xend: float
+        stopping value for the independent variable
+    dx0: float
+        initial step-size
+    atol: float
+        absolute tolerance
+    rtol: float
+        relative tolerance
+    check_callable: bool (default: False)
+        perform signature sanity checks on ``rhs`` and ``jac``
+    check_indexing: bool (default: False)
+        perform item setting sanity checks on ``rhs`` and ``jac``.
+    \*\*kwargs:
+         'method': str
+            One of: 'adams' or 'bdf' (default: 'bdf')
+
+    Returns
+    -------
+    (xout, yout, info):
+        xout: 1-dimensional array of values for the independent variable
+        yout: 2-dimensional array of the dependent variables (axis 1) for
+            values corresponding to xout (axis 0)
+        info: dictionary with information about the integration
+    """
+    # Sanity checks to reduce risk of having a segfault:
+    lband, uband = kwargs.get('lband', None), kwargs.get('uband', None)
+    if check_callable:
+        _check_callable(rhs, jac, x0, y0, lband, uband)
+
+    if check_indexing:
+        _check_indexing(rhs, jac, x0, y0, lband, uband)
+
+    return adaptive(rhs, jac, y0, x0, xend, dx0, atol, rtol, **kwargs)
+
+
+def integrate_predefined(rhs, jac, y0, xout, dx0, atol, rtol,
+                         check_callable=False, check_indexing=False, **kwargs):
+    """
+    Integrates a system of ordinary differential equations.
+
+    Parameters
+    ----------
+    rhs: callable
+        Function with signature f(t, y, fout) which modifies fout *inplace*.
+    jac: callable
+        Function with signature j(t, y, jmat_out, dfdx_out) which modifies
+        jmat_out and dfdx_out *inplace*.
+    y0: array_like
+        initial values of the dependent variables
+    xout: array_like
+        values of the independent variable
+    dx0: float
+        initial step-size
+    atol: float
+        absolute tolerance
+    rtol: float
+        relative tolerance
+    check_callable: bool (default: False)
+        perform signature sanity checks on ``rhs`` and ``jac``
+    check_indexing: bool (default: False)
+        perform item setting sanity checks on ``rhs`` and ``jac``.
+    \*\*kwargs:
+         'method': str
+            One of: 'adams' or 'bdf' (default: 'bdf')
+
+    Returns
+    -------
+    (result, info):
+        result: 2-dimensional array of the dependent variables (axis 1) for
+            values corresponding to xout (axis 0)
+        info: dictionary with information about the integration
+    """
+    # Sanity checks to reduce risk of having a segfault:
+    lband, uband = kwargs.get('lband', None), kwargs.get('uband', None)
+    if check_callable:
+        _check_callable(rhs, jac, xout[0], y0, lband, uband)
+
+    if check_indexing:
+        _check_indexing(rhs, jac, xout[0], y0, lband, uband)
+
+    return predefined(rhs, jac, y0, xout, dx0, atol, rtol, **kwargs)
---
+++ packages/pycvodes/pycvodes-0.2.0.tar.gz/pycvodes-0.2.0/pycvodes/tests/test_cvodes_numpy.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pytest
+
+from pycvodes import (
+    integrate_adaptive, integrate_predefined, requires_jac
+)
+
+decay_analytic = {
+    0: lambda y0, k, t: (
+        y0[0] * np.exp(-k[0]*t)),
+    1: lambda y0, k, t: (
+        y0[1] * np.exp(-k[1] * t) + y0[0] * k[0] / (k[1] - k[0]) *
+        (np.exp(-k[0]*t) - np.exp(-k[1]*t))),
+    2: lambda y0, k, t: (
+        y0[2] * np.exp(-k[2] * t) + y0[1] * k[1] / (k[2] - k[1]) *
+        (np.exp(-k[1]*t) - np.exp(-k[2]*t)) +
+        k[1] * k[0] * y0[0] / (k[1] - k[0]) *
+        (1 / (k[2] - k[0]) * (np.exp(-k[0]*t) - np.exp(-k[2]*t)) -
+         1 / (k[2] - k[1]) * (np.exp(-k[1]*t) - np.exp(-k[2]*t))))
+}
+
+
+def decay_get_Cref(k, y0, tout):
+    coeffs = list(k) + [0]*(3-len(k))
+    return np.column_stack([
+        decay_analytic[i](y0, coeffs, tout) for i in range(
+            min(3, len(k)+1))])
+
+
+def _get_f_j(k):
+    k0, k1, k2 = k
+
+    def f(t, y, fout):
+        fout[0] = -k0*y[0]
+        fout[1] = k0*y[0] - k1*y[1]
+        fout[2] = k1*y[1] - k2*y[2]
+
+    def j(t, y, jmat_out, dfdx_out=None, fy=None):
+        jmat_out[0, 0] = -k0
+        jmat_out[0, 1] = 0
+        jmat_out[0, 2] = 0
+        jmat_out[1, 0] = k0
+        jmat_out[1, 1] = -k1
+        jmat_out[1, 2] = 0
+        jmat_out[2, 0] = 0
+        jmat_out[2, 1] = k1
+        jmat_out[2, 2] = -k2
+        if dfdx_out is not None:
+            dfdx_out[0] = 0
+            dfdx_out[1] = 0
+            dfdx_out[2] = 0
+    return f, j
+
+methods = [('adams', 1.8, False),
+           ('adams', 1.8, True),
+           ('bdf', 1.8, False),
+           ('bdf', 3.4, True)]
+
+
+def bandify(cb, mlower, mupper):
+    def j(t, y, jmat_out, dfdx_out=None, fy=None):
+        jmat = np.empty((len(y), len(y)))
+        cb(t, y, jmat, dfdx_out, fy)
+        for ci in range(len(y)):
+            for ri in range(max(0, ci-mupper), min(len(y), ci+mlower+1)):
+                jmat_out[ri - ci + mupper, ci] = jmat[ri, ci]
+    return j
+
+
+@pytest.mark.parametrize("method,forgiveness,banded", methods)
+def test_integrate_adaptive(method, forgiveness, banded):
+    use_jac = method in requires_jac
+    k = k0, k1, k2 = 2.0, 3.0, 4.0
+    y0 = [0.7, 0.3, 0.5]
+    atol, rtol = 1e-8, 1e-8
+    kwargs = dict(x0=0, xend=3, dx0=1e-10, atol=atol, rtol=rtol,
+                  method=method)
+    f, j = _get_f_j(k)
+    if not use_jac:
+        j = None
+    else:
+        if banded:
+            j = bandify(j, 1, 0)
+            kwargs['lband'] = 1
+            kwargs['uband'] = 0
+    # Run twice to catch possible side-effects:
+    xout, yout, info = integrate_adaptive(f, j, y0, **kwargs)
+    xout, yout, info = integrate_adaptive(f, j, y0, **kwargs)
+    yref = decay_get_Cref(k, y0, xout)
+    assert np.allclose(yout, yref,
+                       rtol=forgiveness*rtol,
+                       atol=forgiveness*atol)
+    assert info['nrhs'] > 0
+    if method in requires_jac:
+        assert info['njac'] > 0
+
+
+@pytest.mark.parametrize("method,forgiveness,banded", methods)
+def test_integrate_predefined(method, forgiveness, banded):
+    use_jac = method in requires_jac
+    k = k0, k1, k2 = 2.0, 3.0, 4.0
+    y0 = [0.7, 0.3, 0.5]
+    f, j = _get_f_j(k)
+    kwargs = {'method': method}
+    if not use_jac:
+        j = None
+    else:
+        if banded:
+            j = bandify(j, 1, 0)
+            kwargs['lband'] = 1
+            kwargs['uband'] = 0
+    xout = np.linspace(0, 3, 31)
+    dx0 = 1e-10
+    atol, rtol = 1e-8, 1e-8
+    # Run twice to catch possible side-effects:
+    yout, nfo = integrate_predefined(f, j, y0, xout, dx0, 1e-8, 1e-8, **kwargs)
+    yout, nfo = integrate_predefined(f, j, y0, xout, dx0, 1e-8, 1e-8, **kwargs)
+    yref = decay_get_Cref(k, y0, xout)
+    print(yout)
+    print(yref)
+    assert np.allclose(yout, yref,
+                       rtol=forgiveness*rtol,
+                       atol=forgiveness*atol)
+    assert nfo['nrhs'] > 0
+    if method in requires_jac:
+        assert nfo['njac'] > 0
