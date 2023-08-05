0f27abc5266f0073ae41d90922ae41e4c4918a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package dbff/1.4.3/dbff-1.4.3.tar.gz

---
+++ packages/dbff/dbff-1.4.3.tar.gz/dbff-1.4.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+PACKAGE = "dbff"
+NAME = "dbff"
+VERSION = "1.4.3"
+
+setup(name=NAME,
+      version=VERSION,
+      author="Xiayi Li",
+      author_email="hi@xiayi.li",
+      url="https://github.com/yosg/dbff",
+      packages=find_packages(),
+      description="Compare tables and rows between MySQL databases.",
+      license="MIT License",
+      install_requires=["MySQL-python"]
+)
