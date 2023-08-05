c99a941f056b80857f58c827f05ec5e591c52952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:16 +0000

    Add package duk/0.3.post0.dev3/duk-0.3.post0.dev3.tar.gz

---
+++ packages/duk/duk-0.3.post0.dev3.tar.gz/duk-0.3.post0.dev3/setup.py
@@ -0,0 +1,26 @@
+""" Duk setup """
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension  # pylint: disable=E0611,F0401
+
+import versioneer
+
+setup(
+    name='duk',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    install_requires=[],
+    packages=['duk'],
+    author='Werner Van Geit',
+    author_email='werner.vangeit@gmail.com',
+    description='Python wrapper around the du -ks command',
+    long_description="",
+    entry_points={'console_scripts': ['duk=duk.duk:main'], },
+    license="LGPLv3",
+    keywords=(),
+    url='https://github.com/wvangeit/duk',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Utilities'])
