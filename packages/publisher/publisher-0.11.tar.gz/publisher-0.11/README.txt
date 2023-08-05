1b6b3f64aa8710001da003cbd40c2d987472bfc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:21 +0000

    Add package publisher/0.11/publisher-0.11.tar.gz

---
+++ packages/publisher/publisher-0.11.tar.gz/publisher-0.11/README.txt
@@ -0,0 +1,3 @@
+Convert an IPython notebook into an HTML file that can be consumed
+by GitHub pages in plotly's documentation repo.
+
---
+++ packages/publisher/publisher-0.11.tar.gz/publisher-0.11/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='publisher',
+    version='0.11',
+    author='chris p',
+    author_email='chris@plot.ly',
+    packages=['publisher'],
+    license='See LICENSE.txt',
+    description='',
+    long_description=open('README.txt').read(),
+    url='https://github.com/plotly/documentation'
+)
