15dbb5d4c6016a5cfd7daf915aa85447619c685c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:19 +0000

    Add package zdir/0.0.3/zdir-0.0.3.tar.gz

---
+++ packages/zdir/zdir-0.0.3.tar.gz/zdir-0.0.3/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+try:
+    from pypandoc import convert
+    description = convert("README.md", 'rst')
+except ImportError:
+    description = lambda f: open(f, 'r').read()
+
+
+# with open('README.rst') as f:
+#     readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='zdir',
+    version="0.0.3",
+    description='Library for handling many small files. Packs files into directory, nameing by hash of content. ',
+    long_description=description,
+    author='oskarnyqvist',
+    author_email='oskarnyqvist@gmail.com',
+    url='https://github.com/oskarnyqvist/zdir',
+    license=license,
+    packages=find_packages(exclude=('tests')),
+)
