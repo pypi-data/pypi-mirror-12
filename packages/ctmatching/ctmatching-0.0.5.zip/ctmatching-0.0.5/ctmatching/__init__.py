f695cbe208af59f84aa922949796403afd9dbb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:17 +0000

    Add package ctmatching/0.0.5/ctmatching-0.0.5.zip

---
+++ packages/ctmatching/ctmatching-0.0.5.zip/ctmatching-0.0.5/ctmatching/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+**Prerequisites**
+
+- numpy
+- scipy
+- sklearn
+- pandas
+"""
+
+from .core import psm
+from .dataset import load_re78
+
+__version__ = "0.0.5"
+__short_description__ = ("Treatment group, control group matching algorithm "
+                         "high level python implementation.")
---
+++ packages/ctmatching/ctmatching-0.0.5.zip/ctmatching-0.0.5/ctmatching/exc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+exceptions
+"""
+
+class InputError(Exception):
+    pass
+
+class NotEnoughControlSampleError(Exception):
+    pass
