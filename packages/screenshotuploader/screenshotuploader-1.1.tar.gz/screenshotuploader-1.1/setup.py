d98b812f181b5e6c0c3da764c90084a0fb8d95c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:29 +0000

    Add package screenshotuploader/1.1/screenshotuploader-1.1.tar.gz

---
+++ packages/screenshotuploader/screenshotuploader-1.1.tar.gz/screenshotuploader-1.1/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name="screenshotuploader",
+	version="1.1",
+	description="Automagically captures the screenshot of the screen , uploads in imgurl , copies the url into clipboard , deletes the created image file from your desktop ",
+	url="https://github.com/yask123/AutoScreenshotUploader",
+	author="Yask Srivastava",
+	author_email="yask123@gmail.com",
+	license='MIT',
+	packages=["screenshotuploader"],
+	scripts=["bin/screenshotuploader"],
+	zip_safe=False)
