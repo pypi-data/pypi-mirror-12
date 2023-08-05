2671e895bca3f0e831b8c72fd2dfc11da70e9149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:28 +0000

    Add package xml_models2/0.10.0/xml_models2-0.10.0.zip

---
+++ packages/xml_models2/xml_models2-0.10.0.zip/xml_models2-0.10.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='xml_models2',
+    version='0.10.0',
+    description='XML backed models queried from external REST apis',
+    long_description=long_description,
+    author='Geoff Ford and Chris Tarttelin and Cam McHugh',
+    author_email='g_ford@hotmail.ccom',
+    url='http://github.com/alephnullplex/xml_models',
+    packages=['xml_models', 'xml_models.rest_client'],
+    install_requires=['lxml', 'python-dateutil', 'pytz', 'future', 'requests'],
+    tests_require=['mock', 'nose', 'coverage'],
+    test_suite="nose.collector"
+)
