ad73b482733075c14aa218c31f2dea36e2edcf9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:37 +0000

    Add package malibu/0.1.4.post5/malibu-0.1.4.post5.tar.gz

---
+++ packages/malibu/malibu-0.1.4.post5.tar.gz/malibu-0.1.4.post5/malibu/__init__.py
@@ -0,0 +1,9 @@
+from malibu import config
+from malibu import connection
+from malibu import database
+from malibu import design
+from malibu import text
+from malibu import util
+
+
+__version__ = '0.1.4-5'
