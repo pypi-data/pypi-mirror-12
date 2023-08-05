443610e045bfe21e1caaa79aa9912a4787392abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package django-donations/0.2.0/django-donations-0.2.0.tar.gz

---
+++ packages/django-donations/django-donations-0.2.0.tar.gz/django-donations-0.2.0/donations/autoconfig.py
@@ -0,0 +1,14 @@
+SETTINGS = {
+    'INSTALLED_APPS': [
+        'django.contrib.auth',
+        'rest_framework',
+        'south',
+        'donations'
+    ],
+    'DONATION_FREQUENCIES': {
+        'single': '0 days',
+    },
+    'DONATION_PROVIDERS': {
+        'Just Giving': 'just_giving.SimpleDonationProvider'
+    }
+}
