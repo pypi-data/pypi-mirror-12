256fd8259d3279cf325aa5502e1c96422f619b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package mwxml/0.2.1/mwxml-0.2.1.tar.gz

---
+++ packages/mwxml/mwxml-0.2.1.tar.gz/mwxml-0.2.1/mwxml/mwxml.py
@@ -0,0 +1,13 @@
+import mwcli
+
+router = mwcli.Router(
+    "mwxml",
+    "This script provides access to a set of utilities for extracting " +
+        "content from MediaWiki XML dumps.",
+    {'dump2revdocs': "Converts XML dumps to revision documents (XML --> JSON)",
+     'validate': "Compares a stream of revision documents against a schema",
+     'normalize': "Converts a stream of old revision documents to documents " +
+                  "that validate against the current schema"}
+)
+
+main = router.main
