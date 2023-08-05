126105f7b3086b7750847475175f1ad56ddb7339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:28 +0000

    Add package inspectshow/0.2/inspectshow-0.2.tar.gz

---
+++ packages/inspectshow/inspectshow-0.2.tar.gz/inspectshow-0.2/setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+  name = 'inspectshow',
+  packages_dir = {'inspectshow': ' '},
+  packages = ['inspectshow'],
+  version = '0.2',
+  description = 'show module internals with inspect in tree format',
+  author = 'Vadivel',
+  author_email = 'vadivelmurugank@gmail.com',
+  url = 'https://github.com/vadivelmurugank/inspectshow',
+  license = 'MIT',
+)
