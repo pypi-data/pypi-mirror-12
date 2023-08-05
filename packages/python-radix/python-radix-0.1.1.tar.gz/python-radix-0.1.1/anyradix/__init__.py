67f7976095ad1d5a5fd2741b7dd03d6c0c7b3b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:31 +0000

    Add package python-radix/0.1.1/python-radix-0.1.1.tar.gz

---
+++ packages/python-radix/python-radix-0.1.1.tar.gz/python-radix-0.1.1/anyradix/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from anyradix import Converter
+from anyradix import cast
+
+__all__ = ['Converter', 'cast']
---
+++ packages/python-radix/python-radix-0.1.1.tar.gz/python-radix-0.1.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='python-radix',
+version='0.1.1',
+description='A tool for conversion numbers from one base to another',
+long_description=long_description,
+url='https://github.com/valbub/python-radix',
+author='valbub, yury-khrustalev',
+license='MIT',
+classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+],
+keywords='radix numbers',
+packages=['anyradix'],
+install_requires=[],
+platforms=['Any'])
