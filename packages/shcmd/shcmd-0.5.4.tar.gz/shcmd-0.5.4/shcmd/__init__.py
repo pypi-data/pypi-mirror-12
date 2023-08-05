7b20a87250769cc635c1c57e89c574026ec319bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:07 +0000

    Add package shcmd/0.5.4/shcmd-0.5.4.tar.gz

---
+++ packages/shcmd/shcmd-0.5.4.tar.gz/shcmd-0.5.4/shcmd/__init__.py
@@ -0,0 +1,49 @@
+__version__ = "0.5.1"
+__author__ = "SkyLothar"
+__email__ = "allothar@gmail.com"
+__url__ = "https://github.com/skylothar/shcmd"
+
+import sys
+import os
+
+version = sys.version_info
+if version.major != 3:
+    raise ValueError("unsupported python version: {0}".format(version))
+
+__all__ = [
+    "cd", "cd_to",
+    "mkdir", "rm", "run", "tailf",
+    "TarGenerator",
+    "ShCmdError"
+]
+
+from .cmd import cd, cd_to, mkdir, rm
+from .errors import ShCmdError
+from .proc import Proc
+from .tar import TarGenerator
+from .utils import expand_args
+from .tailf import tailf
+
+
+DEFAULT_TIMEOUT = 60
+
+
+def run(cmd, cwd=None, env=None, timeout=None, stream=False, warn_only=False):
+    """
+    :param cmd: command to run
+    :param cwd: change dir into before execute, default is current dir
+    :param env: environments to pass to subprocess
+    :param timeout: timeout
+    :param stream: stream output, default is False, block until finished
+    :param warn_only: default False, set to True to allow unsuccessful result
+    """
+    proc = Proc(
+        expand_args(cmd),
+        os.path.realpath(cwd or os.getcwd()),
+        env=env or {},
+        timeout=timeout or DEFAULT_TIMEOUT
+    )
+
+    if not stream:
+        proc.block(warn_only=warn_only)
+    return proc
---
+++ packages/shcmd/shcmd-0.5.4.tar.gz/shcmd-0.5.4/shcmd/cmd.py
@@ -0,0 +1,76 @@
+# -*- coding: utf8 -*-
+
+import contextlib
+import functools
+import os
+import shutil
+
+
+@contextlib.contextmanager
+def cd(cd_path, create=False):
+    """cd to target dir when running in this block
+
+    :param cd_path: dir to cd into
+    :param create: create new dir if destination not there
+
+    Usage::
+
+        >>> with cd("/tmp"):
+        ...     print("we are in /tmp now")
+    """
+    oricwd = os.getcwd()
+    if create:
+        mkdir(cd_path)
+    try:
+        os.chdir(cd_path)
+        yield
+    finally:
+        os.chdir(oricwd)
+
+
+def cd_to(path, mkdir=False):
+    """make a generator like cd, but use it for function
+
+    Usage::
+
+        >>> @cd_to("/")
+        ... def say_where():
+        ...     print(os.getcwd())
+        ...
+        >>> say_where()
+        /
+
+    """
+    def cd_to_decorator(func):
+        @functools.wraps(func)
+        def _cd_and_exec(*args, **kwargs):
+            with cd(path, mkdir):
+                return func(*args, **kwargs)
+        return _cd_and_exec
+    return cd_to_decorator
+
+
+def mkdir(path):
+    """just like mkdir -p
+
+    :param path: dir you want to make
+    """
+    os.makedirs(path, exist_ok=True)
+
+
+def rm(path, isdir=False):
+    """delete file or dir
+    returns True if deleted, if file/dir not exists, return False
+
+    :param path: path to delete
+    :param isdir: set True if you want to delete dir
+    """
+    if isdir:
+        deleted = os.path.isdir(path)
+        shutil.rmtree(path, ignore_errors=True)
+    elif os.path.isfile(path) or os.path.islink(path):
+        deleted = True
+        os.remove(path)
+    else:
+        deleted = False
+    return deleted
