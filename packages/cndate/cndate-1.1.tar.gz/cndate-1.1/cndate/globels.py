32ecdb14ad8271eac5eb949e0c221a3ef629916a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:03 +0000

    Add package cndate/1.1/cndate-1.1.tar.gz

---
+++ packages/cndate/cndate-1.1.tar.gz/cndate-1.1/cndate/globels.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+"""
+时间:
+说明:
+"""
+
+WEEKDAYS = [("Mon", "Monday"),
+            ("Tue", "Tuesday"),
+            ("Wed", "Wednesday"),
+            ("Thu", "Thursday"),
+            ("Fri", "Friday"),
+            ("Sat", "Saturday"),
+            ("Sun", "Sunday")]
+
+MONTHS = [("Jan", "January"),
+          ("Feb", "February"),
+          ("Mar", "March"),
+          ("Apr", "April"),
+          ("May", "May"),
+          ("Jun", "June"),
+          ("Jul", "July"),
+          ("Aug", "August"),
+          ("Sep", "Sept", "September"),
+          ("Oct", "October"),
+          ("Nov", "November"),
+          ("Dec", "December")]
---
+++ packages/cndate/cndate-1.1.tar.gz/cndate-1.1/setup.py
@@ -0,0 +1,7 @@
+# coding:utf-8
+from distutils.core import setup
+
+setup(name='cndate',
+      version='1.1',
+      packages=['cndate'],
+      )
