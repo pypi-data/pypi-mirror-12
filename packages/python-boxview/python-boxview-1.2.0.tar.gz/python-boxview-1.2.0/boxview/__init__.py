d038d3031bbde37b9357f3b9765550b83c6d1df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:20 +0000

    Add package python-boxview/1.2.0/python-boxview-1.2.0.tar.gz

---
+++ packages/python-boxview/python-boxview-1.2.0.tar.gz/python-boxview-1.2.0/boxview/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '1.2.0'
+__author__ = 'Maxim Kamenkov'
+
+from .boxview import BoxView, BoxViewError, RetryAfter
+
+__all__ = ['BoxView', 'BoxViewError', 'RetryAfter']
