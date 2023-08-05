8bd0b0de4ca51f8dc3a264c461d1049ef3f3d321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:47 +0000

    Add package afp-cli/1.1.5.post0/afp-cli-1.1.5.post0.tar.gz

---
+++ packages/afp-cli/afp-cli-1.1.5.post0.tar.gz/afp-cli-1.1.5.post0/afp_cli/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+from .client import AWSFederationClientCmd, print_aws_credentials
+
+__all__ = ['AWSFederationClientCmd', 'print_aws_credentials']
