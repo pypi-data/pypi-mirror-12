604035460d88d41f5fe65d32e2f0bd6f2d2fc89a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:27 +0000

    Add package leftmagic_nester/1.2.0/leftmagic_nester-1.2.0.zip

---
+++ packages/leftmagic_nester/leftmagic_nester-1.2.0.zip/leftmagic_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name	= 'leftmagic_nester',
+		version	= '1.2.0',
+		py_modules	= ['leftmagic_nester'],
+		author	= 'leftmagic',
+		author_email	= 'leftwing871@gmail.com',
+		url	= 'http://www.111.com',
+		description	= 'a simple printer of nested lists',
+	)
