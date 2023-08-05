438a8cacbff8ce298b6b02c4b97fa0b4e81ffa03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:32 +0000

    Add package testforpaul/1.0.0/testforpaul-1.0.0.zip

---
+++ packages/testforpaul/testforpaul-1.0.0.zip/testforpaul-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'testforpaul',
+        version = '1.0.0',
+        py_modules = ['testforpaul'],
+        author = 'paulinus',
+        author_email = 'paulinusforwork@live.com',
+        description = 'a test module',
+     )
+        
