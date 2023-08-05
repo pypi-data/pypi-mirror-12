2725402841c62e659823df90601fe259535c9ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:10 +0000

    Add package zyk_hfp_test1/1.2.0/zyk_hfp_test1-1.2.0.zip

---
+++ packages/zyk_hfp_test1/zyk_hfp_test1-1.2.0.zip/zyk_hfp_test1-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'zyk_hfp_test1',
+    version = '1.2.0',
+    py_modules = ['zyk_hfp_test1'],
+    author = 'zyk',
+    author_email = 'zhangrainkang@163.com',
+    url = 'null',
+    description = 'just a test',
+    )
