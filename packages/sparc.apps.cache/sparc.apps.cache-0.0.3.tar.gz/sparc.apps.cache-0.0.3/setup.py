e298af0676572950164456ac733914c8ef6db86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:42 +0000

    Add package sparc.apps.cache/0.0.3/sparc.apps.cache-0.0.3.tar.gz

---
+++ packages/sparc.apps.cache/sparc.apps.cache-0.0.3.tar.gz/sparc.apps.cache-0.0.3/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.3'
+
+setup(name='sparc.apps.cache',
+      version=version,
+      description="A generic CSV to SQL DB caching utility.",
+      long_description=open("README.md").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+      ],
+      keywords=['zca'],
+      author='David Davis',
+      author_email='davisd50@gmail.com',
+      url='https://github.com/davisd50/sparc.apps.cache',
+      download_url = '',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['sparc', 'sparc.apps'],
+      include_package_data=True,
+      package_data = {
+          '': ['*.zcml']
+        },
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'argparse',
+          'pyinstaller',
+          'SQLAlchemy',
+          'sparc.common',
+          'sparc.db',
+          'sparc.cache'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points={
+          'console_scripts':['cache=sparc.apps.cache.cache:main'],
+          },
+      )
