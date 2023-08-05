ce3790d1dfa39b11bddf9882a4b34bdc17334513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:58 +0000

    Add package directnet/1.1/directnet-1.1.tar.gz

---
+++ packages/directnet/directnet-1.1.tar.gz/directnet-1.1/directnet/__init__.py
@@ -0,0 +1,6 @@
+version = '1.1'
+
+from .dn_client import DNClient
+from .ks_client import KSClient
+
+__all__ = ('DNClient', 'KSClient')
---
+++ packages/directnet/directnet-1.1.tar.gz/directnet-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+import directnet
+
+setup(
+    name='directnet',
+    version=directnet.version,
+    packages=('directnet',),
+    url='https://github.com/cuchac/directnet',
+    license='LGPL v2',
+    author='Cuchac',
+    author_email='cuchac@email.cz',
+    description='DirectNET communication library',
+    install_requires=('pyserial', 'six'),
+)
