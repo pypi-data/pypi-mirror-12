22f15248abf804916eaac4b7045a1ecc729d0067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:34 +0000

    Add package django-lastpass-sp/0.1.2/django_lastpass_sp-0.1.2-py3-none-any.whl

---
+++ packages/django-lastpass-sp/django_lastpass_sp-0.1.2-py3-none-any.whl/lpsp/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
+from lpsp.views import (MetadataView, CompleteAuthenticationView,
+                      InitiateAuthenticationView)
+
+urlpatterns = [
+    url(r'^initiate-login/$', InitiateAuthenticationView.as_view(),
+        name="saml_login_initiate"),
+    url(r'^complete-login/$', csrf_exempt(CompleteAuthenticationView.as_view()),
+        name="saml_login_complete"),
+    url(r'^metadata/$', MetadataView.as_view(), name='saml_metadata'),
+]
