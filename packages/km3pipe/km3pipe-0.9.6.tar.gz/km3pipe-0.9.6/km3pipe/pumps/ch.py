23536eb2e275794cf2eaf9dd3dc902344eaba36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:23 +0000

    Add package km3pipe/0.9.6/km3pipe-0.9.6.tar.gz

---
+++ packages/km3pipe/km3pipe-0.9.6.tar.gz/km3pipe-0.9.6/km3pipe/pumps/ch.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Filename: jpp.py
+# pylint: disable=
+"""
+Pump for the jpp file read through aanet interface.
+
+"""
+from __future__ import division, absolute_import, print_function
+
+import socket
+
+from km3pipe import Pump
+from km3pipe.logger import logging
+
+
+log = logging.getLogger(__name__)  # pylint: disable=C0103
+
+
+class CHPump(Pump):
+    """A pump for ControlHost data."""
+    def __init__(self, **context):
+        super(self.__class__, self).__init__(**context)
+
+        self.host = self.get('host') or '127.0.0.1'
+        self.port = self.get('port') or 5553
+        self.tag = self.get('tag') or 'MSG'
+        self.tags = self.get('tags') or []
+        self.key_for_data = self.get('key_for_data') or 'CHData'
+        self.key_for_prefix = self.get('key_for_prefix') or 'CHPrefix'
+
+        self.client = None
+
+        self._init_controlhost()
+
+    def _init_controlhost(self):
+        """Set up the controlhost connection"""
+        from controlhost import Client
+        self.client = Client(self.host, self.port)
+        self.client._connect()
+        if self.tag:
+            self.client.subscribe(self.tag)
+        for tag in self.tags:
+            self.client.subscribe(tag)
+
+
+    def process(self, blob):
+        """Wait for the next packet and put it in the blob"""
+        try:
+            prefix, data = self.client.get_message()
+        except socket.error:
+            log.warn("Stopping cycle due to socket error")
+            raise StopIteration("Stopping cycle due to socket error")
+        else:
+            blob[self.key_for_prefix] = prefix
+            blob[self.key_for_data] = data
+            return blob
+
+    def finish(self):
+        """Clean up the JLigier controlhost connection"""
+        self.client._disconnect()
---
+++ packages/km3pipe/km3pipe-0.9.6.tar.gz/km3pipe-0.9.6/km3pipe/pumps/jpp.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Filename: jpp.py
+# pylint: disable=
+"""
+Pump for the jpp file read through aanet interface.
+
+"""
+from __future__ import division, absolute_import, print_function
+
+from km3pipe import Pump
+from km3pipe.logger import logging
+
+log = logging.getLogger(__name__)  # pylint: disable=C0103
+
+
+class JPPPump(Pump):
+    """A pump for JPP ROOT files."""
+
+    def __init__(self, **context):
+        super(self.__class__, self).__init__(**context)
+
+        self.index = self.get('index') or 0
+
+        import aa
+        import ROOT
+        if self.get('index'):
+            self.index = self.get('index')
+        else:
+            self.index = 0 
+        self.filename = self.get('filename')
+        if not self.filename:
+            raise ValueError("No filename defined")
+        self.rootfile = ROOT.EventFile(self.filename)
+        self.evt = ROOT.Evt()
+
+    def get_blob(self, index):
+        """Return the blob"""
+        self.rootfile.set_index(index)
+        self.evt = self.rootfile.evt
+        return {'Evt': self.evt}
+
+    def process(self, blob):
+        if self.rootfile.set_index(self.index):
+            self.evt = self.rootfile.evt
+            self.index += 1
+            return {'Evt': self.evt}
+        else:
+            raise StopIteration
+
+
+    def finish(self):
+        self.rootfile.Close()
