0eeb9b95ccbb8ad0eb323aedc1d82832d3e88a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:18 +0000

    Add package covar/0.2/covar-0.2.tar.gz

---
+++ packages/covar/covar-0.2.tar.gz/covar-0.2/setup.py
@@ -0,0 +1,40 @@
+"""Shrinkage covariance estimator for python
+"""
+from setuptools import setup, Extension
+from Cython.Distutils import build_ext
+
+
+##########################
+VERSION = "0.2"
+__version__ = VERSION
+##########################
+
+DOCLINES = __doc__.split("\n")
+CLASSIFIERS = """\
+Development Status :: 3 - Alpha
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: BSD
+Programming Language :: Python
+Operating System :: OS Independent
+"""
+
+extensions = [
+    Extension('covar', ['covar.pyx'], language='c++'),
+]
+
+setup(
+    name='covar',
+    author="Robert T. McGibbon",
+    author_email='rmcgibbo@gmail.com',
+    cmdclass={'build_ext': build_ext},
+    url="https://github.com/rmcgibbo/covar",
+    description=DOCLINES[0],
+    setup_requires=['cython', 'scipy >= 0.16', 'numpy >= 1.6'],
+    install_requires=['scipy >= 0.16', 'numpy >= 1.6'],
+    long_description="\n".join(DOCLINES[2:]),
+    version=__version__,
+    license='BSD',
+    zip_safe=False,
+    ext_modules=extensions,
+)
