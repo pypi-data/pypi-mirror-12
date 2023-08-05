36753442baffa15aa8c38841361784c2108dc991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:30 +0000

    Add package eth-testrpc/0.1.20/eth-testrpc-0.1.20.tar.gz

---
+++ packages/eth-testrpc/eth-testrpc-0.1.20.tar.gz/eth-testrpc-0.1.20/setup.py
@@ -0,0 +1,46 @@
+"""An Ethereum client simulator that provides instant results and quick
+feedback during development involving smart contracts.
+
+https://github.com/ConsenSys/eth-testrpc
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+from testrpc import __version__
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='eth-testrpc',
+    version=__version__,
+    description='An Ethereum simulator for aiding smart contract development.',
+    long_description=long_description,
+    url='https://github.com/ConsenSys/eth-testrpc',
+    author='ConsenSys',
+    author_email='info@consensys.net',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='ethereum blockchain development testing',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=[
+        'jsonrpclib',
+        'serpent',
+        'ethereum',
+    ],
+    entry_points={
+        'console_scripts': [
+            'testrpc=testrpc.__main__:main',
+        ],
+    }
+)
