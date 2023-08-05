7a7c9adda0f6a2b4b27cebab74eda8771171d2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:30 +0000

    Add package omnikassa/0.0.3/omnikassa-0.0.3.tar.gz

---
+++ packages/omnikassa/omnikassa-0.0.3.tar.gz/omnikassa-0.0.3/omnikassa/payment.py
@@ -0,0 +1,37 @@
+import time
+
+from .constants import Settings
+
+from .exceptions import OmnikassaException
+from .helpers import data_to_str
+
+
+class Payment:
+    def __init__(self, settings):
+        self.settings = self._validate_settings(settings.copy())
+
+    def _validate_settings(self, settings):
+        if not settings.get('currencyCode'):
+            raise OmnikassaException('currencyCode is required.')
+
+        if not settings.get('transactionReference'):
+            raise OmnikassaException('transactionReference is required.')
+
+        if not settings.get('amount'):
+            raise OmnikassaException('amount is required.')
+        if type(settings['amount']) != int:
+            raise OmnikassaException('amount should be an integer (cents).')
+
+        if not settings.get('normalReturnUrl'):
+            raise OmnikassaException('normalReturnUrl is required.')
+
+        if settings['merchantId'] == Settings.Test.MERCHANT_ID:
+            # Prefix reference in test with timestamp to prevent collisions.
+            settings['transactionReference'] = '{}_{}'.format(
+                time.time(), settings['transactionReference']
+            )
+
+        return settings
+
+    def __str__(self):
+        return data_to_str(self.settings)
