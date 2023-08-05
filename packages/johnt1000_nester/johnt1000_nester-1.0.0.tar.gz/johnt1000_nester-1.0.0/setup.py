47e60336fc89f81115ff2046e1a88268dc823e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:09 +0000

    Add package johnt1000_nester/1.0.0/johnt1000_nester-1.0.0.tar.gz

---
+++ packages/johnt1000_nester/johnt1000_nester-1.0.0.tar.gz/johnt1000_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+  name          = 'johnt1000_nester',
+  version       = '1.0.0',
+  py_modules    = ['johnt1000_nester'],
+  author        = 'johnt1000',
+  author_email  = 'jonatas.rodriguess@gmail.com',
+  url           = 'http://www.headfirstlabs.com',
+  description   = 'A simple printer of nester lists',
+)
