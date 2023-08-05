fe356059a274fb91472dfce7d0ab0f3787d4c45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:20 +0000

    Add package geoselect/0.2.0/geoselect-0.2.0.tar.gz

---
+++ packages/geoselect/geoselect-0.2.0.tar.gz/geoselect-0.2.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+
+def long_description():
+    with open('README.md', 'r') as readme:
+        return readme.read()
+
+setup(
+    name='geoselect',
+    version='0.2.0',
+    py_modules=['geoselect'],
+    install_requires=[
+        'ExifRead>2.0,<3.0',
+        'path.py>8.0,<9.0',
+        'wheel==0.24.0',
+    ],
+    entry_points='''
+        [console_scripts]
+        geoselect=geoselect:main
+    ''',
+
+
+    author='Freek Kalter',
+    author_email='freek@kalteronline.org',
+    description='A script to select photos from a set, based on geographical location.',
+    long_description=long_description(),
+    license='GPL v3',
+    keywords='gps photos filter',
+    url='https://github.com/FreekKalter/geoselect',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Environment :: Console',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+        'Topic :: Multimedia',
+    ],
+)
