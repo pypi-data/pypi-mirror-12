d9cfa8c9d8f3d5bb02eec9451f14f9ec0b90082b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package district42/0.5.2/district42-0.5.2.tar.gz

---
+++ packages/district42/district42-0.5.2.tar.gz/district42-0.5.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+  name='district42',
+  packages=['district42', 'district42/json_schema'],
+  version='0.5.2',
+  description='',
+  url='https://github.com/nikitanovosibirsk/district42',
+  author='Nikita Tsvetkov',
+  author_email='nikitanovosibirsk@yandex.com',
+  license='MIT'
+)
