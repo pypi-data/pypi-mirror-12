b42e85953a12b134bcaf3ff6c37873df66f02daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:34 +0000

    Add package ch2tester/1.3.0/ch2tester-1.3.0.tar.gz

---
+++ packages/ch2tester/ch2tester-1.3.0.tar.gz/ch2tester-1.3.0/ch2tester.py
@@ -0,0 +1,16 @@
+""" this is the ch2tester.py module containing one function called
+print_lol() which prints lists that may or may not include nested
+lists"""
+def print_lol(the_list,indent=False,level=0):
+    """ This function takes an list as an argument.  This list may
+contain items which are lists themselves.  Each data item in the passed
+list is recursively printed to screen on its own line.  Level is 2nd arg
+which will insert tab stops when nested list is encountered"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if(indent):
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(each_item)
