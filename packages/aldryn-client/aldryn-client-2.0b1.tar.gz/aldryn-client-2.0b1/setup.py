12cfe3c8dfab61d14a63ca580bc5de8f1333156a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:23 +0000

    Add package aldryn-client/2.0b1/aldryn-client-2.0b1.tar.gz

---
+++ packages/aldryn-client/aldryn-client-2.0b1.tar.gz/aldryn-client-2.0b1/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup, find_packages
+
+from aldryn_client import __version__
+
+
+INSTALL_REQUIRES = [
+    'click',
+    'requests',
+    'tabulate',
+]
+
+if sys.platform == 'win32':
+    INSTALL_REQUIRES += [
+        'pyyaml',  # converting docker-compose configs
+        'colorama',  # colored output
+    ]
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Software Development',
+]
+
+setup(
+    name='aldryn-client',
+    version=__version__,
+    description='The command-line client for the Aldryn Cloud',
+    author='Divio AG',
+    author_email='aldryn@divio.ch',
+    url='http://www.aldryn.com/',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    license='BSD',
+    platforms=['OS Independent'],
+    install_requires=INSTALL_REQUIRES,
+    entry_points="""
+    [console_scripts]
+    aldryn = aldryn_client.cli:cli
+    """,
+)
