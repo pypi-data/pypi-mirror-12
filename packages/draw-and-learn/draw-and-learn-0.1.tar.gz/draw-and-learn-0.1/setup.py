0c0d0224614ee40345893d270e9ab557c69ccb04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:19 +0000

    Add package draw-and-learn/0.1/draw-and-learn-0.1.tar.gz

---
+++ packages/draw-and-learn/draw-and-learn-0.1.tar.gz/draw-and-learn-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='draw-and-learn',
+    version='0.1',
+    description='',
+    url='https://github.com/ishirav/draw-and-learn',
+    download_url = 'https://github.com/ishirav/draw-and-learn/tarball/0.1',
+    author='Itai Shirav',
+    license='MIT',
+    packages=['draw'],
+    zip_safe=False
+)
