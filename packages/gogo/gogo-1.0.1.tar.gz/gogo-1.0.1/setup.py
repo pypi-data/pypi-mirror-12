211aa7f7b3670d16188e117c084d6712b54a9b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:57 +0000

    Add package gogo/1.0.1/gogo-1.0.1.tar.gz

---
+++ packages/gogo/gogo-1.0.1.tar.gz/gogo-1.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='gogo',
+    version='1.0.1',
+    description='Golang package navigator',
+    author='Viet Hung Nguyen',
+    author_email='hvn@familug.org',
+    url='https://github.com/hvnsweeting/gogo',
+    scripts=['gogo'],
+)
