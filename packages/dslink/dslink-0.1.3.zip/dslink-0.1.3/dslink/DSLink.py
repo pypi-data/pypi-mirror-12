469ccc0aac5ab817834ca73f2eaeca2e3971c5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:05 +0000

    Add package dslink/0.1.3/dslink-0.1.3.zip

---
+++ packages/dslink/dslink-0.1.3.zip/dslink-0.1.3/dslink/DSLink.py
@@ -0,0 +1,342 @@
+import argparse
+import base64
+import logging
+
+from dslink.Crypto import Keypair
+from dslink.Handshake import Handshake
+from dslink.Node import Node
+from dslink.WebSocket import WebSocket
+
+
+class DSLink:
+    """
+    Base DSLink class which creates the node structure,
+    subscription/stream manager, and connects to the broker.
+    """
+
+    def __init__(self, config):
+        """
+        Construct for DSLink.
+        :param config: Configuration object.
+        """
+        # DSLink Configuration
+        self.config = config
+
+        # Node tree
+        self.super_root = Node("", None)
+        self.super_root.link = self
+
+        # Logger setup
+        self.logger = self.create_logger("DSLink", self.config.log_level)
+        self.logger.info("Starting DSLink")
+
+        # Managers setup
+        self.subman = SubscriptionManager()
+        self.strman = StreamManager()
+        self.reqman = RequestManager()
+
+        # DSLink setup
+        self.rid = 1
+        self.keypair = Keypair()
+        self.handshake = Handshake(self, config.name, config.broker, self.keypair, config.responder, config.requester)
+        self.server_config = self.handshake.run_handshake()
+        self.salt = self.server_config["salt"]
+        self.dsid = self.handshake.get_dsid()
+        self.shared_secret = self.keypair.keypair.get_ecdh_key(
+            base64.urlsafe_b64decode(self.add_padding(self.server_config["tempKey"])))
+
+        # Connection setup
+        self.active = False
+        self.wsp = None
+        self.websocket = WebSocket(self)
+
+        self.logger.info("Started DSLink")
+
+    def get_next_rid(self):
+        """
+        Get the next rid in the sequence. Initially starts from 1.
+        :return: Next rid in sequence.
+        """
+        r = self.rid
+        self.rid += 1
+        return r
+
+    def list(self, path, callback):
+        """
+        List a remote node.
+        :param path: Request path.
+        """
+        if not self.config.requester:
+            raise ValueError("Requester is not enabled.")
+        rid = self.get_next_rid()
+        self.wsp.sendMessage({
+            "requests": [
+                {
+                    "rid": rid,
+                    "method": "list",
+                    "path": path
+                }
+            ]
+        }, self)
+        self.reqman.start_request(rid, callback)
+
+    def set(self, path, value, permit=None, callback=None):
+        """
+        Set a remote value.
+        :param path: Path of value.
+        :param value: Value to set.
+        :param permit: Maximum permission of set.
+        :param callback: Response callback.
+        """
+        if not self.config.requester:
+            raise ValueError("Requester is not enabled.")
+        rid = self.get_next_rid()
+        i = {
+            "rid": rid,
+            "method": "set",
+            "path": path,
+            "value": value
+        }
+        if permit is not None:
+            i["permit"] = permit
+        self.wsp.sendMessage({
+            "requests": [
+                i
+            ]
+        })
+        if callback:
+            self.reqman.start_request(rid, callback)
+
+    def remove(self, path, callback=None):
+        """
+        Remove a remote value.
+        :param path: Path of value.
+        :param callback: Response callback.
+        """
+        if not self.config.requester:
+            raise ValueError("Requester is not enabled.")
+        rid = self.get_next_rid()
+        self.wsp.sendMessage({
+            "rid": rid,
+            "method": "remove",
+            "path": path
+        })
+        if callback:
+            self.reqman.start_request(rid, callback)
+
+    def invoke(self, path, permit=None, params=None, callback=None):
+        """
+        Invoke a remote method.
+        :param path: Path of node.
+        :param permit: Maximum permission of invoke.
+        :param params: Parameters of invoke.
+        :param callback: Response callback.
+        """
+        if not self.config.requester:
+            raise ValueError("Requester is not enabled.")
+        rid = self.get_next_rid()
+        i = {
+            "rid": rid,
+            "method": "invoke",
+            "path": path
+        }
+        if permit is not None:
+            i["permit"] = permit
+        if params is not None:
+            i["params"] = params
+        self.wsp.sendMessage({
+            "requests": [
+                i
+            ]
+        })
+        if callback:
+            self.reqman.start_request(rid, callback)
+
+    # TODO(logangorence): Subscribe method.
+
+    # TODO(logangorence): Unsubscribe method.
+
+    def close(self, rid):
+        """
+        Close a stream.
+        :param rid: ID of request.
+        """
+        if not self.config.requester:
+            raise ValueError("Requester is not enabled.")
+        self.wsp.sendMessage({
+            "requests": [
+                {
+                    "rid": rid,
+                    "method": "close"
+                }
+            ]
+        })
+
+    @staticmethod
+    def create_logger(name, log_level=logging.INFO):
+        """
+        Create a logger with the specified name.
+        :param name: Logger name.
+        :param log_level: Output Logger level.
+        :return: Logger instance.
+        """
+        # Logger setup
+        formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        ch.setLevel(log_level)
+        logger = logging.getLogger(name)
+        logger.setLevel(log_level)
+        logger.addHandler(ch)
+        return logger
+
+    @staticmethod
+    def add_padding(string):
+        """
+        Add padding to a URL safe base64 string.
+        :param string:
+        :return:
+        """
+        while len(string) % 4 != 0:
+            string += "="
+        return string
+
+
+class SubscriptionManager:
+    """
+    Manages subscriptions to Nodes.
+    """
+
+    def __init__(self):
+        """
+        Constructor of SubscriptionManager.
+        """
+        self.subscriptions = {}
+
+    def subscribe(self, node, sid):
+        """
+        Store a Subscription to a Node.
+        :param node: Node to subscribe to.
+        :param sid: SID of Subscription.
+        """
+        self.subscriptions[sid] = node
+        self.subscriptions[sid].add_subscriber(sid)
+
+    def unsubscribe(self, sid):
+        """
+        Remove a Subscription to a Node.
+        :param sid: SID of Subscription.
+        """
+        try:
+            self.subscriptions[sid].remove_subscriber(sid)
+            del self.subscriptions[sid]
+        except KeyError:
+            logging.getLogger("DSlink").debug("Unknown sid %s" % sid)
+
+
+class StreamManager:
+    """
+    Manages streams for Nodes.
+    """
+
+    def __init__(self):
+        """
+        Constructor of StreamManager.
+        """
+        self.streams = {}
+
+    def open_stream(self, node, rid):
+        """
+        Open a Stream.
+        :param node: Node to handle streaming.
+        :param rid: RID of Stream.
+        """
+        self.streams[rid] = node
+        self.streams[rid].streams.append(rid)
+
+    def close_stream(self, rid):
+        """
+        Close a Stream.
+        :param rid: RID of Stream.
+        """
+        try:
+            self.streams[rid].streams.remove(rid)
+            del self.streams[rid]
+        except KeyError:
+            logging.getLogger("DSLink").debug("Unknown rid %s" % rid)
+
+
+class RequestManager:
+    """
+    Manages outgoing requests and callbacks.
+    """
+
+    def __init__(self):
+        """
+        Constructor of RequestManager.
+        """
+        self.requests = {}
+
+    def start_request(self, rid, callback):
+        """
+        Start a Request.
+        :param rid: RID of Request.
+        :param callback: Callback to invoke.
+        """
+        self.requests[rid] = callback
+
+    def stop_request(self, rid):
+        """
+        Stop a Request.
+        :param rid: RID of Request.
+        """
+        del self.requests[rid]
+
+    def invoke_request(self, rid, data):
+        """
+        Invoke a Request.
+        :param rid: RID of Request.
+        :param data: Data of Request.
+        """
+        self.requests[rid](data)
+
+
+class Configuration:
+    """
+    Provides configuration to the DSLink.
+    """
+
+    def __init__(self, name, responder=False, requester=False, ping_time=30):
+        """
+        Object that contains configuration for the DSLink.
+        :param name: DSLink name.
+        :param responder: True if acts as responder, default is False.
+        :param requester: True if acts as requester, default is False.
+        :param ping_time: Time between pings, default is 30.
+        """
+        if not responder and not requester:
+            print("DSLink is neither responder nor requester. Exiting now.")
+            exit(1)
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--broker", default="http://localhost:8080/conn")
+        parser.add_argument("--log", default="info")
+        args = parser.parse_args()
+        self.name = name
+        self.broker = args.broker
+        self.log_level = args.log.lower()
+        self.responder = responder
+        self.requester = requester
+        self.ping_time = ping_time
+
+        if self.log_level == "critical":
+            self.log_level = logging.CRITICAL
+        elif self.log_level == "error":
+            self.log_level = logging.ERROR
+        elif self.log_level == "warning":
+            self.log_level = logging.WARNING
+        elif self.log_level == "info":
+            self.log_level = logging.INFO
+        elif self.log_level == "debug":
+            self.log_level = logging.DEBUG
+        elif self.log_level == "none":
+            self.log_level = logging.NOTSET
---
+++ packages/dslink/dslink-0.1.3.zip/dslink-0.1.3/dslink/Node.py
@@ -0,0 +1,325 @@
+from collections import OrderedDict
+import logging
+
+from dslink.Response import Response
+from dslink.Value import Value
+
+
+class Node:
+    """
+    Represents a Node on the Node structure.
+    """
+
+    def __init__(self, name, parent, standalone=False):
+        """
+        Node Constructor.
+        :param name: Node name.
+        :param parent: Node parent.
+        :param standalone: Standalone Node structure.
+        """
+        self.logger = logging.getLogger("DSLink")
+        if parent is not None:
+            self.link = parent.link
+        self.parent = parent
+        self.standalone = standalone
+        self.value = Value()
+        self.children = {}
+        self.config = OrderedDict([("$is", "node")])
+        self.attributes = OrderedDict()
+        self.subscribers = []
+        self.streams = []
+        self.invoke_callback = None
+        if parent is not None:
+            self.name = name
+            if parent.path.endswith("/"):
+                self.path = parent.path + name
+            else:
+                self.path = parent.path + "/" + name
+        else:
+            if name is not None:
+                self.name = name
+                self.path = "/" + name
+            else:
+                self.name = ""
+                self.path = ""
+
+    def has_value(self):
+        """
+        Check if the Node has a value.
+        :return: True if the Node has a value.
+        """
+        return self.value.type is not None
+
+    def get_type(self):
+        """
+        Get the Node's value type.
+        :return: Value type.
+        """
+        return self.config["$type"]
+
+    def set_type(self, t):
+        """
+        Set the Node's value type.
+        :param t: Type to set.
+        """
+        self.value.set_type(t)
+        self.config["$type"] = t
+
+    def get_value(self):
+        """
+        Get the Node's value.
+        :return: Node value.
+        """
+        if self.value.type is "enum":
+            return Value.build_enum(self.value.value)
+        return self.value.value
+
+    def set_value(self, value):
+        """
+        Set the Node's value.
+        :param value: Value to set.
+        """
+        # Set value and updated timestamp
+        if self.value.set_value(value) and (not self.standalone or self.link.active):
+            # Update any subscribers
+            self.update_subscribers_values()
+
+    def get_config(self, key):
+        """
+        Get a config value.
+        :param key: Key of config.
+        :return: Value of config.
+        """
+        return self.config[key]
+
+    def set_config(self, key, value):
+        """
+        Set a config value.
+        :param key: Key of config.
+        :param value: Value of config.
+        """
+        self.config[key] = value
+        self.update_subscribers()
+
+    def set_attribute(self, key, value):
+        """
+        Set an attribute value.
+        :param key: Key of attribute.
+        :param value: Value of attribute.
+        """
+        self.attributes[key] = value
+        self.update_subscribers()
+
+    def set_name(self, name):
+        """
+        Set the Node name.
+        :param name: Node name.
+        """
+        self.config["$name"] = name
+        self.update_subscribers()
+
+    def set_invokable(self, invokable):
+        """
+        Set invokable state.
+        :param invokable: Invokable permit.
+        :return: True on success.
+        """
+        if type(invokable) is str:
+            self.set_config("$invokable", invokable)
+            return True
+        return False
+
+    def set_parameters(self, parameters):
+        """
+        Set parameters for action.
+        :param parameters: Parameters for action.
+        :return: True on success.
+        """
+        if type(parameters) is list:
+            self.set_config("$params", parameters)
+            return True
+        return False
+
+    def set_columns(self, columns):
+        """
+        Set return columns for action.
+        :param columns: Columns for action.
+        :return: True on success.
+        """
+        if type(columns) is list:
+            self.set_config("$columns", columns)
+            return True
+        return False
+
+    def stream(self):
+        """
+        Stream the Node.
+        :return: Node stream.
+        """
+        out = []
+        for k in self.config:
+            out.append([k, self.config[k]])
+        for child in self.children:
+            child = self.children[child]
+            if child.has_value():
+                val = {
+                    "value": child.value.value,
+                    "ts": child.value.updated_at.isoformat()
+                }
+            else:
+                val = {}
+            l = len(child.path.split("/")) - 1
+            i = dict(child.config)
+            i.update(child.attributes)
+            i.update(val)
+            out.append([
+                child.path.split("/")[l],
+                i
+            ])
+        return out
+
+    def add_child(self, child):
+        """
+        Add a child to this Node.
+        :param child: Child to add.
+        """
+        self.children[child.name] = child
+
+        if self.standalone or self.link.active:
+            self.update_subscribers()
+
+    def remove_child(self, name):
+        """
+        Remove a child from this Node.
+        :param name: Child Node name.
+        """
+        del self.children[name]
+
+    def get(self, path):
+        """
+        Get a Node from this position on the Node structure.
+        :param path: Path of Node wanted.
+        :return: Node of path.
+        """
+        if path == "/":
+            return self
+        elif path.startswith("/$"):
+            return self
+        elif path.startswith("/@"):
+            return self
+        else:
+            try:
+                try:
+                    i = path.index("/", 2)
+                    child = path[1:i]
+                    return self.children[child].get(path[i:])
+                except ValueError:
+                    child = path[1:]
+                    return self.children[child]
+            except KeyError:
+                self.logger.warn("Non-existent Node requested %s on %s" % (path, self.path))
+
+    def set_config_attr(self, path, value):
+        """
+        Set value/config/attribute on Node.
+        :param path: Path of value to set.
+        :param value: Value to set.
+        """
+        if path == "/" or path == self.path:
+            self.set_value(value)
+        elif path.startswith("/$") or path.startswith(self.path + "/$"):
+            self.set_config(path[2:], value)
+        elif path.startswith("/@") or path.startswith(self.path + "/@"):
+            self.set_attribute(path[2:], value)
+        else:
+            self.get(path).set_config_attr(path, value)
+
+    def remove_config_attr(self, path):
+        """
+        Remove config/attribute on Node.
+        :param path: Path of value to remove.
+        """
+        if path.startswith("/$") or path.startswith(self.path + "/$"):
+            del self.config[path[2:]]
+        elif path.startswith("/@") or path.startswith(self.path + "/@"):
+            del self.config[path[2:]]
+        else:
+            self.get(path).remove_config_attr(path)
+
+    def is_subscribed(self):
+        """
+        Is the Node subscribed to?
+        :return: True if the Node is subscribed to.
+        """
+        return len(self.subscribers) is not 0
+
+    def invoke(self, params):
+        """
+        Invoke the Node.
+        :param params: Parameters of invoke.
+        :return: Columns and values
+        """
+        self.logger.debug("%s invoked, with parameters: %s" % (self.path, params))
+        # noinspection PyCallingNonCallable
+        return (self.config["$columns"] if "$columns" in self.config else []), self.invoke_callback(params)
+
+    def set_invoke_callback(self, callback):
+        """
+        Set the invoke callback.
+        :param callback: Callback to call on an invoke method.
+        """
+        if hasattr(callback, "__call__"):
+            self.invoke_callback = callback
+        else:
+            raise ValueError("Provided callback is not a function.")
+
+    def update_subscribers(self):
+        """
+        Send subscription updates.
+        """
+        responses = []
+        for stream in self.streams:
+            responses.append(Response({
+                "rid": stream,
+                "stream": "open",
+                "updates": self.stream()
+            }).get_stream())
+        if responses:
+            self.link.wsp.sendMessage({
+                "responses": responses
+            })
+
+    def update_subscribers_values(self):
+        """
+        Update all Subscribers of a Value change.
+        """
+        for s in self.subscribers:
+            self.link.wsp.sendMessage({
+                "responses": [
+                    {
+                        "rid": 0,
+                        "updates": [
+                            [
+                                s,
+                                self.value.value,
+                                self.value.updated_at.isoformat()
+                            ]
+                        ]
+                    }
+                ]
+            })
+
+    def add_subscriber(self, sid):
+        """
+        Add a Subscriber.
+        :param sid: Subscriber ID.
+        """
+        self.subscribers.append(sid)
+        self.update_subscribers_values()
+
+    def remove_subscriber(self, sid):
+        """
+        Remove a Subscriber.
+        :param sid: Subscriber ID.
+        """
+        self.subscribers.remove(sid)
