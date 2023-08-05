89c3318b66fe2f931aaf6f280860bc9cc57e2627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:59 +0000

    Add package nseter/1.3.0/nseter-1.3.0.zip

---
+++ packages/nseter/nseter-1.3.0.zip/nseter-1.3.0/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup    #从发布工具导入setup函数
+
+setup(
+    name= 'nseter',
+    version='1.3.0',
+    py_modules=['nester'],
+    author='yzj',
+    author_email='359618717@qq.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+ )
