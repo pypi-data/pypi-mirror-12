a7268f97974d03224001c2e2b2a3886c43eca54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:00 +0000

    Add package django-auto-one-to-one/2/django-auto-one-to-one-2.tar.gz

---
+++ packages/django-auto-one-to-one/django-auto-one-to-one-2.tar.gz/django-auto-one-to-one-2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='django-auto-one-to-one',
+    version='2',
+    packages=(
+        'django_auto_one_to_one',
+    ),
+    url='https://chris-lamb.co.uk/projects/django-auto-to-one',
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    description="Automatically create and destroy child model instances",
+)
