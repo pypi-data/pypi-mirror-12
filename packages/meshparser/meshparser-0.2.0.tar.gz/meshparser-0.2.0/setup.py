0506abdfc8043811b8cc1a9113865900618c10bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:20 +0000

    Add package meshparser/0.2.0/meshparser-0.2.0.tar.gz

---
+++ packages/meshparser/meshparser-0.2.0.tar.gz/meshparser-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+dependencies = []
+
+long_description = """A Python library that parses different mesh format files into a Python dict 
+ready for consumption by other libraries.
+"""
+print(find_packages(exclude=['tests', 'tests.*', ]))
+setup(name=u'meshparser',
+      version='0.2.0',
+      description='A Small Python library for parsing files that describe a mesh.',
+      long_description=long_description,
+      classifiers=[],
+      author=u'Hugh Sorby',
+      author_email='',
+      url='http://pypi.org/meshparser',
+      license='GPL',
+      packages=find_packages('src', exclude=['tests', 'tests.*', ]),
+      package_dir={'': 'src'},
+      zip_safe=True,
+      install_requires=dependencies,
+      )
