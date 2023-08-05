d0a953480847cf9934a13c0880964d40bafce157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:30 +0000

    Add package resin-sdk/1.1.0/resin-sdk-1.1.0.zip

---
+++ packages/resin-sdk/resin-sdk-1.1.0.zip/resin-sdk-1.1.0/resin/models/environment_variables.py
@@ -0,0 +1,235 @@
+import re
+
+from ..base_request import BaseRequest
+from .device import Device
+from ..settings import Settings
+
+
+class EnvironmentVariable(object):
+    """
+    This class is a wrapper for device and application environment variable models.
+
+    """
+
+    def __init__(self):
+        self.device = DeviceEnvVariable()
+        self.application = ApplicationEnvVariable()
+
+
+class DeviceEnvVariable(object):
+    """
+    This class implements device environment variable model for Resin Python SDK.
+
+    """
+
+    def __init__(self):
+        self.base_request = BaseRequest()
+        self.device = Device()
+        self.settings = Settings()
+
+    def get_all(self, uuid):
+        """
+        Get all device environment variables.
+
+        Args:
+            uuid (str): device uuid.
+
+        Returns:
+            list: device environment variables.
+
+        """
+
+        device = self.device.get(uuid)
+        params = {
+            'filter': 'device',
+            'eq': device['id']
+        }
+        return self.base_request.request(
+            'device_environment_variable', 'GET', params=params,
+            endpoint=self.settings.get('pine_endpoint')
+        )['d']
+
+    def create(self, uuid, name, value):
+        """
+        Create a device environment variable.
+
+        Args:
+            uuid (str): device uuid.
+            name (str): environment variable name.
+            value (str): environment variable value.
+
+        Returns:
+            dict: new device environment variable info.
+
+        """
+
+        device = self.device.get(uuid)
+        data = {
+            'device': device['id'],
+            'env_var_name': name,
+            'value': value
+        }
+        return self.base_request.request(
+            'device_environment_variable', 'POST', data=data,
+            endpoint=self.settings.get('pine_endpoint')
+        )
+
+    def update(self, var_id, value):
+        """
+        Update a device environment variable.
+
+        Args:
+            var_id (str): environment variable id.
+            value (str): new environment variable value.
+
+        """
+
+        params = {
+            'filter': 'id',
+            'eq': var_id
+        }
+        data = {
+            'value': value
+        }
+        return self.base_request.request(
+            'device_environment_variable', 'PATCH', params=params, data=data,
+            endpoint=self.settings.get('pine_endpoint')
+        )
+
+    def remove(self, var_id):
+        """
+        Remove a device environment variable.
+
+        Args:
+            var_id (str): environment variable id.
+
+        """
+
+        params = {
+            'filter': 'id',
+            'eq': var_id
+        }
+        return self.base_request.request(
+            'device_environment_variable', 'DELETE', params=params,
+            endpoint=self.settings.get('pine_endpoint')
+        )
+
+
+class ApplicationEnvVariable(object):
+    """
+    This class implements application environment variable model for Resin Python SDK.
+
+    Attributes:
+        SYSTEM_VARIABLE_RESERVED_NAMES (list): list of reserved system variable names.
+        OTHER_RESERVED_NAMES_START (list): list of prefix for system variable.
+
+    """
+
+    SYSTEM_VARIABLE_RESERVED_NAMES = ['RESIN', 'USER']
+    OTHER_RESERVED_NAMES_START = 'RESIN_'
+
+    def __init__(self):
+        self.base_request = BaseRequest()
+        self.settings = Settings()
+
+    def get_all(self, app_id):
+        """
+        Get all environment variables by application.
+
+        Args:
+            app_id (str): application id.
+
+        Returns:
+            list: application environment variables.
+
+        """
+
+        params = {
+            'filter': 'application',
+            'eq': app_id
+        }
+        return self.base_request.request(
+            'environment_variable', 'GET', params=params,
+            endpoint=self.settings.get('pine_endpoint')
+        )['d']
+
+    def create(self, app_id, name, value):
+        """
+        Create an environment variable for application.
+
+        Args:
+            app_id (str): application id.
+            name (str): environment variable name.
+            value (str): environment variable value.
+
+        Returns:
+            dict: new application environment info.
+
+        """
+
+        data = {
+            'name': name,
+            'value': value,
+            'application': app_id
+        }
+        return self.base_request.request(
+            'environment_variable', 'POST', data=data,
+            endpoint=self.settings.get('pine_endpoint')
+        )
+
+    def update(self, var_id, value):
+        """
+        Update an environment variable value for application.
+
+        Args:
+            var_id (str): environment variable id.
+            value (str): new environment variable value.
+
+        """
+
+        params = {
+            'filter': 'id',
+            'eq': var_id
+        }
+
+        data = {
+            'value': value
+        }
+        return self.base_request.request(
+            'environment_variable', 'PATCH', params=params, data=data,
+            endpoint=self.settings.get('pine_endpoint')
+        )
+
+    def remove(self, var_id):
+        """
+        Remove application environment variable.
+
+        Args:
+            var_id (str): environment variable id.
+
+        """
+
+        params = {
+            'filter': 'id',
+            'eq': var_id
+        }
+        return self.base_request.request(
+            'environment_variable', 'DELETE', params=params,
+            endpoint=self.settings.get('pine_endpoint')
+        )
+
+    def is_system_variable(self, variable):
+        """
+        Check if a variable is system specific.
+
+        Args:
+            variable (str): environment variable name.
+
+        Returns:
+            bool: True if system variable, False otherwise.
+
+        """
+
+        if variable in self.SYSTEM_VARIABLE_RESERVED_NAMES:
+            return True
+        return variable.startswith(self.OTHER_RESERVED_NAMES_START)
---
+++ packages/resin-sdk/resin-sdk-1.1.0.zip/resin-sdk-1.1.0/resin/settings.py
@@ -0,0 +1,153 @@
+import ConfigParser
+import os.path as Path
+import os
+
+from . import exceptions
+
+
+class Settings(object):
+    """
+    This class handles settings for Resin Python SDK.
+
+    Attributes:
+        HOME_DIRECTORY (str): home directory path.
+        CONFIG_SECTION (str): section name in configuration file.
+        CONFIG_FILENAME (str): configuration file name.
+        _setting (dict): default value to settings.
+
+    """
+
+    HOME_DIRECTORY = Path.expanduser('~')
+    CONFIG_SECTION = 'Settings'
+    CONFIG_FILENAME = 'resin.cfg'
+
+    _setting = {
+        'pine_endpoint': 'https://api.resin.io/ewa/',
+        'api_endpoint': 'https://api.resin.io/',
+        'data_directory': Path.join(HOME_DIRECTORY, '.resin'),
+        # cache time : 1 week in milliseconds
+        'image_cache_time': (1 * 1000 * 60 * 60 * 24 * 7),
+        # token refresh interval: 1 hour in milliseconds
+        'token_refresh_interval': (1 * 1000 * 60 * 60)
+    }
+
+    _setting['cache_directory'] = Path.join(_setting['data_directory'],
+                                            'cache')
+
+    def __init__(self):
+        if Path.isdir(self._setting['data_directory']):
+            if Path.isfile(Path.join(self._setting['data_directory'],
+                                     self.CONFIG_FILENAME)):
+                self.__read_settings()
+            else:
+                self.__write_settings()
+        else:
+            # data directory doesn't exist
+            self.__write_settings()
+
+    def __write_settings(self):
+        config = ConfigParser.ConfigParser()
+        config.add_section(self.CONFIG_SECTION)
+        for key in self._setting:
+            config.set(self.CONFIG_SECTION, key, self._setting[key])
+        if not Path.isdir(self._setting['data_directory']):
+            os.makedirs(self._setting['data_directory'])
+        with open(Path.join(self._setting['data_directory'],
+                            self.CONFIG_FILENAME), 'wb') as config_file:
+            config.write(config_file)
+
+    def __read_settings(self):
+        config_reader = ConfigParser.ConfigParser()
+        config_reader.read(Path.join(self._setting['data_directory'],
+                                     self.CONFIG_FILENAME))
+        config_data = {}
+        options = config_reader.options(self.CONFIG_SECTION)
+        for option in options:
+            try:
+                config_data[option] = config_reader.get(self.CONFIG_SECTION,
+                                                        option)
+            except:
+                config_data[option] = None
+        self._setting = config_data
+
+    def has(self, key):
+        """
+        Check if a setting exists.
+
+        Args:
+            key (str): setting.
+
+        Returns:
+            bool: True if exists, False otherwise.
+
+        """
+
+        self.__read_settings()
+        if key in self._setting:
+            return True
+        return False
+
+    def get(self, key):
+        """
+        Get a setting value.
+
+        Args:
+            key (str): setting.
+
+        Returns:
+            str: setting value.
+
+        Raises:
+            InvalidOption: If getting a non-existent setting.
+
+        """
+
+        try:
+            self.__read_settings()
+            return self._setting[key]
+        except KeyError:
+            raise exceptions.InvalidOption(key)
+
+    def get_all(self):
+        """
+        Get all settings.
+
+        Returns:
+            dict: all settings.
+
+        """
+
+        self.__read_settings()
+        return self._setting
+
+    def set(self, key, value):
+        """
+        Set value for a setting.
+
+        Args:
+            key (str): setting.
+            value (str): setting value.
+
+        """
+
+        self._setting[key] = str(value)
+        self.__write_settings()
+
+    def remove(self, key):
+        """
+        Remove a setting.
+
+        Args:
+            key (str): setting.
+
+        Returns:
+            bool: True if successful, False otherwise.
+
+        """
+
+        # if key is not in settings, return False
+        result = self._setting.pop(key, False)
+        if result is not False:
+            self.__write_settings()
+            return True
+        return False
