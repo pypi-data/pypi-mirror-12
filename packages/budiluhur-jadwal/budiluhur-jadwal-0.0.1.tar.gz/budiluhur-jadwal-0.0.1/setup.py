2ebe37dba8cd9c81d115c0d9e6d3563cbd6328d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:18 +0000

    Add package budiluhur-jadwal/0.0.1/budiluhur-jadwal-0.0.1.tar.gz

---
+++ packages/budiluhur-jadwal/budiluhur-jadwal-0.0.1.tar.gz/budiluhur-jadwal-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+long_description = '''
+`Application Programming Interface` untuk mengambil data Jadwal
+harian perkuliahan dari semua fakultas di Kampus `Universitas Budiluhur Jakarta`.
+
+## Quick Start
+
+```python
+from budiluhur.jadwal import fakultas
+
+# inisialisasi class Fakultas
+f = fakultas.Fakultas()
+fakultas_bl = f.get_all()
+print(fakultas_bl)
+```
+'''
+
+setup(
+	name             = 'budiluhur-jadwal',
+	version          = '0.0.1',
+	description      = 'Python API Jadwal Perkulahan Budiluhur',
+	author           = 'Yanwar Solahudin',
+	author_email     = 'yanwarsolahudinn@gmail.com',
+	url              = 'https://github.com/yanwarsolahudinn/budiluhur-jadwal',
+	download_url     = 'https://github.com/yanwarsolahudinn/budiluhur-jadwal/archive/master.zip',
+	packages         = ['budiluhur'],
+	package_data     = {'budiluhur' : ['jadwal/*.*'] },
+	include_package_data = True,
+	long_description = long_description,
+	keywords         = ['API', 'JSON', 'budiluhur', 'jadwal', 'track', 'schedule', 'kuliah'],
+	classifiers      = [],
+	install_requires = ["requests", "beautifulsoup4", "python-Levenshtein", "rft1d"]
+)
