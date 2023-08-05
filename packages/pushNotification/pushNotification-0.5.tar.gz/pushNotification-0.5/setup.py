ac8eb13616bef5201a03c3c24857487f651cfd51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:36 +0000

    Add package pushNotification/0.5/pushNotification-0.5.tar.gz

---
+++ packages/pushNotification/pushNotification-0.5.tar.gz/pushNotification-0.5/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pushNotification',
+      version='0.5',
+      description='Push Notification to Android Phones using GCM',
+      author='Mohamed El-Kalioby',
+      author_email='mkalioby@mkalioby.com',
+      url='https://github.com/mkalioby/Python_Notifications',
+      download_url="https://github.com/mkalioby/Python_Notifications/archive/0.1.tar.gz",
+      packages=['pushNotification'],
+      keywords = ['android', 'notificatio'],
+     )
