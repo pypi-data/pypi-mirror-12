7b3784cfe0f9bd3ef9b3be5a61368d2868b465b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:51 +0000

    Add package django-sass-processor/0.2.6/django-sass-processor-0.2.6.tar.gz

---
+++ packages/django-sass-processor/django-sass-processor-0.2.6.tar.gz/django-sass-processor-0.2.6/README.md
@@ -0,0 +1,189 @@
+# django-sass-processor
+
+Processor to compile files from markup languages such as SASS/SCSS, if referenced using the
+templatetag ``{% sass_src 'path/to/file.scss' %}``. This special templatetag can be used instead of
+the built-in tag ``static``.
+
+Additionally, **django-sass-processor** is shipped with a management command, which can convert
+the content of all occurrences inside the templatetag ``sass_src`` as an offline operation. Hence
+the **libsass** compiler is not required in a production environment.
+
+During development, a [sourcemap](https://developer.chrome.com/devtools/docs/css-preprocessors) is
+generated along side with the compiled ``*.css`` file. This allows to debug style sheet errors much
+easier.
+
+With this tool, you can safely remove your Ruby installations “Compass” and “SASS” from your Django
+projects.
+
+
+## Project's Home
+
+On GitHub:
+
+https://github.com/jrief/django-sass-processor
+
+Please use the issue tracker to report bugs or propose new features.
+
+## Installation
+
+```
+pip install libsass django-compressor django-sass-processor
+```
+
+``django-compressor`` is required only for offline compilation, when using the command
+``manage.py compilescss``.
+
+``libsass`` is not required on the production environment, if SASS/SCSS files have been precompiled
+and deployed using offline compilation.
+
+## Configuration
+
+In ``settings.py`` add to:
+
+```python
+INSTALLED_APPS = (
+    ...
+    'sass_processor',
+    ...
+)
+```
+
+Optionally, add a list of additional search paths, the SASS compiler may examine when using the
+``@import "...";`` statement in SASS/SCSS files:
+
+```python
+import os
+
+SASS_PROCESSOR_INCLUDE_DIRS = (
+    os.path.join(PROJECT_PATH, 'mystyles/scss'),
+    os.path.join(PROJECT_PATH, 'node_modules'),
+)
+```
+
+During development, or when ``SASS_PROCESSOR_ENABLED`` is set to ``True``, the compiled file is
+placed into the folder referenced by ``SASS_PROCESSOR_ROOT`` (if unset, this setting defaults to
+``STATIC_ROOT``). Having a location outside of the working directory prevents to pollute your local
+``static/css/...`` folders with auto-generated files. Therefore assure, that this directory is
+writable by the Django runserver.
+
+**django-sass-processor** is shipped with a special finder, to locate the generated ``*.css`` files
+in the folder referred by ``SASS_PROCESSOR_ROOT`` (or, if unset ``STATIC_ROOT``). Just add it to
+your ``settings.py``: 
+
+```
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+    ...
+)
+```
+
+## Preprocessing SASS
+
+**django-sass-processor** is shipped with a built-in preprocessor to convert ``*.scss`` or
+``*.sass`` files into ``*.css`` while rendering the template. For performance reasons
+this is done only once, but the preprocessor keeps track on the timestamps and recompiles, if
+any of the imported SASS/SCSS files is younger than the corresponding generated CSS file.
+
+
+### In your Django templates
+
+```html
+{% load sass_tags %}
+
+<link href="{% sass_src 'myapp/css/mystyle.scss' %}" rel="stylesheet" type="text/css" />
+```
+
+You can safely use this templatetag inside a Sekizai's ``{% addtoblock "css" %}`` statement.
+
+
+## Offline compilation
+
+If you want to precompile all occurrences of your SASS/SCSS files for the whole project, on the
+command line invoke:
+
+```
+./manage.py compilescss
+```
+
+This is useful for preparing production environments, where SASS/SCSS files can't be compiled on
+the fly. To simplify the deployment, the compiled ``*.css`` files are stored side-by-side with their
+corresponding SASS/SCSS files; just run ``./manage.py collectstatic`` afterwards. In case you
+don't want to expose the SASS/SCSS files in a production environment, deploy with
+``./manage.py collectstatic --ignore=.scss``.
+
+In case you want to get rid of the compiled ``*.css`` files in your local static directories, simply
+reverse the above command:
+
+```
+./manage.py compilescss --delete-files
+```
+
+This will remove all occurrences of previously generated ``*.css`` files.
+
+
+## Configure SASS variables through settings.py
+
+In SASS, a nasty problem is to set the correct include paths for icons and fonts. Normally this is
+done through a ``_variables.scss`` file, but this inhibits a configuration through your projects
+``settings.py``.
+
+Starting with version 0.2.5 **django-sass-processor** offers a SASS function to fetch any arbitrary
+configuration from ``settings.py``. This is specially handy for setting the include path of your
+Glyphicons font directory. Assume you installed Bootstrap SASS files using
+
+```npm install bootstrap-sass```
+
+then locate your ``node_modules`` folder and add it to your ``settings.py``, so that your fonts are
+accessible through the Django's ``django.contrib.staticfiles.finders.FileSystemFinder``:
+
+```
+STATICFILES_DIRS = (
+    ...
+    ('node_modules', '/path/to/your/project/node_modules/'),
+    ...
+)
+
+NODE_MODULES_URL = STATIC_URL + 'node_modules/'
+
+```
+
+With the SASS function ``get-setting``, you now can override any SASS variable with a configurable
+value. For the Glyphicons font search path, add this to your ``_variables.scss``:
+
+```
+$icon-font-path: unquote(get-setting(NODE_MODULES_URL) + "bootstrap-sass/assets/fonts/bootstrap/");
+```
+
+and ``@import "variables";`` whenever you need Glyphicons. You then can safely remove any
+font references, such as ``<link href="/path/to/your/fonts/bootstrap/glyphicons-whatever.ttf" ...>``
+from you HTML templates.
+
+
+## Changelog
+
+* 0.2.6
+ - Hotfix: added SASS function ``get-setting`` also to offline compiler.
+
+* 0.2.5
+ - Compatible with Python3
+ - Replaced ``SortedDict`` with ``OrderedDict`` to be prepared for Django-1.9
+ - Raise a ``TemplateSyntax`` error, if a SASS ``@include "..."`` fails to find the file.
+ - Added SASS function ``get-setting`` to fetch configuration directives from ``settings.py``.
+
+* 0.2.4
+ - Forcing compiled unicode to bytes, since 'Font Awesome' uses Unicode Private Use Area (PUA)
+   and hence implicit conversion on ``fh.write()`` failed.
+
+* 0.2.3 
+ - Allow for setting template extensions and output style.
+ - Force Django to calculate template_source_loaders from TEMPLATE_LOADERS settings, by asking to find a dummy template.
+
+* 0.2.0 
+ - Removed dependency to **django-sekizai** and **django-classy-tags**. It now can operate in
+   stand-alone mode. Therefore the project has been renamed to **django-sass-processor**.
+
+* 0.1.0 
+ - Initial revision named **django-sekizai-processors**, based on a preprocessor for the Sekizai
+   template tags ``{% addtoblock %}``.
---
+++ packages/django-sass-processor/django-sass-processor-0.2.6.tar.gz/django-sass-processor-0.2.6/sass_processor/management/commands/compilescss.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+import os
+import sass
+from optparse import make_option
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.template.loader import get_template  # noqa Leave this in to preload template locations
+from django.utils.importlib import import_module
+from django.utils.encoding import force_bytes
+from compressor.offline.django import DjangoParser
+from compressor.exceptions import TemplateDoesNotExist, TemplateSyntaxError
+from sass_processor.templatetags.sass_tags import SassSrcNode, get_setting
+from sass_processor.storage import find_file
+
+
+class Command(BaseCommand):
+    help = "Compile SASS/SCSS into CSS outside of the request/response cycle"
+    option_list = BaseCommand.option_list + (make_option('--delete-files', action='store_true',
+        dest='delete_files', default=False, help='Delete generated `*.css` files instead of creating them.'),)
+
+    def __init__(self):
+        self.parser = DjangoParser(charset=settings.FILE_CHARSET)
+        self.template_exts = getattr(settings, 'SASS_TEMPLATE_EXTS', ['.html'])
+        self.output_style = getattr(settings, 'SASS_OUTPUT_STYLE', 'compact')
+        super(Command, self).__init__()
+
+    def handle(self, *args, **options):
+        self.verbosity = int(options['verbosity'])
+        self.delete_files = options['delete_files']
+        self.compiled_files = []
+        templates = self.find_templates()
+        for template_name in templates:
+            self.parse_template(template_name)
+        if self.verbosity > 0:
+            if self.delete_files:
+                self.stdout.write('Successfully deleted {0} previously generated `*.css` files.'.format(len(self.compiled_files)))
+            else:
+                self.stdout.write('Successfully compiled {0} referred SASS/SCSS files.'.format(len(self.compiled_files)))
+
+    def find_templates(self):
+        paths = set()
+        for loader in self.get_loaders():
+            try:
+                module = import_module(loader.__module__)
+                get_template_sources = getattr(module, 'get_template_sources', loader.get_template_sources)
+                paths.update(list(get_template_sources('')))
+            except (ImportError, AttributeError):
+                pass
+        if not paths:
+            raise CommandError("No template paths found. None of the configured template loaders provided template paths")
+        templates = set()
+        for path in paths:
+            for root, _, files in os.walk(path):
+                templates.update(os.path.join(root, name)
+                    for name in files if not name.startswith('.') and
+                        any(name.endswith(ext) for ext in self.template_exts))
+        if not templates:
+            raise CommandError("No templates found. Make sure your TEMPLATE_LOADERS and TEMPLATE_DIRS settings are correct.")
+        return templates
+
+    def get_loaders(self):
+        try:
+            from django.template.loader import (
+                find_template as finder_func)
+        except ImportError:
+            from django.template.loader import (find_template_source as finder_func)
+        try:
+            # Force Django to calculate template_source_loaders from
+            # TEMPLATE_LOADERS settings, by asking to find a dummy template
+            finder_func('test')
+        # Had to transform this Exception, because otherwise even if there
+        # was a try catch it was crashing, this is a broad Exception but at
+        # it does what the try catch does by not crashing the command line
+        # execution.
+        except Exception:
+            pass
+        loaders = []
+        # At the top when you first import template_source_loaders it is set
+        # to None, because in django that is what it is set too. While it
+        # executes the finder_func it is setting the template_source_loaders
+        # I needed to re-import the value of it at this point because it was
+        # still None and importing it again made it filled with the proper
+        # django default values.
+        from django.template.loader import template_source_loaders
+        for loader in template_source_loaders:
+            if hasattr(loader, 'loaders'):
+                loaders.extend(loader.loaders)
+            else:
+                loaders.append(loader)
+        return loaders
+
+    def parse_template(self, template_name):
+        try:
+            template = self.parser.parse(template_name)
+        except IOError:  # unreadable file -> ignore
+            self.stdout.write("Unreadable template at: %s\n" % template_name)
+            return
+        except TemplateSyntaxError as e:  # broken template -> ignore
+            self.stdout.write("Invalid template %s: %s\n" % (template_name, e))
+            return
+        except TemplateDoesNotExist:  # non existent template -> ignore
+            self.stdout.write("Non-existent template at: %s\n" % template_name)
+            return
+        except UnicodeDecodeError:
+            self.stdout.write("UnicodeDecodeError while trying to read template %s\n" % template_name)
+        try:
+            nodes = list(self.walk_nodes(template))
+        except Exception as e:
+            # Could be an error in some base template
+            self.stdout.write("Error parsing template %s: %s\n" % (template_name, e))
+        else:
+            for node in nodes:
+                if self.delete_files:
+                    self.delete_file(node)
+                else:
+                    self.compile(node)
+
+    def compile(self, node):
+        sass_filename = find_file(node.path)
+        if not sass_filename or sass_filename in self.compiled_files:
+            return
+        custom_functions = {'get-setting': get_setting}
+        content = sass.compile(include_paths=node.include_paths, filename=sass_filename,
+            custom_functions=custom_functions, output_style=self.output_style)
+        basename, _ = os.path.splitext(sass_filename)
+        destpath = basename + '.css'
+        with open(destpath, 'w') as fh:
+            fh.write(force_bytes(content))
+        self.compiled_files.append(sass_filename)
+        if self.verbosity > 1:
+            self.stdout.write("Compiled SASS/SCSS file: '{0}'\n".format(node.path))
+
+    def delete_file(self, node):
+        """
+        Delete a *.css file, but only if it has been generated through a SASS/SCSS file.
+        """
+        sass_filename = find_file(node.path)
+        if not sass_filename:
+            return
+        basename, _ = os.path.splitext(sass_filename)
+        destpath = basename + '.css'
+        if os.path.isfile(destpath):
+            os.remove(destpath)
+            self.compiled_files.append(sass_filename)
+            if self.verbosity > 1:
+                self.stdout.write("Deleted '{0}'\n".format(destpath))
+
+    def walk_nodes(self, node):
+        """
+        Iterate over the nodes recursively yielding the templatetag 'sass_src'
+        """
+        for node in self.parser.get_nodelist(node):
+            if isinstance(node, SassSrcNode):
+                if node.is_sass:
+                    yield node
+            else:
+                for node in self.walk_nodes(node):
+                    yield node
