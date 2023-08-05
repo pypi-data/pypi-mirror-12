e138282e4df58a6e9c29ac1b476fb3fc0f590999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:04 +0000

    Add package govtrack2csv/1.0a0/govtrack2csv-1.0a0.tar.gz

---
+++ packages/govtrack2csv/govtrack2csv-1.0a0.tar.gz/govtrack2csv-1.0a0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='govtrack2csv',
+      version='1.0-alpha',
+      description='Toolset for converting govtrack data to csv.',
+      url='http://github.com/hackthefed/govtrack2csv',
+      author='Hack The Fed',
+      author_email='vance@hackthefed.com',
+      license='GPL',
+      packages=['govtrack2csv'],
+      scripts=['bin/convert_congress'],
+      install_requires=[
+          'pandas',
+          'pyyaml'
+      ],
+      zip_safe=False)
