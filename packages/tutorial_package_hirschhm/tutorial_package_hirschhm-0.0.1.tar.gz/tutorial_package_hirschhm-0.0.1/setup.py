79ead7385d34abc02c81d6592c0d856e8209f5c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:37 +0000

    Add package tutorial_package_hirschhm/0.0.1/tutorial_package_hirschhm-0.0.1.tar.gz

---
+++ packages/tutorial_package_hirschhm/tutorial_package_hirschhm-0.0.1.tar.gz/tutorial_package_hirschhm-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="tutorial_package_hirschhm",
+    version="0.0.1",
+    author="Matthias Hirsch-Hoffmann",
+    description="An demonstration of how to create and publish python packages",
+    license="BSD",
+    packages=['tutorial_package_hirschhm'],
+)
