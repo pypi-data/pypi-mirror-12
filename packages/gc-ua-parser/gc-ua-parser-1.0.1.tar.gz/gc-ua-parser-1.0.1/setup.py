d2e9a767d9240833e3063717b3dcac219bfac225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:32 +0000

    Add package gc-ua-parser/1.0.1/gc-ua-parser-1.0.1.tar.gz

---
+++ packages/gc-ua-parser/gc-ua-parser-1.0.1.tar.gz/gc-ua-parser-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+setup(
+    name='gc-ua-parser',
+    version='1.0.1',
+    author='Travis Thieman',
+    author_email='travis@gc.io',
+    packages=['ua_parser'],
+    package_dir={'': '.'},
+    zip_safe=False,
+    url='https://github.com/gamechanger/uap-python',
+    ext_modules = [Extension("ua_parser/user_agent_parser", ["ua_parser/user_agent_parser.c"])]
+)
