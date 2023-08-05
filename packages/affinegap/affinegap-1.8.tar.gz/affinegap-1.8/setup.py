1f321bdc2bc100a4d02d4f9175c3ac559c300903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:46 +0000

    Add package affinegap/1.8/affinegap-1.8.tar.gz

---
+++ packages/affinegap/affinegap-1.8.tar.gz/affinegap-1.8/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, Extension
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+setup(
+    name='affinegap',
+    url='https://github.com/datamade/affinegap',
+    version='1.8',
+    description='A Cython implementation of the affine gap string distance',
+    packages=['affinegap'],
+    ext_modules=[Extension('affinegap.affinegap', ['affinegap/affinegap.c'])],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Cython', 
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis']
+    )
+
