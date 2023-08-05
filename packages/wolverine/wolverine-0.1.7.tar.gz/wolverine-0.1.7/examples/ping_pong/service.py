ca855b90da3b611d998fe8858a5692a9f676b28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:48 +0000

    Add package wolverine/0.1.7/wolverine-0.1.7.tar.gz

---
+++ packages/wolverine/wolverine-0.1.7.tar.gz/wolverine-0.1.7/examples/ping_pong/service.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+from wolverine.module.service import ServiceMessage
+
+logger = logging.getLogger(__name__)
+
+
+class PingPongService(object):
+
+    def __init__(self, **options):
+        self.delay = options.pop('delay', 1)
+        self.routing = options.pop('routing', False)
+        self.name = 'ping'
+        self.options = {}
+
+    def ping1(self, data):
+        logger.debug('--ping1 handler--')
+        logger.debug('data: ' + str(data))
+        yield from asyncio.sleep(self.delay)
+        return data
+
+    def ping(self, data):
+        logger.debug('data: ' + str(data))
+        yield from asyncio.sleep(self.delay)
+        response = ServiceMessage()
+        response.data = data
+        return response
+
+    def ping2(self, data):
+        logger.debug('--ping1 handler--')
+        logger.debug('data: ' + str(data))
+        yield from asyncio.sleep(self.delay)
+        return data
+
