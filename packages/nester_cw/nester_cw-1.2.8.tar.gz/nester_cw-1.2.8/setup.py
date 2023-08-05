e13b06a255e785701a4ae24b6e7ccc4d46575e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:29 +0000

    Add package nester_cw/1.2.8/nester_cw-1.2.8.tar.gz

---
+++ packages/nester_cw/nester_cw-1.2.8.tar.gz/nester_cw-1.2.8/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nester_cw',
+	version		= '1.2.8',
+	py_modules	= ['nester'],
+	author		= 'hfpython',
+	author_mail	= 'carywill.k@gmail.com',
+	description 	= 'A simple printer of nested lists',
+        features        = 'add optional parameter',
+     )
+    
