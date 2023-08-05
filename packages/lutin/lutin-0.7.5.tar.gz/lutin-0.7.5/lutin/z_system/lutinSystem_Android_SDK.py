b219f46dac398a310f2d118ac5afb86c68211521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:17 +0000

    Add package lutin/0.7.5/lutin-0.7.5.tar.gz

---
+++ packages/lutin/lutin-0.7.5.tar.gz/lutin-0.7.5/lutin/z_system/lutinSystem_Android_SDK.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+##
+## @author Edouard DUPIN
+##
+## @copyright 2012, Edouard DUPIN, all right reserved
+##
+## @license APACHE v2.0 (see license file)
+##
+
+from lutin import debug
+from lutin import system
+from lutin import tools
+import os
+
+class System(system.System):
+	def __init__(self, target):
+		system.System.__init__(self)
+		# create some HELP:
+		self.help="SDK: Android SDK basic interface java\n"
+		# TODO : Check if the android sdk android.jar is present ...
+		self.valid = True
+		# todo : create a searcher of the presence of the library:
+		self.add_export_SRC(target.path_sdk + "/platforms/android-" + str(target.boardId) + "/android.jar")
+		self.add_export_flag_LD("-ldl")
+		self.add_export_flag_LD("-llog")
+		self.add_export_flag_LD("-landroid")
+
+
