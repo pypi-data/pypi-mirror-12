1c3e8b54aefd61c7df45e3092eb2626978392d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:01 +0000

    Add package test-kernel/0.5/test-kernel-0.5.zip

---
+++ packages/test-kernel/test-kernel-0.5.zip/test-kernel-0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+try:
+    from testkernel import cmdclass
+except:
+    import pip, importlib
+    pip.main(['install', 'test-kernel']); cmdclass = importlib.import_module('testkernel').cmdclass
+    
+setup(
+    name='test-kernel',
+    version='0.5',
+    description='Test custom kernel pip installable!',
+    author='John Coady',
+    author_email='johncoady@shaw.ca',
+    license='New BSD License',
+    url='https://github.com/jcoady/test-kernel',
+    keywords='python jupyter ipython kernel',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Programming Language :: Python',
+                 'Framework :: IPython',
+                 'License :: OSI Approved'],
+    packages=['testkernel'],
+    install_requires=["test-kernel"],
+    cmdclass=cmdclass('testkernel/data'),
+    package_data={'testkernel': ['data/kernel.json','data/profile_vpython/*.py',
+                                 'data/profile_vpython/log/*','data/profile_vpython/pid/*',
+                                 'data/profile_vpython/security/*','data/profile_vpython/startup/*']},
+
+)
