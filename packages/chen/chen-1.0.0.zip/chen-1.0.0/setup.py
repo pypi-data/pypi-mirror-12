b75f20725e2360ea653136ecb458d11734f5fd38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:01 +0000

    Add package chen/1.0.0/chen-1.0.0.zip

---
+++ packages/chen/chen-1.0.0.zip/chen-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'chen',
+	version = '1.0.0',
+	py_modules = ['chen'],
+	author = 'chen95',
+	author_email = 'chen317826620@gmail.com',
+	url = 'http://www.chen95.com',
+	description = 'A simple printer of nested lists',
+	)
