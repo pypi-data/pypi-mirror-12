5df2500253061fb82cf884e60285eb20e58e187e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package pysend/0.0.5/pysend-0.0.5.tar.gz

---
+++ packages/pysend/pysend-0.0.5.tar.gz/pysend-0.0.5/pysend/sendgridWebApi.py
@@ -0,0 +1,29 @@
+import requests
+from .classes import Contact, Email
+from . import classes
+
+class Server(classes.Server):
+   def __init__(self, url, apiKey):
+      self.url = url
+      self.apiKey = apiKey
+
+   def connect(self):
+      pass
+
+   # FixMe: [correctness] Support more than one recipient
+   # FixMe: [correctness] Support cc and bcc fields
+   def send(self, email):
+      headers = {
+         'Authorization': 'Bearer {}'.format(self.apiKey)
+      }
+
+      payload = {
+         'from': email.sender.email,
+         'fromname': email.sender.name,
+         'to': email.receivers[0].email,
+         'toname': email.receivers[0].name,
+         'subject': email.subject,
+         'html': email.body
+      }
+
+      requests.post(self.url, headers=headers, data=payload)
