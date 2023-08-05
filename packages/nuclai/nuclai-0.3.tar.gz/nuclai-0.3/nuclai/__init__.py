420fb4fddbc99f877b91e0adf6db104c88e6e860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:58 +0000

    Add package nuclai/0.3/nuclai-0.3.tar.gz

---
+++ packages/nuclai/nuclai-0.3.tar.gz/nuclai-0.3/nuclai/__init__.py
@@ -0,0 +1,19 @@
+# Package information for PYPI.
+__name__ = 'nuclai'
+__author__ = 'alexjc'
+
+# Check version numbers.
+import sys
+ver = sys.version_info
+assert ver.major == 3 and ver.minor >= 4, "Unsupported Python version."
+
+# Use colored console output.
+try:
+    import colorama; colorama.init(); del colorama
+except ImportError:
+    pass
+
+# Call the main entry point.
+def run():
+    from .__main__ import main
+    sys.exit(main(sys.argv))
