5306cfe1a611987edc40e379f041702db65b7fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:49 +0000

    Add package yzj_sanitize/1.0.0/yzj_sanitize-1.0.0.zip

---
+++ packages/yzj_sanitize/yzj_sanitize-1.0.0.zip/yzj_sanitize-1.0.0/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup    #从发布工具导入setup函数
+
+setup(
+    name= 'yzj_sanitize',
+    version='1.0.0',
+    py_modules=['yzj_sanitize'],
+    author='yzj',
+    author_email='359618717@qq.com',
+    url='http://www.headfirstlabs.com',
+    description="make '-'or':'to'.'",
+ )
---
+++ packages/yzj_sanitize/yzj_sanitize-1.0.0.zip/yzj_sanitize-1.0.0/yzj_sanitize.py
@@ -0,0 +1,12 @@
+__author__ = 'Administrator'
+#coding=utf-8
+import sys
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter='-'
+    elif ':' in time_string:
+        splitter=':'
+    else:
+        return(time_string)
+    (mins,secs)=time_string.split(splitter)
+    return(mins+'.'+secs)
