427cccffae640e831bf0e1121a98b72d77afe986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:05 +0000

    Add package nester-plough/1.3.0/nester-plough-1.3.0.tar.gz

---
+++ packages/nester-plough/nester-plough-1.3.0.tar.gz/nester-plough-1.3.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+
+setup(
+    name='nester-plough',
+    version='1.3.0',
+    py_modules=['nester'],
+    author='plough',
+    author_email='tzgdaydayup@gmail.com',
+    url='http://plough-man.com',
+    description='A simple printer of nested lists',
+)
