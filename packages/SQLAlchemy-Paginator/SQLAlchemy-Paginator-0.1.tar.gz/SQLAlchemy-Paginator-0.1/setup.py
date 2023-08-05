916a2a5e4cf328274441b119850d9999d232fb66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:25 +0000

    Add package SQLAlchemy-Paginator/0.1/SQLAlchemy-Paginator-0.1.tar.gz

---
+++ packages/SQLAlchemy-Paginator/SQLAlchemy-Paginator-0.1.tar.gz/SQLAlchemy-Paginator-0.1/setup.py
@@ -0,0 +1,29 @@
+'''
+Created on Oct 20, 2015
+
+@author: ahmadjaved.se@gmail.com
+'''
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='SQLAlchemy-Paginator',
+      version='0.1',
+      description='Paginator for SQLAlchemy ORM',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 2.7',
+      ],
+      keywords='sqlalchemy pagination paginator paginate sqlalchemy-orm paging slicing sqlalchemy-query',
+      url='https://github.com/ahmadjavedse/sqlalchemy-paginator.git',
+      author='Ahmad Javed',
+      author_email='ahmadjaved.se@gmail.com',
+      license='',
+      packages=find_packages(exclude=['tests*']),
+      include_package_data=True,
+      zip_safe=False)
