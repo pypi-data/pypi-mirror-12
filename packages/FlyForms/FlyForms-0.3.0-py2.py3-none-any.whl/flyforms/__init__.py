5f4b0dc590611469c611a8c450311f372815f841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:11 +0000

    Add package FlyForms/0.3.0/FlyForms-0.3.0-py2.py3-none-any.whl

---
+++ packages/FlyForms/FlyForms-0.3.0-py2.py3-none-any.whl/flyforms/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+__version__ = '0.1.0'
+
+
+from flyforms.form import *
+from flyforms.fields import *
+from flyforms.validators import ValidationError
+from flyforms.common import *
