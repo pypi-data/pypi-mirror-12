04f76a19d3636be37b8397a676423fd4258c24b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:25 +0000

    Add package midas/0.2.10/midas-0.2.10.tar.gz

---
+++ packages/midas/midas-0.2.10.tar.gz/midas-0.2.10/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="midas",
+    version="0.2.10",
+    description="Python driver for Honeywell Midas gas dectectors.",
+    url="http://github.com/numat/midas/",
+    author="Patrick Fuller",
+    author_email="pat@numat-tech.com",
+    packages=['midas'],
+    package_data={'midas': ['midas/faults.csv']},
+    include_package_data=True,
+    install_requires=['pymodbus'],
+    entry_points={
+        'console_scripts': [('midas = midas:command_line')]
+    },
+    license='GPLv2',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces'
+    ]
+)
