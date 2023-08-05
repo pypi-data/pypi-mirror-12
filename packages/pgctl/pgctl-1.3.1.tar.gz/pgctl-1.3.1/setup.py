deeaf6ddf51fe562a2f7f4ea8b525feb7edc310f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:27 +0000

    Add package pgctl/1.3.1/pgctl-1.3.1.tar.gz

---
+++ packages/pgctl/pgctl-1.3.1.tar.gz/pgctl-1.3.1/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from setuptools import find_packages
+from setuptools import setup
+
+from pgctl import __version__
+
+
+def main():
+    setup(
+        name=str('pgctl'),
+        description='A tool to configure and manage developer playgrounds.',
+        url='http://pgctl.readthedocs.org/en/latest/',
+        version=__version__,
+        platforms='linux',
+        classifiers=[
+            'Programming Language :: Python :: 2.7',
+        ],
+        packages=find_packages(exclude=('tests*',)),
+        install_requires=[
+            'argparse',
+            'frozendict',
+            'cached-property',
+            'py',
+            'six',
+            's6',
+        ],
+        # FIXME: all tests still pass if you break this.
+        entry_points={
+            'console_scripts': [
+                'pgctl-2015 = pgctl.cli:main',
+                'pgctl-poll-ready = pgctl.poll_ready:main',
+                'pgctl-timestamp = pgctl.timestamp:main',
+                'pgctl-fuser = pgctl.fuser:main',
+            ],
+        },
+
+        author='Buck Evan',
+        author_email='buck.2019@gmail.com',
+    )
+
+
+if __name__ == '__main__':
+    exit(main())
