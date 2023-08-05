135c92e03498b4fb32cbdb4d3bfbc37d11049a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:54 +0000

    Add package processout/2.0.0/processout-2.0.0.tar.gz

---
+++ packages/processout/processout-2.0.0.tar.gz/processout-2.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'processout',
+  packages = ['ProcessOut'],
+  version = '2.0.0',
+  description = 'ProcessOut API bindings.',
+  author = 'ProcessOut',
+  author_email = 'hi@processout.com',
+  url = 'https://github.com/processout/processout-python',
+  download_url = 'https://github.com/processout/processout-python/tarball/2.0.0',
+  keywords = ['ProcessOut', 'api', 'bindings'],
+  classifiers = [],
+)
