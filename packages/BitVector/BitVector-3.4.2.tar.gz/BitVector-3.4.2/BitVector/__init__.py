79bfede49a6ffda5fa5e973bd5bfb4c66015e681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:46 +0000

    Add package BitVector/3.4.2/BitVector-3.4.2.tar.gz

---
+++ packages/BitVector/BitVector-3.4.2.tar.gz/BitVector-3.4.2/BitVector/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from BitVector import __version__
+from BitVector import __author__
+from BitVector import __date__
+from BitVector import __url__
+from BitVector import __copyright__
+
+from BitVector import BitVector
+
+
+
+
