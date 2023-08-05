b2e008b035043f268c631337f3d1a9adbb07583d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:49 +0000

    Add package intrazon-python/0.1.2.post2/intrazon-python-0.1.2.post2.tar.gz

---
+++ packages/intrazon-python/intrazon-python-0.1.2.post2.tar.gz/intrazon-python-0.1.2.post2/intrazon/__init__.py
@@ -0,0 +1,18 @@
+__author__ = 'mkoponen'
+
+from intrazon import IntraZon, Application, VirtualGood, VirtualGoods, VirtualGoodPurchase, VirtualGoodPurchases, Player, Players, PaymentMethod, PaymentMethods, IntraZon
+
+__all__ = [
+    'IntraZon',
+    'Application',
+    'VirtualGood',
+    'VirtualGoods',
+    'VirtualGoodPurchase',
+    'VirtualGoodPurchases',
+    'Player',
+    'Players',
+    'PaymentMethod',
+    'PaymentMethods',
+    'IntraZon'
+
+]
