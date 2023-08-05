87e9d3e5d5decba4985bfc531a9af176a545dc3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:37 +0000

    Add package sabertooth/0.3.1/sabertooth-0.3.1.tar.gz

---
+++ packages/sabertooth/sabertooth-0.3.1.tar.gz/sabertooth-0.3.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='sabertooth',
+    version='0.3.1',
+    description="An OpenSubtitles Client",
+    long_description=open('README.md').read(),
+    author='Walid Saad',
+    author_email='walid.sa3d@gmail.com',
+    url='https://github.com/walidsa3d/sabertooth',
+    license="MIT",
+    keywords="cli subtitles opensuhbtitles",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={"console_scripts": ["sabertooth=sabertooth.cli:main"]},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Utilities'
+    ]
+)
