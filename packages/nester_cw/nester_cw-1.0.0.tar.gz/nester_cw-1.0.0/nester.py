f0e346773a7150148cfe5f0f2b9f609636c5fe8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package nester_cw/1.0.0/nester_cw-1.0.0.tar.gz

---
+++ packages/nester_cw/nester_cw-1.0.0.tar.gz/nester_cw-1.0.0/nester.py
@@ -0,0 +1,17 @@
+"""
+This module aims to solve the nested items.
+"""
+
+
+
+def print_lol(the_list):
+        """This funciton take one pareameter, named the_list,
+        it could be anylist in python(it could be the one that contain nested list). Each item in
+        the list take up one line and will be printed into screen."""
+
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                        print_lol(each_item)
+                else:
+                        print(each_item)
+                        
---
+++ packages/nester_cw/nester_cw-1.0.0.tar.gz/nester_cw-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nester_cw',
+	version		= '1.0.0',
+	py_modules	= ['nester'],
+	author		= 'hfpython',
+	author_mail	= 'carywill.k@gmail.com',
+	description 	= 'A simple printer of nested lists',
+     )
