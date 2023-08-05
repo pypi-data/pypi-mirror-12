ac1799ed666458bf245ad0e0224deb9c9d465c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:28 +0000

    Add package django-influxdb-metrics/1.1.0/django-influxdb-metrics-1.1.0.tar.gz

---
+++ packages/django-influxdb-metrics/django-influxdb-metrics-1.1.0.tar.gz/django-influxdb-metrics-1.1.0/influxdb_metrics/utils.py
@@ -0,0 +1,55 @@
+"""Utilities for working with influxdb."""
+import copy
+from threading import Thread
+
+from django.conf import settings
+
+from influxdb import InfluxDBClient
+
+
+def get_client():
+    """Returns an ``InfluxDBClient`` instance."""
+    return InfluxDBClient(
+        settings.INFLUXDB_HOST,
+        settings.INFLUXDB_PORT,
+        settings.INFLUXDB_USER,
+        settings.INFLUXDB_PASSWORD,
+        settings.INFLUXDB_DATABASE,
+        timeout=settings.INFLUXDB_TIMEOUT
+    )
+
+
+def query(query, time_precision='s', chunked=False):
+    """Wrapper around ``InfluxDBClient.query()``."""
+    client = get_client()
+    return client.query(query, time_precision=time_precision, chunked=chunked)
+
+
+def write_points(data):
+    """
+    Writes a series to influxdb.
+
+    :param data: Array of dicts, as required by
+      https://github.com/influxdb/influxdb-python
+
+    """
+    if getattr(settings, 'INFLUXDB_DISABLED', False):
+        return
+
+    client = get_client()
+    if getattr(settings, 'INFLUXDB_USE_THREADING', False):
+        client.write_points(data)
+    else:
+        thread = Thread(target=write_points_threaded, args=(client, data, ))
+        thread.start()
+
+
+def write_points_threaded(client, data):  # pragma: no cover
+    """Method to be called via threading module."""
+    try:
+        client.write_points(data)
+    except Exception, ex:
+        if getattr(settings, 'INFLUXDB_FAIL_SILENTLY', True):
+            pass
+        else:
+            raise ex
