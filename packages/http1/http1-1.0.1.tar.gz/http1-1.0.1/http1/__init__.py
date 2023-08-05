a8d7185a1ebc565aee61884f8c47c6055f37a4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:32 +0000

    Add package http1/1.0.1/http1-1.0.1.tar.gz

---
+++ packages/http1/http1-1.0.1.tar.gz/http1-1.0.1/http1/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+#pylint: disable=W0403
+from .http1 import request, Response, TooManyRedirectsException, get, head, post, put, delete, connect, options, trace
