f99fa73ddcfb428182577940ea179ae055289252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:24 +0000

    Add package kaptl/0.1.0.post3/kaptl-0.1.0.post3.win32.zip

---
+++ packages/kaptl/kaptl-0.1.0.post3.win32.zip/Python27/Scripts/kaptl-script.py
@@ -0,0 +1,10 @@
+#!C:\Python27\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'kaptl==0.1.0.post3','console_scripts','kaptl'
+__requires__ = 'kaptl==0.1.0.post3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('kaptl==0.1.0.post3', 'console_scripts', 'kaptl')()
+    )
