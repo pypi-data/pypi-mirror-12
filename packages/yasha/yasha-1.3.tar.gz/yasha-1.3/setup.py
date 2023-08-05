e64ffb3705a4744093a66b346529e615e7cdfd8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:53 +0000

    Add package yasha/1.3/yasha-1.3.tar.gz

---
+++ packages/yasha/yasha-1.3.tar.gz/yasha-1.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yasha",
+    author="Kim Blomqvist",
+    author_email="kblomqvist@iki.fi",
+    version="1.3",
+    description="A command-line tool to render Jinja templates",
+    keywords=["jinja", "code generator"],
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Click",
+        "Jinja2",
+        "pytoml",
+        "pyyaml",
+    ],
+    entry_points='''
+        [console_scripts]
+        yasha=yasha.scripts.yasha:cli
+    ''',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Code Generators",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+    ],
+    url="https://github.com/kblomqvist/yasha",
+    download_url="https://github.com/kblomqvist/yasha/tarball/1.3",
+)
