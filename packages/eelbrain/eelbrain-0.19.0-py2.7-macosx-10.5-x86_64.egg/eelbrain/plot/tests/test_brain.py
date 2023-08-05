0dc7db7b54ef3f05d3f10a25a8377fc8487862bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:22 +0000

    Add package eelbrain/0.19.0/eelbrain-0.19.0-py2.7-macosx-10.5-x86_64.egg

---
+++ packages/eelbrain/eelbrain-0.19.0-py2.7-macosx-10.5-x86_64.egg/eelbrain/plot/tests/test_brain.py
@@ -0,0 +1,23 @@
+# Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
+from eelbrain import datasets, plot
+
+
+def test_plot_brain():
+    """Test plot.brain plots"""
+    src = datasets.get_mne_sample(src='ico', sub=[0])['src']
+
+    p = plot.brain.dspm(src)
+    cb = p.plot_colorbar(show=False)
+    cb.close()
+
+    p = plot.brain.dspm(src, hemi='lh')
+    cb = p.plot_colorbar(show=False)
+    cb.close()
+
+    p = plot.brain.cluster(src, hemi='rh', views='parietal')
+    cb = p.plot_colorbar(show=False)
+    cb.close()
+
+    image = plot.brain.bin_table(src, tstart=0.1, tstop=0.3, tstep=0.1)
+    print repr(image)
+    print image
