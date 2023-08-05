ec21f3f755f363f337ae436549c1db8786ce27a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:37 +0000

    Add package budiluhur-jadwal/1.0.0/budiluhur-jadwal-1.0.0.tar.gz

---
+++ packages/budiluhur-jadwal/budiluhur-jadwal-1.0.0.tar.gz/budiluhur-jadwal-1.0.0/budiluhur/__init__.py
@@ -0,0 +1,6 @@
+__VERSION__ = "1.0.0"
+__BY__ = "yanwarsolahudinn@gmail.com"
+
+URL_DEPAN = "http://jadwal.budiluhur.ac.id/"
+URL_POST = "http://jadwal.budiluhur.ac.id/fakultas.php"
+SELECT_TAG_NAME = "kdfak"
