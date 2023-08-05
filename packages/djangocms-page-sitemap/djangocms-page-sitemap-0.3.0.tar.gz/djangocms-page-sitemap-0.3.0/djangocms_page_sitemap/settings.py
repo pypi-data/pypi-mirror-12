0d6d64bb1b576cb5b3264508b9aade8cbedbee32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:49 +0000

    Add package djangocms-page-sitemap/0.3.0/djangocms-page-sitemap-0.3.0.tar.gz

---
+++ packages/djangocms-page-sitemap/djangocms-page-sitemap-0.3.0.tar.gz/djangocms-page-sitemap-0.3.0/djangocms_page_sitemap/settings.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from cms.utils import get_cms_setting
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+PAGE_SITEMAP_CHANGEFREQ_DEFAULT = {
+    'always': _('always'),
+    'hourly': _('hourly'),
+    'daily': _('daily'),
+    'weekly': _('weekly'),
+    'monthly': _('monthly'),
+    'yearly': _('yearly'),
+    'never': _('never'),
+}
+PAGE_SITEMAP_CHANGEFREQ = getattr(
+    settings, 'PAGE_SITEMAP_CHANGEFREQ', PAGE_SITEMAP_CHANGEFREQ_DEFAULT
+)
+PAGE_SITEMAP_CACHE = get_cms_setting('CACHE_DURATIONS')['menus']
