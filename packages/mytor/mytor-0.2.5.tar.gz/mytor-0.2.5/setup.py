d673684e18a28ce19412169f476e0bd07f131818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:19 +0000

    Add package mytor/0.2.5/mytor-0.2.5.tar.gz

---
+++ packages/mytor/mytor-0.2.5.tar.gz/mytor-0.2.5/setup.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+from setuptools import setup
+
+setup(
+    name='mytor',
+    version='0.2.5',
+    packages=['mytor'],
+    install_requires=[
+        'tornado>=4.1',
+        'PyMySQL==0.6.7',
+        'greenlet>=0.4.2',
+    ],
+    author=['snower', 'mosquito'],
+    author_email=['sujian199@gmail.com', 'me@mosquito.su'],
+    url='https://github.com/mosquito/mytor',
+    license='MIT',
+    keywords=[
+        "tornado", "mysql"
+    ],
+    description='Tornado asynchronous MySQL Driver [fork of TorMysql]',
+    long_description=open("README.rst").read(),
+    zip_safe=False,
+)
