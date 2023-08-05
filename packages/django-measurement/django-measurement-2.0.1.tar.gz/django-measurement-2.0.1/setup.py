b109523553710def5dc81022a2e9d74512e8cfbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:15 +0000

    Add package django-measurement/2.0.1/django-measurement-2.0.1.tar.gz

---
+++ packages/django-measurement/django-measurement-2.0.1.tar.gz/django-measurement-2.0.1/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, Command
+
+requirements = []
+with open('requirements.txt', 'r') as in_:
+    requirements = in_.readlines()
+
+tests_require = ['django']
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys, subprocess
+
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+
+setup(
+    name='django-measurement',
+    version='2.0.1',
+    url='http://github.com/coddingtonbear/django-measurement/',
+    description='Convenient fields and classes for handling measurements',
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    packages=[
+        'django_measurement',
+    ],
+    cmdclass={'test': PyTest},
+)
