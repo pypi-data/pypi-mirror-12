b75fbc916642a00d0631b064417a5be74c4e0adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:01 +0000

    Add package django-directed-edge/2/django-directed-edge-2.tar.gz

---
+++ packages/django-directed-edge/django-directed-edge-2.tar.gz/django-directed-edge-2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='django-directed-edge',
+    version='2',
+    packages=(
+        'django_directed_edge',
+    ),
+    url='https://chris-lamb.co.uk/projects/django-directed-edge',
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    description="Helpful Django-oriented sugar around around DirectedEdge's Python API",
+)
