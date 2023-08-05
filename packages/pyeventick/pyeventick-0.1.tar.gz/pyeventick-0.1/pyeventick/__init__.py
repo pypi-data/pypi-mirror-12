bc8d5eb7b5c2e988314d2de72d8c74bbd5b51a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:27 +0000

    Add package pyeventick/0.1/pyeventick-0.1.tar.gz

---
+++ packages/pyeventick/pyeventick-0.1.tar.gz/pyeventick-0.1/pyeventick/__init__.py
@@ -0,0 +1,1 @@
+from pyeventick import Eventick
---
+++ packages/pyeventick/pyeventick-0.1.tar.gz/pyeventick-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pyeventick',
+      version='0.1',
+      description='',
+      url='https://github.com/hudsonbrendon/pyeventick',
+      author='Hudson Brendon',
+      author_email='contato.hudsonbrendon@gmail.com',
+      license='MIT',
+      packages=['pyeventick'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
