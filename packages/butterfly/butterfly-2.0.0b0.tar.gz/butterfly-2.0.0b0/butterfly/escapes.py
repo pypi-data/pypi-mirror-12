8086e47c356b13da1e3cec86818296c98daf4df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:37 +0000

    Add package butterfly/2.0.0b0/butterfly-2.0.0b0.tar.gz

---
+++ packages/butterfly/butterfly-2.0.0b0.tar.gz/butterfly-2.0.0b0/butterfly/escapes.py
@@ -0,0 +1,42 @@
+from contextlib import contextmanager
+import sys
+
+
+@contextmanager
+def html():
+    sys.stdout.write('\x1bP;HTML|')
+    yield
+    sys.stdout.write('\x1bP')
+    sys.stdout.flush()
+
+
+@contextmanager
+def image(mime='image'):
+    sys.stdout.write('\x1bP;IMAGE|%s;' % mime)
+    yield
+    sys.stdout.write('\x1bP\n')
+    sys.stdout.flush()
+
+
+@contextmanager
+def prompt():
+    sys.stdout.write('\x1bP;PROMPT|')
+    yield
+    sys.stdout.write('\x1bP')
+    sys.stdout.flush()
+
+
+@contextmanager
+def text():
+    sys.stdout.write('\x1bP;TEXT|')
+    yield
+    sys.stdout.write('\x1bP')
+    sys.stdout.flush()
+
+
+@contextmanager
+def sass():
+    sys.stdout.write('\x1bP;SASS|')
+    yield
+    sys.stdout.write('\x1bP')
+    sys.stdout.flush()
