c268c045dfdd36e856403d0f2fb10a89b23360f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:20 +0000

    Add package moduloif/1.0.0/moduloif-1.0.0.zip

---
+++ packages/moduloif/moduloif-1.0.0.zip/moduloif-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name =  'moduloif',
+    version = '1.0.0',
+    py_modules = ['modulo'],
+    author = 'josino',
+    author_email = 'josinon@gmail.com',
+    url = 'http://josinon.com',
+    description = 'um teste para ensinar python'
+)
