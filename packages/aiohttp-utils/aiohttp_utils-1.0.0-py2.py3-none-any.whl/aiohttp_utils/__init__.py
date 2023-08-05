5ebbf1fbf036d2d4f6dddf8e68e6c06463692722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:14 +0000

    Add package aiohttp-utils/1.0.0/aiohttp_utils-1.0.0-py2.py3-none-any.whl

---
+++ packages/aiohttp-utils/aiohttp_utils-1.0.0-py2.py3-none-any.whl/aiohttp_utils/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__version__ = '1.0.0'
+__author__ = 'Steven Loria'
+__license__ = "MIT"
+
+from .negotiation import Response
+from .constants import APP_KEY
+from .runner import run
+
+__all__ = (
+    'Response',
+    'run',
+    'APP_KEY',
+)
