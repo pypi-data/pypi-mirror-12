970ddd35c7ff364aebea461e1bbde9b8dfe0e58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:37 +0000

    Add package Uriah_nester/1.1.0/Uriah_nester-1.1.0.tar.gz

---
+++ packages/Uriah_nester/Uriah_nester-1.1.0.tar.gz/Uriah_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+      name = 'Uriah_nester',
+      version = '1.1.0',
+      py_modules = ['nester'],
+      author = 'Jack',
+      author_email = 'gy4567@gmail.com',
+      description = 'test distribution',
+     )
