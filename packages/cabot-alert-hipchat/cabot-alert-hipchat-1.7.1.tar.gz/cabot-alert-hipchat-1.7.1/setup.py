f4152f64fd7b858233dc498bd88da28de8214a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:01 +0000

    Add package cabot-alert-hipchat/1.7.1/cabot-alert-hipchat-1.7.1.tar.gz

---
+++ packages/cabot-alert-hipchat/cabot-alert-hipchat-1.7.1.tar.gz/cabot-alert-hipchat-1.7.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='cabot-alert-hipchat',
+      version='1.7.1',
+      description='A Hipchat alert plugin for Cabot by Arachnys',
+      author='Arachnys',
+      author_email='info@arachnys.com',
+      url='http://cabotapp.com',
+      packages=find_packages(),
+      download_url = 'https://github.com/bonniejools/cabot-alert-hipchat/tarball/1.7.1',
+     )
