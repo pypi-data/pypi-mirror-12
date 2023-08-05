51aa24669f78437d6d20db6d48287433d7c90431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package Flask-Analytics/0.5.2/Flask-Analytics-0.5.2.tar.gz

---
+++ packages/Flask-Analytics/Flask-Analytics-0.5.2.tar.gz/Flask-Analytics-0.5.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name="Flask-Analytics",
+    version="0.5.2",
+    author="Mihir Singh (@citruspi)",
+    author_email="hello@mihirsingh.com",
+    url='https://github.com/citruspi/Flask-Analytics',
+    packages=['flask_analytics', 'flask_analytics.providers'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+    ],
+    classifiers=[
+        'License :: Public Domain',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet'
+    ]
+)
