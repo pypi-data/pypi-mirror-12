8f6107a2766448059a5da280d1d943e12853e565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package opencivicdata-django/0.7.2/opencivicdata-django-0.7.2.tar.gz

---
+++ packages/opencivicdata-django/opencivicdata-django-0.7.2.tar.gz/opencivicdata-django-0.7.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="opencivicdata-django",
+      version='0.7.2',
+      author="James Turk",
+      author_email='james.p.turk@gmail.com',
+      license="BSD",
+      description="python opencivicdata library",
+      long_description="",
+      url="",
+      packages=find_packages(),
+      install_requires=[
+          'Django>=1.9a1',
+          'psycopg2',
+          'opencivicdata-divisions',
+      ],
+      platforms=["any"],
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: BSD License",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python :: 3.4",
+                   ],
+      )
