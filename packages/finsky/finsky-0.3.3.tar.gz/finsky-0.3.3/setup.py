330cf20c54901cd20c0611dc23733dfe7d0bb3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:01 +0000

    Add package finsky/0.3.3/finsky-0.3.3.tar.gz

---
+++ packages/finsky/finsky-0.3.3.tar.gz/finsky-0.3.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import re
+
+pkg_init_file = open('finsky/__init__.py').read()
+metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", pkg_init_file))
+
+def readme():
+    return open('./README.rst').read()
+
+setup(name='finsky',
+      version=metadata['version'],
+      description='Google Play API',
+      long_description=readme(),
+      url='https://github.com/mmcloughlin/finsky',
+      author='Michael McLoughlin',
+      author_email='mmcloughlin@gmail.com',
+      license='MIT',
+      packages=['finsky', 'finsky.protos'],
+      entry_points={
+          'console_scripts': [
+              'finsky = finsky.cli:main',
+              ]
+          },
+      install_requires=[
+          'requests',
+          'pydash',
+          'protobuf>=3.0.0a3',
+          'pyyaml',
+          ],
+      )
