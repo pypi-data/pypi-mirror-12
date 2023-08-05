d0cb84e6195c23fd4c580d40d7092fdd7b80c3aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:30 +0000

    Add package tomb_cli/0.0.3/tomb_cli-0.0.3.tar.gz

---
+++ packages/tomb_cli/tomb_cli-0.0.3.tar.gz/tomb_cli-0.0.3/tomb_cli/serve.py
@@ -0,0 +1,8 @@
+import click
+from montague import load_server
+
+@click.command()
+@click.pass_context
+def serve(ctx):
+    server = load_server(ctx.obj.config_file, name=ctx.obj.server_env)
+    server(ctx.obj.app)
