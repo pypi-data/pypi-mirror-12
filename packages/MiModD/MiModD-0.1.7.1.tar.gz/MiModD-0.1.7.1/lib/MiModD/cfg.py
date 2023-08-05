18e04005ec285e7ecf3f5047e76cf030ea80d57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:26 +0000

    Add package MiModD/0.1.7.1/MiModD-0.1.7.1.tar.gz

---
+++ packages/MiModD/MiModD-0.1.7.1.tar.gz/MiModD-0.1.7.1/lib/MiModD/cfg.py
@@ -0,0 +1,11 @@
+# USER-CONTROLLED SETTINGS
+tmpfiles_path = r'/var/tmp'
+multithreading_level = 4
+max_memory = 2
+snpeff_path = r'/not configured'
+
+# INSTALLATION SETTINGS
+# DON'T CHANGE!
+
+bin_path = r'../bin'
+
