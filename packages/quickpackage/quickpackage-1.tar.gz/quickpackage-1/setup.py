959dbe212c77298dcc06d742d2ac9cfa339d3882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:33 +0000

    Add package quickpackage/1/quickpackage-1.tar.gz

---
+++ packages/quickpackage/quickpackage-1.tar.gz/quickpackage-1/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name="quickpackage",
+	version="1",
+	description="Instantly create and upload python package for your python script",
+	url="https://github.com/yask123/quick-package",
+	author="Yask Srivastava",
+	author_email="yask123@gmail.com",
+	license='MIT',
+	packages=["quickpackage"],
+	zip_safe=False)
