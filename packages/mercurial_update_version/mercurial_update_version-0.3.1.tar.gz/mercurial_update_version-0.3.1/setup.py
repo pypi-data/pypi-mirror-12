a0e97b98ffaabd98c1908a2c4974ce68bd15d19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:46 +0000

    Add package mercurial_update_version/0.3.1/mercurial_update_version-0.3.1.tar.gz

---
+++ packages/mercurial_update_version/mercurial_update_version-0.3.1.tar.gz/mercurial_update_version-0.3.1/setup.py
@@ -0,0 +1,40 @@
+
+VERSION = '0.3.1'
+
+#pylint:disable=missing-docstring,unused-import,import-error
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = open("README.txt").read()
+
+setup(
+    name="mercurial_update_version",
+    version=VERSION,
+    author='Marcin Kasperski',
+    author_email='Marcin.Kasperski@mekk.waw.pl',
+    url='http://bitbucket.org/Mekk/mercurial-update_version',
+    description='Mercurial Update Version Extension',
+    long_description=LONG_DESCRIPTION,
+    license='BSD',
+    py_modules=['update_version'],
+    install_requires=[
+        'mercurial_extension_utils>=0.8.1',
+    ],
+    keywords="mercurial hg version auto-update tag",
+    classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: DFSG approved',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Software Development :: Version Control'
+    ],
+    zip_safe=True)
