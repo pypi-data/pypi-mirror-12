ae825796d74480bf371c9baa655fe8832e234f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:45 +0000

    Add package ksr/0.1.1/ksr-0.1.1.tar.gz

---
+++ packages/ksr/ksr-0.1.1.tar.gz/ksr-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='ksr',
+    version='0.1.1',
+    py_modules=['ksr'],
+    url='https://github.com/jedahan/kickstarter',
+    description='commandline kickstarter',
+    author='Jonathan Dahan',
+    author_email='jonathan@jonathan.is',
+    license='ISC',
+    install_requires=[
+        'Click',
+        'TinyDB',
+    ],
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Environment :: Console',
+      'License :: OSI Approved :: ISC License (ISCL)',
+      'Natural Language :: English',
+      'Operating System :: MacOS :: MacOS X',
+      'Operating System :: POSIX :: Linux',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.5',
+      'Topic :: Software Development :: User Interfaces',
+      'Topic :: Utilities',
+    ],
+    entry_points='''
+        [console_scripts]
+        ksr=ksr:cli
+    ''',
+)
