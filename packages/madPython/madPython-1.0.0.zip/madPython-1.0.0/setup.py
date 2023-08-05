ca5a9eb662d16a2c8c9d806a86578849e85dac5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:52 +0000

    Add package madPython/1.0.0/madPython-1.0.0.zip

---
+++ packages/madPython/madPython-1.0.0.zip/madPython-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+name='madPython',
+version='1.0.0',
+py_modules=['day2'],
+author='madhuri',
+author_email='madhuriagrawal4@gmail.com',
+url='http://www.headfirstlabs.com',
+description='A simple printer of day2 lists',
+)
