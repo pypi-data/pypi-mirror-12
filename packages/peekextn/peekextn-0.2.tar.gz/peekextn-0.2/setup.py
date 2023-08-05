ee8b3f2cf8abb18fa9bf677c21141ca1bb28d738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:11 +0000

    Add package peekextn/0.2/peekextn-0.2.tar.gz

---
+++ packages/peekextn/peekextn-0.2.tar.gz/peekextn-0.2/setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+  name = 'peekextn',
+  packages = [''],
+  packages_dir = {'peekextn': ''},
+  version = '0.2',
+  description = 'Peek extension and show respective sources',
+  author = 'Vadivel',
+  author_email = 'vadivelmurugank@gmail.com',
+  url = 'https://github.com/vadivelmurugank/peekextn',
+  license = 'MIT',
+)
