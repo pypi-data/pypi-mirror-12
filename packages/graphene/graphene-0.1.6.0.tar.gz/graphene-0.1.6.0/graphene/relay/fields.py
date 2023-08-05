2eccc69a8fa91bd389e8d42714354adbedb8107b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:26 +0000

    Add package graphene/0.1.6.0/graphene-0.1.6.0.tar.gz

---
+++ packages/graphene/graphene-0.1.6.0.tar.gz/graphene-0.1.6.0/graphene/relay/fields.py
@@ -0,0 +1,112 @@
+from collections import Iterable, OrderedDict
+
+from graphql_relay.connection.arrayconnection import (
+    connection_from_list
+)
+from graphql_relay.connection.connection import (
+    connection_args
+)
+from graphql_relay.node.node import (
+    from_global_id
+)
+from graphql.core.type import (
+    GraphQLNonNull,
+    GraphQLID,
+    GraphQLArgument,
+)
+
+from graphene.core.fields import Field, IDField
+from graphene.utils import memoize
+
+
+class ConnectionField(Field):
+
+    def __init__(self, field_type, resolve=None, description='',
+                 connection_type=None, edge_type=None, **kwargs):
+        super(ConnectionField, self).__init__(field_type, resolve=resolve,
+                                              args=connection_args,
+                                              description=description, **kwargs)
+        self.connection_type = connection_type
+        self.edge_type = edge_type
+
+    def wrap_resolved(self, value, instance, args, info):
+        return value
+
+    def resolve(self, instance, args, info):
+        from graphene.relay.types import PageInfo
+        schema = info.schema.graphene_schema
+
+        orig_resolved = super(ConnectionField, self).resolve(instance, args, info)
+        if orig_resolved:
+            resolved = self.wrap_resolved(orig_resolved, instance, args, info)
+            assert isinstance(
+                resolved, Iterable), 'Resolved value from the connection field have to be iterable'
+
+            node = self.get_object_type(schema)
+            connection_type = self.get_connection_type(node)
+            edge_type = self.get_edge_type(node)
+
+            connection = connection_from_list(resolved, args, connection_type=connection_type,
+                                              edge_type=edge_type, pageinfo_type=PageInfo)
+            connection.set_connection_data(orig_resolved)
+            return connection
+
+    @memoize
+    def get_connection_type(self, node):
+        connection_type = self.connection_type or node.get_connection_type()
+        edge_type = self.get_edge_type(node)
+        return connection_type.for_node(node, edge_type=edge_type)
+
+    @memoize
+    def get_edge_type(self, node):
+        return self.edge_type or node.get_edge_type()
+
+    @memoize
+    def internal_type(self, schema):
+        from graphene.relay.utils import is_node
+        node = self.get_object_type(schema)
+        assert is_node(node), 'Only nodes have connections.'
+        schema.register(node)
+
+        return self.get_connection_type(node).internal_type(schema)
+
+
+class NodeField(Field):
+    def __init__(self, object_type=None, *args, **kwargs):
+        from graphene.relay.types import Node
+        super(NodeField, self).__init__(object_type or Node, *args, **kwargs)
+        self.field_object_type = object_type
+        self.args['id'] = GraphQLArgument(
+            GraphQLNonNull(GraphQLID),
+            description='The ID of an object'
+        )
+
+    def id_fetcher(self, global_id, info):
+        from graphene.relay.utils import is_node
+        schema = info.schema.graphene_schema
+        resolved_global_id = from_global_id(global_id)
+        _type, _id = resolved_global_id.type, resolved_global_id.id
+        object_type = schema.get_type(_type)
+        if not is_node(object_type) or (self.field_object_type and
+           object_type != self.field_object_type):
+            return
+
+        return object_type.get_node(_id)
+
+    def resolve(self, instance, args, info):
+        global_id = args.get('id')
+        return self.id_fetcher(global_id, info)
+
+
+class GlobalIDField(IDField):
+    '''The ID of an object'''
+    required = True
+
+    def contribute_to_class(self, cls, name, add=True):
+        from graphene.relay.utils import is_node, is_node_type
+        in_node = is_node(cls) or is_node_type(cls)
+        assert in_node, 'GlobalIDField could only be inside a Node, but got %r' % cls
+        super(GlobalIDField, self).contribute_to_class(cls, name, add)
+
+    def resolve(self, instance, args, info):
+        return self.object_type.to_global_id(instance, args, info)
---
+++ packages/graphene/graphene-0.1.6.0.tar.gz/graphene-0.1.6.0/graphene/relay/types.py
@@ -0,0 +1,86 @@
+from graphql_relay.node.node import (
+    to_global_id
+)
+
+from graphene.core.types import Interface, ObjectType
+from graphene.core.fields import BooleanField, StringField, ListField, Field
+from graphene.relay.fields import GlobalIDField
+from graphene.utils import memoize
+
+
+class PageInfo(ObjectType):
+    has_next_page = BooleanField(required=True, description='When paginating forwards, are there more items?')
+    has_previous_page = BooleanField(required=True, description='When paginating backwards, are there more items?')
+    start_cursor = StringField(description='When paginating backwards, the cursor to continue.')
+    end_cursor = StringField(description='When paginating forwards, the cursor to continue.')
+
+
+class Edge(ObjectType):
+    '''An edge in a connection.'''
+    class Meta:
+        type_name = 'DefaultEdge'
+
+    node = Field(lambda field: field.object_type.node_type, description='The item at the end of the edge')
+    cursor = StringField(required=True, description='A cursor for use in pagination')
+
+    @classmethod
+    @memoize
+    def for_node(cls, node):
+        from graphene.relay.utils import is_node
+        assert is_node(node), 'ObjectTypes in a edge have to be Nodes'
+        return type('%s%s' % (node._meta.type_name, cls._meta.type_name), (cls, ), {'node_type': node})
+
+
+class Connection(ObjectType):
+    '''A connection to a list of items.'''
+    class Meta:
+        type_name = 'DefaultConnection'
+
+    page_info = Field(PageInfo, required=True, description='The Information to aid in pagination')
+    edges = ListField(lambda field: field.object_type.edge_type, description='Information to aid in pagination.')
+
+    _connection_data = None
+
+    @classmethod
+    @memoize
+    def for_node(cls, node, edge_type=None):
+        from graphene.relay.utils import is_node
+        edge_type = edge_type or Edge
+        assert is_node(node), 'ObjectTypes in a connection have to be Nodes'
+        return type('%s%s' % (node._meta.type_name, cls._meta.type_name), (cls, ), {'edge_type': edge_type.for_node(node)})
+
+    def set_connection_data(self, data):
+        self._connection_data = data
+
+    def get_connection_data(self):
+        return self._connection_data
+
+
+class BaseNode(object):
+    @classmethod
+    def _prepare_class(cls):
+        from graphene.relay.utils import is_node
+        if is_node(cls):
+            assert hasattr(
+                cls, 'get_node'), 'get_node classmethod not found in %s Node' % cls
+
+    @classmethod
+    def to_global_id(cls, instance, args, info):
+        type_name = cls._meta.type_name
+        return to_global_id(type_name, instance.id)
+
+    connection_type = Connection
+    edge_type = Edge
+
+    @classmethod
+    def get_connection_type(cls):
+        return cls.connection_type
+
+    @classmethod
+    def get_edge_type(cls):
+        return cls.edge_type
+
+
+class Node(BaseNode, Interface):
+    '''An object with an ID'''
+    id = GlobalIDField()
