6b4f79cba0ff33e41c32b020253c3a499db51203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:12 +0000

    Add package django-signalmethods/1.0.0/django-signalmethods-1.0.0.tar.gz

---
+++ packages/django-signalmethods/django-signalmethods-1.0.0.tar.gz/django-signalmethods-1.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-signalmethods',
+    version='1.0.0',
+    description='Allows django signals to be used as methods ',
+    author='Mike Amy',
+    author_email='cocoade@googlemail.com',
+    url='https://github.com/MikeAmy/django-signalmethods',
+    packages=['signalmethods']
+)
