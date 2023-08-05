028e5e8de0443093acb99909a31cbfea734f08a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:15 +0000

    Add package 1020-nester/1.00/1020-nester-1.00.zip

---
+++ packages/1020-nester/1020-nester-1.00.zip/1020-nester-1.00/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name    ="1020-nester",
+    version ="1.00",
+    py_modules=["1020-nester"],
+    author    ="treeee",
+    author_email="429669469@qq.com",
+    url="lll.com",
+    description="a simple printer of nested lists",
+    )
+    
