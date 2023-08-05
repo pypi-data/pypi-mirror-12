e7689681ad44ebf5ff3254303af2620af8dcda85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package sparqlhttp/1.5/sparqlhttp-1.5.tar.gz

---
+++ packages/sparqlhttp/sparqlhttp-1.5.tar.gz/sparqlhttp-1.5/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="sparqlhttp",
+      version="1.5",
+      description="HTTP SPARQL server and client for twisted and rdflib",
+      author="Drew Perttula",
+      author_email="drewp@bigasterisk.com",
+      url="http://projects.bigasterisk.com/sparqlhttp/",
+      download_url="http://projects.bigasterisk.com/sparqlhttp/sparqlhttp-1.5.tar.gz",
+
+      packages=['sparqlhttp'],
+      data_files=[('', ['doc.html'])],
+      package_data={'sparqlhttp' : ['query']},
+      
+      classifiers=[ # http://www.python.org/pypi?:action=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)
+
+
+
