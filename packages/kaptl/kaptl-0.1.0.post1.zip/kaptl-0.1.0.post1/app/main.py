5a902c7d4ed7a60253a094c37dd1a0908e8ed6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package kaptl/0.1.0.post1/kaptl-0.1.0.post1.zip

---
+++ packages/kaptl/kaptl-0.1.0.post1.zip/kaptl-0.1.0.post1/app/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""Usage: kaptl new [--backend=mvc] --frontend=angular [<rules> | --rules-file=RULESFILE ] [--ast]
+        kaptl -h | --help
+        kaptl --version
+
+"""
+
+from kaptl import Kaptl
+from docopt import docopt
+from autoupgrade import AutoUpgrade
+
+
+def main():
+    AutoUpgrade("kaptl").upgrade_if_needed(True, True)  # update the deps as well and then restart the app
+    args = docopt(__doc__)
+    kaptl = Kaptl(args)
+    """
+    Parse parameters and launch the proper operation
+    :type arguments: object
+    """
+    if args["new"]:
+        kaptl.create_new_app()
+
+
+if __name__ == '__main__':
+    "Main entry point for KAPTL CLI"
+    main()
