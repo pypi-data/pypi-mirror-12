cd2081ceaae758c8f222937554501620384865fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package vr.runners/2.6/vr.runners-2.6.tar.gz

---
+++ packages/vr.runners/vr.runners-2.6.tar.gz/vr.runners-2.6/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name='vr.runners',
+    namespace_packages=['vr'],
+    version='2.6',
+    author='Brent Tubbs',
+    author_email='brent.tubbs@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://bitbucket.org/yougov/vr.runners',
+    install_requires=[
+        'vr.common>=3.16.2,<5dev',
+        'requests>=1.2.0',
+        'path.py',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vrun = vr.runners.image:ImageRunner.invoke',
+            'vrun_precise = vr.runners.precise:PreciseRunner.invoke',
+        ],
+    },
+    description='Command line tools to launch procs.',
+)
