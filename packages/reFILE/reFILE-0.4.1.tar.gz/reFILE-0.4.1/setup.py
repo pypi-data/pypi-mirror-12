f0d818dfad09595a050582d7a24769741f7b08c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:25 +0000

    Add package reFILE/0.4.1/reFILE-0.4.1.tar.gz

---
+++ packages/reFILE/reFILE-0.4.1.tar.gz/reFILE-0.4.1/setup.py
@@ -0,0 +1,48 @@
+import sys
+import refile
+from setuptools import setup
+
+
+install_requires = ['argparse', 'nose', 'ordereddict', 'pathlib']
+# TODO: look into how to set requirements based on the python it is installed by.
+# # pathlib is new to std lib in 3.4
+# if sys.version_info[:2] < (3, 4):
+#     install_requires.append('pathlib')
+# # argparse is not in 2.6 or lower
+# if sys.version_info[:2] < (2, 7):
+#     install_requires.append('ordereddict')
+#     install_requires.append('argparse')
+# # it's not in 3.1 either
+# elif sys.version_info[1] < 2:
+#     install_requires.append('argparse')
+
+if sys.version_info < (2, 7):
+    import unittest  # noqa
+    if 'nosetests' in sys.argv:
+        import unittest2
+        sys.modules['unittest'] = unittest2
+
+setup(
+    name=refile.__title__,
+    version=refile.__version__,
+    author=refile.__author__,
+    author_email=refile.__email__,
+    description=refile.__description__,
+    long_description=open('README.rst').read(),
+    url=refile.__homepage__,
+    download_url=refile.__download__,
+    install_requires=install_requires,
+    packages=['refile'],
+    entry_points={
+        'console_scripts': ['refile = refile.cmdline:main']
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ]
+)
