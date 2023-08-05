74ff2fd9ef925c399012a65324b6e934b6b8844d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:37 +0000

    Add package troposphere/1.3.0/troposphere-1.3.0.tar.gz

---
+++ packages/troposphere/troposphere-1.3.0.tar.gz/troposphere-1.3.0/troposphere/logs.py
@@ -0,0 +1,39 @@
+from . import AWSObject, AWSProperty
+from .validators import positive_integer
+
+
+class LogGroup(AWSObject):
+    resource_type = "AWS::Logs::LogGroup"
+
+    props = {
+        'RetentionInDays': (positive_integer, False),
+    }
+
+
+class MetricTransformation(AWSProperty):
+    props = {
+        'MetricName': (basestring, True),
+        'MetricNamespace': (basestring, True),
+        'MetricValue': (basestring, True),
+    }
+
+
+class MetricFilter(AWSObject):
+    resource_type = "AWS::Logs::MetricFilter"
+
+    props = {
+        'FilterPattern': (basestring, True),
+        'LogGroupName': (basestring, True),
+        'MetricTransformations': ([MetricTransformation], True),
+    }
+
+
+class SubscriptionFilter(AWSObject):
+    resource_type = "AWS::Logs::SubscriptionFilter"
+
+    props = {
+        'DestinationArn': (basestring, True),
+        'FilterPattern': (basestring, True),
+        'LogGroupName': (basestring, True),
+        'RoleArn': (basestring, True),
+    }
---
+++ packages/troposphere/troposphere-1.3.0.tar.gz/troposphere-1.3.0/troposphere/s3.py
@@ -0,0 +1,197 @@
+# Copyright (c) 2013, Bob Van Zant <bob@veznat.com>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from . import AWSObject, AWSProperty, Tags
+from .validators import positive_integer, s3_bucket_name
+try:
+    from awacs.aws import Policy
+    policytypes = (dict, Policy)
+except ImportError:
+    policytypes = dict,
+
+
+Private = "Private"
+PublicRead = "PublicRead"
+PublicReadWrite = "PublicReadWrite"
+AuthenticatedRead = "AuthenticatedRead"
+BucketOwnerRead = "BucketOwnerRead"
+BucketOwnerFullControl = "BucketOwnerFullControl"
+LogDeliveryWrite = "LogDeliveryWrite"
+
+
+class CorsRules(AWSProperty):
+    props = {
+        'AllowedHeaders': ([basestring], False),
+        'AllowedMethods': ([basestring], True),
+        'AllowedOrigins': ([basestring], True),
+        'ExposedHeaders': ([basestring], False),
+        'Id': (basestring, False),
+        'MaxAge': (positive_integer, False),
+    }
+
+
+class CorsConfiguration(AWSProperty):
+    props = {
+        'CorsRules': ([CorsRules], True),
+    }
+
+
+class VersioningConfiguration(AWSProperty):
+    props = {
+        'Status': (basestring, False),
+    }
+
+
+class RedirectAllRequestsTo(AWSProperty):
+    props = {
+        'HostName': (basestring, True),
+        'Protocol': (basestring, False),
+    }
+
+
+class RedirectRule(AWSProperty):
+    props = {
+        'HostName': (basestring, False),
+        'HttpRedirectCode': (basestring, False),
+        'Protocol': (basestring, False),
+        'ReplaceKeyPrefixWith': (basestring, False),
+        'ReplaceKeyWith': (basestring, False),
+    }
+
+
+class RoutingRuleCondition(AWSProperty):
+    props = {
+        'HttpErrorCodeReturnedEquals': (basestring, False),
+        'KeyPrefixEquals': (basestring, False),
+    }
+
+
+class RoutingRule(AWSProperty):
+    props = {
+        'RedirectRule': (RedirectRule, True),
+        'RoutingRuleCondition': (RoutingRuleCondition, False),
+    }
+
+
+class WebsiteConfiguration(AWSProperty):
+    props = {
+        'IndexDocument': (basestring, False),
+        'ErrorDocument': (basestring, False),
+        'RedirectAllRequestsTo': (RedirectAllRequestsTo, False),
+        'RoutingRules': ([RoutingRule], False),
+    }
+
+
+class LifecycleRuleTransition(AWSProperty):
+    props = {
+        'StorageClass': (basestring, True),
+        'TransitionDate': (basestring, False),
+        'TransitionInDays': (positive_integer, False),
+    }
+
+
+class NoncurrentVersionTransition(AWSProperty):
+    props = {
+        'StorageClass': (basestring, True),
+        'TransitionInDays': (positive_integer, True),
+    }
+
+
+class LifecycleRule(AWSProperty):
+    props = {
+        'ExpirationDate': (basestring, False),
+        'ExpirationInDays': (positive_integer, False),
+        'Id': (basestring, False),
+        'NoncurrentVersionExpirationInDays': (positive_integer, False),
+        'NoncurrentVersionTransition': (NoncurrentVersionTransition, False),
+        'Prefix': (basestring, False),
+        'Status': (basestring, True),
+        'Transition': (LifecycleRuleTransition, False),
+    }
+
+
+class LifecycleConfiguration(AWSProperty):
+    props = {
+        'Rules': ([LifecycleRule], True),
+    }
+
+
+class LoggingConfiguration(AWSProperty):
+    props = {
+        'DestinationBucketName': (s3_bucket_name, False),
+        'LogFilePrefix': (basestring, False),
+    }
+
+
+class LambdaConfigurations(AWSProperty):
+    props = {
+        'Event': (basestring, True),
+        'Function': (basestring, True),
+    }
+
+
+class QueueConfigurations(AWSProperty):
+    props = {
+        'Event': (basestring, True),
+        'Queue': (basestring, True),
+    }
+
+
+class TopicConfigurations(AWSProperty):
+    props = {
+        'Event': (basestring, True),
+        'Topic': (basestring, True),
+    }
+
+
+class NotificationConfiguration(AWSProperty):
+    props = {
+        'LambdaConfigurations': ([LambdaConfigurations], False),
+        'QueueConfigurations': ([QueueConfigurations], False),
+        'TopicConfigurations': ([TopicConfigurations], False),
+    }
+
+
+class Bucket(AWSObject):
+    resource_type = "AWS::S3::Bucket"
+
+    props = {
+        'AccessControl': (basestring, False),
+        'BucketName': (s3_bucket_name, False),
+        'CorsConfiguration': (CorsConfiguration, False),
+        'LifecycleConfiguration': (LifecycleConfiguration, False),
+        'LoggingConfiguration': (LoggingConfiguration, False),
+        'NotificationConfiguration': (NotificationConfiguration, False),
+        'Tags': (Tags, False),
+        'WebsiteConfiguration': (WebsiteConfiguration, False),
+        'VersioningConfiguration': (VersioningConfiguration, False)
+    }
+
+    access_control_types = [
+        Private,
+        PublicRead,
+        PublicReadWrite,
+        AuthenticatedRead,
+        BucketOwnerRead,
+        BucketOwnerFullControl,
+        LogDeliveryWrite,
+    ]
+
+    def __init__(self, name, **kwargs):
+        super(Bucket, self).__init__(name, **kwargs)
+
+        if 'AccessControl' in kwargs:
+            if kwargs['AccessControl'] not in self.access_control_types:
+                raise ValueError('AccessControl must be one of "%s"' % (
+                    ', '.join(self.access_control_types)))
+
+
+class BucketPolicy(AWSObject):
+    resource_type = "AWS::S3::BucketPolicy"
+
+    props = {
+        'Bucket': (basestring, True),
+        'PolicyDocument': (policytypes, True),
+    }
