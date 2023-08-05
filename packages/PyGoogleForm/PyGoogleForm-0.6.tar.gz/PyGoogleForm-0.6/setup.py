37c2dda0374d072e43628c29b16c2218593cfd55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package PyGoogleForm/0.6/PyGoogleForm-0.6.tar.gz

---
+++ packages/PyGoogleForm/PyGoogleForm-0.6.tar.gz/PyGoogleForm-0.6/setup.py
@@ -0,0 +1,44 @@
+from distutils.core import setup
+
+# setting up long description as rst if pandoc is available
+# if pandoc is unavailable, uses the .md anyway
+long_description = ""
+try:
+    import subprocess
+    import pandoc
+
+    open("README.md").read()
+
+    process = subprocess.Popen(
+        ['which pandoc'],
+        shell=True,
+        stdout=subprocess.PIPE,
+        universal_newlines=True
+    )
+
+    pandoc_path = process.communicate()[0].strip('\n')
+    pandoc.core.PANDOC_PATH = pandoc_path
+
+    doc = pandoc.Document()
+    doc.markdown = long_description
+
+    long_description = doc.rst
+except ImportError, IOError:
+    pass
+
+
+setup(
+  name = 'PyGoogleForm',
+  packages = ['PyGoogleForm'], # this must be the same as the name above
+  version = '0.6',
+  description = 'A simple Python library to submit Google Forms in Python.',
+  long_description = long_description,
+  author = 'Sebastien Cagnon',
+  author_email = 'cagnonsebastien@gmail.com',
+  url = 'https://github.com/sebcagnon/PyGoogleForm',
+  download_url = 'https://github.com/sebcagnon/PyGoogleForm/tarball/0.6',
+  licence = 'MIT',
+  keywords = ['Google Form'],
+  install_requires = ['beautifulsoup4'],
+  classifiers = [],
+)
