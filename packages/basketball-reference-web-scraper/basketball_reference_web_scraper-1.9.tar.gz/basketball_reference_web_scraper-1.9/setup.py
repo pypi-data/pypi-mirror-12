338db28b9feda1a032c9736ed1c952c8c9917abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:20 +0000

    Add package basketball-reference-web-scraper/1.9/basketball_reference_web_scraper-1.9.tar.gz

---
+++ packages/basketball-reference-web-scraper/basketball_reference_web_scraper-1.9.tar.gz/basketball_reference_web_scraper-1.9/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'basketball_reference_web_scraper',
+  packages = [
+      'basketball_reference_web_scraper/',
+      'basketball_reference_web_scraper/helper_functions/',
+      'basketball_reference_web_scraper/helper_functions/schedule/',
+      'basketball_reference_web_scraper/helper_functions/box_score/',
+      'basketball_reference_web_scraper/helper_functions/player_season_statistics/',
+      'basketball_reference_web_scraper/json_encoders/',
+      'basketball_reference_web_scraper/models/'
+  ],
+  version = '1.9',
+  description = 'An API for NBA data that is scraped from Basketball Reference',
+  author = 'Jae Bradley',
+  author_email = 'jae.b.bradley@gmail.com',
+  url = 'https://github.com/jaebradley/basketball_reference_web_scraper',
+  download_url = 'https://github.com/jaebradley/basketball_reference_web_scraper/tarball/0.6',
+  keywords = ['basketball', 'web scraping', 'nba', 'data'], # arbitrary keywords
+  classifiers = [],
+  install_requires = [
+      'lxml',
+      'logging',
+      'urllib3',
+      'pytz'
+  ]
+)
