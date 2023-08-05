184edbe8ae53bba9542797fb325feb5b36a80ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:46 +0000

    Add package LKY_nester/1.0.0/LKY_nester-1.0.0.zip

---
+++ packages/LKY_nester/LKY_nester-1.0.0.zip/LKY_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = "LKY_nester",
+        version = "1.0.0",
+        py_modules = ["LKY_nester"],
+        author = "lkypython",
+        author_email = "bychance_lky@163.com",
+        url = "http://www.headfirstlabs.com",
+        description = "A simple printer of nested lists",
+    )
