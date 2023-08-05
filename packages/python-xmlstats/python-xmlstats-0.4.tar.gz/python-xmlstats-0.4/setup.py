4a9356d740f2e3be7f7dfa4779b1c3b0a1f9947f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:16 +0000

    Add package python-xmlstats/0.4/python-xmlstats-0.4.tar.gz

---
+++ packages/python-xmlstats/python-xmlstats-0.4.tar.gz/python-xmlstats-0.4/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+  name = 'python-xmlstats',
+  packages = ['xmlstats'],
+  version = '0.4',
+  description = 'Client for https://erikberg.com/api',
+  author = 'Serbokryl Oleg',
+  author_email = 'chezar1995@gmail.com',
+  url = 'https://github.com/Krokop/python-xmlstats',
+  download_url = 'https://github.com/Krokop/python-xmlstats/tarball/0.4',
+  keywords = ['NBA stas api', 'xmlstats', 'NBA API', 'nba api stats'],
+  classifiers = [],
+  install_requires=['python-dateutil',],
+)
