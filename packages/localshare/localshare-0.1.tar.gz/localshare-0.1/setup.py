ef3de7ec3bc762f92a6c7dd7b87e2ddd048d2ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:33 +0000

    Add package localshare/0.1/localshare-0.1.tar.gz

---
+++ packages/localshare/localshare-0.1.tar.gz/localshare-0.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3 :: Only',
+    'Natural Language :: English',
+]
+
+setup(
+    name='localshare',
+    version='0.1',
+    py_modules=['localshare'],
+    install_requires=[
+        'Click',
+        'zeroconf',
+        'netifaces'
+    ],
+    entry_points='''
+        [console_scripts]
+        localshare=localshare:cli
+    ''',
+    url='https://github.com/sunu/localshare',
+    license='MIT',
+    author='sunu',
+    author_email='sunu@sunu.in',
+    description='localshare: A commandline utility to share files over local network.',
+    classifiers=classifiers,
+)
