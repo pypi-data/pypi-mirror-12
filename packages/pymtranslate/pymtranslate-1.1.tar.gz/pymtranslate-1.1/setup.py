687ac444fd27a7acea3ee09d0d82f20a0fe7d065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:08 +0000

    Add package pymtranslate/1.1/pymtranslate-1.1.tar.gz

---
+++ packages/pymtranslate/pymtranslate-1.1.tar.gz/pymtranslate-1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pymtranslate',
+  packages = ['pymtranslate'],
+  version = '1.1',
+  description = 'A probabilistic text-based language translator',
+  author = 'Andy Craze',
+  author_email = 'accraze@gmail.com',
+  url = 'https://github.com/accraze/pymtranslate', # use the URL to the github repo
+  download_url = 'https://github.com/accraze/pymtranslate/tarball/1.1',
+  keywords = ['translation', 'nlp', 'machine', 'text', 'corpus', 'statistics', 'language', 'probability'], # arbitrary keywords
+  classifiers = [],
+)
