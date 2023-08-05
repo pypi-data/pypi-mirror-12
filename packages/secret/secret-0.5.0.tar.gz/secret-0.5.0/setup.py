0721b40305bc737e86580247e8296281cdd605e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:10 +0000

    Add package secret/0.5.0/secret-0.5.0.tar.gz

---
+++ packages/secret/secret-0.5.0.tar.gz/secret-0.5.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='secret',
+    version='0.5.0',
+    description='Secret is for storing secrets. Backed by Amazon Web Services: IAM for access policies, KMS for encryption keys and S3 for storage',
+    keywords = 'secret secrets project vault aws amazon cloud',
+    author = 'Jussi Vaihia',
+    author_email = 'jussi.vaihia@futurice.com',
+    license='Apache2',
+    url='https://github.com/futurice/secret',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    install_requires=['boto3>=0.0.18', 'pycryptodome>=3.1','trollius>=1.0.4','tabulate>=0.7.5'],
+    packages = ["secret"],
+    include_package_data = True,
+    entry_points={
+        'console_scripts': [
+            'secret = secret.secret:runner'
+            ]
+        }
+)
