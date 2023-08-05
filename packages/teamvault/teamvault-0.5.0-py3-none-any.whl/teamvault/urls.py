a1d5def615073353e09b56f43e2b659dbb99c5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:31 +0000

    Add package teamvault/0.5.0/teamvault-0.5.0-py3-none-any.whl

---
+++ packages/teamvault/teamvault-0.5.0-py3-none-any.whl/teamvault/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
+
+handler404 = 'teamvault.views.handler404'
+
+urlpatterns = patterns('',
+    url(r'^api/', include('teamvault.apps.secrets.api_urls'), name='api'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'', include('teamvault.apps.secrets.urls'), name='secrets'),
+    url(r'', include('teamvault.apps.accounts.urls'), name='accounts'),
+)
