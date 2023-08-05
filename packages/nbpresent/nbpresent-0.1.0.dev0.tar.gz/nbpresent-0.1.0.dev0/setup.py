830605b6dabd58038568d8d455c6114a6fa87cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:01 +0000

    Add package nbpresent/0.1.0.dev0/nbpresent-0.1.0.dev0.tar.gz

---
+++ packages/nbpresent/nbpresent-0.1.0.dev0.tar.gz/nbpresent-0.1.0.dev0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+# should be loaded below
+__version__ = None
+
+with open('nbpresent/_version.py') as version:
+    exec(version.read())
+
+setup(
+    name="nbpresent",
+    version=__version__,
+    description="Next generation slides from Jupyter Notebooks",
+    author="Nicholas Bollweg",
+    author_email="nbollweg@continuum.io",
+    license="BSD-3-Clause",
+    url="https://github.com/ContinuumIO/nbpresent",
+    keywords="ipython jupyter inkscape markdown presentation revealjs d3",
+    classifiers=["Development Status :: 4 - Beta",
+                 "Framework :: IPython",
+                 "Programming Language :: Python",
+                 "License :: OSI Approved :: BSD License"],
+    packages=["nbpresent"],
+    include_package_data=True,
+)
