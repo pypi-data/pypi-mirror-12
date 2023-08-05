33adf50178f1cba01462f5ce090baf0e406d0d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:00 +0000

    Add package skip32/1.6/skip32-1.6.tar.gz

---
+++ packages/skip32/skip32-1.6.tar.gz/skip32-1.6/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+module1 = Extension('skip32', sources = ['skip32.c'])
+
+NAME         = "skip32"
+DESCRIPTION  = 'implementation of skip32 based on http://search.cpan.org/~esh/Crypt-Skip32-0.08/lib/Crypt/Skip32.pm',
+AUTHOR       = "Jefurry"
+AUTHOR_EMAIL = "jefurry@qq.com"
+URL          = "http://www.penqie.com"
+VERSION      = "1.6"
+
+setup (name              = NAME,
+        version          = VERSION,
+        keywords         = ['skip32', 'pyskip32'],
+        description      = DESCRIPTION,
+        dependency_links = [],
+        install_requires = [],
+        url              = URL,
+        license          = "LGPL",
+        ext_modules      = [module1],
+        author           = AUTHOR,
+        author_email     = AUTHOR_EMAIL,
+        scripts          = [])
+
