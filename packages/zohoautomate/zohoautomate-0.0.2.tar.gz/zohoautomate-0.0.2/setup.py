ea5ae5c7c3c343a399d9cc6a7c8f9cf585602f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:59 +0000

    Add package zohoautomate/0.0.2/zohoautomate-0.0.2.tar.gz

---
+++ packages/zohoautomate/zohoautomate-0.0.2.tar.gz/zohoautomate-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'zohoautomate',
+  packages = ['zohoautomate'], # this must be the same as the name above
+  version = '0.0.2',
+  description = '''A library to do basic operations in Zoho Mail, as they don't provide an API.''',
+  author = 'Tom Werner',
+  author_email = 'hello.github@tomwerner.me.uk',
+  url = 'https://github.com/tomwerneruk/zohoautomate', # use the URL to the github repo
+  download_url = 'https://github.com/tomwerneruk/zohoautomate/tarball/v0.0.2', # I'll explain this in a second
+  keywords = ['zoho', 'api', 'automation', 'mail'], # arbitrary keywords
+  setup_requires =[
+    'selenium',
+  ]
+)
