d4d913d0b34cbc7d5db993ebee8d754b4ba9bb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:12 +0000

    Add package django-templates-admin/0.1.4/django-templates-admin-0.1.4.tar.gz

---
+++ packages/django-templates-admin/django-templates-admin-0.1.4.tar.gz/django-templates-admin-0.1.4/setup.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+"""
+This Django app allows to edit Django project template files from within the Django administrative interface.
+"""
+
+setup(
+	name='django-templates-admin',
+	version='0.1.4',
+	url='https://github.com/IlyaSemenov/django-templates-admin',
+	license='BSD',
+	author='Ilya Semenov',
+	author_email='ilya@semenov.co',
+	description='Edit project template files from the Django admin UI',
+	long_description=__doc__,
+	packages=find_packages(),
+	zip_safe=False,
+	platforms='any',
+	install_requires=[],
+	classifiers=[],
+)
