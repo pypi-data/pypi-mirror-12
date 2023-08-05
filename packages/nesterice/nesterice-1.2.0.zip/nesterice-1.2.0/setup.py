b96de8c23648868b28cacf6d68d5ff25d2089317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:11 +0000

    Add package nesterice/1.2.0/nesterice-1.2.0.zip

---
+++ packages/nesterice/nesterice-1.2.0.zip/nesterice-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'nesterice',
+    version         = '1.2.0',
+    py_modules      = ['nesterice'],
+    author          = 'dev.ICE',
+    author_email    = 'hs2000@inbox.ru',
+    url             = 'http://www.headfirstlabs.com',
+    description     = 'A simple printer of nested lists'
+    )
