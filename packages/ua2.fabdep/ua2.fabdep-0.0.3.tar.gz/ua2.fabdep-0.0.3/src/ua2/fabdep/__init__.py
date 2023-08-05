4f50ab34e21e7a67d74cb80317fefcdb86313fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:31 +0000

    Add package ua2.fabdep/0.0.3/ua2.fabdep-0.0.3.tar.gz

---
+++ packages/ua2.fabdep/ua2.fabdep-0.0.3.tar.gz/ua2.fabdep-0.0.3/src/ua2/fabdep/__init__.py
@@ -0,0 +1,5 @@
+from ua2.fabdep.vc import Mercurial, Subversion
+from ua2.fabdep.tasks import depup, depstick, depver, deprefresh
+
+__all__ = ['Mercurial', 'Subversion', 'depup', 'depstick', 'depver',
+           'deprefresh']
