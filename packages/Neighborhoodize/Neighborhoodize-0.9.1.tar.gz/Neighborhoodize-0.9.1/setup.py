45f2dc4a2c5a86bc99ebeb9c63f990d7276e867f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package Neighborhoodize/0.9.1/Neighborhoodize-0.9.1.tar.gz

---
+++ packages/Neighborhoodize/Neighborhoodize-0.9.1.tar.gz/Neighborhoodize-0.9.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="Neighborhoodize",
+      version='0.9.1',
+      description='Utility for translating lat, long coordinates into '
+                  'neighborhoods in various cities',
+      author='Brian Lange',
+      author_email='brian.lange@datascopeanalytics.com',
+      url='https://github.com/bjlange/neighborhoodize',
+      packages=['neighborhoodize', ],
+      package_data={'neighborhoodize': ['data/*']},
+      download_url = 'https://github.com/bjlange/neighborhoodize/tarball/0.9',
+      install_requires=[
+          "Shapely >= 1.5.7",
+          "beautifulsoup4 >= 4.3.2",
+          "fastkml >= 0.9",
+          "fiona >= 1.6.2"
+      ],
+      keywords=['neighborhood','gis','boundaries'],
+      classifiers = [],
+      )
