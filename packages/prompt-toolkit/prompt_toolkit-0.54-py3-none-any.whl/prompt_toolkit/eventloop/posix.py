d32bfa76da25de9c80a5641f53cbea98a10ea46d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:50 +0000

    Add package prompt-toolkit/0.54/prompt_toolkit-0.54-py3-none-any.whl

---
+++ packages/prompt-toolkit/prompt_toolkit-0.54-py3-none-any.whl/prompt_toolkit/eventloop/posix.py
@@ -0,0 +1,258 @@
+from __future__ import unicode_literals
+import os
+import fcntl
+import select
+import signal
+import errno
+import threading
+
+from prompt_toolkit.terminal.vt100_input import InputStream
+from prompt_toolkit.utils import DummyContext, in_main_thread
+from prompt_toolkit.input import Input
+from .base import EventLoop, INPUT_TIMEOUT
+from .callbacks import EventLoopCallbacks
+from .inputhook import InputHookContext
+from .posix_utils import PosixStdinReader
+from .utils import TimeIt
+
+__all__ = (
+    'PosixEventLoop',
+)
+
+
+class PosixEventLoop(EventLoop):
+    """
+    Event loop for posix systems (Linux, Mac os X).
+    """
+    def __init__(self, inputhook=None):
+        assert inputhook is None or callable(inputhook)
+
+        self.running = False
+        self.closed = False
+        self._running = False
+        self._callbacks = None
+
+        self._calls_from_executor = []
+        self._read_fds = {} # Maps fd to handler.
+
+        # Create a pipe for inter thread communication.
+        self._schedule_pipe = os.pipe()
+        fcntl.fcntl(self._schedule_pipe[0], fcntl.F_SETFL, os.O_NONBLOCK)
+
+        # Create inputhook context.
+        self._inputhook_context = InputHookContext(inputhook) if inputhook else None
+
+    def run(self, stdin, callbacks):
+        """
+        The input 'event loop'.
+        """
+        assert isinstance(stdin, Input)
+        assert isinstance(callbacks, EventLoopCallbacks)
+        assert not self._running
+
+        if self.closed:
+            raise Exception('Event loop already closed.')
+
+        self._running = True
+        self._callbacks = callbacks
+
+        inputstream = InputStream(callbacks.feed_key)
+        current_timeout = INPUT_TIMEOUT
+
+        # Create reader class.
+        stdin_reader = PosixStdinReader(stdin.fileno())
+
+        # Only attach SIGWINCH signal handler in main thread.
+        # (It's not possible to attach signal handlers in other threads. In
+        # that case we should rely on a the main thread to call this manually
+        # instead.)
+        if in_main_thread():
+            ctx = call_on_sigwinch(self.received_winch)
+        else:
+            ctx = DummyContext()
+
+        with ctx:
+            while self._running:
+                # Call inputhook.
+                with TimeIt() as inputhook_timer:
+                    if self._inputhook_context:
+                        def ready(wait):
+                            " True when there is input ready. The inputhook should return control. "
+                            return self._ready_for_reading(stdin, current_timeout if wait else 0) != []
+                        self._inputhook_context.call_inputhook(ready)
+
+                # Calculate remaining timeout. (The inputhook consumed some of the time.)
+                if current_timeout is None:
+                    remaining_timeout = None
+                else:
+                    remaining_timeout = max(0, current_timeout - inputhook_timer.duration)
+
+                # Wait until input is ready.
+                r = self._ready_for_reading(stdin, remaining_timeout)
+
+                # If we got a character, feed it to the input stream. If we got
+                # none, it means we got a repaint request.
+                if stdin in r:
+                    # Feed input text.
+                    data = stdin_reader.read()
+                    inputstream.feed(data)
+
+                    # Set timeout again.
+                    current_timeout = INPUT_TIMEOUT
+
+                # When any of the registered read pipes are ready. Call the
+                # appropriate callback.
+                elif set(r) & set(self._read_fds):
+                    for fd in r:
+                        handler = self._read_fds.get(fd)
+                        if handler:
+                            handler()
+
+                # If we receive something on our "call_from_executor" pipe, process
+                # these callbacks in a thread safe way.
+                elif self._schedule_pipe[0] in r:
+                    # Flush all the pipe content.
+                    os.read(self._schedule_pipe[0], 1024)
+
+                    # Process calls from executor.
+                    calls_from_executor, self._calls_from_executor = self._calls_from_executor, []
+                    for c in calls_from_executor:
+                        c()
+
+                else:
+                    # Flush all pending keys on a timeout. (This is most
+                    # important to flush the vt100 'Escape' key early when
+                    # nothing else follows.)
+                    inputstream.flush()
+
+                    # Fire input timeout event.
+                    callbacks.input_timeout()
+                    current_timeout = None
+
+        self._callbacks = None
+
+    def _ready_for_reading(self, stdin, timeout=None):
+        """
+        Return the file descriptors that are ready for reading.
+        """
+        read_fds = [stdin, self._schedule_pipe[0]]
+        read_fds.extend(self._read_fds.keys())
+
+        r, _, _ =_select(read_fds, [], [], timeout)
+        return r
+
+    def received_winch(self):
+        """
+        Notify the event loop that SIGWINCH has been received
+        """
+        # Process signal asynchronously, because this handler can write to the
+        # output, and doing this inside the signal handler causes easily
+        # reentrant calls, giving runtime errors..
+
+        # Furthur, this has to be thread safe. When the CommandLineInterface
+        # runs not in the main thread, this function still has to be called
+        # from the main thread. (The only place where we can install signal
+        # handlers.)
+        def process_winch():
+            if self._callbacks:
+                self._callbacks.terminal_size_changed()
+
+        self.call_from_executor(process_winch)
+
+    def run_in_executor(self, callback):
+        """
+        Run a long running function in a background thread.
+        (This is recommended for code that could block the event loop.)
+        Similar to Twisted's ``deferToThread``.
+        """
+        # Wait until the main thread is idle.
+        # We start the thread by using `call_from_executor`. The event loop
+        # favours processing input over `calls_from_executor`, so the thread
+        # will not start until there is no more input to process and the main
+        # thread becomes idle for an instant. This is good, because Python
+        # threading favours CPU over I/O -- an autocompletion thread in the
+        # background would cause a significantly slow down of the main thread.
+        # It is mostly noticable when pasting large portions of text while
+        # having real time autocompletion while typing on.
+        def start_executor():
+            threading.Thread(target=callback).start()
+        self.call_from_executor(start_executor)
+
+    def call_from_executor(self, callback):
+        """
+        Call this function in the main event loop.
+        Similar to Twisted's ``callFromThread``.
+        """
+        self._calls_from_executor.append(callback)
+
+        if self._schedule_pipe:
+            os.write(self._schedule_pipe[1], b'x')
+
+    def stop(self):
+        """
+        Stop the event loop.
+        """
+        self._running = False
+
+    def close(self):
+        self.closed = True
+
+        # Close pipes.
+        schedule_pipe = self._schedule_pipe
+        self._schedule_pipe = None
+
+        if schedule_pipe:
+            os.close(schedule_pipe[0])
+            os.close(schedule_pipe[1])
+
+        if self._inputhook_context:
+            self._inputhook_context.close()
+
+    def add_reader(self, fd, callback):
+        " Add read file descriptor to the event loop. "
+        self._read_fds[fd] = callback
+
+    def remove_reader(self, fd):
+        " Remove read file descriptor from the event loop. "
+        if fd in self._read_fds:
+            del self._read_fds[fd]
+
+
+def _select(*args, **kwargs):
+    """
+    Wrapper around select.select.
+
+    When the SIGWINCH signal is handled, other system calls, like select
+    are aborted in Python. This wrapper will retry the system call.
+    """
+    while True:
+        try:
+            return select.select(*args, **kwargs)
+        except select.error as e:
+            # Retry select call when EINTR
+            if e.args and e.args[0] == errno.EINTR:
+                continue
+            else:
+                raise
+
+
+class call_on_sigwinch(object):
+    """
+    Context manager which Installs a SIGWINCH callback.
+    (This signal occurs when the terminal size changes.)
+    """
+    def __init__(self, callback):
+        self.callback = callback
+        self.previous_callback = None
+
+    def __enter__(self):
+        self.previous_callback = signal.signal(signal.SIGWINCH, lambda *a: self.callback())
+
+    def __exit__(self, *a, **kw):
+        if self.previous_callback is None:
+            # Normally, `signal.signal` should never return `None`.
+            # For some reason it happens here:
+            # https://github.com/jonathanslenders/python-prompt-toolkit/pull/174
+            signal.signal(signal.SIGWINCH, 0)
+        else:
+            signal.signal(signal.SIGWINCH, self.previous_callback)
---
+++ packages/prompt-toolkit/prompt_toolkit-0.54-py3-none-any.whl/prompt_toolkit/layout/utils.py
@@ -0,0 +1,105 @@
+from __future__ import unicode_literals
+
+from prompt_toolkit.utils import get_cwidth
+
+__all__ = (
+    'token_list_len',
+    'token_list_width',
+    'token_list_to_text',
+    'explode_tokens',
+    'find_window_for_buffer_name',
+)
+
+
+def token_list_len(tokenlist):
+    """
+    Return the amount of characters in this token list.
+
+    :param tokenlist: List of (token, text) tuples.
+    """
+    return sum(len(v) for k, v in tokenlist)
+
+
+def token_list_width(tokenlist):
+    """
+    Return the character width of this token list.
+    (Take double width characters into account.)
+
+    :param tokenlist: List of (token, text) tuples.
+    """
+    return sum(get_cwidth(c) for k, word in tokenlist for c in word)
+
+
+def token_list_to_text(tokenlist):
+    """
+    Concatenate all the text parts again.
+    """
+    return ''.join(v for k, v in tokenlist)
+
+
+def iter_token_lines(tokenlist):
+    """
+    Iterator that yields tokenlists for each line.
+    """
+    line = []
+    for token, c in explode_tokens(tokenlist):
+        line.append((token, c))
+
+        if c == '\n':
+            yield line
+            line = []
+
+    yield line
+
+
+def split_lines(tokenlist):
+    """
+    Take a single list of (Token, text) tuples and yield one such list for each
+    line.
+    """
+    line = []
+
+    for token, string in tokenlist:
+        items = string.split('\n')
+
+        for item in items[:-1]:
+            if item:
+                line.append((token, item))
+            yield line
+            line = []
+
+        line.append((token, items[-1]))
+
+    if line:
+        yield line
+
+
+def explode_tokens(tokenlist):
+    """
+    Turn a list of (token, text) tuples into another list where each string is
+    exactly one character.
+
+    :param tokenlist: List of (token, text) tuples.
+    """
+    result = []
+
+    for token, string in tokenlist:
+        for c in string:
+            result.append((token, c))
+
+    return result
+
+
+def find_window_for_buffer_name(layout, buffer_name):
+    """
+    Look for a :class:`~prompt_toolkit.layout.containers.Window` in the Layout
+    that contains the :class:`~prompt_toolkit.layout.controls.BufferControl`
+    for the given buffer and return it. If no such Window is found, return None.
+    """
+    from .containers import Window
+    from .controls import BufferControl
+
+    for l in layout.walk():
+        if isinstance(l, Window) and isinstance(l.content, BufferControl):
+            if l.content.buffer_name == buffer_name:
+                return l
