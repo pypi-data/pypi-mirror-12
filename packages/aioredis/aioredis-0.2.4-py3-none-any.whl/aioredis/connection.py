d41450f7d861b9fa2277f2ca1802fbec69cc6547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:16 +0000

    Add package aioredis/0.2.4/aioredis-0.2.4-py3-none-any.whl

---
+++ packages/aioredis/aioredis-0.2.4-py3-none-any.whl/aioredis/connection.py
@@ -0,0 +1,355 @@
+import types
+import asyncio
+import hiredis
+from functools import partial
+from collections import deque
+
+from .util import (
+    encode_command,
+    wait_ok, _NOTSET,
+    coerced_keys_dict,
+    Channel,
+    decode,
+    )
+from .errors import RedisError, ProtocolError, ReplyError
+from .log import logger
+
+
+__all__ = ['create_connection', 'RedisConnection']
+
+MAX_CHUNK_SIZE = 65536
+
+_PUBSUB_COMMANDS = (
+    'SUBSCRIBE', b'SUBSCRIBE',
+    'PSUBSCRIBE', b'PSUBSCRIBE',
+    'UNSUBSCRIBE', b'UNSUBSCRIBE',
+    'PUNSUBSCRIBE', b'PUNSUBSCRIBE',
+    )
+
+
+@asyncio.coroutine
+def create_connection(address, *, db=None, password=None,
+                      encoding=None, loop=None):
+    """Creates redis connection.
+
+    Opens connection to Redis server specified by address argument.
+    Address argument is similar to socket address argument, ie:
+    * when address is a tuple it represents (host, port) pair;
+    * when address is a str it represents unix domain socket path.
+    (no other address formats supported)
+
+    Encoding argument can be used to decode byte-replies to strings.
+    By default no decoding is done.
+
+    Return value is RedisConnection instance.
+
+    This function is a coroutine.
+    """
+    assert isinstance(address, (tuple, list, str)), "tuple or str expected"
+
+    if isinstance(address, (list, tuple)):
+        host, port = address
+        reader, writer = yield from asyncio.open_connection(
+            host, port, loop=loop)
+    else:
+        reader, writer = yield from asyncio.open_unix_connection(
+            address, loop=loop)
+    conn = RedisConnection(reader, writer, encoding=encoding, loop=loop)
+
+    try:
+        if password is not None:
+            yield from conn.auth(password)
+        if db is not None:
+            yield from conn.select(db)
+    except Exception:
+        conn.close()
+        yield from conn.wait_closed()
+        raise
+    return conn
+
+
+class RedisConnection:
+    """Redis connection."""
+
+    def __init__(self, reader, writer, *, encoding=None, loop=None):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        self._reader = reader
+        self._writer = writer
+        self._loop = loop
+        self._waiters = deque()
+        self._parser = hiredis.Reader(protocolError=ProtocolError,
+                                      replyError=ReplyError)
+        self._reader_task = asyncio.Task(self._read_data(), loop=self._loop)
+        self._db = 0
+        self._closing = False
+        self._closed = False
+        self._close_waiter = asyncio.Future(loop=self._loop)
+        self._reader_task.add_done_callback(self._close_waiter.set_result)
+        self._in_transaction = False
+        self._transaction_error = None
+        self._in_pubsub = 0
+        self._pubsub_channels = coerced_keys_dict()
+        self._pubsub_patterns = coerced_keys_dict()
+        self._encoding = encoding
+
+    def __repr__(self):
+        return '<RedisConnection [db:{}]>'.format(self._db)
+
+    @asyncio.coroutine
+    def _read_data(self):
+        """Response reader task."""
+        while not self._reader.at_eof():
+            try:
+                data = yield from self._reader.read(MAX_CHUNK_SIZE)
+            except asyncio.CancelledError:
+                break
+            except Exception as exc:
+                # XXX: for QUIT command connection error can be received
+                #       before response
+                logger.error("Exception on data read %r", exc, exc_info=True)
+                break
+            self._parser.feed(data)
+            while True:
+                try:
+                    obj = self._parser.gets()
+                except ProtocolError as exc:
+                    # ProtocolError is fatal
+                    # so connection must be closed
+                    self._closing = True
+                    self._loop.call_soon(self._do_close, exc)
+                    if self._in_transaction:
+                        self._transaction_error = exc
+                    return
+                else:
+                    if obj is False:
+                        break
+                    if self._in_pubsub:
+                        self._process_pubsub(obj)
+                    else:
+                        self._process_data(obj)
+        self._closing = True
+        self._loop.call_soon(self._do_close, None)
+
+    def _process_data(self, obj):
+        """Processes command results."""
+        waiter, encoding, cb = self._waiters.popleft()
+        if waiter.done():
+            logger.debug("Waiter future is already done %r", waiter)
+            assert waiter.cancelled(), (
+                "waiting future is in wrong state", waiter, obj)
+            return
+        if isinstance(obj, RedisError):
+            waiter.set_exception(obj)
+            if self._in_transaction:
+                self._transaction_error = obj
+        else:
+            if encoding is not None:
+                try:
+                    obj = decode(obj, encoding)
+                except Exception as exc:
+                    waiter.set_exception(exc)
+                    return
+            waiter.set_result(obj)
+            if cb is not None:
+                cb(obj)
+
+    def _process_pubsub(self, obj, *, _process_waiters=True):
+        """Processes pubsub messages."""
+        kind, *pattern, chan, data = obj
+        if _process_waiters and self._in_pubsub and self._waiters:
+            self._process_data(obj)
+
+        if kind in (b'subscribe', b'unsubscribe'):
+            if kind == b'subscribe' and chan not in self._pubsub_channels:
+                self._pubsub_channels[chan] = Channel(chan, is_pattern=False,
+                                                      loop=self._loop)
+            elif kind == b'unsubscribe':
+                ch = self._pubsub_channels.pop(chan, None)
+                if ch:
+                    ch.close()
+            self._in_pubsub = data
+        elif kind in (b'psubscribe', b'punsubscribe'):
+            if kind == b'psubscribe' and chan not in self._pubsub_patterns:
+                self._pubsub_patterns[chan] = Channel(chan, is_pattern=True,
+                                                      loop=self._loop)
+            elif kind == b'punsubscribe':
+                ch = self._pubsub_patterns.pop(chan, None)
+                if ch:
+                    ch.close()
+            self._in_pubsub = data
+        elif kind == b'message':
+            self._pubsub_channels[chan].put_nowait(data)
+        elif kind == b'pmessage':
+            pattern = pattern[0]
+            self._pubsub_patterns[pattern].put_nowait((chan, data))
+        else:
+            logger.warning("Unknown pubsub message received %r", obj)
+
+    def execute(self, command, *args, encoding=_NOTSET):
+        """Executes redis command and returns Future waiting for the answer.
+
+        Raises:
+        * TypeError if any of args can not be encoded as bytes.
+        * ReplyError on redis '-ERR' resonses.
+        * ProtocolError when response can not be decoded meaning connection
+          is broken.
+        """
+        assert self._reader and not self._reader.at_eof(), (
+            "Connection closed or corrupted")
+        if command is None:
+            raise TypeError("command must not be None")
+        if None in set(args):
+            raise TypeError("args must not contain None")
+        command = command.upper().strip()
+        is_pubsub = command in _PUBSUB_COMMANDS
+        if self._in_pubsub and not is_pubsub:
+            raise RedisError("Connection in SUBSCRIBE mode")
+        elif is_pubsub:
+            logger.warning("Deprecated. Use `execute_pubsub` method directly")
+            return self.execute_pubsub(command, *args)
+
+        if command in ('SELECT', b'SELECT'):
+            cb = partial(self._set_db, args=args)
+        elif command in ('MULTI', b'MULTI'):
+            cb = self._start_transaction
+        elif command in ('EXEC', b'EXEC', 'DISCARD', b'DISCARD'):
+            cb = self._end_transaction
+        else:
+            cb = None
+        if encoding is _NOTSET:
+            encoding = self._encoding
+        fut = asyncio.Future(loop=self._loop)
+        self._writer.write(encode_command(command, *args))
+        self._waiters.append((fut, encoding, cb))
+        return fut
+
+    def execute_pubsub(self, command, *channels):
+        """Executes redis (p)subscribe/(p)unsubscribe commands.
+
+        Returns asyncio.gather coroutine waiting for all channels/patterns
+        to receive answers.
+        """
+        command = command.upper().strip()
+        assert command in _PUBSUB_COMMANDS, (
+            "Pub/Sub command expected", command)
+        if None in set(channels):
+            raise TypeError("args must not contain None")
+        if not len(channels):
+            raise ValueError("No channels/patterns supplied")
+        cmd = encode_command(command, *channels)
+        res = []
+        for ch in channels:
+            fut = asyncio.Future(loop=self._loop)
+            res.append(fut)
+            self._waiters.append((fut, None, self._update_pubsub))
+        self._writer.write(cmd)
+        return asyncio.gather(*res, loop=self._loop)
+
+    def close(self):
+        """Close connection."""
+        self._do_close(None)
+
+    def _do_close(self, exc):
+        if self._closed:
+            return
+        self._closed = True
+        self._closing = False
+        self._writer.transport.close()
+        self._reader_task.cancel()
+        self._reader_task = None
+        self._writer = None
+        self._reader = None
+        while self._waiters:
+            waiter, *spam = self._waiters.popleft()
+            logger.debug("Cancelling waiter %r", (waiter, spam))
+            if exc is None:
+                waiter.cancel()
+            else:
+                waiter.set_exception(exc)
+        # TODO: close all subscribed channels
+
+    @property
+    def closed(self):
+        """True if connection is closed."""
+        closed = self._closing or self._closed
+        if not closed and self._reader and self._reader.at_eof():
+            self._closing = closed = True
+            self._loop.call_soon(self._do_close, None)
+        return closed
+
+    @asyncio.coroutine
+    def wait_closed(self):
+        yield from self._close_waiter
+
+    @property
+    def db(self):
+        """Currently selected db index."""
+        return self._db
+
+    @property
+    def encoding(self):
+        """Current set codec or None."""
+        return self._encoding
+
+    def select(self, db):
+        """Change the selected database for the current connection."""
+        if not isinstance(db, int):
+            raise TypeError("DB must be of int type, not {!r}".format(db))
+        if db < 0:
+            raise ValueError("DB must be greater or equal 0, got {!r}"
+                             .format(db))
+        fut = self.execute('SELECT', db)
+        return wait_ok(fut)
+
+    def _set_db(self, ok, args):
+        assert ok in {b'OK', 'OK'}, ok
+        self._db = args[0]
+
+    def _start_transaction(self, ok):
+        assert not self._in_transaction
+        self._in_transaction = True
+        self._transaction_error = None
+
+    def _end_transaction(self, ok):
+        assert self._in_transaction
+        self._in_transaction = False
+        self._transaction_error = None
+
+    def _update_pubsub(self, obj):
+        *head, subscriptions = obj
+        self._in_pubsub, was_in_pubsub = subscriptions, self._in_pubsub
+        if not was_in_pubsub:
+            self._process_pubsub(obj, _process_waiters=False)
+
+    @property
+    def in_transaction(self):
+        """Set to True when MULTI command was issued."""
+        return self._in_transaction
+
+    @property
+    def in_pubsub(self):
+        """Indicates that connection is in PUB/SUB mode.
+
+        Provides the number of subscribed channels.
+        """
+        return self._in_pubsub
+
+    @property
+    def pubsub_channels(self):
+        """Returns read-only channels dict."""
+        return types.MappingProxyType(self._pubsub_channels)
+
+    @property
+    def pubsub_patterns(self):
+        """Returns read-only patterns dict."""
+        return types.MappingProxyType(self._pubsub_patterns)
+
+    def auth(self, password):
+        """Authenticate to server."""
+        fut = self.execute('AUTH', password)
+        return wait_ok(fut)
+
+    @asyncio.coroutine
+    def get_atomic_connection(self):
+        return self
---
+++ packages/aioredis/aioredis-0.2.4-py3-none-any.whl/aioredis/util.py
@@ -0,0 +1,237 @@
+import asyncio
+import json
+import sys
+
+from .errors import ChannelClosedError
+
+PY_35 = sys.version_info >= (3, 5)
+
+_NOTSET = object()
+
+# NOTE: never put here anything else;
+#       just this basic types
+_converters = {
+    bytes: lambda val: val,
+    bytearray: lambda val: val,
+    str: lambda val: val.encode('utf-8'),
+    int: lambda val: str(val).encode('utf-8'),
+    float: lambda val: str(val).encode('utf-8'),
+    }
+
+
+def _bytes_len(sized):
+    return str(len(sized)).encode('utf-8')
+
+
+def encode_command(*args):
+    """Encodes arguments into redis bulk-strings array.
+
+    Raises TypeError if any of args not of bytes, str, int or float type.
+    """
+    buf = bytearray()
+
+    def add(data):
+        return buf.extend(data + b'\r\n')
+
+    add(b'*' + _bytes_len(args))
+    for arg in args:
+        if type(arg) in _converters:
+            barg = _converters[type(arg)](arg)
+            add(b'$' + _bytes_len(barg))
+            add(barg)
+        else:
+            raise TypeError("Argument {!r} expected to be of bytes,"
+                            " str, int or float type".format(arg))
+    return buf
+
+
+def decode(obj, encoding):
+    if isinstance(obj, bytes):
+        return obj.decode(encoding)
+    elif isinstance(obj, list):
+        return [o.decode(encoding) if isinstance(o, bytes) else o
+                for o in obj]
+    return obj
+
+
+class Channel:
+    """Wrapper around asyncio.Queue."""
+    __slots__ = ('_queue', '_name',
+                 '_closed', '_waiter',
+                 '_is_pattern', '_loop')
+
+    def __init__(self, name, is_pattern, loop=None):
+        self._queue = asyncio.Queue(loop=loop)
+        self._name = name
+        self._is_pattern = is_pattern
+        self._loop = loop
+        self._closed = False
+        self._waiter = None
+
+    def __repr__(self):
+        return "<Channel name:{}, is_pattern:{}, qsize:{}>".format(
+            self._name, self._is_pattern, self._queue.qsize())
+
+    @property
+    def name(self):
+        """Encoded channel name/pattern."""
+        return self._name
+
+    @property
+    def is_pattern(self):
+        """Set to True if channel is subscribed to pattern."""
+        return self._is_pattern
+
+    @property
+    def is_active(self):
+        """Returns True until there are messages in channel or
+        connection is subscribed to it.
+
+        Can be used with ``while``:
+
+        >>> ch = conn.pubsub_channels['chan:1']
+        >>> while ch.is_active():
+        ...     msg = yield from ch.get()   # may stuck for a long time
+
+        """
+        return not (self._queue.qsize() <= 1 and self._closed)
+
+    @asyncio.coroutine
+    def get(self, *, encoding=None, decoder=None):
+        """Coroutine that waits for and returns a message.
+
+        Raises (TBD) exception if channel is unsubscribed and has no messages.
+        """
+        assert decoder is None or callable(decoder), decoder
+        if not self.is_active:
+            raise ChannelClosedError()
+        msg = yield from self._queue.get()
+        if msg is None:
+            return
+        if self._is_pattern:
+            dest_channel, msg = msg
+        if encoding is not None:
+            msg = msg.decode(encoding)
+        if decoder is not None:
+            msg = decoder(msg)
+        if self._is_pattern:
+            return dest_channel, msg
+        return msg
+
+    @asyncio.coroutine
+    def get_json(self, encoding='utf-8'):
+        """Shortcut to get JSON messages."""
+        return (yield from self.get(encoding=encoding, decoder=json.loads))
+
+    @asyncio.coroutine
+    def wait_message(self):
+        """Waits for message to become available in channel.
+
+        Possible usage:
+
+        >>> while (yield from ch.wait_message()):
+        ...     msg = yield from ch.get()
+        """
+        if not self.is_active:
+            return False
+        if not self._queue.empty():
+            return True
+        if self._waiter is None:
+            self._waiter = asyncio.Future(loop=self._loop)
+        yield from self._waiter
+        return self.is_active
+
+    # internale methods
+
+    def put_nowait(self, data):
+        self._queue.put_nowait(data)
+        if self._waiter is not None:
+            fut, self._waiter = self._waiter, None
+            fut.set_result(None)
+
+    def close(self):
+        """Marks channel as inactive.
+
+        Internal method, will be called from connection
+        on `unsubscribe` command.
+        """
+        if not self._closed:
+            self.put_nowait(None)
+        self._closed = True
+
+
+@asyncio.coroutine
+def wait_ok(fut):
+    res = yield from fut
+    if res in (b'QUEUED', 'QUEUED'):
+        return res
+    return res in (b'OK', 'OK')
+
+
+@asyncio.coroutine
+def wait_convert(fut, type_):
+    result = yield from fut
+    if result in (b'QUEUED', 'QUEUED'):
+        return result
+    return type_(result)
+
+
+@asyncio.coroutine
+def wait_make_dict(fut):
+    res = yield from fut
+    if res in (b'QUEUED', 'QUEUED'):
+        return res
+    it = iter(res)
+    return dict(zip(it, it))
+
+
+class coerced_keys_dict(dict):
+
+    def __getitem__(self, other):
+        if not isinstance(other, bytes):
+            other = _converters[type(other)](other)
+        return dict.__getitem__(self, other)
+
+    def __contains__(self, other):
+        if not isinstance(other, bytes):
+            other = _converters[type(other)](other)
+        return dict.__contains__(self, other)
+
+
+if PY_35:
+    class _BaseScanIter:
+        __slots__ = ('_scan', '_cur', '_ret')
+
+        def __init__(self, scan):
+            self._scan = scan
+            self._cur = b'0'
+            self._ret = []
+
+        @asyncio.coroutine
+        def __aiter__(self):
+            return self
+
+    class _ScanIter(_BaseScanIter):
+
+        @asyncio.coroutine
+        def __anext__(self):
+            while not self._ret and self._cur:
+                self._cur, self._ret = yield from self._scan(self._cur)
+            if not self._cur and not self._ret:
+                raise StopAsyncIteration  # noqa
+            else:
+                ret = self._ret.pop(0)
+                return ret
+
+    class _ScanIterPairs(_BaseScanIter):
+
+        @asyncio.coroutine
+        def __anext__(self):
+            while not self._ret and self._cur:
+                self._cur, ret = yield from self._scan(self._cur)
+                self._ret = list(zip(ret[::2], ret[1::2]))
+            if not self._cur and not self._ret:
+                raise StopAsyncIteration  # noqa
+            else:
+                ret = self._ret.pop(0)
+                return ret
