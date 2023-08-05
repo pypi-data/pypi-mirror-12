9a9a081f9ea3335a48918c15e1d12a7c2497319e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:35 +0000

    Add package castroredux/0.1.2/castroredux-0.1.2.tar.gz

---
+++ packages/castroredux/castroredux-0.1.2.tar.gz/castroredux-0.1.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='castroredux',
+    version='0.1.2',
+    description="'screencast robot' - a tiny fork of vnc2flv",
+    long_description=readme,
+    author="Brome-HQ",
+    author_email='brome.hq@gmail.com',
+    url='https://github.com/brome-hq/castroredux',
+    packages=[
+        'castroredux',
+        'castroredux/tools',
+        'castroredux/tools/vnc2flv'
+    ],
+    package_dir={'castroredux':
+                 'castroredux'},
+    include_package_data=True,
+    license="ISCL",
+    zip_safe=False,
+    keywords='castroredux',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
