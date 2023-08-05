802958f3d0da99917c45f7516d87c702a31cc0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:40 +0000

    Add package magicserver/0.1/magicserver-0.1.tar.gz

---
+++ packages/magicserver/magicserver-0.1.tar.gz/magicserver-0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(name='magicserver',
+      version='0.1',
+      description='Basic HTTP Server',
+      author='geekskool',
+      author_email='hereisarunkrishnan@gmail.com',
+      url='https://www.geekskool.com',
+      py_modules=['magicserver'],
+     )
