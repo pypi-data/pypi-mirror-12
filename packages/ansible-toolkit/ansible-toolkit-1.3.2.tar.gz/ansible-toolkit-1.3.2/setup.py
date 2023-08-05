f0c8b9d0e8d9083a2ac73ad005eef372d3f19c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:23 +0000

    Add package ansible-toolkit/1.3.2/ansible-toolkit-1.3.2.tar.gz

---
+++ packages/ansible-toolkit/ansible-toolkit-1.3.2.tar.gz/ansible-toolkit-1.3.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(name='ansible-toolkit',
+      version='1.3.2',
+      description='The missing Ansible tools',
+      url='http://github.com/dellis23/ansible-toolkit',
+      author='Daniel Ellis',
+      author_email='ellisd23@gmail.com',
+      license='GPLv3',
+      install_requires=['ansible'],
+      packages=['ansible_toolkit'],
+      scripts=[
+          'bin/atk-git-diff',
+          'bin/atk-show-vars',
+          'bin/atk-show-template',
+          'bin/atk-vault',
+      ])
