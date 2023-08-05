8e7c5a5f70c72bd57f3a3dd6ea8aa8deb81f0056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:29 +0000

    Add package django-python-code-field-py3/0.0.1/django-python-code-field-py3-0.0.1.tar.gz

---
+++ packages/django-python-code-field-py3/django-python-code-field-py3-0.0.1.tar.gz/django-python-code-field-py3-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+import python_field
+
+setup(
+    name = "django-python-code-field-py3",
+    version = python_field.__version__,
+    description = "Store python source code (syntax checked) in database.",
+    url = "https://github.com/mwaaas/django-python-code-field.git",
+    author = "Chris Spencer",
+    author_email = "chrisspen@gmail.com",
+    packages = [
+        "python_field",
+    ],
+    classifiers = [
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Programming Language :: Python :: 2.7',
+        'Environment :: Web Environment',
+    ],
+    zip_safe = False,
+)
