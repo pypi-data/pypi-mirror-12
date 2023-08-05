b55626e1ddbc71589fcccdba5c84de3a396342c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:58 +0000

    Add package slbsl/0.2.3/slbsl-0.2.3.zip

---
+++ packages/slbsl/slbsl-0.2.3.zip/slbsl-0.2.3/setup.py
@@ -0,0 +1,25 @@
+# :coding: utf-8
+
+import setuptools
+
+setuptools.setup(
+    name="slbsl",
+    version="0.2.3",
+    description="Convert paths from Windows to UNIX and vice versa.",
+    long_description=open("README.rst").read(),
+    keywords="slash, backslash, linux, unix, windows, conversion",
+    url="https://github.com/cb109/slbsl.git",
+    author="Christoph Buelter",
+    author_email="info@cbuelter.de",
+    py_modules=["slbsl"],
+    install_requires=[
+        "pyperclip"
+    ],
+    entry_points={
+        "console_scripts": [
+            "sl=slbsl:sl",
+            "bsl=slbsl:bsl",
+            "fsl=slbsl:fsl",
+        ],
+    }
+)
