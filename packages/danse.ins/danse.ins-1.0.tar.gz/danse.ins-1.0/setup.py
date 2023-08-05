2316c92fd8cd83a389f1ebd995a349d4d7e571a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:36 +0000

    Add package danse.ins/1.0/danse.ins-1.0.tar.gz

---
+++ packages/danse.ins/danse.ins-1.0.tar.gz/danse.ins-1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='danse.ins',
+      version='1.0',
+      description='danse.ins namespace',
+      author='Jiao Lin',
+      author_email='jiao.lin@gmail.com',
+      url='https://github.com/danse-inelastic',
+      packages=['danse', 'danse.ins'],
+     )
