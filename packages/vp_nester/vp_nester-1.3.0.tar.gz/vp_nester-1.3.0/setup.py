8a0bcaa6c371d8b53377afc3ae3aa83b78f8669d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:55 +0000

    Add package vp_nester/1.3.0/vp_nester-1.3.0.tar.gz

---
+++ packages/vp_nester/vp_nester-1.3.0.tar.gz/vp_nester-1.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+	name = 'vp_nester',
+	version = '1.3.0',
+	py_module = ['nester'],
+	author = 'valmur prado',
+	autor_mail = 'valmur_prado@yahoo.com.br',
+	url = 'www.linkedin.com.br',
+	description = 'A simple printer of nested list'
+	)
