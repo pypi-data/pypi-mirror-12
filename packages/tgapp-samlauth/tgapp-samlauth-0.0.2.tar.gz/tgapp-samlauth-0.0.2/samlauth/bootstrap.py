4248cde23c873c5b4a4ec32ef7904bd877d1fd44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:53 +0000

    Add package tgapp-samlauth/0.0.2/tgapp-samlauth-0.0.2.tar.gz

---
+++ packages/tgapp-samlauth/tgapp-samlauth-0.0.2.tar.gz/tgapp-samlauth-0.0.2/samlauth/bootstrap.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Setup the samlauth application"""
+from __future__ import print_function
+
+
+def bootstrap(command, conf, vars):
+    pass
---
+++ packages/tgapp-samlauth/tgapp-samlauth-0.0.2.tar.gz/tgapp-samlauth-0.0.2/samlauth/helpers.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+"""WebHelpers used in tgapp-samlauth."""
+
+#from webhelpers import date, feedgenerator, html, number, misc, text
