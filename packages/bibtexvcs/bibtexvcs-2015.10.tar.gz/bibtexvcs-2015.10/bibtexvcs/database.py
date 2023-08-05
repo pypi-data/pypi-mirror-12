f0784f5471f992a721c4f63af5b756f8c01dda28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:08 +0000

    Add package bibtexvcs/2015.10/bibtexvcs-2015.10.tar.gz

---
+++ packages/bibtexvcs/bibtexvcs-2015.10.tar.gz/bibtexvcs-2015.10/bibtexvcs/database.py
@@ -0,0 +1,350 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2014-2015 Michael Helmling
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation
+
+"""The :mod:`database <bibtexvcs.database>` module contains classes for managing a BibTeX VCS
+database, which consists of a config file, a bib file, a documents directory, and a journals file.
+"""
+from __future__ import division, print_function, unicode_literals
+import configparser, io, os, subprocess
+from collections import OrderedDict
+from os.path import join, exists, relpath
+
+from pkg_resources import resource_string, resource_filename
+
+from bibtexvcs.bibfile import BibFile, MacroReference
+from bibtexvcs.vcs import VCSInterface
+
+BTVCSCONF = 'bibtexvcs.conf'  # name of the configuration file
+
+
+class DatabaseFormatError(Exception):
+    """Raised on any problem in the database layout (unparsable config-file, non-existing
+    bib file, etc.).
+    """
+    pass
+
+class NoDefaultDatabaseError(Exception):
+    """Raised when the default database is requested but none exists."""
+
+
+class Database:
+    """Represents a complete literature database.
+
+    The database configuration is read from the file `bbibtexvcs.conf` and from the layout of the
+    directory that file resides in.
+
+    Parameters
+    ----------
+    directory : str
+        Base path of the database.
+
+    Attributes
+    ----------
+    directory : str
+        Absolute path of the root directory of the literature database.
+    bibfileName : str
+        Name of the main bibtex database file.
+    bibfile : :class:`BibFile`
+        :class:`BibFile` object parsed from the bibtex file.
+    journalsName : str
+        Name of the journals file.
+    journals : :class:`JournalsFile`
+        :class:`JournalsFile` object read from the journals file.
+    name : str
+        Name of the database.
+    documents : str
+        Name of the documents directory (relative to :attr:`directory`).
+    vcsType : ("git", "mercurial", "local")
+        Type of the used VCS system.
+    """
+
+    def __init__(self, directory, vcs=None):
+        self.directory = directory
+        if exists(join(self.directory, '.git')):
+            self.vcsType = 'git'
+        elif exists(join(self.directory, '.hg')):
+            self.vcsType = 'mercurial'
+        else:
+            self.vcsType = 'local'
+        self._vcs = vcs
+        self.reload()
+
+    def reload(self):
+        """(Re-)loads the database from filesystem."""
+        parser = configparser.ConfigParser()
+        try:
+            with io.open(self.configPath, encoding='UTF-8') as f:
+                confFile = f.read()
+        except IOError:
+            raise DatabaseFormatError('Invalid BibTeX VCS directory "{}": '
+                                      'Configuration file bibtexvcs.conf not found.'
+                                      .format(self.directory))
+        # workaround because ConfigParser does not support sectionless entries
+        try:
+            parser.read_string("[root]\n" + confFile)
+        except configparser.Error as e:
+            raise DatabaseFormatError("Could not parse configuration file '{}':\n\n{}"
+                                      .format(BTVCSCONF, e))
+        config = parser['root']
+
+        self.bibfileName = config.get('bibfile', 'references.bib')
+        self.journalsName = config.get('journals', 'journals.txt')
+
+        for path in self.bibfilePath, self.journalsPath:
+            if not exists(path):
+                open(path, 'a').close()
+                self.vcs.add(relpath(path, self.directory))
+
+        self.bibfile = BibFile(join(self.directory, self.bibfileName))
+        self.journals = JournalsFile(join(self.directory, self.journalsName))
+
+        self.name = config.get('name', "Untitled Bibtex Database")
+        self.documents = config.get('documents', 'Documents')
+
+        self.makeJournalBibfiles()  # ensure these are up-to-date
+
+        self.publicLink = config.get('publicLink', None)
+        if not exists(self.documentsPath):
+            os.mkdir(self.documentsPath)
+
+    def setDefault(self):
+        """Set this database as default in config."""
+        from bibtexvcs import config
+        config.setDefaultDatabase(self)
+
+    @classmethod
+    def getDefault(cls):
+        from bibtexvcs import config
+        directory = config.getDefaultDirectory()
+        if directory is None:
+            raise NoDefaultDatabaseError('No default database configured')
+        return cls(directory)
+
+    @property
+    def journalsPath(self):
+        """The absolute path of the `journals` file."""
+        return join(self.directory, self.journalsName)
+
+    @property
+    def documentsPath(self):
+        """Absolute path of the `documents` directory."""
+        return join(self.directory, self.documents)
+
+    @property
+    def bibfilePath(self):
+        """Absolute path of the bib file."""
+        return join(self.directory, self.bibfileName)
+
+    @property
+    def configPath(self):
+        """Absolute path of the conf file."""
+        return join(self.directory, BTVCSCONF)
+
+    @property
+    def checksPath(self):
+        """Absolute path of the (optional) checks file."""
+        return join(self.directory, 'checks.py')
+
+    def referencedDocuments(self):
+        """Returns a list of all filenames of documents referenced to in the bib file."""
+        docs = []
+        for entry in self.bibfile.values():
+            fname = entry.filename()
+            if fname:
+                docs.append(fname)
+        return docs
+
+    def existingDocuments(self):
+        """Walks recursively through the :attr:`documents` directory and return the paths of all
+        files contained in there, relative to :attr:`documentsPath`.
+        """
+        for dirpath, _, filenames in os.walk(self.documentsPath):
+            for file in filenames:
+                if file != '.DS_Store':
+                    yield relpath(join(dirpath, file), self.documentsPath)
+
+    def strval(self, value):
+        """Returns a string value for *value*. If *value* is a :class:`MacroReference`, substitutes
+        the value (if known).
+        """
+        if isinstance(value, MacroReference):
+            if value.name in self.bibfile.macroDefinitions:
+                return self.bibfile.macroDefinitions[value.name]
+            if value.name in self.journals:
+                return self.journals[value.name].full
+        return str(value)
+
+    @property
+    def abbrJournalsName(self):
+        """Name of the abbreviated journals string file."""
+        return self.bibfileName[:-4] + '_abbr.bib'
+
+    @property
+    def fullJournalsName(self):
+        """Name of the full journals string file."""
+        return self.bibfileName[:-4] + '_full.bib'
+
+    def makeJournalBibfiles(self):
+        """Creates or updates the files containing journal macro definitions in full and
+        abbreviated form, respectively.
+        """
+        abbr, full = [join(self.directory, name) for name in
+                      (self.abbrJournalsName, self.fullJournalsName)]
+        if not exists(full) or not exists(abbr) or \
+                        os.path.getmtime(self.journalsPath) > os.path.getmtime(abbr):
+            self.journals.writeBibfiles(self.bibfileName[:-4])
+
+    def runJabref(self):
+        """Tries to open this database's ``.bib`` file with `JabRef`_. Will do the following:
+
+        - If there is a file named ``jabref.jar`` in :attr:`directory`, it is run with the
+          java interpreter through ``java -jar jabref.jar``. If additionally there is a file
+          ``jabref.prefs``, JabRef`s options are imported from that file.
+        - Otherwise, the command ``jabref`` is executed. To that end, the ``jabref`` binary must
+          be in the system's ``PATH``.
+
+        :returns: The :class:`subprocess.Popen` object corresponding to JabRef process.
+        """
+        shell = False
+        if exists(join(self.directory, 'jabref.jar')):
+            if os.name == 'nt':
+                cmdline = ['start', 'jabref.jar']
+                shell = True
+            else:
+                cmdline = ['java', '-jar', 'jabref.jar']
+        else:
+            cmdline = ['jabref']
+        if exists(join(self.directory, 'jabref.prefs')):
+            cmdline += ['--primp', join('jabref.prefs')]
+        else:
+            cmdline += ['--primp', resource_filename(__name__, 'defaultJabref.prefs')]
+        cmdline.append(os.curdir + os.sep + self.bibfileName)
+        try:
+            return subprocess.Popen(cmdline, shell=shell, cwd=self.directory)
+        except FileNotFoundError as fnf:
+            if cmdline[0] in ('java', 'start'):
+                fnf.strerror = 'Please install Java from http://java.com.'
+            elif cmdline[0] == 'jabref':
+                fnf.strerror = 'Please install JabRef from http://jabref.sf.net.'
+            raise fnf
+
+    @property
+    def vcs(self):
+        """The :class:`VCSInterface` object associated to this db.
+
+        Will be created on first access.
+        """
+        if self._vcs is None:
+            self._vcs = VCSInterface.get(self)
+        return self._vcs
+
+    def export(self, templateString=None, docDir=None):
+        """Exports the BibTeX database to a string by using the jinja template engine."""
+        import datetime, hashlib, bibtexvcs
+        try:
+            import jinja2
+        except ImportError:
+            raise ImportError('You need to install the jinja2 package in order to export.')
+        if docDir is None:
+            docDir = self.documentsPath
+        def md5filter(value):
+            return hashlib.md5(value.encode()).hexdigest()
+        env = jinja2.Environment(autoescape=False)
+        env.filters['md5'] = md5filter
+        if templateString is None:
+            templateString = resource_string(__name__, 'defaultTemplate.html').decode('UTF-8')
+        template = env.from_string(templateString)
+        revision = self.vcs.revision()
+        import locale
+        locale.setlocale(locale.LC_ALL, '')
+        now = datetime.datetime.now().strftime('%c')
+        version = bibtexvcs.__version__
+        return template.render(database=self, docDir=docDir, version=version, revision=revision, now=now)
+
+
+class Journal:
+    """A single journal entry in the journals file.
+
+    .. attribute:: macro
+
+        The macro defining the journal.
+    .. attribute:: abbr
+
+        The abbreviated journal name.
+    .. attribute:: full
+
+        The full (unabbreviated) journal name.
+    """
+    def __init__(self, macro, abbr, full):
+        self.macro = macro
+        self.abbr = abbr
+        self.full = full
+
+    def __iter__(self):
+        yield self.full
+        yield self.abbr
+        yield self.macro
+
+
+class JournalsFile(OrderedDict):
+    """Represents the file in a bibtexvcs database containing journal abbreviations.
+
+    In the journal file, each journal is represented by a configuration section of the
+    following form::
+
+        [JOURNAL_MACRO]
+        full = A Journal of Nice and Interesting Topics
+        abbr = J. Nice Int. Top.
+
+    :param filename: The path of the journals file.
+    :type  filename: str
+    :param journals: As an alternative of giving a ``filename``, you can also provide a list
+                     of journals entries to create the :class:`JournalsFile` object.
+    :type journals: iterable
+    """
+
+    def __init__(self, filename=None, journals=None):
+        super(JournalsFile, self).__init__()
+
+        if filename:
+            journals = []
+            parser = configparser.ConfigParser()
+            try:
+                parser.read(filename, encoding='UTF-8')
+            except configparser.Error as error:
+                raise DatabaseFormatError('Malformed journals file:\n\n{}'.format(error))
+            for section in parser.sections():
+                journals.append(Journal(macro=section,
+                                        abbr=parser.get(section, "abbr"),
+                                        full=parser.get(section, "full")))
+        for journal in sorted(journals, key=lambda j: j.full):
+            self[journal.macro] = journal
+
+    def write(self, filename):
+        """Write the journals file to the given filename."""
+        config = configparser.ConfigParser()
+        for journal in self.values():
+            config.add_section(journal.macro)
+            config[journal.macro]['abbr'] = journal.abbr
+            config[journal.macro]['full'] = journal.full
+        with io.open(filename, 'w', encoding='UTF-8', newline='\n') as journalfile:
+            config.write(journalfile)
+
+    def writeBibfiles(self, basename):
+        """Creates two ``.bib`` files that contain macro definitions for all journals defined in
+        this file, resolving to the full and abbreviated journal names, respectively.
+
+        `basename` is the base path of the output files, which will have the file names
+        ``<basename>_full.bib`` and ``<basename>_abbr.bib``, respectively.
+        """
+        for jrnlType in 'full', 'abbr':
+            outFile = '{}_{}.bib'.format(basename, jrnlType)
+            with io.open(outFile, 'w', encoding='UTF-8') as bibfile:
+                for journal in self.values():
+                    bibfile.write('@STRING{' + journal.macro + ' = {' + getattr(journal, jrnlType)
+                                  + '}}\n')
---
+++ packages/bibtexvcs/bibtexvcs-2015.10.tar.gz/bibtexvcs-2015.10/bibtexvcs/vcs.py
@@ -0,0 +1,282 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2014-2015 Michael Helmling
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation
+
+from __future__ import division, print_function, unicode_literals
+import os, subprocess, sys
+
+from bibtexvcs import config
+
+
+class Login:
+    """Data class representing login information for a remote repository.
+    """
+    def __init__(self, username=None, password=None):
+        self.username = username
+        self.password = password
+
+
+class MergeConflict(Exception):
+    """Raised when merge conflicts occur on a VCS update operation."""
+    pass
+
+
+class AuthError(Exception):
+    """Raised on authentication errors when accessing remote repositories."""
+    pass
+
+
+class VCSNotFoundError(Exception):
+    """Raised when the necessary VCS binary (e.g. "hg") is not installed."""
+
+
+class VCSInterface:
+    """Interface to the version control system (VCS) of a :mod:`bibtexvcs` database.
+    """
+
+    __typeMap = {}
+
+    def __init__(self, database):
+        self.database = database
+        storedLogin = Login(*config.getLogin(database))
+        self.login = storedLogin
+
+    @property
+    def root(self):
+        """Returns the root directory of this repository.
+        """
+        return self.database.directory
+
+    def storeLogin(self, login):
+        """Permanently store login information for this repository.
+        """
+        config.storeLogin(self.database, self.login.username, self.login.password)
+
+    def add(self, path):
+        """Adds a file or directory to the repository.
+        """
+
+    def hasLocalChanges(self):
+        """Determines whether any local changes have been made to the repository.
+        Returns
+        -------
+        bool
+            Return value is ``True`` iff one of the following requirements are met:
+
+            - any versioned file has been modified,
+            - any versioned file in the documents folder has been deleted, or
+            - any unversioned file has been placed in the documents folder.
+        """
+        raise NotImplementedError()
+
+    def update(self):
+        """Checks if there are updates in the remote repository. If so, tries to merge
+        them and reloads the database.
+
+        Returns
+        -------
+        bool
+            Flag indicating if something was updated or not.
+        Raises
+        ------
+        MergeConflict
+            If update results in a merge that cannot be handled automatically.
+        """
+        raise NotImplementedError()
+
+    def commit(self, commitMessage=None):
+        """Commit local changes and push to remote, if remote is configured.
+
+        Returns
+        ----------
+        bool
+            Indicator being ``True`` iff there actually were any changes.
+        """
+        raise NotImplementedError()
+
+    def revision(self):
+        """Returns a tuple containing the current revision and its date."""
+        raise NotImplementedError()
+
+
+    @staticmethod
+    def vcsTypeNames():
+        """Returns the names of all registered VCS types."""
+        return VCSInterface.__typeMap.keys()
+
+    @staticmethod
+    def registerVCSType(vcsTypeName, vcsTypeClass):
+        """Register a new VCS type."""
+        VCSInterface.__typeMap[vcsTypeName] = vcsTypeClass
+
+    @staticmethod
+    def getImplementation(vcsTypeName):
+        if vcsTypeName == 'local':
+            return None
+        try:
+            return VCSInterface.__typeMap[vcsTypeName]
+        except KeyError:
+            raise KeyError('No VCS implementation of type "{}" available'.format(vcsTypeName))
+
+    @staticmethod
+    def get(database):
+        return VCSInterface.getImplementation(database.vcsType)(database)
+
+    @classmethod
+    def clone(cls, url, target, login=None):
+        """Clones a remote repository.
+
+        Parameters
+        ----------
+        url : str
+            The remote repository URL.
+        target : str
+            The local target directory.
+        login : Login
+            Login information.
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    def getClonedDatabase(url, target, vcsType, login=None, storeLogin=False):
+        """Clone and return a database from a remote location specified by `url`.
+
+        Parameters
+        ----------
+        url : str
+            Source URL of the repository.
+        target : str
+            Local target directory.
+        vcsType
+            VCS system to use.
+        login : Login
+            Login information (optional).
+        storeLogin : bool (optional)
+            Store login on successful authentication.
+        """
+        vcsCls = VCSInterface.getImplementation(vcsType)
+        if login is None:
+            login = Login()
+        vcsCls.clone(url, target, login)
+        from bibtexvcs.database import Database
+        database = Database(target)
+        # copy login information
+        database.vcs.login = login
+        if storeLogin:
+            database.vcs.storeLogin()
+        return database
+
+
+class MercurialInterface(VCSInterface):
+    """Interface to the `Mercurial <http://mercurial.selenic.com>`_ version control system.
+    """
+
+    cmdline = ['hg', '--noninteractive', '--config', 'auth.x.prefix=*']
+
+    def __init__(self, *args, **kwargs):
+        VCSInterface.__init__(self, *args, **kwargs)
+        try:
+            self.hasRemote = len(self.callHg('showconfig', 'paths.default')) > 0
+        except subprocess.CalledProcessError:
+            # some hg versions appear to return exit status 1 if req'ed config noit set
+            self.hasRemote = False
+        self.hgid = self.callHg('id', '-i')
+
+    def callHg(self, *args):
+        """Calls the ``hg`` script in the database directory with the given arguments.
+        """
+        return MercurialInterface._callHg(*args, login=self.login, cwd=self.root)
+
+    @classmethod
+    def _callHg(cls, *args, **kwargs):
+        cmdline = cls.cmdline[:]
+        kwargs = kwargs.copy()
+        if kwargs.get('login'):
+            login = kwargs['login']
+            if login.username:
+                cmdline += ['--config', 'auth.x.username={}'.format(login.username)]
+            if login.password:
+                cmdline += ['--config', 'auth.x.password={}'.format(login.password)]
+        if 'login' in kwargs:
+            del kwargs['login']
+        env = os.environ.copy()
+        env['LANG'] = 'C'
+        env['LANGUAGE'] = 'C'  # on Ubuntu 12.04, LANG=C does not convince hg to use english output
+        try:
+            return subprocess.check_output(cmdline + list(args), env=env,
+                                           stderr=subprocess.STDOUT, **kwargs)
+        except subprocess.CalledProcessError as e:
+            output = e.output.decode(errors='replace')
+            if "authorization required" in output:
+                raise AuthError('Authorization required for the mercurial repository.')
+            if "authorization failed" in output:
+                raise AuthError('Authorization for the mercurial repository failed.')
+            if 'conflicts during merge' in output:
+                raise MergeConflict('Conflict arised when merging remote and local changes!\n'
+                                    'You need to fix this issue by hand. Error message:\n{}'
+                                    .format(e.output))
+            if 'unresolved merge conflicts' in output:
+                raise MergeConflict('There are unresolved merge conflicts in your repository.\n'
+                                    'You have to fix them manually before proceeding to use this '
+                                    'tool.')
+            print(output)
+            raise e
+        except OSError as e:
+            raise VCSNotFoundError('Could not run "hg". Please install mercurial')
+
+    def add(self, path):
+        self.callHg('add', path)
+
+    def hasLocalChanges(self):
+        if len(self.callHg('status', '--modified', '--added')) > 0:
+            return True
+        # by this we ignore unversioned files in the base folder
+        return len(self.callHg('status', '--deleted', '--unknown', self.database.documents)) > 0
+
+    def update(self):
+        if self.hasRemote:
+            self.callHg('pull')
+        # force internal merge algorithm with "-t merge" to prevent GUI tools opening
+        self.callHg('update', '-t', ':merge')
+        newId = self.callHg('id', '-i')
+        if newId != self.hgid:
+            self.hgid = newId
+            self.database.reload()
+            return True
+        return False
+
+    def commit(self, commitMessage=None):
+        if not self.hasLocalChanges():
+            return False
+        hgOutput = self.callHg('status', '--deleted', '--no-status', self.database.documents)
+        deletedDocs = hgOutput.decode(sys.getfilesystemencoding()).splitlines()
+        if len(deletedDocs) > 0:
+            self.callHg('remove', *deletedDocs)
+        hgOutput = self.callHg('status', '--unknown', '--no-status', self.database.documents)
+        newDocs = hgOutput.decode().splitlines()
+        if len(newDocs) > 0:
+            self.callHg('add', *newDocs)
+        # TODO: sophisticated analysis of diff to current head
+        self.callHg('commit', '--message', commitMessage or 'Auto-Commit by BibTeX VCS')
+        if self.hasRemote:
+            self.callHg('push')
+        self.database.reload()
+        self.hgid = self.callHg('id', '-i')
+        return True
+
+    def revision(self):
+        return self.callHg('log', '-l', '1', '--template', '{rev}\n{date|isodate}'
+                           ).decode().splitlines()
+
+    @classmethod
+    def clone(cls, url, target, login=None):
+        if login is None:
+            login = Login()
+        cls._callHg('clone', url, target, login=login)
+
+
+VCSInterface.registerVCSType('mercurial', MercurialInterface)
