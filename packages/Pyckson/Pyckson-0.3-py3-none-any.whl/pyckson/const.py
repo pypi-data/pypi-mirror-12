2a7f3da300c2274e294e40e72d0f1e22acfe561c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:23 +0000

    Add package Pyckson/0.3/Pyckson-0.3-py3-none-any.whl

---
+++ packages/Pyckson/Pyckson-0.3-py3-none-any.whl/pyckson/const.py
@@ -0,0 +1,8 @@
+from pyckson.model import ListType, UnresolvedListType
+
+PYCKSON_ATTR = '__pyckson'
+PYCKSON_TYPEINFO = '__pyckson_typeinfo'
+PYCKSON_MODEL = '__pyckson_model'
+PYCKSON_ENUM_PARSER = '__pyckson_enum'
+BASIC_TYPES = [int, str, float, bool]
+LIST_TYPES = [ListType, UnresolvedListType]
