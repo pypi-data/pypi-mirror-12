1540d974d74218ddb4be4cce1c5df7644ece318a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:43 +0000

    Add package yaprt/0.4.3/yaprt-0.4.3.tar.gz

---
+++ packages/yaprt/yaprt-0.4.3.tar.gz/yaprt-0.4.3/yaprt/executable.py
@@ -0,0 +1,204 @@
+# Copyright 2014, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# (c) 2014, Kevin Carter <kevin.carter@rackspace.com>
+
+"""Repository build tool.
+
+The idea behind this tool is to provide for an application that can build a
+repository of python wheels in a format that is pypi index capable, can be used
+as a pool of links, and resolves dependencies and dependent versions when
+duplicate packages are marked for building.
+
+This tool can:
+    * Build reports from information obtained using the github api about
+      specific repositories and or from the repositories of an entire account.
+      When scanning through the repositories, any repo with a
+      `requirements.txt` and or a `setup.py` file with the discovered path will
+      be added into the report as a list of requirements and or installable
+      URLs on a per branch basis. Also any thing that has been tagged within a
+      project will be listed in the report.
+    * Build the python wheels from a "pre-built" report or via input on the
+      CLI. If user input can also be added to an existing report which can be
+      used to modify or add to the build requirements. When the wheels are
+      built the structure will consist of the repository directory indexing,
+      a links directory, and an option "release" directory.
+    * Store the git sources from the report into a specific location. If the
+      source code already exists within the targeted location the git repo will
+      be updated with any changes that may have been made upstream.
+    * Create a static html index for all files within a directory. Because
+      this is a recursive function, each index will be created within the
+      directory and only reference files within that directory.
+"""
+
+import os
+import tempfile
+
+from cloudlib import arguments
+from cloudlib import indicator
+from cloudlib import logger
+
+import yaprt
+
+
+def _importer(module, method):
+    """Returns a method from an imported module.
+
+    :param module: String name of the module to import. This should be in
+                   dotted notation.
+    :type module: ``str``
+    :param method: Name of the method to return.
+    :type method: ``str``
+    :returns: ``object``
+    """
+    module_object = getattr(
+        __import__(module, fromlist=list()),
+        module.split('.')[-1]
+    )
+    return getattr(module_object, method)
+
+
+def _arguments():
+    """Return CLI arguments."""
+    return arguments.ArgumentParserator(
+        arguments_dict=yaprt.ARGUMENTS_DICT,
+        epilog='Licensed Apache2',
+        title='Python package builder Licensed "Apache 2.0"',
+        detail='Openstack package builder',
+        description='Openstack, Python wheel builder',
+        env_name='OS_REPO'
+    ).arg_parser()
+
+
+def preload_for_main():
+    """Return arguments dict, sinner bol, and logging object."""
+    args = _arguments()
+
+    # set boolean objects for spinner and stream logging.
+    if args['debug'] is True:
+        run_spinner = False
+        stream_logs = True
+    elif args['quiet'] is True:
+        run_spinner = False
+        stream_logs = False
+    else:
+        run_spinner = True
+        stream_logs = False
+
+    if 'git_repo_path' not in args or not args['git_repo_path']:
+        args['git_repo_path'] = os.path.join(
+            tempfile.gettempdir(), 'yaprt_temp_git_dir'
+        )
+        try:
+            os.makedirs(args['git_repo_path'])
+        except OSError:
+            pass
+
+    link_dir = args.get('link_dir')
+    if link_dir:
+        args['link_dir'] = os.path.abspath(os.path.expanduser(link_dir))
+
+    # Load the logging.
+    _logging = logger.LogSetup(debug_logging=args['debug'])
+    log = _logging.default_logger(
+        name='repo_builder',
+        enable_stream=stream_logs
+    )
+
+    return args, run_spinner, log
+
+
+def _ensure_repos(args, organize_data):
+    """Ensure that all of the repositories that are needed are local.
+
+    :type args: ``dict``
+    :param args: User defined arguments.
+    :type organize_data: ``dict``
+    :param organize_data: Built data from all processed repos and packages
+    """
+    repos = [
+        (i.get('git_url'), i.get('branch')) for i in organize_data.values()
+        if i.get('git_url')
+    ]
+    _importer('yaprt.clone_repos', 'store_repos')(args=args, repo_list=repos)
+
+
+def runner(args, module, method, process_data=False, args_only=False):
+    """Import a module and run a method.
+
+    :type args: ``dict``
+    :param args: User defined arguments.
+    :param module: full module to import
+    :type module: ``str``
+    :param method: method name to run
+    :type method: ``str``
+    :param process_data: Enable data processing from arguments or a report
+    :type process_data: ``bol``
+    :param args_only: force method args to be user_args only
+    :type args_only: ``bol``
+    """
+    function_args = [args]
+    if process_data:
+        organize_data = _importer('yaprt.data_process', 'organize_data')(
+            args=args
+        )
+        function_args.append(organize_data)
+        _ensure_repos(*function_args)
+
+    if module and method:
+        action = _importer(module, method)
+        if args_only:
+            function_args = [args]
+
+        action(*function_args)
+
+
+def main():
+    """Run the main application."""
+    args, run_spinner, log = preload_for_main()
+    with indicator.Spinner(run=run_spinner):
+
+        if args['parsed_command'] == 'create-report':
+            function_args = [
+                'yaprt.packaging_report',
+                'create_report',
+                True
+            ]
+        elif args['parsed_command'] == 'build-wheels':
+            function_args = [
+                'yaprt.wheel_builder',
+                'build_wheels',
+                True,
+                True
+            ]
+        elif args['parsed_command'] == 'create-html-indexes':
+            function_args = [
+                'yaprt.html_indexer',
+                'create_html_indexes',
+                False
+            ]
+        elif args['parsed_command'] == 'store-repos':
+            function_args = [None, None, True]
+        else:
+            # This is imported here because its not used unless there is an
+            # error. If imported above, this caused a double log entry.
+            from yaprt import utils
+            raise utils.AError(
+                'No known parsed command, Current Args: "%s"', args
+            )
+
+        runner(args, *function_args)
+
+if __name__ == '__main__':
+    main()
---
+++ packages/yaprt/yaprt-0.4.3.tar.gz/yaprt-0.4.3/yaprt/packaging_report.py
@@ -0,0 +1,266 @@
+# Copyright 2014, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# (c) 2015, Kevin Carter <kevin.carter@rackspace.com>
+
+import json
+import os
+import urlparse
+
+from cloudlib import logger
+
+import yaprt
+from yaprt import utils
+
+
+LOG = logger.getLogger('repo_builder')
+
+
+def _create_report(args, organize_data):
+    """Return a package building report.
+
+    :param args: Parsed arguments in dictionary format.
+    :type args: ``dict``
+    :param organize_data: Built data from all processed repos and packages
+    :type organize_data: ``dict``
+    :return: ``dict``
+    """
+    repo_data = dict()
+    grp = GitRepoProcess(user_args=args)
+    if '__user__' in organize_data and organize_data['__user__']:
+        # Remove any user defined packages from the organized data
+        packages = organize_data.pop('__user__')
+        requirements = packages['requirements']
+        grp.process_packages(packages=requirements)
+
+    for git_repo in organize_data.values():
+        LOG.info('Git repo: %s', git_repo)
+        grp.process_repo(repo=git_repo)
+
+    repo_data.update(grp.requirements)
+    return repo_data
+
+
+def create_report(args, organize_data):
+    """Create a package building report.
+
+    :param args: Parsed arguments in dictionary format.
+    :type args: ``dict``
+    """
+    repos = _create_report(args=args, organize_data=organize_data)
+    report_file = utils.get_abs_path(file_name=args['report_file'])
+    built_report = json.dumps(repos, indent=4, sort_keys=True)
+    LOG.info('Built report: %s', built_report)
+    with open(report_file, 'w') as f:
+        f.write(built_report)
+
+
+class GitRepoProcess(utils.RepoBaseClass):
+    def __init__(self, user_args):
+        """Process github repos for requirements.
+
+        :param user_args: User defined arguments.
+        :type user_args: ``dict``
+        """
+        super(GitRepoProcess, self).__init__(
+            user_args=user_args,
+            log_object=LOG
+        )
+
+        self.requirements = dict()
+        self.pip_install = 'git+%s@%s'
+
+    def _process_sub_plugin(self, requirement, repo_data):
+        """process the entry like a subdirectory package.
+
+        :param requirement: Name of the requirement
+        :type requirement: ``str``
+        :param repo_data: Repository data
+        :type repo_data: ``dict``
+        """
+        requirement_item = requirement.split('-e', 1)[-1].strip()
+        try:
+            requirement_url = urlparse.urlparse(requirement_item)
+            assert all([requirement_url.scheme, requirement_url.netloc])
+            assert requirement_url.scheme in ['https', 'http', 'git']
+        except AssertionError:
+            item_name = os.path.basename(requirement_item)
+            item_req = self.pip_install % (
+                repo_data['git_url'],
+                repo_data['branch']
+            )
+            repo = '%s#egg=%s&subdirectory=%s' % (
+                item_req, item_name, requirement_item
+            )
+        else:
+            repo = requirement_url
+
+        self.process_repo(repo=self.define_new_repo(repo=repo))
+
+    def define_new_repo(self, repo):
+        """From a repo entry return a dict object with its data.
+
+        :param repo: repository string
+        :type repo: ``str``
+        :returns: ``dict``
+        """
+        name, branch, plugin_path, url, o_data = utils.git_pip_link_parse(repo)
+        if plugin_path:
+            name = '%s_plugin_pkg_%s' % (
+                name.split('.git')[0],
+                os.path.basename(plugin_path)
+            )
+        # Process the new requirement item
+        return {
+            'name': name,
+            'branch': branch,
+            'plugin_path': plugin_path,
+            'git_url': url,
+            'original_data': o_data
+        }
+
+    def _process_repo_requirements(self, repo_data, base_report_data):
+        """Parse and populate requirements from within branches.
+
+        This method will populate the dictionary items that are within the
+        ``base_branches``. While there is nothing being returned within this
+        method, the modifications made to the base branches will be available
+        to the calling method.
+
+        :param repo_data: Repository data
+        :type repo_data: ``dict``
+        :param base_report_data: Dictionary items of branches that will be
+                                 populated with information parsed within this
+                                 method.
+        :type base_report_data: ``dict``
+        """
+        name = utils.git_pip_link_parse(repo=repo_data['original_data'])[0]
+        repo_path = os.path.join(self.args['git_repo_path'], name)
+        if repo_data['plugin_path']:
+            repo_path = os.path.join(repo_path, repo_data['plugin_path'])
+
+        with utils.ChangeDir(repo_path):
+            LOG.debug(
+                'Discovered branch "%s" for repo "%s"',
+                repo_data['branch'],
+                repo_data['name']
+            )
+
+            git_branches, int_branch = self.split_git_branches(
+                git_branch=repo_data['branch']
+            )
+            patched_from = None
+            if len(git_branches) > 1 or 'refs/changes' in repo_data['branch']:
+                repo_data['branch'] = int_branch
+                patched_from = True
+
+            self._run_command(command=['git', 'checkout', repo_data['branch']])
+            branch_data = base_report_data[repo_data['branch']] = dict()
+            # Record the items that make up a patched branch
+            if patched_from:
+                branch_data['patched_from'] = git_branches
+            branch_reqs = branch_data['requirements'] = dict()
+
+            if 'yaprtignorerequirements=true' in repo_data['original_data']:
+                requirement_files = list()
+            else:
+                requirement_files = yaprt.REQUIREMENTS_FILE_TYPES
+
+            for type_name, file_name in requirement_files:
+                file_path = os.path.join(repo_path, file_name)
+                if os.path.isfile(file_path):
+                    repo_data['file'] = file_name
+                    with open(file_path, 'r') as f:
+                        _file_requirements = f.readlines()
+
+                    # If the requirement file has a -e item within it treat
+                    #  it like a local subdirectory plugin and process it.
+                    _requirements = list()
+                    for item in _file_requirements:
+                        requirement = item.split('#')[0].strip()
+                        if requirement.startswith('-e'):
+                            if requirement.endswith('.'):  # skip if "-e ."
+                                continue
+                            elif 'git+' in item:
+                                repo_str = item.split('-e')[-1].strip()
+                                self.process_repo(
+                                    repo=self.define_new_repo(
+                                        repo=repo_str
+                                    )
+                                )
+                            else:
+                                self._process_sub_plugin(
+                                    requirement=requirement,
+                                    repo_data=repo_data
+                                )
+                        else:
+                            _requirements.append(requirement)
+
+                    _requirements = [
+                        i.split('#')[0].strip() for i in _requirements
+                        if not i.startswith('#')
+                        if i.strip()
+                    ]
+
+                    LOG.debug('Found requirements: %s', _requirements)
+                    if _requirements:
+                        branch_reqs[type_name] = sorted(_requirements)
+
+            setup_file_path = os.path.join(repo_path, 'setup.py')
+            if os.path.isfile(setup_file_path):
+                branch_data['pip_install_url'] = repo_data['original_data']
+
+    def _process_repo(self, repo):
+        """Parse a given repo and populate the requirements dictionary.
+
+        :param repo: Dictionary object containing git repo data.
+        :type repo: ``dict``
+        """
+        _repo = self.requirements[repo['name']] = dict()
+        _repo['git_url'] = repo['git_url']
+        report_data = _repo['branches'] = dict()
+
+        report_data['original_data'] = repo['original_data']
+        if not repo['git_url'].endswith('/'):
+            repo['git_url'] = '%s/' % repo['git_url']
+
+        LOG.info('Processing repo for [ %s ]', repo['name'])
+        self._process_repo_requirements(
+            repo_data=repo.copy(),
+            base_report_data=report_data
+        )
+
+    def process_packages(self, packages):
+        """If packages were defined  by the user add them to the requirements.
+
+        These "packages" will be added within the the requirements as a
+        special item which should eliminate collisions.
+
+        :param packages: list of packages that were user defined.
+        :type packages: ``list``
+        :return:
+        """
+        pkgs = self.requirements['_requirements_'] = dict()
+        branches = pkgs['branches'] = dict()
+        _master = branches['_master_'] = dict()
+        _requirements = _master['requirements'] = dict()
+        _requirements['base_requirements'] = packages
+
+    def process_repo(self, repo):
+        """Process a given repository.
+
+        :param repo: Dictionary object containing git repo data.
+        :type repo: ``dict``
+        """
+        self._process_repo(repo=repo)
