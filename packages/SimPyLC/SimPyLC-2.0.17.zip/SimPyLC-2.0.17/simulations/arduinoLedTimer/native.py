aa3fcf22776e64ec8fcc7cd7ab07b8de743de7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:43 +0000

    Add package SimPyLC/2.0.17/SimPyLC-2.0.17.zip

---
+++ packages/SimPyLC/SimPyLC-2.0.17.zip/SimPyLC-2.0.17/simulations/arduinoLedTimer/native.py
@@ -0,0 +1,43 @@
+# ====== Legal notices
+#
+# Copyright (C) 2013 GEATEC engineering
+#
+# This program is free software.
+# You can use, redistribute and/or modify it, but only under the terms stated in the QQuickLicence.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY, without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the QQuickLicence for details.
+#
+# The QQuickLicense can be accessed at: http://www.geatec.com/qqLicence.html
+#
+# __________________________________________________________________________
+#
+#
+#  THIS PROGRAM IS FUNDAMENTALLY UNSUITABLE FOR CONTROLLING REAL SYSTEMS !!
+#
+# __________________________________________________________________________
+#
+# It is meant for training purposes only.
+#
+# Removing this header ends your licence.
+#
+
+from SimPyLC import *
+
+class Native (Code):
+	def __init__ (self):
+		Code.__init__ (self)
+		
+	def get (self):
+		return '''
+void setup () {
+	pinMode  (13, OUTPUT);
+}
+
+void loop () {
+	cycle ();
+	digitalWrite  (13, led);  
+}		
+		''' 
