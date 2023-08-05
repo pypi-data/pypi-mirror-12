551bf59f93fa16abea32c0ac7ccab2c46bf5197c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:51 +0000

    Add package pyDPMP/0.1/pyDPMP-0.1.tar.gz

---
+++ packages/pyDPMP/pyDPMP-0.1.tar.gz/pyDPMP-0.1/pyDPMP/util.py
@@ -0,0 +1,16 @@
+import numpy as np
+import random
+
+from functools import wraps
+from nose.tools import make_decorator
+
+def set_seed(seed=0):
+  np.random.seed(seed)
+  random.seed(seed)
+
+def seeded(f, seed=0):
+  @wraps(f)
+  def f_seeded(*args, **kwargs):
+    set_seed(seed)
+    return f(*args, **kwargs)
+  return f_seeded
