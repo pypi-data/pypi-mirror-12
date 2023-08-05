281fee6c373792b72b451db7d5fd0843c5825f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:31 +0000

    Add package KlarnaCheckout/3.0.1/KlarnaCheckout-3.0.1.tar.gz

---
+++ packages/KlarnaCheckout/KlarnaCheckout-3.0.1.tar.gz/KlarnaCheckout-3.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='KlarnaCheckout',
+    version='3.0.1',
+    packages=['klarnacheckout'],
+    license='Apache 2',
+    description='Klarna Checkout library',
+    long_description=open('README.md').read(),
+    author='Merchant Integration',
+    author_email='support@klarna.com',
+    url='https://developers.klarna.com'
+)
