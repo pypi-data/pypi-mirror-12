c6ff7b9161cda60487696d75f79875e57e1426a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:32 +0000

    Add package huffman_compression/0.1/huffman_compression-0.1.tar.gz

---
+++ packages/huffman_compression/huffman_compression-0.1.tar.gz/huffman_compression-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'huffman_compression',
+  packages = ['huffman_compression'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Lossless compression of text data',
+  author = 'Asish Panda',
+  author_email = 'asishrocks95@gmail.com',
+  url = 'https://github.com/kaichogami/huffman_compression', 
+  download_url = 'https://github.com/kaichogami/huffman_compression/tarball/0.1',
+  keywords = ['compression', 'binary tree', 'lossless'], 
+  classifiers = [],
+)
