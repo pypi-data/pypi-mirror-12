9b9971e3b623dc5bb338ed12d9b61565b84ae61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:42 +0000

    Add package justprint/1.0.0/justprint-1.0.0.tar.gz

---
+++ packages/justprint/justprint-1.0.0.tar.gz/justprint-1.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from distutils.core import setup
+
+
+setup(
+        name        = 'justprint',
+        version     = '1.0.0',
+        py_module   = ['justprint'],
+        author      = 'guog',
+        author_email= 'mykulky@foxmail.com',
+        description = 'A simple printer of lists'
+        )
