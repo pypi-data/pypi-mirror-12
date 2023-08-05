8b99e9ab2b81cb038583eb9ad02f5e8aa4ca752a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:29 +0000

    Add package django-dbbackup/2.3.1/django-dbbackup-2.3.1.tar.gz

---
+++ packages/django-dbbackup/django-dbbackup-2.3.1.tar.gz/django-dbbackup-2.3.1/setup.py
@@ -0,0 +1,43 @@
+import dbbackup
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+
+def get_requirements():
+    return open('requirements.txt').read().splitlines()
+
+
+def get_test_requirements():
+    return open('requirements-tests.txt').read().splitlines()
+
+
+setup(
+    name='django-dbbackup',
+    version=dbbackup.__version__,
+    description=dbbackup.__doc__,
+    author=dbbackup.__author__,
+    author_email=dbbackup.__email__,
+    install_requires=get_requirements(),
+    tests_require=get_test_requirements(),
+    license='BSD',
+    url='https://github.com/django-dbbackup/django-dbbackup',
+    keywords=['django', 'dropbox', 'database', 'backup', 'amazon', 's3'],
+    packages=find_packages(exclude=['tests.runtests.main']),
+    test_suite='tests.runtests.main',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
