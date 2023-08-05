570ef6fa575cb91ed5bfe8829aac9959d9ddfc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:35 +0000

    Add package fastlz/0.0.1/fastlz-0.0.1.tar.gz

---
+++ packages/fastlz/fastlz-0.0.1.tar.gz/fastlz-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup, Extension
+
+
+setup(
+    name='fastlz',
+    version='0.0.1',
+    description='Python wrapper for FastLZ, a lightning-fast lossless'
+                'compression library.',
+    author='Jared Suttles',
+    url='https://github.com/jaredsuttles/python-fastlz',
+    license='BSD License',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: C',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Archiving :: Compression',
+        'Topic :: Utilities'
+    ],
+    ext_modules = [
+        Extension(
+            'fastlz',
+            sources=['fastlz.c', 'fastlz/fastlz.c'],
+            include_dirs=['fastlz']
+        )
+    ]
+)
