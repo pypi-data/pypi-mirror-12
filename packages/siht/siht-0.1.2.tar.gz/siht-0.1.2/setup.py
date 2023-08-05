c231e0053649a7c9463472a4b6bab49c5ad55584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:12 +0000

    Add package siht/0.1.2/siht-0.1.2.tar.gz

---
+++ packages/siht/siht-0.1.2.tar.gz/siht-0.1.2/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding=utf-8
+b'This library requires pypy or Python 2.6, 2.7, 3.3, pypy or newer'
+import io
+import os
+from setuptools import setup, find_packages
+
+
+def get_path(*args):
+    return os.path.join(os.path.dirname(__file__), *args)
+
+
+def read_from(filepath):
+    with io.open(filepath, 'rt', encoding='utf8') as f:
+        return f.read()
+
+
+readme = read_from(get_path('README.rst'))
+
+
+setup(
+    name='siht',
+    version='0.1.2',
+    author='Juan-Pablo Scaletti',
+    author_email='juanpablo@lucumalabs.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    url='https://github.com/jpscaletti/siht',
+    license='MIT license',
+    description='An anti-explicit manifesto',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
---
+++ packages/siht/siht-0.1.2.tar.gz/siht-0.1.2/siht/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+    siht.py
+"""
+from __future__ import print_function
+
+
+print("""The Zen of Web Development, by me
+
+Sensible defaults are better than explicit.
+""")
