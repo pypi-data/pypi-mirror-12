92799634a2c0eaff4d3676461e0769266007fcc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:20 +0000

    Add package nester_luckzpz/1.5.0/nester_luckzpz-1.5.0.zip

---
+++ packages/nester_luckzpz/nester_luckzpz-1.5.0.zip/nester_luckzpz-1.5.0/setup.py
@@ -0,0 +1,9 @@
+﻿from distutils.core import setup
+setup(
+    name = 'nester_luckzpz',
+    version = '1.5.0',
+    py_modules = ['nester_luckzpz'],
+    author = 'luckzpz',
+    author_email = 'luckzpz@seu.edu.cn',
+    description = 'A simple print tool',
+    )
