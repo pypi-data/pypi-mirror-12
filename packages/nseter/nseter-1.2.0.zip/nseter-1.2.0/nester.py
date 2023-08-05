1ceb5640dc2de35f59718c0827ce441cb859d386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:59 +0000

    Add package nseter/1.2.0/nseter-1.2.0.zip

---
+++ packages/nseter/nseter-1.2.0.zip/nseter-1.2.0/nester.py
@@ -0,0 +1,12 @@
+﻿__author__ = 'Administrator'
+#coding=utf-8
+"""这是‘nester.py'模块，提供了一个名为print_lol的函数，这个函数的作用是打印列表，其中有可能包含（也可能不包含）嵌套列表"""
+def print_lol(the_list,level=0):
+    """这个函数去一个位置参数，名为"the_list",这可以是任何python列表，列表中的每一项数据被递归的输出到屏幕上，各数据项各占一行"""
+    for each_movies in the_list:
+        if isinstance(each_movies,list):
+            print_lol(each_movies,level+1)
+        else:
+            for tab_level in range(level):
+                print("\t",end='')
+            print(each_movies)
