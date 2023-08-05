905f3baa28ca99892fc37b3039627f2ac1d0b622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:27 +0000

    Add package jitter/0.1.13/jitter-0.1.13.tar.gz

---
+++ packages/jitter/jitter-0.1.13.tar.gz/jitter-0.1.13/setup.py
@@ -0,0 +1,32 @@
+VERSION = "0.1.13"
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'JITT Command Line Tool',
+    'author': 'Adam Kariv',
+    'url': 'http://jitt.it',
+    'download_url': 'https://github.com/EverythingMe/jitter/tarball/'+VERSION,
+    'author_email': 'adam@everything.me',
+    'version': VERSION,
+    'install_requires': [
+        'click',
+        'nose',
+        'lxml',
+    ],
+    'package_data':{
+        'jitter':['android/*json']
+    },
+    'packages': ['jitter', 'jitter.android'],
+    'scripts': [],
+    'name': 'jitter',
+    'entry_points':{
+        'console_scripts': [
+            'jitter = jitter.cli:entrypoint',
+        ]
+    },
+}
+
+setup(**config)
