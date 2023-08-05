63333b59b835e4974f012c5dec9015a4bc7f58c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:48 +0000

    Add package handroll/2.1/handroll-2.1-py2.py3-none-any.whl

---
+++ packages/handroll/handroll-2.1-py2.py3-none-any.whl/handroll/scaffolder.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2015, Matt Layman
+
+import os
+import shutil
+
+from handroll.exceptions import AbortError
+from handroll.i18n import _
+
+BUILTIN_SCAFFOLDS = {
+    'default': _('A complete site to get you going'),
+}
+LIST_SCAFFOLDS = 1
+SCAFFOLDS_PATH = os.path.join(os.path.dirname(__file__), 'scaffolds')
+
+
+def make(scaffold, site):
+    """Use the given scaffold to make a site or list what is available."""
+    if scaffold == LIST_SCAFFOLDS:
+        list_scaffolds()
+    else:
+        make_scaffold(scaffold, site)
+
+
+def list_scaffolds():
+    """List out all available scaffolds."""
+    print(_('Available scaffolds:\n'))
+    for scaffold in sorted(BUILTIN_SCAFFOLDS.keys()):
+        print(display_scaffold(scaffold, get_label(scaffold)))
+    print('')
+
+
+def display_scaffold(scaffold, label):
+    """Display a scaffold in a consistent way for listing."""
+    return '  {0: <12}| {1}'.format(scaffold, label)
+
+
+def get_label(scaffold):
+    """Get the label for the scaffold."""
+    return BUILTIN_SCAFFOLDS[scaffold]
+
+
+def make_scaffold(scaffold, site):
+    """Make a site from the scaffold."""
+    if scaffold not in BUILTIN_SCAFFOLDS:
+        raise AbortError(_('There is no {scaffold} scaffold.'.format(
+            scaffold=scaffold)))
+    if os.path.exists(site):
+        raise AbortError(_('{site} already exists.'.format(site=site)))
+    os.makedirs(site)
+    scaffold_path = os.path.join(SCAFFOLDS_PATH, scaffold)
+    shutil.copytree(scaffold_path, os.path.join(site, 'source'))
