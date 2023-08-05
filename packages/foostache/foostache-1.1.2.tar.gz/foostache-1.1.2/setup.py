5ace9cc01c1636568b82278cad09581ddaca33af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package foostache/1.1.2/foostache-1.1.2.tar.gz

---
+++ packages/foostache/foostache-1.1.2.tar.gz/foostache-1.1.2/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(*paths):
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+
+setup(
+    name='foostache',
+    version='1.1.2',
+    description='Implementation of foostache template language',
+    long_description=read('README.rst'),
+    url='https://github.com/ldgabbay/foostache-python/',
+    author='Lynn Gabbay',
+    author_email='gabbay@gmail.com',
+    license='MIT',
+    classifiers = [
+#        'Development Status :: 3 - Alpha',
+#        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries'
+    ],
+    packages=find_packages(),
+    scripts=['bin/foostache'],
+    keywords='foostache mustache',
+    install_requires=[
+        "antlr4-python2-runtime>=4.5,<4.6"
+    ]
+)
