e615bb20ee860e52d6a7965fd7776c943749f0bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package LogDefer/0.1.1/LogDefer-0.1.1.tar.gz

---
+++ packages/LogDefer/LogDefer-0.1.1.tar.gz/LogDefer-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import LogDefer
+
+setup(
+    name='LogDefer',
+    version=LogDefer.__version__,
+    description='Structured Logging for Python',
+    classifiers=[],
+    keywords='structured logging log logdefer',
+    author='Michael Pucyk',
+    author_email='michael.pucyk@gmail.com',
+    url='https://github.com/mikep/LogDefer',
+    py_modules=['LogDefer'],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={},
+    zip_safe=True,
+)
+
