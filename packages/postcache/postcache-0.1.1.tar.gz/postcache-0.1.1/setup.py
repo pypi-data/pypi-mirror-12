d69d475dd8c9754e3fca60cbeb2d20f8a6d95b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:47 +0000

    Add package postcache/0.1.1/postcache-0.1.1.tar.gz

---
+++ packages/postcache/postcache-0.1.1.tar.gz/postcache-0.1.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from setuptools import setup
+
+setup(
+  name = "postcache",
+  version = "0.1.1",
+  description = """A Python library to cache data that is being 
+POSTed to a remote server in the JSON format (such as periodic log items 
+or measurements.""",
+  packages = ["postcache"],
+  author='Peter Den Hartog',
+  author_email='pddenhar@gmail.com',
+  install_requires=["requests>=2.4.3"],
+  url='https://github.com/pddenhar/python-post-cache')
