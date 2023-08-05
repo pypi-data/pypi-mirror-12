c62e42f8796973db10e67fc39667d75f1b85bf82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:33 +0000

    Add package cheat/2.1.17/cheat-2.1.17.tar.gz

---
+++ packages/cheat/cheat-2.1.17.tar.gz/cheat-2.1.17/setup.py
@@ -0,0 +1,34 @@
+"""cheat
+~~~~~~~~
+cheat allows you to create and view interactive cheatsheets on the
+command-line. It was designed to help remind *nix system administrators of
+options for commands that they use frequently, but not frequently enough
+to remember.
+:license: GPL3
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'cheat',
+    version          = '2.1.17',
+    author           = 'Chris Lane',
+    author_email     = 'chris@chris-allen-lane.com',
+    license          = 'GPL3',
+    description      = 'cheat allows you to create and view interactive cheatsheets on the command-line',
+    long_description = __doc__,
+    url              = 'https://github.com/chrisallenlane/cheat',
+    packages         = find_packages(),
+    package_data     = {
+        'cheat.cheatsheets': ['*'],
+    },
+    entry_points = {
+        'console_scripts': [
+            'cheat = cheat.app:main',
+        ],
+    },
+    install_requires = [
+        'docopt >= 0.6.1',
+        'pygments >= 1.6.0',
+    ]
+)
