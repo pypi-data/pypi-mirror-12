8a2d8c39f1c5978c959b52842a02f76e10421633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:32 +0000

    Add package nestermanu/1.0.0/nestermanu-1.0.0.tar.gz

---
+++ packages/nestermanu/nestermanu-1.0.0.tar.gz/nestermanu-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='nestermanu',
+      version='1.0.0',
+      py_modules = ['nestermanu'],
+      description='Python Distribution Utilities',
+      author='Manu Mathew',
+      author_email='manu.mathew@aspiresys.com',
+      url='https://www.python.oorg',
+     )
