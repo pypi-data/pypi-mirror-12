18f8f5951454c836cd67c3005ae53e12945f5e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:42 +0000

    Add package enerdata/0.8.1/enerdata-0.8.1.tar.gz

---
+++ packages/enerdata/enerdata-0.8.1.tar.gz/enerdata-0.8.1/enerdata/profiles/__init__.py
@@ -0,0 +1,11 @@
+from collections import Counter
+from decimal import Decimal
+
+
+class Dragger(Counter):
+
+    def drag(self, number, key='default'):
+        number = Decimal(str(number)) + self[key]
+        aprox = int(round(number))
+        self[key] = number - aprox
+        return aprox
---
+++ packages/enerdata/enerdata-0.8.1.tar.gz/enerdata-0.8.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='enerdata',
+    version='0.8.1',
+    packages=find_packages(),
+    url='http://code.gisce.net',
+    license='MIT',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    install_requires=[
+        'pytz'
+    ],
+    description=''
+)
