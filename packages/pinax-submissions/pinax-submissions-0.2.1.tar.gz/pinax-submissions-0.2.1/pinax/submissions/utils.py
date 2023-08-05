718c7f974c5861f396f074a3e46cefc0fbfa26dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:27 +0000

    Add package pinax-submissions/0.2.1/pinax-submissions-0.2.1.tar.gz

---
+++ packages/pinax-submissions/pinax-submissions-0.2.1.tar.gz/pinax-submissions-0.2.1/pinax/submissions/utils.py
@@ -0,0 +1,8 @@
+import os
+import uuid
+
+
+def uuid_filename(instance, filename):
+    ext = filename.split(".")[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join("document", filename)
