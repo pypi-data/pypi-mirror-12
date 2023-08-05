263a8224db84432ddf3834404fbc8a4f2b53d274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:14 +0000

    Add package djdt-flamegraph/0.2.4/djdt_flamegraph-0.2.4.tar.gz

---
+++ packages/djdt-flamegraph/djdt_flamegraph-0.2.4.tar.gz/djdt_flamegraph-0.2.4/tests/test_djdt_flamegraph.py
@@ -0,0 +1,23 @@
+"""
+test_djdt_flamegraph
+----------------------------------
+
+Tests for `djdt_flamegraph` module.
+"""
+
+import unittest
+
+from djdt_flamegraph import djdt_flamegraph
+from djdt_flamegraph import FlamegraphPanel
+
+
+class TestDjdtFlamegraph(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
