95288462838afe7a478cd411a22aae1cd14df809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:52 +0000

    Add package cyvcf2/0.2.0/cyvcf2-0.2.0.tar.gz

---
+++ packages/cyvcf2/cyvcf2-0.2.0.tar.gz/cyvcf2-0.2.0/cyvcf2/helpers.c
@@ -0,0 +1,102 @@
+#include <htslib/vcf.h>
+
+int as_gts(int *gts, int num_samples, int ploidy) {
+    int j = 0, i, k;
+	int found = 0;
+    for (i = 0; i < ploidy * num_samples; i += ploidy){
+		found = 0;
+		for (k = 0; k < ploidy; k++) {
+			if (!bcf_gt_is_missing(gts[i+k])) {
+				found = 1;
+				break;
+			}
+        }
+		if (found == 0) {
+			gts[j++] = 2; // unknown
+			continue;
+		}
+
+		if(ploidy == 1) {
+			int a = bcf_gt_allele(gts[i]);
+			if (a == 0) {
+			   	gts[j++] = 0;
+			} else if (a == 1) {
+				gts[j++] = 3;
+			} else {
+				gts[j++] = 2;
+			}
+			continue;
+		}
+
+        int a = bcf_gt_allele(gts[i]);
+        int b = bcf_gt_allele(gts[i+1]);
+
+        if((a == 0) && (b == 0)) {
+            gts[j] = 0; //  HOM_REF
+        }
+        else if((a == 1) && (b == 1)) {
+            gts[j] = 3; //  HOM_ALT
+        }
+        else if((a  != b)) {
+            gts[j] = 1; //  HET
+        } else {
+            gts[j] = 2; // unknown
+        }
+        j++;
+    }
+    // free the latter part of the array that we don't need.
+    //free((void *)(&(gts[j])));
+    return j;
+}
+
+int as_gts012(int *gts, int num_samples, int ploidy) {
+    int j = 0, i, k, found;
+    for (i = 0; i < ploidy * num_samples; i += ploidy){
+		found = 0;
+		for (k = 0; k < ploidy; k++) {
+			if (!bcf_gt_is_missing(gts[i+k])) {
+				found = 1;
+				break;
+			}
+        }
+		if (found == 0) {
+			gts[j++] = 3; // unknown
+			continue;
+		}
+
+		if(ploidy == 1) {
+			int a = bcf_gt_allele(gts[i]);
+			if (a == 0) {
+			   	gts[j++] = 0;
+			} else if (a == 1) {
+				gts[j++] = 2;
+			} else {
+				gts[j++] = 3; // unknown
+			}
+			continue;
+		}
+
+        if (bcf_gt_is_missing(gts[i]) && bcf_gt_is_missing(gts[i+1])){
+            gts[j++] = 3; // unknown
+            continue;
+        }
+        int a = bcf_gt_allele(gts[i]);
+        int b = bcf_gt_allele(gts[i+1]);
+
+        if((a == 0) && (b == 0)) {
+            gts[j] = 0; //  HOM_REF
+        }
+        else if((a == 1) && (b == 1)) {
+            gts[j] = 2; //  HOM_ALT
+        }
+        else if((a  != b)) {
+            gts[j] = 1; //  HET
+        } else {
+            gts[j] = 3; // unknown
+        }
+        j++;
+    }
+    // free the latter part of the array that we don't need.
+    //free((void *)(&(gts[j])));
+    return j;
+}
