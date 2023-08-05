73603937f1f634226aece177830c901f003ef976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:40 +0000

    Add package inkscape-openscad-dxf/0.0.3/inkscape-openscad-dxf-0.0.3.tar.gz

---
+++ packages/inkscape-openscad-dxf/inkscape-openscad-dxf-0.0.3.tar.gz/inkscape-openscad-dxf-0.0.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import multiprocessing
+import re
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='inkscape-openscad-dxf',
+    version='0.0.3',
+    description='Inkscape OpenSCAD DXF Export',
+    long_description=open('README.rst').read(),
+    author='Brad Pitcher',
+    author_email='bradpitcher@gmail.com',
+    url='https://github.com/brad/Inkscape-OpenSCAD-DXF-Export',
+    packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
+    package_data={'openscad_dxf': ['../openscad_dxf.inx']},
+    include_package_data=True,
+    install_requires=["setuptools"],
+    license='GPL',
+    test_suite='nose.collector',
+    tests_require='nose',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ),
+)
