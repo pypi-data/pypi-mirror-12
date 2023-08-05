36b9258c4b747b7c5e663b6e4ca4c1f4b9bca853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:24 +0000

    Add package singularity-logfetch/0.21.0/singularity-logfetch-0.21.0.tar.gz

---
+++ packages/singularity-logfetch/singularity-logfetch-0.21.0.tar.gz/singularity-logfetch-0.21.0/logfetch/cat.py
@@ -0,0 +1,13 @@
+import os
+import sys
+from termcolor import colored
+
+def cat_files(args, all_logs):
+    if all_logs:
+        all_logs.sort()
+        for log in all_logs:
+            sys.stderr.write(colored(log, 'cyan') + '\n')
+            command = 'cat {0}'.format(log)
+            sys.stdout.write(os.popen(command).read() + '\n')
+    else:
+        sys.stderr.write(colored('No log files found\n', 'magenta'))
