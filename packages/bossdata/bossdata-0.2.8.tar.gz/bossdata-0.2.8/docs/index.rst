52ca4f7d42feebe66690aa573f007f02640d4e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:53 +0000

    Add package bossdata/0.2.8/bossdata-0.2.8.tar.gz

---
+++ packages/bossdata/bossdata-0.2.8.tar.gz/bossdata-0.2.8/docs/index.rst
@@ -0,0 +1,29 @@
+.. bossdata documentation master file, created by
+   sphinx-quickstart on Tue Jul  9 22:26:36 2013.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+bossdata
+========
+
+A python package for working with `spectroscopic data <http://www.sdss.org/dr12/spectro/spectro_basics/>`__ from the `Sloan Digital Sky Survey <http://www.sdss.org>`__.  Bossdata is free software (MIT license) `hosted on github <https://github.com/dkirkby/bossdata>`__ and `released via pypi <https://pypi.python.org/pypi/bossdata>`__.
+
+.. toctree::
+   :maxdepth: 2
+
+   installation
+   overview
+   scripts
+   envvar
+   usage
+   examples
+   contributing
+   history
+
+Modules API Reference
+---------------------
+
+.. toctree::
+   :maxdepth: 3
+
+   src/bossdata
---
+++ packages/bossdata/bossdata-0.2.8.tar.gz/bossdata-0.2.8/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup, Command
+except ImportError:
+    from distutils.core import setup, Command
+
+# Run pre-built py.tests as described at
+# https://pytest.org/latest/goodpractises.html#integrating-with-distutils-python-setup-py-test
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+        import sys
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+with open('DESCRIPTION.rst') as f:
+    long_description = f.read()
+
+requirements = [
+    'requests>=2.7.0',
+    'progressbar>=2.3',
+    'astropy>=1.0.1',
+    'fitsio>=0.9.7',
+    'numpy>=1.9.2',
+    'pydl>=0.4.1'
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='bossdata',
+    version='0.2.8',
+    description='Tools to access SDSS spectroscopic data.',
+    long_description=long_description,
+    author='bossdata developers',
+    author_email='dkirkby@uci.edu',
+    url='https://github.com/dkirkby/bossdata',
+    packages=[
+        'bossdata',
+    ],
+    package_dir={'bossdata':
+                 'bossdata'},
+    scripts = [
+        'bin/bossfetch',
+        'bin/bossquery',
+        'bin/bossplot',
+        'bin/bossraw',
+    ],
+    #include_package_data=True,
+    #zip_safe=False,
+    install_requires=requirements,
+    license='MIT',
+    keywords='bossdata',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    cmdclass = {'test': PyTest},
+    #test_suite='tests',
+    #tests_require=test_requirements
+)
