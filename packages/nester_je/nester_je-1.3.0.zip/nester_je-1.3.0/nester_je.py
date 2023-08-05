bb975f7c15543e1c4e1230aa0268a3d7022a6063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:03 +0000

    Add package nester_je/1.3.0/nester_je-1.3.0.zip

---
+++ packages/nester_je/nester_je-1.3.0.zip/nester_je-1.3.0/nester_je.py
@@ -0,0 +1,31 @@
+"""Python3 practice"""
+import nester_je
+
+
+movies = ["The Holy Grail", 1975,"TerryHones & Terry Gilliam", 91,
+          ["Graham Chapman",
+           ["Michael Palin","John Cleese","Terry Gilliam",
+            "Eric Idle", "Terry Jones"]]]
+
+
+"""
+This is nester.py module, provides one function which is print_lol() function.
+"""
+
+
+def print_lol(the_list,indent=False, level=0):
+    """This function takes oen positional argument called "the list", which
+       is any Python list (of -possibly - nested lists). Each data item in
+       the provided list is ( recursively) printed to the screen on it's own
+       line."""
+
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item,indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end="")
+            print(each_item)
+        
+print_lol(movies,0)
