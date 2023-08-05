0cc04ca38482b5a763be50e0699d62a65ca35110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package charmy/0.1.2/charmy-0.1.2-py2.py3-none-any.whl

---
+++ packages/charmy/charmy-0.1.2-py2.py3-none-any.whl/charmy/__init__.py
@@ -0,0 +1,6 @@
+__title__ = 'charmy'
+__version__ = '0.1.2'
+__build__ = 0x000003
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2015 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
