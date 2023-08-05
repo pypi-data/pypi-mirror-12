728d9c72894b119abb5fbcbe4cbddae911eb3270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:18 +0000

    Add package pluginmanager/0.0.4/pluginmanager-0.0.4-py3-none-any.whl

---
+++ packages/pluginmanager/pluginmanager-0.0.4-py3-none-any.whl/pluginmanager/__init__.py
@@ -0,0 +1,8 @@
+from .iplugin import IPlugin
+from .file_manager import FileManager
+from .module_manager import ModuleManager
+from .plugin_manager import PluginManager
+from .interface import Interface
+
+__all__ = ["IPlugin", "FileManager", "ModuleManager",
+           "PluginManager", "Interface"]
