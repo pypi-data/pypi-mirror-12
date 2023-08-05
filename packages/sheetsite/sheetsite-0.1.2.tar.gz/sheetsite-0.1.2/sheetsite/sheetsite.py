7c6ad6662e2352aa7118e81cf3ffa09de554eddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:55 +0000

    Add package sheetsite/0.1.2/sheetsite-0.1.2.tar.gz

---
+++ packages/sheetsite/sheetsite-0.1.2.tar.gz/sheetsite-0.1.2/sheetsite/sheetsite.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import argparse
+import os
+import sys
+
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+from spreadsheet import Spreadsheet
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Fetch a google spreadsheet.')
+    parser.add_argument('--credential', nargs=1, required=True, help='credential json file '
+                        'from google, see http://gspread.readthedocs.org/en/latest/oauth2.html '
+                        '(and don\'t forget to share the spreadsheet with client_email)')
+    parser.add_argument('spreadsheet_key', help='key for spreadsheet (e.g. 2LvBgFe...F2qAIuBk)')
+    parser.add_argument('output_file', help='file to write to, either output.xls or output.json. '
+                        'When saving to xls, the file will not retain original formatting. '
+                        'When saving to json, the first row is assumed to contain column names.')
+
+    args = parser.parse_args()
+
+    ss = Spreadsheet()
+    ss.connect(args.credential[0])
+    ss.load_remote(args.spreadsheet_key)
+    ss.save_local(args.output_file)
