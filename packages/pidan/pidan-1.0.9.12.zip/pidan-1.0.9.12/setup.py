a33da98cdef6b28c73730ccace50a8b7008f2e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:59 +0000

    Add package pidan/1.0.9.12/pidan-1.0.9.12.zip

---
+++ packages/pidan/pidan-1.0.9.12.zip/pidan-1.0.9.12/setup.py
@@ -0,0 +1,16 @@
+# install:python setup.py sdist upload
+
+from distutils.core import setup
+ 
+setup (
+    name = 'pidan',
+    version = '1.0.9.12',
+    packages=["pidan"],
+    platforms=['any'],
+    author = 'chengang',
+    author_email = 'chengang.net@gmail.com',
+    description = 'modify html.filter_tags2 function,add special code replace',
+    package_data = {
+        '': ['*.data'],
+    },
+)
