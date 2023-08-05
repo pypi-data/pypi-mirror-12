649d3977b249996db2b0176f773e0d2825fc4b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:43 +0000

    Add package toolkit/0.0.2/toolkit-0.0.2.tar.gz

---
+++ packages/toolkit/toolkit-0.0.2.tar.gz/toolkit-0.0.2/toolkit/struct.py
@@ -0,0 +1,16 @@
+# coding=utf8
+
+
+class_template = """
+class _MetaClass(object):
+    {}
+"""
+
+
+def metaclass(keys):
+    pass
+
+
+def dict2obj(kv):
+    dictclass = dict2class(kv)
+    dictclass()
