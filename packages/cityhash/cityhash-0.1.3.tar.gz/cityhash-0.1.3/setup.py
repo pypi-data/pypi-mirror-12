c4dcb57464ed3474fcb2bb60daf20b37acc79565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:58 +0000

    Add package cityhash/0.1.3/cityhash-0.1.3.tar.gz

---
+++ packages/cityhash/cityhash-0.1.3.tar.gz/cityhash-0.1.3/setup.py
@@ -0,0 +1,93 @@
+__author__  = "Alexander [Amper] Marshalov"
+__email__   = "alone.amper+cityhash@gmail.com"
+__icq__     = "87-555-3"
+__jabber__  = "alone.amper@gmail.com"
+__twitter__ = "amper"
+__url__     = "http://amper.github.com/cityhash"
+
+from setuptools import setup, Extension
+from setuptools.dist import Distribution
+from pkg_resources import resource_string
+from Cython.Distutils import build_ext
+
+
+class BinaryDistribution(Distribution):
+    """
+    Subclass the setuptools Distribution to flip the purity flag to false.
+    See http://lucumr.pocoo.org/2014/1/27/python-on-wheels/
+    """
+    def is_pure(self):
+        # TODO: check if this is still necessary with Python v2.7
+        return False
+
+
+class build_ext_subclass(build_ext):
+    """
+    This class is an ugly hack to a problem that arises when one must force
+    a compiler to use specific flags by adding to the environment somethiing
+    like the following:
+
+        CXX="clang --some_flagA --some_flagB -I/usr/bin/include/mylibC"
+
+    (as opposed to setting CXXFLAGS). Distutils in that case will complain
+    that it cannot run the entire command as given because it is not
+    found as an executable (specific error message is: "unable to execute...
+    ... no such file or directory").
+
+    This subclass of ``build_ext`` will extract the compiler name from the
+    command line and insert any remaining arguments right after it.
+    """
+    def build_extensions(self):
+        ccm = self.compiler.compiler
+        if ' ' in ccm[0]:
+            self.compiler.compiler = ccm[0].split(' ') + ccm[1:]
+        cxx = self.compiler.compiler_cxx
+        if ' ' in cxx[0]:
+            self.compiler.compiler_cxx = cxx[0].split(' ') + cxx[1:]
+        build_ext.build_extensions(self)
+
+
+CXXFLAGS = u"""
+-O3
+-msse4.2
+-Wno-unused-value
+-Wno-unused-function
+""".split()
+
+VERSION = '0.1.3'
+URL = "https://github.com/escherba/python-cityhash"
+
+setup(
+    version=VERSION,
+    description="Python-bindings for CityHash, a fast non-cryptographic hash algorithm",
+    author="Alexander [Amper] Marshalov",
+    author_email="alone.amper+cityhash@gmail.com",
+    url=URL,
+    download_url=URL + "/tarball/master/" + VERSION,
+    name='cityhash',
+    license='MIT',
+    cmdclass={'build_ext': build_ext_subclass},
+    ext_modules=[Extension("cityhash", ["src/city.cc", "src/cityhash.pyx"],
+                           language="c++",
+                           extra_compile_args=CXXFLAGS,
+                           include_dirs=['include'])],
+    keywords=['hash', 'hashing', 'cityhash'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: C++',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+    long_description=resource_string(__name__, 'README.rst'),
+    distclass=BinaryDistribution,
+)
