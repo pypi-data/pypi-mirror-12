aecbb7f455ed5fdb11e3aaac8db757b068424fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:38 +0000

    Add package django-pdf-download/0.0.6/django-pdf-download-0.0.6.tar.gz

---
+++ packages/django-pdf-download/django-pdf-download-0.0.6.tar.gz/django-pdf-download-0.0.6/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from setuptools.command.install import install
+from setuptools import setup
+
+from distutils import log
+
+class CustomInstallCommand(install):
+    """Customized setuptools install command - prints a friendly greeting."""
+    def run(self):
+        install.run(self)
+        log.info("Print activating headless printing")
+        # from pdfdownload import activate_headless_print
+        # activate_headless_print()
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='django-pdf-download',
+    version='0.0.6',
+    author='Paul Tax',
+    author_email='paultax@gmail.com',
+    include_package_data=True,
+    install_requires=['PyVirtualDisplay==0.1.5', 'selenium==2.47.3', 'Django>=1.8.0'],
+    py_modules=['pdfdownload'],
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+    # entry_points={
+    #     'console_scripts': ['activate_headless_print=drax.commands:main'],
+    # },    
+    url='https://github.com/tax/django-pdf-download',
+    license='BSD licence, see LICENCE.txt',
+    description='',
+    long_description=open('README.md').read(),
+)
