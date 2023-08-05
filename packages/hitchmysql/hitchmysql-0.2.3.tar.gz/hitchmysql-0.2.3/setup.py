25bd64982e66b44c86c341971204d736bd007c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:35 +0000

    Add package hitchmysql/0.2.3/hitchmysql-0.2.3.tar.gz

---
+++ packages/hitchmysql/hitchmysql-0.2.3.tar.gz/hitchmysql-0.2.3/setup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*
+from setuptools.command.install import install
+from setuptools import find_packages
+from setuptools import setup
+import subprocess
+import codecs
+import sys
+import os
+
+
+class CustomInstall(install):
+    def run(self):
+        try:
+            import unixpackage
+            unixpackage.install(["libaio1", "libmysqlclient-dev", "libncurses5-dev"], polite=True)
+        except ImportError:
+            sys.stderr.write("WARNING : unixpackage unavailable; cannot check for system dependencies.")
+            sys.stderr.flush()
+        install.run(self)
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    # see here: https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    return codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts), 'r').read()
+
+setup(name="hitchmysql",
+      version="0.2.3",
+      description="Plugin to run MySQL using the Hitch testing framework.",
+      long_description=read('README.rst'),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+          'Topic :: Software Development :: Quality Assurance',
+          'Topic :: Software Development :: Testing',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Utilities',
+          'Environment :: Console',
+          'Topic :: Database',
+          'Operating System :: Unix',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      keywords='hitch testing framework bdd tdd declarative tests testing service mysql database',
+      author='Colm O\'Connor',
+      author_email='colm.oconnor.github@gmail.com',
+      url='https://hitchtest.readthedocs.org/',
+      license='AGPL',
+      install_requires=['hitchserve', 'hitchtest', ],
+      packages=find_packages(exclude=[]),
+      package_data={},
+      zip_safe=False,
+      include_package_data=True,
+      cmdclass={'install': CustomInstall},
+)
