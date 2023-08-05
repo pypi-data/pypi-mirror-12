089940b8b9f176514012762fa49444c8f28b4ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:15 +0000

    Add package ramses/0.5.0/ramses-0.5.0.tar.gz

---
+++ packages/ramses/ramses-0.5.0.tar.gz/ramses-0.5.0/setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+VERSION = open(os.path.join(here, 'VERSION')).read()
+
+requires = [
+    'cryptacular',
+    'inflection',
+    'nefertari>=0.6.0',
+    'pyramid',
+    'ramlfications==0.1.5',
+    'six',
+    'transaction',
+]
+
+setup(name='ramses',
+      version=VERSION,
+      description='Generate a RESTful API for Pyramid using RAML',
+      long_description=README,
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='Brandicted',
+      author_email='hello@brandicted.com',
+      url='https://github.com/brandicted/ramses',
+      keywords='web pyramid pylons nefertari raml',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="ramses",
+      entry_points="""\
+        [pyramid.scaffold]
+        ramses_starter = ramses.scaffolds:RamsesStarterTemplate
+      """)
