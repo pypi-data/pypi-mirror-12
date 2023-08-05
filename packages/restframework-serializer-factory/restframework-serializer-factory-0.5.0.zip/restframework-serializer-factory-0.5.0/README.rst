e08ee3abbb926880b08138d04cb3989662baffe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:33 +0000

    Add package restframework-serializer-factory/0.5.0/restframework-serializer-factory-0.5.0.zip

---
+++ packages/restframework-serializer-factory/restframework-serializer-factory-0.5.0.zip/restframework-serializer-factory-0.5.0/README.rst
@@ -0,0 +1,17 @@
+========================================
+django-rest-framework-serializer-factory
+========================================
+
+Factories for creating instances of `restframework.serializers.ModelSerializer` on demand.
+
+Install
+=======
+
+.. code-block::
+
+    $ pip install -e restframework-serializer-factory
+
+Usage
+=====
+TODO
+
---
+++ packages/restframework-serializer-factory/restframework-serializer-factory-0.5.0.zip/restframework-serializer-factory-0.5.0/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+
+
+setup(name='restframework-serializer-factory',
+      version="0.5.0",
+      packages=find_packages(),
+      requires=['djangorestframework'],
+      install_requires=['djangorestframework'],
+      tests_require=[
+          'Django>=1.5',
+          ],
+      url='https://github.com/arpheno/django-rest-framework-serializer-factory/',
+      author='Sebastian Wozny',
+      author_email='sswozny@gmail.com',
+      keywords='django serializer factory rest framework',
+      description=('Factories for creating instances of restframework.serializers.ModelSerializer on demand.',),
+      license='GPL',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Framework :: Django',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          ('License :: OSI Approved :: '
+           'GNU General Public License v3 or later (GPLv3+)'),
+          'Topic :: Software Development :: Libraries',
+          ],
+      )
