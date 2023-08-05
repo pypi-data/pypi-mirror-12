ffb4249acec7933cc8824b4534f120aad6ff3e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:40 +0000

    Add package nginxctl/1.1/nginxctl-1.1.tar.gz

---
+++ packages/nginxctl/nginxctl-1.1.tar.gz/nginxctl-1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = 'nginxctl',
+    packages = ['nginxctl'],
+    version = '1.1',
+    description = 'This tool is similar to apachectl but for nginx. The \
+                    important feature of this tool is ability to list vhosts \
+                    configured on a nginx server',
+    author='Alex Newton Alexander',
+    author_email='fooltruth1980@gmail.com',
+    url='https://github.com/fooltruth/nginxctl',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Environment :: Console',
+    ]
+)
