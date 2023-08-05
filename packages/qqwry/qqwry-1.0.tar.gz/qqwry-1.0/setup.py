7cddba94efe4db2233fce76ac106716d8c59efa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:04 +0000

    Add package qqwry/1.0/qqwry-1.0.tar.gz

---
+++ packages/qqwry/qqwry-1.0.tar.gz/qqwry-1.0/setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='qqwry',
+      version='1.0',
+      description='QQWry can get ip-related locations information from qqwry ip database.',
+      author='Nixawk',
+      author_email='hap.ddup@gmail.com',
+      py_modules=['qqwry'],
+      license='MIT',
+     )
