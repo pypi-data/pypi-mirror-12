9baa38bf08849c6cb08541b90abdb6f54d542ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:18 +0000

    Add package dronekit-sitl/2.4.1/dronekit_sitl-2.4.1.tar.gz

---
+++ packages/dronekit-sitl/dronekit_sitl-2.4.1.tar.gz/dronekit_sitl-2.4.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='dronekit_sitl',
+      version='2.4.1',
+      description='DroneKit library to run SITL (simulation environment)',
+      author='Tim Ryan',
+      author_email='tim@3drobotics.com',
+      url='https://github.com/dronekit/dronekit-sitl/',
+      install_requires = [
+        'psutil>=3.0',
+        'dronekit>=2.0.0b5',
+      ],
+      package_data={
+        'dronekit_sitl': ['*.parm'],
+        'dronekit_sitl.pysim': ['*.py'],
+      },
+      entry_points={
+          'console_scripts': [
+              'dronekit-sitl = dronekit_sitl.__init__:main'
+          ]
+      },
+      packages = ['dronekit_sitl'],
+      )
