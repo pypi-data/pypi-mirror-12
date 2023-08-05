bc207620d90b2ae19a173378097f9d9f028afc21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:19 +0000

    Add package mailjet-rest/v1.0.3/mailjet_rest-v1.0.3.tar.gz

---
+++ packages/mailjet-rest/mailjet_rest-v1.0.3.tar.gz/mailjet_rest-v1.0.3/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import sys
+import os
+import re
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+PACKAGE_NAME = 'mailjet_rest'
+VERSION = 'v1.0.3'
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author='starenka',
+    author_email='starenka0@gmail.com',
+    maintainer='Guillaume Badi',
+    maintainer_email='gbadi@mailjet.com',
+	download_url='https://github.com/mailjet/mailjet-apiv3-python/tarball/v1.0.3',
+    url='https://github.com/mailjet/mailjet-apiv3-python',
+    description=('Mailjet V3 API wrapper'),
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Environment :: Console',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: GNU General Public License (GPL)',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.2',
+                 'Programming Language :: Python :: 3.5',
+                 'Topic :: Utilities'],
+    license='GPLv3',
+    keywords='mailjet api wrapper email client',
+	packages=find_packages(),
+    include_package_data=True,
+    install_requires=['requests>=2.4.3'],
+    tests_require=['unittest'],
+    entry_points={},
+)
