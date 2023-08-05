b3a95c331806e30ff1b3b97f416d6c23fd81ce0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:32 +0000

    Add package yask123/1/yask123-1.tar.gz

---
+++ packages/yask123/yask123-1.tar.gz/yask123-1/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name="yask123",
+	version="1",
+	description="jkl",
+	url="http://google.com",
+	author="yask",
+	author_email="yask123@gmail.com",
+	license='MIT',
+	packages=["yask123"],
+	zip_safe=False)
