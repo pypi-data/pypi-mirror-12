00ac12fc26f7c5423450c9b1d705450e2593ea1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:11 +0000

    Add package block-disposable-email/0.1/block-disposable-email-0.1.tar.gz

---
+++ packages/block-disposable-email/block-disposable-email-0.1.tar.gz/block-disposable-email-0.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""Setup script."""
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+__author__ = 'Max Arnold <arnold.maxim@gmail.com>'
+__version__ = '0.1'
+
+
+setup(
+    name='block-disposable-email',
+    version=__version__,
+
+    # Package dependencies.
+
+    # Metadata for PyPI.
+    author='Max Arnold',
+    author_email='arnold.maxim@gmail.com',
+    license='BSD',
+    url='http://github.com/max-arnold/block-disposable-email',
+    keywords='block disposable email domains',
+    description='Python client for http://block-disposable-email.com',
+    long_description=open(
+        os.path.abspath(os.path.join(os.path.dirname(__file__),
+                        'README.md')
+                        ),
+        'r'
+        ).read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Intended Audience :: System Administrators",
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet'
+    ],
+    packages=['bdea'],
+    platforms='any',
+)
