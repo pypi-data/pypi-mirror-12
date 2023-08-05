7aafa91781d1c869bd5bf66e1d38bb4c63aeb070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:05 +0000

    Add package theanify/0.1.3/theanify-0.1.3.tar.gz

---
+++ packages/theanify/theanify-0.1.3.tar.gz/theanify-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "theanify",
+    version = "0.1.3",
+    author = "Sharad Vikram",
+    author_email = "sharad.vikram@gmail.com",
+    description = "Allows you to annotate instance methods to be compiled by Theano.",
+    license = "MIT",
+    keywords = "theano",
+    url = "https://github.com/sharadmv/theanify",
+    install_requires=['theano'],
+    packages=find_packages(include=[
+        'theanify'
+    ]),
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+     'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 2.7',
+],
+)
