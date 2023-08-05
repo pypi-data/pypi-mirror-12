0d998177bf4d312d5033fcf3e20dfa7f4fc2a79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:05 +0000

    Add package thunderclient/1.0.1/thunderclient-1.0.1.tar.gz

---
+++ packages/thunderclient/thunderclient-1.0.1.tar.gz/thunderclient-1.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='thunderclient',
+    version='1.0.1',
+    author='Krzysztof Jagiello',
+    author_email='me@kjagiello.com',
+    description='A Python library for sending messages to Thunderpush server.',
+    packages=find_packages(),
+    zip_safe=False,
+    license='BSD',
+    include_package_data=True,
+    url='https://github.com/thunderpush/python-thunderclient',
+    install_requires=[
+        'requests==2.7.0',
+    ],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+)
