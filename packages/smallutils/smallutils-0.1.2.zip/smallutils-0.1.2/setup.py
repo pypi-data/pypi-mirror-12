d94f1acc35ef49b6cf10a7a9f118891ca6a1f61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package smallutils/0.1.2/smallutils-0.1.2.zip

---
+++ packages/smallutils/smallutils-0.1.2.zip/smallutils-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='smallutils',
+      version='0.1.2',
+      description='Collection of small utils',
+      author='Tan Kok Hua',
+      author_email='kokhua81@gmail.com',
+      url='https://github.com/spidezad/smallutils',
+      packages=['smallutils'],
+      zip_safe=False)
