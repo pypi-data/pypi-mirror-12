7588061dcfeb5ad753cccbff04c70dc5c2fe6311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:50 +0000

    Add package pywayback/0.10.9.1/pywayback-0.10.9.1.tar.gz

---
+++ packages/pywayback/pywayback-0.10.9.1.tar.gz/pywayback-0.10.9.1/pywb/framework/cache.py
@@ -0,0 +1,34 @@
+try:  # pragma: no cover
+    import uwsgi
+    uwsgi_cache = True
+except ImportError:
+    uwsgi_cache = False
+
+
+#=================================================================
+class UwsgiCache(object):  # pragma: no cover
+    def __setitem__(self, item, value):
+        uwsgi.cache_update(item, value)
+
+    def __getitem__(self, item):
+        return uwsgi.cache_get(item)
+
+    def __contains__(self, item):
+        return uwsgi.cache_exists(item)
+
+    def __delitem__(self, item):
+        uwsgi.cache_del(item)
+
+
+#=================================================================
+class DefaultCache(dict):
+    def __getitem__(self, item):
+        return self.get(item)
+
+
+#=================================================================
+def create_cache():
+    if uwsgi_cache:  # pragma: no cover
+        return UwsgiCache()
+    else:
+        return DefaultCache()
