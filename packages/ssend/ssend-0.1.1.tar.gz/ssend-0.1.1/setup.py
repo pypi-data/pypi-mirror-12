43c1e0a09a8395c90cad8946704479fe50feda45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package ssend/0.1.1/ssend-0.1.1.tar.gz

---
+++ packages/ssend/ssend-0.1.1.tar.gz/ssend-0.1.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+def file_contents(path):
+    with open(path, 'r') as fn:
+        return fn.read()
+
+setup(
+    name='ssend',
+    version="0.1.1",
+    description="Send messages to a room/user in slack from the command line",
+    long_description=(file_contents('README.md')),
+    url="https://github.com/sarlalian/ssend",
+    install_requires=[ 'slacker >= 0.7.0' ],
+    license="MIT",
+    author='Will L. Fife',
+    author_email='sarlalian+pip@gmail.com',
+    packages=['slacksender'],
+    entry_points={
+        'console_scripts': [
+            'ssend=slacksender:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
