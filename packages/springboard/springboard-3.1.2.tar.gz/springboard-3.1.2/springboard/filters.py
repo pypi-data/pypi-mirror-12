26a56ff0d6a1fa3bdd637d5eca507b2ada75b014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:33 +0000

    Add package springboard/3.1.2/springboard-3.1.2.tar.gz

---
+++ packages/springboard/springboard-3.1.2.tar.gz/springboard-3.1.2/springboard/filters.py
@@ -0,0 +1,75 @@
+from dateutil import parser
+
+from jinja2 import contextfilter
+
+from libthumbor import CryptoURL
+
+from markdown import markdown
+
+from pyramid.threadlocal import get_current_registry
+
+from jinja2 import Markup
+from babel import Locale
+from pycountry import languages
+
+from springboard.utils import Paginator
+
+
+# known right-to-left language codes
+KNOWN_RTL_LANGUAGES = {"urd", "ara", "arc", "per", "heb", "kur", "yid"}
+
+
+@contextfilter
+def format_date_filter(ctx, timestamp, format):
+    if isinstance(timestamp, basestring):
+        timestamp = parser.parse(timestamp)
+    return timestamp.strftime(format)
+
+
+@contextfilter
+def thumbor_filter(ctx, image, width, height=0):
+    registry = get_current_registry(context=ctx)
+    security_key = registry.settings.get('thumbor.security_key')
+    if not all([security_key, image]):
+        return ''
+
+    # libthumbor generates an invalid url when height is None:
+    # https://github.com/thumbor/libthumbor/blob/master/libthumbor/url.py#L19
+    # Coerce it to 0. This scales the height proportionally.
+    if height is None:
+        height = 0
+
+    crypto = CryptoURL(key=security_key)
+    return crypto.generate(width=width, height=height, image_url=image)
+
+
+@contextfilter
+def markdown_filter(ctx, content):
+    if not content:
+        return content
+    return Markup(markdown(content))
+
+
+@contextfilter
+def display_language_name_filter(ctx, locale):
+    language_code, _, country_code = locale.partition('_')
+    term_code = languages.get(bibliographic=language_code).terminology
+    return Locale.parse(term_code).language_name
+
+
+def language_direction_filter(locale):
+    language_code, _, country_code = locale.partition('_')
+    if language_code in KNOWN_RTL_LANGUAGES:
+        return 'rtl'
+    return 'ltr'
+
+
+def paginate_filter(results, page, results_per_page=10, slider_value=5):
+    return Paginator(results, page, results_per_page, slider_value)
+
+
+@contextfilter
+def get_category_title_filter(ctx, primary_category_uuid, all_categories):
+    for category in all_categories:
+        if primary_category_uuid == category.uuid:
+            return category.title
---
+++ packages/springboard/springboard-3.1.2.tar.gz/springboard-3.1.2/springboard/tests/test_filters.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+
+from pyramid import testing
+
+from jinja2 import Markup
+from libthumbor import CryptoURL
+
+from springboard.tests import SpringboardTestCase
+from springboard.filters import (
+    format_date_filter, thumbor_filter, markdown_filter,
+    language_direction_filter, paginate_filter)
+
+
+class TestFilters(SpringboardTestCase):
+
+    def setUp(self):
+        self.workspace = self.mk_workspace()
+
+    def test_format_date_filter_string(self):
+        self.assertEqual(
+            format_date_filter({}, '2012-12-31', '%d-%m-%y'),
+            '31-12-12')
+
+    def test_format_date_filter_date(self):
+        self.assertEqual(
+            format_date_filter({}, datetime(2012, 12, 31), '%d-%m-%y'),
+            '31-12-12')
+
+    def test_thumbor_filter(self):
+        testing.setUp(settings={
+            'thumbor.security_key': 'foo',
+        })
+        crypto = CryptoURL(key='foo')
+        self.assertEqual(
+            thumbor_filter({}, 'image', 25, 25),
+            crypto.generate(width=25, height=25, image_url='image'))
+        self.assertEqual(
+            thumbor_filter({}, 'image', 25),
+            crypto.generate(width=25, image_url='image'))
+        self.assertEqual(
+            thumbor_filter({}, 'image', 25, None),
+            crypto.generate(width=25, height=0, image_url='image'))
+
+    def test_thumbor_filter_without_security_key(self):
+        testing.setUp(settings={
+            'thumbor.security_key': None,
+        })
+        self.assertEqual(
+            thumbor_filter({}, 'image', 25, 25), '')
+        self.assertEqual(
+            thumbor_filter({}, 'image', 25), '')
+
+    def test_markdown_filter(self):
+        result = markdown_filter({}, '*foo*')
+        self.assertIsInstance(result, Markup)
+        self.assertEqual(
+            result,
+            '<p><em>foo</em></p>')
+
+    def test_markdown_filter_none(self):
+        self.assertEqual(markdown_filter({}, None), None)
+        self.assertEqual(markdown_filter({}, ''), '')
+
+    def test_language_direction_filter(self):
+        self.assertEqual(language_direction_filter('eng_GB'), 'ltr')
+        self.assertEqual(language_direction_filter('urd_PK'), 'rtl')
+
+    def test_paginate_filter(self):
+        paginator = paginate_filter([1, 2, 3, 4, 5], 2,
+                                    results_per_page=1,
+                                    slider_value=2)
+        self.assertEqual(paginator.get_page(), [3])
+        self.assertEqual(paginator.page_numbers(), [1, 2, 3])
