35b291d5435cb9986ef8ac51b3d2cf9790577775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:14 +0000

    Add package webassets_ng_annotate/0.0.2/webassets_ng_annotate-0.0.2.tar.gz

---
+++ packages/webassets_ng_annotate/webassets_ng_annotate-0.0.2.tar.gz/webassets_ng_annotate-0.0.2/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import os
+import sys
+import doctest
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# TODO change verison
+VERSION = '0.0.2'
+
+name = 'webassets_ng_annotate'
+with open('./webassets_ng_annotate.py') as f:
+    readme = f.read().split("'''")[1]
+
+
+if sys.argv[-1] == 'publish':
+    with open('README.md', 'wt') as f:
+        f.write(readme)
+    if not doctest.testfile('README.md', verbose=True).failed:
+        os.system('python setup.py sdist upload')
+        sys.exit(0)
+
+setup(**{
+    'name': name,
+    'url': 'https://github.com/imbolc/%s' % name.replace('_', '-'),
+    'version': VERSION,
+    'description': readme.split('===\n')[1].strip().split('\n\n')[0],
+    'long_description': readme,
+
+    'py_modules': [name],
+    'install_requires': ['webassets'],
+
+    'author': 'Imbolc',
+    'author_email': 'imbolc@imbolc.name',
+    'license': 'ISC',
+
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+})
