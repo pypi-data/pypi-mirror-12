a4b836069435501483e809bc9659fb5f828349c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:26 +0000

    Add package pyfastaq/3.9.0/pyfastaq-3.9.0.tar.gz

---
+++ packages/pyfastaq/pyfastaq-3.9.0.tar.gz/pyfastaq-3.9.0/setup.py
@@ -0,0 +1,24 @@
+import glob
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyfastaq',
+    version='3.9.0',
+    description='Script to manipulate FASTA and FASTQ files, plus API for developers',
+    packages = find_packages(),
+    author='Martin Hunt',
+    author_email='mh12@sanger.ac.uk',
+    url='https://github.com/sanger-pathogens/Fastaq',
+    scripts=glob.glob('scripts/*'),
+    test_suite='nose.collector',
+    tests_require=['nose >= 1.3'],
+    install_requires=['numpy >= 1.7.1'],
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
