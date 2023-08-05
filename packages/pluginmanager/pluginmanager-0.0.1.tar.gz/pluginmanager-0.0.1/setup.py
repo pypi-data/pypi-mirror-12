73974e1f18cf6ab8a4f78df449ff36787e22efa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:18 +0000

    Add package pluginmanager/0.0.1/pluginmanager-0.0.1.tar.gz

---
+++ packages/pluginmanager/pluginmanager-0.0.1.tar.gz/pluginmanager-0.0.1/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import find_packages, setup
+
+
+directory = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(directory, 'README.md')) as f:
+    long_description = f.read()
+setup(
+    name="pluginmanager",
+    version='0.0.1',
+    description='Python Plugin Management, simplified',
+    long_description=long_description,
+    url='https://github.com/benhoff/pluginmanager',
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Operating System :: OS Independent'],
+    keywords='plugin manager',
+    author='Ben Hoff',
+    author_email='beohoff@gmail.com',
+    packages= find_packages(exclude=['docs', 'tests']),
+    extras_require={
+        'dev': ['flake8']
+    },
+)
