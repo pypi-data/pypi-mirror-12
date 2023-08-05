d21ca03210865239811ef45bd993ecf9acc98831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:18 +0000

    Add package amp-player/0.1.9/amp-player-0.1.9.tar.gz

---
+++ packages/amp-player/amp-player-0.1.9.tar.gz/amp-player-0.1.9/setup.py
@@ -0,0 +1,33 @@
+import sys
+from distutils.core import setup
+from distutils.extension import Extension
+
+if sys.version_info[0] == 2:
+    print("Python 2.x detected.")
+    base_dir = 'python2'
+elif sys.version_info[0] == 3:
+    print("Python 3.x detected.")
+    base_dir = 'python3'
+
+setup(
+    name='amp-player',
+    version='0.1.9',
+    description='Asynchronous command-line YouTube interface',
+    keywords=["music", "audio", "video", "stream", "youtube"],
+    url='https://github.com/nivekuil/amp',
+    download_url='https://github.com/nivekuil/amp/tarball/master',
+    author='Kevin Liu',
+    author_email='mail@nivekuil.com',
+    license='GPL3',
+    entry_points={'console_scripts': ['amp = amp.main:main']},
+    packages = ['amp'],
+    package_dir={
+        '' : base_dir
+    },
+    install_requires=['pafy', 'psutil'],
+    classifiers = [
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+)
