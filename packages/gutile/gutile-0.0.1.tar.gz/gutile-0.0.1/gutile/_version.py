193f28ab2b362aeb95118504a60fb497c4e8d448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:48 +0000

    Add package gutile/0.0.1/gutile-0.0.1.tar.gz

---
+++ packages/gutile/gutile-0.0.1.tar.gz/gutile-0.0.1/gutile/_version.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Eduard Trott
+# @Date:   2015-09-15 08:57:35
+# @Email:  etrott@redhat.com
+# @Last modified by:   etrott
+# @Last Modified time: 2015-09-16 13:50:57
+
+
+from __future__ import unicode_literals, absolute_import
+
+version_info = ('0', '0', '1')
+__version__ = '.'.join(version_info[0:3])  # + '-' + version_info[3]
