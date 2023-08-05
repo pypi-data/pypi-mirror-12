416aa66a4977d5111e02ee3c8117a647b7712378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package medium/0.1/medium-0.1.tar.gz

---
+++ packages/medium/medium-0.1.tar.gz/medium-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'medium',
+    packages = ['medium'],
+    version = '0.1',
+    description = 'SDK for working with the Medium API',
+    author = 'Kyle Hardgrave',
+    author_email = 'kyle@medium.com',
+    url = 'https://github.com/Medium/medium-sdk-python',
+    download_url = 'https://github.com/Medium/medium-sdk-python/tarball/v0.1',
+    keywords = ['medium', 'sdk', 'oauth', 'api'],
+    classifiers = [],
+)
