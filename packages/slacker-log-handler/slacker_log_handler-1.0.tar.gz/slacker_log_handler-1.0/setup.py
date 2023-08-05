1b63acaa209ded525e76a8dce238cd5de6243371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:51 +0000

    Add package slacker-log-handler/1.0/slacker_log_handler-1.0.tar.gz

---
+++ packages/slacker-log-handler/slacker_log_handler-1.0.tar.gz/slacker_log_handler-1.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='slacker_log_handler',
+    packages=['slacker_log_handler'],
+    version='1.0',
+    description='Posts log events to Slack via API',
+    url='https://github.com/mathiasose/slacker_log_handler',
+    download_url='https://github.com/mathiasose/slacker_log_handler/tarball/1.0',
+    author='Mathias Ose',
+    author_email='mathias.ose@gmail.com',
+    keywords=['slack', 'logging'],
+    install_requires=[
+        "slacker==0.7.3"
+    ]
+)
