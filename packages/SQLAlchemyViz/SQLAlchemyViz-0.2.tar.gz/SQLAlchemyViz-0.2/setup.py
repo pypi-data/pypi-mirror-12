7fff30c58b5528a8bcc963163c58d54c545fbc54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:18 +0000

    Add package SQLAlchemyViz/0.2/SQLAlchemyViz-0.2.tar.gz

---
+++ packages/SQLAlchemyViz/SQLAlchemyViz-0.2.tar.gz/SQLAlchemyViz-0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import os
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='SQLAlchemyViz',
+    version='0.2',
+    packages=['sqlalchemyviz'],
+    url='',
+    license='MIT',
+    author='Sebastian Eckweiler',
+    author_email='seb.eckweiler@gmail.com',
+    description='Package to create ER diagrams from SQLAlchemy schemas using Graphviz.',
+    long_description=read('README.rst'),
+    install_requires=['sqlalchemy', 'pydot'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Topic :: Database",
+        "Topic :: Documentation",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python"
+    ],
+    entry_points={
+        'console_scripts': [
+            'sqlaviz = sqlalchemyviz.cli:main',
+        ]
+    }
+)
---
+++ packages/SQLAlchemyViz/SQLAlchemyViz-0.2.tar.gz/SQLAlchemyViz-0.2/sqlalchemyviz/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# This module is part of SQLAlchemyViz and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+from viz import ERDiagram, UMLDiagram
