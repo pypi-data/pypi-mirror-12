6fdb927ac5cb38ed27b85ff7019e7f1397a36aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package seedboxsync/1.0.0/seedboxsync-1.0.0.tar.gz

---
+++ packages/seedboxsync/seedboxsync-1.0.0.tar.gz/seedboxsync-1.0.0/seedboxsync/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Guillaume Kulakowski <guillaume@kulakowski.fr>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# flake8: noqa
+
+__version__ = '1.0.0'
