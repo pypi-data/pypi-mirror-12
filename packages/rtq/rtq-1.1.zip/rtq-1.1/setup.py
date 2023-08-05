91188ee8053cc0b2e5172042fc6d573c16f87dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:25 +0000

    Add package rtq/1.1/rtq-1.1.zip

---
+++ packages/rtq/rtq-1.1.zip/rtq-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name='rtq',
+	version = '1.01',
+	scripts=['rtqinfo.py'],
+	install_requires=['BeautifulSoup', 'yahoo_finance', 'BeautifulSoup', 'argparse', 'urllib3'])
+ 
+
+
