26a11b038bfdc87aec59828210c4c38d4ec3dbcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:10 +0000

    Add package flake8-future-import/0.3.2/flake8-future-import-0.3.2.tar.gz

---
+++ packages/flake8-future-import/flake8-future-import-0.3.2.tar.gz/flake8-future-import-0.3.2/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import codecs
+
+from setuptools import setup
+
+
+def get_version():
+    with open('flake8_future_import.py') as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return eval(line.split('=')[-1])
+
+
+def get_long_description():
+    with codecs.open('README.rst', 'r', 'utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='flake8-future-import',
+    version=get_version(),
+    description='__future__ import checker, plugin for flake8',
+    long_description=get_long_description(),
+    keywords='flake8 import future',
+    install_requires=['flake8'],
+    maintainer='Fabian Neundorf',
+    maintainer_email='CommodoreFabianus@gmx.de',
+    url='https://github.com/xZise/flake8-future-import',
+    license='MIT License',
+    py_modules=['flake8_future_import'],
+    zip_safe=False,
+    entry_points={
+        'flake8.extension': [
+            'flake8-future-import = flake8_future_import:FutureImportChecker',
+        ],
+    },
+    tests_require=['six'],
+    test_suite='test_flake8_future_import',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+)
