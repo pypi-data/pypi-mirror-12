12728d1963e4ca5439c80df801f5f1c05346ef1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package akadata/0.3.0/akadata-0.3.0-py2.py3-none-any.whl

---
+++ packages/akadata/akadata-0.3.0-py2.py3-none-any.whl/akadata/status.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2015, RedJack, LLC.
+# All rights reserved.
+#
+# Please see the COPYING file in this distribution for license details.
+AKAMAI_OK = 0
+AKAMAI_DEFAULT = 1
+AKAMAI_GENERIC_ERROR = 2
+AKAMAI_SMALL_BUFFER = 3
+AKAMAI_TRY_AGAIN = 4
+AKAMAI_PERMISSION_DENIED = 5
+AKAMAI_NOT_FOUND = 6
+AKAMAI_GO_AWAY = 7
+AKAMAI_LATEST = 8
