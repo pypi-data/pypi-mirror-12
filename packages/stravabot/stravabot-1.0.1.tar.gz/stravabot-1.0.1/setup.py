a2659c461f9d65bae6bc478c5976b53bbb30f55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:35 +0000

    Add package stravabot/1.0.1/stravabot-1.0.1.tar.gz

---
+++ packages/stravabot/stravabot-1.0.1.tar.gz/stravabot-1.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='stravabot',
+
+    url="https://chris-lamb.co.uk/projects/stravabot",
+    version='1.0.1',
+    description="Strava -> IRC gateway",
+
+
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    license="BSD",
+
+    packages=(),
+    scripts=(
+        'stravabot',
+    ),
+)
