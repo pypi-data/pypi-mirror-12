cd4710b5e1d87d3e21519ee0326c14954dc1ed3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:37 +0000

    Add package indentation/1.0.0/indentation-1.0.0.zip

---
+++ packages/indentation/indentation-1.0.0.zip/indentation-1.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name = "indentation",
+    packages = ["indentation"],
+    version = "1.0.0",
+    description="Text indentation functions.",
+    author = 'Henry Weickert',
+    author_email = 'henryweickert@gmail.com',
+    url = 'https://github.com/hweickert/indentation',
+    keywords = ['indentation', 'indent', 'text']
+)
