251cea4c9e0bc48bfabff908cfe77a587330a9e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:27 +0000

    Add package nester_cw/1.2.2/nester_cw-1.2.2.tar.gz

---
+++ packages/nester_cw/nester_cw-1.2.2.tar.gz/nester_cw-1.2.2/nester.py
@@ -0,0 +1,20 @@
+"""
+This module aims to solve the nested items.
+"""
+
+
+
+def print_lol(the_list,level):
+        """This funciton take one pareameter, named the_list,
+        it could be anylist in python(it could be the one that contain nested list). Each item in the list take up one line and will be printed into screen.
+
+"""
+
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                        print_lol(each_item,level+1)
+		else:
+			for tab_stop in range(level):
+				print("\t", end = '')
+        print(each_item)
+                        
