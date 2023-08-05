96fa142e63320857a5e58935561a8598953acbe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:52 +0000

    Add package waple/1.0.0/waple-1.0.0.tar.gz

---
+++ packages/waple/waple-1.0.0.tar.gz/waple-1.0.0/waple.py
@@ -0,0 +1,8 @@
+"""This is a python comment"""
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+#this is also a python comment
