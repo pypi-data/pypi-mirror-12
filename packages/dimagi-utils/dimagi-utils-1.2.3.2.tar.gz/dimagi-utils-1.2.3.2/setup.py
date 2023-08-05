2f1fe3a9ea9c44bdedda9b1bf0576efcb55ca17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:10 +0000

    Add package dimagi-utils/1.2.3.2/dimagi-utils-1.2.3.2.tar.gz

---
+++ packages/dimagi-utils/dimagi-utils-1.2.3.2.tar.gz/dimagi-utils-1.2.3.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='dimagi-utils',
+    version='1.2.3.2',
+    description='Dimagi Shared Utilities',
+    author='Dimagi',
+    author_email='dev@dimagi.com',
+    url='http://www.dimagi.com/',
+    packages=find_packages(exclude=['*.pyc']),
+    test_suite='dimagi.test_utils',
+    test_loader='unittest2:TestLoader',
+    install_requires=[
+        'jsonobject-couchdbkit>=0.7.0.1',
+        'django==1.7.10',
+        'django-redis==4.2',
+        'mock>=0.8.0',
+        'openpyxl',
+        'Pillow',
+        'python-dateutil',
+        'pytz',
+        'simplejson',
+        'unittest2',
+        "celery",
+        "django-devserver"
+    ],
+)
