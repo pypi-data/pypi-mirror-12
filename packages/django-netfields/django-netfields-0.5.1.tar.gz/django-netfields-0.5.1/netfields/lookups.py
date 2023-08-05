9721c71e585bb64eed6a8546b7b209d5ce3077e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:26 +0000

    Add package django-netfields/0.5.1/django-netfields-0.5.1.tar.gz

---
+++ packages/django-netfields/django-netfields-0.5.1.tar.gz/django-netfields-0.5.1/netfields/lookups.py
@@ -0,0 +1,94 @@
+from django.db.models import Lookup, Transform, IntegerField
+from django.db.models.lookups import BuiltinLookup
+from netfields.fields import InetAddressField, CidrAddressField
+
+
+class InvalidLookup(BuiltinLookup):
+    def as_sql(self, qn, connection):
+        raise ValueError('Invalid lookup type "%s"' % self.lookup_name)
+
+
+class NetFieldDecoratorMixin(object):
+    def process_lhs(self, qn, connection, lhs=None):
+        lhs = lhs or self.lhs
+        lhs_string, lhs_params = qn.compile(lhs)
+        if isinstance(lhs.source if hasattr(lhs, 'source') else lhs.output_field, InetAddressField):
+            lhs_string = 'HOST(%s)' % lhs_string
+        elif isinstance(lhs.source if hasattr(lhs, 'source') else lhs.output_field, CidrAddressField):
+            lhs_string = 'TEXT(%s)' % lhs_string
+        return lhs_string, lhs_params
+
+
+class EndsWith(NetFieldDecoratorMixin, BuiltinLookup):
+    lookup_name = 'endswith'
+
+
+class IEndsWith(NetFieldDecoratorMixin, BuiltinLookup):
+    lookup_name = 'iendswith'
+
+
+class StartsWith(NetFieldDecoratorMixin, BuiltinLookup):
+    lookup_name = 'startswith'
+
+
+class IStartsWith(NetFieldDecoratorMixin, BuiltinLookup):
+    lookup_name = 'istartswith'
+
+
+class Regex(NetFieldDecoratorMixin, BuiltinLookup):
+    lookup_name = 'regex'
+
+
+class IRegex(NetFieldDecoratorMixin, BuiltinLookup):
+    lookup_name = 'iregex'
+
+
+class NetContains(Lookup):
+    lookup_name = 'net_contains'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s >> %s' % (lhs, rhs), params
+
+
+class NetContained(Lookup):
+    lookup_name = 'net_contained'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s << %s' % (lhs, rhs), params
+
+
+class NetContainsOrEquals(Lookup):
+    lookup_name = 'net_contains_or_equals'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s >>= %s' % (lhs, rhs), params
+
+class NetContainedOrEqual(Lookup):
+    lookup_name = 'net_contained_or_equal'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s <<= %s' % (lhs, rhs), params
+
+
+class Family(Transform):
+    lookup_name = 'family'
+
+    def as_sql(self, compiler, connection):
+        lhs, params = compiler.compile(self.lhs)
+        return "family(%s)" % lhs, params
+
+    @property
+    def output_field(self):
+        return IntegerField()
