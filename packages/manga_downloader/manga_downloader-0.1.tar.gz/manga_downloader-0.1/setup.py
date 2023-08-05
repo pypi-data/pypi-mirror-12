2f759fb5944c3dd5a04ce830f43cfb2d0df95097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:11 +0000

    Add package manga_downloader/0.1/manga_downloader-0.1.tar.gz

---
+++ packages/manga_downloader/manga_downloader-0.1.tar.gz/manga_downloader-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+        name = 'manga_downloader',
+        packages = ['manga_downloader', 'manga_downloader/core'],
+        version = '0.1',
+        description = 'Package to download mangas',
+        author = 'Asish Panda',
+        author_email = 'asishrocks95@gmail.com',
+        url = 'https://github.com/kaichogami/manga_downloader',
+        download_url = 'https://github.com/kaichogami/manga_downloader/tarball/0.1',
+        keywords = ['manga', 'download'],
+        classifiers = [],
+        )
+
