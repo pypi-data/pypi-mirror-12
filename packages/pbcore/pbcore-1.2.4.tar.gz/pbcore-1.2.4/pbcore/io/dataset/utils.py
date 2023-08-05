22fedfcb84fc999342f6891cffa5b9ded117a475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:56 +0000

    Add package pbcore/1.2.4/pbcore-1.2.4.tar.gz

---
+++ packages/pbcore/pbcore-1.2.4.tar.gz/pbcore-1.2.4/pbcore/io/dataset/utils.py
@@ -0,0 +1,144 @@
+"""
+Utils that support fringe DataSet features.
+"""
+import os
+import tempfile
+import logging
+import json
+import shutil
+import pysam
+from pbcore.util.Process import backticks
+
+log = logging.getLogger(__name__)
+
+def consolidateBams(inFiles, outFile, filterDset=None, useTmp=True):
+    """Take a list of infiles, an outFile to produce, and optionally a dataset
+    (filters) to provide the definition and content of filtrations."""
+    # check space available
+    if useTmp:
+        tmpout = tempfile.mkdtemp(suffix="consolidation-filtration")
+        if (disk_free(os.path.split(outFile)[0]) >
+                sum(file_size(infn) for infn in inFiles)):
+            tmpInFiles = _tmpFiles(inFiles, tmpout)
+            origOutFile = outFile
+            origInFiles = inFiles[:]
+            inFiles = tmpInFiles
+            outFile = os.path.join(tmpout, "outfile.bam")
+        else:
+            useTmp = False
+
+    if filterDset and filterDset.filters:
+        finalOutfile = outFile
+        outFile = _infixFname(outFile, "_unfiltered")
+    _mergeBams(inFiles, outFile)
+    if filterDset and filterDset.filters:
+        _filterBam(outFile, finalOutfile, filterDset)
+        outFile = finalOutfile
+    _indexBam(outFile)
+    _pbindexBam(outFile)
+    if useTmp:
+        shutil.copy(outFile, origOutFile)
+        shutil.copy(outFile + ".bai", origOutFile + ".bai")
+        shutil.copy(outFile + ".pbi", origOutFile + ".pbi")
+
+def _tmpFiles(inFiles, tmpout=None):
+    tmpInFiles = []
+    if tmpout is None:
+        tmpout = tempfile.mkdtemp(suffix="consolidation-filtration")
+    for i, fname in enumerate(inFiles):
+        newfn = _infixFname(os.path.join(tmpout, os.path.basename(fname)), i)
+        shutil.copy(fname, newfn)
+        tmpInFiles.append(newfn)
+    return tmpInFiles
+
+def disk_free(path):
+    if path == '':
+        path = os.getcwd()
+    space = os.statvfs(path)
+    return space.f_bavail * space.f_frsize
+
+def file_size(path):
+    return os.stat(path).st_size
+
+def _pbindexBam(fname):
+    cmd = "pbindex {i}".format(i=fname)
+    log.info(cmd)
+    o, r, m = backticks(cmd)
+    if r != 0:
+        raise RuntimeError(m)
+
+def _sortBam(fname):
+    tmpname = _infixFname(fname, "_sorted")
+    cmd = "bamtools sort -in {i} -out {o}".format(i=fname, o=tmpname)
+    log.info(cmd)
+    o, r, m = backticks(cmd)
+    if r != 0:
+        raise RuntimeError(m)
+    shutil.move(tmpname, fname)
+
+def _indexBam(fname):
+    pysam.index(fname)
+
+def _indexFasta(fname):
+    pysam.faidx(fname)
+
+def _mergeBams(inFiles, outFile):
+    if len(inFiles) > 1:
+        cmd = "bamtools merge -in {i} -out {o}".format(i=' -in '.join(inFiles),
+                                                       o=outFile)
+        log.info(cmd)
+        o, r, m = backticks(cmd)
+        if r != 0:
+            raise RuntimeError(m)
+    else:
+        shutil.copy(inFiles[0], outFile)
+
+def _filterBam(inFile, outFile, filterDset):
+    tmpout = tempfile.mkdtemp(suffix="consolidation-filtration")
+    filtScriptName = os.path.join(tmpout, "filtScript.json")
+    _emitFilterScript(filterDset, filtScriptName)
+    cmd = "bamtools filter -in {i} -out {o} -script {s}".format(
+        i=inFile, o=outFile, s=filtScriptName)
+    log.info(cmd)
+    o, r, m = backticks(cmd)
+    if r != 0:
+        raise RuntimeError(m)
+
+def _infixFname(fname, infix):
+    prefix, extension = os.path.splitext(fname)
+    return prefix + str(infix) + extension
+
+def _emitFilterScript(filterDset, filtScriptName):
+    """Use the filter script feature of bamtools. Use with specific filters if
+    all that are needed are available, otherwise filter by readname (easy but
+    uselessly expensive)"""
+    filterMap = {'rname': 'reference',
+                 'pos': 'position',
+                 'length': 'queryBases',
+                 'qname': 'name',
+                 'rq': 'mapQuality'}
+    cheapFilters = True
+    for filt in filterDset.filters:
+        for req in filt:
+            if not filterMap.get(req.name):
+                cheapFilters = False
+    if cheapFilters:
+        script = {"filters":[]}
+        for filt in filterDset.filters:
+            filtDict = {}
+            for req in filt:
+                name = filterMap[req.name]
+                if name == 'reference':
+                    if req.operator == '=' or req.operator == '==':
+                        filtDict[name] = req.value
+                    else:
+                        raise NotImplementedError()
+                else:
+                    filtDict[name] = req.operator + req.value
+            script['filters'].append(filtDict)
+    else:
+        names = [rec.qName for rec in filterDset]
+        script = {"filters":[{"name": name} for name in names]}
+    with open(filtScriptName, 'w') as scriptFile:
+        scriptFile.write(json.dumps(script))
+
---
+++ packages/pbcore/pbcore-1.2.4.tar.gz/pbcore-1.2.4/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, Extension, find_packages
+import sys
+
+if ("install" in sys.argv) and sys.version_info < (2, 7, 0):
+    print "pbcore requires Python 2.7"
+    sys.exit(-1)
+
+globals = {}
+execfile("pbcore/__init__.py", globals)
+__VERSION__ = globals["__VERSION__"]
+
+setup(
+    name = 'pbcore',
+    version=__VERSION__,
+    author='Pacific Biosciences',
+    author_email='devnet@pacificbiosciences.com',
+    description="A Python library for reading and writing PacBio® data files",
+    license=open('LICENSES.txt').read(),
+    packages = find_packages('.'),
+    package_dir = {'':'.'},
+    package_data = {'pbcore': ['data/*.h5', 'data/*.gff', 'data/*.fasta',
+                               'data/*.fasta.fai', 'data/*.fofn', 'data/*.m4',
+                               'data/*.fa', 'data/*.fa.fai',
+                               'data/*.m5', 'data/*.bam', 'data/*.bam.bai', "data/*.bam.pbi",
+                               'chemistry/resources/*.xml',
+                               'data/datasets/*.*',
+                               'data/datasets/yieldtest/*.*']
+                               },
+    zip_safe = False,
+    entry_points = { "console_scripts" : [ ".open = pbcore.io.opener:entryPoint" ] },
+    scripts=['bin/dataset.py'],
+    install_requires=[
+        'h5py >= 2.0.1',
+        'numpy >= 1.7.1',
+        'pysam >= 0.8.1'
+    ])
