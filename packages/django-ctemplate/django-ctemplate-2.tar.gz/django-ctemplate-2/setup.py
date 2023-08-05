a7f8976766844ae50a966ff4f5cd0c7cbdfa4678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:01 +0000

    Add package django-ctemplate/2/django-ctemplate-2.tar.gz

---
+++ packages/django-ctemplate/django-ctemplate-2.tar.gz/django-ctemplate-2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='django-ctemplate',
+    version='2',
+    packages=(
+        'ctemplate',
+    ),
+    url='https://chris-lamb.co.uk/projects/django-ctemplate',
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    description="Compile Django templates to C",
+)
