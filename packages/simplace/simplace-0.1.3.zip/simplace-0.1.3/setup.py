db33faa30b2857f746781ae9cfbf9b6d553d7c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:36 +0000

    Add package simplace/0.1.3/simplace-0.1.3.zip

---
+++ packages/simplace/simplace-0.1.3.zip/simplace-0.1.3/setup.py
@@ -0,0 +1,19 @@
+'''
+Created on 06.10.2015
+
+@author: k
+'''
+from setuptools import setup
+
+setup(name='simplace',
+      version='0.1.3',
+      description='Interact with the simulation framework Simplace',
+      url='http://www.simplace.net/',
+      author='Gunther Krauss',
+      author_email='guntherkrauss@ui-bonn.de',
+      license='GPL3',
+      packages=['simplace'],
+      install_requires=[
+          'Jpype1-py3',
+      ],
+      zip_safe=False)
