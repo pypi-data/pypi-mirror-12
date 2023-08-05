600de43cd30e5b9526e562be7b6a6a1d3e1a35d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:29 +0000

    Add package django-select2/5.2.0/Django-Select2-5.2.0.tar.gz

---
+++ packages/django-select2/Django-Select2-5.2.0.tar.gz/Django-Select2-5.2.0/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import codecs
+import os
+
+from setuptools import Command, find_packages, setup
+
+
+def read(file_name):
+    file_path = os.path.join(os.path.dirname(__file__), file_name)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+PACKAGE = "django_select2"
+NAME = "Django-Select2"
+DESCRIPTION = "Select2 option fields for Django"
+AUTHOR = "Nirupam Biswas, Johannes Hoppe"
+AUTHOR_EMAIL = "admin@applegrew.com"
+URL = "https://github.com/applegrew/django-select2"
+VERSION = __import__(PACKAGE).__version__
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys
+        import subprocess
+
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=read("README.md"),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="LICENSE.txt",
+    url=URL,
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django :: 1.7",
+        "Framework :: Django :: 1.8",
+        "Framework :: Django",
+    ],
+    install_requires=[
+        'django-appconf>=0.6.0',
+    ],
+    zip_safe=False,
+    cmdclass={'test': PyTest},
+)
