1021049783f070197641315228ccd3f39606b1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:11 +0000

    Add package mclient/0.1.6/mclient-0.1.6.tar.gz

---
+++ packages/mclient/mclient-0.1.6.tar.gz/mclient-0.1.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='mclient',
+    packages=['mclient'],
+    url='https://github.com/rusenask/mirage-client',
+    license='GPLv3',
+    author='Karolis Rusenas',
+    author_email='karolis.rusenas@gmail.com',
+    keywords=['testing', 'cli', 'mirage', 'client'],
+    description='CLI and Client library for Mirage external service virtualization',
+    install_requires=[
+        'docopt', 'requests', 'PyYAML'
+    ],
+    requires=['docopt', 'requests', 'PyYAML'],
+    entry_points={
+        'console_scripts': ['mirage=mclient.mclient:main'],
+    },
+    version='0.1.6'
+)
