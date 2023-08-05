77bd92a4d717be86a49d4509d989a3a7adc88df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package mdx_audio/0.1.0/mdx_audio-0.1.0.tar.gz

---
+++ packages/mdx_audio/mdx_audio-0.1.0.tar.gz/mdx_audio-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from mdx_audio import __version__, __author__
+
+setup(name='mdx_audio',
+      version=__version__,
+      description='Markdown 2.0 extension for audio',
+      author=__author__,
+      author_email='panosktn@gmail.com',
+      url='https://github.com/pgk/python-markdown-audio',
+      py_modules = ['mdx_audio'],
+)
