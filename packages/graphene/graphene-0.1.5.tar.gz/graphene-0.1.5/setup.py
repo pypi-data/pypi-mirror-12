3ccc4d6c2432738ef21df19c3b0236cdc7676ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package graphene/0.1.5/graphene-0.1.5.tar.gz

---
+++ packages/graphene/graphene-0.1.5.tar.gz/graphene-0.1.5/setup.py
@@ -0,0 +1,74 @@
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+setup(
+    name='graphene',
+    version='0.1.5',
+
+    description='Graphene: Python DSL for GraphQL',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/graphql-python/graphene',
+
+    author='Syrus Akbary',
+    author_email='me@syrusakbary.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    keywords='api graphql protocol rest relay graphene',
+
+    packages=find_packages(exclude=['tests']),
+
+    install_requires=[
+        'six>=1.10.0',
+        'blinker',
+        'graphql-core==0.1a3',
+        'graphql-relay==0.2.0'
+    ],
+    tests_require=[
+        'pytest>=2.7.2',
+        'pytest-django',
+    ],
+    extras_require={
+        'django': [
+            'Django>=1.6.0,<1.9',
+            'singledispatch>=3.4.0.3',
+        ],
+    },
+
+    cmdclass={'test': PyTest},
+)
