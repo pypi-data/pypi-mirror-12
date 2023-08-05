4b05a979030c990e43c2b0cbab3b6c1d5ba38a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:04 +0000

    Add package tethne/0.7.12.2/tethne-0.7.12.2.tar.gz

---
+++ packages/tethne/tethne-0.7.12.2.tar.gz/tethne-0.7.12.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, Extension
+
+DISTNAME = 'tethne'
+AUTHOR = 'E. Peirson, Digital Innovation Group @ ASU'
+MAINTAINER = 'Erick Peirson'
+MAINTAINER_EMAIL = 'erick [dot] peirson [at] asu [dot] edu'
+DESCRIPTION = ('Bibliographic network and corpus analysis for historians')
+LICENSE = 'GNU GPL 3'
+URL = 'http://diging.github.io/tethne/'
+VERSION = '0.7.12.2'
+
+PACKAGES = ['tethne',
+            'tethne.analyze',
+            'tethne.classes',
+            'tethne.model',
+            'tethne.model.corpus',
+            'tethne.networks',
+            'tethne.readers',
+            'tethne.writers']
+
+setup(
+    name=DISTNAME,
+    author=AUTHOR,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    description=DESCRIPTION,
+    license=LICENSE,
+    url=URL,
+    version=VERSION,
+    packages = PACKAGES,
+    include_package_data=True,
+    install_requires=[
+        "networkx >= 1.8.1",
+        "Unidecode >= 0.04.16",
+        "iso8601",
+        "rdflib"
+    ],
+)
