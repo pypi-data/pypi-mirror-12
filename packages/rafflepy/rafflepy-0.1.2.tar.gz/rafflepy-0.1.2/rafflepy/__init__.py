d88ef5c3853d667c2a6b88b1a06b67808fcc58d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:38 +0000

    Add package rafflepy/0.1.2/rafflepy-0.1.2.tar.gz

---
+++ packages/rafflepy/rafflepy-0.1.2.tar.gz/rafflepy-0.1.2/rafflepy/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Eduard Trott
+# @Date:   2015-09-17 11:41:18
+# @Email:  etrott@redhat.com
+# @Last modified by:   etrott
+# @Last Modified time: 2015-09-17 11:41:37
