78e849b07ce36092dc7406df5ae9981e6155a85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:31 +0000

    Add package batch_sms/0.1.0/batch_sms-0.1.0.tar.gz

---
+++ packages/batch_sms/batch_sms-0.1.0.tar.gz/batch_sms-0.1.0/batch_sms/sender.py
@@ -0,0 +1,7 @@
+from abc import ABCMeta, abstractmethod
+
+class Sender:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def send(self, message_body, to_number, from_number, media_url=None, callback=None): pass
