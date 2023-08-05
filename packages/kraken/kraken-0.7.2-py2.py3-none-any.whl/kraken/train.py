e45506436210aff590aa04746d947e8d4c872078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:46 +0000

    Add package kraken/0.7.2/kraken-0.7.2-py2.py3-none-any.whl

---
+++ packages/kraken/kraken-0.7.2-py2.py3-none-any.whl/kraken/train.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Utility functions for training CLSTM neural networks.
+"""
+
+from __future__ import absolute_import, division, print_function
+from __future__ import unicode_literals
+
+import logging
+
+logger = logging.getLogger(__name__)
