6f4b65aad3814d3fabf5e80e594f8251cd71406c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:18 +0000

    Add package rookout/0.4.5/rookout-0.4.5.tar.gz

---
+++ packages/rookout/rookout-0.4.5.tar.gz/rookout-0.4.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "rookout",
+    version = "0.4.5",
+    description = "A rookie's workout library by zrong.",
+    author = "zrong",
+    author_email = "zrongzrong@gmail.com",
+    url = "http://github.com/zrong/python",
+    license = "MIT",
+    keywords = "utils development zrong rookie workout",
+    packages = find_packages(exclude=["tests*","docs"]),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)
