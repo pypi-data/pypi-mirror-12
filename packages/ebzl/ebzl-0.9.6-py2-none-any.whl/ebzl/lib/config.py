0b3dd2e12de9b0e88cea2785991e383923296fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:35 +0000

    Add package ebzl/0.9.6/ebzl-0.9.6-py2-none-any.whl

---
+++ packages/ebzl/ebzl-0.9.6-py2-none-any.whl/ebzl/lib/config.py
@@ -0,0 +1,110 @@
+"""Helper module for parsing AWS ini config files."""
+
+import os
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
+
+AWS_CLI_CREDENTIALS_PATH = "~/.aws/credentials"
+
+AWS_CLI_CONFIG_PATH = "~/.aws/config"
+
+DEFAULT_PROFILE_NAME = os.getenv("AWS_DEFAULT_PROFILE", "default")
+
+
+class NoConfigFoundException(Exception):
+    """Config file not present."""
+
+    pass
+
+
+def _get_config_parser(path):
+    """Open and parse given config.
+
+    :type path: basestring
+    :rtype: ConfigParser.ConfigParser
+    """
+
+    config_parser = configparser.ConfigParser()
+
+    try:
+        with open(os.path.expanduser(path), "rb") as f:
+            config_parser.readfp(f)
+    except IOError:
+        raise NoConfigFoundException(
+            "Can't find the config file: %s"
+            % config_paths)
+    else:
+        return config_parser
+
+
+def get_credentials(profile):
+    """Returns credentials for given profile as a (key, secret) tuple.
+
+    :type profile: basestring
+    :rtype: tuple
+    """
+
+    config = _get_config_parser(path=AWS_CLI_CREDENTIALS_PATH)
+
+    key = config.get(profile, "aws_access_key_id")
+    secret = config.get(profile, "aws_secret_access_key")
+
+    return key, secret
+
+
+def get_credentials_dict(profile):
+    """Returns credentials as a dict (for use as kwargs).
+
+    :type profile: basestring
+    :rtype: dict
+    """
+
+    key, secret = get_credentials(profile)
+
+    return {"aws_access_key_id": key,
+            "aws_secret_access_key": secret}
+
+
+def get_profile_names():
+    """Get available profile names.
+
+    :rtype: list
+    :returns: list of profile names (strings)
+    """
+
+    return _get_config_parser(path=AWS_CLI_CREDENTIALS_PATH).sections()
+
+
+def has_default_profile():
+    """Is default profile present?
+
+    :rtype: bool
+    """
+
+    return DEFAULT_PROFILE_NAME in get_profile_names()
+
+
+def get_default_region(profile):
+    """Get the default region for given profile from AWS CLI tool's config.
+
+    :type profile: basestring
+    :rtype: basestring
+    :returns: name of defalt region if defined in config, None otherwise
+    """
+
+    config = _get_config_parser(path=AWS_CLI_CONFIG_PATH)
+
+    try:
+        return config.get("profile %s" % profile, "region")
+    except (configparser.NoSectionError, configparser.NoOptionError):
+        pass
+
+    try:
+        return config.get("default", "region")
+    except (configparser.NoSectionError, configparser.NoOptionError):
+        pass
+
+    return None
---
+++ packages/ebzl/ebzl-0.9.6-py2-none-any.whl/ebzl/modules/instances.py
@@ -0,0 +1,85 @@
+import argparse
+
+from .. lib import (
+    parameters,
+    eb,
+    ec2
+)
+
+import boto
+
+
+def get_argument_parser():
+    parser = argparse.ArgumentParser("ebzl instances")
+
+    parameters.add_profile(parser)
+    parameters.add_region(parser, required=False)
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-e", "--env-name",
+                       help="ElasticBeanstalk environment name.")
+    group.add_argument("-n", "--name",
+                       help="Instance Name. Wilcards allowed.")
+
+    return parser
+
+
+def env(args):
+    layer1 = eb.get_layer1(profile=args.profile, region=args.region)
+
+    try:
+        data = layer1.describe_environment_resources(
+            environment_name=args.env_name)
+    except boto.exception.BotoServerError as exc:
+        print(exc.message)
+        return
+
+    instance_ids = (data["DescribeEnvironmentResourcesResponse"]
+                        ["DescribeEnvironmentResourcesResult"]
+                        ["EnvironmentResources"]
+                        ["Instances"])
+
+    instance_ids = [x["Id"] for x in instance_ids]
+
+    ec2_conn = ec2.get_connection(args.profile, args.region)
+    instances = ec2_conn.get_only_instances(instance_ids=instance_ids)
+
+    for i in instances:
+        print("%s-%s\tansible_ssh_host=%s\tansible_ssh_user=ec2-user" % (
+            args.env_name, i.id, i.private_ip_address))
+
+
+def instance(args):
+    ec2_conn = ec2.get_connection(args.profile, args.region)
+    instances = ec2_conn.get_only_instances(filters={"tag:Name": args.name})
+
+    max_name_len = 25
+
+    for i in sorted(instances, key=lambda x: x.tags.get("Name", "")):
+        if i.state != "running":
+            continue
+
+        i.ip_address = i.ip_address or ""
+
+        name = i.tags.get("Name", "?")
+        if len(name) > max_name_len:
+            name = "%s..." % i.tags.get("Name", "?")[:max_name_len - 3]
+
+        print("%s%s %s %s %s" % (
+            name,
+            " " * (max_name_len - len(name)),
+            i.id,
+            i.ip_address.center(15),
+            i.private_ip_address
+        ))
+
+
+def run(argv):
+    args = parameters.parse(parser=get_argument_parser(),
+                            argv=argv,
+                            postprocessors=[parameters.add_default_region])
+
+    if args.env_name:
+        env(args)
+    else:
+        instance(args)
