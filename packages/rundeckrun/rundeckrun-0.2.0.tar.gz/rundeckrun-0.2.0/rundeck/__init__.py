2643fab3161060260d88162e7ce5ee79f2636153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:21 +0000

    Add package rundeckrun/0.2.0/rundeckrun-0.2.0.tar.gz

---
+++ packages/rundeckrun/rundeckrun-0.2.0.tar.gz/rundeckrun-0.2.0/rundeck/__init__.py
@@ -0,0 +1,12 @@
+"""
+:summary: rundeckrun package
+
+:license: Apache License, Version 2.0
+:author: Mark LaPerriere
+:contact: rundeckrun@mindmind.com
+:copyright: Mark LaPerriere 2015
+"""
+__docformat__ = "restructuredtext en"
+
+__VERSION__ = (0, 2, 0)
+VERSION = '.'.join(map(str, __VERSION__))
