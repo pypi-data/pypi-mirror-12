d0febbdbff73927f22d5395ecb79ca187f3c762b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:22 +0000

    Add package tsne/0.1.3/tsne-0.1.3.tar.gz

---
+++ packages/tsne/tsne-0.1.3.tar.gz/tsne-0.1.3/setup.py
@@ -0,0 +1,64 @@
+# encoding: utf-8
+import sys
+import platform
+
+from setuptools import find_packages
+from distutils.core import setup
+from distutils.extension import Extension
+
+import versioneer
+import numpy
+from Cython.Distutils import build_ext
+
+
+if sys.platform == 'darwin':
+    # OS X
+    version, _, _ = platform.mac_ver()
+    parts = version.split('.')
+    v1 = int(parts[0])
+    v2 = int(parts[1])
+    v3 = int(parts[2]) if len(parts) == 3 else None
+
+    if v2 >= 10:
+        # More than 10.10
+        extra_compile_args=['-I/System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/Headers']
+    else:
+        extra_compile_args=['-I/System/Library/Frameworks/vecLib.framework/Headers']
+
+    ext_modules = [Extension(
+                   name='bh_sne',
+                   sources=['tsne/bh_sne_src/quadtree.cpp', 'tsne/bh_sne_src/tsne.cpp', 'tsne/bh_sne.pyx'],
+                   include_dirs=[numpy.get_include(), 'tsne/bh_sne_src/'],
+                   extra_compile_args=extra_compile_args,
+                   extra_link_args=['-Wl,-framework', '-Wl,Accelerate', '-lcblas'],
+                   language='c++')]
+else:
+    # LINUX
+    ext_modules = [Extension(
+                   name='bh_sne',
+                   sources=['tsne/bh_sne_src/quadtree.cpp', 'tsne/bh_sne_src/tsne.cpp', 'tsne/bh_sne.pyx'],
+                   include_dirs=[numpy.get_include(), '/usr/local/include', 'tsne/bh_sne_src/'],
+                   library_dirs=['/usr/local/lib'],
+                   extra_compile_args=['-msse2', '-O3', '-fPIC', '-w'],
+                   extra_link_args=['-lcblas'],
+                   language='c++')]
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+cmdclass = versioneer.get_cmdclass()
+cmdclass['build_ext'] = build_ext
+
+setup(name='tsne',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='TSNE implementations for python',
+      long_description='',
+      author='Daniel Rodriguez',
+      author_email='df.rodriguez@gmail.com',
+      url='https://github.com/danielfrg/py_tsne',
+      license='Apache 2.0',
+      packages=find_packages(),
+      ext_modules=ext_modules,
+      install_requires=required
+)
