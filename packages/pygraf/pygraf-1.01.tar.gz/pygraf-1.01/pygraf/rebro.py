99c9c8a16245864495e7a9f4c3354424845bad33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:12 +0000

    Add package pygraf/1.01/pygraf-1.01.tar.gz

---
+++ packages/pygraf/pygraf-1.01.tar.gz/pygraf-1.01/pygraf/rebro.py
@@ -0,0 +1,8 @@
+__author__ = 'dwliv_000'
+class rebro:# ����� ����� �����(��� �������� ����� ����� ��� ���������� �����)
+    def __init__(self,u,v,c):
+        self.first=u
+        self.second=v
+        self.ves=c
+    def change_ves(self,c):
+        self.ves=c
---
+++ packages/pygraf/pygraf-1.01.tar.gz/pygraf-1.01/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(name='pygraf',
+      version='1.01',
+      description='Python Graph library',
+      author='Panarin Yura',
+      author_email='dw.livion@yandex.ru',
+      url='https://pypi.python.org/pypi/pygraf',
+      packages=['pygraf'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Python Software Foundation License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Communications :: Email',
+          'Topic :: Office/Business',
+          'Topic :: Software Development :: Bug Tracking',
+          ]
+     )
