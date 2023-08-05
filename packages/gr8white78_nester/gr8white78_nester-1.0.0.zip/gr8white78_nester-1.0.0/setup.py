4db48a9b1e240a002403dc38200af31f1902e583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:21 +0000

    Add package gr8white78_nester/1.0.0/gr8white78_nester-1.0.0.zip

---
+++ packages/gr8white78_nester/gr8white78_nester-1.0.0.zip/gr8white78_nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name		= 'gr8white78_nester',
+	version		= '1.0.0',
+	py_modules	= ['gr8white78_nester'],
+	author		= 'newbie',
+	author_email	= 'gr8white78@gmail.com',
+	url		= 'htp://www.doesntexistyet.org',
+	description	= 'A simple printer of nested lists',
+	)
+
