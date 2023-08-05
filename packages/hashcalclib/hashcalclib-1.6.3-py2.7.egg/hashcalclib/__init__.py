8816c23500a623e7cefb80ebeb720cf7c0da5807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package hashcalclib/1.6.3/hashcalclib-1.6.3-py2.7.egg

---
+++ packages/hashcalclib/hashcalclib-1.6.3-py2.7.egg/hashcalclib/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# python version: 2.7.5 final, serial: 0
+
+__author__ = 'Leonard Wei'
+__maintainer__ = 'Leonard Wei'
+__email__ = 'gooxxgle.mail@gmail.com'
+__url__ = 'https://github.com/leonard-wei/hashcalclib'
+__copyright__ = 'Copyright (c) 2014-2015, Leonard Wei'
+__credits__ = ''
+__license__ = 'PSFL or GPLv2'
+__date__ = 'SEP 17 2015'
+__version__ = '1.6.3'
+
