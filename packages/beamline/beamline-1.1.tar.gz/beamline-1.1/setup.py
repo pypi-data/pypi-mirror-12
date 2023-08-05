c4cd732a184f70ce82cacec8d9da9aef4c308994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:07 +0000

    Add package beamline/1.1/beamline-1.1.tar.gz

---
+++ packages/beamline/beamline-1.1.tar.gz/beamline-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+        name     = "beamline",
+        version  = "1.1",
+        description = "beamline plot module",
+        author   = "Tong Zhang",
+        author_email = "warriorlance@gmail.com",
+        platforms = ["Linux"],
+        license  = "MIT",
+        packages = find_packages(),
+     )
+
