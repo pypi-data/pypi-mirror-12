fcebf4ca89cfc4772fd67086b61afa457a7b1b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package bidon/0.4.6/bidon-0.4.6.tar.gz

---
+++ packages/bidon/bidon-0.4.6.tar.gz/bidon-0.4.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+version = "0.4.6"
+
+setup(name="bidon",
+      version=version,
+      license="BSD",
+      platforms="any",
+      author="Trey Cucco",
+      author_email="fcucco@gmail.com",
+      packages=find_packages(),
+      description="A simple, easy to use, and flexible data handling library",
+      url="https://github.com/treycucco/bidon",
+      download_url="https://github.com/treycucco/bidon/tarball/master",
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3 :: Only"
+      ])
