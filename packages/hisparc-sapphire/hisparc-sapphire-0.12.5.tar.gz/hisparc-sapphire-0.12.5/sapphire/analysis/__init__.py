30405f60459d4ee6a468157657948d3d9036ea56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package hisparc-sapphire/0.12.5/hisparc-sapphire-0.12.5.tar.gz

---
+++ packages/hisparc-sapphire/hisparc-sapphire-0.12.5.tar.gz/hisparc-sapphire-0.12.5/sapphire/analysis/__init__.py
@@ -0,0 +1,63 @@
+"""Data analysis
+
+This package contains modules for performing various analysis tasks:
+
+:mod:`~sapphire.analysis.calibration`
+    Determine calibration values
+
+:mod:`~sapphire.analysis.coincidence_queries`
+    Filter coincidences analysed with
+    :class:`~sapphire.analysis.coincidences.CoincidencesESD`
+
+:mod:`~sapphire.analysis.coincidences`
+    Search for coincidences between HiSPARC stations
+
+:mod:`~sapphire.analysis.core_reconstruction`
+    Shower core and shower size reconstruction
+
+:mod:`~sapphire.analysis.direction_reconstruction`
+    EAS direction reconstruction
+
+:mod:`~sapphire.analysis.event_utils`
+    Module for getting data from processed events
+
+:mod:`~sapphire.analysis.find_mpv`
+    Module for finding the MIP peak MPV in pulseintegral data
+
+:mod:`~sapphire.analysis.landau`
+    Landau distribution
+
+:mod:`~sapphire.analysis.process_events`
+    Process HiSPARC events
+
+:mod:`~sapphire.analysis.process_traces`
+    Process HiSPARC traces
+
+:mod:`~sapphire.analysis.reconstructions`
+    Perform shower reconstructions
+
+"""
+from . import calibration
+from . import coincidence_queries
+from . import coincidences
+from . import core_reconstruction
+from . import direction_reconstruction
+from . import event_utils
+from . import find_mpv
+from . import landau
+from . import process_events
+from . import process_traces
+from . import reconstructions
+
+
+__all__ = ['calibration',
+           'coincidence_queries',
+           'coincidences',
+           'core_reconstruction',
+           'direction_reconstruction',
+           'event_utils',
+           'find_mpv',
+           'landau',
+           'process_events',
+           'process_traces',
+           'reconstructions']
