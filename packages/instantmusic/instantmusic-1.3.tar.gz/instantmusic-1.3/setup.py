db1251ef541e6654422fbb3864c09d76367a769e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:59 +0000

    Add package instantmusic/1.3/instantmusic-1.3.tar.gz

---
+++ packages/instantmusic/instantmusic-1.3.tar.gz/instantmusic-1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='instantmusic',
+      version='1.3',
+      description='Instantly download any song! Without knowing the name of the song!!!!',
+      url='https://github.com/yask123/Instant-Music-Downloader',
+      author='Yask Srivastava',
+      author_email='yask123@gmail.com',
+      license='MIT',
+      packages=['instantmusic'],
+      scripts=['bin/instantmusic'],
+      install_requires=[
+          'youtube-dl',
+          'BeautifulSoup4',
+      ],
+      zip_safe=False)
