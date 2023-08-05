6b63daf7239077de7a9a52f2bc98c7586ce7e495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:30 +0000

    Add package django-donations/0.1.1/django_donations-0.1.1-py3-none-any.whl

---
+++ packages/django-donations/django_donations-0.1.1-py3-none-any.whl/donations/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Donation, Frequency, DonationProvider
+
+
+class DonationAdmin(admin.ModelAdmin):
+    pass
+
+
+class FrequencyAdmin(admin.ModelAdmin):
+    pass
+
+
+class DonationProviderAdmin(admin.ModelAdmin):
+    readonly_fields = ('name', 'klass')
+
+
+admin.site.register(Donation, DonationAdmin)
+admin.site.register(Frequency, FrequencyAdmin)
+admin.site.register(DonationProvider, DonationProviderAdmin)
