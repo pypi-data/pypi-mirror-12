89d0ab07af4db5872674e5351deb4f8d1c109855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package sklearn-pmml/0.1.1/sklearn-pmml-0.1.1.tar.gz

---
+++ packages/sklearn-pmml/sklearn-pmml-0.1.1.tar.gz/sklearn-pmml-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, Command
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+        import sys
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+setup(
+    name='sklearn-pmml',
+    version='0.1.1',
+    packages=['sklearn_pmml', 'sklearn_pmml.convert'],
+    install_requires=[
+        "pyxb",
+        "scikit-learn",
+        "pandas",
+        "scipy",
+        "pytest",
+        "lxml",
+        "enum34",
+    ],
+    cmdclass={'test': PyTest},
+    url='https://github.com/alex-pirozhenko/sklearn-pmml',
+    license='MIT',
+    author='Alex Pirozhenko',
+    author_email='apirozhenko@pulsepoint.com',
+    description='A library that allows serialization of SciKit-Learn estimators into PMML'
+)
