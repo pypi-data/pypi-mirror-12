c842738513db04b455b70133a8a707ad46efae6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:37 +0000

    Add package bitjws/0.1.0/bitjws-0.1.0.tar.gz

---
+++ packages/bitjws/bitjws-0.1.0.tar.gz/bitjws-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+readme = open('./README.md').read()
+
+classifiers = [
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Security :: Cryptography"
+]
+
+setup(
+    name="bitjws",
+    version="0.1.0",
+    description='JWS using Bitcoin message signing',
+    long_description=readme,
+    author='Guilherme Polo',
+    author_email='gp@deginner.com',
+    url='https://github.com/g-p-g/bitjws',
+    license='MIT',
+    classifiers=classifiers,
+    include_package_data=True,
+    packages=['bitjws'],
+    setup_requires=['pytest-runner'],
+    install_requires=['base58', 'secp256k1'],
+    tests_require=['pytest', 'pytest-cov']
+)
