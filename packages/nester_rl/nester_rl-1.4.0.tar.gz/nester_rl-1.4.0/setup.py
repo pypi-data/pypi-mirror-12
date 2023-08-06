a436d20c4067fc0c62cc03a2a65b2b3d2b70b019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:12 +0000

    Add package nester_rl/1.4.0/nester_rl-1.4.0.tar.gz

---
+++ packages/nester_rl/nester_rl-1.4.0.tar.gz/nester_rl-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name ='nester_rl',
+    version='1.4.0',
+    py_modules=['nester_rl'],
+    author='hfpython',
+    author_email='hfpython@headfirstlabs.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nester lists',
+    )
