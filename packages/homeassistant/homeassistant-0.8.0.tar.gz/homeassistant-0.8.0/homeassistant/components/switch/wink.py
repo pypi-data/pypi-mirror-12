2a5e5e01ea0a6f1fbfb55b804fadeccdfdce0ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:07 +0000

    Add package homeassistant/0.8.0/homeassistant-0.8.0.tar.gz

---
+++ packages/homeassistant/homeassistant-0.8.0.tar.gz/homeassistant-0.8.0/homeassistant/components/switch/wink.py
@@ -0,0 +1,34 @@
+"""
+homeassistant.components.switch.wink
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Support for Wink switches.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/switch.wink/
+"""
+import logging
+
+from homeassistant.components.wink import WinkToggleDevice
+from homeassistant.const import CONF_ACCESS_TOKEN
+
+REQUIREMENTS = ['https://github.com/balloob/python-wink/archive/'
+                '9eb39eaba0717922815e673ad1114c685839d890.zip'
+                '#python-wink==0.1.1']
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """ Sets up the Wink platform. """
+    import pywink
+
+    if discovery_info is None:
+        token = config.get(CONF_ACCESS_TOKEN)
+
+        if token is None:
+            logging.getLogger(__name__).error(
+                "Missing wink access_token. "
+                "Get one at https://winkbearertoken.appspot.com/")
+            return
+
+        pywink.set_bearer_token(token)
+
+    add_devices(WinkToggleDevice(switch) for switch in pywink.get_switches())
