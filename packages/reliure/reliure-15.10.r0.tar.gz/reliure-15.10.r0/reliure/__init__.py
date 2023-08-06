493bdaab47678162bd1448a43ee50a7e882c74fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:12 +0000

    Add package reliure/15.10.r0/reliure-15.10.r0.tar.gz

---
+++ packages/reliure/reliure-15.10.r0.tar.gz/reliure-15.10.r0/reliure/__init__.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+""":mod:`reliure`
+=================
+"""
+__version__ = '15.10'
+__all__ = ['Composable', 'Optionable']
+
+from reliure.pipeline import Composable, Optionable
