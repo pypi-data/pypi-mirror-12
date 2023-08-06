b429ef752cd4016911e78bf24494335050fe1c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:15 +0000

    Add package nonstdlib/1.5/nonstdlib-1.5.tar.gz

---
+++ packages/nonstdlib/nonstdlib-1.5.tar.gz/nonstdlib-1.5/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import distutils.core
+import subprocess, shlex
+
+# Uploading to PyPI
+# =================
+# The first time only:
+# $ python setup.py register -r pypi
+#
+# Every version bump:
+# $ git tag <version>; git push
+# $ python setup.py sdist upload -r pypi
+
+version = '1.5'
+distutils.core.setup(
+        name='nonstdlib',
+        version=version,
+        author='Kale Kundert',
+        author_email='kale@thekunderts.net',
+        url='https://github.com/kalekundert/nonstdlib',
+        download_url='https://github.com/kalekundert/nonstdlib/tarball/'+version,
+        license='MIT',
+        description="A collection of general-purpose utilities.",
+        long_description=open('README.rst').read(),
+        keywords=['utilities', 'library'],
+        packages=['nonstdlib'],
+)
