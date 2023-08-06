06886fa41aaccdb39928cb2f3a7569636fa81a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:37 +0000

    Add package rockwell/0.0.3/rockwell-0.0.3.zip

---
+++ packages/rockwell/rockwell-0.0.3.zip/rockwell-0.0.3/rockwell/rockwell.py
@@ -0,0 +1,20 @@
+import requests
+from datetime import datetime
+from fake_useragent import UserAgent
+from pytz import timezone
+
+ua = UserAgent()
+HEADERS = {"User-Agent": ua.chrome}
+
+def watch(url):
+   return requests.get(url, headers=HEADERS).status_code
+
+
+def judge(status_code):
+   eastern = timezone("US/Eastern")
+   now = datetime.now(eastern).strftime("%Y-%m-%d %H:%M")
+   if status_code in (200, 302):
+      return {"status": "up", "timestamp": now}
+   else:
+      return {"status": "down", "timestamp": now}
+
