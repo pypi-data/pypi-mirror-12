7941d2577dda6413474de7ea81f95861ca0102b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:25 +0000

    Add package appium_selector/1.0.20/appium_selector-1.0.20.tar.gz

---
+++ packages/appium_selector/appium_selector-1.0.20.tar.gz/appium_selector-1.0.20/appium_selector/CapGenerators/SauceWeb.py
@@ -0,0 +1,31 @@
+from appium_selector.CapGenerators.Caps import Caps
+
+
+class SauceWeb(Caps):
+
+    sauceNode = []
+    options = {}
+    caps = {}
+
+    def __init__(self, environment):
+        self.env = environment
+
+    def displayString(self):
+        platform = self.env.find('platformName').text
+        browser = self.env.find('browserName').text
+        version = self.env.find('version').text
+        return "Sauce -- %s -- %s -- %s" % (platform, browser, version)
+
+    def desiredCaps(self, mustard=True):
+        self.options['provider'] = 'sauceWeb'
+        self.options['manufacturer'] = self.env.find('platformName').text
+        self.options['model'] = self.env.find('browserName').text
+        self.options['osv'] = self.env.find('version').text
+        self.options['mustard'] = mustard
+
+        self.caps['platform'] = self.env.find('platformName').text
+        self.caps['browserName'] = self.env.find('browserName').text
+        self.caps['version'] = self.env.find('version').text
+        self.caps['udid'] = "%s  --  %s  --  %s" % (self.env.find('platformName').text, self.env.find('browserName').text, self.env.find('version').text)
+        self.caps['deviceName'] = "%s  --  %s  --  %s" % (self.env.find('platformName').text, self.env.find('browserName').text, self.env.find('version').text)
+        return {'desiredCaps': self.caps, 'options': self.options}
