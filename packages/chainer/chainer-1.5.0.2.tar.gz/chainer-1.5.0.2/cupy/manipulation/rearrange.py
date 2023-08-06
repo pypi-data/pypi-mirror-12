0e7fd1e3f00803f7243bcef56c04e23a2d7c5951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:34 +0000

    Add package chainer/1.5.0.2/chainer-1.5.0.2.tar.gz

---
+++ packages/chainer/chainer-1.5.0.2.tar.gz/chainer-1.5.0.2/cupy/manipulation/rearrange.py
@@ -0,0 +1,13 @@
+# flake8: NOQA
+# "flake8: NOQA" to suppress warning "H104  File contains nothing but comments"
+
+# TODO(okuta): Implement fliplr
+
+
+# TODO(okuta): Implement flipud
+
+
+# TODO(okuta): Implement roll
+
+
+# TODO(okuta): Implement rot90
