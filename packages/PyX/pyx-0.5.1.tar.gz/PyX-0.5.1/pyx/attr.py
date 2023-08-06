568c81dae1e5e5d2eab18ac1a5326a9c5378e61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:55 +0000

    Add package PyX/0.5.1/pyx-0.5.1.tar.gz

---
+++ packages/PyX/pyx-0.5.1.tar.gz/PyX-0.5.1/pyx/attr.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+#
+#
+# Copyright (C) 2003-2004 Jörg Lehmann <joergl@users.sourceforge.net>
+# Copyright (C) 2003-2004 Michael Schindler <m-schindler@users.sourceforge.net>
+# Copyright (C) 2003-2004 André Wobst <wobsta@users.sourceforge.net>
+#
+# This file is part of PyX (http://pyx.sourceforge.net/).
+#
+# PyX is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# PyX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyX; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#
+# some helper functions for the attribute handling
+#
+
+def mergeattrs(attrs, defaults=[]):
+    """perform merging of the attribute list attrs as defined by the
+    merge methods of the attributes"""
+    newattrs = []
+    if attrs is None:
+        return None
+    # XXX Should we skip merging of the default attributes?
+    for a in defaults:
+        newattrs = a.merge(newattrs)
+    for a in attrs:
+        if a is None:
+            return None
+        # XXX Do we really need this test?
+        if isinstance(a, attr):
+            newattrs = a.merge(newattrs)
+        else:
+            raise TypeError("only instances of class attr.attr are allowed")
+    return newattrs
+
+
+def getattrs(attrs, getclasses):
+    """return all attributes in the attribute list attrs, which are
+    instances of one of the classes in getclasses"""
+    result = []
+    try:
+        for attr in attrs:
+            if isinstance(attr, getclasses):
+                result.append(attr)
+    except TypeError: # workaround for Python 2.1 and older
+        for attr in attrs:
+            for getclass in getclasses:
+                if isinstance(attr, getclass):
+                    result.append(attr)
+                    break
+    return result
+
+
+def checkattrs(attrs, allowedclasses):
+    """check whether only attributes which are instances of classes in
+    allowedclasses are present in the attribute list attrs; if not it
+    raises a TypeError"""
+    if len(attrs) != len(getattrs(attrs, allowedclasses)):
+        for attr1, attr2 in zip(attrs, getattrs(attrs, allowedclasses)):
+            if attr1 is not attr2:
+                raise TypeError("instance %r not allowed" % attr1)
+        else:
+            raise TypeError("instance %r not allowed" % attrs[len(getattrs(attrs, allowedclasses))])
+
+#
+# attr class and simple descendants
+#
+
+class attr:
+
+    """ attr is the base class of all attributes, i.e., colors, decorators,
+    styles, text attributes and trafos"""
+
+    def merge(self, attrs):
+        """merge self into list of attrs
+
+        self may either be appended to attrs or inserted at a proper position
+        immediately before a dependent attribute. Attributes of the same type
+        should be removed, if redundant. Note that it is safe to modify
+        attrs."""
+
+        attrs.append(self)
+        return attrs
+
+
+class exclusiveattr(attr):
+
+    """an attribute which swallows all but the last of the same type (specified
+    by the exlusiveclass argument to the constructor) in an attribute list"""
+
+    def __init__(self, exclusiveclass):
+        self.exclusiveclass = exclusiveclass
+
+    def merge(self, attrs):
+        attrs = [attr for attr in attrs if not isinstance(attr, self.exclusiveclass)]
+        attrs.append(self)
+        return attrs
+
+
+class sortbeforeattr(attr):
+
+    """an attribute which places itself previous to all attributes given
+    in the beforetheclasses argument to the constructor"""
+
+    def __init__(self, beforetheclasses):
+        self.beforetheclasses = beforetheclasses
+
+    def merge(self, attrs):
+        first = 1
+        result = []
+        try:
+            for attr in attrs:
+                if first and isinstance(attr, self.beforetheclasses):
+                    result.append(self)
+                    first = 0
+                result.append(attr)
+        except TypeError: # workaround for Python 2.1 and older
+            for attr in attrs:
+                if first:
+                    for dependedclass in self.beforetheclasses:
+                        if isinstance(attr, dependedclass):
+                            result.append(self)
+                            first = 0
+                            break
+                result.append(attr)
+        if first:
+            result.append(self)
+        return result
+
+
+class sortbeforeexclusiveattr(attr):
+
+    """an attribute which swallows all but the last of the same type (specified
+    by the exlusiveclass argument to the constructor) in an attribute list and
+    places itself previous to all attributes given in the beforetheclasses
+    argument to the constructor"""
+
+    def __init__(self, exclusiveclass, beforetheclasses):
+        self.exclusiveclass = exclusiveclass
+        self.beforetheclasses = beforetheclasses
+
+    def merge(self, attrs):
+        first = 1
+        result = []
+        try:
+            for attr in attrs:
+                if first and isinstance(attr, self.beforetheclasses):
+                    result.append(self)
+                    first = 0
+                if not isinstance(attr, self.exclusiveclass):
+                    result.append(attr)
+        except TypeError: # workaround for Python 2.1 and older
+            for attr in attrs:
+                if first:
+                    for dependedclass in self.beforetheclasses:
+                        if isinstance(attr, dependedclass):
+                            result.append(self)
+                            first = 0
+                            break
+                if not isinstance(attr, self.exclusiveclass):
+                    result.append(attr)
+        if first:
+            result.append(self)
+        return result
+
+
+class clearclass(attr):
+
+    """a special attribute which allows to remove all predecessing attributes of
+    the same type in an attribute list"""
+
+    def __init__(self, clearclass):
+        self.clearclass = clearclass
+
+    def merge(self, attrs):
+        return [attr for attr in attrs if not isinstance(attr, self.clearclass)]
+
+
+# XXX is _clear a good choice?
+
+class _clear(attr):
+
+    """a special attribute which removes all predecessing attributes
+    in an attribute list"""
+
+    def merge(self, attrs):
+        return []
+
+# we define the attribute "clear", an instance of "_clear",
+# which can be used to remove all predecessing attributes
+# in an attribute list
+
+clear = _clear()
+
+#
+# changeable attrs
+#
+
+def select(attrs, index, total):
+    """performs select calls for all changeable attributes and
+    returns the resulting attribute list
+    - attrs should be a list containing attributes and changeable
+      attributes
+    - index should be an unsigned integer
+    - total should be a positive number
+    - valid sections fullfill 0<=index<total"""
+    result = []
+    for a in attrs:
+        if isinstance(a, changeattr):
+            result.append(a.select(index, total))
+        else:
+            result.append(a)
+
+
+class changeattr:
+
+    """changeattr is the base class of all changeable attributes"""
+
+    def select(self, index, total):
+        """returns an attribute for a given index out of a total number
+        if attributes to be provided
+        - index should be an unsigned integer
+        - total should be a positive number
+        - valid selections fullfill 0 <= index < total
+        - the select method may raise a ValueError, when the
+          changeable attribute does not allow for a requested
+          selection"""
+
+        raise RuntimeError("not implemented")
+
+
+class changelist(changeattr):
+
+    """a changeable attribute over a list of attribute choises"""
+
+    def __init__(self, attrs, restartable=1):
+        """initializes the instance
+        - attrs is a list of attributes to cycle
+        - restartable is a boolean, allowing to start from
+          the beginning after the end was reached; otherwise
+          selecting beyond the list returns a None"""
+        self.attrs = attrs
+        self.restartable = restartable
+
+    def select(self, index, total):
+        if self.restartable:
+            return self.attrs[index % length(self.attrs)]
+        elif index < length(self.attrs):
+            return self.attrs[index]
+
---
+++ packages/PyX/pyx-0.5.1.tar.gz/PyX-0.5.1/pyx/connector.py
@@ -0,0 +1,371 @@
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+#
+#
+# Copyright (C) 2003-2004 Michael Schindler <m-schindler@users.sourceforge.net>
+#
+# This file is part of PyX (http://pyx.sourceforge.net/).
+#
+# PyX is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# PyX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyX; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+import math
+from math import pi, sin, cos, atan2, tan, hypot, acos, sqrt
+import path, trafo, unit, helper
+try:
+    from math import radians, degrees
+except ImportError:
+    # fallback implementation for Python 2.1 and below
+    def radians(x): return x*pi/180
+    def degrees(x): return x*180/pi
+
+
+#########################
+##   helpers
+#########################
+
+def _topt(length, default_type=None):
+    if length is None: return None
+    if default_type is not None:
+        return unit.topt(unit.length(length, default_type=default_type))
+    else:
+        return unit.topt(unit.length(length))
+
+class _connector(path.normpath):
+
+    def omitends(self, box1, box2):
+        """intersect a path with the boxes' paths"""
+
+        sp = self.intersect(box1.path())[0]
+        try: self.path = self.split(sp[:1])[1].path
+        except: pass
+
+        sp = self.intersect(box2.path())[0]
+        try: self.path = self.split(sp[-1:])[0].path
+        except: pass
+
+    def shortenpath(self, dists):
+        """shorten a path by the given distances"""
+
+        center = [unit.topt(self.begin()[i]) for i in [0,1]]
+        sp = self.intersect( path.circle_pt(center[0], center[1], dists[0]) )[0]
+        try: self.path = self.split(sp[:1])[1].path
+        except: pass
+
+        center = [unit.topt(self.end()[i]) for i in [0,1]]
+        sp = self.intersect( path.circle_pt(center[0], center[1], dists[1]) )[0]
+        try: self.path = self.split(sp[-1:])[0].path
+        except: pass
+
+
+################
+## classes
+################
+
+
+class _line(_connector):
+
+    def __init__(self, box1, box2, boxdists=[0,0]):
+
+        self.box1 = box1
+        self.box2 = box2
+
+        _connector.__init__(self,
+            path.moveto_pt(*self.box1.center),
+            path.lineto_pt(*self.box2.center))
+
+        self.omitends(box1, box2)
+        self.shortenpath(boxdists)
+
+
+class _arc(_connector):
+
+    def __init__(self, box1, box2, relangle=45,
+                 absbulge=None, relbulge=None, boxdists=[0,0]):
+
+        self.box1 = box1
+        self.box2 = box2
+
+        rel = [self.box2.center[0] - self.box1.center[0],
+               self.box2.center[1] - self.box1.center[1]]
+        distance = hypot(*rel)
+
+        # usage of bulge overrides the relangle parameter
+        if relbulge is not None or absbulge is not None:
+            relangle = None
+            bulge = 0
+            try: bulge += absbulge
+            except: pass
+            try: bulge += relbulge*distance
+            except: pass
+
+            try: radius = abs(0.5 * (bulge + 0.25 * distance**2 / bulge))
+            except: radius = 10 * distance # default value for too straight arcs
+            radius = min(radius, 10 * distance)
+            center = 2.0*(radius-abs(bulge))/distance
+            center *= 2*(bulge>0.0)-1
+        # otherwise use relangle
+        else:
+            bulge=None
+            try: radius = 0.5 * distance / abs(cos(0.5*math.pi - radians(relangle)))
+            except: radius = 10 * distance
+            try: center = tan(0.5*math.pi - radians(relangle))
+            except: center = 0
+
+        # up to here center is only
+        # the distance from the middle of the straight connection
+        center = [0.5 * (self.box1.center[0] + self.box2.center[0] - rel[1]*center),
+                  0.5 * (self.box1.center[1] + self.box2.center[1] + rel[0]*center)]
+        angle1 = atan2(*[self.box1.center[i] - center[i]  for i in [1,0]])
+        angle2 = atan2(*[self.box2.center[i] - center[i]  for i in [1,0]])
+
+        # draw the arc in positive direction by default
+        # negative direction if relangle<0 or bulge<0
+        if (relangle is not None and relangle < 0) or (bulge is not None and bulge < 0):
+            _connector.__init__(self,
+                path.moveto_pt(*self.box1.center),
+                path.arcn_pt(center[0], center[1], radius, degrees(angle1), degrees(angle2)))
+        else:
+            _connector.__init__(self,
+                path.moveto_pt(*self.box1.center),
+                path.arc_pt(center[0], center[1], radius, degrees(angle1), degrees(angle2)))
+
+        self.omitends(box1, box2)
+        self.shortenpath(boxdists)
+
+
+class _curve(_connector):
+
+    def __init__(self, box1, box2,
+                 relangle1=45, relangle2=45,
+                 absangle1=None, absangle2=None,
+                 absbulge=0, relbulge=0.39, boxdists=[0,0]):
+        # relbulge=0.39 and relangle1,2=45 leads
+        # approximately to the arc with angle=45
+
+        self.box1 = box1
+        self.box2 = box2
+
+        rel = [self.box2.center[0] - self.box1.center[0],
+               self.box2.center[1] - self.box1.center[1]]
+        distance = hypot(*rel)
+        dangle = atan2(rel[1], rel[0])
+
+        # calculate the armlength and angles for the control points
+        bulge = abs(distance*relbulge + absbulge)
+
+        if absangle1 is not None:
+            angle1 = radians(absangle1)
+        else:
+            angle1 = dangle - radians(relangle1)
+        if absangle2 is not None:
+            angle2 = radians(absangle2)
+        else:
+            angle2 = dangle + radians(relangle2)
+
+        control1 = [cos(angle1), sin(angle1)]
+        control2 = [cos(angle2), sin(angle2)]
+        control1 = [self.box1.center[i] + control1[i] * bulge  for i in [0,1]]
+        control2 = [self.box2.center[i] - control2[i] * bulge  for i in [0,1]]
+
+        _connector.__init__(self,
+            path.moveto_pt(*self.box1.center),
+            path.curveto_pt(*(control1 + control2 + helper.ensurelist(self.box2.center))))
+
+        self.omitends(box1, box2)
+        self.shortenpath(boxdists)
+
+
+class _twolines(_connector):
+
+    def __init__(self, box1, box2,
+                 absangle1=None, absangle2=None,
+                 relangle1=None, relangle2=None, relangleM=None,
+                 length1=None, length2=None,
+                 bezierradius=None, beziersoftness=1,
+                 arcradius=None,
+                 boxdists=[0,0]):
+
+        self.box1 = box1
+        self.box2 = box2
+
+        begin = self.box1.center
+        end = self.box2.center
+        rel = [self.box2.center[0] - self.box1.center[0],
+               self.box2.center[1] - self.box1.center[1]]
+        distance = hypot(*rel)
+        dangle = atan2(rel[1], rel[0])
+
+        if relangle1 is not None: relangle1 = radians(relangle1)
+        if relangle2 is not None: relangle2 = radians(relangle2)
+        if relangleM is not None: relangleM = radians(relangleM)
+        # absangle has priority over relangle:
+        if absangle1 is not None: relangle1 = dangle - radians(absangle1)
+        if absangle2 is not None: relangle2 = math.pi - dangle + radians(absangle2)
+
+        # check integrity of arguments
+        no_angles, no_lengths=0,0
+        for anangle in (relangle1, relangle2, relangleM):
+            if anangle is not None: no_angles += 1
+        for alength in (length1, length2):
+            if alength is not None: no_lengths += 1
+
+        if no_angles + no_lengths != 2:
+            raise NotImplementedError, "Please specify exactly two angles or lengths"
+
+        # calculate necessary angles and sidelengths
+        # always length1 and relangle1 !
+        # the case with two given angles
+        if no_angles==2:
+            if relangle1 is None: relangle1 = math.pi - relangle2 - relangleM
+            elif relangle2 is None: relangle2 = math.pi - relangle1 - relangleM
+            elif relangleM is None: relangleM = math.pi - relangle1 - relangle2
+            length1 = distance * abs(sin(relangle2)/sin(relangleM))
+            middle = self._middle_a(begin, dangle, length1, relangle1)
+        # the case with two given lengths
+        elif no_lengths==2:
+            relangle1 = acos((distance**2 + length1**2 - length2**2) / (2.0*distance*length1))
+            middle = self._middle_a(begin, dangle, length1, relangle1)
+        # the case with one length and one angle
+        else:
+            if relangle1 is not None:
+                if length1 is not None:
+                    middle = self._middle_a(begin, dangle, length1, relangle1)
+                elif length2 is not None:
+                    length1 = self._missinglength(length2, distance, relangle1)
+                    middle = self._middle_a(begin, dangle, length1, relangle1)
+            elif relangle2 is not None:
+                if length1 is not None:
+                    length2 = self._missinglength(length1, distance, relangle2)
+                    middle = self._middle_b(end, dangle, length2, relangle2)
+                elif length2 is not None:
+                    middle = self._middle_b(end, dangle, length2, relangle2)
+            elif relangleM is not None:
+                if length1 is not None:
+                    length2 = self._missinglength(distance, length1, relangleM)
+                    relangle1 = acos((distance**2 + length1**2 - length2**2) / (2.0*distance*length1))
+                    middle = self._middle_a(begin, dangle, length1, relangle1)
+                elif length2 is not None:
+                    length1 = self._missinglength(distance, length2, relangleM)
+                    relangle1 = acos((distance**2 + length1**2 - length2**2) / (2.0*distance*length1))
+                    middle = self._middle_a(begin, dangle, length1, relangle1)
+            else:
+                raise NotImplementedError, "I found a strange combination of arguments"
+
+        _connector.__init__(self,
+            path.moveto_pt(*self.box1.center),
+            path.lineto_pt(*middle),
+            path.lineto_pt(*self.box2.center))
+
+        self.omitends(box1, box2)
+        self.shortenpath(boxdists)
+
+    def _middle_a(self, begin, dangle, length1, angle1):
+        a = dangle - angle1
+        dir = [cos(a), sin(a)]
+        return [begin[i] + length1*dir[i]  for i in [0,1]]
+
+    def _middle_b(self, end, dangle, length2, angle2):
+        # a = -math.pi + dangle + angle2
+        return self._middle_a(end, -math.pi+dangle, length2, -angle2)
+
+    def _missinglength(self, lenA, lenB, angleA):
+        # calculate lenC, where side A and angleA are opposite
+        tmp1 = lenB*cos(angleA)
+        tmp2 = sqrt(tmp1**2 - lenB**2 + lenA**2)
+        if tmp1>tmp2: return tmp1-tmp2
+        return tmp1+tmp2
+
+
+
+class line(_line):
+
+    """a line is the straight connector between the centers of two boxes"""
+
+    def __init__(self, box1, box2, boxdists=[0,0]):
+
+        boxdists_pt = [_topt(helper.getitemno(boxdists,i), default_type="v") for i in [0,1]]
+
+        _line.__init__(self, box1, box2, boxdists=boxdists_pt)
+
+
+class curve(_curve):
+
+    """a curve is the curved connector between the centers of two boxes.
+    The constructor needs both angle and bulge"""
+
+
+    def __init__(self, box1, box2,
+                 relangle1=45, relangle2=45,
+                 absangle1=None, absangle2=None,
+                 absbulge=0, relbulge=0.39,
+                 boxdists=[0,0]):
+
+        boxdists_pt = [_topt(helper.getitemno(boxdists,i), default_type="v") for i in [0,1]]
+
+        _curve.__init__(self, box1, box2,
+                        relangle1=relangle1, relangle2=relangle2,
+                        absangle1=absangle1, absangle2=absangle2,
+                        absbulge=_topt(absbulge), relbulge=relbulge,
+                        boxdists=boxdists_pt)
+
+class arc(_arc):
+
+    """an arc is a round connector between the centers of two boxes.
+    The constructor gets
+         either an angle in (-pi,pi)
+         or a bulge parameter in (-distance, distance)
+           (relbulge and absbulge are added)"""
+
+    def __init__(self, box1, box2, relangle=45,
+                 absbulge=None, relbulge=None, boxdists=[0,0]):
+
+        boxdists_pt = [_topt(helper.getitemno(boxdists,i), default_type="v") for i in [0,1]]
+
+        _arc.__init__(self, box1, box2,
+                      relangle=relangle,
+                      absbulge=_topt(absbulge), relbulge=relbulge,
+                      boxdists=boxdists_pt)
+
+
+class twolines(_twolines):
+
+    """a twolines is a connector consisting of two straight lines.
+    The construcor takes a combination of angles and lengths:
+      either two angles (relative or absolute)
+      or two lenghts
+      or one length and one angle"""
+
+    def __init__(self, box1, box2,
+                 absangle1=None, absangle2=None,
+                 relangle1=None, relangle2=None, relangleM=None,
+                 length1=None, length2=None,
+                 bezierradius=None, beziersoftness=1,
+                 arcradius=None,
+                 boxdists=[0,0]):
+
+        boxdists_pt = [_topt(helper.getitemno(boxdists,i), default_type="v") for i in [0,1]]
+
+        _twolines.__init__(self, box1, box2,
+                       absangle1=absangle1, absangle2=absangle2,
+                       relangle1=relangle1, relangle2=relangle2,
+                       relangleM=relangleM,
+                       length1=_topt(length1), length2=_topt(length2),
+                       bezierradius=_topt(bezierradius), beziersoftness=1,
+                       arcradius=_topt(arcradius),
+                       boxdists=boxdists_pt)
+
+
+
