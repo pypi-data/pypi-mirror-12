6fa4d873c9c4fc6bb11d824458571bd045b0e296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:21 +0000

    Add package guessit/2.0b1/guessit-2.0b1.tar.gz

---
+++ packages/guessit/guessit-2.0b1.tar.gz/guessit-2.0b1/guessit/rules/properties/mimetype.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+mimetype property
+"""
+from __future__ import unicode_literals
+
+import mimetypes
+from guessit.rules.processors import Processors
+from rebulk import Rebulk, CustomRule, POST_PROCESS
+
+from rebulk.match import Match
+
+
+def mimetype():
+    """
+    Builder for rebulk object.
+    :return: Created Rebulk object
+    :rtype: Rebulk
+    """
+    return Rebulk().rules(Mimetype)
+
+
+class Mimetype(CustomRule):
+    """
+    Mimetype post processor
+    :param matches:
+    :type matches:
+    :return:
+    :rtype:
+    """
+    priority = POST_PROCESS
+
+    dependency = Processors
+
+    def when(self, matches, context):
+        mime, _ = mimetypes.guess_type(matches.input_string, strict=False)
+        return mime
+
+    def then(self, matches, when_response, context):
+        mime = when_response
+        matches.append(Match(len(matches.input_string), len(matches.input_string), name='mimetype', value=mime))
