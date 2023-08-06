1982a81fd3d16fda9c7bad5873948c3f9b41b065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:26 +0000

    Add package python-morfeusz/0.3300/python-morfeusz-0.3300.tar.gz

---
+++ packages/python-morfeusz/python-morfeusz-0.3300.tar.gz/python-morfeusz-0.3300/setup.py
@@ -0,0 +1,47 @@
+'''
+*python-morfeusz* is a set of bindings for Morfeusz_,
+a Polish morphological analyser.
+
+.. _Morfeusz:
+   http://sgjp.pl/morfeusz/
+'''
+
+classifiers = '''
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Natural Language :: Polish
+Operating System :: POSIX :: Linux
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 3
+Topic :: Text Processing :: Linguistic
+'''.strip().splitlines()
+
+import distutils.core
+
+def get_version():
+    file = open('morfeusz.py')
+    try:
+        for line in file:
+            if line.startswith('__version__ ='):
+                version = line.split('=', 1)[1]
+                return eval(version, {}, {})
+    finally:
+        file.close()
+    raise IOError('Unexpected end-of-file')
+
+distutils.core.setup(
+    name='python-morfeusz',
+    version=get_version(),
+    license='MIT',
+    description='bindings for Morfeusz',
+    long_description=__doc__.strip(),
+    classifiers=classifiers,
+    url='http://jwilk.net/software/python-morfeusz',
+    author='Jakub Wilk',
+    author_email='jwilk@jwilk.net',
+    py_modules=['morfeusz']
+)
+
+# vim:ts=4 sts=4 sw=4 et
