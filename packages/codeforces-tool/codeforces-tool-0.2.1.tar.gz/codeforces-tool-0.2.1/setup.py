b484462c333c2bb8175ab45f1bbf17bb9b81aac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:11 +0000

    Add package codeforces-tool/0.2.1/codeforces-tool-0.2.1.tar.gz

---
+++ packages/codeforces-tool/codeforces-tool-0.2.1.tar.gz/codeforces-tool-0.2.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+ 
+ 
+"""setup.py: setuptools control."""
+ 
+ 
+from setuptools import setup
+ 
+setup(
+    name = "codeforces-tool",
+    packages = ["cftool"],
+    entry_points = {
+        "console_scripts": ['cf = cftool.tool:main']
+    },
+    version = "0.2.1",
+    description = "Codeforces tool python command line application.",
+    long_description = "No description yet",
+    author = "Roberto Sales",
+    author_email = "iseme.beto@gmail.com",
+    package_data = {
+        "":["config.json", "template.cpp"]
+    },
+    install_requires = [
+        "colorama",
+        "requests",
+        "pyquery",
+        "prettytable",
+        "mechanize",
+        "ago"
+    ]
+)
