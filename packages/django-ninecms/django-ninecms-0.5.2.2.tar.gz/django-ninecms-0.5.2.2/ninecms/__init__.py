b0f5f4309d23bb0f79c0b7dd55e1b6885da0fde9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:55 +0000

    Add package django-ninecms/0.5.2.2/django-ninecms-0.5.2.2.tar.gz

---
+++ packages/django-ninecms/django-ninecms-0.5.2.2.tar.gz/django-ninecms-0.5.2.2/ninecms/__init__.py
@@ -0,0 +1,8 @@
+""" NineCMS package """
+__version__ = '0.5.2.2'
+__author__ = 'George Karakostas'
+__copyright__ = 'Copyright 2015, George Karakostas'
+__licence__ = 'BSD-3'
+__email__ = 'gkarak@9-dev.com'
+
+default_app_config = 'ninecms.apps.NineCMSConfig'
