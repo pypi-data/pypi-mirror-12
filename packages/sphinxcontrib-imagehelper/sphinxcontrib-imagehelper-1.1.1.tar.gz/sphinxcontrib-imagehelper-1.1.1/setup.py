fa6779008bf1238f5ab9710592f0fa5f5c622245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:31 +0000

    Add package sphinxcontrib-imagehelper/1.1.1/sphinxcontrib-imagehelper-1.1.1.tar.gz

---
+++ packages/sphinxcontrib-imagehelper/sphinxcontrib-imagehelper-1.1.1.tar.gz/sphinxcontrib-imagehelper-1.1.1/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+long_desc = open('README.rst').read()
+
+requires = ['Sphinx>=0.6']
+
+setup(
+    name='sphinxcontrib-imagehelper',
+    version='1.1.1',
+    url='https://github.com/tk0miya/sphinxcontrib-imagehelper',
+    license='BSD',
+    author='Takeshi KOMIYA',
+    author_email='i.tkomiya@gmail.com',
+    description='Sphinx extension helper about images',
+    long_description=long_desc,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Documentation',
+        'Topic :: Documentation :: Sphinx',
+    ],
+    platforms='any',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
