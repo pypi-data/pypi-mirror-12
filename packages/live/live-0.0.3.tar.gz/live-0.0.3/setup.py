5cd772777c25877559ed7e4535771e3dd86fbf4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:36 +0000

    Add package live/0.0.3/live-0.0.3.tar.gz

---
+++ packages/live/live-0.0.3.tar.gz/live-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+__version__ = '0.0.3'
+
+try:
+    with open('README.rst') as f:
+        long_description = f.read()
+except IOError:
+    long_description = 'Simple library to support live coding'
+
+config = {
+    'name': 'live',
+    'version': __version__,
+    'description': 'Simple library to support live coding',
+    'long_description': long_description,
+    'author': 'Fritz Obermeyer',
+    'author_email': 'fritz.obermeyer@gmail.com',
+    'url': 'https://github.com/fritzo/livecoder',
+    'py_modules': ['live'],
+    'entry_points': {'console_scripts': ['live = live.__main__:main']},
+}
+
+setup(**config)
