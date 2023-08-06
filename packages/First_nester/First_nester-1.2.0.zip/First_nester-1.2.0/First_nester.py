2f92c975bc8a2d9a9c8bfee7ea2171ea0724b3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:01 +0000

    Add package First_nester/1.2.0/First_nester-1.2.0.zip

---
+++ packages/First_nester/First_nester-1.2.0.zip/First_nester-1.2.0/First_nester.py
@@ -0,0 +1,11 @@
+"""这是一个“nestpy”模块，提供了一个print_list()的函数，这个函数作用是打印列表，可能包含（也可能不包含）嵌套列表。"""
+def print_list(the_list,level=0):
+       """这个函数取一个位置参数，名为the_list，这是可以在任意Python列表（也可以在包含嵌套列表的列表）。所指定的列表中的每个数据项会递归地输出到屏幕上，每个数据项各占一行。"""
+       for every_one in the_list:
+              if isinstance(every_one,list):
+                     print_list(every_one,level+1)
+              else:
+                     for tab_stop in range(level):
+                            print("\t",end='')
+                     print(every_one)
+                     
