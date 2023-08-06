36b3fb68fba8286f801769eb09daf07451e533db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:33 +0000

    Add package PiStorms/1.3/PiStorms-1.6.tar.gz

---
+++ packages/PiStorms/PiStorms-1.6.tar.gz/PiStorms-1.6/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PiStorms',
+    version='1.6',
+    description='PiStorms libraries',
+    author='mindsensors.com',
+    author_email='contact@mindsensors.com',
+    url='http://www.mindsensors.com',
+    py_modules=['PiStorms', 'mindsensors', 'PiStormsBrowser', 'PiStormsCom', 'PiStormsDriver', 'ps_messenger_check'],
+    data_files=[('mindsensors_images', ['btns_center.png', 'btns_left.png', 'btns_right.png', 'button.png', 'dialogbg.png', 'Exclamation-mark-icon.png', 'Pane1.png']),
+    ('/etc/init.d', ['PiStormsDriver.sh', 'PiStormsBrowser.sh'])],
+    install_requires=['mindsensors_i2c', 'mindsensorsUI', 'RPi.GPIO'],
+    )
