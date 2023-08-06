b94cec20a616b371ac1de8e96b985175202f1232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:38 +0000

    Add package nestercrs1000/1.2.0/nestercrs1000-1.2.0.zip

---
+++ packages/nestercrs1000/nestercrs1000-1.2.0.zip/nestercrs1000-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name			= 'nestercrs1000',
+	version			= '1.2.0',
+	py_modules		= ['nester'],
+	author			= 'c.stevens',
+	author_email		= 'crstevens1000@hotmail.com',
+	description		= 'A simple printer of nested lists',
+)
