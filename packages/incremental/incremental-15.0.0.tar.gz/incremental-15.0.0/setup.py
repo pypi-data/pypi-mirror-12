bf37326a9a3351b4e32c6e4e758044a47e23ed8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:41 +0000

    Add package incremental/15.0.0/incremental-15.0.0.tar.gz

---
+++ packages/incremental/incremental-15.0.0.tar.gz/incremental-15.0.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
+import os, sys
+
+from setuptools import setup
+
+base_dir = os.path.dirname(__file__)
+src_dir = os.path.join(base_dir, "src")
+
+# We need to import outselves
+sys.path.insert(0, src_dir)
+
+import incremental
+
+setup(
+    name='incremental',
+    version=incremental.__version__.base(),
+    maintainer='Amber Brown',
+    maintainer_email='hawkowl@twistedmatrix.com',
+    url="https://github.com/hawkowl/incremental",
+    classifiers = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+    package_dir={"": "src"},
+    py_modules=['incremental'],
+    license="MIT",
+    long_description=open('README.rst').read()
+)
