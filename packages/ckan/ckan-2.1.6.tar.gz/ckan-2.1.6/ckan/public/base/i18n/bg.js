a7423c661fe72f5a5b2cff5d043b028577359fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:36 +0000

    Add package ckan/2.1.6/ckan-2.1.6.tar.gz

---
+++ packages/ckan/ckan-2.1.6.tar.gz/ckan-2.1.6/ckan/public/base/i18n/bg.js
@@ -0,0 +1,51 @@
+{
+  "": {
+    "domain": "ckan",
+    "lang": "bg",
+    "plural-forms": "nplurals=2; plural=(n != 1);"
+  },
+  "An Error Occurred": [
+    null,
+    "Възникна грешка"
+  ],
+  "Cancel": [
+    null,
+    "Отказ"
+  ],
+  "Confirm": [
+    null,
+    "Потвърди"
+  ],
+  "Edit": [
+    null,
+    "Редакция"
+  ],
+  "Follow": [
+    null,
+    "Следвай"
+  ],
+  "Hide": [
+    null,
+    "Скрий"
+  ],
+  "Loading...": [
+    null,
+    "Зареждане...."
+  ],
+  "URL": [
+    null,
+    "URL"
+  ],
+  "Upload a file": [
+    null,
+    "Качване на файл"
+  ],
+  "show less": [
+    null,
+    "покажи по-малко"
+  ],
+  "show more": [
+    null,
+    "покажи повече"
+  ]
+}
