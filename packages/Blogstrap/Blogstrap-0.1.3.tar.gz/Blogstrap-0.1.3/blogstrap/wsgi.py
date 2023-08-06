c9fb68ef51aba5fc3b864cfc1176622662e3a3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:28 +0000

    Add package Blogstrap/0.1.3/Blogstrap-0.1.3.tar.gz

---
+++ packages/Blogstrap/Blogstrap-0.1.3.tar.gz/Blogstrap-0.1.3/blogstrap/wsgi.py
@@ -0,0 +1,3 @@
+from blogstrap import create_app
+
+application = create_app('.blogstrap.conf')
---
+++ packages/Blogstrap/Blogstrap-0.1.3.tar.gz/Blogstrap-0.1.3/setup.py
@@ -0,0 +1,21 @@
+"""A dumb blogging platform based on Strapdown.js
+"""
+
+from setuptools import setup
+
+repo_url="http://github.com/joehakimrahme/blogstrap"
+
+setup(
+    name='Blogstrap',
+    author="Joe H. Rahme",
+    author_email="joehakimrahme@gmail.com",
+    version='0.1.3',
+    description="A dumb blogging platform based on Strapdown.js",
+    url=repo_url,
+    download_url=repo_url + "/tarball/0.1.3",
+    long_description=__doc__,
+    packages=['blogstrap'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask']
+)
