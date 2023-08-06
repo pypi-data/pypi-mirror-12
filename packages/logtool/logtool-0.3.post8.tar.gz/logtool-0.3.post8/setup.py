0ce1b0a818e2b8dd4e3a6401ebd421610ab53f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:48 +0000

    Add package logtool/0.3.post8/logtool-0.3.post8.tar.gz

---
+++ packages/logtool/logtool-0.3.post8.tar.gz/logtool-0.3.post8/setup.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+import versioneer
+
+setup (
+    name = "logtool",
+    version = versioneer.get_version (),
+    description = "Methods and tools that assist logging.",
+    long_description = file ("README.rst").read (),
+    cmdclass = versioneer.get_cmdclass (),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: "
+        + "GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Topic :: Utilities",
+    ],
+    keywords = "logging, exceptions, callpoints, call tracing",
+    author = "J C Lawrence",
+    author_email = "claw@kanga.nu",
+    url = "https://github.com/clearclaw/logtool",
+    license = "LGPL v3.0",
+    test_suite = "tests",
+    packages = find_packages (exclude = ["tests",]),
+    package_data = {},
+    data_files = [],
+    zip_safe = False,
+    install_requires = [line.strip ()
+        for line in file ("requirements.txt").readlines ()],
+    entry_points = {
+        "console_scripts": [
+        ],
+    },
+)
