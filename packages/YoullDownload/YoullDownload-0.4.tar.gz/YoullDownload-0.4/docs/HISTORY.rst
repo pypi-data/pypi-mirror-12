b7a1b32da6631cc7175bb4307a3979aaacb39b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:51 +0000

    Add package YoullDownload/0.4/YoullDownload-0.4.tar.gz

---
+++ packages/YoullDownload/YoullDownload-0.4.tar.gz/YoullDownload-0.4/docs/HISTORY.rst
@@ -0,0 +1,38 @@
+Changelog
+=========
+
+0.4 (2015-11-06)
+----------------
+
+- Fix setup.py to properly create a package
+  [ale-rt]
+
+
+0.3 (2015-05-28)
+----------------
+
+- Remove duplicated URLs from final report
+  [keul]
+- Do not include same version of an URL with anchors
+  [keul]
+- Inspect also resources from CSS (backgroun images, fonts, ...)
+  [keul]
+- Script was not properly working outside homepage if a "base" tag
+  was not provided
+  [keul]
+
+0.2 (2014-04-02)
+----------------
+
+- Added support for ``src`` attribute of ``iframe`` tag
+  [keul]
+- Added support for ``src`` attribute of ``source`` tag
+  (HTML 5 video element)
+  [keul]
+- Do not break if ``base`` tag is not present
+  [keul]
+
+0.1 (2013-01-30)
+----------------
+
+- initial release
---
+++ packages/YoullDownload/YoullDownload-0.4.tar.gz/YoullDownload-0.4/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import sys, os
+
+setup(name='YoullDownload',
+      # scripts=['src/youlldownload.py',],
+      version='0.4',
+      description="Grab from a remote site page all resources that a browser "
+                  "will probably download visiting the page",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
+      classifiers=["Development Status :: 4 - Beta",
+                   "License :: OSI Approved :: GNU General Public License (GPL)",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 2.7",
+                   "Topic :: Utilities",
+                   "Topic :: Internet :: WWW/HTTP",
+                   ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='crawler log web',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.it',
+      url='https://github.com/RedTurtle/YoullDownload',
+      license='GPL',
+      packages=find_packages('src', exclude=['ez_setup',]),
+      py_modules=['youlldownload',],
+      package_dir = {'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'requests',
+          'pyquery',
+      ],
+      entry_points={'console_scripts': ['youlldownload = YoullDownload.youlldownload:main', ]}
+      )
