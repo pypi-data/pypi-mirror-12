590283cf16cf1f55e3db38e8ab49e2760501796a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:35 +0000

    Add package MotherNature/0.1.2/MotherNature-0.1.2.tar.gz

---
+++ packages/MotherNature/MotherNature-0.1.2.tar.gz/MotherNature-0.1.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='MotherNature',
+    version='0.1.2',
+    description='Python Environment Manager',
+    long_description="Manage python environment variables using yaml files.",
+    author='Fauzan Emmerling',
+    author_email='erich@emfeld.com',
+    url='https://github.com/femmerling/mothernature',
+    license='MIT',
+    packages=['mothernature'],
+    install_requires=['pyyaml'],
+    zip_safe=False)
