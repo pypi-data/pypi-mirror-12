29bd3df203ba6d73854b748a09d433cfd44473d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:17 +0000

    Add package tornkts/0.4.3/tornkts-0.4.3.tar.gz

---
+++ packages/tornkts/tornkts-0.4.3.tar.gz/tornkts-0.4.3/tornkts/base/mongodb/base_document.py
@@ -0,0 +1,14 @@
+from mongoengine import document
+from tornkts.mixins.to_dict_mixin import ToDictMixin
+from tornkts.mixins.validate_mixin import ValidateMixin
+
+
+class BaseDocument(ToDictMixin, ValidateMixin, document.Document):
+    meta = {
+        'allow_inheritance': True,
+        'abstract': True
+    }
+
+    def get_id(self):
+        return str(self.pk)
+        
