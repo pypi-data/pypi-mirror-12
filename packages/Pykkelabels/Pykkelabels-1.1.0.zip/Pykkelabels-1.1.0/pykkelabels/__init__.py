167539ab4d75ed5076bce7c5e4fbff21ce664e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:56 +0000

    Add package Pykkelabels/1.1.0/Pykkelabels-1.1.0.zip

---
+++ packages/Pykkelabels/Pykkelabels-1.1.0.zip/Pykkelabels-1.1.0/pykkelabels/__init__.py
@@ -0,0 +1,9 @@
+from .pykkelabels import Pykkelabels
+from .exceptions import *
+
+__author__ = "Anders Brandt"
+__license__ = "MIT"
+__version__ = "1.1.0"
+__maintainer__ = "Anders Brandt"
+__email__ = "anderswb at  gmail dot com"
+__status__ = "Production"
