e354c5d881228155cb814312b9a689a7eb0de65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:12 +0000

    Add package clip.py/0.3.2/clip.py-0.3.2.tar.gz

---
+++ packages/clip.py/clip.py-0.3.2.tar.gz/clip.py-0.3.2/setup.py
@@ -0,0 +1,23 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+setup(
+	name='clip.py',
+	version='0.3.2',
+	url='https://github.com/willyg302/clip.py',
+	license='MIT',
+	author='William Gaul',
+	author_email='willyg302@gmail.com',
+	description='Embeddable, composable command line interface parsing',
+	py_modules=['clip'],
+	include_package_data=True,
+	test_suite='tests',
+	platforms='any',
+	classifiers=[
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.4',
+	],
+)
