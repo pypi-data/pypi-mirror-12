721a8c8c063f517acb389240cb9a2185bf930c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:14 +0000

    Add package python-documentcloud/1.0.4/python-documentcloud-1.0.4.tar.gz

---
+++ packages/python-documentcloud/python-documentcloud-1.0.4.tar.gz/python-documentcloud-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(
+    name='python-documentcloud',
+    version='1.0.4',
+    description='A simple Python wrapper for the DocumentCloud API',
+    author='Ben Welsh',
+    author_email='ben.welsh@gmail.com',
+    url='http://datadesk.github.com/python-documentcloud/',
+    license="MIT",
+    packages=("documentcloud",),
+    test_suite="tests.test_all",
+    include_package_data=True,
+    install_requires=(
+        'python-dateutil>=2.1',
+        'six>=1.4.1',
+        'rfc3987',
+    ),
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ),
+)
