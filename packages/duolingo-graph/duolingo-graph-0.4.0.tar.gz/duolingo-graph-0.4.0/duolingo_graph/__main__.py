ed93b9844b5fe331751d7c99cbbd7915f0a01889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:06 +0000

    Add package duolingo-graph/0.4.0/duolingo-graph-0.4.0.tar.gz

---
+++ packages/duolingo-graph/duolingo-graph-0.4.0.tar.gz/duolingo-graph-0.4.0/duolingo_graph/__main__.py
@@ -0,0 +1,135 @@
+"""Main program for the duolingo-graph command."""
+import click
+from .core import pull_graph, plot
+from .numbers import CourseType
+from .numbers import rank_courses, rank_max_distance, \
+    rank_progress, rank_avg_progress
+
+
+__all__ = ['main']
+
+
+class CLIData(object):
+    """Data used by duolingo-graph command."""
+    __slots__ = ('graph', 'n', 'kind')
+
+
+_HELPS = {
+    'no-hatching': "don't include hatching courses",
+    'no-beta': "don't include hatching or beta courses",
+    'progress': "don't include courses below given progress",
+    'n': "limit number of courses to show (0 for no limit)",
+    'learners': "only rank languages by courses for learners",
+    'speakers': "only rank languages by courses for speakers",
+}
+
+
+@click.group()
+@click.option('--no-hatching', '-h', 'phase', flag_value=2, type=click.INT,
+              help=_HELPS['no-hatching'])
+@click.option('--no-beta', '-b', 'phase', flag_value=3, type=click.INT,
+              help=_HELPS['no-beta'])
+@click.option('--progress', '-p', default=0, type=click.FLOAT,
+              help=_HELPS['progress'])
+@click.pass_context
+def cli(ctx, phase, progress):
+    """Graph the courses on Duolingo."""
+    def load_graph():
+        """Lazy function for loading the graph of languages."""
+        click.echo('Connecting to Duolingo Incubator API...',
+                   nl=False, err=True)
+        graph = pull_graph(phase=phase or 1, progress=progress)
+        click.echo('done.', err=True)
+        return graph
+    ctx.obj.graph = load_graph
+
+
+@cli.group(name='rank-by')
+@click.option('-n', default=0, type=click.IntRange(0, None),
+              help=_HELPS['n'])
+@click.option('--learners', '-l', 'kind', flag_value=CourseType.LEARNERS,
+              help=_HELPS['learners'])
+@click.option('--speakers', '-s', 'kind', flag_value=CourseType.SPEAKERS,
+              help=_HELPS['speakers'])
+@click.pass_context
+def rank_by(ctx, n, kind):
+    """Rank languages by some criteria."""
+    ctx.obj.n = n
+    ctx.obj.kind = kind or CourseType.BOTH
+
+
+def print_ranks(ranks, label, labels, fmt):
+    """
+    Beautifully prints a list that's returned from either rank_courses or
+    rank_progress.
+    """
+    if len(ranks) == 0:
+        return
+    width = max(len(('{:' + fmt + '}').format(amt)) for amt, _ in ranks)
+    for amt, langs in ranks:
+        num = float(amt[0] if isinstance(amt, tuple) else amt)
+        print(('  {:' + str(width) + fmt + '}{} {}').format(
+            amt,
+            labels if num > 1 else label,
+            (',\n' + ' ' * (2 + width + len(labels) + 1)).join(langs)
+        ))
+
+
+@rank_by.command()
+@click.pass_context
+def courses(ctx):
+    """Rank by number of courses."""
+    ranks = rank_courses(ctx.obj.graph(), ctx.obj.kind)
+    print_ranks(
+        ranks if ctx.obj.n == 0 else ranks[:ctx.obj.n],
+        ' course: ', ' courses:', 'd',
+    )
+
+
+@rank_by.command(name='max-distance')
+@click.pass_context
+def max_distance(ctx):
+    """Rank by maximum distance to a language."""
+    ranks = rank_max_distance(ctx.obj.graph(), ctx.obj.kind)
+    print_ranks(
+        ranks if ctx.obj.n == 0 else ranks[:ctx.obj.n],
+        ' course:', ' courses:', '',
+    )
+
+
+@rank_by.command(name='total-progress')
+@click.pass_context
+def total_progress(ctx):
+    """Rank by total course progress."""
+    ranks = rank_progress(ctx.obj.graph(), ctx.obj.kind)
+    print_ranks(
+        ranks if ctx.obj.n == 0 else ranks[:ctx.obj.n],
+        '% progress:', '% progress:', '.4f',
+    )
+
+
+@rank_by.command(name='avg-progress')
+@click.pass_context
+def avg_progress(ctx):
+    """Rank by average course progress."""
+    ranks = rank_avg_progress(ctx.obj.graph(), ctx.obj.kind)
+    print_ranks(
+        ranks if ctx.obj.n == 0 else ranks[:ctx.obj.n],
+        '% progress:', '% progress:', '.4f',
+    )
+
+
+@cli.command(name='plot')
+@click.argument('file', required=False)
+@click.pass_context
+def plot_cli(ctx, file=None):
+    """Visually graph the courses."""
+    plot(ctx.obj.graph(), file=file)
+
+
+def main():
+    """Main function for duolingo_graph"""
+    cli(obj=CLIData())
+
+if __name__ == '__main__':
+    main()
---
+++ packages/duolingo-graph/duolingo-graph-0.4.0.tar.gz/duolingo-graph-0.4.0/duolingo_graph/numbers.py
@@ -0,0 +1,147 @@
+"""Number-crunching functions for the duolingo-graph command."""
+from collections import Counter, namedtuple
+from enum import Enum
+from itertools import groupby
+# from igraph.summary import OUT, IN, ALL
+OUT = 1
+IN = 2
+ALL = 3
+
+__all__ = [
+    'CourseType',
+    'count', 'rank',
+    'rank_courses', 'rank_progress', 'rank_avg_progress'
+]
+
+
+class CourseType(Enum):
+    """
+    Identifies whether a course is for speakers and/or learners of a language.
+    """
+    SPEAKERS = 1
+    LEARNERS = 2
+    BOTH = 3
+
+    @property
+    def _tuple(self):
+        """
+        Tuple of indices into graph.vs; 0 indicates speakers and 1 indicates
+        learners.
+        """
+        return {1: (0,),
+                2: (1,),
+                3: (0, 1,)}[self.value]
+
+    @property
+    def _mode(self):
+        """Graph edge mode for this course type."""
+        return {1: OUT,
+                2: IN,
+                3: ALL}[self.value]
+
+
+class LangDist(namedtuple('LangDist', ('dist', 'lang'))):
+    """Tuple of a maximum distance and a target language."""
+    def __format__(self, format_spec):
+        if format_spec[0:1].isdigit():
+            width = int(format_spec)
+        else:
+            width = 0
+        return '{}, {}'.format(self.lang, self.dist).rjust(width)
+
+
+def count(graph, kind, key):
+    """Sums up a function of course, language for courses of a given type."""
+    counts = Counter()
+    for edge in graph.es:
+        for i in kind._tuple:
+            name = graph.vs[edge.tuple[i]]['name']
+            counts[name] += key(edge, graph.vs[edge.tuple[i]])
+    return counts
+
+
+def distance(graph, kind, aggregator=max):
+    """
+    Figures out the maximum distance along the given paths (learning, speaking,
+    or both) to a given language.
+    """
+    dists = {}
+    for vertex in graph.vs:
+        paths = graph.get_all_shortest_paths(vertex, mode=kind._mode)
+        name = vertex['name']
+        dists[name] = aggregator(
+            (
+                LangDist(len(path), graph.vs[list(path)[-1]]['name'])
+                for path in paths
+            ), key=lambda x: x[0]
+        )
+    return dists
+
+
+def rank(counts):
+    """
+    Calculates a list of number, list pairs which correspond to the counts
+    returned by the count function and languages which match that count, sorted
+    in descending order by count.
+    """
+    return [
+        (key, [lang[0] for lang in group])
+        for key, group in groupby(
+            sorted(
+                counts,
+                key=lambda x: x[1],
+                reverse=True,
+            ),
+            key=lambda x: x[1],
+        )
+    ]
+
+
+def rank_max_distance(graph, kind=CourseType.BOTH):
+    """
+    Calculates a list of int, list pairs which correspond to the maximum
+    distance from the learner of one language to to another language, sorted
+    in ascending order by maximum distance.
+
+    graph:    the Duolingo graph
+    kind:     CourseType to describe who the courses are for (default BOTH)
+    """
+    return rank(distance(graph, kind).items())
+
+
+def rank_courses(graph, kind=CourseType.BOTH):
+    """
+    Calculates a list of int, list pairs which correspond to the amount of
+    courses offered the languages which match that number, sorted in descending
+    order by course amount.
+
+    graph:    the Duolingo graph
+    type:     CourseType to describe who the courses are for (default BOTH)
+    """
+    return rank(count(graph, kind, lambda e, v: 1).items())
+
+
+def rank_progress(graph, kind=CourseType.BOTH):
+    """
+    Calculates a list of float, list pairs which correspond to the cumulative
+    progress of courses offered and the languages which match that number,
+    sorted in descending order by course progress.
+
+    graph:    the Duolingo graph
+    kind:     CourseType to describe who the courses are for (default BOTH)
+    """
+    return rank(count(graph, kind, lambda e, v: e['progress']).items())
+
+
+def rank_avg_progress(graph, kind=CourseType.BOTH):
+    """
+    Calculates a list of float, list pairs which correspond to the average
+    progress of courses offered and the languages which match that number,
+    sorted in descending order by average progress.
+
+    graph:    the Duolingo graph
+    kind:     CourseType to describe who the courses are for (default BOTH)
+    """
+    courses = count(graph, kind, lambda e, v: 1)
+    progress = count(graph, kind, lambda e, v: e['progress'])
+    return rank((k, progress[k] / courses[k]) for k in courses.keys())
