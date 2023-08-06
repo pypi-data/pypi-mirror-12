03e7600b3ec8aa336bf1e2f064f191ad2e8da7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:18 +0000

    Add package nester_zzn/1.0.0/nester_zzn-1.0.0.zip

---
+++ packages/nester_zzn/nester_zzn-1.0.0.zip/nester_zzn-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester_zzn',
+    version = '1.0.0',
+    py_moduls = ['nester_zzn'],
+    author = 'sunnyAngel',
+    author_email = 'netfreeperson@sina.cn',
+    url = 'http://www.netfreeperson.com',
+    description = 'A simple printer of nested lists',
+    )
