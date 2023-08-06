66f550391d7ddda215de441769f805b2afd836ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:06 +0000

    Add package stopwords/0.1.2/stopwords-0.1.2.tar.gz

---
+++ packages/stopwords/stopwords-0.1.2.tar.gz/stopwords-0.1.2/README.rst
@@ -0,0 +1,29 @@
+===============================
+stopwords
+===============================
+
+.. image:: https://img.shields.io/pypi/v/stopwords.svg
+        :target: https://pypi.python.org/pypi/stopwords
+
+.. image:: https://readthedocs.org/projects/stopwords/badge/?version=latest
+        :target: https://readthedocs.org/projects/stopwords/?badge=latest
+        :alt: Documentation Status
+
+Stopwords removal 
+
+* Free software: ISC license
+* Documentation: https://stopwords.readthedocs.org.
+
+Features
+--------
+
+* TODO
+
+Credits
+---------
+
+Based on:
+https://github.com/Alir3z4/stop-words
+
+Tested some functions from 
+https://gist.github.com/glenbot/4684356
---
+++ packages/stopwords/stopwords-0.1.2.tar.gz/stopwords-0.1.2/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='stopwords',
+    version='0.1.2',
+    description="Stopwords filter for 40 languages",
+    long_description=readme + '\n\n' + history,
+    author="Len Dierickx",
+    author_email='len@astuanax.com',
+    url='https://github.com/astuanax/stopwords',
+    download_url = 'https://github.com/astuanax/stopwords/tarball/0.1', # I'll explain this in a second
+    packages=[
+        'stopwords',
+    ],
+    package_dir={'stopwords':
+                 'stopwords'},
+    include_package_data=True,
+    package_data={
+        'stopwords': [
+            'languages/*/*.txt',
+            'languages/languages.json',
+        ]
+    },
+    install_requires=requirements,
+    license="ISCL",
+    zip_safe=False,
+    keywords=['stopwords','language processing','nlp','filter'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    #test_suite='tests',
+    #tests_require=test_requirements
+)
