525aa90bed33066ea51d5f0b75fb57309f9746af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:39 +0000

    Add package fooTestX/1.112/fooTestX-1.112.tar.gz

---
+++ packages/fooTestX/fooTestX-1.112.tar.gz/fooTestX-1.112/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='fooTestX',
+      version='1.112',
+      url='www.quadlane.com/fooTestX',
+      author='Regimantas Baublys',
+      author_email='regtech0@gmail.com',
+      license='GPL3',
+      description='UNKNOWN',
+      py_modules=['fooTestX'],
+      )
