33bc235e15f2cbaa4d8c6b2e175b9c04511215c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:48 +0000

    Add package dmsa/0.5.7/dmsa-0.5.7-py2.7.egg

---
+++ packages/dmsa/dmsa-0.5.7-py2.7.egg/dmsa/main.py
@@ -0,0 +1,31 @@
+import sys
+from dmsa import ddl, erd, service, __version__
+
+
+def main():
+    usage = """Data Model DDL and ERD Generator
+
+    Usage: main.py (ddl | erd | start) [<args>...]
+
+    """  # noqa
+
+    from docopt import docopt
+
+    # Ignore command name.
+    argv = sys.argv[1:]
+
+    args = docopt(usage, argv=argv, version=__version__, options_first=True)
+
+    # Trim subcommand.
+    sub_argv = argv[1:]
+
+    if args['ddl']:
+        ddl.main(sub_argv)
+    elif args['erd']:
+        erd.main(sub_argv)
+    elif args['start']:
+        service.main(sub_argv)
+
+
+if __name__ == '__main__':
+    main()
