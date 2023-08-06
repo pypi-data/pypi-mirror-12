8ce152bc7247ba6e6e8243602e3b0ed4af6a1f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:50 +0000

    Add package test_CC/1.1.0/test_CC-1.1.0.zip

---
+++ packages/test_CC/test_CC-1.1.0.zip/test_CC-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='test_CC',
+    version='1.1.0',
+    py_modules=['test_CC'],
+    author='hfpython',
+    author_email='hfpython@headfirstlabs.com',
+    url='http://www.headfirstlabs.com',
+    descaription='A simple printer of nested list',
+    )
