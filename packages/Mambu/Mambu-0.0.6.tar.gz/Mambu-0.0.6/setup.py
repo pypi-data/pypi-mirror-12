06be684bacd70befe9015f136808ed68b3091a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:41 +0000

    Add package Mambu/0.0.6/Mambu-0.0.6.tar.gz

---
+++ packages/Mambu/Mambu-0.0.6.tar.gz/Mambu-0.0.6/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='Mambu',
+    version='0.0.6',
+    description='Python module for Mambu API',
+    author='Paze.me Limited',
+    author_email='hansel.dunlop@paze.me',
+    url='https://www.mambu.com',
+    packages=['mambu', 'mambu.etc', 'mambu.tools'],
+    install_requires=[
+        'requests'
+    ]
+)
