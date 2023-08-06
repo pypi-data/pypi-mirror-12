3bb5e94191b7e087c5fd2e18700ff6f128f22488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:10 +0000

    Add package docx2txt/0.4/docx2txt-0.4.tar.gz

---
+++ packages/docx2txt/docx2txt-0.4.tar.gz/docx2txt-0.4/docx2txt/docx2txt.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python
+
+import re
+import xml.etree.ElementTree as ET
+import zipfile
+
+
+nsmap = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+
+
+def qn(tag):
+    """
+    Stands for 'qualified name', a utility function to turn a namespace
+    prefixed tag name into a Clark-notation qualified tag name for lxml. For
+    example, ``qn('p:cSld')`` returns ``'{http://schemas.../main}cSld'``.
+    Source: https://github.com/python-openxml/python-docx/
+    """
+    prefix, tagroot = tag.split(':')
+    uri = nsmap[prefix]
+    return '{%s}%s' % (uri, tagroot)
+
+
+def xml2text(xml):
+    """
+    A string representing the textual content of this run, with content
+    child elements like ``<w:tab/>`` translated to their Python
+    equivalent.
+    Adapted from: https://github.com/python-openxml/python-docx/
+    """
+    text = u''
+    root = ET.fromstring(xml)
+    for child in root.iter():
+        if child.tag == qn('w:t'):
+            t_text = child.text
+            text += t_text if t_text is not None else ''
+        elif child.tag == qn('w:tab'):
+            text += '\t'
+        elif child.tag in (qn('w:br'), qn('w:cr')):
+            text += '\n'
+        elif child.tag == qn("w:p"):
+            text += '\n\n'
+    return text
+
+
+def process(docx):
+    text = u''
+
+    # unzip the docx in memory
+    zipf = zipfile.ZipFile(docx)
+    filelist = zipf.namelist()
+
+    # get header text
+    # there can be 3 header files in the zip
+    header_xmls = 'word/header[0-9]*.xml'
+    for fname in filelist:
+        if re.match(header_xmls, fname):
+            text += xml2text(zipf.read(fname))
+
+    # get main text
+    doc_xml = 'word/document.xml'
+    text += xml2text(zipf.read(doc_xml))
+
+    # get footer text
+    # there can be 3 footer files in the zip
+    footer_xmls = 'word/footer[0-9]*.xml'
+    for fname in filelist:
+        if re.match(footer_xmls, fname):
+            text += xml2text(zipf.read(fname))
+
+    zipf.close()
+    return text.strip()
+
+if __name__ == '__main__':
+    import sys
+    docx = sys.argv[1]
+    text = process(docx)
+    sys.stdout.write(text.encode('utf-8'))
---
+++ packages/docx2txt/docx2txt-0.4.tar.gz/docx2txt-0.4/setup.py
@@ -0,0 +1,19 @@
+import glob
+from distutils.core import setup
+
+# get all of the scripts
+scripts = glob.glob('bin/*')
+
+setup(
+  name='docx2txt',
+  packages=['docx2txt'],
+  version='0.4',
+  description='A pure python-based utility to extract text from docx files.',
+  author='Ankush Shah',
+  author_email='ankush.shah.nitk@gmail.com',
+  url='https://github.com/ankushshah89/python-docx2txt',
+  download_url='https://github.com/ankushshah89/python-docx2txt/tarball/0.4',
+  keywords=['python', 'docx', 'text'],
+  scripts=scripts,
+  classifiers=[],
+)
