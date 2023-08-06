9089795c55e3dd12e22bcac3bcecd275e1a4faed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:50 +0000

    Add package sql_query_dict/0.4/sql_query_dict-0.4.tar.gz

---
+++ packages/sql_query_dict/sql_query_dict-0.4.tar.gz/sql_query_dict-0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from pip.req import parse_requirements
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+install_requires = [
+    str(ir.req) for ir in parse_requirements('requirements.txt', session=False)
+]
+
+setup(
+    name='sql_query_dict',
+    version='0.4',
+    description='express sql queries as python dictionaries',
+    url='http://github.com/PlotWatt/sql_query_dict',
+    author='PlotWatt',
+    author_email='zdwiel@plotwatt.com',
+    license='Apache',
+    py_modules=['sql_query_dict'],
+    install_requires=install_requires,
+)
