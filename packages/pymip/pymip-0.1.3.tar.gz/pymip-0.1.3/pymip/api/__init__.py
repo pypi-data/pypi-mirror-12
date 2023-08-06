4510f4458c22b9f02074cf99d17e6a5cb3bb8aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:07 +0000

    Add package pymip/0.1.3/pymip-0.1.3.tar.gz

---
+++ packages/pymip/pymip-0.1.3.tar.gz/pymip-0.1.3/pymip/api/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from .core import MIP
+from .family import Family
+from .analysis import FamilyOutput, SampleOutput
---
+++ packages/pymip/pymip-0.1.3.tar.gz/pymip-0.1.3/tests/test_pymip.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+test_pymip
+----------------------------------
+
+Tests for `pymip` module.
+"""
+import pytest
+import pymip
+
+
+class TestPymip(object):
+    @classmethod
+    def set_up(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def tear_down(self):
+        pass
