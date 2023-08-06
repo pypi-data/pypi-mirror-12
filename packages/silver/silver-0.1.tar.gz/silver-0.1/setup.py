287aeb4bd5617fb4eff39d38aa3ea1b12f97be6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:11 +0000

    Add package silver/0.1/silver-0.1.tar.gz

---
+++ packages/silver/silver-0.1.tar.gz/silver-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'silver',
+  packages = ['silver'], # this must be the same as the name above
+  version = '0.1',
+  description = 'SilverCore API python wrapper',
+  author = 'Alejandro Saucedo',
+  author_email = 'alejandro@hackpartners.com.com',
+  url = 'https://github.com/HackTrain/silver',
+  download_url = 'https://github.com/HackTrain/silver/tarball/0.1', 
+  keywords = ['silvercore', 'silver', 'hackpartners', 'hacktrain', 'hack', 'partners'], 
+  classifiers = [],
+)
