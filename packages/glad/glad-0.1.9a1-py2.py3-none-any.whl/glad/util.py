06be9f3ce6b802259bd06bc82c291b2fc4299e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:58 +0000

    Add package glad/0.1.9a1/glad-0.1.9a1-py2.py3-none-any.whl

---
+++ packages/glad/glad-0.1.9a1-py2.py3-none-any.whl/glad/util.py
@@ -0,0 +1,25 @@
+
+_API_NAMES = {
+    'egl': 'EGL',
+    'gl': 'OpenGL',
+    'gles1': 'OpenGL ES',
+    'gles2': 'OpenGL ES',
+    'glx': 'GLX',
+    'wgl': 'WGL',
+}
+
+
+def api_name(api):
+    api = api.lower()
+    return _API_NAMES[api]
+
+
+def get_glad_version():
+    try:
+        import pkg_resources
+    except ImportError:
+        return 'Unknown'
+    try:
+        return pkg_resources.get_distribution('glad').version
+    except pkg_resources.DistributionNotFound:
+        return 'Unknown'
