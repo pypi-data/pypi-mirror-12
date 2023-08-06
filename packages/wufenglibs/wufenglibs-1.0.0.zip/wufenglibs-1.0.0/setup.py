6431cd125e73412c657667c86f49935d114ab6d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:54 +0000

    Add package wufenglibs/1.0.0/wufenglibs-1.0.0.zip

---
+++ packages/wufenglibs/wufenglibs-1.0.0.zip/wufenglibs-1.0.0/setup.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+__author__ = 'wufeng'
+
+from distutils.core import setup
+#从python发布工具中导入setup
+
+setup(
+    name='wufenglibs',
+    version='1.0.0',
+    author='wufeng',
+    description='simple python demo'
+
+
+)
