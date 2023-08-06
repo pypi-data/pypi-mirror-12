2155212982fa40091f8e9b1d3cd1bd671c1116c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:24 +0000

    Add package kvm/1.0.5/kvm-1.0.5.tar.gz

---
+++ packages/kvm/kvm-1.0.5.tar.gz/kvm-1.0.5/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+
+
+setup (
+    name='kvm',
+    version='1.0.5',
+    author='François Ménabé',
+    author_email='francois.menabe@gmail.com',
+    packages=['kvm'],
+    package_dir={'kvm': 'kvm'},
+    package_data={'kvm': ['kvm.json']},
+    license='MIT License',
+    description='An API for managing KVM host.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'unix',
+        'lxml'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: Unix',
+        'Topic :: System :: Systems Administration']
+)
