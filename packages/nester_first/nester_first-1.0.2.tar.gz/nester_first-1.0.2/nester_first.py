25eb637b9398c4cbb833e08d782a62afbfcc5403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:03 +0000

    Add package nester_first/1.0.2/nester_first-1.0.2.tar.gz

---
+++ packages/nester_first/nester_first-1.0.2.tar.gz/nester_first-1.0.2/nester_first.py
@@ -0,0 +1,22 @@
+"""This is ths "nester.py" module andd it provides one function called print_lol() which prints lists that may or may not include nested lists."""
+def print_lol(the_list, level = 0):
+    """This function takes one positional argument called "the_list", which is any python list. Each data item in the provided list is printed to the screen on it's own line.
+indent provide level"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level + 1)
+        else:
+            for level_item in range(level):
+                print("\t", end='')
+            print(each_item)
+
+
+
+
+
+
+
+
+
+
+            
