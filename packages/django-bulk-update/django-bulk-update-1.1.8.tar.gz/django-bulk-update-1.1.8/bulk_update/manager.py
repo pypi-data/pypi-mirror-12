de2c8ce908384a5e8d59143231a33c8bb6a3d7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:25 +0000

    Add package django-bulk-update/1.1.8/django-bulk-update-1.1.8.tar.gz

---
+++ packages/django-bulk-update/django-bulk-update-1.1.8.tar.gz/django-bulk-update-1.1.8/bulk_update/manager.py
@@ -0,0 +1,9 @@
+from django.db import models
+from .helper import bulk_update
+
+
+class BulkUpdateManager(models.Manager):
+    def bulk_update(self, objs, update_fields=None, exclude_fields=None):
+        return bulk_update(
+            objs, update_fields=update_fields,
+            exclude_fields=exclude_fields, using=self.db)
