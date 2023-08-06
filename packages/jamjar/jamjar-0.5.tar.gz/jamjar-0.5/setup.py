2d2719ae1458138cd81a7c3535135780ddf562c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:15 +0000

    Add package jamjar/0.5/jamjar-0.5.tar.gz

---
+++ packages/jamjar/jamjar-0.5.tar.gz/jamjar-0.5/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name="jamjar",
+      version="0.5",
+      description="Jam target and dependency inspection tool",
+      author="Phil Connell, Zoe Kelly, Jonathan Loh, Antony Wallace, Ensoft Ltd",
+      author_email="philc@ensoft.co.uk",
+      packages=["jamjar", "jamjar.parsers"],
+      )
+
