9dad0e217f87b29b23a2fd9cdfaa70e4771a1e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:21 +0000

    Add package vi3o/0.3.1/vi3o-0.3.1.tar.gz

---
+++ packages/vi3o/vi3o-0.3.1.tar.gz/vi3o-0.3.1/vi3o/utils.py
@@ -0,0 +1,14 @@
+import sys
+if sys.version_info > (3,):
+    xrange = range
+
+class SlicedView(object):
+    def __init__(self, parent, indexes):
+        self.parent = parent
+        self.range = xrange(*indexes.indices(len(parent)))
+
+    def __getitem__(self, item):
+        return self.parent[self.range[item]]
+
+    def __len__(self):
+        return len(self.range)
