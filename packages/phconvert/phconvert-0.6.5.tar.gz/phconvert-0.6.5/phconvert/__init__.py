e12cc8e55185b1c2c9760aa90fcd90fcb0597400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:46 +0000

    Add package phconvert/0.6.5/phconvert-0.6.5.tar.gz

---
+++ packages/phconvert/phconvert-0.6.5.tar.gz/phconvert-0.6.5/phconvert/__init__.py
@@ -0,0 +1,13 @@
+#
+# phconvert - Reference library to read and save Photon-HDF5 files
+#
+# Copyright (C) 2015 Antonino Ingargiola <tritemio@gmail.com>
+#
+
+from . import loader
+from . import plotter
+from . import hdf5
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
