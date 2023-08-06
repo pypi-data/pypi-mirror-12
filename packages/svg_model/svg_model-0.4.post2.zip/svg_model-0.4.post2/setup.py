6cb667a8722f9724190b3845eb6c08b155b4b54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:13 +0000

    Add package svg_model/0.4.post2/svg_model-0.4.post2.zip

---
+++ packages/svg_model/svg_model-0.4.post2.zip/svg_model-0.4.post2/setup.py
@@ -0,0 +1,17 @@
+import sys
+from distutils.core import setup
+
+sys.path.insert(0, '.')
+import version
+
+
+setup(name='svg_model',
+      version=version.getVersion(),
+      description='A Python module for parsing an SVG file to a group of '
+      'paths.',
+      keywords='svg model pymunk',
+      author='Christian Fobel',
+      author_email='christian@fobel.net',
+      url='http://github.com/cfobel/svg_model.git',
+      license='GPL',
+      packages=['svg_model', 'svg_model.svgload'])
