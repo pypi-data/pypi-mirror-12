f3fffa6c7be5e2e8a8e56b8dc53596d7698c602a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:21 +0000

    Add package pyEDFlib/0.1.1/pyEDFlib-0.1.1.zip

---
+++ packages/pyEDFlib/pyEDFlib-0.1.1.zip/pyEDFlib-0.1.1/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.1'
+version = '0.1.1'
+full_version = '0.1.1'
+git_revision = 'a2d64f75b790eee68228ff19047b5351089767ba'
+release = True
+
+if not release:
+    version = full_version
