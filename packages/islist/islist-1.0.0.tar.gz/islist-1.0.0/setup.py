c7fc3b19732fdfa0b9ddf126fd2ee4b6f0bc54d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:03 +0000

    Add package islist/1.0.0/islist-1.0.0.tar.gz

---
+++ packages/islist/islist-1.0.0.tar.gz/islist-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name		= 'islist',
+	version		= '1.0.0',
+	py_modules	= ['nester'],
+	author		= 'taehwan.koo',
+	author_email	= 'simple_guy87@naver.com',
+	description	= 'A simple printer of nested lists',
+     )
