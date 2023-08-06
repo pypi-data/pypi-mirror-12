ac7820073593af5766fe1ccd4b66a952d0dd7d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:59 +0000

    Add package dot-cli/0.0.4/dot-cli-0.0.4.tar.gz

---
+++ packages/dot-cli/dot-cli-0.0.4.tar.gz/dot-cli-0.0.4/dot/git.py
@@ -0,0 +1,69 @@
+import os
+import click
+import subprocess
+from time import strftime
+
+class Git:
+    """Git Functionality for Dot"""
+    
+    _dotdir = '/.dot/'
+    _github = 'git@github.com:'
+
+    def __init__(self, home, gitname, reponame):
+        """Initializer for Git class object"""
+        self.home = home
+        self.gitname = gitname
+        self.reponame = reponame
+    
+    def clone(self):
+        """Git clone functionality"""
+        dest_path = self.home + self._dotdir
+        git_url = self._github + self.gitname + '/' + self.reponame + '.git'
+        os.chdir(dest_path)
+        with open(os.devnull, 'w') as fnull:
+            gclone = subprocess.call(['git', 'clone', git_url, '.'], stdout=fnull, stderr=fnull)
+
+        already_has_managed_text = False
+        managed_text = '### Managed by [dot](https://github.com/kylefrost/dot-cli)'
+
+        if os.path.exists('README.md'):
+            with open('README.md', 'r') as f:
+                for line in f:
+                    if 'Managed by [dot]' in line:
+                        already_has_managed_text = True
+
+            with open('README.md', 'a+') as readme:
+                if not already_has_managed_text:
+                    readme.write('\n\n' + managed_text)
+        else:
+            click.secho('No README.md file found in repo.', fg='yellow')
+
+        return gclone
+
+    def pull(self):
+        """Git pull functionality"""
+        dest_path = self.home + self._dotdir
+        os.chdir(dest_path)
+        with open(os.devnull, 'w+') as fnull:
+            gpull = subprocess.call(['git', 'pull'], stdout=fnull, stderr=fnull)
+
+        return gpull
+
+    def push(self, commit_time_format=None):
+        """Git push functionality"""
+        path = self.home + self._dotdir
+
+        if commit_time_format is None or commit_time_format == "12":
+            commit_msg = 'dot: Updated dotfiles at ' + strftime('%I:%M:%S %p') + ' on ' + strftime('%b %d')
+        elif commit_time_format == "24":
+            commit_msg = 'dot: Updated dotfiles at ' + strftime('%H:%M:%S') + ' on ' + strftime('%d %b')
+        else:
+            commit_msg = 'dot: Updated dotfiles at ' + strftime('%I:%M:%S %p') + ' on ' + strftime('%b %d')
+
+        os.chdir(path)
+        with open(os.devnull, 'w+') as fnull:
+            add = subprocess.call(['git', 'add', '--all'], stdout=fnull, stderr=fnull)
+            commit = subprocess.call(['git', 'commit', '-m', commit_msg], stdout=fnull, stderr=fnull)
+            gpush = subprocess.call(['git', 'push', 'origin', 'master'], stdout=fnull, stderr=fnull)
+
+        return [add, commit, gpush]
---
+++ packages/dot-cli/dot-cli-0.0.4.tar.gz/dot-cli-0.0.4/setup.py
@@ -0,0 +1,49 @@
+"""
+Easily manage dotfiles through the command line.
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='dot-cli',
+    version='0.0.4',
+    url='https://github.com/kylefrost/dot',
+    download_url='https://github.com/kylefrost/dot-cli/tarball/0.0.4',
+    author='Kyle Frost',
+    author_email='kyle@kylefrost.me',
+    description='Lightweight tool for managing dotfiles with git and the command line.',
+    license='GPLv3',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'dot = dot.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
