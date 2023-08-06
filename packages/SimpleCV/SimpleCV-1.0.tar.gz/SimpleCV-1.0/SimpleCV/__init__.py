9869f62f3333d5920479b7a3dc57eeead68b925a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:02 +0000

    Add package SimpleCV/1.0/SimpleCV-1.0.tar.gz

---
+++ packages/SimpleCV/SimpleCV-1.0.tar.gz/SimpleCV-1.0/SimpleCV/__init__.py
@@ -0,0 +1,11 @@
+from SimpleCV.base import *
+from SimpleCV.Camera import *
+from SimpleCV.Color import *
+from SimpleCV.Detection import *   
+from SimpleCV.Features import *
+from SimpleCV.ImageClass import *
+from SimpleCV.Stream import *
+
+if (__name__ == '__main__'):
+  from SimpleCV.Shell import *
+  main()
