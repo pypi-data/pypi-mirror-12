1b7662ec32f6a0e37b252fab692176ed1522bcc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:01 +0000

    Add package yehen_nester/1.1.0/yehen_nester-1.1.0.zip

---
+++ packages/yehen_nester/yehen_nester-1.1.0.zip/yehen_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='yehen_nester',
+        version='1.1.0',
+        py_modules=['nester'],
+        author='yehen',
+        author_email='jianwei8947@163.com',
+        url='http://www.headfirstlabs.com',
+        description='A simple printer of nested lists'
+        )
