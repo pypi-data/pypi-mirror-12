05063a0e91c289b9861b18794900e7a981fe853b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:58 +0000

    Add package ciworker/0.2.2/ciworker-0.2.2.tar.gz

---
+++ packages/ciworker/ciworker-0.2.2.tar.gz/ciworker-0.2.2/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ciworker',
+    version='0.2.2',
+    description='Cytoscape CI ciworker service template',
+    long_description='Template worker code for Cytoscape CI.',
+    author='Keiichiro Ono',
+    author_email='kono@ucsd.edu',
+    url='https://github.com/idekerlab/ci-service-template',
+    license='MIT License',
+    keywords=['graph', 'network', 'cytoscape'],
+    install_requires=[
+        'requests',
+        'pyzmq',
+        'redis',
+        'pyaml'
+    ],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    test_suite='test',
+    packages=find_packages(),
+    include_package_data=True,
+)
