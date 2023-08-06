f2f2493493ae6215295542118c0484a0c9f7770e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:59 +0000

    Add package texty/0.0.2/texty-0.0.2.tar.gz

---
+++ packages/texty/texty-0.0.2.tar.gz/texty-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='texty',
+      version='0.0.2',
+      description='A cutility & wrapper functions for text processing and linguistics.',
+      url='https://github.com/magnusnissel/texty',
+      author='Magnus Nissel',
+      author_email='magnus@nissel.org',
+      license='GPL',
+      packages=find_packages(),
+      keywords=["text", "utility", "linguistics", "digital humanities"],
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"]
+      )
