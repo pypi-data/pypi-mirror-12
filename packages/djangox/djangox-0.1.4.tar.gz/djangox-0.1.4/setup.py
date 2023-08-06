a47e091c7eb781e183b696f336fbb13fa3a178bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:30 +0000

    Add package djangox/0.1.4/djangox-0.1.4.tar.gz

---
+++ packages/djangox/djangox-0.1.4.tar.gz/djangox-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(name='djangox',
+      description='Another way of using django. Convention over configuration for urls, mako template language support,'
+                  'json response, REST style API, convenient scripts, ...',
+      author='Youngrok Pak',
+      author_email='pak.youngrok@gmail.com',
+      keywords= 'rest route autodiscover django djangox mako',
+      url='https://github.com/youngrok/djangox',
+      version='0.1.4',
+      package_dir={'djangox': 'djangox'},
+      include_package_data=True,
+
+      classifiers = [
+                     'Development Status :: 3 - Alpha',
+                     'Topic :: Software Development :: Libraries',
+                     'License :: OSI Approved :: BSD License']
+      )
