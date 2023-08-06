ab42cec65f56ebdd703c1e8e8f72c6507b7394c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:42 +0000

    Add package exceltowiki/0.1.15/exceltowiki-0.1.15.tar.gz

---
+++ packages/exceltowiki/exceltowiki-0.1.15.tar.gz/exceltowiki-0.1.15/exceltowiki/example/example.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 28, 2015
+
+@author: venkman69@yahoo.com
+'''
+
+from exceltowiki import excelToWiki
+import exceltowiki
+
+exceltowiki.INLINE_FMT=True
+e2w = excelToWiki("./test.xlsx",["Sheet1"],"blue","yellow")
+print e2w.sheetnames
+print e2w.getSheet("Sheet1")
+print e2w.getWorkbook()
+# do not honor the widths
+e2w = excelToWiki("./test.xlsx",["Sheet1"],"blue","yellow",False)
+print e2w.getSheet("Sheet1")
