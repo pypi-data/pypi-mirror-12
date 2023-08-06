5ab448027c7a8a5cf9fd87299c2dd896d28a24ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:35 +0000

    Add package Ocelot/1.1/Ocelot-1.1.tar.gz

---
+++ packages/Ocelot/Ocelot-1.1.tar.gz/Ocelot-1.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name='Ocelot',
+    version='1.1',
+    description='Static site generator designed for stories',
+    author='Damien Howard',
+    author_email='foxscotch@gmail.com',
+    url='http://foxscotch.us/',
+    py_modules=['ocelot'],
+    requires=[
+        'Markdown',
+        'Jinja2',
+        'PyYaml'
+    ]
+)
