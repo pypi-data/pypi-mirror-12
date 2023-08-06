340ac6e52fe6d5d083505e8680f1d7eb1eeee401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:15 +0000

    Add package marten/0.2.1/marten-0.2.1.tar.gz

---
+++ packages/marten/marten-0.2.1.tar.gz/marten-0.2.1/marten/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+import os as _os
+
+__version__ = '0.2.1'
+
+
+
+# Attempt to auto-load a default configuration from files in <cwd>/.marten/ based on the MARTEN_ENV env variable
+# MARTEN_ENV defaults to 'default'
+config = None
+_marten_dir = _os.path.join(_os.getcwd(), '.marten')
+
+if _os.path.isdir(_marten_dir):
+	try:
+		from .conf import parse_directory as _parse_directory
+		config = _parse_directory(_marten_dir, _os.environ.get('MARTEN_ENV', 'default'))
+	except Exception:
+		pass
