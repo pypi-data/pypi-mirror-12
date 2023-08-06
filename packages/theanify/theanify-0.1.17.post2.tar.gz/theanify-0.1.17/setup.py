0bbe0420cd8641283070afd7ff123ab1d75d6487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:07 +0000

    Add package theanify/0.1.17/theanify-0.1.17.post2.tar.gz

---
+++ packages/theanify/theanify-0.1.17.post2.tar.gz/theanify-0.1.17/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name = "theanify",
+    version = "0.1.17",
+    author = "Sharad Vikram",
+    author_email = "sharad.vikram@gmail.com",
+    description = "Allows you to annotate instance methods to be compiled by Theano.",
+    license = "MIT",
+    keywords = "theano",
+    url = "https://github.com/sharadmv/theanify",
+    packages=[
+        'theanify'
+    ],
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+     'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 2.7',
+],
+)
