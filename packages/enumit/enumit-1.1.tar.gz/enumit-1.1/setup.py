419445c1c0a505bddc539e3c25aee32e3f9ab219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:14 +0000

    Add package enumit/1.1/enumit-1.1.tar.gz

---
+++ packages/enumit/enumit-1.1.tar.gz/enumit-1.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+
+setup(
+    name='enumit',
+    version='1.1',
+    description='Enumeration library for python.',
+    long_description=open('README.rst', 'r').read(),
+    author='Faruk Cepni',
+    author_email='farukcepni@gmail.com',
+    url='https://github.com/farukcepni/enumit',
+    keywords=['enumeration', 'enum'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
