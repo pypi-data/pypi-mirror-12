960de3333510d6fd8008d22c79f50ad410bcb8ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:44 +0000

    Add package python-tutum/0.20.0/python-tutum-0.20.0.tar.gz

---
+++ packages/python-tutum/python-tutum-0.20.0.tar.gz/python-tutum-0.20.0/tutum/api/container.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import
+
+from .base import Mutable, StreamingLog
+
+
+class Container(Mutable):
+    """Represents a Tutum Container object"""
+
+    endpoint = "/container"
+
+    def save(self):
+        raise AttributeError("'save' is not supported in 'Container' object. "
+                             "Please use the related 'Service' object instead.")
+
+    def start(self):
+        """Starts the container in Tutum.
+
+        :returns: bool -- whether or not the operation succeeded
+        :raises: TutumApiError
+        """
+        return self._perform_action("start")
+
+    def stop(self):
+        """Stops the container in Tutum.
+
+        :returns: bool -- whether or not the operation succeeded
+        :raises: TutumApiError
+        """
+        return self._perform_action("stop")
+
+    def redeploy(self, reuse_volumes=True):
+        """Redeploy the container in Tutum.
+
+        :returns: bool -- whether or not the operation succeeded
+        :raises: TutumApiError
+        """
+        params = {'reuse_volumes': reuse_volumes}
+        return self._perform_action("redeploy", params=params)
+
+    def logs(self, tail, follow, log_handler=StreamingLog.default_log_handler):
+        """Follow logs for the container from Tutum streaming API
+
+        :returns: None
+        """
+        logs = StreamingLog("container", self.pk, tail, follow)
+        logs.on_message(log_handler)
+        logs.run_forever()
