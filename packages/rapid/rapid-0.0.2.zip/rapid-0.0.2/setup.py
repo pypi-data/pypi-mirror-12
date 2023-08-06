165f2f2fbb67c92a8cd0715773e569e60a61f9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:19 +0000

    Add package rapid/0.0.2/rapid-0.0.2.zip

---
+++ packages/rapid/rapid-0.0.2.zip/rapid-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "rapid",
+    version = "0.0.2",
+    author = "Keshav Gupta",
+    author_email = "keshav@keshav.xyz",
+    description = ("rapid generates boilerplate code through command line"),
+    license = "MIT",
+    keywords = ["boilerplate", "cli", "generate" "command line"],
+    url = "https://github.com/keshav11/rapid",
+    packages=['rapid'],
+    download_url='https://github.com/keshav11/rapid/tarball/0.0.1',
+    package_data={
+        'rapid': [
+        'boilerplate/c/*.bp',
+        'boilerplate/csharp/*.bp',
+        'boilerplate/html/*.bp',
+        'boilerplate/java/*.bp'
+        ]
+    },
+      entry_points = {
+        'console_scripts': [
+            'rapid = rapid.main:main',
+        ],
+    }
+)
