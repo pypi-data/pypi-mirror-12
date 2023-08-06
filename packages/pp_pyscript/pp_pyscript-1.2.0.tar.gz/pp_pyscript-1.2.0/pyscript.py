e90304fa55424c57791d96bd24ce5cd200059b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:21 +0000

    Add package pp_pyscript/1.2.0/pp_pyscript-1.2.0.tar.gz

---
+++ packages/pp_pyscript/pp_pyscript-1.2.0.tar.gz/pp_pyscript-1.2.0/pyscript.py
@@ -0,0 +1,26 @@
+"""I 
+    love 
+        u,
+           my darlin"""
+
+def print_lol(the_list,level=0):
+    for each_1 in the_list:
+        if isinstance(each_1,list):
+            print_lol(each_1,level+1)
+        else:
+            for num in range(level):
+                print('\t',end="")
+            print(each_1)
+if __name__ == '__main__':
+    movie = ["Haixu Jin",
+             "Leizhang Wang",
+             "Pai Pang",
+             ["1L1O1L",["Jian Ji","Shi Zigou", "Nan Dao"]],
+             "Lingyu Zhang"]
+
+    print_lol(movie,4)
+
+
+ 
+            
+
