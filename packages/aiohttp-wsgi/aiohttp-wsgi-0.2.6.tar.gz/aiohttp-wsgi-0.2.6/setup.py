d4af0c383034033ebb5971b3d999308d6445f4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:47 +0000

    Add package aiohttp-wsgi/0.2.6/aiohttp-wsgi-0.2.6.tar.gz

---
+++ packages/aiohttp-wsgi/aiohttp-wsgi-0.2.6.tar.gz/aiohttp-wsgi-0.2.6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "aiohttp-wsgi",
+    version = "0.2.6",
+    license = "BSD",
+    description = "WSGI adapter for aiohttp.",
+    author = "Dave Hall",
+    author_email = "dave@etianen.com",
+    url = "https://github.com/etianen/aiohttp-wsgi",
+    packages = find_packages(exclude=("tests",)),
+    install_requires = [
+        "aiohttp>=0.17.0",
+    ],
+    extras_require = {
+        "test":  [
+            "pytest",
+            "pytest-cov",
+            "pytest-asyncio",
+        ],
+    },
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Framework :: Django",
+    ],
+)
