5fe83446934b4fbe1443ef4cf8da14796d551034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:02 +0000

    Add package pykafka/2.0.3/pykafka-2.0.3.tar.gz

---
+++ packages/pykafka/pykafka-2.0.3.tar.gz/pykafka-2.0.3/README.rst
@@ -0,0 +1,135 @@
+.. image:: https://travis-ci.org/Parsely/pykafka.svg?branch=master
+    :target: https://travis-ci.org/Parsely/pykafka
+.. image:: https://codecov.io/github/Parsely/pykafka/coverage.svg?branch=master
+    :target: https://codecov.io/github/Parsely/pykafka?branch=master
+
+PyKafka
+=======
+
+.. image:: http://i.imgur.com/ztYl4lG.jpg
+
+PyKafka is a cluster-aware Kafka 0.8.2 protocol client for python. It includes python
+implementations of Kafka producers and consumers, and runs under python 2.7.
+
+PyKafka's primary goal is to provide a similar level of abstraction to the
+`JVM Kafka client`_ using idioms familiar to python programmers and exposing
+the most pythonic API possible.
+
+You can install PyKafka from PyPI with
+
+::
+
+    $ pip install pykafka
+
+Full documentation and usage examples for PyKafka can be found on `readthedocs`_.
+
+You can install PyKafka for local development and testing with
+
+    $ python setup.py develop
+
+.. _JVM Kafka client: https://github.com/apache/kafka/tree/0.8.2/clients/src/main/java/org/apache/kafka
+.. _readthedocs: http://pykafka.readthedocs.org/en/latest/
+
+Getting Started
+---------------
+
+Assuming you have a Kafka instance running on localhost, you can use PyKafka
+to connect to it.
+
+.. sourcecode:: python
+
+    >>> from pykafka import KafkaClient
+    >>> client = KafkaClient(hosts="127.0.0.1:9092")
+
+If the cluster you've connected to has any topics defined on it, you can list
+them with:
+
+.. sourcecode:: python
+
+    >>> client.topics
+    {'my.test': <pykafka.topic.Topic at 0x19bc8c0 (name=my.test)>}
+    >>> topic = client.topics['my.test']
+
+Once you've got a `Topic`, you can create a `Producer` for it and start
+producing messages.
+
+.. sourcecode:: python
+
+    >>> with topic.get_producer() as producer:
+    ...     for i in range(4):
+    ...         producer.produce('test message ' + i ** 2)
+
+You can also consume messages from this topic using a `Consumer` instance.
+
+.. sourcecode:: python
+
+    >>> consumer = topic.get_simple_consumer()
+    >>> for message in consumer:
+    ...     if message is not None:
+    ...         print message.offset, message.value
+    0 test message 0
+    1 test message 1
+    2 test message 4
+    3 test message 9
+
+This `SimpleConsumer` doesn't scale - if you have two `SimpleConsumers`
+consuming the same topic, they will receive duplicate messages. To get around
+this, you can use the `BalancedConsumer`.
+
+.. sourcecode:: python
+
+    >>> balanced_consumer = topic.get_balanced_consumer(
+    ...     consumer_group='testgroup',
+    ...     auto_commit_enable=True,
+    ...     zookeeper_connect='myZkClusterNode1.com:2181,myZkClusterNode2.com:2181/myZkChroot'
+    ... )
+
+You can have as many `BalancedConsumer` instances consuming a topic as that
+topic has partitions. If they are all connected to the same zookeeper instance,
+they will communicate with it to automatically balance the partitions between
+themselves.
+
+Operational Tools
+-----------------
+
+PyKafka includes a small collection of `CLI tools`_ that can help with common tasks
+related to the administration of a Kafka cluster, including offset and lag monitoring and
+topic inspection. The full, up-to-date interface for these tools can be fould by running
+
+.. sourcecode::
+
+    $ python cli/kafka_tools.py --help
+
+or after installing PyKafka via setuptools or pip:
+
+.. sourcecode::
+
+    $ kafka-tools --help
+
+.. _CLI tools: https://github.com/Parsely/pykafka/blob/master/pykafka/cli/kafka_tools.py
+
+What happened to Samsa?
+-----------------------
+
+This project used to be called samsa. It has been renamed PyKafka and has been
+fully overhauled to support Kafka 0.8.2. We chose to target 0.8.2 because the offset
+Commit/Fetch API is stabilized.
+
+The Samsa `PyPI package`_  will stay up for the foreseeable future and tags for
+previous versions will always be available in this repo.
+
+.. _PyPI package: https://pypi.python.org/pypi/samsa/0.3.11
+
+pykafka or kafka-python?
+------------------------
+
+These are two different projects.
+See `the discussion here <https://github.com/Parsely/pykafka/issues/334>`_.
+
+Support
+-------
+
+If you need help using PyKafka or have found a bug, please open a `github issue`_ or use the `Google Group`_.
+
+.. _github issue: https://github.com/Parsely/pykafka/issues
+.. _Google Group: https://groups.google.com/forum/#!forum/pykafka-user
---
+++ packages/pykafka/pykafka-2.0.3.tar.gz/pykafka-2.0.3/tests/pykafka/test_producer.py
@@ -0,0 +1,123 @@
+from __future__ import division
+
+import time
+import unittest2
+from uuid import uuid4
+
+from pykafka import KafkaClient
+from pykafka.exceptions import ProducerQueueFullError
+from pykafka.test.utils import get_cluster, stop_cluster
+
+
+class ProducerIntegrationTests(unittest2.TestCase):
+    maxDiff = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.kafka = get_cluster()
+        cls.topic_name = b'test-data'
+        cls.kafka.create_topic(cls.topic_name, 3, 2)
+        cls.client = KafkaClient(cls.kafka.brokers)
+        cls.consumer = cls.client.topics[cls.topic_name].get_simple_consumer(
+            consumer_timeout_ms=1000)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.consumer.stop()
+        stop_cluster(cls.kafka)
+
+    def test_produce(self):
+        # unique bytes, just to be absolutely sure we're not fetching data
+        # produced in a previous test
+        payload = uuid4().bytes
+
+        prod = self.client.topics[self.topic_name].get_sync_producer(min_queued_messages=1)
+        prod.produce(payload)
+
+        # set a timeout so we don't wait forever if we break producer code
+        message = self.consumer.consume()
+        assert message.value == payload
+
+    def test_async_produce(self):
+        payload = uuid4().bytes
+
+        prod = self.client.topics[self.topic_name].get_producer(min_queued_messages=1)
+        prod.produce(payload)
+
+        message = self.consumer.consume()
+        assert message.value == payload
+
+    def test_async_produce_context(self):
+        """Ensure that the producer works as a context manager"""
+        payload = uuid4().bytes
+
+        with self.client.topics[self.topic_name].get_producer(min_queued_messages=1) as producer:
+            producer.produce(payload)
+
+        message = self.consumer.consume()
+        assert message.value == payload
+
+    def test_async_produce_queue_full(self):
+        """Ensure that the producer raises an error when its queue is full"""
+        topic = self.client.topics[self.topic_name]
+        with topic.get_producer(block_on_queue_full=False,
+                                max_queued_messages=1,
+                                linger_ms=1000) as producer:
+            with self.assertRaises(ProducerQueueFullError):
+                while True:
+                    producer.produce(uuid4().bytes)
+        while self.consumer.consume() is not None:
+            time.sleep(.05)
+
+    def test_async_produce_lingers(self):
+        """Ensure that the context manager waits for linger_ms milliseconds"""
+        linger = 3
+        topic = self.client.topics[self.topic_name]
+        with topic.get_producer(linger_ms=linger * 1000) as producer:
+            start = time.time()
+            producer.produce(uuid4().bytes)
+            producer.produce(uuid4().bytes)
+        self.assertEqual(int(time.time() - start), int(linger))
+        self.consumer.consume()
+        self.consumer.consume()
+
+    def test_async_produce_thread_exception(self):
+        """Ensure that an exception on a worker thread is raised to the main thread"""
+        topic = self.client.topics[self.topic_name]
+        with self.assertRaises(ValueError):
+            with topic.get_producer(min_queued_messages=1) as producer:
+                # get some dummy data into the queue that will cause a crash when flushed
+                # specifically, this tuple causes a crash since its first element is
+                # not a two-tuple
+                producer._produce(("anything", 0))
+        while self.consumer.consume() is not None:
+            time.sleep(.05)
+
+    def test_required_acks(self):
+        """Test with non-default values for `required_acks`
+
+        See #278 for a related bug.  Here, we only test that no exceptions
+        occur (hence `sync=True`, which would surface most exceptions)
+        """
+        kwargs = dict(linger_ms=1, sync=True, required_acks=0)
+        prod = self.client.topics[self.topic_name].get_producer(**kwargs)
+        prod.produce(uuid4().bytes)
+
+        kwargs["required_acks"] = -1
+        prod = self.client.topics[self.topic_name].get_producer(**kwargs)
+        prod.produce(uuid4().bytes)
+
+    def test_null_payloads(self):
+        """Test that None is accepted as a null payload"""
+        prod = self.client.topics[self.topic_name].get_sync_producer(
+                min_queued_messages=1)
+        prod.produce(None)
+        self.assertIsNone(self.consumer.consume().value)
+        prod.produce(None, partition_key=b"whatever")
+        self.assertIsNone(self.consumer.consume().value)
+        prod.produce(b"")  # empty string should be distinguished from None
+        self.assertEqual(b"", self.consumer.consume().value)
+
+
+if __name__ == "__main__":
+    unittest2.main()
