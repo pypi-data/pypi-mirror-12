1801896a6ff71b39f1d51d1c41880afc94fbb6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:53 +0000

    Add package nester2501/1.0.0/nester2501-1.0.0.zip

---
+++ packages/nester2501/nester2501-1.0.0.zip/nester2501-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'nester2501', 
+        version      = '1.0.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['nester2501'],
+        author       = 'clobert',
+        author_email = 'hfpython@headfirstlabs.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested2501 lists',
+     )
