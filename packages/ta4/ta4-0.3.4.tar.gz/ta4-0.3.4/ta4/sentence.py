c2db29adb8414f8bbaf000d5c4d01d37c7239bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:29 +0000

    Add package ta4/0.3.4/ta4-0.3.4.tar.gz

---
+++ packages/ta4/ta4-0.3.4.tar.gz/ta4-0.3.4/ta4/sentence.py
@@ -0,0 +1,45 @@
+#! coding: utf-8
+from operator import attrgetter
+
+
+class Sentence(object):
+    __slots__ = ['text', 'place_holders']
+
+    def __init__(self, text, placeholders=None):
+        self.text = text
+        self.place_holders = placeholders or []
+        if not self.place_holders:
+            from .utils import get_sentences
+            _, sentences = get_sentences(text)
+            self.place_holders = sentences[0].place_holders
+
+    def __len__(self):
+        return len(self.place_holders)
+
+    def __repr__(self):
+        return self.text.encode('utf-8')
+
+    def __iter__(self):
+        for ph in self.place_holders:
+            yield ph
+
+    def __getitem__(self, item):
+        return self.place_holders[item]
+
+    @property
+    def is_exact_task(self):
+        """
+        Если задание по словоформам - вернёт False(например "[пластиковые] [окна]")
+        """
+        return not any(map(attrgetter('is_subform_word'), self.place_holders))
+
+    @property
+    def is_special(self):
+        return any(map(attrgetter('is_special'), self.place_holders))
+
+    def count(self, word):
+        return len([ph for ph in self.place_holders if ph.word == word])
+
+    @property
+    def exact_count(self):
+        return len(filter(lambda x: not x.is_subform_word, self.place_holders))
