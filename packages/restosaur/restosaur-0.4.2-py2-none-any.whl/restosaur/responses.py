a5ab6ede95b3e7b5c019815e1fcc3fc433c815ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:41 +0000

    Add package restosaur/0.4.2/restosaur-0.4.2-py2-none-any.whl

---
+++ packages/restosaur/restosaur-0.4.2-py2-none-any.whl/restosaur/responses.py
@@ -0,0 +1,119 @@
+from django.utils.http import http_date
+from .utils import model_to_dict
+import times
+
+
+class Response(object):
+    def __init__(self, context, data=None, status=200, headers=None,
+            last_modified=None, extra=None):
+        self.headers = {}
+        self.headers.update(headers or {})
+        self.representation = None
+        self.content_type = None
+        self.context = context
+        self.status = status
+        self.extra = extra
+        self.data = data
+        if last_modified:
+            self.set_last_modified(last_modified)
+
+    def get_converter(self, representation):
+        dummy_converter = lambda x, context: x
+        converter = self.context.resource.representations.get(representation)
+        return converter or dummy_converter
+
+    def set_last_modified(self, dt):
+        if dt:
+            self.headers['Last-Modified'] = http_date(times.to_unix(dt))
+        else:
+            self.headers.pop('Last-Modified', None)
+
+    def serialize(self, data, representation):
+        if data is None and not self.extra:
+            return ''
+        else:
+            if self.status >= 200 and self.status <300:
+                output = {}
+                output.update(self.extra or {})
+                output.update(self.context.resource.convert(self.context, data, representation))
+            else:
+                output = data
+            return output
+
+
+class CreatedResponse(Response):
+    def __init__(self, context, data=None, headers=None):
+        super(CreatedResponse, self).__init__(context, data=data, status=201,
+                headers=headers)
+
+
+class NoContentResponse(Response):
+    def __init__(self, context, data=None, headers=None):
+        super(NoContentResponse, self).__init__(context, data=data, status=204,
+                headers=headers)
+
+
+class NotModifiedResponse(Response):
+    def __init__(self, context, data=None, headers=None):
+        super(NotModifiedResponse, self).__init__(context, data=data, status=304,
+                headers=headers)
+
+
+class UnauthorizedResponse(Response):
+    def __init__(self, context, data=None, headers=None):
+        super(UnauthorizedResponse, self).__init__(context, data=data,
+                status=401, headers=headers)
+
+
+class ForbiddenResponse(Response):
+    def __init__(self, context, data=None, headers=None):
+        super(ForbiddenResponse, self).__init__(context, data=data, status=403,
+                headers=headers)
+
+
+class NotFoundResponse(Response):
+    def __init__(self, context, data=None, headers=None):
+        super(NotFoundResponse, self).__init__(context, data=data, status=404,
+                headers=headers)
+
+
+class MethodNotAllowedResponse(Response):
+    def __init__(self, context, data=None, headers=None):
+        super(MethodNotAllowedResponse, self).__init__(context, data=data,
+                status=405, headers=headers)
+
+
+class CollectionResponse(Response):
+    def __init__(self, context, iterable, totalCount=None, key=None, **kwargs):
+        super(CollectionResponse, self).__init__(context, data=iterable,
+                **kwargs)
+        self.key = key or 'items'
+        self.totalCount = totalCount
+
+    def serialize(self, iterable, representation):
+        resp = {
+                self.key: map(lambda x: self.context.resource.convert(self.context, x, representation), iterable),
+                'totalCount': self.totalCount if self.totalCount is not None else len(iterable),
+                }
+        resp.update(self.extra or {})
+        return resp
+
+
+class EntityResponse(Response):
+    pass
+
+
+class NotAcceptableResponse(Response):
+    def __init__(self, context, headers=None):
+        super(NotAcceptableResponse, self).__init__(context, data=None,
+                status=406, headers=headers)
+
+
+class ValidationErrorResponse(Response):
+    def __init__(self, context, errors, headers=None):
+        resp = {
+                'errors': errors,
+                }
+        super(ValidationErrorResponse, self).__init__(context, data=resp,
+                status=422, headers=headers)
+
