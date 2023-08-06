66e70ed05d1e4b6f8be7a3212b22a714a8005d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:55 +0000

    Add package aioxmpp/0.5.1/aioxmpp-0.5.1.tar.gz

---
+++ packages/aioxmpp/aioxmpp-0.5.1.tar.gz/aioxmpp-0.5.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import codecs
+import os.path
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+import aioxmpp
+
+setup(
+    name="aioxmpp",
+    version=aioxmpp.__version__,
+    description="Pure-python XMPP library for asyncio",
+    long_description=long_description,
+    url="https://github.com/horazont/aioxmpp",
+    author="Jonas Wielicki",
+    author_email="jonas@wielicki.name",
+    license="Apache20",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Communications :: Chat",
+    ],
+    keywords="asyncio xmpp library",
+    install_requires=['dnspython3',
+                      'lxml',
+                      'orderedset',
+                      'pyOpenSSL',
+                      'pyasn1',
+                      'pyasn1_modules',
+                      'tzlocal'],
+    packages=find_packages(exclude=["tests*"])
+)
