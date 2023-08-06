75ce4504c7ff179acb070c2ba806b142006e350a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:11 +0000

    Add package Prevening_wuwei/1.0.0/Prevening_wuwei-1.0.0.zip

---
+++ packages/Prevening_wuwei/Prevening_wuwei-1.0.0.zip/Prevening_wuwei-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = "Prevening_wuwei",
+    version = "1.0.0",
+    py_modules = ["nester"],
+    author = "wuweipython",
+    author_email = "jxdyww2011@163.com",
+    url = "http://www.headfirstlabs.com",
+    description = "A simple printer of nested lists",
+    )
