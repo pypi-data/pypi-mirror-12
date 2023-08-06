80fc35b42e72cadf533063046e9de39cb768ed54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:03 +0000

    Add package datalake/0.11.0/datalake-0.11.0.tar.gz

---
+++ packages/datalake/datalake-0.11.0.tar.gz/datalake-0.11.0/datalake/dlfile.py
@@ -0,0 +1,98 @@
+# Copyright 2015 Planet Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import os
+from pyblake2 import blake2b
+from translator import Translator
+
+from datalake_common import Metadata
+
+
+class File(object):
+    '''A File to be manipulated by the Archive'''
+
+    def __init__(self, fd, **metadata_fields):
+        '''Create a File
+
+        Args:
+
+            fd: file-like object from which the file data can be read.
+
+            metadata_fields: known metadata fields that go with this
+            file. Missing fields will be added if they can be
+            determined. Othwerise, InvalidDatalakeMetadata will be raised.
+
+        '''
+        self._fd = fd
+        self._initialize_methods_from_fd()
+        self._infer_metadata_fields(metadata_fields)
+        self.metadata = Metadata(metadata_fields)
+
+    @classmethod
+    def from_filename(cls, filename, **metadata_fields):
+        '''Create a File from a filename
+
+        This is a convenience method that opens the file and adds the filename
+        to the metadata_fields for convenience.
+
+        '''
+        filename = os.path.abspath(filename)
+        if 'path' not in metadata_fields:
+            metadata_fields['path'] = filename
+        fd = open(filename)
+        return cls(fd, **metadata_fields)
+
+    def _initialize_methods_from_fd(self):
+        for m in ['read', 'readlines', 'seek', 'tell', 'close']:
+            setattr(self, m, getattr(self._fd, m))
+
+    def _infer_metadata_fields(self, metadata_fields):
+        self._infer_hash(metadata_fields)
+        self._infer_where(metadata_fields)
+        self._apply_translations(metadata_fields)
+
+    def _infer_hash(self, metadata_fields):
+        if 'hash' not in metadata_fields:
+            # do not recalculate the hash if it is already known
+            metadata_fields['hash'] = self._calculate_hash()
+
+    def _infer_where(self, metadata_fields):
+        default_where = os.environ.get('DATALAKE_DEFAULT_WHERE')
+        where = metadata_fields.get('where')
+        if where is None and default_where is not None:
+            metadata_fields['where'] = default_where
+
+    def _apply_translations(self, metadata_fields):
+        for f in ['where', 'what', 'work_id']:
+            value = metadata_fields.get(f)
+            if value is None or '~' not in value:
+                continue
+            t = Translator(value)
+            metadata_fields[f] = t.translate(metadata_fields['path'])
+
+    _HASH_BUF_SIZE = 65536
+
+    def _calculate_hash(self):
+        '''16-byte blake2b hash over the content of this file'''
+        # this takes just under 2s on my laptop for a 1GB file.
+        b2 = blake2b(digest_size=16)
+        current = self.tell()
+        self.seek(current)
+        while True:
+            data = self.read(self._HASH_BUF_SIZE)
+            if not data:
+                break
+            b2.update(data)
+        self.seek(current)
+        return b2.hexdigest()
---
+++ packages/datalake/datalake-0.11.0.tar.gz/datalake-0.11.0/test/test_file.py
@@ -0,0 +1,64 @@
+# Copyright 2015 Planet Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import pytest
+from datalake_common.tests import random_word, random_metadata
+from datalake_common import InvalidDatalakeMetadata
+
+from datalake import File
+
+
+def random_file(tmpdir, metadata=None):
+    name = random_word(10)
+    content = random_word(256)
+    f = tmpdir.join(name)
+    f.write(content)
+    if metadata is None:
+        metadata = random_metadata()
+    return File.from_filename(f.strpath, **metadata)
+
+
+@pytest.fixture
+def random_files(tmpdir):
+    def get_randfiles(n):
+        return [random_file(tmpdir) for _ in range(n)]
+    return get_randfiles
+
+
+def test_file_hash_different(random_files):
+    files = random_files(2)
+    assert files[0].metadata['hash'] != files[1].metadata['hash']
+
+
+def test_non_existent_file():
+    with pytest.raises(IOError):
+        File.from_filename('surelythisfiledoesnotexist.txt')
+
+
+def test_not_enough_metadata(tmpdir):
+    with pytest.raises(InvalidDatalakeMetadata):
+        random_file(tmpdir, metadata={'where': 'foo'})
+
+
+def test_hash_not_overwritten(tmpdir, random_metadata):
+    random_metadata['hash'] = '1234'
+    f = random_file(tmpdir, metadata=random_metadata)
+    assert f.metadata['hash'] == random_metadata['hash']
+
+
+def test_default_where(monkeypatch, tmpdir, random_metadata):
+    monkeypatch.setenv('DATALAKE_DEFAULT_WHERE', 'here')
+    del(random_metadata['where'])
+    f = random_file(tmpdir, metadata=random_metadata)
+    assert f.metadata['where'] == 'here'
