cfa25744a60c7ae0eafa2d67aa0489bdb7e4a2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:04 +0000

    Add package list2016/1.1.0/list2016-1.1.0.tar.gz

---
+++ packages/list2016/list2016-1.1.0.tar.gz/list2016-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import  setup
+
+setup(
+	name='list2016',
+	version='1.1.0',
+	py_modules=['list2016'],
+	author='xiaoxiao',
+	author_email='mng1990@163.com',
+	url ='http://www.baidu.com',
+	description='a simple printer of list',
+	)
