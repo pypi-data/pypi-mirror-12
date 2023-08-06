cc047c601513147841b314a8dd4f9b56fe997fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:59 +0000

    Add package django-knockout-modeler/0.10.1/django-knockout-modeler-0.10.1.tar.gz

---
+++ packages/django-knockout-modeler/django-knockout-modeler-0.10.1.tar.gz/django-knockout-modeler-0.10.1/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup
+
+# Set external files
+try:
+    from pypandoc import convert
+    README = convert('README.md', 'rst')	 
+except ImportError:
+    README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    required = f.read().splitlines()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-knockout-modeler',
+    version='0.10.1',
+    packages=['knockout_modeler'],
+    install_requires=required,
+    include_package_data=True,
+    license='MIT License',
+    description='Super easy ModelViews for knockout.js for your Django models',
+    long_description=README,
+    url='https://github.com/Miserlou/django-knockout-modeler',
+    author='Rich Jones',
+    author_email='rich@openwatch.net',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
