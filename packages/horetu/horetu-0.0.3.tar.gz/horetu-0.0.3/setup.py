4191b51d588602efd69e4141ab846cd446df26fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:23 +0000

    Add package horetu/0.0.3/horetu-0.0.3.tar.gz

---
+++ packages/horetu/horetu-0.0.3.tar.gz/horetu-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='horetu',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Make a command-line interface from a function.',
+      url='http://dada.pink/horetu/',
+      py_modules=['horetu'],
+      install_requires = [
+          'Sphinx>=1.3.1',
+          'inflection>=0.3.1',
+      ],
+      tests_require = [
+          'pytest>=2.6.4',
+      ],
+      version='0.0.3',
+      license='AGPL',
+)
