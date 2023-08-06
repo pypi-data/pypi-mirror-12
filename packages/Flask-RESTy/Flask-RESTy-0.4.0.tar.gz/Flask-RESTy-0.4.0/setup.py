f5970a886e2fef840946b989ff53cc0862d00ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:28 +0000

    Add package Flask-RESTy/0.4.0/Flask-RESTy-0.4.0.tar.gz

---
+++ packages/Flask-RESTy/Flask-RESTy-0.4.0.tar.gz/Flask-RESTy-0.4.0/setup.py
@@ -0,0 +1,62 @@
+from setuptools import Command, setup
+import subprocess
+
+# -----------------------------------------------------------------------------
+
+
+def system(command):
+    class SystemCommand(Command):
+        user_options = []
+
+        def initialize_options(self):
+            pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            subprocess.check_call(command, shell=True)
+
+    return SystemCommand
+
+
+# -----------------------------------------------------------------------------
+
+setup(
+    name="Flask-RESTy",
+    version='0.4.0',
+    description="Building blocks for REST APIs for Flask",
+    url='https://github.com/4Catalyzer/flask-resty',
+    author="Jimmy Jia",
+    author_email='tesrin@gmail.com',
+    license='MIT',
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Flask',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    keywords='rest flask',
+    packages=('flask_resty', 'flask_resty.ext'),
+    install_requires=(
+        'Flask >= 0.10',
+        'Flask-SQLAlchemy >= 1.0',
+        'marshmallow >= 2.2.0',
+        'SQLAlchemy >= 1.0.0',
+        'Werkzeug >= 0.11',
+    ),
+    extras_require={
+        'jwt': ('PyJWT >= 1.4.0',),
+    },
+    cmdclass={
+        'pandoc': system('pandoc README.md -o README.rst'),
+        'release': system('python setup.py pandoc sdist upload'),
+        'test': system('flake8 . && py.test --cov'),
+    },
+)
