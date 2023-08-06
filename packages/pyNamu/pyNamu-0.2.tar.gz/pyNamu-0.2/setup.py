6c4300cf7679b6253f4458149b03b903e40668de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:00 +0000

    Add package pyNamu/0.2/pyNamu-0.2.tar.gz

---
+++ packages/pyNamu/pyNamu-0.2.tar.gz/pyNamu-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'pyNamu',
+  packages = ['pyNamu'], # this must be the same as the name above
+  version = '0.2',
+  description = 'My First Package!',
+  author = 'Naman Jain',
+  author_email = 'namanjn07@hotmail.com',
+  url = 'https://github.com/Namanjn/namu', # use the URL to the github repo
+  download_url = 'https://github.com/Namanjn/PyNamu/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
