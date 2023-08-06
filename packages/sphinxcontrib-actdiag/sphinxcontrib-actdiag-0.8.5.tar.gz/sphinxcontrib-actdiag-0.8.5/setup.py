7dc738578d2d713eb8567106b6a5a5f1f506eceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:01 +0000

    Add package sphinxcontrib-actdiag/0.8.5/sphinxcontrib-actdiag-0.8.5.tar.gz

---
+++ packages/sphinxcontrib-actdiag/sphinxcontrib-actdiag-0.8.5.tar.gz/sphinxcontrib-actdiag-0.8.5/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+requires = ['blockdiag>=1.5.0', 'actdiag>=0.5.3', 'Sphinx>=0.6']
+
+setup(
+    name='sphinxcontrib-actdiag',
+    version='0.8.5',
+    url='http://github.com/blockdiag/sphinxcontrib-actdiag',
+    license='BSD',
+    author='Takeshi KOMIYA',
+    author_email='i.tkomiya@gmail.com',
+    description='Sphinx "actdiag" extension',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Documentation',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
