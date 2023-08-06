43c642f32d3b1a37ecd5294713917a0dde2efae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:45 +0000

    Add package mypython/1.0.0/mypython-1.0.0.zip

---
+++ packages/mypython/mypython-1.0.0.zip/mypython-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+
+
+setup(
+    
+name          = 'mypython',
+    
+version       = '1.0.0',
+    
+py_modules = ['nester'],
+    
+author          = 'Stephen',
+    
+author_email = 'hetianbiao@126.com',
+    
+url                  = 'http://www.headfirstlabs.com',
+    
+description   = 'A simple printer of nested lists',
+    
+)
