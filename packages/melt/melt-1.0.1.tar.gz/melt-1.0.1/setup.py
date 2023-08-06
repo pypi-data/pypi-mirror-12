c2b2f6bbe9d8751811d795551a7a957b7a78b93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:44 +0000

    Add package melt/1.0.1/melt-1.0.1.tar.gz

---
+++ packages/melt/melt-1.0.1.tar.gz/melt-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+VERSION = open('VERSION').read().strip()
+
+setup(
+    name="melt",
+    py_modules=['melting'],
+    version=VERSION,
+    author='Erik Clarke',
+    author_email='ecl@mail.med.upenn.edu',
+    url='https://github.com/eclarke/melt',
+    description='A nucleotide melt temp calculator',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'        
+    ],
+    license='GPL 3',
+    entry_points={'console_scripts': ['Tm = melting:main']}
+)
