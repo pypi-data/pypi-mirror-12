a720506928c0090b57787afccdce6913c8d60142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:52 +0000

    Add package simpleflow/0.10.0/simpleflow-0.10.0.tar.gz

---
+++ packages/simpleflow/simpleflow-0.10.0.tar.gz/simpleflow-0.10.0/simpleflow/settings/default.py
@@ -0,0 +1,33 @@
+import logging  # NOQA
+
+
+WORKFLOW_DEFAULT_TASK_LIST = 'default'
+WORKFLOW_DEFAULT_VERSION = 'default'
+WORKFLOW_DEFAULT_EXECUTION_TIME = str(60 * 60)  # 1 hour.
+WORKFLOW_DEFAULT_DECISION_TASK_TIMEOUT = str(5 * 60)  # 5 minutes.
+
+ACTIVITY_DEFAULT_TASK_LIST = 'default'
+ACTIVITY_DEFAULT_VERSION = 'default'
+ACTIVITY_DEFAULT_TIMEOUT = str(5 * 60)  # 5 minutes.
+ACTIVITY_START_TO_CLOSE_TIMEOUT = ACTIVITY_DEFAULT_TIMEOUT
+ACTIVITY_SCHEDULE_TO_CLOSE_TIMEOUT = ACTIVITY_DEFAULT_TIMEOUT
+ACTIVITY_SCHEDULE_TO_START_TIMEOUT = ACTIVITY_DEFAULT_TIMEOUT
+ACTIVITY_HEARTBEAT_TIMEOUT = ACTIVITY_DEFAULT_TIMEOUT
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'loggers': {
+        'simpleflow': {
+            'level': 'INFO',
+            'handlers': ['console'],
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'stream': 'ext://sys.stderr',
+        },
+    },
+}
