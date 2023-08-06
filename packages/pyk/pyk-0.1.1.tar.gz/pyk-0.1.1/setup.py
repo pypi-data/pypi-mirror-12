fff18a6613f8158a6ca69aed9264573762e1a90a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:33 +0000

    Add package pyk/0.1.1/pyk-0.1.1.tar.gz

---
+++ packages/pyk/pyk-0.1.1.tar.gz/pyk-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyk",
+    version="0.1.1",
+    description="A simple, yet useful Kubernetes toolkit in Python",
+    author="Michael Hausenblas",
+    author_email="michael.hausenblas@gmail.com",
+    license="Apache",
+    url="https://github.com/mhausenblas/pyk",
+    keywords = ['Kubernetes', 'containers', 'appops'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "PyYAML"
+    ],
+)
