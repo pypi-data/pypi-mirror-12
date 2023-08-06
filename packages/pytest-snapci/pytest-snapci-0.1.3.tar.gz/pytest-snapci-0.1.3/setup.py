a1ca13b3114fc688a6983c9f71e81f224a3bbc9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:31 +0000

    Add package pytest-snapci/0.1.3/pytest-snapci-0.1.3.tar.gz

---
+++ packages/pytest-snapci/pytest-snapci-0.1.3.tar.gz/pytest-snapci-0.1.3/setup.py
@@ -0,0 +1,27 @@
+import textwrap
+
+from setuptools import setup
+
+from pytest_snapci import __version__
+from pytest_snapci import plugin
+
+setup(
+    name="pytest-snapci",
+    version=__version__,
+    description="py.test plugin for Snap-CI",
+    long_description=textwrap.dedent(plugin.pytest_collection_modifyitems.__doc__),
+    author="Omer Katz",
+    author_email="omer.drow@gmail.com",
+    url="https://github.com/thedrow/pytest-snapci",
+    packages=["pytest_snapci"],
+    entry_points={
+        'pytest11': [
+            'snapci = pytest_snapci.plugin'
+        ],
+    },
+    classifiers=(
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        # "Programming Language :: Python :: 3",  Not tested yet
+    ),
+)
