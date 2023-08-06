d670f41827629e7b8899e78e88a7be6eca67eba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:49 +0000

    Add package koday_modules/0.1.0/koday_modules-0.1.0.tar.gz

---
+++ packages/koday_modules/koday_modules-0.1.0.tar.gz/koday_modules-0.1.0/setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages  
+version = '0.1.0'
+
+# pypandocを使ってREADME.mdをrstに変換する。最初からrstで書いた場合は不要。
+try:  
+    import pypandoc
+    read_md = lambda f: pypandoc.convert(f, 'rst')
+except ImportError:  
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+setup(  
+        name='koday_modules', # パッケージ名
+        version=version, # バージョン
+        description="A simple wrapper for multiple tokens of multiple APIs", # 一言で説明
+        # 詳細は http://pypi.python.org/pypi?:action=list_classifiers を参照
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+            "Topic :: Utilities",
+            "License :: OSI Approved :: MIT License",
+            ],
+        # キーワードを書いておく
+        keywords='ontology, mecab',
+        # 作者の名前
+        author='koday',
+        # 連絡先
+        author_email='kodai.nakamura.215@gmail.com',
+        # GitHubのリポジトリとか
+#         url='https://github.com/AkihikoITOH/capybara',
+        # ライセンス
+        license='MIT',
+        packages=find_packages(exclude=['examples', 'tests']),
+        include_package_data=True,
+        zip_safe=True,
+        # Pypiのページで表示する説明文(README)
+        long_description=read_md('README.md'),
+        # インストールする依存パッケージ
+        # install_requires=["python-amazon-simple-product-api", "requests"],
+    )
