d2339c6c01049546fc2dce3acc74a0f6c338568d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:53 +0000

    Add package mtgjson/0.4.0/mtgjson-0.4.0.tar.gz

---
+++ packages/mtgjson/mtgjson-0.4.0.tar.gz/mtgjson-0.4.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    buf = open(os.path.join(os.path.dirname(__file__), fname), 'rb').read()
+    return buf.decode('utf8')
+
+
+setup(name='mtgjson',
+      version='0.4.0',
+      description='A python library for working with data from mtgjson.com.',
+      long_description=read('README.rst'),
+      author='Marc Brinkmann',
+      author_email='git@marcbrinkmann.de',
+      url='http://github.com/mbr/pymtgjson',
+      license='MIT',
+      packages=find_packages(exclude=['tests']),
+      install_requires=['docutils', 'requests', 'six'],
+      classifiers=[
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ])
