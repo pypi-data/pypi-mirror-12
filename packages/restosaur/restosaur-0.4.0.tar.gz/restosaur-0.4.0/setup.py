c8b3cd4601a940b052804bbd6c2b97a5672e3202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:35 +0000

    Add package restosaur/0.4.0/restosaur-0.4.0.tar.gz

---
+++ packages/restosaur/restosaur-0.4.0.tar.gz/restosaur-0.4.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name='restosaur',
+      version='0.4.0',
+      description='Damn simple RESTful library',
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        ],
+      author='Marcin Nowak',
+      author_email='marcin.j.nowak@gmail.com',
+      url='https://github.com/marcinn/restosaur',
+      install_requires = ['mimeparse', 'times>=0.7'],
+      keywords='web rest python django',
+      packages=find_packages('.'),
+      include_package_data=True,
+      zip_safe=True,
+      )
