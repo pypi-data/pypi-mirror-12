d75e10f0e2cdfedb0f7d4e5fc0f7cb1a49642d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:43 +0000

    Add package reportermd/0.0.5/reportermd-0.0.5.tar.gz

---
+++ packages/reportermd/reportermd-0.0.5.tar.gz/reportermd-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='reportermd',
+    packages=['reportermd'],
+    version='0.0.5',
+    description='Convert your Reporter App dates to MD then import to Day One',
+    author='Sam Beveridge',
+    author_email='sam.bev87@gmail.com',
+    license='MIT',
+    url='https://github.com/sambev/reportermd',
+    entry_points={
+        'console_scripts': [
+            'reportermd=reportermd.cli:main',
+        ]
+    },
+    include_package_data=True,
+    install_requires=[
+        'Jinja2>=2.8',
+        'python-dateutil==2.4.2',
+    ]
+)
