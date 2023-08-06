771254d743621fd635aab742fc3f295da2c2b4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:07 +0000

    Add package randomfoldername/3.3.0/randomfoldername-3.3.0.zip

---
+++ packages/randomfoldername/randomfoldername-3.3.0.zip/randomfoldername-3.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'randomfoldername', 
+        version      = '3.3.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['randomfoldername'],
+        author       = 'hfpython',
+        author_email = 'hfpython@headfirstlabs.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+     )
