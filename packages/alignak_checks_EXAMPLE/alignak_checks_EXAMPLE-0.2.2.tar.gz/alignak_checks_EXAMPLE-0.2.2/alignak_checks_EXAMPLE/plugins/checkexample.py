0575f96154ba8c6fca53d40c12120fe498c38bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:19 +0000

    Add package alignak_checks_EXAMPLE/0.2.2/alignak_checks_EXAMPLE-0.2.2.tar.gz

---
+++ packages/alignak_checks_EXAMPLE/alignak_checks_EXAMPLE-0.2.2.tar.gz/alignak_checks_EXAMPLE-0.2.2/alignak_checks_EXAMPLE/plugins/checkexample.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015:
+#   Frederic Mohier, frederic.mohier@gmail.com
+#
+
+"""
+    Alignak - Check plugin as an example ...
+"""
+
+
+def main():
+    print("Plugin example")
+
+if __name__ == "__main__":
+    main()
