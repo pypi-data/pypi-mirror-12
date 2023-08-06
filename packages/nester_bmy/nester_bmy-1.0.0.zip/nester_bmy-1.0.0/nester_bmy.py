c50c3edce56238bf558397998cfa37a77c816f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:27 +0000

    Add package nester_bmy/1.0.0/nester_bmy-1.0.0.zip

---
+++ packages/nester_bmy/nester_bmy-1.0.0.zip/nester_bmy-1.0.0/nester_bmy.py
@@ -0,0 +1,12 @@
+'''这是"nester.py"模块,提供了一个名为print_lol()的函数,这个函数的作用是打印列表。
+其中有可能包含（也可能不包含）且套列表。'''
+def print_lol(the_list):
+        for each_item in the_list:
+                """这个函数去一个位置参数,名为"the_list",这可以使任何python列表
+               (也可以是包含嵌套列表的列表)所指的列表中的每个数据项会（递归的）输出到屏幕上,个数据项各占一行"""
+                if isinstance(each_item,list):
+                        #回调函数
+                        print_lol(each_item)
+                else:
+                        #打印输出
+                        print(each_item)
