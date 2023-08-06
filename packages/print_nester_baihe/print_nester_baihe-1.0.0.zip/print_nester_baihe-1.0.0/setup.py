196e663d1e8f17d466b2147ff003f5aa48650dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:28 +0000

    Add package print_nester_baihe/1.0.0/print_nester_baihe-1.0.0.zip

---
+++ packages/print_nester_baihe/print_nester_baihe-1.0.0.zip/print_nester_baihe-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'print_nester_baihe',
+        version      = '1.0.0',
+        py_modules   = ['nester'],
+        author       = 'bhpython',
+        author_email = 'bh7618@gmail.com',
+        url          = 'http://www.baihe.com',
+        description  = 'A simple printer of nested lists',
+      )
