9a69b5c0c220d792cfdf6de72dbb2401017466b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:11 +0000

    Add package packageversion/0.0.1/packageversion-0.0.1.tar.gz

---
+++ packages/packageversion/packageversion-0.0.1.tar.gz/packageversion-0.0.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+try:
+    from packageversion import PackageVersion
+    pv = PackageVersion()
+    v = pv.generate_next_stable(package_name='packageversion')
+except ImportError:
+    v = '1.0.0'
+
+setup(name='packageversion',
+      version=v,
+      description='Library to generate python package version for CI',
+      author='Jon Skarpeteig',
+      author_email='jon.skarpeteig@gmail.com',
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      url='https://github.com/Yuav/python-packageversion',
+      packages=find_packages(),
+      install_requires=[
+          'semantic_version',
+          'flexmock'
+      ]
+      )
