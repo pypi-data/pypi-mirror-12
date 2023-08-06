a39a0e739db22f61bd6f050fc43d55788e5cb2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:01 +0000

    Add package SRNG/0.2/SRNG-0.2.tar.gz

---
+++ packages/SRNG/SRNG-0.2.tar.gz/SRNG-0.2/srng/srng.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""SRNG is a statistical random number generator."""
+
+
+__author__ = 'Mansour Moufid'
+__copyright__ = 'Copyright 2015, Mansour Moufid'
+__license__ = 'ISC'
+__version__ = '0.2'
+__email__ = 'mansourmoufid@gmail.com'
+__status__ = 'Development'
+
+
+if __name__ == '__main__':
+    pass
