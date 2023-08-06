9c88985eb96dbb0f9fb0f6fc2a2db9ed39ad1038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:50 +0000

    Add package remotecv/2.0.0rc1/remotecv-2.0.0rc1.tar.gz

---
+++ packages/remotecv/remotecv-2.0.0rc1.tar.gz/remotecv-2.0.0rc1/remotecv/detectors/__init__.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/globocom/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com timehome@corp.globo.com
+
+from os.path import join, dirname, abspath, isabs
+
+import cv2
+import numpy as np
+
+
+class BaseDetector(object):
+    def get_np_img(self, image):
+        return np.array(image)
+
+    def detect(self, context):
+        raise NotImplementedError()
+
+
+class CascadeLoaderDetector(BaseDetector):
+
+    def load_cascade_file(self, module_path, cascade_file_path):
+        # if not hasattr(self.__class__, 'cascade'):
+        #     if isabs(cascade_file_path):
+        #         cascade_file = cascade_file_path
+        #     else:
+        cascade_file = join(abspath(dirname(module_path)), cascade_file_path)
+        self.__class__.cascade = cv2.CascadeClassifier(cascade_file)
+
+    def get_features(self, image):
+        img = self.get_np_img(image)
+
+        faces = self.__class__.cascade.detectMultiScale(
+            img,
+            1.2,
+            2,
+        )
+        faces_scaled = []
+
+        for (x, y, w, h) in faces:
+            faces_scaled.append(((x, y, w, h), 0))
+
+        return faces_scaled
+
+    def detect(self, image):
+        features = self.get_features(image)
+
+        if features:
+            points = [[left, top, width, height] for (left, top, width, height), neighbors in features]
+        else:
+            points = []
+
+        return points
