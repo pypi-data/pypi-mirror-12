361e587897f071867326ff08445f81e69e98f834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package pykwiki2/2.0.3/pykwiki2-2.0.3.tar.gz

---
+++ packages/pykwiki2/pykwiki2-2.0.3.tar.gz/pykwiki2-2.0.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+import sys
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(__file__))
+from pykwiki2 import __version__
+
+setup(name='pykwiki2',
+    version = __version__,
+    description = 'A simple pure-python wiki engine.',
+    long_description = '''
+A pure-python, light-weight, static wiki, blog, and website engine designed to be
+fast to setup, secure, and to handle lots of web traffic.
+    ''',
+    author = 'Aaron Meier',
+    author_email = 'webgovernor@gmail.com',
+    packages = ['pykwiki2','pykwiki2.ext'],
+    package_dir={'pykwiki2':'pykwiki2'},
+    scripts=['pykwiki2/scripts/pykwiki2'],
+    package_data={'pykwiki2':['data/*.zip', 'data/default_theme/', 'data/default_theme/*']},
+    url = 'http://pykwiki.nullism.com',
+    license = 'MIT',
+    install_requires = ['markdown>=2.6', 'jinja2>=2.6', 'pyyaml>=3.0', 'pyscss>=1.3.4'],
+    provides = ['pykwiki2']
+)
+
