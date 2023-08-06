e236b00cc40bd594e17549cf224a42fd56c3e789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:23 +0000

    Add package ascribe/0.0.5.dev1/ascribe-0.0.5.dev1.tar.gz

---
+++ packages/ascribe/ascribe-0.0.5.dev1.tar.gz/ascribe-0.0.5.dev1/README.rst
@@ -0,0 +1,44 @@
+ascribe SPOOL API Python Wrapper
+================================
+
+This is a communauty-based python wrapper around the `ascribe ownership REST
+API <http://docs.ascribe.apiary.io>`_.
+
+
+Installation
+------------
+
+.. code-block:: python
+
+    pip install ascribe
+
+
+Usage Example: Registering a Piece
+----------------------------------
+
+.. code-block:: python
+    
+    >>> from ascribe import AscribeWrapper
+    >>> ascribe_wrapper = AscribeWrapper('bearer your-token')
+    >>> piece = {
+        'file_url': 'https://s3.eu-central-1.amazonaws.com/bucket/img.jpg',
+        'artist_name': 'mystery',
+        'title': 'universe',
+    }
+    >>> ascribe_wrapper.create_piece(piece)
+    {'piece': {
+        'artist_name': 'mystery',
+        'bitcoin_id': 'bitcoin-id',
+        'date_created': '2015-01-01',
+        'datetime_registered': '2015-11-28T20:42:19.917896Z',
+        'license_type': {
+            'code': 'default',
+            'name': 'All rights reserved',
+            'organization': 'ascribe',
+            'url': 'https://www.ascribe.io/faq/#legals'
+        },
+        'title': 'universe',
+        'user_registered': 'mystery'
+        ...
+        ...
+    }}
---
+++ packages/ascribe/ascribe-0.0.5.dev1.tar.gz/ascribe-0.0.5.dev1/setup.py
@@ -0,0 +1,49 @@
+from codecs import open
+from os import path
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ascribe',
+    version='0.0.5.dev1',
+    description='ascribe api',
+    long_description=long_description,
+    url='https://github.com/ascribe/ascribe-api-wrapper',
+    author='Oskar Paolini, Sylvain Bellemare',
+    author_email='sylvain@ascribe.io',
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords=(
+        'intellectual property, attribution, ownership, art, blockchain, api '
+        'digital work, provenance, spool'
+    ),
+
+    py_modules=["ascribe"],
+
+    install_requires=['requests'],
+
+    extras_require={
+        'test': ['coverage', 'pytest'],
+    },
+)
