464cb50ee045c863ddb9994a2b0669501c9c949b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:39 +0000

    Add package CLAUDE-tools/0.1.8a1/CLAUDE_tools-0.1.8a1.tar.gz

---
+++ packages/CLAUDE-tools/CLAUDE_tools-0.1.8a1.tar.gz/CLAUDE_tools-0.1.8a1/receiver/__init__.py
@@ -0,0 +1,32 @@
+'''
+Created on Sep 10, 2015
+
+@author: andrei
+'''
+from abc import ABCMeta, abstractmethod
+import logging
+
+import tools.messaging as messaging
+import tools.auxiliary as auxiliary
+
+class MessageReceiver(object):
+    '''
+    classdocs
+    '''
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def callback(self, message):
+        pass
+    
+    def run(self):
+        consumer = messaging.MessageConsumer(
+                                                    messaging.buildRabbitMQConnectionLink(),
+                                                    self.queue,
+                                                    self.callback,
+                                                    )
+        consumer.constantConsuming()
+    
+    def __init__(self, log_file, queue, log_lvl = logging.DEBUG):
+        self.logger = auxiliary.initLogger(log_file, log_lvl)
+        self.queue = queue
