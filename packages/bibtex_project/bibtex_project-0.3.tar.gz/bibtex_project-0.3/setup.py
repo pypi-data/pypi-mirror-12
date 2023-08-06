2f466f10401c5885f8fb072a6156afb828d5fd2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:02 +0000

    Add package bibtex_project/0.3/bibtex_project-0.3.tar.gz

---
+++ packages/bibtex_project/bibtex_project-0.3.tar.gz/bibtex_project-0.3/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+#This is a list of files to install, and where
+#(relative to the 'root' dir, where setup.py is)
+#You could be more specific.
+# files = ["things/*"]
+
+# setup(name = "testing_packaging",
+#     version = "100",
+#     description = "Testing packaging",
+#     author = "Zia",
+#     author_email = "email@someplace.com",
+#     url = "whatever",
+#     #Name the folder where your packages live:
+#     #(If you have other packages (dirs) or modules (py files) then
+#     #put them into the package directory - they will be found
+#     #recursively.)
+#     packages = ['package'],
+#     #'package' package must contain files (see list above)
+#     #I called the package 'package' thus cleverly confusing the whole issue...
+#     #This dict maps the package name =to=> directories
+#     #It says, package *needs* these files.
+#     package_data = {'package' : files },
+#     #'runner' is in the root.
+#     scripts = ["runner"],
+#     long_description = """Really long text here."""
+#     #
+#     #This next part it for the Cheese Shop, look a little down the page.
+#     #classifiers = []
+# )
+
+setup(name='bibtex_project',
+      version='0.3',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['bibtex_project'],
+      install_requires=[
+          'bibtexparser','gitpython','pymongo'
+      ],      
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=['script'],      
+      zip_safe=False)
