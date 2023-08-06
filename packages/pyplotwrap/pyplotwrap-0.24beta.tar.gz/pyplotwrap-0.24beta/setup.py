a05da95f4ec4078ede91c10bd247caf7ab7f264b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:22 +0000

    Add package pyplotwrap/0.24beta/pyplotwrap-0.24beta.tar.gz

---
+++ packages/pyplotwrap/pyplotwrap-0.24beta.tar.gz/pyplotwrap-0.24beta/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+def readme():
+    with open('README.txt') as f:
+        return f.read()
+
+def license():
+    with open('License_notice.txt') as f:
+        return f.read()
+
+    with open('GNU_GPL.txt') as f:
+        return f.read()
+
+setup(
+    name='pyplotwrap',
+    version='0.24beta',
+    packages=['pyplotwrap',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='A way to plot data directly from python dictionaries',
+    long_description=open('README.txt').read(),
+    url='https://github.com/nenanth/python_plot_wrappers',
+    author='Ananth Sridharan',
+    author_email='sridharan.ananth@gmail.com',
+)
