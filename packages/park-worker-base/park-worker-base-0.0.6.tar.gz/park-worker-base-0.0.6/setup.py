a56272546575ead3e2556affd13dbc3b7285d317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:21 +0000

    Add package park-worker-base/0.0.6/park-worker-base-0.0.6.tar.gz

---
+++ packages/park-worker-base/park-worker-base-0.0.6.tar.gz/park-worker-base-0.0.6/setup.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+# python setup.py sdist register upload
+from distutils.core import setup
+
+setup(
+    name='park-worker-base',
+    version='0.0.6',
+    description='Base logic of workers for park-keeper project.',
+    author='Telminov Sergey',
+    url='https://github.com/telminov/park-worker-base',
+    packages=[
+        'parkworker',
+        'parkworker/monits',
+    ],
+    license='The MIT License',
+    install_requires=[
+        'pytz',
+    ],
+)
