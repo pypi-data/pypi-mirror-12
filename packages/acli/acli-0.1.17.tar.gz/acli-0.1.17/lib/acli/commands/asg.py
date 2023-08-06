9e8622bed791c3f3e79cc52848b4117a06790964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:36 +0000

    Add package acli/0.1.17/acli-0.1.17.tar.gz

---
+++ packages/acli/acli-0.1.17.tar.gz/acli-0.1.17/lib/acli/commands/asg.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Usage:
+    acli asg (ls | list) [options]
+    acli asg  info <asg_name> [options]
+    acli asg (cpu | mem | net | delete) <asg_name> [options]
+
+    -s, --start=<start_date>        metrics start-date
+    -e, --end=<end_date>            metrics end-date
+    -p, --period<period>            metrics period
+    -i, --intervals=<intervals>     metrics intervals
+    -o, --output=<output_type>      table, json, yaml or graph [default: graph].
+    -h, --help
+"""
+from __future__ import (absolute_import, print_function, unicode_literals)
+from docopt import docopt
+if __name__ == '__main__':
+    print(docopt(__doc__))
