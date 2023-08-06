9f03ea72555011b28c5a93b7d5625df5bc09f2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:53 +0000

    Add package Snester_nested/1.0.0/Snester_nested-1.0.0.zip

---
+++ packages/Snester_nested/Snester_nested-1.0.0.zip/Snester_nested-1.0.0/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+        name         = 'Snester_nested',        #发布名称
+        version      = '1.0.0',                 #发布版本
+        py_module    = ['Snester_nested'],      #发布的模块
+        author       = 'HunterPy',              #作者
+        author_email = '1542874495@qq.com',     #作者邮箱
+        url          = 'http://weibo.com/2926283135',       #主页
+        description  = '一个简单的嵌套列表的打印（用于《Head First Python（中文版）》第二章），A simple printer of nested lists(for "Head First Python" chapter 2)',     #描述
+        )
