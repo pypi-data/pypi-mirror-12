1c4fa3fb47f7b6b4dee1db295a3e6ddaa998facc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:26 +0000

    Add package Snester/1.0.0/Snester-1.0.0.zip

---
+++ packages/Snester/Snester-1.0.0.zip/Snester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+        name         = 'Snester',
+        version      = '1.0.0',
+        py_module    = ['Snester'],
+        author       = 'HunterPy',
+        author_email = '1542874495@qq.com',
+        url          = 'http://weibo.com/2926283135',
+        description  = '一个简单的嵌套列表的打印，A simple printer of nested lists',
+        )
