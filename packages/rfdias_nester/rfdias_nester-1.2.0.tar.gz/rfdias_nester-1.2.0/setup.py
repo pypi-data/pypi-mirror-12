a04c21467da95cdc1e17659ce3512add1a1cbcc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:51 +0000

    Add package rfdias_nester/1.2.0/rfdias_nester-1.2.0.tar.gz

---
+++ packages/rfdias_nester/rfdias_nester-1.2.0.tar.gz/rfdias_nester-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+	name			= 'rfdias_nester',
+	version			= '1.2.0',
+	py_modules		= ['nester'],
+        author			= 'hfpython',
+        author_mail 		= 'hfpython@headfirstlabs.com',
+	url			= 'http://www.headfirstlabs.com',
+	description		= 'A simple printer of nested lists'
+      )
