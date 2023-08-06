0ea64a14daa6f4c8df26aeebae29840347982aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:41 +0000

    Add package Carson_nester/1.2.3/Carson_nester-1.2.3.tar.gz

---
+++ packages/Carson_nester/Carson_nester-1.2.3.tar.gz/Carson_nester-1.2.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'Carson_nester',
+    version = '1.2.3',
+    py_modules = ['Carson_nester'],
+    author = 'Carson',
+    author_email = '183100386@qq.com',
+    url = 'heep://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
