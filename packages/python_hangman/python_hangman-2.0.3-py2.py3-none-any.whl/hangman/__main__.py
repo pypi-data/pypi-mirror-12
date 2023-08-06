c623f2e72783e400179ffaae0c750657ed414205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:41 +0000

    Add package python_hangman/2.0.3/python_hangman-2.0.3-py2.py3-none-any.whl

---
+++ packages/python_hangman/python_hangman-2.0.3-py2.py3-none-any.whl/hangman/__main__.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+"""
+Entry point for ``hangman`` command.
+"""
+
+
+import click
+
+from hangman import controller
+
+
+@click.command()
+def cli():
+    """
+    Start a new game.
+    """
+    controller.game_loop()
+
+
+if __name__ == '__main__':
+    cli()  # pragma: no cover
