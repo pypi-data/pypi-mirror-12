78f97136ad3e1b245ba59411b2626ae3c1b6b788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:53 +0000

    Add package pyramid_orb/2016.0.dev5/pyramid_orb-2016.0.dev5-py2-none-any.whl

---
+++ packages/pyramid_orb/pyramid_orb-2016.0.dev5-py2-none-any.whl/pyramid_orb/__init__.py
@@ -0,0 +1,32 @@
+"""
+ORB stands for Object Relation Builder and is a powerful yet simple to use \
+database class generator.
+"""
+
+# define authorship information
+__authors__ = ['Eric Hulser']
+__author__ = ','.join(__authors__)
+__credits__ = []
+__copyright__ = 'Copyright (c) 2011, Projex Software'
+__license__ = 'LGPL'
+
+# maintenance information
+__maintainer__ = 'Eric Hulser'
+__email__ = 'eric.hulser@gmail.com'
+
+# auto-generated version file from releasing
+try:
+    from ._version import __major__, __minor__, __revision__, __hash__
+except ImportError:
+    __major__ = 0
+    __minor__ = 0
+    __revision__ = 0
+    __hash__ = ''
+
+__version_info__ = (__major__, __minor__, __revision__)
+__version__ = '{0}.{1}.{2}'.format(*__version_info__)
+
+
+def includeme(config):
+    # define a new renderer for json
+    config.add_renderer('json2', factory='pyramid_orb.renderer.json2_renderer_factory')
---
+++ packages/pyramid_orb/pyramid_orb-2016.0.dev5-py2-none-any.whl/pyramid_orb/_version.py
@@ -0,0 +1,4 @@
+__major__ = 2016
+__minor__ = 0
+__revision__ = "dev5"
+__hash__ = "gc9a3d11"
