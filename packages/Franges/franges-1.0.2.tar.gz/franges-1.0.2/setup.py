bb9e5c990e049d730c48f85b1633f8a5d06e3805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:54 +0000

    Add package Franges/1.0.2/franges-1.0.2.tar.gz

---
+++ packages/Franges/franges-1.0.2.tar.gz/franges-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name = "franges",
+    version = "1.0.2",
+    author = "Nisan Haramati",
+    author_email = "hanisan@gmail.com",
+    description = ("Franges adds support for floating point and fixed precision (Decimal) range generator functions."),
+    license = "LGPLv3+",
+    maintainer = "Nisan Haramati",
+    url = "https://github.com/nisanharamati/franges",
+    packages=['franges',],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
