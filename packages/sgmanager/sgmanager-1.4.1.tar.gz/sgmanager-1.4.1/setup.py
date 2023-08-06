7d89a53c93b18ec169f6f2c0e4c024faf6416ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:47 +0000

    Add package sgmanager/1.4.1/sgmanager-1.4.1.tar.gz

---
+++ packages/sgmanager/sgmanager-1.4.1.tar.gz/sgmanager-1.4.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2007-2013, GoodData(R) Corporation. All rights reserved
+
+from setuptools import setup
+
+setup(
+    name='sgmanager',
+    version='1.4.1',
+    packages=['sgmanager', 'sgmanager.logger', 'sgmanager.securitygroups'],
+    entry_points = {
+        'console_scripts': [ 'sgmanager = sgmanager.cli:main' ]
+    },
+    license='BSD',
+    author='Filip Pytloun',
+    author_email='filip.pytloun@gooddata.com',
+    maintainer='Filip Pytloun',
+    maintainer_email='filip@pytloun.cz',
+    description='Security Groups Management Tool',
+    long_description='Tooling for management of security groups. Load local configuration, load remote groups and apply differences.',
+    url='https://github.com/gooddata/sgmanager',
+    download_url='https://github.com/gooddata/sgmanager',
+    platform='POSIX',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Topic :: System :: Networking :: Firewalls',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    install_requires=['boto<2.35', 'PyYAML', 'argparse'],
+    dependency_links=[
+        'https://github.com/boto/boto/archive/2.34.0.tar.gz#egg=boto-2.34'
+    ]
+)
