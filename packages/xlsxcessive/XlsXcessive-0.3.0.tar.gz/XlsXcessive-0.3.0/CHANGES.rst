389c6c59fb8e3e0c7f542dc58b2909cc1d42bbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:31 +0000

    Add package xlsxcessive/0.3.0/XlsXcessive-0.3.0.tar.gz

---
+++ packages/xlsxcessive/XlsXcessive-0.3.0.tar.gz/XlsXcessive-0.3.0/CHANGES.rst
@@ -0,0 +1,20 @@
+XlsXcessive - Changes
+=====================
+
+0.3.0 - 2015/11/23
+------------------
+
+* Fixes for Python 3. Tests now pass on Python 3.
+
+0.2.0 - 2015/11/23
+------------------
+
+* Improved compatibility with Python 3
+* Add support for the style parameter on a column.
+* Add support for 'best fit' parameter on a column.
+
+0.1.6 - 2010/11/15
+------------------
+
+* Performance improvements (memory and speed)
+
---
+++ packages/xlsxcessive/XlsXcessive-0.3.0.tar.gz/XlsXcessive-0.3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup_params = dict(
+    name="XlsXcessive",
+    version="0.3.0",
+    description="A Python library for writing .xlsx files.",
+    packages=['xlsxcessive'],
+    author="Christian Wyglendowski",
+    author_email="christian@dowski.com",
+    maintainer="Jason R. Coombs",
+    maintainer_email="jaraco@jaraco.com",
+    url="https://bitbucket.org/jaraco/xlsxcessive",
+    install_requires = [
+        'openpack',
+        'six',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**setup_params)
