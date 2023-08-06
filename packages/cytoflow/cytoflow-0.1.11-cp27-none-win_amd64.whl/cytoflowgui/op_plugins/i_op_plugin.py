b74d2f92fd25400970573ea6cc1b3085345b1d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:56 +0000

    Add package cytoflow/0.1.11/cytoflow-0.1.11-cp27-none-win_amd64.whl

---
+++ packages/cytoflow/cytoflow-0.1.11-cp27-none-win_amd64.whl/cytoflowgui/op_plugins/i_op_plugin.py
@@ -0,0 +1,82 @@
+"""
+Created on Mar 15, 2015
+
+@author: brian
+"""
+from traits.api import Interface, Str, HasTraits, Instance, Property, List
+from traitsui.api import Handler
+from cytoflowgui.workflow_item import WorkflowItem
+
+OP_PLUGIN_EXT = 'edu.mit.synbio.cytoflow.op_plugins'
+
+class IOperationPlugin(Interface):
+    """
+    Attributes
+    ----------
+    
+    id : Str
+        The Envisage ID used to refer to this plugin
+        
+    operation_id : Str
+        Same as the "id" attribute of the IOperation this plugin wraps.
+        
+    short_name : Str
+        The operation's "short" name - for menus and toolbar tool tips
+        
+    menu_group : Str
+        If we were to put this op in a menu, what submenu to use?
+        Not currently used.
+    """
+    
+    operation_id = Str
+    short_name = Str
+    menu_group = Str
+
+    def get_operation(self):
+        """
+        Return an instance of the IOperation that this plugin wraps, along
+        with the factory for the handler
+        """
+        
+        
+    def get_default_view(self):
+        """
+        Return an IView instance set up to be the default view for the operation.
+        """
+
+    def get_icon(self):
+        """
+        
+        """
+
+class PluginOpMixin(HasTraits):
+    error = Str(transient = True)
+        
+class OpHandlerMixin(HasTraits):
+    wi = Instance(WorkflowItem)
+    
+    previous_channels = Property(List, depends_on = 'wi.previous.metadata')
+    previous_conditions = Property(List, depends_on = 'wi.previous.conditions')
+
+    # MAGIC: provides dynamically updated values for the "channels" trait
+    def _get_previous_channels(self):
+        """
+        doc
+        """
+        if self.wi and self.wi.previous and self.wi.previous.metadata:
+            return [x for x in experiment.metadata 
+                    if 'type' in experiment.metadata[x] 
+                    and experiment.metadata[x]['type'] == "channel"]
+        else:
+            return []
+         
+    # MAGIC: provides dynamically updated values for the "conditions" trait
+    def _get_previous_conditions(self):
+        """
+        doc
+        """
+        if self.wi and self.wi.previous and self.wi.previous.conditions:
+            return self.wi.conditions.keys()
+        else:
+            return []
+    
---
+++ packages/cytoflow/cytoflow-0.1.11-cp27-none-win_amd64.whl/cytoflowgui/op_plugins/threshold.py
@@ -0,0 +1,72 @@
+from traitsui.api import View, Item, EnumEditor, Controller
+from envisage.api import Plugin, contributes_to
+from traits.api import provides, Callable
+from cytoflowgui.op_plugins.i_op_plugin \
+    import IOperationPlugin, OpHandlerMixin, PluginOpMixin, OP_PLUGIN_EXT
+from pyface.api import ImageResource
+from cytoflow.operations.threshold import ThresholdOp, ThresholdSelection
+from cytoflowgui.view_plugins.i_view_plugin import ViewHandlerMixin, PluginViewMixin
+from cytoflowgui.subset_editor import SubsetEditor
+from cytoflowgui.color_text_editor import ColorTextEditor
+
+
+class ThresholdHandler(Controller, OpHandlerMixin):
+    def default_traits_view(self):
+        return View(Item('object.name'),
+                    Item('object.channel',
+                         editor=EnumEditor(name='handler.previous_channels'),
+                         label = "Channel"),
+                    Item('object.threshold'),
+                    Item('handler.wi.error',
+                         label = 'Error',
+                         visible_when = 'handler.wi.error',
+                         editor = ColorTextEditor(foreground_color = "#000000",
+                                                  background_color = "#ff9191",
+                                                  word_wrap = True))) 
+        
+class ThresholdViewHandler(Controller, ViewHandlerMixin):
+    def default_traits_view(self):
+        return View(Item('object.name',
+                         style = "readonly"),
+                    Item('object.channel', 
+                         label = "Channel",
+                         style = "readonly"),
+                    Item('object.huefacet',
+                         editor=EnumEditor(name='handler.conditions'),
+                         label="Color\nFacet"),
+                    Item('_'),
+                    Item('object.subset',
+                         label = "Subset",
+                         editor = SubsetEditor(experiment = 'handler.wi.previous.result')))
+
+class ThresholdSelectionView(ThresholdSelection, PluginViewMixin):
+    handler_factory = Callable(ThresholdViewHandler)
+    
+class ThresholdPluginOp(ThresholdOp, PluginOpMixin):
+    handler_factory = Callable(ThresholdHandler)
+
+@provides(IOperationPlugin)
+class ThresholdPlugin(Plugin):
+    """
+    class docs
+    """
+    
+    id = 'edu.mit.synbio.cytoflowgui.op_plugins.threshold'
+    operation_id = 'edu.mit.synbio.cytoflow.operations.threshold'
+
+    short_name = "Threshold"
+    menu_group = "Gates"
+    
+    def get_operation(self):
+        return ThresholdPluginOp()
+    
+    def get_default_view(self):
+        return ThresholdSelectionView()
+
+    def get_icon(self):
+        return ImageResource('threshold')
+    
+    @contributes_to(OP_PLUGIN_EXT)
+    def get_plugin(self):
+        return self
+    
