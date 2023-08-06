38b0054f4f990a22f1975f587a7c34d336729b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:14 +0000

    Add package agate-stats/0.3.1/agate-stats-0.3.1.tar.gz

---
+++ packages/agate-stats/agate-stats-0.3.1.tar.gz/agate-stats-0.3.1/agatestats/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from agatestats.aggregations import PearsonCorrelation
+from agatestats.computations import ZScores
+
+def patch():
+    """
+    Patch the methods of :class:`.TableStats` onto :class:`agate.Table <agate.table.Table>`.
+    """
+    import agate
+    from agatestats.table import TableStats
+
+    agate.Table.monkeypatch(TableStats)
