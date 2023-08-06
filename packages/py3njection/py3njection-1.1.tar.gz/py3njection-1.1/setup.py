25151007333d7dee4b8d999c308a28ee658eb5ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:42 +0000

    Add package py3njection/1.1/py3njection-1.1.tar.gz

---
+++ packages/py3njection/py3njection-1.1.tar.gz/py3njection-1.1/setup.py
@@ -0,0 +1,44 @@
+from distutils.core import setup
+
+setup(
+    name='py3njection',
+    packages=['py3njection'],  # this must be the same as the name above
+    version='1.1',
+    description='A dependency injection module using python 3 annotations',
+    long_description=open('README.rst').read(),
+    author='Julien Tellier',
+    author_email='julien.pascal.tellier@gmail.com',
+    maintainer='Julien Tellier',
+    maintainer_email='julien.pascal.tellier@gmail.com',
+    url='https://github.com/Aigrefin/py3njection',
+    download_url='https://github.com/Aigrefin/py3njection/tarball/0.1',
+    license='BSD New',
+    keywords=[
+        'dependency',
+        'injection',
+        'annotation',
+        'inject',
+        'di',
+        'dependency injection',
+        'dependency injector',
+        'dependency management',
+        'ioc',
+        'inversion of control'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
