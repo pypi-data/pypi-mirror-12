38e6c4b0dc1e0743ef6294258a9b3b9df1d26526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:34 +0000

    Add package glorpen-di/1.2/glorpen_di-1.2-py2.py3-none-any.whl

---
+++ packages/glorpen-di/glorpen_di-1.2-py2.py3-none-any.whl/glorpen/di/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''Dependency injection component.
+
+.. moduleauthor:: Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
+
+'''
+
+from glorpen.di.container import Container
+
+__version__ = "1.2"
+__all__ = ['Container', "__version__"]
