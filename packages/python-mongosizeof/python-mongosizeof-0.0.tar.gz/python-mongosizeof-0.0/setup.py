aad5710b0255e2dc73adb0c8dc616c2700ebcdc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:07 +0000

    Add package python-mongosizeof/0.0/python-mongosizeof-0.0.tar.gz

---
+++ packages/python-mongosizeof/python-mongosizeof-0.0.tar.gz/python-mongosizeof-0.0/setup.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    author_email='contact@ionelmc.ro',
+    description='A fast (and possibly inaccurate) estimator of amount of memory python objects would take in a mongodb collection.',
+    install_requires=['mongosizeof'],
+    long_description='''Use `mongosizeof <https://pypi.python.org/pypi/mongosizeof/>`_ instead.''',
+    maintainer='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    maintainer_email='contact@ionelmc.ro',
+    name='python-mongosizeof',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://pypi.python.org/pypi/mongosizeof/',
+    version="0.0",
+    zip_safe=False,
+)
