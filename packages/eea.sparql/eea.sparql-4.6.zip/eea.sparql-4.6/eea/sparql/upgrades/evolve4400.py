699fd5395fc8f41f82f4092495b80547c913d097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:50 +0000

    Add package eea.sparql/4.6/eea.sparql-4.6.zip

---
+++ packages/eea.sparql/eea.sparql-4.6.zip/eea.sparql-4.6/eea/sparql/upgrades/evolve4400.py
@@ -0,0 +1,34 @@
+""" Migrate sparqls with the new arguments format (name:type query)
+"""
+
+import logging
+from Products.CMFCore.utils import getToolByName
+from zope.component import getUtility
+from plone.app.async.interfaces import IAsyncService
+from eea.sparql.content.sparql import async_updateLastWorkingResults
+import DateTime
+
+logger = logging.getLogger("eea.sparql.upgrades")
+
+def restart_sparqls(context):
+    """ Migrate sparqls with the new arguments format (name:type query)
+    """
+
+    async = getUtility(IAsyncService)
+    catalog = getToolByName(context, 'portal_catalog')
+    brains = catalog.searchResults(portal_type='Sparql')
+
+    restarted = 0
+    for brain in brains:
+        obj = brain.getObject()
+        if obj.getRefresh_rate() != 'Once':
+            obj.scheduled_at = DateTime.DateTime()
+            async.queueJob(async_updateLastWorkingResults,
+                            obj,
+                            scheduled_at=obj.scheduled_at,
+                            bookmarks_folder_added=False)
+            restarted += 1
+
+    message = 'Restarted %s Sparqls ...' %restarted
+    logger.info(message)
+    return message
