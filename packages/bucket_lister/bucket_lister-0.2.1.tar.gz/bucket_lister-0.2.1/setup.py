911b62270246c0403be8375248826f75eca014f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:37 +0000

    Add package bucket_lister/0.2.1/bucket_lister-0.2.1.tar.gz

---
+++ packages/bucket_lister/bucket_lister-0.2.1.tar.gz/bucket_lister-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'bucket_lister',
+    packages = find_packages(),
+    version = '0.2.1',
+    description = 'List S3 buckets for an account',
+    author = 'Chris Barr',
+    author_email = 'chris.barr@ntlworld.com',
+    url = 'https://github.com/chrisbarr/bilious-rutabaga',
+    download_url = 'https://github.com/chrisbarr/bilious-rutabaga/tarball/0.1',
+    keywords = ['aws', 's3'],
+    classifiers = [],
+)
