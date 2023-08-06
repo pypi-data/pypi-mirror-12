6cb1f64ce33c7177b150e67bdc07e6716fb33311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:35 +0000

    Add package djwebsockets/0.9.1/djwebsockets-0.9.1.zip

---
+++ packages/djwebsockets/djwebsockets-0.9.1.zip/djwebsockets-0.9.1/djwebsockets/decorator.py
@@ -0,0 +1,13 @@
+from djwebsockets.server import WebSocketServer
+import djwebsockets as settings
+
+
+def Namespace(namespace):
+    def socketplacer(clsitem):
+        if WebSocketServer.NameSpaces.get(namespace) is None:
+            WebSocketServer.AddNameSpace(namespace, clsitem)
+            print("Websocket namespace \"{}\" registered for {}".format(settings.WEBSOCKET_BASE_URI+namespace, clsitem.__name__))
+            print(WebSocketServer.NameSpaces)
+        return clsitem
+    return socketplacer
+
