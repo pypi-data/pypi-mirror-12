a596838717a7d726df891a79cc18a6f51e7ed80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:55 +0000

    Add package yac/0.31/yac-0.31.tar.gz

---
+++ packages/yac/yac-0.31.tar.gz/yac-0.31/yac/yaccli/clidriver.py
@@ -0,0 +1,38 @@
+import argparse,sys
+import yac.yaccli.stack
+
+def yac_options(string):
+    # options can be most anything. Let the 
+    # sub-functions argparse handle validation
+    return string
+
+def main():
+
+    parser = argparse.ArgumentParser('Your Atlassian Cloud',add_help=False)
+    
+    # required args
+    parser.add_argument('operation',  
+                        help='the yac operation to perform', 
+                        choices=['stack','app','db','container'])
+    
+    parser.add_argument('option', 
+                        help='options',
+                        nargs='*',
+                        type=yac_options)
+
+    # trap -h. let operations handle most of the help
+    parser.add_argument('-h', action='store_true')
+
+    args = parser.parse_args()
+
+    # strip operation from args list
+    sys.argv = sys.argv[1:]
+
+    if args.operation == 'stack':
+
+        return yac.yaccli.stack.main()
+
+    else:
+
+        return "operation not yet implemented"
+        
