96ac28a568dce29fa87e38457975b6463b5c70e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:09 +0000

    Add package uranium/0.2.20/uranium-0.2.20.tar.gz

---
+++ packages/uranium/uranium-0.2.20.tar.gz/uranium-0.2.20/tests/conftest.py
@@ -0,0 +1,27 @@
+import os
+import pytest
+from uranium.lib.sandbox import Sandbox
+from uranium.executables import Executables
+from uranium.history import History
+
+BASE = os.path.dirname(os.path.abspath(__file__))
+URANIUM_SOURCE_ROOT = os.path.dirname(BASE)
+
+
+@pytest.fixture
+def sandbox(tmpdir):
+    sandbox = Sandbox(tmpdir.strpath)
+    sandbox.initialize()
+    return sandbox
+
+
+@pytest.fixture
+def history(tmpdir):
+    history_file = os.path.join(tmpdir.strpath, "history.json")
+    return History(history_file)
+
+
+@pytest.fixture
+def executables(tmpdir):
+    executables = Executables(tmpdir.strpath)
+    return executables
