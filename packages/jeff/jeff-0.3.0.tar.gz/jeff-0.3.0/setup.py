0cdb616f596f0b87700b7f4b1b1bf99251b5308a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:59 +0000

    Add package jeff/0.3.0/jeff-0.3.0.tar.gz

---
+++ packages/jeff/jeff-0.3.0.tar.gz/jeff-0.3.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='jeff',
+    version='0.3.0',
+    description='Jeff generates license file from the command line.',
+    author='Oskar Cieslik',
+    author_email='oskar.cieslik@gmail.com',
+    url='https://github.com/oskarcieslik/Jeff',
+    download_url='https://github.com/oskarcieslik/Jeff',
+    keywords=['license', 'github', 'python', 'command line', 'cli'],
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'docopt>=0.6.2',
+    ],
+    package_data={
+        'jeff': ['licenses/*']
+    },
+    entry_points={
+        'console_scripts': [
+            'jeff=jeff.jeff:main'
+        ],
+    }
+)
