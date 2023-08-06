bfafd5d5d5bd8b6d8015c413be56f08559be175e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:02 +0000

    Add package Print_Nested_Lists/1.2.0/Print_Nested_Lists-1.2.0.zip

---
+++ packages/Print_Nested_Lists/Print_Nested_Lists-1.2.0.zip/Print_Nested_Lists-1.2.0/function_list.py
@@ -0,0 +1,13 @@
+"""This prints all lists, including nested lists in a readable format.
+Each data item is printed on its own line. A second argument called level
+is used to create a tab-stop when a nested list is encountered. Adds an
+option to turn on/off tab-stops"""
+
+def print_list(the_list, level=0): #level option enable to turn tab_stop off/on
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                        print_list(each_item, level+1) #second argument increments level by 1
+                else:
+                    for tab_stop in range(level): #range is used to control how many tab-stop are needed
+                        print("\t", end= ''"") #Displays a tab for each nested list and indents to denote the new nested list. '\' can not be the last character. Add "" to the end of the line to break out into a next line.
+                    print(each_item)
