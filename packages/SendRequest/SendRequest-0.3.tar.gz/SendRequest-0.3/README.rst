9269aecb425deef74b701dd233b00e9b38f97288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:19 +0000

    Add package SendRequest/0.3/SendRequest-0.3.tar.gz

---
+++ packages/SendRequest/SendRequest-0.3.tar.gz/SendRequest-0.3/README.rst
@@ -0,0 +1,7 @@
+更新历史：
+    0.3：
+        查明了requests的ssl handshake问题，把urllib2替换为requests简化代码逻辑
+    0.2：
+        fix bug
+    0.1：
+        init
---
+++ packages/SendRequest/SendRequest-0.3.tar.gz/SendRequest-0.3/setup.py
@@ -0,0 +1,10 @@
+#encoding:utf-8
+from __future__ import unicode_literals
+from setuptools import setup, find_packages
+
+setup(
+    name = 'SendRequest',
+    version = '0.3',
+    description = 'http request with retry, download pic.封装网络请求，自带重试',
+    packages = find_packages()
+)
