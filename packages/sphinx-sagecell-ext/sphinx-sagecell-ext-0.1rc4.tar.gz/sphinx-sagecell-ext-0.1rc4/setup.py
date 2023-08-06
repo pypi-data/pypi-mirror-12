c8ae2a35a2750763036a2129f09c07028e3ce5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:41 +0000

    Add package sphinx-sagecell-ext/0.1rc4/sphinx-sagecell-ext-0.1rc4.tar.gz

---
+++ packages/sphinx-sagecell-ext/sphinx-sagecell-ext-0.1rc4.tar.gz/sphinx-sagecell-ext-0.1rc4/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from os.path import dirname, join
+from setuptools import setup
+
+setup(
+    author = "Ruslan Korniichuk",
+    author_email = "ruslan.korniichuk@gmail.com",
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Framework :: Sphinx",
+        "Framework :: Sphinx :: Extension",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: Public Domain",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only",
+        "Topic :: Documentation",
+        "Topic :: Documentation :: Sphinx",
+        "Topic :: Scientific/Engineering"
+    ],
+    description = ("The Sphinx extension "
+                   "embedding a Sage cell into a webpage"),
+    download_url = ("https://github.com/korniichuk/sphinx-sagecell-ext/"
+                    "archive/0.1.zip"),
+    include_package_data = True,
+    install_requires = [
+        "Sphinx"
+    ],
+    keywords = ["extension", "python2", "sagecell", "sphinx",
+                "sphinx-sagecell-ext"],
+    license = "Public Domain",
+    long_description = open(join(dirname(__file__), "README.rst")).read(),
+    name = "sphinx-sagecell-ext",
+    packages = ["sphinx-sagecell-ext"],
+    platforms = ["Linux"],
+    url = "https://github.com/korniichuk/sphinx-sagecell-ext",
+    version = "0.1rc4",
+    zip_safe = True
+)
---
+++ packages/sphinx-sagecell-ext/sphinx-sagecell-ext-0.1rc4.tar.gz/sphinx-sagecell-ext-0.1rc4/sphinx-sagecell-ext/sagecell.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+from docutils.nodes import Element, General
+from docutils.parsers.rst import directives
+from sphinx.util.compat import Directive
+
+linked = True # The default value for 'Linked Cells' option
+
+class sagecell(General, Element):
+    pass
+
+class SageCell(Directive):
+
+    has_content = True
+    required_arguments = 0
+    optional_arguments = 1
+    final_argument_whitespace = False
+    option_spec = {
+        "linked": directives.unchanged
+    }
+
+    def run(self):
+
+        node = sagecell()
+        node['content'] = "\n".join(self.content)
+        node['linked'] = self.options.get("linked")
+        return [node]
+
+def visit_sagecell_node(self, node):
+
+    global linked
+
+    if node['linked'] == "true":
+        self.body.append("<div class='sage_linked'>")
+    elif node['linked'] == "false":
+        self.body.append("<div class='sage_unlinked'>")
+    elif linked == True:
+        self.body.append("<div class='sage_linked'>")
+    elif linked == False:
+        self.body.append("<div class='sage_unlinked'>")
+    else:
+        self.body.append("<div class='sage_unlinked'>")
+    self.body.append("<script type='text/x-sage'>")
+    self.body.append(node['content'])
+    self.body.append("</script>")
+    self.body.append("</div>")
+
+def depart_sagecell_node(self, node):
+    pass
+
+def setup(app):
+
+    # Register a Docutils node class
+    app.add_node(sagecell,
+                 html=(visit_sagecell_node, depart_sagecell_node))
+    # Register a Docutils directive
+    app.add_directive("sagecellserver", SageCell)
