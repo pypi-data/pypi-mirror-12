d1771868ac232946390db08e3f2a83c9ef3b7beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:46 +0000

    Add package celery-bungiesearch/1.2.0/celery-bungiesearch-1.2.0.tar.gz

---
+++ packages/celery-bungiesearch/celery-bungiesearch-1.2.0.tar.gz/celery-bungiesearch-1.2.0/celery_bungiesearch/tasks/bulkdelete.py
@@ -0,0 +1,27 @@
+from .celerybungie import CeleryBungieTask
+from bungiesearch import Bungiesearch
+from bungiesearch.utils import update_index
+
+from elasticsearch import TransportError
+from elasticsearch.helpers import BulkIndexError
+
+
+class BulkDeleteTask(CeleryBungieTask):
+
+    def run(self, model, instance_pks, **kwargs):
+        settings = Bungiesearch.BUNGIE.get('SIGNALS', {})
+        buffer_size = settings.get('BUFFER_SIZE', 100)
+
+        try:
+            update_index(instance_pks, model.__name__, action='delete', bulk_size=buffer_size)
+
+        except BulkIndexError as e:
+            for error in e.errors:
+                if error['delete'] and error['delete']['status'] == 404:
+                    continue
+                raise
+
+        except TransportError as e:
+            if e.status_code == 404:
+                return
+            raise
