e17fe7b345df9d50b0a633a3dc50bf34ea8c3ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:14 +0000

    Add package typegenius/1.0.3/typegenius-1.0.3.tar.gz

---
+++ packages/typegenius/typegenius-1.0.3.tar.gz/typegenius-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='typegenius',
+    packages=['typegenius'],
+    version='1.0.3',
+    description='A library that can recognize and parse variables to the appropriate built-in type',
+    author='Phivos Stylianides',
+    author_email='stphivos@gmail.com',
+    url='https://github.com/stphivos/type-genius',
+    keywords=['type', 'parsing'],
+    classifiers=[],
+)
