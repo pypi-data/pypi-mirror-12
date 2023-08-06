a824be00d67596aadc49a17f9ffb3647f317d994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:57 +0000

    Add package myNesterPyPI/1.0.3/myNesterPyPI-1.0.3.tar.gz

---
+++ packages/myNesterPyPI/myNesterPyPI-1.0.3.tar.gz/myNesterPyPI-1.0.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name = "myNesterPyPI",
+  version = "1.0.3",
+  py_modules = ['myNesterPyPI'],
+  author = "Alex",
+  author_email = "123@123.com",
+  description = "A simple printer of nested lists, add default value for second parameter"
+)
