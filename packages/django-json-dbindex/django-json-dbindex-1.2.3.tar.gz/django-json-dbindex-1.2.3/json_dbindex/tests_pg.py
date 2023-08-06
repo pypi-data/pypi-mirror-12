4ebc3e1bf45a5d659aecebb5072577f24036ae7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:10 +0000

    Add package django-json-dbindex/1.2.3/django-json-dbindex-1.2.3.tar.gz

---
+++ packages/django-json-dbindex/django-json-dbindex-1.2.3.tar.gz/django-json-dbindex-1.2.3/json_dbindex/tests_pg.py
@@ -0,0 +1,17 @@
+"""
+django-json-dbindex tests on pgcommands
+"""
+import pgcommands
+from mock import MagicMock
+from django.test import TestCase
+
+
+class PostgreSQLTest(TestCase):
+
+    def test_create_extensions(self):
+        """
+        Create a list of extensions
+        """
+        pgcommands.execute_raw = MagicMock(return_value=0)
+        res = pgcommands.create_extensions(['unaccent', 'pg_trgm'])
+        self.assertEqual(res, 0)
