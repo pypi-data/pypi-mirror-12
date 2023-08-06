0d26c6962bfe4d0f023d6e8ead3c284dedd874d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:48 +0000

    Add package factorial_qxj/0.3/factorial_qxj-0.3.zip

---
+++ packages/factorial_qxj/factorial_qxj-0.3.zip/factorial_qxj-0.3/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""Factorial project"""
+from setuptools import find_packages, setup
+
+setup(name = 'factorial_qxj',
+    version = '0.3',
+    description = "Factorial module.",
+    long_description = "A test module for qxj.",
+    platforms = ["Linux"],
+    author="qxj",
+    author_email="qxj2016@126.com",
+    url="http://www.qxj.com",
+    license = "MIT",
+    packages=find_packages()
+    )
