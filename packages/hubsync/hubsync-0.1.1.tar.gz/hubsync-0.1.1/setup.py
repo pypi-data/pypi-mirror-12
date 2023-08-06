bc5cef981680930129a8edc3d066ea2d13b7741d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:59 +0000

    Add package hubsync/0.1.1/hubsync-0.1.1.tar.gz

---
+++ packages/hubsync/hubsync-0.1.1.tar.gz/hubsync-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'hubsync',
+  packages = ['hubsync'],
+  version = '0.1.1',
+  description = 'Get your github workspace synced!',
+  author = 'Mario Corchero',
+  author_email = 'mariocj89@gmail.com',
+  url = 'https://github.com/Mariocj89/hubsync',
+  keywords = ['github', 'sync', 'workspace'],
+  scripts=['hubsync.py']
+)
