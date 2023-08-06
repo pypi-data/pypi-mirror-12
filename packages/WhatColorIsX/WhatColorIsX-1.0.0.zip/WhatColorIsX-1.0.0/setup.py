ceee3d42441b8b6d75f2f573a56a80c287821711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:45 +0000

    Add package WhatColorIsX/1.0.0/WhatColorIsX-1.0.0.zip

---
+++ packages/WhatColorIsX/WhatColorIsX-1.0.0.zip/WhatColorIsX-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(name='WhatColorIsX',
+      version='1.0.0',
+      description='Get color of a string via Google iamge search API',
+      long_description='Using the Google image search API, this allows a string to be converted into a (usually) related hex color value.',
+      url='https://github.com/tommilligan/WhatColorIsX',
+      author='Tom Milligan',
+      author_email='code@tommilligan.net',
+      license='GPL',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Programming Language :: Python'
+      ],
+      keywords='colour color google image text search find get string',
+      py_modules=['WhatColorIsX'],
+      install_requires=[
+          'colour',
+          'simplejson',
+          'Pillow'
+      ],
+      entry_points={
+        'console_scripts': [
+            'whatcoloris = WhatColorIsX:main'
+        ]
+      }
+)
