2cba535237d9be0b6c3440121bd254f10a1de94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:27 +0000

    Add package OpOpGadget/0.1.10/OpOpGadget-0.1.10.tar.gz

---
+++ packages/OpOpGadget/OpOpGadget-0.1.10.tar.gz/OpOpGadget-0.1.10/OpOp/Model.py
@@ -0,0 +1,6 @@
+from .src.model_src import Model
+from .src.model_src.GeneralModel import GeneralModel
+from .src.model_src.NbodyModel import NbodyModel
+from .src.model_src.Plummer import Plummer
+
+
