5c5f5880bf2aa549c0b3c28333ed3a0fde313366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:33 +0000

    Add package walls/0.1.0/walls-0.1.0-py2.py3-none-any.whl

---
+++ packages/walls/walls-0.1.0-py2.py3-none-any.whl/walls-0.1.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,64 @@
+walls
+=====
+
+.. image:: https://img.shields.io/travis/nickfrostatx/walls.svg
+    :target: https://travis-ci.org/nickfrostatx/walls
+
+.. image:: https://img.shields.io/coveralls/nickfrostatx/walls.svg
+    :target: https://coveralls.io/github/nickfrostatx/walls
+
+.. image:: https://img.shields.io/pypi/v/walls.svg
+    :target: https://pypi.python.org/pypi/walls
+
+.. image:: https://img.shields.io/pypi/l/walls.svg
+    :target: https://raw.githubusercontent.com/nickfrostatx/walls/master/LICENSE
+
+``walls`` downloads random wallpapers from Flickr. It searches recent Flickr
+images matching a given list of tags, and downloads the first one with
+dimensions large enough to be used as a wallpaper.
+
+Installation
+------------
+
+.. code-block:: bash
+
+    $ pip install walls
+
+Config
+------
+
+The default config location is ``~/.wallsrc``. Here is an example configuration:
+
+.. code-block:: ini
+
+    [walls]
+    api_key = YOUR_API_KEY
+    api_secret = YOUR_API_SECRET
+    tags = cats,dogs
+    image_dir = ~/Pictures
+    width = 1920
+    height = 1080
+
+Usage
+-----
+
+.. code-block:: bash
+
+    $ walls [-c] [config_file]
+
+``-c`` or ``--clear`` will clear out images in the destination directory.
+
+If supplied, ``config_file`` will be used instead of ``~/.wallsrc``.
+
+
+.. :changelog:
+
+History
+-------
+
+0.1.0 (2015-11-28)
+++++++++++++++++++
+
+- Initial release
+
+
---
+++ packages/walls/walls-0.1.0-py2.py3-none-any.whl/walls.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+"""
+Random Flickr wallpapers.
+
+:copyright: (c) 2015 by Nicholas Frost.
+:license: MIT, see LICENSE for more details.
+"""
+
+import flickrapi
+import os.path
+import requests
+import sys
+try:
+    # Python 3.x
+    from configparser import ConfigParser
+except ImportError:
+    # Python 2.x
+    from ConfigParser import SafeConfigParser as ConfigParser
+
+
+__author__ = 'Nick Frost'
+__copyright__ = 'Copyright 2015, Nicholas Frost'
+__license__ = 'MIT'
+__version__ = '0.1.0'
+__email__ = 'nickfrostatx@gmail.com'
+
+
+def stderr_and_exit(msg):
+    """Write out an error message and exit with code 1."""
+    sys.stderr.write(msg)
+    sys.exit(1)
+
+
+def load_config(path):
+    """Load the config value from various arguments."""
+    config = ConfigParser()
+
+    if len(config.read(path)) == 0:
+        stderr_and_exit("Couldn't load config {0}\n".format(path))
+    if not config.has_section('walls'):
+        stderr_and_exit('Config missing [walls] section.\n')
+
+    # Print out all of the missing keys
+    keys = ['api_key', 'api_secret', 'tags', 'image_dir', 'width', 'height']
+    for key in set(keys):
+        if config.has_option('walls', key):
+            keys.remove(key)
+    if keys:
+        stderr_and_exit("Missing config keys: '{0}'\n"
+                        .format("', '".join(keys)))
+
+    # Parse integer values
+    int_keys = ['width', 'height']
+    for key in set(int_keys):
+        try:
+            config.getint('walls', key)
+            int_keys.remove(key)
+        except ValueError:
+            pass
+    if int_keys:
+        stderr_and_exit("The following must be integers: '{0}'\n"
+                        .format("', '".join(int_keys)))
+
+    # Check destination directory
+    path = os.path.expanduser(config.get('walls', 'image_dir'))
+    if not os.path.isdir(path):
+        stderr_and_exit('The directory {0} does not exist.\n'
+                        .format(config.get('walls', 'image_dir')))
+
+    return config
+
+
+def clear_dir(path):
+    """Empty out the image directory."""
+    for f in os.listdir(path):
+        f_path = os.path.join(path, f)
+        if os.path.isfile(f_path) or os.path.islink(f_path):
+            os.unlink(f_path)
+
+
+class Walls(object):
+
+    """The main object."""
+
+    def __init__(self, config):
+        """Initial setup."""
+        self.config = config
+        self.flickr = flickrapi.FlickrAPI(config.get('walls', 'api_key'),
+                                          config.get('walls', 'api_secret'))
+
+    def run(self):
+        """Find a photo, download it to disk."""
+        photo_url = self.first_photo()
+        if not photo_url:
+            stderr_and_exit('No matching photos found.\n')
+        try:
+            self.download(photo_url)
+        except IOError:
+            stderr_and_exit('Error downloading image.\n')
+
+    def first_photo(self):
+        """Find the id of the first criteria-matching photo."""
+        tags = self.config.get('walls', 'tags')
+        for photo in self.flickr.walk(tags=tags, format='etree'):
+            try:
+                url = self.smallest_url(photo.get('id'))
+            except (KeyError, ValueError, TypeError):
+                stderr_and_exit('Unexpected data from Flickr.\n')
+            if url:
+                return url
+
+    def smallest_url(self, pid):
+        """Return the url of the smallest photo above the dimensions.
+
+        If no such photo exists, return None.
+        """
+        sizes = self.flickr.photos_getSizes(photo_id=pid, format='parsed-json')
+        min_width = self.config.getint('walls', 'width')
+        min_height = self.config.getint('walls', 'height')
+        smallest_url = None
+        smallest_area = None
+        for size in sizes['sizes']['size']:
+            width = int(size['width'])
+            height = int(size['height'])
+            # Enforce a minimum height and width
+            if width >= min_width and height >= min_height:
+                if not smallest_url or height * width < smallest_area:
+                    smallest_area = height * width
+                    smallest_url = size['source']
+        return smallest_url
+
+    def download(self, url):
+        """Download the image to disk."""
+        parent = os.path.expanduser(self.config.get('walls', 'image_dir'))
+        path = os.path.join(parent, url.split('/')[-1])
+        r = requests.get(url, stream=True)
+        r.raise_for_status()
+        with open(path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+        return path
+
+
+def main(args=sys.argv):
+    """Run the downloader from the command line."""
+    # Don't make changes to sys.argv
+    args = list(args)
+
+    # Remove arg[0]
+    args.pop(0)
+
+    # Pop off the options
+    clear_opt = False
+    if '-c' in args:
+        args.remove('-c')
+        clear_opt = True
+    elif '--clear' in args:
+        args.remove('--clear')
+        clear_opt = True
+
+    if len(args) == 0:
+        cfg_path = os.path.expanduser('~/.wallsrc')
+    elif len(args) == 1:
+        cfg_path = args[0]
+    else:
+        stderr_and_exit('Usage: walls [-c] [config_file]\n')
+
+    config = load_config(cfg_path)
+
+    if clear_opt:
+        clear_dir(os.path.expanduser(config.get('walls', 'image_dir')))
+
+    walls = Walls(config)
+    walls.run()
+
+
+if __name__ == '__main__':
+    main()
