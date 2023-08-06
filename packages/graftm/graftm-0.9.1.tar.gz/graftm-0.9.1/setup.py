03e4ce94beffb927bde28d365fadaf8c0a565138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:58 +0000

    Add package graftm/0.9.1/graftm-0.9.1.tar.gz

---
+++ packages/graftm/graftm-0.9.1.tar.gz/graftm-0.9.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+exec(open('graftm/version.py').read()) # loads __version__
+
+setup(name='graftm',
+      version=__version__,
+      author='Joel Boyd, Ben Woodcroft',
+      description='GraftM is a pipeline used for identifying and classifying marker gene reads from metagenomic datasets',
+      long_description=readme,
+      license='see LICENSE.txt',
+      keywords="",
+      packages=find_packages(exclude='docs'),
+      install_requires=('biopython ==1.64',
+                        'seqmagick ==0.5.0',
+                        'scikit-bio ==0.2.2',
+                        'subprocess32 ==3.2.6',
+                        'biom-format ==2.1.4',
+                        'extern ==0.0.4'),
+      url='http://geronimp.github.io/graftM',
+      scripts=['bin/graftM']
+)
