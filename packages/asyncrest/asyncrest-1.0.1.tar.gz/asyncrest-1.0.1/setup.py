0995af3a9caf0c7506fa62a9cd610a5cbd5ddeab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:20 +0000

    Add package asyncrest/1.0.1/asyncrest-1.0.1.tar.gz

---
+++ packages/asyncrest/asyncrest-1.0.1.tar.gz/asyncrest-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'asyncrest',
+  modules = ['asyncrest'],
+  version = '1.0.1',
+  description = 'RESTful helper for asyncio',
+  author = 'Kamyar Inanloo',
+  author_email = 'kamyar1979@gmail.com',
+  install_requires = ['aiohttp'],
+  url = 'https://bitbucket.org/kamyar1979/asyncrest',
+  keywords = ['asyncio', 'rest', 'pep3156','aiohttp','restful'], 
+  classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP'],
+)
