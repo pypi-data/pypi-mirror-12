5b54b7cf8ad2aaede1b48e1269e9a427e8022b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:55 +0000

    Add package django-externaltestserver/0.2.0/django-externaltestserver-0.2.0.tar.gz

---
+++ packages/django-externaltestserver/django-externaltestserver-0.2.0.tar.gz/django-externaltestserver-0.2.0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    try:
+        import wheel
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+
+setup(
+    name='django-externaltestserver',
+    version='0.2.0',
+    description='Run your Django selenium tests against an external server',
+    long_description=open('README.md').read(),
+    author='Saul Shanabrook',
+    author_email='s.shanabrook@gmail.com',
+    url='https://github.com/saulshanabrook/django-externaltestserver',
+    packages=[
+        'externaltestserver',
+    ],
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="MIT",
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
