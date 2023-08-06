5ad1bb69ff6b52553d4e234cae5102682f11e9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:22 +0000

    Add package cmddocs/0.12.2/cmddocs-0.12.2.tar.gz

---
+++ packages/cmddocs/cmddocs-0.12.2.tar.gz/cmddocs-0.12.2/cmddocs/utils.py
@@ -0,0 +1,26 @@
+import re
+import os
+
+def add_fileextension(article, extension):
+    "add file extension to article"
+
+    article = article + '.' + extension
+
+    return article
+
+def remove_fileextension(article, extension):
+    "remove file extension"
+
+    extension = '\.' + extension + '$'
+
+    # python3 compatibility
+    try:
+        extension = bytes(extension,"UTF-8")
+    except TypeError:
+        pass
+
+    # in python3, re is expecting byte arrays
+    article = re.sub(extension, b"", article, flags=re.M)
+    article = article.decode("UTF-8")
+
+    return article
