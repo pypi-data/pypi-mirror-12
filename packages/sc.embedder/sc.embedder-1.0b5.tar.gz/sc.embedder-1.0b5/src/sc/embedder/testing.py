d89a4374e5916776b9bae572c6010652fbdaf3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:16 +0000

    Add package sc.embedder/1.0b5/sc.embedder-1.0b5.tar.gz

---
+++ packages/sc.embedder/sc.embedder-1.0b5.tar.gz/sc.embedder-1.0b5/src/sc/embedder/testing.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from plone.app.robotframework.testing import AUTOLOGIN_LIBRARY_FIXTURE
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+
+
+class Fixture(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        import collective.cover
+        self.loadZCML(package=collective.cover)
+        import sc.embedder
+        self.loadZCML(package=sc.embedder)
+
+    def setUpPloneSite(self, portal):
+        self.applyProfile(portal, 'collective.cover:default')
+        self.applyProfile(portal, 'sc.embedder:default')
+
+FIXTURE = Fixture()
+INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FIXTURE,),
+    name='sc.embedder:Integration',
+)
+FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FIXTURE, z2.ZSERVER_FIXTURE),
+    name='sc.embedder:Functional',
+)
+ROBOT_TESTING = FunctionalTesting(
+    bases=(FIXTURE, AUTOLOGIN_LIBRARY_FIXTURE, z2.ZSERVER_FIXTURE),
+    name='sc.embedder:Robot')
