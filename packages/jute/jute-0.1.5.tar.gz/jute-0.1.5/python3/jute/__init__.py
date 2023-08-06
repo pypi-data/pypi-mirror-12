35a8c3079e04646fa8e29e90fc2d6a2e961f6782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:04 +0000

    Add package jute/0.1.5/jute-0.1.5.tar.gz

---
+++ packages/jute/jute-0.1.5.tar.gz/jute-0.1.5/python3/jute/__init__.py
@@ -0,0 +1,12 @@
+from ._jute import (
+    Interface, Dynamic, implements, underlying_object,
+    InterfaceConformanceError
+)
+
+__all__ = [
+    'Interface',
+    'Dynamic',
+    'implements',
+    'underlying_object',
+    'InterfaceConformanceError',
+]
