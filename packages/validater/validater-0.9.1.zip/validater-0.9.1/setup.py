2923d27ba6794c64c9374bc14a83c9751b4b96e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:44 +0000

    Add package validater/0.9.1/validater-0.9.1.zip

---
+++ packages/validater/validater-0.9.1.zip/validater-0.9.1/setup.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+
+from setuptools import setup
+setup(
+    name="validater",
+    version="0.9.1",
+    description="a python tool to "
+    "validate json/dict/list and convert value by schema",
+    author="kk",
+    url="https://github.com/guyskk/validater",
+    license="MIT",
+    packages=['validater'],
+    install_requires=[
+        'python-dateutil>=2.4',
+        'six>=1.10.0',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
