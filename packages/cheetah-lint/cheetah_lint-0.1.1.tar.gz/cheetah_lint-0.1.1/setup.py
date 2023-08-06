7b9de304184c7c2cedb59218a1c4700f19d905aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:46 +0000

    Add package cheetah-lint/0.1.1/cheetah_lint-0.1.1.tar.gz

---
+++ packages/cheetah-lint/cheetah_lint-0.1.1.tar.gz/cheetah_lint-0.1.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='cheetah_lint',
+    description='Linting tools for the Cheetah templating language.',
+    url='https://github.com/asottile/cheetah_lint',
+    version='0.1.1',
+    author='Anthony Sottile',
+    author_email='asottile@umich.edu',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    packages=find_packages('.', exclude=('tests*', 'testing*')),
+    install_requires=[
+        'argparse',
+        'aspy.refactor_imports>=0.2.1',
+        'cached-property',
+        'flake8',
+        'refactorlib[cheetah]>=0.9.0,<=0.9.999',
+        'yelp-cheetah>=0.12.1,<=0.12.999',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cheetah-reorder-imports = cheetah_lint.reorder_imports:main',
+            'cheetah-flake = cheetah_lint.flake:main',
+        ],
+    },
+)
