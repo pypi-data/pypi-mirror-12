b05ccec597ac861fc2869b942eaf71f9c63d8844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:40 +0000

    Add package nestername/1.0.0/nestername-1.0.0.zip

---
+++ packages/nestername/nestername-1.0.0.zip/nestername-1.0.0/nester.py
@@ -0,0 +1,10 @@
+"""this is a function called print_lol in Lesson 1"""
+def print_lol (the_list,level):
+    """hi"""
+    for each_item in the_list :
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print ("\t"),
+            print (each_item)
---
+++ packages/nestername/nestername-1.0.0.zip/nestername-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "nestername",
+    version = "1.0.0",
+    py_modules = ["nester"],
+    author = "yang",
+    author_email ="",
+    description = " lesson 1 in head first python",
+    )
