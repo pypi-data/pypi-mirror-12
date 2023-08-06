7e26dbae99641dfd0f3aed29a2a636968f53a5f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:13 +0000

    Add package utilex/1.1.0/utilex-1.1.0.zip

---
+++ packages/utilex/utilex-1.1.0.zip/utilex-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+		name  			= 'utilex',
+		version 		= '1.1.0',
+		py_modules 		= ['utilex'],
+		author 			= 'zsp',
+		author_email 	= 'zsp1982@naver.com',
+		url          	= '',
+		description  	= 'A sample',
+	)
