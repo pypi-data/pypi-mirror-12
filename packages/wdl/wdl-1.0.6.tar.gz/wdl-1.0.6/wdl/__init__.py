fbb3516183c1f19716467492f055d5a160da6b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:55 +0000

    Add package wdl/1.0.6/wdl-1.0.6.tar.gz

---
+++ packages/wdl/wdl-1.0.6.tar.gz/wdl-1.0.6/wdl/__init__.py
@@ -0,0 +1,23 @@
+import wdl.binding
+
+def load(fp, resource):
+    return wdl.binding.parse_document(fp.read(), resource)
+
+def loads(s):
+    return wdl.binding.parse_document(s, "string")
+
+def find_asts(ast_root, name):
+    nodes = []
+    if isinstance(ast_root, wdl.parser.AstList):
+        for node in ast_root:
+            nodes.extend(find_asts(node, name))
+    elif isinstance(ast_root, wdl.parser.Ast):
+        if ast_root.name == name:
+            nodes.append(ast_root)
+        for attr_name, attr in ast_root.attributes.items():
+            nodes.extend(find_asts(attr, name))
+    return nodes
+
+def parse_expr(expr_string):
+    ctx = wdl.parser.ParserContext(wdl.parser.lex(expr_string, 'string'), wdl.parser.DefaultSyntaxErrorHandler())
+    return wdl.binding.Expression(wdl.parser.parse_e(ctx).ast())
