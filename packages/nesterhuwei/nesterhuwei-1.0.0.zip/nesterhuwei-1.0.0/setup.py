5e9533628f7660d41959210db7b8a9460347f859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:37 +0000

    Add package nesterhuwei/1.0.0/nesterhuwei-1.0.0.zip

---
+++ packages/nesterhuwei/nesterhuwei-1.0.0.zip/nesterhuwei-1.0.0/setup.py
@@ -0,0 +1,15 @@
+﻿from distutils.core import setup
+
+setup(
+name='nesterhuwei',
+version='1.0.0',
+py_modules=['nesterhuwei'],
+author='huweipython',
+author_email='821513268@qq.com',
+url='http://www.headfirstlabs.com',
+description='A list',
+
+
+
+
+)
