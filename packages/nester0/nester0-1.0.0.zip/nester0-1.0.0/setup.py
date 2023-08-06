9c2a9335e31e39131aafcaf048e2ce929128d5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:48 +0000

    Add package nester0/1.0.0/nester0-1.0.0.zip

---
+++ packages/nester0/nester0-1.0.0.zip/nester0-1.0.0/setup.py
@@ -0,0 +1,14 @@
+
+
+from distutils.core import setup
+
+setup(
+      name='nester0',
+      version='1.0.0',
+      py_modules=['nester0'],
+      author='hfpython',
+      author_email='hao@126.com',
+      url='http://www.baidu.com',
+      description='A simple printer of nester lists',
+
+    )
