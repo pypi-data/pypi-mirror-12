641613a5f94e83504e39c94403c1b48635d40065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:54 +0000

    Add package horizon_contrib/2015.11.2/horizon_contrib-2015.11.2.tar.gz

---
+++ packages/horizon_contrib/horizon_contrib-2015.11.2.tar.gz/horizon_contrib-2015.11.2/horizon_contrib/api/base.py
@@ -0,0 +1,126 @@
+
+
+
+import json
+import logging
+
+import requests
+from requests import exceptions
+from django.conf import settings
+from horizon import messages
+from horizon_contrib.utils import to_dotdict
+
+LOG = logging.getLogger("client.base")
+
+TOKEN_FORMAT = "  Token {0}"
+
+
+class ClientBase(object):
+
+    """Base Client Object with main method ``request``
+
+    this is only simple wrapper which is overwritten in 99%
+
+    but provide consitent request method
+
+    """
+
+    def do_request(self, path, method="GET", params={}, headers={}):
+        '''make raw request'''
+
+        if method == "GET":
+            response = requests.get(path, headers=headers)
+        elif method == "POST":
+            headers["Content-Type"] = "application/json"
+            response = requests.post(
+                path,
+                data=json.dumps(params),
+                headers=headers)
+        elif method == "PUT":
+            headers["Content-Type"] = "application/json"
+            response = requests.put(
+                path,
+                data=json.dumps(params),
+                headers=headers)
+        elif method == "DELETE":
+            response = requests.delete(
+                path,
+                data=json.dumps(params),
+                headers=headers)
+        return response
+
+    def process_response(self, response, request):
+        '''process response and handle statues and exceptions'''
+
+        if response.status_code <= 204:
+            result = response.json()
+            if "error" in result:
+                msg = result.get("error")
+                # populate exception
+                messages.error(request, msg)
+                if settings.DEBUG:
+                    LOG.exception(msg)
+            return to_dotdict(result)
+        else:
+            if response.status_code == 401:
+                raise exceptions.HTTPError('Unautorized 401')
+            if response.status_code == 400:
+                raise exceptions.HTTPError('Bad Request 400')
+            if response.status_code == 500:
+                LOG.exception(request.body)
+                raise exceptions.HTTPError('Unexpected exception 500')
+            return Exception(response.status_code)
+
+    def process_data(self, result, request):
+        '''process result and returns data'''
+        return result
+
+    def request(self, path, method="GET", params={}, request={}, headers={}):
+        """main method which provide
+
+        .. attribute:: path
+
+        Relative URI '/projects' -> <self.api>/projects
+
+        .. attribute:: method
+
+        String Rest method
+
+        .. attribute:: params
+
+        Dictionary data which will be serialized to json
+
+        .. attribute:: request
+
+        Original request where lives user
+        with permissions AUTH_TOKEN or something else
+
+        If is provided, additional messages will be pushed.
+
+        """
+
+        _request = request
+        self.set_api()
+
+        LOG.debug("%s - %s%s - %s" % (method, self.api, path, params))
+
+        # do request
+        response = self.do_request(
+            '%s%s' % (self.api, path),
+            method,
+            params,
+            headers)
+
+        # process response
+        result = self.process_response(response, _request)
+        # process data
+        data = self.process_data(result, _request)
+
+        return data
+
+    def set_api(self):
+        self.api = '%s://%s:%s%s' % (
+            getattr(self, "protocol", "http"),
+            getattr(self, "host", "127.0.0.1"),
+            getattr(self, "port"),
+            getattr(self, "api_prefix", "/api"))
---
+++ packages/horizon_contrib/horizon_contrib-2015.11.2.tar.gz/horizon_contrib-2015.11.2/horizon_contrib/api/managers.py
@@ -0,0 +1,113 @@
+# -*- coding: UTF-8 -*-
+
+import operator
+from horizon_contrib.api.base import ClientBase
+from urllib import urlencode
+
+
+class SearchOptionsMixin(object):
+
+    '''Encapsulation of search options'''
+
+    def get_search_options(self, opts={}):
+        '''merge search options with defaults'''
+        opts.update(getattr(self, 'search_options', {}))
+        return opts
+
+    def get_query_string(self, search_opts):
+        '''returns query string from search options'''
+        return urlencode(self.get_search_options(search_opts))
+
+    def get_url(self, id=None, search_opts={}):
+        query = self.get_query_string(search_opts)
+        if not id:
+            base_url = '/%s' % self.scope
+        else:
+            base_url = '/{0}/{1}'.format(self.scope, id)
+        if query:
+            url = '?'.join([base_url,
+                            query])
+        else:
+            url = base_url
+        return url if url[-1] == '/' else url + '/'
+
+
+class Manager(ClientBase, SearchOptionsMixin):
+
+    """base manager class which provide consistent interface
+    for working with API
+    in many cases you want add extra method
+    like an object specific actions subscribe/like etc
+    its only interface not golden rule
+
+    :attr:scope: string which specify api uri::
+
+        scope = 'tickets'
+
+    will produce -> api/tickets
+    """
+
+    def all(self, *args, **kwargs):
+        return self.list(*args, **kwargs)
+
+    def get(self, id, request=None):
+        return self.request(
+            self.get_url(id),
+            'GET',
+            request=request)
+
+    def create(self, data, request=None):
+        return self.request(
+            self.get_url(),
+            'POST',
+            params=data,
+            request=request)
+
+    def update(self, id, data, request=None):
+        return self.request(
+            self.get_url(id),
+            'PUT',
+            params=data,
+            request=request)
+
+    def delete(self, id, request=None, *args, **kwargs):
+        return self.request(
+            self.get_url(id),
+            'DELETE',
+            request=request)
+
+    def list(self, request=None, search_opts={}):
+        return self.request(
+            self.get_url(search_opts=search_opts),
+            'GET',
+            request=request)
+
+    def choices(self, request, data=None, pk='id',
+                label='{hostname}', name='Host', empty=True):
+        '''make choices from data or self.list
+
+        data for making choices
+        label is string with item context
+        name is singular name of item
+        if you want create multiple choices use empty=False
+        '''
+        choices = []
+        if not data:
+            data = self.list(request)
+        for item in data:
+            choices.append((item[pk], label.format(**item)))
+        if choices:
+            choices.sort(key=operator.itemgetter(1))
+            if empty:
+                choices.insert(0, ("", "Select %s" % name))
+        else:
+            choices.insert(0, ("", "No %s available" % name))
+        return choices
+
+    # other common stuff
+
+    def order_by(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def filter(self, *args, **kwargs):
+        raise NotImplementedError
