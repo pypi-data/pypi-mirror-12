656df9db8f45390dcea099d9bb0f6ab548460d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:50 +0000

    Add package rnc2rng/2.1/rnc2rng-2.1.tar.gz

---
+++ packages/rnc2rng/rnc2rng-2.1.tar.gz/rnc2rng-2.1/rnc2rng/__main__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from . import parser, serializer
+import sys
+
+def main():
+
+    args = sys.argv[1:]
+    input = open(args[0]) if len(args) > 0 else sys.stdin
+    try:
+        xml = serializer.XMLSerializer().toxml(parser.parse(f=input))
+    except parser.ParseError as e:
+        print 'parse error ' + e.msg
+        sys.exit(1)
+
+    if len(args) > 1:
+        open(sys.argv[2], 'w').write(xml + '\n')
+    else:
+        print xml
+
+if __name__ == '__main__':
+    main()
