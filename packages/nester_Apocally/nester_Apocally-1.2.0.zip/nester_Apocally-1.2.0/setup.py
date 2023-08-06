9c6dd132b788466ffc740834ace984cc27f2b80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:27 +0000

    Add package nester_Apocally/1.2.0/nester_Apocally-1.2.0.zip

---
+++ packages/nester_Apocally/nester_Apocally-1.2.0.zip/nester_Apocally-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name            ='nester_Apocally',
+        version         ='1.2.0',
+        py_modules      =['nester_Apocally'],
+        author          ='Apocally',
+        author_email    ='zhouhanqivip@gmail.com',
+        url             ='',
+        description     ='An exercise, a simple printer of nested lists',
+        )
