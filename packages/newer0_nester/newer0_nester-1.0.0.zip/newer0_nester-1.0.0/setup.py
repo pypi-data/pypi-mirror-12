c68cc60d15213bb80534ba20a785ff180af26c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package newer0_nester/1.0.0/newer0_nester-1.0.0.zip

---
+++ packages/newer0_nester/newer0_nester-1.0.0.zip/newer0_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name            = 'newer0_nester',
+    version         = '1.0.0',
+    py_modules      = ['newer0_nester'],
+    author          = 'xc',
+    author_email    = 'xc@gmail.com',
+    description     = 'A simple printer of nested lists',
+    )
