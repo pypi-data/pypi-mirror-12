384d45b43394c75b0b420bedb1b52658751b9571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:16 +0000

    Add package timesheet/0.8.3/timesheet-0.8.3-py2-none-any.whl

---
+++ packages/timesheet/timesheet-0.8.3-py2-none-any.whl/timesheet/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import sys
+from timesheet.configuration import init_config, config
+__author__ = 'vahid'
+__version__ = '0.8.3'
+
+
+def entrypoint():
+    global config
+
+    # initializing configuration
+    init_config()
+
+    # initializing models if not initialized yet
+    from timesheet import models
+    models.init()
+
+    # Preparing cli arguments
+    from timesheet import cli
+    args = cli.parse_ars()
+
+    # Dispatch and execute the command
+    args.command_class(args).do_job()
+
+    sys.exit(0)
