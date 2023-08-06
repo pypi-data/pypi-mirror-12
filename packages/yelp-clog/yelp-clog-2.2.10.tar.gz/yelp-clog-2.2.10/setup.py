c3cf1c5c98cd67832b326661a0266234a196833d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:00 +0000

    Add package yelp-clog/2.2.10/yelp-clog-2.2.10.tar.gz

---
+++ packages/yelp-clog/yelp-clog-2.2.10.tar.gz/yelp-clog-2.2.10/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='yelp-clog',
+    version='2.2.10',
+    description='A package which provides logging and reading from scribe.',
+    author='Yelp Infra Team',
+    author_email='infra@yelp.com',
+    packages=['clog'],
+    package_data={
+        'clog': ['fb303.thrift', 'scribe.thrift'],
+    },
+    install_requires=[
+        'boto>=2.0.0',
+        'future>=0.14.0',
+        'thriftpy==0.1.15',
+        'PyStaticConfiguration >= 0.8',
+        'simplejson',
+    ]
+)
