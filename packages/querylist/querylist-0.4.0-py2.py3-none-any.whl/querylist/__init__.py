84f2fac5493495319eba74bcb44efc7a28f5f291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:51 +0000

    Add package querylist/0.4.0/querylist-0.4.0-py2.py3-none-any.whl

---
+++ packages/querylist/querylist-0.4.0-py2.py3-none-any.whl/querylist/__init__.py
@@ -0,0 +1,8 @@
+"""QueryList provides a simple way to filter lists of objects."""
+from querylist.list import QueryList
+from querylist.dict import BetterDict
+
+__version__ = '0.4.0'
+__author__ = 'Thomas Welfley'
+
+__all__ = ['QueryList', 'BetterDict']
