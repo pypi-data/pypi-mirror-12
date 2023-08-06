ed95d8a626537e274fd16489e74aeeb68cbcb11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:44 +0000

    Add package rebulk/0.6.4/rebulk-0.6.4.tar.gz

---
+++ packages/rebulk/rebulk-0.6.4.tar.gz/rebulk-0.6.4/rebulk/processors.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Processor functions
+"""
+from .utils import IdentitySet
+
+DEFAULT = '__default__'
+
+from .rules import Rule, RemoveMatch
+
+from logging import getLogger
+log = getLogger(__name__).log
+
+POST_PROCESS = -2048
+PRE_PROCESS = 2048
+
+
+def _default_conflict_solver(match, conflicting_match):
+    """
+    Default conflict solver for matches, shorter matches if they conflicts with longer ones
+
+    :param conflicting_match:
+    :type conflicting_match:
+    :param match:
+    :type match:
+    :return:
+    :rtype:
+    """
+    if len(conflicting_match.initiator) < len(match.initiator):
+        return conflicting_match
+    elif len(match.initiator) < len(conflicting_match.initiator):
+        return match
+    return None
+
+
+class ConflictSolver(Rule):
+    """
+    Remove conflicting matches.
+    """
+    priority = PRE_PROCESS
+
+    consequence = RemoveMatch
+
+    @property
+    def default_conflict_solver(self):  # pylint:disable=no-self-use
+        """
+        Default conflict solver to use.
+        """
+        return _default_conflict_solver
+
+    def when(self, matches, context):
+        to_remove_matches = IdentitySet()
+        for match in filter(lambda match: not match.private, matches):
+            conflicting_matches = matches.conflicting(match)
+
+            if conflicting_matches:
+                # keep the match only if it's the longest
+                for conflicting_match in filter(lambda match: not match.private, conflicting_matches):
+                    reverse = False
+                    conflict_solvers = [(self.default_conflict_solver, False)]
+
+                    if match.conflict_solver:
+                        conflict_solvers.append((match.conflict_solver, False))
+                    if conflicting_match.conflict_solver:
+                        conflict_solvers.append((conflicting_match.conflict_solver, True))
+
+                    for conflict_solver, reverse in reversed(conflict_solvers):
+                        if reverse:
+                            to_remove = conflict_solver(conflicting_match, match)
+                        else:
+                            to_remove = conflict_solver(match, conflicting_match)
+                        if to_remove == DEFAULT:
+                            continue
+                        if to_remove and to_remove not in to_remove_matches:
+                            to_remove_matches.add(to_remove)
+                        break
+        return to_remove_matches
+
+
+class PrivateRemover(Rule):
+    """
+    Removes private matches rule.
+    """
+    priority = POST_PROCESS
+
+    consequence = RemoveMatch
+
+    def when(self, matches, context):
+        return [match for match in matches if match.private]
+
---
+++ packages/rebulk/rebulk-0.6.4.tar.gz/rebulk-0.6.4/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from platform import python_implementation
+
+import sys
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+install_requires = ['six', 'regex']
+if sys.version_info < (2, 7):
+    install_requires.extend(['ordereddict'])
+
+if python_implementation() == 'PyPy':
+    install_requires.remove('regex')  # PyPy doesn't support regex module
+
+setup_requires=['pytest-runner']
+
+tests_require=['pytest']
+
+exec(open("rebulk/__version__.py").read())  # load version without importing rebulk
+
+args = dict(name='rebulk',
+            version=__version__,
+            description='Rebulk - Define simple search patterns in bulk to perform advanced matching on any string.',
+            long_description=README,
+            # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+            classifiers=['Development Status :: 5 - Production/Stable',
+                         'License :: OSI Approved :: MIT License',
+                         'Operating System :: OS Independent',
+                         'Intended Audience :: Developers',
+                         'Programming Language :: Python :: 2',
+                         'Programming Language :: Python :: 2.6',
+                         'Programming Language :: Python :: 2.7',
+                         'Programming Language :: Python :: 3',
+                         'Programming Language :: Python :: 3.3',
+                         'Programming Language :: Python :: 3.4',
+                         'Programming Language :: Python :: 3.5',
+                         'Topic :: Software Development :: Libraries :: Python Modules'
+                         ],
+            keywords='re regexp regular expression search pattern string match',
+            author='Rémi Alvergnat',
+            author_email='toilal.dev@gmail.com',
+            url='https://github.com/Toilal/rebulk/',
+            download_url='https://pypi.python.org/packages/source/r/rebulk/rebulk-%s.tar.gz' % __version__,
+            license='MIT',
+            packages=find_packages(),
+            include_package_data=True,
+            setup_requires=setup_requires,
+            install_requires=install_requires,
+            tests_require=tests_require,
+            test_suite='rebulk.test',
+            zip_safe=True
+            )
+
+setup(**args)
