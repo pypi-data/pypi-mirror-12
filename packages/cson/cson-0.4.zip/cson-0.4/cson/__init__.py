a19e7c09ac050e0351830e7770c8d12c6593ae46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:11 +0000

    Add package cson/0.4/cson-0.4.zip

---
+++ packages/cson/cson-0.4.zip/cson-0.4/cson/__init__.py
@@ -0,0 +1,3 @@
+from .parser import load, loads
+from .peg import ParseError
+from json import dump, dumps
---
+++ packages/cson/cson-0.4.zip/cson-0.4/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='cson',
+    version='0.4',
+
+    description='A parser for Coffeescript Object Notation (CSON)',
+    author='Martin Vejnár',
+    author_email='vejnar.martin@gmail.cz',
+    url='https://github.com/avakar/pycson',
+    license='MIT',
+
+    packages=['cson'],
+    )
