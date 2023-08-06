553abc8c295c70dd159d92f5ed4c024dce661eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:31 +0000

    Add package getpivots/1.0/getpivots-1.0.zip

---
+++ packages/getpivots/getpivots-1.0.zip/getpivots-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name='getpivots',
+	version = '1.0',
+	scripts=['gp.py'],
+	install_requires=['ystockquote', 'yahoo_finance', 'regex', 'argparse','datetime'])
+ 
+
+
