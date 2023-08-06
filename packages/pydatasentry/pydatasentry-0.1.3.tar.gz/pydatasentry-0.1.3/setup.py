434423752e5e5b39c62f84ba06b0cd6c1b23bb02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:39 +0000

    Add package pydatasentry/0.1.3/pydatasentry-0.1.3.tar.gz

---
+++ packages/pydatasentry/pydatasentry-0.1.3.tar.gz/pydatasentry-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='pydatasentry',
+      version='0.1.3',
+      description='Memory tool for Python-Based Data Science',
+      url='http://github.com/FourthLion/pydatasentry',
+      download_url='https://github.com/FourthLion/pydatasentry/tarball/0.1.3',
+      author='Venkata Pingali',
+      author_email='pingali@gmail.com',
+      packages=['pydatasentry'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Scientific/Engineering :: Information Analysis'
+      ],
+      keywords=['pydata', 'pandas', 'documentation', 
+                'regressions', 'tool'],
+      install_requires=[
+      ],
+      license='MIT',
+      )
