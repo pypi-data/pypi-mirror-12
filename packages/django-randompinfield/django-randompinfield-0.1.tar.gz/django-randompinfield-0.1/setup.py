1fe964e99a02f444d20a1e4df19c10b00f2c3320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:37 +0000

    Add package django-randompinfield/0.1/django-randompinfield-0.1.tar.gz

---
+++ packages/django-randompinfield/django-randompinfield-0.1.tar.gz/django-randompinfield-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django-randompinfield',
+  packages = ['randompinfield'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A django model field that generates a random pin of a desired length',
+  author = 'Danny Wilson',
+  author_email = 'dannywilson32@gmail.com',
+  url = 'https://github.com/bjldigital/django-randompinfield/', # use the URL to the github repo
+  download_url = 'https://github.com/bjldigital/django-randompinfield/archive/v0.1.tar.gz', # I'll explain this in a second
+  keywords = ['django', 'model', 'field'], # arbitrary keywords
+  classifiers = [],
+)
