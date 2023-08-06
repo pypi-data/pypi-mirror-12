07270e1ad79242c9aba770b5f2056828d05c4def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:26 +0000

    Add package oneconsole/0.1.7/oneconsole-0.1.7.tar.gz

---
+++ packages/oneconsole/oneconsole-0.1.7.tar.gz/oneconsole-0.1.7/README.rst
@@ -0,0 +1,9 @@
+
+
+
+Usage Instruction
+==================
+
+pip install oneconsole
+
+Documentation : http://oneconsole.us/developer/
---
+++ packages/oneconsole/oneconsole-0.1.7.tar.gz/oneconsole-0.1.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='oneconsole',
+      version='0.1.7',
+      description='Python Package for Oneconsole',
+      url='http://github.com/sachinvettithanam/oneconsole',
+      author='Sachin Philip Mathew',
+      author_email='sachinvettithanam@gmail.com',
+      license='MIT',
+      packages=['oneconsole'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
