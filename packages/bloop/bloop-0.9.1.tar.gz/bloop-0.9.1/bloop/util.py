906f01bd73cfe8d0e974a51c7300cc168c4d81ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:02 +0000

    Add package bloop/0.9.1/bloop-0.9.1.tar.gz

---
+++ packages/bloop/bloop-0.9.1.tar.gz/bloop-0.9.1/bloop/util.py
@@ -0,0 +1,12 @@
+def ordered(obj):
+    """
+    Return sorted version of nested dicts/lists for comparing.
+
+    http://stackoverflow.com/a/25851972
+    """
+    if isinstance(obj, dict):
+        return sorted((k, ordered(v)) for k, v in obj.items())
+    if isinstance(obj, list):
+        return sorted(ordered(x) for x in obj)
+    else:
+        return obj
