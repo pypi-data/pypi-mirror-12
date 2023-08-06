93392bfd5fffb34ff8b18f9c023117b6df6475a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:20 +0000

    Add package pp_pyscript/1.1.0/pp_pyscript-1.1.0.tar.gz

---
+++ packages/pp_pyscript/pp_pyscript-1.1.0.tar.gz/pp_pyscript-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name   ='pp_pyscript',
+      version='1.1.0',
+      py_modules=['pyscript'],
+      author='pp',
+      author_email='ppmorning@foxmail.com',
+      url='none',
+      description='A simple printer of nester list',
+)
