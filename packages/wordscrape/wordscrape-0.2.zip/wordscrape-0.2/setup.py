5c9170e2ba53d47dafe37c28ad32dc1b1d52716a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:32 +0000

    Add package wordscrape/0.2/wordscrape-0.2.zip

---
+++ packages/wordscrape/wordscrape-0.2.zip/wordscrape-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wordscrape',
+      version='0.2',
+      description='Extract data from forms in Microsoft Word documents.',
+      url='http://github.com/Radcliffe/wordscrape',
+      author='David Radcliffe',
+      author_email='dradcliffe@gmail.com',
+      license='MIT',
+      packages=['wordscrape'],
+      zip_safe=False)
---
+++ packages/wordscrape/wordscrape-0.2.zip/wordscrape-0.2/wordscrape/tester.py
@@ -0,0 +1,5 @@
+from w import *
+
+path = '/temp'
+d = read_dir(path)
+write_csv(d, 'report.csv')
