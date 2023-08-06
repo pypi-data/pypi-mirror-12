b56b2a6884adae517f35845589484109efaee656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:58 +0000

    Add package bleachfields/1.0.4/bleachfields-1.0.4.tar.gz

---
+++ packages/bleachfields/bleachfields-1.0.4.tar.gz/bleachfields-1.0.4/bleachfields/bleachfield.py
@@ -0,0 +1,26 @@
+import re
+from HTMLParser import HTMLParser
+
+from bleach import clean
+
+ILLEGAL_CHARACTERS_RE = re.compile(r'[\000-\010]|[\013-\014]|[\016-\037]')
+
+
+class BleachField(object):
+
+    # Parser for unescaping HTML entities
+    _parser = HTMLParser()
+
+    def __init__(self, tags=(), strip=True, *args, **kwargs):
+        super(BleachField, self).__init__(*args, **kwargs)
+        self.strip = strip
+        self.tags = tags
+
+    def clean_text(self, text):
+        '''Clean text using bleach.'''
+        if text is None:
+            return ''
+        text = re.sub(ILLEGAL_CHARACTERS_RE, '', text)
+        if '<' in text or '&lt' in text:
+            text = clean(text, tags=self.tags, strip=self.strip)
+        return self._parser.unescape(text)
