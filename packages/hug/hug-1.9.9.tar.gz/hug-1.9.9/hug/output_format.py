042dfa15868487ac434a2f880ab40b73db2528d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:12 +0000

    Add package hug/1.9.9/hug-1.9.9.tar.gz

---
+++ packages/hug/hug-1.9.9.tar.gz/hug-1.9.9/hug/output_format.py
@@ -0,0 +1,173 @@
+"""hug/output_format.py
+
+Defines Hug's built-in output formatting methods
+
+Copyright (C) 2015  Timothy Edmund Crosley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+import base64
+import json as json_converter
+import mimetypes
+import os
+from decimal import Decimal
+from datetime import date, datetime
+from io import BytesIO
+
+from falcon import HTTP_NOT_FOUND
+from hug.format import camelcase, content_type
+
+IMAGE_TYPES = ('png', 'jpg', 'bmp', 'eps', 'gif', 'im', 'jpeg', 'msp', 'pcx', 'ppm', 'spider', 'tiff', 'webp', 'xbm',
+               'cur', 'dcx', 'fli', 'flc', 'gbr', 'gd', 'ico', 'icns', 'imt', 'iptc', 'naa', 'mcidas', 'mpo', 'pcd',
+               'psd', 'sgi', 'tga', 'wal', 'xpm', 'svg', 'svg+xml')
+
+VIDEO_TYPES = (('flv', 'video/x-flv'), ('mp4', 'video/mp4'), ('m3u8', 'application/x-mpegURL'), ('ts', 'video/MP2T'),
+               ('3gp', 'video/3gpp'), ('mov', 'video/quicktime'), ('avi', 'video/x-msvideo'), ('wmv', 'video/x-ms-wmv'))
+
+
+def _json_converter(item):
+    if hasattr(item, '__native_types__'):
+        return item.__native_types__()
+    elif isinstance(item, (date, datetime)):
+        return item.isoformat()
+    elif isinstance(item, bytes):
+        try:
+            return item.decode('utf8')
+        except UnicodeDecodeError:
+            return base64.b64encode(item)
+    elif hasattr(item, '__iter__'):
+        return list(item)
+    elif isinstance(item, Decimal):
+        return str(item)
+
+    raise TypeError("Type not serializable")
+
+
+@content_type('application/json')
+def json(content, **kwargs):
+    '''JSON (Javascript Serialized Object Notation)'''
+    if hasattr(content, 'read'):
+        return content
+
+    if isinstance(content, tuple) and getattr(content, '_fields', None):
+        content = {field: getattr(content, field) for field in content._fields}
+    return json_converter.dumps(content, default=_json_converter, **kwargs).encode('utf8')
+
+
+@content_type('text/plain')
+def text(content):
+    '''Free form UTF8 text'''
+    if hasattr(content, 'read'):
+        return content
+
+    return str(content).encode('utf8')
+
+
+@content_type('text/html')
+def html(content):
+    '''HTML (Hypertext Markup Language)'''
+    if hasattr(content, 'read'):
+        return content
+
+    return str(content).encode('utf8')
+
+
+def _camelcase(dictionary):
+    if not isinstance(dictionary, dict):
+        return dictionary
+
+    new_dictionary = {}
+    for key, value in dictionary.items():
+        if isinstance(key, str):
+            key = camelcase(key)
+        new_dictionary[key] = _camelcase(value)
+    return new_dictionary
+
+
+@content_type('application/json')
+def json_camelcase(content):
+    '''JSON (Javascript Serialized Object Notation) with all keys camelCased'''
+    return json(_camelcase(content))
+
+
+@content_type('application/json')
+def pretty_json(content):
+    '''JSON (Javascript Serialized Object Notion) pretty printed and indented'''
+    return json(content, indent=4, separators=(',', ': '))
+
+
+def image(image_format, doc=None):
+    '''Dynamically creates an image type handler for the specified image type'''
+    @content_type('image/{0}'.format(image_format))
+    def image_handler(data):
+        if hasattr(data, 'read'):
+            return data
+        elif hasattr(data, 'save'):
+            output = BytesIO()
+            data.save(output, format=image_format.upper())
+            output.seek(0)
+            return output
+        elif hasattr(data, 'render'):
+            return data.render()
+        elif os.path.isfile(data):
+            return open(data, 'rb')
+
+    image_handler.__doc__ = doc or "{0} formatted image".format(image_format)
+    return image_handler
+
+
+for image_type in IMAGE_TYPES:
+    globals()['{0}_image'.format(image_type.replace("+", "_"))] = image(image_type)
+
+
+def video(video_type, video_mime, doc=None):
+    '''Dynamically creates a video type handler for the specified video type'''
+    @content_type(video_mime)
+    def video_handler(data):
+        if hasattr(data, 'read'):
+            return data
+        elif hasattr(data, 'save'):
+            output = BytesIO()
+            data.save(output, format=video_type.upper())
+            output.seek(0)
+            return output
+        elif hasattr(data, 'render'):
+            return data.render()
+        elif os.path.isfile(data):
+            return open(data, 'rb')
+
+    video_handler.__doc__ = doc or "{0} formatted video".format(video_type)
+    return video_handler
+
+
+for (video_type, video_mime) in VIDEO_TYPES:
+    globals()['{0}_video'.format(video_type)] = video(video_type, video_mime)
+
+
+@content_type('file/dynamic')
+def file(data, response):
+    '''A dynamically retrieved file'''
+    if hasattr(data, 'read'):
+        name, data = getattr(data, 'name', ''), data
+    elif os.path.isfile(data):
+        name, data = data, open(data, 'rb')
+    else:
+        response.content_type = 'text/plain'
+        response.status = HTTP_NOT_FOUND
+        return 'File not found!'
+
+    response.content_type = mimetypes.guess_type(name, None)[0]
+    return data
---
+++ packages/hug/hug-1.9.9.tar.gz/hug-1.9.9/hug/types.py
@@ -0,0 +1,183 @@
+"""hug/types.py
+
+Defines hugs built-in supported types / validators
+
+Copyright (C) 2015  Timothy Edmund Crosley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+from decimal import Decimal
+from json import loads as load_json
+
+
+def accept(formatter, doc=None, error_text=None, cli_behaviour=None, exception_handlers=None):
+    '''Allows quick wrapping any Python type converter for use with Hug type annotations'''
+    defined_exception_handlers = exception_handlers or {}
+    if defined_exception_handlers or error_text:
+        def hug_formatter(data):
+            try:
+                return formatter(data)
+            except Exception as exception:
+                for take_exception, rewrite in defined_exception_handlers.items():
+                    if isinstance(exception, take_exception):
+                        if isinstance(rewrite, str):
+                            raise ValueError(rewrite)
+                        else:
+                            raise rewrite(data)
+                if error_text:
+                    raise ValueError(error_text)
+                raise exception
+    else:
+        def hug_formatter(data):
+            return formatter(data)
+
+    new_cli_behaviour = getattr(formatter, 'cli_behaviour', {})
+    if cli_behaviour:
+        new_cli_behaviour.update(cli_behaviour)
+    if new_cli_behaviour:
+        hug_formatter.cli_behaviour = new_cli_behaviour
+    hug_formatter.__doc__ = doc or formatter.__doc__
+    return hug_formatter
+
+
+number = accept(int, 'A whole number', 'Invalid whole number provided')
+float_number = accept(float, 'A float number', 'Invalid float number provided')
+decimal = accept(Decimal, 'A decimal number', 'Invalid decimal number provided')
+text = accept(str, 'Basic text / string value', 'Invalid text value provided')
+boolean = accept(bool, 'Providing any value will set this to true',
+                 'Invalid boolean value provided', cli_behaviour={'action': 'store_true'})
+
+
+def multiple(value):
+    '''Multiple Values'''
+    return value if isinstance(value, list) else [value]
+multiple.cli_behaviour = {'action': 'append', 'type':text}
+
+
+def comma_separated_list(value):
+    '''Multiple values, separated by a comma'''
+    return value.split(",")
+
+
+def smart_boolean(input_value):
+    '''Accepts a true or false value'''
+    if type(input_value) == bool or input_value in (None, 1, 0):
+        return bool(input_value)
+
+    value = input_value.lower()
+    if value in ('true', 't', '1'):
+        return True
+    elif value in ('false', 'f', '0', ''):
+        return False
+
+    raise KeyError('Invalid value passed in for true/false field')
+
+smart_boolean.cli_behaviour = {'action': 'store_true'}
+
+
+def inline_dictionary(input_value):
+    '''A single line dictionary, where items are separted by commas and key:value are separated by a pipe'''
+    return {key.strip(): value.strip() for key, value in (item.split(":") for item in input_value.split("|"))}
+
+
+def one_of(values):
+    '''Ensures the value is within a set of acceptable values'''
+    def matches(value):
+        if not value in values:
+            raise KeyError('Invalid value passed. The accepted values are: ({0})'.format("|".join(values)))
+        return value
+
+    matches.__doc__ = 'Accepts one of the following values: ({0})'.format("|".join(values))
+    matches.cli_behaviour = {'choices': values}
+    return matches
+
+
+def mapping(value_map):
+    '''Ensures the value is one of an acceptable set of values mapping those values to a Python equivelent'''
+    values = value_map.keys()
+    def matches(value):
+        if not value in value_map.keys():
+            raise KeyError('Invalid value passed. The accepted values are: ({0})'.format("|".join(values)))
+        return value_map[value]
+
+    matches.__doc__ = 'Accepts one of the following values: ({0})'.format("|".join(values))
+    matches.cli_behaviour = {'choices': values}
+    return matches
+
+
+def json(value):
+    '''Accepts a JSON formatted data structure'''
+    if type(value) in (str, bytes):
+        try:
+            return load_json(value)
+        except Exception:
+            raise ValueError('Incorrectly formatted JSON provided')
+    else:
+        return value
+
+
+def multi(*types):
+    '''Enables accepting one of multiple type methods'''
+    type_strings = (type_method.__doc__ for type_method in types)
+    doc_string = 'Accepts any of the following value types:{0}\n'.format('\n  - '.join(type_strings))
+    def multi_type(value):
+        for type_method in types:
+            try:
+                return type_method(value)
+            except:
+                pass
+        raise ValueError(doc_string)
+    multi_type.__doc__ = doc_string
+    return multi_type
+
+
+def in_range(lower, upper, convert=number):
+    '''Accepts a number within a lower and upper bound of acceptable values'''
+    def check_in_range(value):
+        value = convert(value)
+        if value < lower:
+            raise ValueError("'{0}' is less than the lower limit {1}".format(value, lower))
+        if value >= upper:
+            raise ValueError("'{0}' reaches the limit of {1}".format(value, upper))
+        return value
+
+    check_in_range.__doc__ = ("{0} that is greater or equal to {1} and less than {2}".format(
+                              convert.__doc__, lower, upper))
+    return check_in_range
+
+
+def less_than(limit, convert=number):
+    '''Accepts a value up to the specified limit'''
+    def check_less_than(value):
+        value = convert(value)
+        if not value < limit:
+            raise ValueError("'{0}' must be less than {1}".format(value, limit))
+        return value
+
+    check_less_than.__doc__ = "{0} that is less than {1}".format(convert.__doc__, limit)
+    return check_less_than
+
+
+def greater_than(minimum, convert=number):
+    '''Accepts a value above a given minimum'''
+    def check_greater_than(value):
+        value = convert(value)
+        if not value > minimum:
+            raise ValueError("'{0}' must be greater than {1}".format(value, minimum))
+        return value
+
+    check_greater_than.__doc__ = "{0} that is greater than {1}".format(convert.__doc__, minimum)
+    return check_greater_than
