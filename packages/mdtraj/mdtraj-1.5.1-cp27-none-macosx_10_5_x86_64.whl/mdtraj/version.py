dda95a60ace35596a97650360314ab082edd216b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:15 +0000

    Add package mdtraj/1.5.1/mdtraj-1.5.1-cp27-none-macosx_10_5_x86_64.whl

---
+++ packages/mdtraj/mdtraj-1.5.1-cp27-none-macosx_10_5_x86_64.whl/mdtraj/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM MDTRAJ SETUP.PY
+short_version = '1.5.1'
+version = '1.5.1'
+full_version = '1.5.1'
+git_revision = '737db804d1fbf25c70095dae339aa67c2b9e90d6'
+release = True
+
+if not release:
+    version = full_version
