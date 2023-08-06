afddd53224da5e524b1a89d7da10b5465102d0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:37 +0000

    Add package getalbum/1.0.0/getalbum-1.0.0.tar.gz

---
+++ packages/getalbum/getalbum-1.0.0.tar.gz/getalbum-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.rst", "rb") as f:
+    long_descr = f.read().decode('utf-8')
+
+setup(
+    name = "getalbum",
+    packages = ["getalbum"],
+    install_requires = ['requests'],
+    entry_points = {
+        "console_scripts": ['getalbum = getalbum.getalbum:main']
+        },
+    version = "1.0.0",
+    description = "Get album track list from Spotify",
+    long_description = long_descr,
+    author = "Steven Smith",
+    author_email = "stevensmith.ome@gmail.com",
+    license = "MIT",
+    url = "https://blha303.github.io/getalbum/",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "Topic :: Multimedia :: Sound/Audio"
+        ]
+    )
