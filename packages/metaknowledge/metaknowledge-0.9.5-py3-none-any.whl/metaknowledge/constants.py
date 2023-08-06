2947ac3ed3a6138014312b90d1e1827cfd7d23ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:37 +0000

    Add package metaknowledge/0.9.5/metaknowledge-0.9.5-py3-none-any.whl

---
+++ packages/metaknowledge/metaknowledge-0.9.5-py3-none-any.whl/metaknowledge/constants.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+def isInteractive():
+    """
+    A basic check of if the program is running in interactive mode
+    """
+    if sys.stdout.isatty() and os.name != 'nt':
+        try:
+            import threading
+        except ImportError:
+            return False
+        else:
+            return True
+    else:
+        return False
+
+VERBOSE_MODE = isInteractive()
