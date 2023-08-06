a7aa64851ccb99778939355e17f5b5ddf8b4b9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:12 +0000

    Add package network_tester/0.3.0/network_tester-0.3.0.tar.gz

---
+++ packages/network_tester/network_tester-0.3.0.tar.gz/network_tester-0.3.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys
+
+with open("network_tester/version.py", "r") as f:
+    exec(f.read())
+
+requirements = ["rig>=0.11.0", "numpy>1.6", "six"]
+if sys.version_info < (3, 4, 0):
+    # Python versions < 3.4 require "enum34" to be installed
+    requirements.append("enum34")
+
+setup(
+    name="network_tester",
+    version=__version__,
+    packages=find_packages(),
+    package_data={'network_tester': ['binaries/*.aplx']},
+
+    # Metadata for PyPi
+    url="https://github.com/project-rig/network_tester",
+    author="Jonathan Heathcote",
+    description="SpiNNaker network experiment library.",
+    license="GPLv2",
+
+    # Requirements
+    install_requires=requirements,
+)
