9aa7fbfe3580d323a7729d2b0ee1dfd755179087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:32 +0000

    Add package ebi/0.1/ebi-0.1.tar.gz

---
+++ packages/ebi/ebi-0.1.tar.gz/ebi-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+long_description = open('./README.rst').read()
+
+setup(
+    name='ebi',
+    version='0.1',
+    install_requires=[
+        'awsebcli==3.5.5',
+    ],
+    description='Simple CLI tool for ElasticBeanstalk',
+    long_description=long_description,
+    url='https://github.com/hirokiky/ebi',
+    author='Hiroki KIYOHARA',
+    author_email='hirokiky@gmail.com',
+    license='MIT',
+    packages=['ebi'],
+    entry_points={
+        'console_scripts': [
+            'ebi = ebi.core:main',
+        ]
+    }
+)
