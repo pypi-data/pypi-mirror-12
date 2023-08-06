f6e74b3ab5fdfdde46bfcd8da7718ee501116733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:16 +0000

    Add package metacorus-django-utils/1.1.5/metacorus-django-utils-1.1.5.tar.gz

---
+++ packages/metacorus-django-utils/metacorus-django-utils-1.1.5.tar.gz/metacorus-django-utils-1.1.5/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+from setuptools import setup, find_packages
+
+
+setup(name='metacorus-django-utils',
+    version='1.1.5',
+    author='S.Prymak',
+    author_email='sprymak@metacorus.com',
+    url='https://github.com/sprymak/metacorus-django-utils',
+    license='LICENSE',
+    packages=find_packages(),
+    install_requires=[
+        "html5lib",
+        "unidecode",
+    ],
+)
