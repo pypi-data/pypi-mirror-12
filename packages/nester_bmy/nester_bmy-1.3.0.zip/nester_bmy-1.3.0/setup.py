3fc4c21fc25385f54ab977cfe2823eb41076513f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:06 +0000

    Add package nester_bmy/1.3.0/nester_bmy-1.3.0.zip

---
+++ packages/nester_bmy/nester_bmy-1.3.0.zip/nester_bmy-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester_bmy',
+    version = '1.3.0',
+    py_modules = ['nester_bmy'],
+    author = 'hfpython',
+    author_email = 'hfpython@headfisrstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists'
+    )
