76691bc10b9703b97dc40045c1dac5d2bc343ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:31 +0000

    Add package django-epic-sample/0.2.7/django-epic-sample-0.2.7.tar.gz

---
+++ packages/django-epic-sample/django-epic-sample-0.2.7.tar.gz/django-epic-sample-0.2.7/epic-sample/epic-sample/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tstproject.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^autocomplete/', include('autocomplete_light.urls')),
+    url(r'^epic/', include('epic.urls', namespace='epic')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        { 'document_root': settings.MEDIA_ROOT })
+)
