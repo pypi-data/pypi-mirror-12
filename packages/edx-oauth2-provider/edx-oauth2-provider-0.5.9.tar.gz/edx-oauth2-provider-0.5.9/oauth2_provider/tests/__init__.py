87bd974ac41a87dbf187adb034d759e8c5fe4942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:00 +0000

    Add package edx-oauth2-provider/0.5.9/edx-oauth2-provider-0.5.9.tar.gz

---
+++ packages/edx-oauth2-provider/edx-oauth2-provider-0.5.9.tar.gz/edx-oauth2-provider-0.5.9/oauth2_provider/tests/__init__.py
@@ -0,0 +1,5 @@
+# pylint: disable=missing-docstring
+
+from oauth2_provider.tests.base import OAuth2TestCase, IDTokenTestCase
+from oauth2_provider.tests.test_userinfo import UserInfoTestCase
+from oauth2_provider.tests.handlers import TestHandler
