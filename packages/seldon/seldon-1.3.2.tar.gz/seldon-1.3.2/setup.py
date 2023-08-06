d053739af59a3691c52a7ec064d9282cee152753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:08 +0000

    Add package seldon/1.3.2/seldon-1.3.2.tar.gz

---
+++ packages/seldon/seldon-1.3.2.tar.gz/seldon-1.3.2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+setup(name='seldon',
+      version='1.3.2',
+      description='Seldon Python Utilities',
+      author='Clive Cox',
+      author_email='support@seldon.io',
+      url='https://github.com/SeldonIO/seldon-server',
+      license='Apache 2 License',
+      install_requires=['wabbit_wappa==3.0.2','xgboost','kazoo','scikit-learn>=0.17','scipy','numpy','boto','filechunkio','keras', 'pylibmc', 'annoy', 'gensim', "Flask", "pandas>=0.17"],
+      dependency_links = [
+          'https://github.com/SeldonIO/wabbit_wappa/archive/3.0.2.zip#egg=wabbit-wappa-3.0.2'
+      ],
+      packages=['seldon', 'seldon.pipeline', 'seldon.microservice', 'seldon.text'],
+      )
