5aa8c2c72fad806b6915a3778dbc4b75a4e65995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:23 +0000

    Add package JPMyNumber/0.1.0/JPMyNumber-0.1.0.tar.gz

---
+++ packages/JPMyNumber/JPMyNumber-0.1.0.tar.gz/JPMyNumber-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from jpmynumber import __version__
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='JPMyNumber',
+    version=__version__,
+    author='itkr',
+    author_email='itkrst@gmail.com',
+    description=(
+        'MyNumber (Japanese common number of social security and tax) library'),
+    long_description=long_description,
+    license='MIT',
+    keywords=['MyNumber', 'Japanese'],
+    url='https://github.com/itkr/JPMyNumber',
+    packages=['jpmynumber'],
+)
