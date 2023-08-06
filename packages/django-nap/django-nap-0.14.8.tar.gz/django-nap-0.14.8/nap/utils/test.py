f70b8e084bc8a048191a1d244176f4202ea9458c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:39 +0000

    Add package django-nap/0.14.8/django-nap-0.14.8.tar.gz

---
+++ packages/django-nap/django-nap-0.14.8.tar.gz/django-nap-0.14.8/nap/utils/test.py
@@ -0,0 +1,22 @@
+import json
+
+from django.test import Client, TestCase
+
+
+class JsonClient(Client):
+
+    def generic(self, method, path, data='', *args, **kwargs):
+        if 'json' in kwargs:
+            if data:
+                raise ValueError("May pass at most one of 'data' and 'json'.")
+            data = json.dumps(kwargs.pop('json'))
+            kwargs.setdefault('content_type', 'text/json')
+        return super(Client, self).generic(method, path, data, *args, **kwargs)
+
+
+class ApiTestCase(TestCase):
+
+    def assertResponseCode(self, url, status_code, method='get', **kwargs):
+        result = self.client.generic(method, **kwargs)
+        self.assertEqual(result.status_code, status_code)
+        return result
