eeb5b412839e4e051fc566432a7b940cf101cecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:16 +0000

    Add package Arachne/0.3.0/Arachne-0.3.0.tar.gz

---
+++ packages/Arachne/Arachne-0.3.0.tar.gz/Arachne-0.3.0/arachne/tests/test_flaskapp.py
@@ -0,0 +1,14 @@
+# from arachne.flaskapp import Arachne, check_dir
+# from unittest import TestCase
+
+# class TestFlaskApp(TestCase):
+
+#     def setUp(self):
+#         self.app = Arachne(__name__)
+#         self.app.config['SPIDER_SETTINGS'] = [{
+#             'endpoints': 'abc',
+#             'location': 'spiders.abc.ABC',
+#             'spider': 'ABC',
+#         }]
+#         self.client = self.app.test_client()
+
