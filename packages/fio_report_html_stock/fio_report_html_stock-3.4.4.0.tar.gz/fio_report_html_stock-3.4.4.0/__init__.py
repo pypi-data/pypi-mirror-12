6496d37057b7cc337cea19cf83d40560056a10c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:17 +0000

    Add package fio_report_html_stock/3.4.4.0/fio_report_html_stock-3.4.4.0.tar.gz

---
+++ packages/fio_report_html_stock/fio_report_html_stock-3.4.4.0.tar.gz/fio_report_html_stock-3.4.4.0/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from trytond.pool import Pool
+from stock import ShipmentOut, Move
+from report_html_stock import PickingList, SupplierRestockingList, \
+    CustomerReturnRestockingList, ConsolidatedPickingList, DeliveryNote, \
+    ProductLedger, ProductLedgerStartView, ProductLedgerReport
+
+
+def register():
+    Pool.register(
+        ShipmentOut,
+        Move,
+        ProductLedgerStartView,
+        module='report_html_stock', type_='model'
+    )
+    Pool.register(
+        ProductLedger,
+        module='report_html_stock', type_='wizard'
+    )
+    Pool.register(
+        PickingList,
+        SupplierRestockingList,
+        CustomerReturnRestockingList,
+        ConsolidatedPickingList,
+        DeliveryNote,
+        ProductLedgerReport,
+        module='report_html_stock', type_='report'
+    )
