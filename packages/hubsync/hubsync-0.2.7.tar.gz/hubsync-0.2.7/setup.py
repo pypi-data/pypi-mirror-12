8f96617c7ddd9f332b2eb61a989a15b81f016fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:55 +0000

    Add package hubsync/0.2.7/hubsync-0.2.7.tar.gz

---
+++ packages/hubsync/hubsync-0.2.7.tar.gz/hubsync-0.2.7/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='hubsync',
+    packages=['hubsync'],
+    version='0.2.7',
+    description='Get your github workspace synced!',
+    author='Mario Corchero',
+    author_email='mariocj89@gmail.com',
+    url='https://github.com/Mariocj89/hubsync',
+    keywords=['github', 'sync', 'workspace'],
+    scripts=['bin/hubsync'],
+    test_suite='nose.collector',
+    use_2to3=True,
+    install_requires=['gitpython', 'requests', 'six'],
+    tests_require=['mock']
+)
