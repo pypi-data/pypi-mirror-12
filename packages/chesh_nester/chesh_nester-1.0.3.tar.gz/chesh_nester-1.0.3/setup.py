ee2d4adaedceb7cd3ad814d72eccf4fd5423209c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:42 +0000

    Add package chesh_nester/1.0.3/chesh_nester-1.0.3.tar.gz

---
+++ packages/chesh_nester/chesh_nester-1.0.3.tar.gz/chesh_nester-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name 			= 'chesh_nester',
+	version			= '1.0.3',
+	py_modules		= ['nester'],
+	author			= 'Chesh',
+	author_email	= 'chesh@chesh.info',
+	url				= 'http:chesh.info',
+	description		= 'simple printer of nested lists'
+	)
