c84bdbe19b38405344af3885f62eabbad73c28b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:07 +0000

    Add package hug/1.9.6/hug-1.9.6.tar.gz

---
+++ packages/hug/hug-1.9.6.tar.gz/hug-1.9.6/hug/documentation.py
@@ -0,0 +1,90 @@
+"""hug/documentation.py
+
+Defines tools that automate the creation of documentation for an API build using the Hug Framework
+
+Copyright (C) 2015  Timothy Edmund Crosley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+from collections import OrderedDict
+
+import hug.types
+
+
+def generate(module, base_url="", api_version=None):
+    '''Generates documentation based on a Hug API module, base_url, and api_version (if applicable)'''
+    documentation = OrderedDict()
+    overview = module.__doc__
+    if overview:
+        documentation['overview'] = overview
+
+    documentation['versions'] = OrderedDict()
+    versions = module.__hug__.versions
+    for version in (api_version, ) if api_version else versions:
+        documentation['versions'][version] = OrderedDict()
+
+    for url, methods in module.__hug__.routes.items():
+        for method, method_versions in methods.items():
+            for version, handler in method_versions.items():
+                if version == None:
+                    applies_to = versions
+                else:
+                    applies_to = (version, )
+                for version in applies_to:
+                    if api_version and version != api_version:
+                        continue
+
+                    doc = documentation['versions'][version].setdefault(url, OrderedDict())
+                    doc = doc.setdefault(method, OrderedDict())
+
+                    usage = handler.api_function.__doc__
+                    if usage:
+                        doc['usage'] = usage
+                    for example in handler.examples:
+                        example_text =  "{0}{1}{2}".format(base_url, '/v{0}'.format(version) if version else '', url)
+                        if isinstance(example, str):
+                            example_text += "?{0}".format(example)
+                        doc_examples = doc.setdefault('examples', [])
+                        if not example_text in doc_examples:
+                            doc_examples.append(example_text)
+                    doc['outputs'] = OrderedDict(format=handler.output_format.__doc__,
+                                                 content_type=handler.content_type)
+                    if handler.output_type:
+                        doc['outputs']['type'] = handler.output_type
+
+                    parameters = [param for param in handler.accepted_parameters if not param in ('request',
+                                                                                                  'response')
+                                                                                    and not param.startswith('hug_')
+                                                                                    and not param == 'self'
+                                                                                    and not hasattr(param, 'directive')]
+                    if parameters:
+                        inputs = doc.setdefault('inputs', OrderedDict())
+                        types = handler.api_function.__annotations__
+                        for argument in parameters:
+                            kind = types.get(argument, hug.types.text)
+                            input_definition = inputs.setdefault(argument, OrderedDict())
+                            input_definition['type'] = kind if isinstance(kind, str) else kind.__doc__
+                            default = handler.defaults.get(argument, None)
+                            if default is not None:
+                                input_definition['default'] = default
+
+    if len(documentation['versions']) == 1:
+        documentation.update(tuple(documentation['versions'].values())[0])
+        documentation.pop('versions')
+    else:
+        documentation['versions'].pop(None, '')
+
+    return documentation
---
+++ packages/hug/hug-1.9.6.tar.gz/hug-1.9.6/hug/types.py
@@ -0,0 +1,144 @@
+"""hug/types.py
+
+Defines hugs built-in supported types / validators
+
+Copyright (C) 2015  Timothy Edmund Crosley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+from decimal import Decimal
+from json import loads as load_json
+
+
+def accept(formatter, doc=None, error_text=None, cli_behaviour=None, exception_handlers=None):
+    '''Allows quick wrapping any Python type converter for use with Hug type annotations'''
+    defined_exception_handlers = exception_handlers or {}
+    if defined_exception_handlers or error_text:
+        def hug_formatter(data):
+            try:
+                return formatter(data)
+            except Exception as exception:
+                for take_exception, rewrite in defined_exception_handlers.items():
+                    if isinstance(exception, take_exception):
+                        if isinstance(rewrite, str):
+                            raise ValueError(rewrite)
+                        else:
+                            raise rewrite(data)
+                if error_text:
+                    raise ValueError(error_text)
+                raise exception
+    else:
+        def hug_formatter(data):
+            return formatter(data)
+
+    new_cli_behaviour = getattr(formatter, 'cli_behaviour', {})
+    if cli_behaviour:
+        new_cli_behaviour.update(cli_behaviour)
+    if new_cli_behaviour:
+        hug_formatter.cli_behaviour = new_cli_behaviour
+    hug_formatter.__doc__ = doc or formatter.__doc__
+    return hug_formatter
+
+
+number = accept(int, 'A whole number', 'Invalid whole number provided')
+float_number = accept(float, 'A float number', 'Invalid float number provided')
+decimal = accept(Decimal, 'A decimal number', 'Invalid decimal number provided')
+text = accept(str, 'Basic text / string value', 'Invalid text value provided')
+boolean = accept(bool, 'Providing any value will set this to true',
+                 'Invalid boolean value provided', cli_behaviour={'action': 'store_true'})
+
+
+def multiple(value):
+    '''Multiple Values'''
+    return value if isinstance(value, list) else [value]
+multiple.cli_behaviour = {'action': 'append', 'type':text}
+
+
+def comma_separated_list(value):
+    '''Multiple values, separated by a comma'''
+    return value.split(",")
+
+
+def smart_boolean(input_value):
+    '''Accepts a true or false value'''
+    if type(input_value) == bool or input_value in (None, 1, 0):
+        return bool(input_value)
+
+    value = input_value.lower()
+    if value in ('true', 't', '1'):
+        return True
+    elif value in ('false', 'f', '0', ''):
+        return False
+
+    raise KeyError('Invalid value passed in for true/false field')
+
+smart_boolean.cli_behaviour = {'action': 'store_true'}
+
+
+def inline_dictionary(input_value):
+    '''A single line dictionary, where items are separted by commas and key:value are separated by a pipe'''
+    return {key.strip(): value.strip() for key, value in (item.split(":") for item in input_value.split("|"))}
+
+
+def one_of(values):
+    '''Ensures the value is within a set of acceptable values'''
+    def matches(value):
+        if not value in values:
+            raise KeyError('Invalid value passed. The accepted values are: ({0})'.format("|".join(values)))
+        return value
+
+    matches.__doc__ = 'Accepts one of the following values: ({0})'.format("|".join(values))
+    matches.cli_behaviour = {'choices': values}
+    return matches
+
+
+def mapping(value_map):
+    '''Ensures the value is one of an acceptable set of values mapping those values to a Python equivelent'''
+    values = value_map.keys()
+    def matches(value):
+        if not value in value_map.keys():
+            raise KeyError('Invalid value passed. The accepted values are: ({0})'.format("|".join(values)))
+        return value_map[value]
+
+    matches.__doc__ = 'Accepts one of the following values: ({0})'.format("|".join(values))
+    matches.cli_behaviour = {'choices': values}
+    return matches
+
+
+def json(value):
+    '''Accepts a JSON formatted data structure'''
+    if type(value) in (str, bytes):
+        try:
+            return load_json(value)
+        except Exception:
+            raise ValueError('Incorrectly formatted JSON provided')
+    else:
+        return value
+
+
+def multi(*types):
+    '''Enables accepting one of multiple type methods'''
+    type_strings = (type_method.__doc__ for type_method in types)
+    doc_string = 'Accepts any of the following value types:{0}\n'.format('\n  - '.join(type_strings))
+    def multi_type(value):
+        for type_method in types:
+            try:
+                return type_method(value)
+            except:
+                pass
+        raise ValueError(doc_string)
+    multi_type.__doc__ = doc_string
+    return multi_type
