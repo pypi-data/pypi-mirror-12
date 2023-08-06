3ebdf20b7b18f9a376d5e8ac2172b67a81cfc3af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:53 +0000

    Add package wagtail-stacks-embed/0.2/wagtail-stacks-embed-0.2.tar.gz

---
+++ packages/wagtail-stacks-embed/wagtail-stacks-embed-0.2.tar.gz/wagtail-stacks-embed-0.2/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='wagtail-stacks-embed',
+    packages=find_packages(),
+    version='0.2',
+    author=u'Jonathan Ellenberger',
+    author_email='jonathan_ellenberger@wgbh.org',
+    url='http://stacks.wgbhdigital.org/',
+    license='MIT License, see LICENSE',
+    description=(
+        "A Wagtail Stacks application for handling embeddable media."
+    ),
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    install_requires=[
+        'wagtail-streamfieldtools>=0.1',
+        'requests==2.5.1'
+    ],
+    package_data={
+        'wagtail_stacks_embed': [
+            'static/images/*.png',
+            'static/sass/*.scss',
+            'static/js/*.js',
+            'templates/wagtail_stacks_embed/single/*.html',
+            'templates/wagtail_stacks_embed/list/*.html'
+        ]
+    },
+    classifiers=[
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 3 - Alpha'
+    ]
+)
