7a4b79b48962902c49328f4ee3801c365353d7c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:46 +0000

    Add package django-sekizai/0.9.0/django-sekizai-0.9.0.tar.gz

---
+++ packages/django-sekizai/django-sekizai-0.9.0.tar.gz/django-sekizai-0.9.0/setup.py
@@ -0,0 +1,41 @@
+import sys
+
+from setuptools import setup, find_packages
+
+version = __import__('sekizai').__version__
+
+
+if sys.hexversion < 0x02070000:
+    EXTRA_INSTALL_REQUIRES = ['backport_collections']
+else:
+    EXTRA_INSTALL_REQUIRES = []
+
+setup(
+    name='django-sekizai',
+    version=version,
+    description='Django Sekizai',
+    author='Jonas Obrist',
+    author_email='ojiidotch@gmail.com',
+    url='http://github.com/ojii/django-sekizai',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'django-classy-tags>=0.3.1',
+    ] + EXTRA_INSTALL_REQUIRES,
+    test_suite='runtests.main',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
