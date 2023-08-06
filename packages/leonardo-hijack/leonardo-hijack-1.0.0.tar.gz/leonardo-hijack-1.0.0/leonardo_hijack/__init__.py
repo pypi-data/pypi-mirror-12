3b4a50ddb9d0565c9e47ac9860ae679f76086319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:54 +0000

    Add package leonardo-hijack/1.0.0/leonardo-hijack-1.0.0.tar.gz

---
+++ packages/leonardo-hijack/leonardo-hijack-1.0.0.tar.gz/leonardo-hijack-1.0.0/leonardo_hijack/__init__.py
@@ -0,0 +1,25 @@
+
+from django.apps import AppConfig
+
+
+default_app_config = 'leonardo_hijack.Config'
+
+
+LEONARDO_URLS_CONF = "hijack.urls"
+
+LEONARDO_APPS = [
+	'leonardo_hijack',
+	"hijack",
+	"compat"
+]
+
+LEONARDO_MIDDLEWARES = [
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'hijack.middleware.HijackRemoteUserMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
+]
+
+
+class Config(AppConfig):
+    name = 'leonardo_hijack'
+    verbose_name = "leonardo-hijack"
---
+++ packages/leonardo-hijack/leonardo-hijack-1.0.0.tar.gz/leonardo-hijack-1.0.0/leonardo_hijack/settings.py
@@ -0,0 +1,3 @@
+
+HIJACK_LOGIN_REDIRECT_URL = "/admin/"  # where you want to be redirected to, after hijacking the user.
+REVERSE_HIJACK_LOGIN_REDIRECT_URL = "/admin/"  # where you want to be redirected to, after releasing the user.
