88ac9fddd0ce9c0e57cd6bf006f2c0b886ffb209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:47 +0000

    Add package cpenv/0.1.6/cpenv-0.1.6.zip

---
+++ packages/cpenv/cpenv-0.1.6.zip/cpenv-0.1.6/cpenv/util.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+package_root = os.path.dirname(__file__)
+
+
+def expandpath(path):
+    '''Expand user and envvars in path.'''
+
+    return os.path.expandvars(os.path.expanduser(path))
+
+
+def unipath(*paths):
+    '''Like os.path.join but also expands and normalizes path parts.'''
+
+    return os.path.normpath(expandpath(os.path.join(*paths)))
+
+
+def binpath(*paths):
+    '''Like os.path.join but acts relative to this packages bin path.'''
+
+    return os.path.normpath(os.path.join(package_root, 'bin', *paths))
