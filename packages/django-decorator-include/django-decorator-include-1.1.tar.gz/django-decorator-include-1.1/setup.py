51c3ba05b76ab505e40ebb64f20b311a0c26c110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:08 +0000

    Add package django-decorator-include/1.1/django-decorator-include-1.1.tar.gz

---
+++ packages/django-decorator-include/django-decorator-include-1.1.tar.gz/django-decorator-include-1.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'django-decorator-include',
+    version = '1.1',
+    license = 'BSD',
+    description = 'Include Django URL patterns with decorators.',
+    long_description = read('README.rst'),
+    author = 'Jeff Kistler',
+    author_email = 'jeff@jeffkistler.com',
+    url = 'https://github.com/twidi/django-decorator-include/',
+    packages = ['decorator_include', 'decorator_include.tests', 'decorator_include.tests.testproject'],
+    package_dir = {'': 'src'},
+    install_requires=['future'],
+    classifiers = [
+        'Framework :: Django',
+        "Framework :: Django :: 1.4",
+        "Framework :: Django :: 1.5",
+        "Framework :: Django :: 1.6",
+        "Framework :: Django :: 1.7",
+        "Framework :: Django :: 1.8",
+#        "Framework :: Django :: 1.9",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+#        'Programming Language :: Python :: 3.5',
+    ]
+)
