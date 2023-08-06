905c4c5de244efbf8e3a4757718767cff9969204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:56 +0000

    Add package flopy/3.2.3/flopy-3.2.3.zip

---
+++ packages/flopy/flopy-3.2.3.zip/flopy-3.2.3/flopy/utils/__init__.py
@@ -0,0 +1,34 @@
+"""
+    the main entry point of utils
+
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+
+    See Also
+    --------
+
+    Notes
+    -----
+
+    Examples
+    --------
+
+    """
+from .mfreadnam import parsenamefile
+from .util_array import Util3d, Util2d, Transient2d, read1d
+from .util_list import MfList
+from .binaryfile import BinaryHeader, HeadFile, UcnFile, CellBudgetFile
+from .formattedfile import FormattedHeadFile
+from .modpathfile import PathlineFile, EndpointFile
+from .binaryswrfile import SwrObs, SwrFile
+from .binaryhydmodfile import HydmodObs
+from .reference import SpatialReference  # , TemporalReference
+from .flopy_io import model_attributes_to_shapefile, shape_attr_name, write_grid_shapefile
+from .mflistfile import MfListBudget, MfusgListBudget, SwtListBudget, \
+    SwrListBudget
---
+++ packages/flopy/flopy-3.2.3.zip/flopy-3.2.3/flopy/version.py
@@ -0,0 +1,4 @@
+# flopy version file
+#            created on......December 10, 2015
+__version__='3.2.3'
+__build__='3.2.3.1064'
