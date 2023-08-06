9598128429da90d4ab0c19e5ebeee1d8fe72d437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:42 +0000

    Add package zopyx.plone.cassandra/0.4.0.1/zopyx.plone.cassandra-0.4.0.1.zip

---
+++ packages/zopyx.plone.cassandra/zopyx.plone.cassandra-0.4.0.1.zip/zopyx.plone.cassandra-0.4.0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.4.0.1'
+
+setup(name='zopyx.plone.cassandra',
+      version=version,
+      description="Show all assigned local roles within a subtree for any Plone 4 site",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      url='http://pypi.python.org/pypi/zopyx.plone.cassandra',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zopyx', 'zopyx.plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- entry_points -*- 
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
