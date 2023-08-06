3d6b111a2ab6bf2a6c5104e9af9af65020223a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:12 +0000

    Add package solnado/0.9.3/solnado-0.9.3.tar.gz

---
+++ packages/solnado/solnado-0.9.3.tar.gz/solnado-0.9.3/setup.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from os.path import join, dirname
+from setuptools import setup, find_packages
+import sys
+import os
+
+VERSION = (0, 9, 3)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+f = open(join(dirname(__file__), 'README.rst'))
+long_description = f.read().strip()
+f.close()
+
+install_requires = [
+    'tornado>=3.2',
+]
+tests_require = [
+    'nose',
+    'coverage',
+    'mock',
+    'nosexcover',
+    'tornado>=3.2.2',
+]
+
+# use external unittest for 2.6
+if sys.version_info[:2] == (2, 6):
+    install_requires.append('unittest2')
+    install_requires.append('argparse')
+
+setup(
+    name             = 'solnado',
+    description      = "Tornado HTTP client for Solr",
+    license          = "Apache License, Version 2.0",
+    url              = "https://github.com/hodgesds/solnado",
+    long_description = long_description,
+    version          = __versionstr__,
+    author           = "Daniel Hodges",
+    author_email     = "hodges.daniel.scott@gmail.com",
+    scripts          = ['bin/solnado'],
+    packages         = find_packages(
+        where        = '.',
+        exclude      = ('tests*', 'bin*')
+    ),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    install_requires=install_requires,
+
+    test_suite='tests.run_tests.run_all',
+    tests_require=tests_require,
+)
---
+++ packages/solnado/solnado-0.9.3.tar.gz/solnado-0.9.3/solnado/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from .client import SolrClient
+VERSION = (0, 9, 3)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+
