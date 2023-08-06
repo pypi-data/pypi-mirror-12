2a818074a49e2ef46598a0bf061536497ea7fde9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:44 +0000

    Add package metapub/0.3.17.8/metapub-0.3.17.8.tar.gz

---
+++ packages/metapub/metapub-0.3.17.8.tar.gz/metapub-0.3.17.8/tests/test_pubmed_advquery.py
@@ -0,0 +1,56 @@
+import unittest
+
+from metapub import PubMedFetcher
+from metapub.pubmedcentral import *
+
+class TestPubmedFetcher(unittest.TestCase):
+
+    def setUp(self):
+        self.fetch = PubMedFetcher()
+
+    def tearDown(self):
+        pass
+
+    def test_pmids_for_query(self):
+        params = { 'jtitle': 'American Journal of Medical Genetics', 
+                    'year': 1996, 
+                    'volume': 61, 
+                    'author1_lastfm': 'Hegmann' }
+
+        pmids = self.fetch.pmids_for_query(**params)
+        assert len(pmids)==1
+
+        # this pubmed ID was deleted
+        params = { 'TA':'Journal of Neural Transmission', 
+                    'pdat':2014, 
+                    'vol':121, 
+                    'aulast': 'Freitag'
+         } 
+
+        pmids = self.fetch.pmids_for_query(**params)
+        assert len(pmids) == 0
+
+    def test_medical_genetics_query(self):
+        results = self.fetch.pmids_for_medical_genetics_query('Brugada Syndrome', 'diagnosis', debug=True, year=2013)
+
+        # these results do change over time...
+        assert '24484568' in results
+
+    def test_clinical_query(self):
+        results = self.fetch.pmids_for_clinical_query('Global developmental delay', 'etiology', 'narrow', debug=True, year=2013)
+
+        # these results do change over time...
+        assert '24123848' in results
+
+    def test_specified_return_slice(self):
+        pmids = self.fetch.pmids_for_query(since='2015/3/1', retmax=1000)
+        assert len(pmids)==1000
+
+        pmids = self.fetch.pmids_for_query(since='2015/3/1', retstart=200, retmax=500)
+        assert len(pmids)==500
+
+    def test_pmc_only(self):
+        params = { 'mesh': 'breast neoplasm' }
+        stuff = self.fetch.pmids_for_query(since='2015/1/1', until='2015/3/1', pmc_only=True, **params)
+        print stuff
+
