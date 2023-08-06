2bd31efa0c64edcc9e23f238a4fa22acaf904893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:50 +0000

    Add package nesterNiwesh/1.2.0/nesterNiwesh-1.2.0.tar.gz

---
+++ packages/nesterNiwesh/nesterNiwesh-1.2.0.tar.gz/nesterNiwesh-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		=	'nesterNiwesh',
+	version		=	'1.2.0',
+	py_modules	=	['nester'],
+	author		=	'niwesh',
+	author_email	=	'niweshbaraj@yahoo.com',
+	url		=	'http://www.niweshbaraj.com',
+	description	=	'A simple printer of nested lists.',
+     )
