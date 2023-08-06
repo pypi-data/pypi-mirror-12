3609a1fb05bc0a9b1805ee8046e793cd6597b205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:56 +0000

    Add package plan-py/0.0.1/plan-py-0.0.1.tar.gz

---
+++ packages/plan-py/plan-py-0.0.1.tar.gz/plan-py-0.0.1/plan/__init__.py
@@ -0,0 +1,10 @@
+def shell(s, on=None, start_new_session=False, asynchronous=True):
+    import subprocess
+    import os
+
+    if on is not None:
+        on = os.path.expanduser(on)
+
+    if asynchronous:
+        return subprocess.Popen(s, cwd=on, start_new_session=start_new_session)
+    return subprocess.run(s, cwd=on, start_new_session=start_new_session)
