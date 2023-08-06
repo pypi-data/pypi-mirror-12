60c3766213a83aea02b225cf3029f689f423f300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:02 +0000

    Add package vinisto/0.1.9/vinisto-0.1.9.tar.gz

---
+++ packages/vinisto/vinisto-0.1.9.tar.gz/vinisto-0.1.9/vinisto/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+
+__PATH__ = os.path.dirname(__file__)
+__APP_NAME__ = "vinisto"
+
+logging.basicConfig(level=logging.DEBUG)
+LOG = logging.getLogger(__name__)
