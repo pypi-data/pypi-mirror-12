aa1b11852e6ab8587212c5f79a62c2b05ce103bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:14 +0000

    Add package mao_nester/1.1.0/mao_nester-1.1.0.tar.gz

---
+++ packages/mao_nester/mao_nester-1.1.0.tar.gz/mao_nester-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+		name		= 'mao_nester',
+		version		= '1.1.0',
+		py_modules	= ['nester'],
+		author		= 'XZZ92',
+		author_email= '624532602@qq.com',
+		description	= 'a simple printer of nested list',
+	
+	
+	
+	)
