365f8f4e0cc43affc00266c54bff3d5ac7ed2ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:09 +0000

    Add package pluginmanager/0.0.9/pluginmanager-0.0.9-py3-none-any.whl

---
+++ packages/pluginmanager/pluginmanager-0.0.9-py3-none-any.whl/pluginmanager/plugin_filters/__init__.py
@@ -0,0 +1,6 @@
+from .by_class import by_class
+from .by_name import by_name
+from .activated import activated
+from .deactivated import deactivated
+
+__all__ = ['by_class', 'by_name', 'activated', 'deactivated']
