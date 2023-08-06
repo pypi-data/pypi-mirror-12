b3782278adbc3f935fe4efdb736b10ee6ce45d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:11 +0000

    Add package mbe/0.7.8/mbe-0.7.8.tar.gz

---
+++ packages/mbe/mbe-0.7.8.tar.gz/mbe-0.7.8/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3.4
+
+'''
+Created on Aug 11, 2013
+
+@author: Nicklas Boerjesson
+'''
+
+from setuptools import setup
+
+import sys
+import os
+
+
+
+from mbe import __release__
+
+if __name__ == "__main__":
+    setup(
+        name='mbe',
+        version=__release__,
+        description='MBE is a backend written in Python 3 that provides database access, authentication, access '
+                    'control and other features on top of the MongoDB document database.',
+        author='Nicklas Boerjesson',
+        author_email='nicklas_attheold_optimalbpm.se',
+        maintainer='Nicklas Boerjesson',
+        maintainer_email='nicklas_attheold_optimalbpm.se',
+        long_description="""'MBE is a backend written in Python 3 that provides database access, authentication,
+            access control and other features on top of the MongoDB document database.
+          """,
+        url='https://github.com/OptimalBPM/mbe',
+        packages=['mbe', 'mbe.schemas', 'mbe.misc', 'mbe.features'],
+        package_data = {
+            # If any package contains *.txt or *.xml files, include them:
+            '': ['*.json', '*.xml', '*.feature']
+        },
+        license='BSD')
