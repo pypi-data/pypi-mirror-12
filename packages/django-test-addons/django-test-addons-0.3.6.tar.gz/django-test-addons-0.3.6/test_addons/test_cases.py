8b3e165b7d88673b6e9602a4aabb31c854617606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:26 +0000

    Add package django-test-addons/0.3.6/django-test-addons-0.3.6.tar.gz

---
+++ packages/django-test-addons/django-test-addons-0.3.6.tar.gz/django-test-addons-0.3.6/test_addons/test_cases.py
@@ -0,0 +1,77 @@
+# inbuild python imports
+
+# inbuilt django imports
+from django.test import LiveServerTestCase
+
+# third party imports
+
+# inter-app imports
+
+# local imports
+from . import mixins
+from .mixins import SimpleTestCase
+
+
+class MongoTestCase(mixins.MongoTestMixin, SimpleTestCase):
+
+    """ TestCase that creates a mongo collection and clears it after each test """
+    pass
+
+
+class MongoLiveServerTestCase(mixins.MongoTestMixin, LiveServerTestCase):
+
+    """ TestCase that runs liveserver using mongodb instead of relational database  """
+    pass
+
+class Neo4jTestCase(mixins.Neo4jTestMixin, SimpleTestCase):
+
+    pass
+
+
+class MongoNeo4jTestCase(mixins.MongoNeo4jTestMixin, mixins.SimpleTestCase):
+
+    pass
+
+
+class RedisTestCase(mixins.RedisTestMixin, mixins.SimpleTestCase):
+
+    pass
+
+
+class MongoRedisTestCase(mixins.MongoRedisTestMixin, mixins.SimpleTestCase):
+
+    pass
+
+
+class RedisMongoNeo4jTestCase(mixins.RedisMongoNeo4jTestMixin, mixins.SimpleTestCase):
+
+    pass
+
+
+class APIRedisTestCase(mixins.ApiTestMixin, RedisTestCase):
+
+    pass
+
+
+class APIMongoTestCase(mixins.ApiTestMixin, MongoTestCase):
+
+    pass
+
+
+class APINeo4jTestCase(mixins.ApiTestMixin, Neo4jTestCase):
+
+    pass
+
+
+class APIMongoRedisTestCase(mixins.ApiTestMixin, MongoRedisTestCase):
+
+    pass
+
+
+class APIRedisMongoNeo4jTestCase(mixins.ApiTestMixin, RedisMongoNeo4jTestCase):
+
+    pass
+
+
+
+
