1005391d4f88b8c348906ac06c9f941847de38ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:21 +0000

    Add package shakyo/0.0.2/shakyo-0.0.2.tar.gz

---
+++ packages/shakyo/shakyo-0.0.2.tar.gz/shakyo-0.0.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import setuptools
+import sys
+
+if not ((sys.version_info.major >= 3 and sys.version_info.minor >= 5)
+    or sys.version_info.major > 3):
+  exit("Sorry, Python's version must be later than 3.5.")
+
+import shakyo
+
+
+setuptools.setup(
+    name=shakyo.__name__,
+    version=shakyo.__version__,
+    description="a tool to learn about something just by copying it by hand",
+    license="Public Domain",
+    author="raviqqe",
+    author_email="raviqqe@gmail.com",
+    url="http://github.com/raviqqe/shakyo/",
+    py_modules=[shakyo.__name__],
+    entry_points={"console_scripts" : ["shakyo=shakyo:main"]},
+    install_requires=["text_unidecode", "validators"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console :: Curses",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Public Domain",
+        "Operating System :: POSIX",
+        "Topic :: Education :: Computer Aided Instruction (CAI)",
+        "Topic :: Games/Entertainment",
+    ],
+)
