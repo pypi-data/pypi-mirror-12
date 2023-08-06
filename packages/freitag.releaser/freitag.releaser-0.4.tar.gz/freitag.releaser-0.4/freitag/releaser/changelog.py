052225ed814a484a31dd2cd72794f6dccac4678a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:55 +0000

    Add package freitag.releaser/0.4/freitag.releaser-0.4.tar.gz

---
+++ packages/freitag.releaser/freitag.releaser-0.4.tar.gz/freitag.releaser-0.4/freitag/releaser/changelog.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+from git import Repo
+
+import os
+
+
+class VersionException(Exception):
+    """Raised when a version is not in the expected or value range."""
+    pass
+
+
+class GatherChangelog(object):
+
+    newest_version = 0
+    earliest_version = 0
+
+    def __call__(self):
+        # ask which changelog entries are wanted (between which versions)
+        self.ask_versions()
+
+        msg = '\nGATHERING CHANGELOG FROM {0} to {1}'
+        print(msg.format(self.earliest_version, self.newest_version))
+
+        changelog = self.gather_changelog(
+            self.newest_version,
+            self.earliest_version
+        )
+        print(changelog)
+
+    def ask_versions(self):
+        last_version = self._get_buildout_version()
+
+        # newest release
+        question = 'Newest version from where to gather changelog ({0})? '
+        self.newest_version = self.ask_question(
+            question.format(last_version),
+            last_version,
+            default=True
+        )
+
+        # oldest release
+        previous_release = last_version - 1
+        question = 'Oldest version to gather changelog from ({0})? '
+        self.earliest_version = self.ask_question(
+            question.format(previous_release),
+            previous_release,
+            default=True
+        )
+
+        if self.newest_version <= self.earliest_version:
+            msg = 'newest release {0} must be bigger than oldest release {1}!'
+            raise VersionException(
+                msg.format(
+                    self.newest_version,
+                    self.earliest_version
+                )
+            )
+
+    @staticmethod
+    def _get_buildout_version():
+        version_file = open('VERSION')
+        line = version_file.read()
+        version_file.close()
+        # minus one as VERSION file always points to the next release
+        return int(line.strip()) - 1
+
+    def ask_question(self, question, last_version, default=False):
+        answer = False
+        while not answer:
+            try:
+                answer = self.ask(question, last_version, default=default)
+            except VersionException:
+                print('Please provide a valid value for version.')
+        return answer
+
+    @staticmethod
+    def ask(question, last_version, default=False):
+        """Interactively ask a question, sanitize the input and return it.
+
+        :param question: Question to be asked to the user.
+        :type question: string
+        :param last_version: Last buildout release version.
+        :type last_version: int
+        :param default: If no answer is provided the `last_version` is used.
+        :type default: bool
+        :returns: Sanitized answer provided by user interactively.
+        :rtype: int
+        :raises:
+            :class:`VersionException`
+        """
+        response = raw_input(question)
+
+        # if there's a default answer and no input is provided return
+        # last_version.
+        if default and response == '':
+            return last_version
+
+        # check if the returned value can be casted as a number.
+        try:
+            response = int(response)
+        except ValueError:
+            raise VersionException('Version must be a number')
+
+        if response > last_version:
+            raise VersionException('This version still does not exist!')
+        if response < 1:
+            raise VersionException(
+                'This version does not exist, first version was 1'
+            )
+
+        return response
+
+    @staticmethod
+    def gather_changelog(newest, oldest):
+        repo = Repo(os.path.curdir)
+
+        new_releases = '\n\n'
+        other_commits = '\n\n'
+        begin = 'New version: {0}'.format(newest)
+        end = 'New version: {0}'.format(oldest)
+        at_begin = False
+
+        for commit in repo.iter_commits():
+            msg = commit.message
+            if msg.startswith(end):
+                break
+            if msg.startswith(begin):
+                at_begin = True
+            if at_begin:
+                if msg.startswith('New releases of:'):
+                    new_releases = new_releases + msg + '\n'
+                elif not msg.startswith('New version:'):
+                    other_commits = other_commits + msg + '\n'
+
+        return new_releases + other_commits
---
+++ packages/freitag.releaser/freitag.releaser-0.4.tar.gz/freitag.releaser-0.4/freitag/releaser/release.py
@@ -0,0 +1,297 @@
+# -*- coding: utf-8 -*-
+from freitag.releaser.utils import create_branch_locally
+from freitag.releaser.utils import is_everything_pushed
+from git import InvalidGitRepositoryError
+from git import Repo
+from git.exc import GitCommandError
+from plone.releaser.buildout import Buildout
+from plone.releaser.package import git_repo
+from zest.releaser import fullrelease
+from zest.releaser.utils import ask
+
+import os
+import re
+import sys
+
+
+DISTRIBUTION = '\033[1;91m{0}\033[0m'
+BRANCH = PATH = '\033[1;30m{0}\033[0m'
+
+
+class FullRelease(object):
+    """Releases all distributions that have changes and want to be released
+
+    Does lots of QA before and after any release actually happens as well as
+    another bunch of boring tasks worth automating.
+    """
+
+    #: system path where to look for distributions to be released
+    path = 'src'
+
+    #: distributions that will be released
+    distributions = []
+
+    #: plone.releaser.buildout.Buildout instance to get distribution's info
+    #: and save new versions
+    buildout = None
+
+    #: git branches that will be updated/checked, etc
+    branches = (
+        'master',
+        'develop',
+    )
+
+    #: changelog for each released distribution
+    changelogs = {}
+
+    #: version for each released distribution
+    versions = {}
+
+    def __init__(self, path='src'):
+        self.path = path
+        self.buildout = Buildout(
+            sources_file='develop.cfg',
+            checkouts_file='develop.cfg',
+        )
+
+    def __call__(self):
+        """Go through all distributions and release them if needed *and* wanted
+        """
+        self.get_all_distributions()
+        self.check_pending_local_changes()
+        self.check_changes_to_be_released()
+        self.ask_what_to_release()
+        self.release_all()
+        self.update_buildout()
+        self.update_batou()
+
+    def get_all_distributions(self):
+        """Get all distributions that are found in self.path"""
+        for folder in sorted(os.listdir(self.path)):
+            path = '{0}/{1}'.format(self.path, folder)
+            if not os.path.isdir(path):
+                continue
+
+            try:
+                Repo(path)
+            except InvalidGitRepositoryError:
+                continue
+
+            self.distributions.append(path)
+
+    def check_pending_local_changes(self):
+        """Check that the distributions do not have local changes"""
+        clean_distributions = []
+        for distribution_path in self.distributions:
+            repo = Repo(distribution_path)
+
+            dirty = False
+            local_changes = False
+
+            if repo.is_dirty():
+                dirty = True
+
+            if is_everything_pushed(repo, branches=self.branches):
+                local_changes = True
+
+            if dirty or local_changes:
+                print('{0} has non-committed/unpushed changes.')
+
+                msg = 'Do you want to continue? {0} will NOT be released '
+                if not ask(msg, default=True):
+                    exit(0)
+
+                continue
+
+            clean_distributions.append(distribution_path)
+
+        self.distributions = clean_distributions
+
+    def check_changes_to_be_released(self):
+        """Check which distributions have changes that could need a release"""
+        need_a_release = []
+        for distribution_path in self.distributions:
+            dist_name = distribution_path.split('/')[-1]
+            dist_clone = self.buildout.sources.get(dist_name)
+
+            with git_repo(dist_clone) as repo:
+                # get the latest tag
+                try:
+                    latest_tag = repo.git.describe('--abbrev=0', '--tags')
+                except GitCommandError:
+                    # if there is no tag it definitely needs a release
+                    need_a_release.append(distribution_path)
+                    continue
+
+                tag = repo.tags[latest_tag]
+                tag_sha = tag.commit.hexsha
+
+                for branch in self.branches:
+                    branch_sha = repo.refs[branch].commit.hexsha
+
+                    if tag_sha != branch_sha:
+                        # a branch is ahead of the last tag: needs a release
+                        need_a_release.append(distribution_path)
+                        break
+
+        self.distributions = need_a_release
+
+    def ask_what_to_release(self):
+        """Check that develop branch can be rebased on top of master branch"""
+        to_release = []
+        for distribution_path in self.distributions:
+            dist_name = distribution_path.split('/')[-1]
+            dist_clone = self.buildout.sources.get(dist_name)
+
+            with git_repo(dist_clone) as repo:
+                create_branch_locally(repo, 'develop')
+                repo.heads['develop'].checkout()
+                try:
+                    repo.git.rebase('master')
+                except GitCommandError:
+                    msg = '{0} Could not rebase develop on top of master.'
+                    print(msg.format(dist_name))
+                    if not ask('Would you like to continue?', default=False):
+                        exit(1)
+
+                git_changes = repo.git.log(
+                    '--oneline',
+                    '--graph',
+                    'origin/master~1..develop'
+                )
+
+                change_log_path = '{0}/CHANGES.rst'.format(
+                    repo.working_tree_dir
+                )
+                changes = self._grab_changelog(change_log_path)
+                self.changelogs[dist_name] = changes
+
+                print(git_changes)
+                print(changes)
+                if ask('Is the change log ready for release?'):
+                    to_release.append(distribution_path)
+
+        self.distributions = to_release
+
+    def release_all(self):
+        """Release all distributions"""
+        for distribution_path in self.distributions:
+            dist_name = distribution_path.split('/')[-1]
+            dist_clone = self.buildout.sources.get(dist_name)
+
+            with git_repo(dist_clone) as repo:
+                release = ReleaseDistribution(repo.working_tree_dir)
+                new_version = release()
+                self.versions[dist_name] = new_version
+
+                self.buildout.set_version(dist_name, new_version)
+
+    def update_buildout(self):
+        """Commit the changes on buildout"""
+        msg = ['New releases:', '', ]
+        changelogs = ['', 'Changelogs:', '', ]
+        for dist in sorted(self.versions.keys()):
+            tmp_msg = '{0} {1}'.format(
+                dist,
+                self.versions[dist]
+            )
+            msg.append(tmp_msg)
+
+            changelogs.append(dist)
+            changelogs.append('-' * len(dist))
+            changelogs.append(self.changelogs[dist])
+
+        commit_message = '\n'.join(msg + changelogs)
+
+        repo = Repo(os.path.curdir)
+        repo.git.add('versions.cfg')
+        repo.git.commit(message=commit_message)
+
+    def update_batou(self):
+        pass
+
+    def _grab_changelog(self, changelog_path):
+        release_regex = re.compile(r'^([0-9]{1,3}.){1,2}[0-9]{1,3} ')
+        lines = []
+        with open(changelog_path) as changelog:
+            release_counter = 0
+            for line in changelog:
+                if release_regex.search(line):
+                    release_counter += 1
+                    if release_counter == 2:
+                        break
+
+                if release_counter == 1:
+                    lines.append(line)
+        return lines
+
+
+class ReleaseDistribution(object):
+    """Release a single distribution with zest.releaser
+
+    It does some QA checks before/after the actual release happens.
+    """
+
+    #: system path where the distribution should be found
+    path = None
+    #: name of the distribution
+    name = None
+    #: git repository of the distribution
+    repo = None
+
+    #: parent repository which will be updated with the new release
+    parent_repo = None
+    #: plone.releaser.buildout.Buildout instance of the parent repository
+    buildout = None
+
+    def __init__(self, path):
+        self.path = path
+        self.name = path.split('/')[1]
+
+    def __call__(self):
+        self._check_parent_branch()
+        self._check_distribution_exists()
+        self._zest_releaser()
+
+        return self.get_version()
+
+    def _check_parent_branch(self):
+        self.parent_repo = Repo(os.path.curdir)
+        current_branch = self.parent_repo.active_branch.name
+
+        if current_branch != 'master':
+            text = '{0} is not on master branch, but on {1}'
+            raise ValueError(
+                text.format(
+                    DISTRIBUTION.format('zope repository'),
+                    BRANCH.format(current_branch)
+                )
+            )
+
+    def _check_distribution_exists(self):
+        """Check that the folder exists"""
+        if not os.path.exists(self.path):
+            raise IOError(
+                'Path {0} does NOT exist'.format(PATH.format(self.path))
+            )
+
+    def _zest_releaser(self):
+        """Release the distribution"""
+        # remove arguments so zest.releaser is not confused
+        # will most probably *not* be fixed by zest.releaser itself:
+        # https://github.com/zestsoftware/zest.releaser/issues/146
+        original_args = sys.argv
+        sys.argv = ['']
+
+        # change to the distribution root folder
+        original_path = os.getcwd()
+        os.chdir(self.path)
+
+        fullrelease.main()
+
+        os.chdir(original_path)
+        sys.argv = original_args
+
+    def get_version(self):
+        self.repo = Repo(self.path)
+        return self.repo.tags[-1].name
