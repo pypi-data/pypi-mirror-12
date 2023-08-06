5ba507170dcdc271eb0a0dcd54b3bffc91255da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:06 +0000

    Add package compliance-checker/2.0.0/compliance-checker-2.0.0.tar.gz

---
+++ packages/compliance-checker/compliance-checker-2.0.0.tar.gz/compliance-checker-2.0.0/setup.py
@@ -0,0 +1,62 @@
+from __future__ import with_statement
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+from compliance_checker import __version__
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+reqs = [line.strip() for line in open('requirements.txt')]
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+setup(name                 = "compliance-checker",
+    version              = __version__,
+    description          = "Checks Datasets and SOS endpoints for standards compliance",
+    long_description     = readme(),
+    license              = 'Apache License 2.0',
+    author               = "Dave Foster",
+    author_email         = "dave@axiomdatascience.com",
+    url                  = "https://github.com/ioos/compliance-checker",
+    packages             = find_packages(),
+    install_requires     = reqs,
+    tests_require        = ['pytest'],
+    cmdclass             = {'test': PyTest},
+    classifiers          = [
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering',
+        ],
+    include_package_data = True,
+    scripts=['cchecker.py'],
+    entry_points         = {
+        'console_scripts': [
+            'compliance-checker = cchecker:main'
+        ],
+        'compliance_checker.suites': [
+            'cf = compliance_checker.cf.cf:CFBaseCheck',
+            'acdd = compliance_checker.acdd:ACDDBaseCheck',
+            'ioos = compliance_checker.ioos:IOOSBaseCheck',
+        ]
+    },
+    package_data         = {
+        'compliance_checker':['data/*.json', 'data/*.xml', 'tests/data/*.nc', 'tests/data/non-comp/*.nc', 'data/templates/*.j2'],
+    }
+)
+
