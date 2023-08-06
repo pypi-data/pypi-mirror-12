9f755aa2d3162b01c24755d43f9f6019322ead6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:36 +0000

    Add package cbipy/0.1/cbipy-0.1.tar.gz

---
+++ packages/cbipy/cbipy-0.1.tar.gz/cbipy-0.1/setup.py
@@ -0,0 +1,15 @@
+# Minimal setup
+
+
+from setuptools import setup
+
+setup(name='cbipy',
+      version='0.1',
+      description='My little application',
+      url='http://github.com/kip-marks/cbipy',
+      author='Kip Marks',
+      author_email='kip.marks@gmail.com',
+      license='MIT',
+      packages=['cbipy'],
+      zip_safe=False)
+
