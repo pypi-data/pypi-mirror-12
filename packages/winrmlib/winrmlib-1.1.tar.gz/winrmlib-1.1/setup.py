82e139c48c7df694bf5515e96179e720bda7033b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:33 +0000

    Add package winrmlib/1.1/winrmlib-1.1.tar.gz

---
+++ packages/winrmlib/winrmlib-1.1.tar.gz/winrmlib-1.1/setup.py
@@ -0,0 +1,55 @@
+import os
+import versioneer
+from setuptools import setup
+
+project_name = 'winrmlib'
+
+# versioneer configuration
+versioneer.VCS = 'git'
+versioneer.versionfile_source = os.path.join('winrmlib', '_version.py')
+versioneer.versionfile_build = os.path.join('winrmlib', '_version.py')
+versioneer.tag_prefix = ''
+versioneer.parentdir_prefix = 'winrmlib'
+
+# PyPi supports only reStructuredText, so pandoc should be installed
+# before uploading package
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    long_description = ''
+
+setup(
+    name=project_name,
+    version=versioneer.get_version(),
+    description='Python library for Windows Remote Management',
+    long_description=long_description,
+    keywords='winrm winrw ws-man ws-management powershell'.split(' '),
+    author='Ian Clegg',
+    author_email='ian.clegg@sourcewarp.com',
+    url='https://github.com/ianclegg/winrmlib/',
+    license='MIT license',
+    packages=['winrmlib', 'winrmlib.api'],
+    package_data={'winrmlib.api': ['assets/xsd/*.xsd', 'assets/*.wsdl']},
+    cmdclass=versioneer.get_cmdclass(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: System :: Systems Administration',
+    ],
+)
