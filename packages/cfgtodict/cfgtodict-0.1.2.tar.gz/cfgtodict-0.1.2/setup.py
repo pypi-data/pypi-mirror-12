26dad935df7ba3f3185cf4edf0a82b6fe341d704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:13 +0000

    Add package cfgtodict/0.1.2/cfgtodict-0.1.2.tar.gz

---
+++ packages/cfgtodict/cfgtodict-0.1.2.tar.gz/cfgtodict-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+  name='cfgtodict',
+  version='0.1.2',
+  description='A simple class for converting a *.cfg file to a Python Dictionary',
+  url='https://github.com/joelklabo/cfgtodict',
+  author='Joel Klabo',
+  author_email='joelklabo@gmail.com',
+  license='MIT',
+  classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries"
+  ]
+)
