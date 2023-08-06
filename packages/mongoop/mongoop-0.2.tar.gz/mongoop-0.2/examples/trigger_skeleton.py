76c32d4940b757461359caa6126187e70d9dc889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:14 +0000

    Add package mongoop/0.2/mongoop-0.2.tar.gz

---
+++ packages/mongoop/mongoop-0.2.tar.gz/mongoop-0.2/examples/trigger_skeleton.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+    mongoop.triggers.skeleton
+    ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Is an example tigger to be used as a template.
+    See https://github.com/lujeni/mongoop/wiki/Write-your-own-triggers for more details.
+
+    :copyright: (c) 2015 by Lujeni.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import logging
+
+from mongoop.triggers import BaseTrigger
+
+logging.basicConfig(
+    level=logging.INFO, format='[%(asctime)s] [%(levelname)s] %(message)s')
+
+
+class MongoopTrigger(BaseTrigger):
+    """
+    The MongoopTrigger class name is mandatory.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        NO NEED TO IMPLEMENT in your trigger.
+
+        more details: mongoop.triggers.__init__.BaseTrigger.__call__
+        """
+        pass
+
+    def __call__(self, *args, **kwargs):
+        """
+        NO NEED TO IMPLEMENT in your trigger.
+
+        more details: mongoop.triggers.__init__.BaseTrigger.__call__
+        """
+        pass
+
+    def pre_run(self, *args, **kwargs):
+        """
+        OPTIONAL task.
+
+        Necessary to call the parent method (super).
+        Returns True to continue the process (run, post_run).
+
+        more details: mongoop.triggers.__init__.BaseTrigger.pre_run
+        """
+        try:
+            super(MongoopTrigger, self).run(*args, **kwargs)
+
+            print "pre_run task"
+        except Exception as e:
+            logging.error('unable to pre_run :: {} :: {}'.format(self.trigger_name, e))
+            return False
+        else:
+            return True
+
+    def run(self):
+        """ Main function, do what you want.
+            Returns True to continue the process (post_run).
+        """
+        try:
+            print "main task"
+        except Exception as e:
+            logging.error('unable to run :: {} :: {}'.format(self.trigger_name, e))
+            return False
+        else:
+            return True
+
+    def post_run(self, * args, **kwargs):
+        """
+        OPTIONAL task
+
+        Necessary to call the parent method (super)
+
+        more details: mongoop.triggers.__init__.BaseTrigger.post_run
+        """
+        try:
+            super(MongoopTrigger, self).run(*args, **kwargs)
+
+            print "i am a skeleton post_run"
+        except Exception as e:
+            logging.error('unable to post_run :: {} :: {}'.format(self.trigger_name, e))
+            return False
+        else:
+            return True
---
+++ packages/mongoop/mongoop-0.2.tar.gz/mongoop-0.2/mongoop/triggers/mongodb.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+    mongoop.triggers.mongodb
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: (c) 2015 by Lujeni.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import logging
+
+from pymongo.database import Database
+from pymongo import DESCENDING
+
+from mongoop.triggers import BaseTrigger
+
+
+logging.basicConfig(
+    level=logging.INFO, format='[%(asctime)s] [%(levelname)s] %(message)s')
+
+
+class MongoopTrigger(BaseTrigger):
+
+    def __init__(self, *args, **kwargs):
+        super(MongoopTrigger, self).__init__(*args, **kwargs)
+
+        database = self.params.get('database', 'mongoop')
+        collection = self.params.get('collection', 'history')
+
+        self.db = Database(self.mongoop.conn, database)
+        self.collection = self.db.get_collection(collection)
+        self.collection.create_index([('opid', DESCENDING)], unique=True, background=True)
+
+    def run(self):
+        try:
+            self.collection.insert_many(self.operations)
+        except Exception:
+            # TODO: logging
+            return False
+        else:
+            logging.info('run :: {} :: bulk insert {} operations'.format(self.trigger_name, len(self.operations)))
+            return True
