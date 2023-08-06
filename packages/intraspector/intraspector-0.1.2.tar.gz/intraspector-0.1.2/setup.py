a23286b7f4b236ff008f3543f48ed9eba46f5453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:57 +0000

    Add package intraspector/0.1.2/intraspector-0.1.2.tar.gz

---
+++ packages/intraspector/intraspector-0.1.2.tar.gz/intraspector-0.1.2/setup.py
@@ -0,0 +1,13 @@
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+setup(
+    name='intraspector',
+    version='0.1.2',
+    description='Intraspector decorator for python',
+    url='https://github.com/BespokeInsights/python-intraspector',
+    author='Aaron Hayes',
+    author_email='aaron.hayes92@gmail.com',
+    license='MIT',
+    packages=['intraspector']
+)
