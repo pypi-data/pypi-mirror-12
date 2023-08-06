8505cb818043725eec27b9033cfb4ae9f09d2746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:30 +0000

    Add package simpletransfers/1.0.6/simpletransfers-1.0.6.tar.gz

---
+++ packages/simpletransfers/simpletransfers-1.0.6.tar.gz/simpletransfers-1.0.6/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+	name = 'simpletransfers',
+	version = '1.0.6',
+	description = 'Simple file transfer library',
+	author = 'Jesters Ghost',
+	author_email = 'jestersghost@gmail.com',
+	url = 'https://bitbucket.org/jestersghost/simpletransfers',
+	requires = [ 'ctxlogger', 'Flask', 'paramiko', 'requests', 'suds' ],
+	package_dir = { 'simpletransfers': '.' },
+	packages = [
+		'simpletransfers',
+		'simpletransfers.tests',
+	],
+	package_data = { 'simpletransfers.tests': [ 'one.txt', 'test.zip', ] },
+)
