65cbe15f6a861af830e33a3dddf507c3353a159e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:58 +0000

    Add package subtitlematcher/0.0.1/subtitlematcher-0.0.1.tar.gz

---
+++ packages/subtitlematcher/subtitlematcher-0.0.1.tar.gz/subtitlematcher-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="subtitlematcher",
+    version="0.0.1",
+    description="Matching videos and subtitles file names",
+    url="http://github.com/itsjef/subtitlematcher.git",
+    author="Duc Anh Tran",
+    author_email="td.anh0812@gmail.com",
+    license='MIT',
+    scripts=["src/subtitlematcher"],
+    zip_safe=False
+)
