af61b5ab1376793418babc9429d0d11d757863f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:08 +0000

    Add package Geckopush/0.4/geckopush-0.4.tar.gz

---
+++ packages/Geckopush/geckopush-0.4.tar.gz/geckopush-0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='geckopush',
+      packages=['geckopush'],
+      version='0.4',
+      description="Easy to use python 3 library for pushing data your custom Geckoboard widgets",
+      author="Patrick Lee",
+      author_email="me@patricklee.nyc",
+      url="http://www.github.com/patleeman/geckopush",
+      download_url="http://www.github.com/patleeman/geckopush/tarball/0.1",
+      keywords = ['geckoboard', 'api', 'widget'],
+      classifiers=[]
+      )
