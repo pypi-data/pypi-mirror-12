05f375f849d21baeca775fbb995acf4baf8f9aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:41 +0000

    Add package netjsonconfig/0.3/netjsonconfig-0.3.tar.gz

---
+++ packages/netjsonconfig/netjsonconfig-0.3.tar.gz/netjsonconfig-0.3/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup, find_packages
+
+# avoid ImportError
+sys.path.insert(0, 'netjsonconfig')
+from version import get_version
+sys.path.remove('netjsonconfig')
+
+
+if sys.argv[-1] == 'setup.py':
+    print("To install, run 'python setup.py install'\n")
+
+
+if sys.argv[-1] == 'publish':
+    import os
+    os.system("python setup.py sdist bdist_wheel upload -s")
+    args = {'version': get_version()}
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %(version)s -m 'version %(version)s'" % args)
+    print("  git push --tags")
+    sys.exit()
+
+
+def get_install_requires():
+    """
+    parse requirements.txt, ignore links, exclude comments
+    """
+    requirements = []
+    for line in open('requirements.txt').readlines():
+        # skip to next iteration if comment or empty line
+        if line.startswith('#') or line == '' or line.startswith('http') or line.startswith('git'):
+            continue
+        # add line to requirements
+        requirements.append(line.replace('\n', ''))
+    # add py2-ipaddress if python2
+    if sys.version_info.major < 3:
+        requirements.append('py2-ipaddress')
+    return requirements
+
+
+setup(
+    name='netjsonconfig',
+    version=get_version(),
+    description="Netjsonconfig is a python library that converts NetJSON DeviceConfiguration objects into real router configurations that can be installed on systems like OpenWRT or OpenWisp Firmware.",
+    long_description=open('README.rst').read(),
+    author='Federico Capoano (nemesisdesign)',
+    author_email='f.capoano@cineca.it',
+    license='GPL3',
+    url='https://github.com/openwisp/netjsonconfig',
+    download_url='https://github.com/openwisp/netjsonconfig/releases',
+    packages=find_packages(exclude=['tests', 'tests.*', 'docs', 'docs.*']),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Networking',
+    ],
+    install_requires=get_install_requires(),
+    test_suite='nose.collector',
+    scripts=['bin/netjsonconfig'],
+)
