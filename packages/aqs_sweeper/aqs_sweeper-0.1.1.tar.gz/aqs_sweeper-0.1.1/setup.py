f5fa4f29623afd775b8e070e0b62ad8329090073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:38 +0000

    Add package aqs_sweeper/0.1.1/aqs_sweeper-0.1.1.tar.gz

---
+++ packages/aqs_sweeper/aqs_sweeper-0.1.1.tar.gz/aqs_sweeper-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.1'
+
+setup(
+    name = 'aqs_sweeper',
+    version = version,
+    description = "Extendable Azure Queue Storage dumper that copy your data into Azure Blob Storage.",
+    packages = find_packages( exclude = [ 'ez_setup'] ),
+    include_package_data = True,
+    zip_safe = False,
+    entry_points={
+        'console_scripts': [
+            'aqs-sweep = aqs_sweeper:main',
+        ],
+    },
+    author = 'Bence Faludi',
+    author_email = 'bence@ozmo.hu',
+    license = 'MIT',
+    install_requires = [
+        'azure-storage',
+        'progressbar2',
+    ],
+    test_suite = "asqsweeper.tests"
+)
