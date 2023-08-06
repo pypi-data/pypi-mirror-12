b2c6dedcde62149ae88dbe797a904a44bd73e0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:32 +0000

    Add package voevent-parse/0.8.3/voevent-parse-0.8.3.tar.gz

---
+++ packages/voevent-parse/voevent-parse-0.8.3.tar.gz/voevent-parse-0.8.3/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="voevent-parse",
+    version="0.8.3",
+    packages=['voeventparse', 'voeventparse.tests', 'voeventparse.tests.resources'],
+    package_data={'voeventparse':['tests/resources/*.xml']},
+    description="Convenience routines for parsing and manipulation of "
+                "VOEvent XML packets.",
+    author="Tim Staley",
+    author_email="timstaley337@gmail.com",
+    url="https://github.com/timstaley/voevent-parse",
+    install_requires=["lxml>=2.3, <4.0",
+                      'six'
+                      ],
+    test_suite='voeventparse.tests'
+)
