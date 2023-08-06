569e286642d234b2d4206ec1482b2802b78efffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:36 +0000

    Add package nestea/1.74.0/nestea-1.74.0.zip

---
+++ packages/nestea/nestea-1.74.0.zip/nestea-1.74.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nestea',
+    version = '1.74.0',
+    py_modules = ['pythonProjects'],
+    author = 'leonard mesiera',
+    author_email = 'leonardmesiera@outlook.com',
+    url = 'http://www.nonesofar.com',
+    description = 'A simple printer of nester list',
+    )
