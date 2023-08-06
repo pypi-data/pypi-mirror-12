85b27d6f59af15428d5d5d0d700b44c029caefa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:03 +0000

    Add package nesteryyang/1.0.0/nesteryyang-1.0.0.zip

---
+++ packages/nesteryyang/nesteryyang-1.0.0.zip/nesteryyang-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nesteryyang',
+        version = '1.0.0',
+        py_modules = ['nester'],
+        author = 'hfpython',
+        author_email = '',
+        url = 'http://',
+        description = '',
+    )
