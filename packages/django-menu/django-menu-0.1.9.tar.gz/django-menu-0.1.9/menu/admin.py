4af99c75bbc3a7c05eea0698866bafcaab451772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:54 +0000

    Add package django-menu/0.1.9/django-menu-0.1.9.tar.gz

---
+++ packages/django-menu/django-menu-0.1.9.tar.gz/django-menu-0.1.9/menu/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from menu.models import Menu, MenuItem
+
+class MenuItemInline(admin.TabularInline):
+    model = MenuItem
+    ordering = ('order',)
+
+class MenuAdmin(admin.ModelAdmin):
+    inlines = [MenuItemInline,]
+
+admin.site.register(Menu, MenuAdmin)
