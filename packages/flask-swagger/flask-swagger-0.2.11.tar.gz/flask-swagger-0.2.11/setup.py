4b8a9fb44c17a4061b1b37f4b7d3eac2eb675bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:39 +0000

    Add package flask-swagger/0.2.11/flask-swagger-0.2.11.tar.gz

---
+++ packages/flask-swagger/flask-swagger-0.2.11.tar.gz/flask-swagger-0.2.11/setup.py
@@ -0,0 +1,42 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(name='flask-swagger',
+      version='0.2.11',
+      url='https://github.com/gangverk/flask-swagger',
+      description='Extract swagger specs from your flask project',
+      author='Atli Thorbjornsson',
+      license='MIT',
+      py_modules=['flask_swagger', 'build_swagger_spec'],
+      long_description=long_description,
+      install_requires=['Flask>=0.10', 'PyYAML>=3.0'],
+      entry_points = """
+      [console_scripts]
+      flaskswagger = build_swagger_spec:run
+      """,
+      options={
+        'bdist_rpm':{
+          'build_requires':[
+            'python',
+            'python-setuptools',
+            'python-itsdangerous',
+            'python-flask',
+            'python-markupsafe',
+            'PyYAML',
+          ],
+          'requires':[
+            'python',
+            'python-setuptools',
+            'python-itsdangerous',
+            'python-flask',
+            'python-markupsafe',
+            'PyYAML',
+          ],
+        },
+      },
+      )
