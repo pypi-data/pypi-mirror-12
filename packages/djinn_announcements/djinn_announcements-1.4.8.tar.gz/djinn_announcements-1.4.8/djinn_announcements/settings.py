7b1df3af64c83a9608e13270f6d214d56d499ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:39 +0000

    Add package djinn_announcements/1.4.8/djinn_announcements-1.4.8.tar.gz

---
+++ packages/djinn_announcements/djinn_announcements-1.4.8.tar.gz/djinn_announcements-1.4.8/djinn_announcements/settings.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+ANNOUNCEMENT_STATUS = {0: _("Open"), 20: _("Closed"), 10: _("In progress")}
+
+ANNOUNCEMENT_STATUS_VOCAB = (("", "---"), ) + \
+    tuple(sorted(ANNOUNCEMENT_STATUS.items()[:2]))
+
+SERVICEANNOUNCEMENT_STATUS_VOCAB = (("", "---"), ) + \
+    tuple(sorted(ANNOUNCEMENT_STATUS.items()))
+
+ANNOUNCEMENT_PRIORITY = {0: _("Normal"), 1: _("High")}
+
+ANNOUNCEMENT_PRIORITY_VOCAB = sorted(ANNOUNCEMENT_PRIORITY.items())
+
+STATUS_CLASSES = {0: "important", 20: "success", 10: "warning"}
+
+SHOW_N_ANNOUNCEMENTS = None
+SHOW_N_SERVICEANNOUNCEMENTS = 5
+
+DEFAULT_DATETIME_INPUT_FORMAT = getattr(
+    settings, 'DEFAULT_DATETIME_INPUT_FORMAT', '%d/%m/%Y %H:%M')
+
+DEFAULT_DATE_INPUT_FORMAT = getattr(
+    settings, 'DEFAULT_DATE_INPUT_FORMAT', '%d/%m/%Y')
