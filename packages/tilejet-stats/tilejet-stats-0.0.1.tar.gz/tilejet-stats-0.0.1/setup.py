89a58114c5e534f3ae080a40fb8947194bfb7984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:49 +0000

    Add package tilejet-stats/0.0.1/tilejet-stats-0.0.1.tar.gz

---
+++ packages/tilejet-stats/tilejet-stats-0.0.1.tar.gz/tilejet-stats-0.0.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='tilejet-stats',
+    version='0.0.1',
+    install_requires=[],
+    author='TileJet Developers',
+    author_email='tilejet.dev@gmail.com',
+    license='MIT License',
+    url='https://github.com/tilejet/tilejet-stats/',
+    keywords='python gis tilejet',
+    description='A python utility library containing functions for generating statistics on tile service usage.',
+    long_description=open('README.md').read(),
+    download_url="https://github.com/tilejet/tilejet-stats/zipball/master",
+    packages=["tilejetstats"],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
