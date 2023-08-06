d5cbfad467b57eecb5c4f72d04fc05b2008a3009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:25 +0000

    Add package gem/v0.1.5/gem-v0.1.5.tar.gz

---
+++ packages/gem/gem-v0.1.5.tar.gz/gem-v0.1.5/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+setup(
+  name = 'gem',
+  packages = find_packages(where="gem"),
+  package_dir = {'':'gem'},
+  version = 'v0.1.5',
+  description = 'Math library for game programming in python. ',
+  author = 'Alex Marinescu',
+  author_email = 'ale632007@gmail.com',
+  license='BSD 2-Clause',
+  url = 'https://github.com/explosiveduck/pyGameMath',
+  download_url = 'https://github.com/explosiveduck/pyGameMath/releases',
+  install_requires=['six'],
+  keywords = ['math', 'game', 'library'],
+  classifiers = [
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: Unix',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Software Development :: Libraries',
+            'License :: OSI Approved :: BSD License',
+
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: Implementation',
+            ],
+)
