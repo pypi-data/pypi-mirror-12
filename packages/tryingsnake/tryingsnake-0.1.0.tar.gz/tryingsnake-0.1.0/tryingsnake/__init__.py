7fcae80dc744b75edf53bbc5d62d0cfd9445e87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:13 +0000

    Add package tryingsnake/0.1.0/tryingsnake-0.1.0.tar.gz

---
+++ packages/tryingsnake/tryingsnake-0.1.0.tar.gz/tryingsnake-0.1.0/tryingsnake/__init__.py
@@ -0,0 +1,271 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, division, absolute_import
+
+
+class _Try(object):
+    @staticmethod
+    def _identity_if_try_or_raise(v, msg="Invalid return type for f: {0}"):
+        if not isinstance(v, _Try):
+            raise TypeError(msg.format(type(v)))
+        return v
+
+    @staticmethod
+    def _raise_if_not_exception(e, msg="Invalid type for Failure: {0}"):
+        if not isinstance(e, Exception):
+            raise TypeError(msg.format(type(e)))
+        return True
+
+    @property
+    def isFailure(self):
+        """Check if this is a Failure.
+
+        >>> Success(1).isFailure
+        False
+        >>> Failure(Exception()).isFailure
+        True
+        """
+        return not self._isSuccess
+
+    @property
+    def isSuccess(self):
+        """Check if this is a Success.
+
+        >>> Success(1).isSuccess
+        True
+        >>> Failure(Exception()).isSuccess
+        False
+        """
+        return self._isSuccess
+
+    def get(self):
+        """If this is Success get wrapped value otherwise
+        throw stored exception
+
+        :return: stored value
+
+        >>> Success(1).get()
+        1
+        >>> Failure(Exception("e")).get()
+        Traceback (most recent call last):
+            ...
+        Exception: e
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    def getOrElse(self, default):
+        """If this is a Success get stored value otherwise
+        return default
+
+        :param default: value to return if this is a Failure
+        :return:
+
+        >>> Success(1).getOrElse(0)
+        1
+        >>> Failure(Exception("e")).getOrElse(0)
+        0
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    def orElse(self, default):
+        """If this is a Success return self otherwise
+        default
+
+        :param default:
+        :return:
+
+        >>> Success(1).orElse(Success(0))
+        Success(1)
+        >>> Failure(Exception("e")).orElse(Success(0))
+        Success(0)
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    def map(self, f):
+        """Apply function to the value.
+
+        :param f: function to be applied
+        :return: self if this is a Failure otherwise Try(f, self.get)
+
+        >>> inc = lambda x: x + 1
+        >>> def f(e): raise Exception("e")
+        >>> Success(1).map(inc)
+        Success(2)
+        >>> Failure(Exception("e")).map(inc)
+        Failure(Exception('e',))
+        >>> Success("1").map(f)
+        Failure(Exception('e',))
+        """
+        raise NotImplementedError   # pragma: no cover
+
+    def flatMap(self, f):
+        """ Apply potentially function  returning _Try to the value.
+
+        :param f: function to be applied.
+        :return: self if this is a Failure otherwise f applied to self.get
+
+        >>> from operator import add
+        >>> Success(1).flatMap(lambda x: Try(add, x, 1))
+        Success(2)
+        >>> Failure(Exception("e")).flatMap(lambda x: Try(add, x, 1))
+        Failure(Exception('e',))
+        >>> Success(1).flatMap(lambda x: Try(add, x, "0")) # doctest:+ELLIPSIS
+        Failure(TypeError(...))
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    def filter(self, f, exception_cls=Exception, msg=None):
+        """Convert this to Failure if f(self.get()) evaluates to False
+
+        :param f:
+        :param exception_cls: optional exception class to return
+        :param msg: optional message
+        :return: self if f evaluates to True otherwise Failure
+
+        >>> Success(1).filter(lambda x: x > 0)
+        Success(1)
+        >>> Success(1).filter(lambda x: x < 0, msg="Greater than zero")
+        Failure(Exception('Greater than zero',))
+        >>> Failure(Exception("e")).filter(lambda x: x)
+        Failure(Exception('e',))
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    def recover(self, f):
+        """If this is a Failure apply f to value otherwise
+
+        :param f: function to be applied
+        :return: Either Success of Failure
+
+        >>> def f(e): raise Exception("e")
+        >>> Success(1).recover(lambda e: 0)
+        Success(1)
+        >>> Failure(Exception("e")).recover(lambda e: 0)
+        Success(0)
+        >>> Failure(Exception("e")).recover(f)
+        Failure(Exception('e',))
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    def recoverWith(self, f):
+        """If this is a Failure apply f to self otherwise
+        return this
+
+        :param f: function to be applied
+        :return: Either Success of Failure
+
+        >>> Success(1).recoverWith(lambda t: Try(lambda: 0))
+        Success(1)
+        >>> Failure(Exception("e")).recoverWith(lambda t: Try(lambda: 0))
+        Success(0)
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    def failed(self):
+        """If this is a failure complete wrapped exception
+        otherwise throw TypeError
+
+        :return: None
+
+        >>> Success(1).failed()
+        Traceback (most recent call last):
+            ...
+        TypeError: Cannot fail Success
+        >>> Failure(Exception("e")).failed()
+        Traceback (most recent call last):
+            ...
+        Exception: e
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    def __repr__(self):
+        return self._fmt.format(repr(self._v))
+
+
+class Failure(_Try):
+    def __init__(self, e):
+        _Try._raise_if_not_exception(e)
+        self._v = e
+        self._isSuccess = False
+        self._fmt = "Failure({0})"
+
+    def get(self): raise self._v
+
+    def getOrElse(self, default):
+        return default
+
+    def orElse(self, default):
+        return _Try._identity_if_try_or_raise(default)
+
+    def map(self, f):
+        return self
+
+    def flatMap(self, f):
+        return self
+
+    def filter(self, f, exception_cls=Exception, msg=None):
+        return self
+
+    def recover(self, f):
+        return Try(f, self._v)
+
+    def recoverWith(self, f):
+        v = Try(f, self._v)
+        return _Try._identity_if_try_or_raise(v if v.isFailure else v.get())
+
+    def failed(self):
+        return self.get()
+
+
+class Success(_Try):
+    def __init__(self, v):
+        self._v = v
+        self._isSuccess = True
+        self._fmt = "Success({0})"
+
+    def get(self): return self._v
+
+    def getOrElse(self, default): return self.get()
+
+    def orElse(self, defualt): return self
+
+    def map(self, f):
+        return Try(f, self._v)
+
+    def flatMap(self, f):
+        v = Try(f, self._v)
+        return _Try._identity_if_try_or_raise(v if v.isFailure else v.get())
+
+    def filter(self, f, exception_cls=Exception, msg=None):
+        return (self if f(self.get())
+                else Failure(exception_cls(msg if msg else repr(f))))
+
+    def recover(self, f):
+        return self
+
+    def recoverWith(self, f):
+        return self
+
+    def failed(self):
+        raise TypeError("Cannot fail Success")
+
+
+def Try(f, *args, **kwargs):
+    """Calls f with provided arguments and wraps the result
+    using either Success or Failure.
+
+    :param f: Callable which should be evaluated
+    :param args: args which should be passed to f
+    :param kwargs: kwargs which should be passed to f
+    :return: Either success or Failure
+
+    >>> from operator import add, truediv
+    >>> Try(truediv, 1, 0) # doctest:+ELLIPSIS
+    Failure(ZeroDivisionError(...))
+    >>> Try(add, 1, 2)
+    Success(3)
+    """
+    try:
+        return Success(f(*args, **kwargs))
+    except Exception as e:
+        return Failure(e)
---
+++ packages/tryingsnake/tryingsnake-0.1.0.tar.gz/tryingsnake-0.1.0/tryingsnake/test/__init__.py
@@ -0,0 +1,120 @@
+import unittest
+from operator import add, truediv
+from tryingsnake import _Try, Try, Success, Failure
+
+
+class TryTestCase(unittest.TestCase):
+    def test_failure_should_be_failure(self):
+        self.assertTrue(Try(truediv, 1, 0).isFailure)
+        self.assertFalse(Try(truediv, 1, 0).isSuccess)
+
+    def test_success_should_be_success(self):
+        self.assertFalse(Try(add, 1, -1).isFailure)
+        self.assertTrue(Try(truediv, 1, -1).isSuccess)
+
+    def test_failure_should_raise_an_exception_if_created_from_non_exception(self):
+        self.assertRaises(TypeError, Failure, 1)
+
+    def test_get_should_raise_an_exception_if_failure(self):
+        self.assertRaises(ZeroDivisionError, Try(truediv, 1, 0).get)
+        self.assertRaises(TypeError, Try(add, "1", 0).get)
+
+    def test_get_should_return_value_if_success(self):
+        self.assertEqual(Try(truediv, 4, 2).get(), 2)
+        self.assertEqual(Try(add, "1", "2").get(), "12")
+
+    def test_repr(self):
+        def fail(): raise Exception("failure")
+        self.assertEqual(repr(Try(add, 1, 0)), "Success(1)")
+        self.assertEqual(repr(Try(fail)), "Failure(Exception('failure',))")
+
+    def test_flatmap_on_failure_should_return_failure(self):
+        self.assertTrue(Failure(Exception("")).flatMap(lambda x: Success(1)).isFailure)
+
+    def test_flatmap_on_success_should_return_value_depending_on_a_function(self):
+        self.assertTrue(Success(1).flatMap(lambda x: Success(1)).isSuccess)
+        self.assertTrue(Success(1).flatMap(lambda x: Failure(Exception())).isFailure)
+
+    def test_flatmap_should_fail_if_f_doesnt_return_try(self):
+        self.assertRaises(TypeError, Success(1).flatMap, lambda x: x)
+
+    def test_map_on_failure_should_return_failure(self):
+        self.assertTrue(Failure(Exception("")).map(lambda x: 1).isFailure)
+
+    def test_map_on_success_should_return_value_depending_on_a_function(self):
+        success = Success(1).map(lambda x: -x)
+        self.assertTrue(success.isSuccess)
+        self.assertEqual(success.get(), -1)
+
+    def test_filter_on_failure_should_return_failure(self):
+        self.assertTrue(Failure(Exception("")).filter(lambda x: True).isFailure)
+        self.assertTrue(Failure(Exception("")).filter(lambda x: False).isFailure)
+
+    def test_filter_on_success_should_return_value_depending_on_a_predicate(self):
+        self.assertTrue(Success(1).filter(lambda x: x > 0).isSuccess)
+        self.assertTrue(Success(-1).filter(lambda x: x > 0).isFailure)
+
+    def test_filter_should_accept_custom_exception_and_message(self):
+        class DummyException(Exception): pass
+        failure = Success(1).filter(lambda x: False, DummyException, "dummy")
+        self.assertRaises(DummyException, failure.get)
+        self.assertEqual(repr(failure), "Failure(DummyException('dummy',))")
+
+    def test_recover_on_failure_should_return_value_depending_on_f(self):
+        failure = Failure(Exception("e"))
+        self.assertTrue(failure.recover(lambda x: 1 / 0).isFailure)
+        self.assertTrue(failure.recover(lambda x: 1).isSuccess)
+
+    def test_recover_on_success_should_return_identity(self):
+        success = Success(1)
+        self.assertEqual(success.recover(lambda x: 1 / 0), success)
+
+    def test_recover_with_on_failure_should_return_value_depending_on_f(self):
+        failure = Failure(Exception("e"))
+        self.assertTrue(failure.recoverWith(lambda x: Try(lambda: 1 / 0)).isFailure)
+        self.assertTrue(failure.recoverWith(lambda x: Try(lambda: 1)).isSuccess)
+
+    def test_recover_with_on_success_should_return_identity(self):
+        success = Success(1)
+        self.assertEqual(success.recoverWith(lambda x: Try(lambda x: -1)), success)
+
+    def test_recover_with_should_throw_an_exception_f_doesnt_return_try(self):
+        self.assertRaises(TypeError, Failure(Exception("e")).recoverWith, lambda x: 1)
+
+    def test_get_or_else_with_success_should_return_this_value(self):
+        self.assertEqual(Success(1).getOrElse(lambda: -1), 1)
+
+    def test_get_or_else_with_failure_should_return_else(self):
+        self.assertEqual(Failure(Exception("e")).getOrElse(-1), -1)
+
+    def test_or_else_on_success_should_return_identity(self):
+        success = Success(1)
+        self.assertEqual(success.orElse(lambda: 1), success)
+
+    def test_or_else_on_failure_should_return_else(self):
+        success = Success(1)
+        self.assertEqual(Failure(Exception("e")).orElse(success), success)
+
+    def test_or_else_on_failure_should_throw_an_exception_default_is_not_try(self):
+        self.assertRaises(TypeError, Failure(Exception("e")).orElse, 1)
+
+    def test_failed_on_failure_should_throw_stored_exception(self):
+        self.assertRaises(Exception, Failure(Exception("e")).failed)
+
+    def test_failed_on_success_should_throw_type_error_exception(self):
+        self.assertRaises(TypeError, Success(1).failed)
+
+    def test__try_raise_if_not_exception(self):
+        self.assertRaises(TypeError, _Try._raise_if_not_exception, 1)
+        self.assertTrue(_Try._raise_if_not_exception(Exception("e")))
+
+    def test__try_identity_if_try_or_raise(self):
+        success = Success(1)
+        failure = Failure(Exception("e"))
+        self.assertRaises(TypeError, _Try._identity_if_try_or_raise, 1)
+        self.assertEqual(_Try._identity_if_try_or_raise(success), success)
+        self.assertEqual(_Try._identity_if_try_or_raise(failure), failure)
+
+
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
