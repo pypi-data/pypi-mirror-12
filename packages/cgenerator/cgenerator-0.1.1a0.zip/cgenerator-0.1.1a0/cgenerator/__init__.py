824a395eabecbde392a0531dcb9f57f73ab0bbed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:14 +0000

    Add package cgenerator/0.1.1a0/cgenerator-0.1.1a0.zip

---
+++ packages/cgenerator/cgenerator-0.1.1a0.zip/cgenerator-0.1.1a0/cgenerator/__init__.py
@@ -0,0 +1,12 @@
+
+
+import cgenerator
+
+__version__ = "0.1.1a"
+__project__ = "cgenerator"
+__author__ = "Alexey Voronin"
+__author_email__ = "alexeyvoronin777@gmail.com"
+__license__ = "GPL"
+
+def command_line():
+    cgenerator.main()
