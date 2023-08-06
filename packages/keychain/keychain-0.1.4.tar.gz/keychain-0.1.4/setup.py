9e99fb3ebdffa55487c69a49f73d2b8f515a334c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:46 +0000

    Add package keychain/0.1.4/keychain-0.1.4.tar.gz

---
+++ packages/keychain/keychain-0.1.4.tar.gz/keychain-0.1.4/setup.py
@@ -0,0 +1,33 @@
+"""
+keychain
+==============
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='keychain',
+    version='0.1.4',
+    url='https://github.com/blockstack/keychain-manager-py',
+    license='MIT',
+    author='Blockstack Developers',
+    author_email='hello@onename.com',
+    description="""Library for BIP32 hierarchical deterministic keychains / wallets.""",
+    keywords='bitcoin blockchain bip32 HD hierarchical deterministic keychain wallet',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'bitmerchant>=0.1.8',
+        'bitcoin>=1.1.39'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
