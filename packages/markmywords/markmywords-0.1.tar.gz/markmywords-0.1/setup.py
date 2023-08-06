e34c7c3237dcc375efe802fad5cd850fffbec7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:11 +0000

    Add package markmywords/0.1/markmywords-0.1.tar.gz

---
+++ packages/markmywords/markmywords-0.1.tar.gz/markmywords-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='markmywords',
+      version='0.1',
+      description='A text-generating Markov chain library',
+      url='http://github.com/useanalias/markmywords',
+      author='useanalias',
+      author_email='use.an.alias@gmail.com',
+      license='MIT',
+      packages=['markmywords'],
+      zip_safe=False)
