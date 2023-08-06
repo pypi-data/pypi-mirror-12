1a2f2be8512d694c7b940d396f62e062a24bc210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:29 +0000

    Add package Leo_nester/1.3.0/Leo_nester-1.3.0.zip

---
+++ packages/Leo_nester/Leo_nester-1.3.0.zip/Leo_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'Leo_nester',
+	version	= '1.3.0',
+	py_modules	= ['Leo_nester'],
+	author	= 'Leonard',
+	author_email	= 'None@qq.com',
+	url	= 'http://www.qq.com',
+	description	= 'A simple printer of nested lists',
+)
