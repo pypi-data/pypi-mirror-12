98bdb2e875d1e77b2d2b2e70ce78f09351d3ad10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:49 +0000

    Add package jsonobjects/1.0.3/jsonobjects-1.0.3.tar.gz

---
+++ packages/jsonobjects/jsonobjects-1.0.3.tar.gz/jsonobjects-1.0.3/jsonobjects/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .exceptions import GenericError, NotFound, ValidationError
+from .fields import (
+    Field, BooleanField, StringField, IntegerField, FloatField, DecimalField,
+    DateField, DateTimeField, RegexField, ListField, DictField
+)
+from .validators import (
+    MinValue, MaxValue, MinLength, MaxLength, RegexValidator, ChoiceValidator
+)
+from .schema import Schema
+from . import path
+from .path import Path
+from .utils import NULL, ISO_8601
+
+
+__version__ = '1.0.3'
+
+
+__all__ = ['GenericError', 'NotFound', 'ValidationError',
+           'path', 'Path', 'Schema', 'NULL', 'ISO_8601',
+           'Field', 'BooleanField', 'StringField', 'IntegerField',
+           'FloatField', 'DecimalField', 'DateField', 'DateTimeField',
+           'RegexField', 'ListField', 'DictField',
+           'MinValue', 'MaxValue', 'MinLength', 'MaxLength', 'RegexValidator',
+           'ChoiceValidator']
