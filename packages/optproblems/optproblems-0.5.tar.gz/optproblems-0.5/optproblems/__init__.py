4367ccf87efd9f025a1c0f52eb981501c98cc9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:23 +0000

    Add package optproblems/0.5/optproblems-0.5.tar.gz

---
+++ packages/optproblems/optproblems-0.5.tar.gz/optproblems-0.5/optproblems/__init__.py
@@ -0,0 +1,17 @@
+"""
+A collection of test problems for black-box optimization.
+
+"""
+
+from optproblems.base import *
+from optproblems.binary import *
+import optproblems.cec2005
+from optproblems.cec2007 import *
+from optproblems.dtlz import *
+from optproblems.mpm import *
+from optproblems.multiobjective import *
+from optproblems.real import *
+from optproblems.wfg import *
+from optproblems.zdt import *
+
+__version__ = "0.5"
