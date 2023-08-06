2c77fb5dfe4bafa3933d3956477b16bf706b915d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:44 +0000

    Add package ab_nester/1.3.0/ab_nester-1.3.0.zip

---
+++ packages/ab_nester/ab_nester-1.3.0.zip/ab_nester-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name       =   'ab_nester',
+    version    =   '1.3.0',
+    py_modules =   ['ab_nester'],
+    author     =   'akshay',
+    author_email=  'bakshay_22@yahoo.com',
+    description=   'A simple printer of nested lists',
+    )
