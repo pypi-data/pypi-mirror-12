609b0f3e92a246d79e6ea48de34dd3287541c968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:33 +0000

    Add package json-tricks/1.2/json_tricks-1.2.tar.gz

---
+++ packages/json-tricks/json_tricks-1.2.tar.gz/json_tricks-1.2/json_tricks/__init__.py
@@ -0,0 +1,14 @@
+
+try:
+	import numpy
+except ImportError:
+	NUMPY_MODE = False
+	from .nonp import dumps, dump, loads, load, strip_comments
+else:
+	NUMPY_MODE = True
+	from .np import dumps, dump, loads, load, strip_comments
+
+
+__all__ = ['dumps', 'dump', 'loads', 'load', 'strip_comments']
+
+
