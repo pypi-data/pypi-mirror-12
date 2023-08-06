3dca5a3c59e0fac29e75623ccfac2e206bec4c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:40 +0000

    Add package bravia-remote/1.1/bravia-remote-1.1.zip

---
+++ packages/bravia-remote/bravia-remote-1.1.zip/bravia-remote-1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='bravia-remote',
+    version='1.1',
+    packages=['bravia'],
+    package_dir={'': 'src'},
+    url='',
+    license='MIT',
+    author='Petro Zdeb',
+    author_email='peterzdeb@gmail.com',
+    description='Remote Control API client to Sony Bravia TV'
+)
