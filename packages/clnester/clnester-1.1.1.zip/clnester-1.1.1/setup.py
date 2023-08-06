01436303369cdddab81f2aeb0270c9039fd5c751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:23 +0000

    Add package clnester/1.1.1/clnester-1.1.1.zip

---
+++ packages/clnester/clnester-1.1.1.zip/clnester-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name            =       "clnester",
+        version         =       "1.1.1",
+        py_modules      =       ["clnester"],
+        author          =        "Chloe_Hwang",
+        author_email    =       "yuraree_90@hotmail.com",
+        url             =       "http://www.facebook.com/yuraree",
+        description     =       "A simple printer of nested lists and tab key application",
+
+        )
