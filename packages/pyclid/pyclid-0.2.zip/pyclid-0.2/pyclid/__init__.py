217be2c4d7483680d09ca0672aa1a617140c84f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:54 +0000

    Add package pyclid/0.2/pyclid-0.2.zip

---
+++ packages/pyclid/pyclid-0.2.zip/pyclid-0.2/pyclid/__init__.py
@@ -0,0 +1,127 @@
+import math
+
+
+class Vec2:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __abs__(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def __add__(self, other):
+        assert isinstance(other, Vec2), 'Requires a Vec2'
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        assert isinstance(other, Vec2), 'Requires a Vec2'
+        return Vec2(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        assert isinstance(other, (int, float, long)), 'Requires a int, float or long'
+        self.x *= other
+        self.y *= other
+        return self
+
+    def __div__(self, other):
+        assert isinstance(other, (int, float, long)), 'Requires a int, float or long'
+        self.x /= other
+        self.y /= other
+        return self
+
+    def magnitude(self):
+        return self.__abs__()
+
+    def normalize(self):
+        mag = self.magnitude()
+        if mag:
+            self.x /= mag
+            self.y /= mag
+        return self
+
+    def cross(self, other):
+        assert isinstance(other, Vec2), 'Requires a Vec2'
+        return self.x*other.y - self.y*other.x
+
+    def dot(self, other):
+        assert isinstance(other, Vec2), 'Requires a Vec2'
+        return self.x*other.x + self.y*other.y
+
+    def angle(self, other):
+        assert isinstance(other, Vec2), 'Requires a Vec2'
+        return math.acos(self.dot(other)/(self.magnitude() * other.magnitude()))
+
+    def mid_point(self, other):
+        assert isinstance(other, Vec2), 'Requires a Vec2'
+        return Vec2((self.x + other.x)/2.0, (self.y + other.y)/2.0)
+
+
+class Vec3:
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __abs__(self):
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+
+    def __add__(self, other):
+        assert isinstance(other, Vec3), 'Requires a Vec3'
+        return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        assert isinstance(other, Vec3), 'Requires a Vec3'
+        return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, other):
+        assert isinstance(other, (int, float, long)), 'Requires a int, float or long'
+        self.x *= other
+        self.y *= other
+        self.z *= other
+        return self
+
+    def __div__(self, other):
+        assert isinstance(other, (int, float, long)), 'Requires a int, float or long'
+        self.x /= other
+        self.y /= other
+        self.z /= other
+        return self
+
+    def magnitude(self):
+        return self.__abs__()
+
+    def normalize(self):
+        mag = self.magnitude()
+        if mag:
+            self.x /= mag
+            self.y /= mag
+            self.z /= mag
+        return self
+
+    def cross(self, other):
+        assert isinstance(other, Vec3), 'Requires a Vec3'
+        return Vec3((self.y*other.z - other.y*self.z),
+                    -(self.x*other.z - other.x*self.z),
+                    (self.x*other.y - other.x*self.y))
+
+    def dot(self, other):
+        assert isinstance(other, Vec3), 'Requires a Vec3'
+        return self.x*other.x + self.y*other.y + self.z*other.z
+
+    def angle(self, other):
+        assert isinstance(other, Vec3), 'Requires a Vec3'
+        return math.acos(self.dot(other)/(self.magnitude()*other.magnitude()))
+
+    def mid_point(self, other):
+        assert isinstance(other, Vec3), 'Requires a Vec3'
+        return Vec3((self.x + other.x)/2.0, (self.y + other.y)/2.0, (self.z + other.z)/2.0)
+
+    def triple_s(self, other, other2):
+        assert isinstance(other, Vec3), 'Requires all inputs to be Vec3'
+        assert isinstance(other2, Vec3), 'Requires all inputs to be Vec3'
+        return self.dot(other.cross(other2))
+
+    def triple_v(self, other, other2):
+        assert isinstance(other, Vec3), 'Requires all inputs to be Vec3'
+        assert isinstance(other2, Vec3), 'Requires all inputs to be Vec3'
+        return self.cross(other.cross(other2))
---
+++ packages/pyclid/pyclid-0.2.zip/pyclid-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='pyclid',
+    packages=['pyclid'],
+    version='0.2',
+    description='A simple vector mathematics library',
+    author='Gavin Jones',
+    author_email='gav.j.jones@googlemail.com',
+    url='https://github.com/Kaundur/pyclid',
+    download_url='https://github.com/Kaundur/pyclid/tarball/0.1',
+    keywords=['Vector', 'Euclid'],
+    classifiers=[],
+)
