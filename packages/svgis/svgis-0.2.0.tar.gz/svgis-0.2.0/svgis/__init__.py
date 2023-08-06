fa24b3c9cc0d091fa2e18ab9f72f2e3e08ec5df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:37 +0000

    Add package svgis/0.2.0/svgis-0.2.0.tar.gz

---
+++ packages/svgis/svgis-0.2.0.tar.gz/svgis-0.2.0/svgis/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of svgis.
+# https://github.com/fitnr/svgis
+
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2015, Neil Freeman <contact@fakeisthenewreal.org>
+
+__version__ = '0.2.0'  # NOQA
+
+__all__ = [
+    'convert',
+    'draw',
+    'projection',
+    'svg',
+    'svgis',
+]
+
+from . import convert
+from . import draw
+from . import projection
+from . import svg
+from .svgis import SVGIS
