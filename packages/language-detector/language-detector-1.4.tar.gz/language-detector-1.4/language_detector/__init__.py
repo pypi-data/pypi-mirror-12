ca3e37724e34fa9006880975fdbe6f6080ff9fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:51 +0000

    Add package language-detector/1.4/language-detector-1.4.tar.gz

---
+++ packages/language-detector/language-detector-1.4.tar.gz/language-detector-1.4/language_detector/__init__.py
@@ -0,0 +1,22 @@
+from os.path import dirname, realpath
+directory = dirname(realpath(__file__))
+
+language_chars = {}
+with open(directory + "/prep/language_chars.txt") as f:
+    for line in f:
+        if line:
+            items = line.strip().split("\t")
+            language = items[0]
+            if language:
+                language_chars[language] = [char.decode("utf-8") for char in items[1:] if char]
+
+def detect_language(text):
+    if isinstance(text, str):
+        text = text.decode("utf-8")
+    for char in text:
+        for i in range(3):
+            for language, chars in language_chars.iteritems():
+                char_to_compare = chars[i]
+                if char == chars[i]:
+                    return language
+dl = detect_language
