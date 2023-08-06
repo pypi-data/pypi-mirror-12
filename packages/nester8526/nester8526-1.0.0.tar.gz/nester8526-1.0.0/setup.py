462d7c755cd30d22cf05850b5310920b6319ce95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:28 +0000

    Add package nester8526/1.0.0/nester8526-1.0.0.tar.gz

---
+++ packages/nester8526/nester8526-1.0.0.tar.gz/nester8526-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+          name             = 'nester8526',
+          version          = '1.0.0',
+          py_modules       = ['nester8526'],
+          author           = 'konata8526',
+          author_email     = 'konata8526@gmail.com',
+          url              = 'http://www.konatah.com',
+          description      = 'A simple printer of nester lists',
+
+        )
