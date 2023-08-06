30105f171b872c09566b926f0437fc3a3a6afaa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:22 +0000

    Add package adapted-logger/1.0.2/adapted_logger-1.0.2-py2.py3-none-any.whl

---
+++ packages/adapted-logger/adapted_logger-1.0.2-py2.py3-none-any.whl/logger/custom_adapter.py
@@ -0,0 +1,45 @@
+#__author__ = 'ismailkaboubi'
+
+
+import logging
+#from logging.config import dictConfig
+#import yaml
+
+
+class CustomAdapter(logging.LoggerAdapter):
+    """
+    This  adapter expects the passed in dict-like object to have a
+    'ip' key, whose value in brackets is prepended to the log message.
+    """
+    def process(self, msg, kwargs):
+        return '%s    %s' % (self.extra['ip'], msg), kwargs
+
+
+
+### Exemple of usage #######################################################################
+#ip = "10.0.0.129"
+
+#logging_config = yaml.load(open('config/config.yml', 'r'))
+#dictConfig(logging_config)
+
+#logger_dashboard = logging.getLogger("retail_crm_server.dashboard_api")
+#logger_dashboard = CustomAdapter(logger_dashboard, {'ip': ip})
+
+#logger_core_api = logging.getLogger("retail_crm_server.core_api")
+#logger_core_api = CustomAdapter(logger_core_api, {'ip': ip})
+
+#for i in range(10):
+#    logger_core_api.info('This is a info Message', extra={'ip': "127.0.0.1"})
+#    logger_core_api.debug('This is a debug Message')
+#    logger_core_api.warning('This is a warning Message')
+#    logger_core_api.critical('This is a critical Message')
+#    logger_core_api.error('This is a error Message')
+#    logger_core_api.info('----------------------------------------------------------', extra={'ip': "127.0.0.1"})
+
+#    logger_dashboard.info('This is a info Message', extra={'ip': "127.0.0.1"})
+#    logger_dashboard.debug('This is a debug Message')
+#    logger_dashboard.warning('This is a warning Message')
+#    logger_dashboard.critical('This is a critical Message')
+#    logger_dashboard.error('This is a error Message')
+#    logger_dashboard.info('----------------------------------------------------------', extra={'ip': "127.0.0.1"})
+#############################################################################################
