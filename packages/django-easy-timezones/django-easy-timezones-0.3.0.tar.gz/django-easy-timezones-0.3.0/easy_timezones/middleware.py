dbc672118c057079ccc0cb2b724383261d0bc322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:40 +0000

    Add package django-easy-timezones/0.3.0/django-easy-timezones-0.3.0.tar.gz

---
+++ packages/django-easy-timezones/django-easy-timezones-0.3.0.tar.gz/django-easy-timezones-0.3.0/easy_timezones/middleware.py
@@ -0,0 +1,56 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+import pytz
+import pygeoip
+
+from .signals import detected_timezone
+from .utils import get_ip_address_from_request
+
+GEOIP_DATABASE = getattr(settings, 'GEOIP_DATABASE', None)
+
+if not GEOIP_DATABASE:
+    raise ImproperlyConfigured("GEOIP_DATABASE setting has not been defined.")
+
+
+db_loaded = False
+db = None
+
+def load_db():
+    global db
+    db = pygeoip.GeoIP(settings.GEOIP_DATABASE, pygeoip.MEMORY_CACHE)
+
+    global db_loaded
+    db_loaded = True
+
+
+def is_valid_ip(ip):
+    import re
+    valid_ipv4_pattern = r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+    valid_ipv6_pattern = r'^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$'
+    return bool(re.match(valid_ipv4_pattern, ip)) == True or bool(re.match(valid_ipv4_pattern, ip)) == True
+
+
+class EasyTimezoneMiddleware(object):
+    def process_request(self, request):
+        if not db_loaded:
+            load_db()
+
+        tz = request.session.get('django_timezone')
+
+        if not tz:
+            # use the default timezone (settings.TIME_ZONE) for localhost
+            tz = timezone.get_default_timezone()
+
+            client_ip = get_ip_address_from_request(request)
+            ip_addrs = client_ip.split(',')
+            for ip in ip_addrs:
+                if is_valid_ip(ip) and ip != '127.0.0.1':
+                    tz = db.time_zone_by_addr(ip)
+                    break
+
+        if tz:
+            timezone.activate(tz)
+            detected_timezone.send(sender=get_user_model(), instance=request.user, timezone=tz)
+        else:
+            timezone.deactivate()
