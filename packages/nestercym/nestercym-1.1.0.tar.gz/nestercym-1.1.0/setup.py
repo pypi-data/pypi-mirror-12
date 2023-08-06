a5cde01fffdaa026ef5bc23c666fbff45d9a092f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:15 +0000

    Add package nestercym/1.1.0/nestercym-1.1.0.tar.gz

---
+++ packages/nestercym/nestercym-1.1.0.tar.gz/nestercym-1.1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+   name = "nestercym",
+   version = "1.1.0",
+   py_modules = ['nestercym'],
+   author = 'cym',
+   author_email = 'hbzxcym@163.com'
+    )
