6ab708ce217152ad363c0088f2d95222ff6deed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:51 +0000

    Add package querylist/0.4.0/querylist-0.4.0-py2-none-any.whl

---
+++ packages/querylist/querylist-0.4.0-py2-none-any.whl/tests/betterdictlookup_tests.py
@@ -0,0 +1,20 @@
+from querylist.dict import BetterDictLookUp
+
+from tests.base import TestCase
+
+
+class BetterDictLookUpInstancesCan(TestCase):
+    def setUp(self):
+        self.src_dict = {
+            'foo': 1,
+            'bar': 2,
+            'yay': 'kittens'
+        }
+        self.bdlu = BetterDictLookUp(self.src_dict)
+
+    def test_do_dot_lookups_for_key_values(self):
+        self.assertEqual(self.bdlu.foo, self.src_dict['foo'])
+
+    def test_do_dot_assignments_for_key_values(self):
+        self.bdlu.yay = False
+        self.assertEqual(self.bdlu.yay, self.src_dict['yay'])
