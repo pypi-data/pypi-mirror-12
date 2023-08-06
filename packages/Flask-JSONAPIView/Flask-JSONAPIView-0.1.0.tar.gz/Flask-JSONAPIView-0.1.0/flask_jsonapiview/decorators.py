e787b6a3bbf8c0f0853b8a3711cc8a8272c5a57d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:03 +0000

    Add package Flask-JSONAPIView/0.1.0/Flask-JSONAPIView-0.1.0.tar.gz

---
+++ packages/Flask-JSONAPIView/Flask-JSONAPIView-0.1.0.tar.gz/Flask-JSONAPIView-0.1.0/flask_jsonapiview/decorators.py
@@ -0,0 +1,32 @@
+import functools
+
+from .filtering import ModelFilterField
+
+# -----------------------------------------------------------------------------
+
+
+def get_item_or_404(method=None, **decorator_kwargs):
+    # Allow using this as either a decorator or a decorator factory.
+    if method is None:
+        return functools.partial(get_item_or_404, **decorator_kwargs)
+
+    @functools.wraps(method)
+    def wrapped(self, *args, **kwargs):
+        item = self.get_item_or_404(
+            kwargs.pop(self.url_id_key), **decorator_kwargs
+        )
+        return method(self, item, *args, **kwargs)
+
+    return wrapped
+
+
+# -----------------------------------------------------------------------------
+
+
+def filter_function(field):
+    def wrapper(function):
+        filter_field = ModelFilterField(field, function)
+        functools.update_wrapper(filter_field, function)
+        return filter_field
+
+    return wrapper
