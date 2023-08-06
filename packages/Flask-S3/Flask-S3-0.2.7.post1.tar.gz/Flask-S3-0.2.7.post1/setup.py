6ca30124569d5387464a199eff2f70b74b0d6d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:39 +0000

    Add package Flask-S3/0.2.7.post1/Flask-S3-0.2.7.post1.tar.gz

---
+++ packages/Flask-S3/Flask-S3-0.2.7.post1.tar.gz/Flask-S3-0.2.7.post1/setup.py
@@ -0,0 +1,41 @@
+"""
+Flask-S3
+-------------
+
+Easily serve your static files from Amazon S3.
+"""
+from setuptools import setup
+from flask_s3 import __version__
+
+
+
+setup(
+    name='Flask-S3',
+    version=".".join(map(str, __version__)),
+    url='http://github.com/e-dard/flask-s3',
+    license='WTFPL',
+    author='Edward Robinson',
+    author_email='hi@edd.io',
+    description='Seamlessly serve the static files of your Flask app from Amazon S3',
+    long_description=__doc__,
+    py_modules=['flask_s3'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Boto3>=1.1.1',
+        'six'
+    ],
+    tests_require=['nose', 'mock'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite = 'nose.collector'
+)
