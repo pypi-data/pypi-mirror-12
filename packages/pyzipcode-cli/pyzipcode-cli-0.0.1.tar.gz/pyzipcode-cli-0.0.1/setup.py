99a878f84182a639fe1ddf3890a742fbe7b85c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:28 +0000

    Add package pyzipcode-cli/0.0.1/pyzipcode-cli-0.0.1.tar.gz

---
+++ packages/pyzipcode-cli/pyzipcode-cli-0.0.1.tar.gz/pyzipcode-cli-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyzipcode-cli',
+  packages = ['pyzipcode-cli'], 
+  version = '0.0.1',
+  description = "pyzipcode-cli is a thin wrapper around getziptastic's API v2",
+  author = 'Tasdik Rahman',
+  author_email = 'tasdik95@gmail.com',
+  url = 'https://github.com/prodicus/pyzipcode-cli', 
+  download_url = 'https://github.com/prodicus/pyzipcode-cli/tarball/0.0.3', 
+  keywords = ['api', 'geo-location', 'zipcode'], 
+  classifiers = [],
+)
