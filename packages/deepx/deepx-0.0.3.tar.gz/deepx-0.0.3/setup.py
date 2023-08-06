685321e23e02fa7d20c76e1a8f1592c60f28634e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:12 +0000

    Add package deepx/0.0.3/deepx-0.0.3.tar.gz

---
+++ packages/deepx/deepx-0.0.3.tar.gz/deepx-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "deepx",
+    version = "0.0.3",
+    author = "Sharad Vikram and Zachary Chase Lipton",
+    author_email = "sharad.vikram@gmail.com",
+    description = "A basic deep learning library for sequence learning built using Theano.",
+    license = "MIT",
+    keywords = "theano",
+    install_requires = ['theanify'],
+    packages=find_packages(
+        '.'
+    ),
+    classifiers=[
+    ],
+)
