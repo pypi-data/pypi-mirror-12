27145d04e428db5f39861a6a3ab04b39a368c8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:29 +0000

    Add package toolkit/0.0.3/toolkit-0.0.3-py2-none-any.whl

---
+++ packages/toolkit/toolkit-0.0.3-py2-none-any.whl/toolkit/__init__.py
@@ -0,0 +1,26 @@
+# coding=utf8
+
+import sys
+import logging
+
+logger = logging.getLogger(__name__)
+
+PY2 = sys.version_info[0] == 2
+
+if not PY2:
+    iteritems = lambda d: d.items()
+    text_type = str
+    binary_type = bytes
+    long_type = float
+    basestring = (str, bytes)
+    string_types = (str, )
+else:
+    iteritems = lambda d: d.iteritems()
+    text_type = unicode  # noqa
+    binary_type = str
+    long_type = long  # noqa
+    string_types = (str, unicode)
+
+
+class ToolkitException(Exception):
+    pass
