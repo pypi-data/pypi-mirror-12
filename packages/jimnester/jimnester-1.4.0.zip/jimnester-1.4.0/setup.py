9bc8f810f1ba9dae7ea904c73e90cd8de6563aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:51 +0000

    Add package jimnester/1.4.0/jimnester-1.4.0.zip

---
+++ packages/jimnester/jimnester-1.4.0.zip/jimnester-1.4.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'jimnester', 
+        version      = '1.4.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['jimnester'],
+        author       = 'jimburchel',
+        author_email = 'jburchel@gmail.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+     )
