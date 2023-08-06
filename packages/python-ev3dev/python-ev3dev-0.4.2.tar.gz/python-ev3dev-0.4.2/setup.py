ee7923774f998bc68d84e9f222bd6415b1b08632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:56 +0000

    Add package python-ev3dev/0.4.2/python-ev3dev-0.4.2.tar.gz

---
+++ packages/python-ev3dev/python-ev3dev-0.4.2.tar.gz/python-ev3dev-0.4.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from git_version import git_version
+
+
+setup(
+    name='python-ev3dev',
+    version=git_version(),
+    description='Python language bindings for ev3dev',
+    author='Ralph Hempel',
+    author_email='rhempel@hempeldesigngroup.com',
+    license='MIT',
+    url='https://github.com/rhempel/ev3dev-lang-python',
+    include_package_data=True,
+    py_modules=['ev3dev'],
+    install_requires=['pil']
+    )
+
