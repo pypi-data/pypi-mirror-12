3c7446a4985523872c747379c10af3ee32a26795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:49 +0000

    Add package jaraco.functools/1.7/jaraco.functools-1.7.tar.gz

---
+++ packages/jaraco.functools/jaraco.functools-1.7.tar.gz/jaraco.functools-1.7/CHANGES.rst
@@ -0,0 +1,43 @@
+1.7
+===
+
+Moved hosting to github.
+
+1.6
+===
+
+``method_cache`` now accepts a cache_wrapper parameter, allowing
+for custom parameters to an ``lru_cache`` or an entirely different
+cache implementation.
+
+Use ``backports.functools_lru_cache`` to provide ``lru_cache`` for
+Python 2.
+
+1.5
+===
+
+Implement ``Throttler`` as a descriptor so it may be used to decorate
+methods. Introduces ``first_invoke`` function.
+
+Fixed failure in Throttler on Python 2 due to improper use of integer
+division.
+
+1.4
+===
+
+Added ``Throttler`` class from `irc <https://bitbucket.org/jaraco/irc>`_.
+
+1.3
+===
+
+Added ``call_aside`` decorator.
+
+1.2
+===
+
+Added ``apply`` decorator.
+
+1.0
+===
+
+Initial release drawn from jaraco.util.
---
+++ packages/jaraco.functools/jaraco.functools-1.7.tar.gz/jaraco.functools-1.7/test_functools.py
@@ -0,0 +1,71 @@
+import itertools
+import time
+import copy
+
+from jaraco.functools import Throttler, method_cache
+
+
+class TestThrottler(object):
+	def test_function_throttled(self):
+		"""
+		Ensure the throttler actually throttles calls.
+		"""
+		# set up a function to be called
+		counter = itertools.count()
+		# set up a version of `next` that is only called 30 times per second
+		limited_next = Throttler(next, 30)
+		# for one second, call next as fast as possible
+		deadline = time.time() + 1
+		while time.time() < deadline:
+			limited_next(counter)
+		# ensure the counter was advanced about 30 times
+		assert 28 <= next(counter) <= 32
+
+		# ensure that another burst of calls after some idle period will also
+		# get throttled
+		time.sleep(1)
+		deadline = time.time() + 1
+		counter = itertools.count()
+		while time.time() < deadline:
+			limited_next(counter)
+		assert 28 <= next(counter) <= 32
+
+	def test_reconstruct_unwraps(self):
+		"""
+		The throttler should be re-usable - if one wants to throttle a
+		function that's aready throttled, the original function should be
+		used.
+		"""
+		wrapped = Throttler(next, 30)
+		wrapped_again = Throttler(wrapped, 60)
+		assert wrapped_again.func is next
+		assert wrapped_again.max_rate == 60
+
+	def test_throttled_method(self):
+		class ThrottledMethodClass(object):
+			@Throttler
+			def echo(self, arg):
+				return arg
+
+		tmc = ThrottledMethodClass()
+		assert tmc.echo('foo') == 'foo'
+
+
+class TestMethodCache:
+	def test_deepcopy(self):
+		"""
+		A deepcopy of an object with a method cache should still
+		succeed.
+		"""
+		class ClassUnderTest:
+			calls = 0
+
+			@method_cache
+			def method(self, value):
+				self.calls += 1
+				return value
+
+		ob = ClassUnderTest()
+		copy.deepcopy(ob)
+		ob.method(1)
+		copy.deepcopy(ob)
