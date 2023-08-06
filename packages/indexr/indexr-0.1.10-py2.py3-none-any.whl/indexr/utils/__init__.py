a7e37d1a5c205483b9f39c685de50ce2f95c7b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:04 +0000

    Add package indexr/0.1.10/indexr-0.1.10-py2.py3-none-any.whl

---
+++ packages/indexr/indexr-0.1.10-py2.py3-none-any.whl/indexr/utils/__init__.py
@@ -0,0 +1,27 @@
+def tokenize(text):
+    """
+    Convert a text to tokens.
+
+    Example:
+        >>> print(tokenize('Hello world!'))
+        ['Hello', 'world', '!']
+
+    :param    text: Text to tokenize.
+    :type     text: str
+    :return:        List of tokens.
+    :rtype:         list
+    """
+
+    # Find delimiters and make sure that these are isolated tokens
+    delimiters = ['.', ',', ':', ';', '?', '!', '<', '>', '[', ']', '(', ')', '"']
+    for delimiter in delimiters:
+        text = text.replace(delimiter, ' ' + delimiter + ' ')
+
+    # Now find all the tokens
+    tokens = []
+    for token in text.split(' '):
+        if len(token) > 0:
+            tokens.append(token)
+
+    # And give back the tokens
+    return tokens
