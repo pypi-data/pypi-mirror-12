7bdefd278fd8eae64f482b4887d73995f40eb0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:21 +0000

    Add package stockfighter-io/0.1.4/stockfighter-io-0.1.4.tar.gz

---
+++ packages/stockfighter-io/stockfighter-io-0.1.4.tar.gz/stockfighter-io-0.1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md')) as f:
+  long_description = f.read()
+
+setup(
+  name = 'stockfighter-io',
+  packages = find_packages(exclude=['dist']),
+  version = '0.1.4',
+  description = 'An API wrapper for Stockfighter.io',
+  author = 'Bobby Larson',
+  author_email = 'karma0@gmail.com',
+  url = 'https://github.com/karma0/stockfighter',
+  download_url = 'https://github.com/karma0/stockfighter/tarball/0.1',
+  keywords = ['stockfighter', 'exchange', 'trades'],
+  classifiers = [],
+  install_requires = [
+    'requests',
+    'ws4py'
+  ],
+)
