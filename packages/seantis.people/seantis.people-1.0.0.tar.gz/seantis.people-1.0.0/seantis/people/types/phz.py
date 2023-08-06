0efaff72f9660ab4d6494dc76aa924d69246c562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:56 +0000

    Add package seantis.people/1.0.0/seantis.people-1.0.0.tar.gz

---
+++ packages/seantis.people/seantis.people-1.0.0.tar.gz/seantis.people-1.0.0/seantis/people/types/phz.py
@@ -0,0 +1,16 @@
+from plone.directives import form
+from seantis.people.types.base import PersonBase
+
+
+class IPHZ(form.Schema):
+    form.model("phz.xml")
+
+
+class PHZ(PersonBase):
+
+    @property
+    def org_units(self):
+        return [
+            org for org in (self.org_unit_1, self.org_unit_2, self.org_unit_3)
+            if org
+        ]
