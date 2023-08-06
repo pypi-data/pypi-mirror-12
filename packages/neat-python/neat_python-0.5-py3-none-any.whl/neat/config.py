9706d2ed8e52196be2acf90511ab232249f02955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:31 +0000

    Add package neat-python/0.5/neat_python-0.5-py3-none-any.whl

---
+++ packages/neat-python/neat_python-0.5-py3-none-any.whl/neat/config.py
@@ -0,0 +1,73 @@
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import SafeConfigParser as ConfigParser
+import os
+
+
+class Config(object):
+    '''
+    A simple container for all of the user-configurable parameters of NEAT.
+    '''
+
+    # TODO: Split out the configuration into implementation-specific sections. For example,
+    # a node gene class FooNode would expect to find a [FooNode] section within the configuration
+    # file, and the NEAT framework doesn't need to know about this section in any way. This
+    # allows all the configuration to stay in one text file, without unnecessary complication.
+    # It also makes the config file and associated setup code somewhat self-documenting, as the
+    # classes you need to give to NEAT are shown in the config file.
+
+    def __init__(self, filename):
+        if not os.path.isfile(filename):
+            raise Exception('No such config file: ' + os.path.abspath(filename))
+
+        with open(filename) as f:
+            parameters = ConfigParser()
+            if hasattr(parameters, 'read_file'):
+                parameters.read_file(f)
+            else:
+                parameters.readfp(f)
+
+        # Phenotype configuration
+        self.input_nodes = int(parameters.get('phenotype', 'input_nodes'))
+        self.output_nodes = int(parameters.get('phenotype', 'output_nodes'))
+        self.hidden_nodes = int(parameters.get('phenotype', 'hidden_nodes'))
+        self.fully_connected = bool(int(parameters.get('phenotype', 'fully_connected')))
+        self.max_weight = float(parameters.get('phenotype', 'max_weight'))
+        self.min_weight = float(parameters.get('phenotype', 'min_weight'))
+        self.feedforward = bool(int(parameters.get('phenotype', 'feedforward')))
+        self.nn_activation = parameters.get('phenotype', 'nn_activation')  # exp or tanh
+        self.weight_stdev = float(parameters.get('phenotype', 'weight_stdev'))
+
+        # Genetic algorithm configuration
+        self.pop_size = int(parameters.get('genetic', 'pop_size'))
+        self.max_fitness_threshold = float(parameters.get('genetic', 'max_fitness_threshold'))
+        self.prob_add_conn = float(parameters.get('genetic', 'prob_add_conn'))
+        self.prob_add_node = float(parameters.get('genetic', 'prob_add_node'))
+        self.prob_delete_conn = float(parameters.get('genetic', 'prob_delete_conn'))
+        self.prob_delete_node = float(parameters.get('genetic', 'prob_delete_node'))
+        self.prob_mutate_bias = float(parameters.get('genetic', 'prob_mutate_bias'))
+        self.bias_mutation_power = float(parameters.get('genetic', 'bias_mutation_power'))
+        self.prob_mutate_response = float(parameters.get('genetic', 'prob_mutate_response'))
+        self.response_mutation_power = float(parameters.get('genetic', 'response_mutation_power'))
+        self.prob_mutate_weight = float(parameters.get('genetic', 'prob_mutate_weight'))
+        self.prob_replace_weight = float(parameters.get('genetic', 'prob_replace_weight'))
+        self.weight_mutation_power = float(parameters.get('genetic', 'weight_mutation_power'))
+        self.prob_toggle_link = float(parameters.get('genetic', 'prob_toggle_link'))
+        self.elitism = float(parameters.get('genetic', 'elitism'))
+
+        # genotype compatibility
+        self.compatibility_threshold = float(parameters.get('genotype compatibility', 'compatibility_threshold'))
+        self.compatibility_change = float(parameters.get('genotype compatibility', 'compatibility_change'))
+        self.excess_coefficient = float(parameters.get('genotype compatibility', 'excess_coefficient'))
+        self.disjoint_coefficient = float(parameters.get('genotype compatibility', 'disjoint_coefficient'))
+        self.weight_coefficient = float(parameters.get('genotype compatibility', 'weight_coefficient'))
+
+        # species
+        self.species_size = int(parameters.get('species', 'species_size'))
+        self.survival_threshold = float(parameters.get('species', 'survival_threshold'))
+        self.old_threshold = int(parameters.get('species', 'old_threshold'))
+        self.youth_threshold = int(parameters.get('species', 'youth_threshold'))
+        self.old_penalty = float(parameters.get('species', 'old_penalty'))
+        self.youth_boost = float(parameters.get('species', 'youth_boost'))
+        self.max_stagnation = int(parameters.get('species', 'max_stagnation'))
---
+++ packages/neat-python/neat_python-0.5-py3-none-any.whl/neat/species.py
@@ -0,0 +1,110 @@
+# -*- coding: UTF-8 -*-
+import random
+from neat.indexer import Indexer
+from neat.math_util import mean
+
+
+class Species(object):
+    """ A collection of genetically similar individuals."""
+    indexer = Indexer(1)
+
+    def __init__(self, first_individual, previous_id=None):
+        self.representative = first_individual
+        self.ID = Species.indexer.next(previous_id)
+        self.age = 0
+        self.members = []
+        self.add(first_individual)
+        self.spawn_amount = 0
+        self.last_avg_fitness = 0
+        self.no_improvement_age = 0
+
+    def add(self, individual):
+        individual.species_id = self.ID
+        self.members.append(individual)
+        # choose a new random representative for the species
+        self.representative = random.choice(self.members)
+
+    def __str__(self):
+        s = "\n   Species %2d   size: %3d   age: %3d   spawn: %3d   " \
+            % (self.ID, len(self.members), self.age, self.spawn_amount)
+        s += "\n   No improvement: %3d \t avg. fitness: %1.8f" \
+             % (self.no_improvement_age, self.last_avg_fitness)
+        return s
+
+    def tournament_selection(self, k=2):
+        """ Tournament selection with size k (default k=2).
+            Make sure the population has at least k individuals """
+        # TODO: Unless some side-effect of this shuffle is necessary, it is inefficient given
+        # that we only want to select two members without replacement.
+        random.shuffle(self.members)
+
+        return max(self.members[:k])
+
+    def average_fitness(self):
+        """ Returns the raw average fitness for this species """
+        avg_fitness = mean([c.fitness for c in self.members])
+
+        # Check for increase in mean fitness and adjust "no improvement" count as necessary.
+        if avg_fitness > self.last_avg_fitness:
+            self.last_avg_fitness = avg_fitness
+            self.no_improvement_age = 0
+        else:
+            self.no_improvement_age += 1
+
+        return avg_fitness
+
+    def reproduce(self, config):
+        """ Returns a list of 'spawn_amount' new individuals """
+
+        offspring = []  # new offspring for this species
+        self.age += 1  # increment species age
+
+        # print "Reproducing species %d with %d members" %(self.id, len(self.members))
+
+        # this condition is useless since no species with spawn_amount < 0 will
+        # reach this point - at least it shouldn't happen.
+        assert self.spawn_amount > 0, "Species %d with non-positive spawn amount!" % self.ID
+
+        self.members.sort()  # sort species's members by their fitness
+        self.members.reverse()  # best members first
+
+        if config.elitism:
+            # TODO: Wouldn't it be better if we set elitism=2,3,4...
+            # depending on the size of each species?
+            offspring.append(self.members[0])
+            self.spawn_amount -= 1
+
+        # Keep a fraction of the current population for reproduction.
+        survivors = int(round(len(self.members) * config.survival_threshold))
+        # We always need at least one member for reproduction.
+        survivors = max(1, survivors)
+        self.members = self.members[:survivors]
+
+        while self.spawn_amount > 0:
+
+            self.spawn_amount -= 1
+
+            if len(self.members) > 1:
+                # Selects two parents from the remaining species and produces a single individual
+                # Stanley selects at random, here we use tournament selection (although it is not
+                # clear if has any advantages)
+                parent1 = self.tournament_selection()
+                parent2 = self.tournament_selection()
+
+                assert parent1.species_id == parent2.species_id, "Parents has different species id."
+                child = parent1.crossover(parent2)
+                offspring.append(child.mutate())
+            else:
+                # mutate only
+                parent1 = self.members[0]
+                # TODO: temporary hack - the child needs a new id (not the parent's)
+                child = parent1.crossover(parent1)
+                offspring.append(child.mutate())
+
+        # reset species (new members will be added again when speciating)
+        self.members = []
+
+        # select a new random representative member
+        self.representative = random.choice(offspring)
+
+        return offspring
