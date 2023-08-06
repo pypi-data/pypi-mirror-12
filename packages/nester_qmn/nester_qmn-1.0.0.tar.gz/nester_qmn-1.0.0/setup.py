29498a9d8b5d186ddfeb4cc4416a988fb1cdfdd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:35 +0000

    Add package nester_qmn/1.0.0/nester_qmn-1.0.0.tar.gz

---
+++ packages/nester_qmn/nester_qmn-1.0.0.tar.gz/nester_qmn-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name		='nester_qmn',
+	version		='1.0.0',
+	py_modules 	=['nester_qmn'],
+	author		='sugar',
+	author_email	='qmn1180@163.com',
+	url		='http://www.headfristlabs.com',
+	description	='A simple printer of nested lists',
+     )
