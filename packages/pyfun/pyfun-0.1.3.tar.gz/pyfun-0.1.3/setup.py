8b6b1f4b56555a4f6ba82b4c7b0890e9d5a9e6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:14 +0000

    Add package pyfun/0.1.3/pyfun-0.1.3.tar.gz

---
+++ packages/pyfun/pyfun-0.1.3.tar.gz/pyfun-0.1.3/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(name='pyfun',
+      version='0.1.3',
+      description='The pyfun joke in the world',
+      url='http://github.com/storborg/pyfun',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['pyfun'],
+      install_requires=[
+          'markdown',
+          ],
+      zip_safe=False)
