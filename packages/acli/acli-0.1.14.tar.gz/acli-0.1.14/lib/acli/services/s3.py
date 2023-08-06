b4d4061d567fea857b2c9c1898eae278709ba25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:24 +0000

    Add package acli/0.1.14/acli-0.1.14.tar.gz

---
+++ packages/acli/acli-0.1.14.tar.gz/acli-0.1.14/lib/acli/services/s3.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, print_function, unicode_literals)
+from acli.output.s3 import (output_s3_list, output_s3_info)
+import botocore.exceptions
+from acli.connections import get_client
+
+
+def check_bucket_accessible(s3_client=None, bucket_name=None):
+    try:
+        s3_client.head_bucket(Bucket=bucket_name)
+        return True
+    except botocore.exceptions.ClientError:
+        exit("Unable to access bucket.")
+    except Exception as unhandled:
+        exit('Unhandled exception: {0}'.format(unhandled))
+
+
+def s3_list(aws_config=None, item=None):
+    """
+    @type aws_config: Config
+    """
+    s3_client = get_client(client_type='s3', config=aws_config)
+    buckets = s3_client.list_buckets()
+    if not item:
+        if buckets.get('Buckets'):
+            output_s3_list(buckets=buckets.get('Buckets'))
+        else:
+            exit("No buckets found.")
+    else:
+        prefix = ''
+        if item and '/' in item:
+            path_elements = item.split('/')
+            bucket_name = path_elements[0]
+            prefix = "/".join(path_elements[1:])
+        else:
+            bucket_name = item
+        check_bucket_accessible(s3_client=s3_client, bucket_name=bucket_name)
+        try:
+            objects = s3_client.list_objects(Bucket=bucket_name, Prefix=prefix, Delimiter='/')
+            if not any((objects.get('CommonPrefixes'),
+                        (objects.get('Contents')))):
+                exit('Nothing found in: {0}'.format(item[:-1]))
+            common_prefixes = objects.get('CommonPrefixes', list())
+            folders = list()
+            for first_bit in common_prefixes:
+                folders.append(first_bit)
+            output_s3_list(objects=objects, folders=folders, item=item, bucket_name=bucket_name)
+        except botocore.exceptions.ClientError as error:
+            if 'NoSuchBucket' in error.response['Error']['Code']:
+                exit('Bucket not found.')
+            else:
+                exit('Unhandled error: {0}'.format(error.response['Error']['Code']))
+
+
+def s3_info(aws_config=None, item=None):
+    """
+    @type aws_config: Config
+    @type item: unicode
+    """
+    s3_client = get_client(client_type='s3', config=aws_config)
+    prefix = ''
+    if item and '/' in item:
+        path_elements = item.split('/')
+        bucket_name = path_elements[0]
+        prefix = "/".join(path_elements[1:])
+        if prefix.endswith('/'):
+            prefix = prefix[:-1]
+    else:
+        bucket_name = item
+    check_bucket_accessible(s3_client=s3_client, bucket_name=bucket_name)
+    try:
+        s3_object = s3_client.get_object(Bucket=bucket_name, Key=prefix)
+        output_s3_info(s3_object=s3_object, key=prefix, bucket=bucket_name)
+    except botocore.exceptions.ClientError as error:
+        if 'NoSuchBucket' in error.response['Error']['Code']:
+            exit('Bucket not found.')
+        else:
+            exit('Unhandled error: {0}'.format(error.response['Error']['Code']))
---
+++ packages/acli/acli-0.1.14.tar.gz/acli-0.1.14/lib/acli/utils.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, print_function, unicode_literals)
+import sys
+import os
+import platform
+import base64
+
+
+BASH_COMPLETION_SCRIPT = "TBC"
+BASH_COMPLETION_PATH_OSX = "/usr/local/etc/bash_completion.d"
+BASH_COMPLETION_PATH_LINUX = "/etc/bash_completion.d"
+
+
+def install_completion():
+    if platform.system() == 'Darwin':
+        if os.path.exists(BASH_COMPLETION_PATH_OSX):
+            with open("{0}/acli".format(BASH_COMPLETION_PATH_OSX), 'w') as acli_file:
+                acli_file.write(base64.b64decode(BASH_COMPLETION_SCRIPT))
+            sys.exit("bash completion script written to: {0}/acli\nrestart terminal to use.".format(
+                BASH_COMPLETION_PATH_OSX)
+            )
+        else:
+            sys.exit("bash completion not installed. try 'brew install bash-completion'.")
+    elif platform.system() == 'Linux':
+        if os.path.exists(BASH_COMPLETION_PATH_LINUX):
+            with open("{0}/acli".format(BASH_COMPLETION_PATH_LINUX), 'w') as acli_file:
+                acli_file.write(base64.b64decode(BASH_COMPLETION_SCRIPT))
+            sys.exit("bash completion script written to: {0}/acli\nrestart terminal to use.".format(
+                BASH_COMPLETION_PATH_LINUX)
+            )
+        else:
+            sys.exit("bash completion not installed.")
+
+    else:
+        sys.exit("Shell completion only available on Linux and OS X.")
+
+
+def get_console_dimensions():
+    try:
+        rows, columns = os.popen('stty size', 'r').read().split()
+    except ValueError:
+        rows, columns = 80, 25
+    return rows, columns
