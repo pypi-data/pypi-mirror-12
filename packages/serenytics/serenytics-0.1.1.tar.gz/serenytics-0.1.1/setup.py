0566fa930e17759a909ec8faedf63b7f75ccb6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:15 +0000

    Add package serenytics/0.1.1/serenytics-0.1.1.tar.gz

---
+++ packages/serenytics/serenytics-0.1.1.tar.gz/serenytics-0.1.1/setup.py
@@ -0,0 +1,48 @@
+import sys
+import os.path as osp
+import re
+from setuptools import setup, find_packages
+
+
+RE_REQUIREMENT = re.compile(r'^\s*-r\s*(?P<filename>.*)$')
+
+
+def pip(filename):
+    requirements = []
+    this_directory = osp.dirname(osp.abspath(__file__))
+    for line in open(osp.join(this_directory, filename)).readlines():
+        match = RE_REQUIREMENT.match(line)
+        if match:
+            requirements.extend(pip(match.group('filename')))
+        else:
+            requirements.append(line)
+    return requirements
+
+
+__version__ = None
+path_version = osp.join(osp.dirname(__file__), 'serenytics/version.py')
+if sys.version_info[0] == 3:
+    exec(open(path_version).read())
+else:
+    execfile(path_version)
+print __version__
+
+
+setup(
+    name='serenytics',
+    version=__version__,
+    description='Serenytics API client for python',
+    install_requires=pip('requirements-serenytics.txt'),
+    packages=find_packages(),
+    include_package_data=True,
+    author='Serenytics Team',
+    author_email='support@serenytics.com',
+    url='https://github.com/serenytics/docker-python-scripts',
+    keywords=['serenytics', 'backend', 'hosted', 'cloud',
+              'bi', 'dashboard', 'scripts', 'etl'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
