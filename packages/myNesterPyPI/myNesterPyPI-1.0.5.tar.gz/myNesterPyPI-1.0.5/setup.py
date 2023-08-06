a122169803bb3dbc0987954e2fa88d44d33fcd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:58 +0000

    Add package myNesterPyPI/1.0.5/myNesterPyPI-1.0.5.tar.gz

---
+++ packages/myNesterPyPI/myNesterPyPI-1.0.5.tar.gz/myNesterPyPI-1.0.5/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name = "myNesterPyPI",
+  version = "1.0.5",
+  py_modules = ['myNesterPyPI'],
+  author = "Alex",
+  author_email = "123@123.com",
+  description = "Support indent/deindent function"
+)
