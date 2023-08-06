d26a6d23d1bdb8095a8d059903c758fc768ccf7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:01 +0000

    Add package pydbusdecorator/2.0/pydbusdecorator-2.0.tar.gz

---
+++ packages/pydbusdecorator/pydbusdecorator-2.0.tar.gz/pydbusdecorator-2.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+'''
+Created on Nov 6, 2011
+
+@author: hugosenari
+'''
+
+from distutils.core import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(name='pydbusdecorator',
+      version='2.0',
+      description='Python decorator for dbus interface client definition',
+      author='hugosenari',
+      author_email='hugosenari@gmail.com',
+      url='https://github.com/hugosenari/pydbusdecorator',
+      keywords = ["dbus"],
+      packages=('dbusdecorator',),
+      requires=(),
+      license = "GPL",
+      classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: GNU General Public License (GPL)",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.4"
+      ],
+      long_description = readme
+)
