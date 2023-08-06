21eeaf6e3a9e8b862c9d449034cf84e33b77ccdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:27 +0000

    Add package image-diet2/0.8/image-diet2-0.8.tar.gz

---
+++ packages/image-diet2/image-diet2-0.8.tar.gz/image-diet2-0.8/image_diet/storage.py
@@ -0,0 +1,63 @@
+import importlib
+from io import BytesIO
+import os
+from os.path import abspath, basename, dirname, join, exists
+import shutil
+
+from django.conf import settings
+from django.core.files.base import File
+import pyimagediet as diet
+
+
+THIS_DIR = abspath(dirname(__file__))
+
+DEFAULT_STORAGE = 'django.core.files.storage.FileSystemStorage'
+STORAGE_MODULE, STORAGE_CLASSNAME = getattr(
+    settings, 'DIET_STORAGE', DEFAULT_STORAGE).rsplit('.', 1)
+
+storage_module = importlib.import_module(STORAGE_MODULE)
+STORAGE_CLASS = getattr(storage_module, STORAGE_CLASSNAME)
+
+CUSTOM_CONFIG = getattr(settings, 'DIET_CONFIG', '')
+
+
+class DietMixin(object):
+    def __init__(self, *args, **kwargs):
+        default_config = join(THIS_DIR, 'default.yml')
+
+        config = diet.read_yaml_configuration(default_config)
+        diet.update_configuration(config,
+                                  diet.read_yaml_configuration(CUSTOM_CONFIG))
+        self.config = config
+        self.temp_dir = self.config.get('tempdir', '/tmp')
+        super(DietMixin, self).__init__(*args, **kwargs)
+
+
+    def save_to_temp(self, fullname, content):
+        name = basename(fullname)
+        path = join(self.temp_dir, name)
+        with open(path, 'wb') as f:
+            f.write(content)
+        return path
+
+    def _save(self, name, content):
+        file_content = content.read()
+        tmppath = ""
+
+        try:
+            tmppath = self.save_to_temp(name, file_content)
+            changed = diet.diet(tmppath, self.config)
+            if changed:  # pragma: no branch
+                # If changed, then tmppath points to compressed contents.
+                with open(tmppath, 'rb') as f:
+                    file_content = f.read() # pragma: no branch
+
+            f = File(BytesIO(file_content))
+        finally:
+            # Always clean up after ourselves
+            os.remove(tmppath)
+        return super(DietMixin, self)._save(name, File(f))
+
+
+class DietStorage(DietMixin, STORAGE_CLASS):
+    pass
---
+++ packages/image-diet2/image-diet2-0.8.tar.gz/image-diet2-0.8/setup.py
@@ -0,0 +1,61 @@
+import codecs
+import os
+import re
+from setuptools import setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+'''Next two functions borrowed from pip's setup.py'''
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    # see here: https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    return codecs.open(os.path.join(HERE, *parts), 'r').read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+long_description = '''\
+image-diet2 is a Django application for removing unnecessary bytes from image
+files.  It optimizes images without changing their look or visual quality
+("losslessly").
+
+DEFINITELY NOT READY FOR PRODUCTION USE YET!
+
+It works on images in JPEG, GIF, PNG or any format with configured a
+processing pipeline. Integration with Django's storage system provides a
+seamless integration with most thumbnailing apps.'''
+
+setup(
+    author="Marko Samastur",
+    author_email="markos@gaivo.net",
+    name='image-diet2',
+    version=find_version('image_diet', '__init__.py'),
+    description='Remove unnecessary bytes from images',
+    long_description=long_description,
+    url='https://github.com/samastur/image-diet2/',
+    platforms=['OS Independent'],
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'Django>=1.7',
+        'pyimagediet>=0.9',
+    ],
+    include_package_data=True,
+    packages=['image_diet'],
+    zip_safe=False
+)
