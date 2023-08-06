bb08712a47d37e20d42ad1d6a5b08b087259b244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:11 +0000

    Add package intercom_python/1.0.1/intercom_python-1.0.1.tar.gz

---
+++ packages/intercom_python/intercom_python-1.0.1.tar.gz/intercom_python-1.0.1/intercom/__init__.py
@@ -0,0 +1,4 @@
+try:
+	from .client import IntercomClient
+except:
+	pass
---
+++ packages/intercom_python/intercom_python-1.0.1.tar.gz/intercom_python-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import intercom
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="intercom_python",
+    version="1.0.1",
+    description="A not official python bindings to Intercom API",
+    author="Ferdinand Silva",
+    author_email="ferdinandsilva@ferdinandsilva.com",
+    install_requires=['requests'],
+    packages=['intercom'],
+    url="https://github.com/six519/intercom_python",
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'License :: Freeware',
+    ),
+    download_url="https://github.com/six519/intercom_python",
+)
