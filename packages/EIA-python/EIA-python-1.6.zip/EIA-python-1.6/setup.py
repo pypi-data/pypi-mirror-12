539cb876f14b988ceebde6a4a9f62aa1ba2d66b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:34 +0000

    Add package EIA-python/1.6/EIA-python-1.6.zip

---
+++ packages/EIA-python/EIA-python-1.6.zip/EIA-python-1.6/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='EIA-python',
+    version='1.6',
+    packages=['EIA'],
+    url='https://github.com/mra1385/EIA-python',
+    license='BSD',
+    author='Mike Azar',
+    author_email='mikeazar@gmail.com',
+    description='Python Wrapper for Energy Information Administration API'
+)
