5ebc739c7ad9d0aab5da71b7f3e63d435d4b1a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:48 +0000

    Add package pinguino/12.0.1/pinguino-12.0.1.tar.gz

---
+++ packages/pinguino/pinguino-12.0.1.tar.gz/pinguino-12.0.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+# import site
+# site.addsitedir("/usr/share/pinguino-12")
+
+setup(name = "pinguino",
+      version = "12.0.1",
+      description = "",
+      long_description = "",
+
+      author = "",
+      author_email = "",
+      maintainer = "Yeison Cardona",
+      maintainer_email = "yeison.eng@gmail.com",
+
+      url = "http://www.pinguino.cc/",
+      download_url = "",
+
+      license = "GPLv2",
+      install_requires = ["gitpython", "hgapi", "pyusb==1.0.0b2"],
+      keywords = 'microchip, electronic, prototyping, IDE',
+
+      classifiers=[#list of classifiers in https://pypi.python.org/pypi?:action=list_classifiers
+                   "Development Status :: 4 - Beta",
+                   "Environment :: X11 Applications :: Qt",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+                   "Operating System :: POSIX :: Linux",
+                   "Programming Language :: Python",
+                   ],
+
+
+      scripts = ["bin/pinguino",
+                 "bin/pinguino-cmd",
+                 "bin/pinguino-reset",],
+
+      packages = find_packages(),
+
+      include_package_data = True,
+
+      )
