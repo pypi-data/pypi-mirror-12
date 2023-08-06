4acbbd10d560705b3f8d06dd4258c56ee8232dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:32 +0000

    Add package dbkit/0.2.3/dbkit-0.2.3.tar.gz

---
+++ packages/dbkit/dbkit-0.2.3.tar.gz/dbkit-0.2.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os.path
+
+from setuptools import setup
+
+
+def read(filename):
+    """Read files relative to this file."""
+    full_path = os.path.join(os.path.dirname(__file__), filename)
+    with open(full_path, 'r') as fh:
+        return fh.read()
+
+
+setup(
+    name='dbkit',
+    version='0.2.3',
+    description='DB-API made easier',
+    long_description=read('README') + "\n\n" + read('ChangeLog'),
+    url='https://github.com/kgaughan/dbkit/',
+    license='MIT',
+    py_modules=['dbkit'],
+
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database',
+    ),
+
+    author='Keith Gaughan',
+    author_email='k@stereochro.me',
+)
