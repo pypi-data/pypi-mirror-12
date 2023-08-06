d186ba48c69b51609d955ade1f89a209e11db712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:25 +0000

    Add package chartjs/1.2/chartjs-1.2.zip

---
+++ packages/chartjs/chartjs-1.2.zip/chartjs-1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'chartjs',
+  packages = ['chartjs'],
+  version = '1.2',
+  description = 'Python 3.x implementation of ChartJS to create web based charts',
+  author = 'Patrick Lambert',
+  author_email = 'dendory@live.ca',
+  url = 'https://github.com/dendory/chartjs', 
+  download_url = 'https://github.com/dendory/chartjs/archive/master.zip',
+  keywords = ['chart', 'charts', 'matplotlib'],
+  classifiers = [],
+)
