f02a27cd3632fc2ba84b0ecf72351bb493606d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:02 +0000

    Add package nexusadspy/0.1.0/nexusadspy-0.1.0.tar.gz

---
+++ packages/nexusadspy/nexusadspy-0.1.0.tar.gz/nexusadspy-0.1.0/nexusadspy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (
+    print_function, division, generators,
+    absolute_import, unicode_literals
+)
+
+from .client import AppnexusClient
+from .report import AppnexusReport
