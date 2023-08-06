4c94c7b305b40049bd4a5bf44d316adcd62cd5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:10 +0000

    Add package outfancy/0.1.4/outfancy-0.1.4.tar.gz

---
+++ packages/outfancy/outfancy-0.1.4.tar.gz/outfancy-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'outfancy',
+  packages = ['outfancy'],
+  version = '0.1.4',
+  description = 'Sistema de renderizado para salidas en pantalla (tty).',
+  author = 'Carlos A. Planchon',
+  author_email = 'urucrafters@gmail.com',
+  url = 'https://github.com/carlosplanchon/outfancy',
+  download_url = 'https://github.com/carlosplanchon/outfancy/archive/0.1.tar.gz',
+  keywords = ['rendering', 'graphical', 'terminal'],
+  classifiers = [],
+)
