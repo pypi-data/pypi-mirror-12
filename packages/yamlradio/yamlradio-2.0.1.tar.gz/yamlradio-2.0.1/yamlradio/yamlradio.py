1acc3bfd374f966f093332a046d499e1f453b114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:45 +0000

    Add package yamlradio/2.0.1/yamlradio-2.0.1.tar.gz

---
+++ packages/yamlradio/yamlradio-2.0.1.tar.gz/yamlradio-2.0.1/yamlradio/yamlradio.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+## Dependencies:    mplayer, argparse, argcomplete, pyYAML
+## Author:          Gijs Timmers: https://github.com/GijsTimmers
+
+## Licence:         CC-BY-SA-4.0
+##                  http://creativecommons.org/licenses/by-sa/4.0/
+
+## This work is licensed under the Creative Commons
+## Attribution-ShareAlike 4.0 International License. To  view a copy of
+## this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or
+## send a letter to Creative Commons, PO Box 1866, Mountain View,
+## CA 94042, USA.
+
+from .keypress import Keypress
+from .fabriek import Fabriek
+from .parser import Parser
+from .radio import Radio
+
+import threading                ## Voor multithreading
+import cursor                   ## Cursor tonen/verbergen
+import time                     ## Polling voor opvangen keypress
+import sys
+import re                       ## Regex
+
+def main():
+    pa = Parser()
+    naam, url, comm = pa.zendervinden()
+    
+    cursor.hide()
+    
+    fa = Fabriek()
+    co = fa.returnCommunicatorObject(comm)
+        
+    rd = Radio()
+    t1 = threading.Thread(target=rd.afspelen, args=(naam, url, co))
+    
+        
+    with Keypress() as kp:
+        t1.start()
+        while t1.isAlive():
+            keypress = kp.getKeypress()
+            if keypress == "exit":
+                break
+            elif keypress == "volumeUp":
+                rd.volumeUp()
+            elif keypress == "volumeDown":
+                rd.volumeDown()
+            else:
+                pass
+                
+            ## Polling
+            time.sleep(0.1)
+            
+    
+    cursor.show()
+    rd.stoppen()
+    return 0
