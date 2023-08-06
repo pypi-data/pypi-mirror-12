4ade3c1b365fb1358d4ce62b5f077fcb5f655e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:30 +0000

    Add package netman/1.1.14/netman-1.1.14.tar.gz

---
+++ packages/netman/netman-1.1.14.tar.gz/netman-1.1.14/netman/adapters/switches/__init__.py
@@ -0,0 +1,23 @@
+# Copyright 2015 Internap.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from netman.adapters.switches.backward_compatible_brocade import BackwardCompatibleBrocade
+from netman.core.objects.switch_transactional import SwitchTransactional
+
+
+def brocade_factory(switch_descriptor, lock):
+    return SwitchTransactional(
+        impl=BackwardCompatibleBrocade(switch_descriptor=switch_descriptor),
+        lock=lock
+    )
