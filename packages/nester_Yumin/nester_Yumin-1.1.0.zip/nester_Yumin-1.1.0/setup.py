072efb36b21f4f455e71db265fac0529665f9446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:30 +0000

    Add package nester_Yumin/1.1.0/nester_Yumin-1.1.0.zip

---
+++ packages/nester_Yumin/nester_Yumin-1.1.0.zip/nester_Yumin-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_Yumin',
+    version = '1.1.0',
+    py_modules = ['nester_Yumin'],
+    author = 'Yumin Kim',
+    author_email = 'green4469@kookmin.ac.kr',
+    url = 'http://www.kookmin.ac.kr',
+    description = 'A simple printer of nested list'
+    )
