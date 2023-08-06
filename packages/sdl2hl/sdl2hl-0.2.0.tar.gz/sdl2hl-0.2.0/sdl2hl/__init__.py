94d3dc088eea0dbb3a40aeec6ef7d27008b603ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:35 +0000

    Add package sdl2hl/0.2.0/sdl2hl-0.2.0.tar.gz

---
+++ packages/sdl2hl/sdl2hl-0.2.0.tar.gz/sdl2hl-0.2.0/sdl2hl/__init__.py
@@ -0,0 +1,11 @@
+from constants import *
+from video import *
+from sdl import *
+from renderer import *
+from rect import *
+from surface import *
+from events import *
+from keycode import *
+from scancode import *
+import timer
+from error import SDLError
