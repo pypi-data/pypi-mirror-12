cc02e7cc563b735285b95913acace813d1a74b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:41 +0000

    Add package exceltowiki/0.1.13/exceltowiki-0.1.13a.tar.gz

---
+++ packages/exceltowiki/exceltowiki-0.1.13a.tar.gz/exceltowiki-0.1.13/exceltowiki/example/example.py
@@ -0,0 +1,14 @@
+'''
+Created on Jul 28, 2015
+
+@author: venkman69@yahoo.com
+'''
+
+from exceltowiki import excelToWiki
+import exceltowiki
+
+exceltowiki.INLINE_FMT=True
+e2w = excelToWiki("./test.xlsx",["Sheet1"],"blue","yellow")
+print e2w.sheetnames
+print e2w.getSheet("Sheet1")
+print e2w.getWorkbook()
