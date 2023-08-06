0c9b85ba9833e62abbdd2c5c0933bb3d2923f297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:56 +0000

    Add package jaobi/0.12.7.1/jaobi-0.12.7.1.tar.gz

---
+++ packages/jaobi/jaobi-0.12.7.1.tar.gz/jaobi-0.12.7.1/jaobi/reports/js/content_consumers.js
@@ -0,0 +1,42 @@
+// these functions will be executed on the content_consumption collection
+// to produce a reduced collection to reports content consumers
+
+// This is here 'cause is pretty shit to write js code inside a """big"""
+// python string.
+
+// Please, do not mess with the // ... function ... and // end ... comments, they
+// are used in the regexes to get this js code. Thanks.
+
+// map function
+mapf = function(){
+  // date is a date time only with yyyy-mm-dd, no HH:MM:SS, or better said,
+  // 00:00:00 for everybody!
+  var date = ISODate(this.inclusion_date.toISOString().split('T')[0]);
+  emit({'content': this.content, 'date': date}, this.consumer);
+}
+// end map function
+
+// reduce function
+reducef = function(key, values){
+  var reduced = [];
+
+  values.forEach(function(value){
+    if(reduced.indexOf(value.toString()) < 0){
+      reduced.push(value.toString());
+    }
+  });
+
+  return reduced.length;
+}
+// end reduce function
+
+// finalize function
+finalizef = function(key, value){
+  if(typeof(value) == 'object'){
+    return 1;
+  }
+  else{
+    return value;
+  }
+}
+// end finalize function
