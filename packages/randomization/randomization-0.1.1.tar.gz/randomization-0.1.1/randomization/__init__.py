d35c931d5ab66beb6bc672a2bcd43e4f71ccec0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:09 +0000

    Add package randomization/0.1.1/randomization-0.1.1.tar.gz

---
+++ packages/randomization/randomization-0.1.1.tar.gz/randomization-0.1.1/randomization/__init__.py
@@ -0,0 +1,13 @@
+# from randomization import (
+#     simple,
+#     complete,
+#     block,
+#     random_block,
+#     random_treatment_order,
+#     efrons_biased_coin,
+#     weis_urn,
+#     stratification,
+#     double_biased_coin_minimize,
+#     double_biased_coin_urn
+# )
+import randomization
