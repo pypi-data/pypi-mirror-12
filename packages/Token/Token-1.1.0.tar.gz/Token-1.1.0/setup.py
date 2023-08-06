d5c4115087f6d18e1510fb5b565f8c184d0be835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:41 +0000

    Add package Token/1.1.0/Token-1.1.0.tar.gz

---
+++ packages/Token/Token-1.1.0.tar.gz/Token-1.1.0/setup.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+import sys
+from setuptools import setup, find_packages
+
+NAME = "Token"
+VERSION = "1.1.0"
+
+
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.10", "six >= 1.9", "certifi", "python-dateutil"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Banknote Service",
+    author="Mariano Sorgente",
+    author_email="mariano.sorgente@token.io",
+    url="https://mariano54@bitbucket.org/tokenio/token-python.git",
+    keywords=["Swagger", "Token API"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    Interact with Token REST Apis
+    """
+)
