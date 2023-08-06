bf40381296d8fdab2e0e36babea0294f619c6efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:39 +0000

    Add package forgetSQL/0.5.1/forgetSQL-0.5.1.tar.gz

---
+++ packages/forgetSQL/forgetSQL-0.5.1.tar.gz/forgetSQL-0.5.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+# Whyyyyyyy oh whyyy doesn't distutils do this !??
+# (if you forget this - root with umask 0077 
+#  will install non-readable libraries)
+import os
+if os.geteuid() == 0:
+    os.umask(0022)
+
+setup(name="forgetSQL",
+      version="0.5.1",
+      author="Stian Soiland",
+      author_email="stian@soiland.no",
+      url="http://forgetsql.sourceforge.net/",
+      license="LGPL",
+      description=
+"""forgetSQL is a Python module for accessing SQL databases by creating
+classes that maps SQL tables to objects, normally one class pr. SQL
+table. The idea is to forget everything about SQL and just worrying
+about normal classes and objects.""",
+      py_modules=['forgetSQL'],
+      scripts = ['bin/forgetsql-generate'],
+      package_dir = {'': 'lib'},
+     )
