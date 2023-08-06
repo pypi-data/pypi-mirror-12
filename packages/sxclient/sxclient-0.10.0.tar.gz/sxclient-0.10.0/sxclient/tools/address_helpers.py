999f467c8aacd977a1c484d0db81728aa401a52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:38 +0000

    Add package sxclient/0.10.0/sxclient-0.10.0.tar.gz

---
+++ packages/sxclient/sxclient-0.10.0.tar.gz/sxclient-0.10.0/sxclient/tools/address_helpers.py
@@ -0,0 +1,14 @@
+'''
+Copyright (c) 2015 Skylable Ltd.
+License: Apache 2.0, see LICENSE for more details.
+'''
+import socket
+
+__all__ = ['get_addresses']
+
+
+def get_addresses(host):
+    '''For a given host, obtain its IP addresses.'''
+    info = socket.getaddrinfo(host, None, 0, 0, socket.IPPROTO_TCP)
+    addresses = [elt[4][0] for elt in info]
+    return addresses
