d0174811ee23d92c55ccf17f254dd3611956a70a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:34 +0000

    Add package tonyg-rfc3339/0.1/tonyg-rfc3339-0.1.tar.gz

---
+++ packages/tonyg-rfc3339/tonyg-rfc3339-0.1.tar.gz/tonyg-rfc3339-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import os
+
+setup(name='tonyg-rfc3339',
+      version='0.1',
+      description='Python implementation of RFC 3339',
+      author='Tony Garnock-Jones',
+      author_email='tonyg@lshift.net',
+      url='https://github.com/cardforcoin/python-rfc3339',
+      py_modules=['rfc3339'],
+      )
