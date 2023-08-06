f78678fd9b27e255eb8786f6ad324c7d7a1dde04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:16 +0000

    Add package jsonpyes/1.1.1/jsonpyes-1.1.1.tar.gz

---
+++ packages/jsonpyes/jsonpyes-1.1.1.tar.gz/jsonpyes-1.1.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup 
+
+setup(name='jsonpyes',
+        version='1.1.1',
+        author="Alexander Liu",
+        license="GPL V3",
+        description="Import JSON raw data to ElasticSearch using Python in one line of commands",
+        platforms=["Unix","Linux","OSX","Android","Windows"],
+        url="https://github.com/xros/json-py-es",
+        download_url="https://github.com/xros/json-py-es/tarball/1.1.1",
+        #py_modules=['elasticsearch','jsonpyes','simplejson'],
+        install_requires=[
+            'elasticsearch',
+            'simplejson',
+        ],
+        keywords=["elasticsearch","json","json2es","jsonpyes"],
+        classifiers=[],
+        )
