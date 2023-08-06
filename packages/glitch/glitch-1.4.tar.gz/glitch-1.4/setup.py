8547cb1b9840b5488ee51bf2bbc1978b0b46fb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:58 +0000

    Add package glitch/1.4/glitch-1.4.tar.gz

---
+++ packages/glitch/glitch-1.4.tar.gz/glitch-1.4/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name = "glitch",
+    version = "1.4",
+    description = "glitch jpg files",
+    license = 'MIT',
+    author = "trsqxyz",
+    author_email = "trsqxyz@gmail.com",
+    url = "https://github.com/trsqxyz/glitch",
+    classifiers = [
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: MIT License"
+    ],
+    packages = ["glitch"],
+    entry_points = """
+        [console_scripts]
+            glitch = glitch.glitch:main
+        """,
+    install_requires = [
+        'docopt',
+    ]
+)
