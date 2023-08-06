19769b7e1c3587702bf809274d27b6b33e57a030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:32 +0000

    Add package abilian-core/0.5.0/abilian-core-0.5.0.tar.gz

---
+++ packages/abilian-core/abilian-core-0.5.0.tar.gz/abilian-core-0.5.0/abilian/web/admin/panels/dashboard.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+"""
+"""
+from __future__ import absolute_import, print_function, division
+
+from datetime import datetime, timedelta
+import sqlalchemy as sa
+
+from flask import render_template, current_app
+
+from abilian.i18n import _l
+from abilian.core.models.subjects import User
+from abilian.services.audit import AuditEntry, CREATION
+
+from ..panel import AdminPanel
+
+
+class DashboardPanel(AdminPanel):
+  id = 'dashboard'
+  path = ''
+  label = _l('Dashboard')
+  icon = 'eye-open'
+
+  def get(self):
+    stats = {
+      'today': stats_since(timedelta(days=1)),
+      'this_week': stats_since(timedelta(days=7)),
+      'this_month': stats_since(timedelta(days=30)),
+    }
+    return render_template("admin/dashboard.html", stats=stats)
+
+
+def stats_since(dt):
+  new_members = new_documents = new_messages = 0
+  after_date = datetime.utcnow() - dt
+  session = current_app.db.session()
+  counts_per_type = session\
+    .query(AuditEntry.entity_type.label('type'),
+           sa.func.count(AuditEntry.entity_type).label('count'))\
+    .group_by(AuditEntry.entity_type)\
+    .filter(AuditEntry.happened_at > after_date) \
+    .filter(AuditEntry.type == CREATION)\
+    .all()
+
+  for entity_type, count in counts_per_type:
+    entity_class = entity_type.split('.')[-1]
+    if entity_class == 'User':
+      new_members = count
+    elif entity_class == 'Document':
+      new_documents = count
+    elif entity_class == 'Message':
+      new_messages = count
+
+  active_users = session.query(sa.func.count(User.id))\
+                        .filter(User.last_active > after_date).scalar()
+
+  return {
+    'new_members': new_members,
+    'active_users': active_users,
+    'new_documents': new_documents,
+    'new_messages': new_messages,
+  }
---
+++ packages/abilian-core/abilian-core-0.5.0.tar.gz/abilian-core-0.5.0/abilian/web/resources/js/widgets/dynamic-row.js
@@ -0,0 +1,95 @@
+(function(factory) {
+    'use strict';
+    require(['AbilianWidget', 'jquery'], factory);
+}
+ (function(Abilian, $) {
+     'use strict';
+     // multiple row widget
+    function DynamicRowWidget(table, options) {
+        var self = this;
+        self.table = table;
+        self.prefix = table.data('prefix');
+        self.tbody = table.children("tbody");
+        self.options = options;
+
+        self.current_index = table.find("> tbody > tr").length;
+
+        self.add_button = $("<th><span class=\"glyphicon glyphicon-plus\"></span></th>");
+        self.add_button.click(function(e) {
+                                  self.addRow();
+                                  e.preventDefault();
+                              });
+
+        self.minus_button = $("<td><span class=\"glyphicon glyphicon-remove\"></span></td>");
+        self.minus_button.click(function(e) {
+                                    $(this).closest('tr').remove();
+                                    e.preventDefault();
+                                });
+
+        table.find("> thead > tr").append(self.add_button);
+        table.find("> tbody > tr").append(self.minus_button);
+
+        self.templateRow = table.find("tbody > tr:first").clone(true, true);
+        /* remove value except for specific controls that have constant (and
+         * required) value
+         */
+        self.templateRow.find('input' +
+                               '[type!="checkbox"]' +
+                               '[type!="radio"]' +
+                               '[data-short-name!="csrf_token"]')
+            .attr('value', '');
+        self.templateRow.find("textarea").text('');
+    }
+
+    DynamicRowWidget.prototype = {
+        'addRow': function() {
+            var self = this;
+            var new_row = self.templateRow.clone(true, true);
+            new_row.find("input").each(
+                function() {
+                    var item  = $(this);
+                    var shortName = item.data("shortName");
+                    var name = self.prefix + '-' + self.current_index;
+                    if (shortName) {
+                        name =  name + '-' + shortName;
+                    }
+                    item.attr('name', name);
+                    item.attr("id", name);
+                });
+            new_row.find("select").each(
+                function() {
+                    var item  = $(this);
+                    var id_splitted = item.attr("id").split('-');
+
+                    for (var i = 0; i < id_splitted.length; i++) {
+                        if (!isNaN(id_splitted[i])){
+                            id_splitted[i] = self.current_index
+                        }
+                    }
+                    var name = id_splitted.join('-')
+                    item.attr('name', name);
+                    item.attr("id", name);
+                }
+            );
+            self.tbody.append(new_row);
+            new_row.find('.js-widget').each(Abilian.initJsWidget);
+            self.current_index += 1;
+        }
+    };
+
+    function dynamicRowWidget(params) {
+        var table = $(this);
+        return new DynamicRowWidget(table, params);
+    }
+
+    Abilian.registerWidgetCreator('dynamicRowWidget', dynamicRowWidget);
+
+    $.fn.dynamicRowWidget = function(options) {
+        var defaults = {};
+        var opts = $.extend(defaults, options);
+        return this.each(
+            function() { dynamicRowWidget.bind(this)(opts); }
+        );
+    };
+
+}));
