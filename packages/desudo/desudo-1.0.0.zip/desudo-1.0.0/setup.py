4462c3095833b8a6a6f34c88cbdfe3dea22741b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:55 +0000

    Add package desudo/1.0.0/desudo-1.0.0.zip

---
+++ packages/desudo/desudo-1.0.0.zip/desudo-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='desudo',
+    version='1.0.0',
+    py_modules=['sudo'],
+    author='jinn',
+    author_email='ourobotos@gmail.com',
+    url='',
+    description='just for study',
+)
+
---
+++ packages/desudo/desudo-1.0.0.zip/desudo-1.0.0/sudo.py
@@ -0,0 +1,8 @@
+import random
+
+def throwOne(alist):
+    return random.choice(alist)
+
+grid81 = [1, 2, 3, 4, 5, 6]
+
+print(throwOne(grid81))
