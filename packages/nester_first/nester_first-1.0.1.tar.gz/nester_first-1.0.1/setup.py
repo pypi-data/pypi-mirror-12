80f1e40253fc94374534a224c4cd9c8471fc7997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:02 +0000

    Add package nester_first/1.0.1/nester_first-1.0.1.tar.gz

---
+++ packages/nester_first/nester_first-1.0.1.tar.gz/nester_first-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name             = 'nester_first',
+        version          = '1.0.1',
+        py_modules       = ['nester_first'],
+        author           = 'idyll',
+        author_email     = 'idyllzhou@gmail.com',
+        url              = 'www.sponia.com',
+        description      = 'A simple printer of nested lists',
+    )
