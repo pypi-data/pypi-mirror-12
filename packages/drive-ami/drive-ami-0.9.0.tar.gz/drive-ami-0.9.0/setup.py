d5e769455ff5d82460debbe41365a629588ef8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:28 +0000

    Add package drive-ami/0.9.0/drive-ami-0.9.0.tar.gz

---
+++ packages/drive-ami/drive-ami-0.9.0.tar.gz/drive-ami-0.9.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+requirements = ['pexpect',
+                'astropy',
+                'colorlog',
+]
+
+
+setup(
+    name="drive-ami",
+    version="0.9.0",
+    packages=['driveami'],
+    scripts=['bin/driveami_filter_rawfile_listing.py',
+             'bin/driveami_list_rawfiles.py',
+             'bin/driveami_calibrate_rawfiles.py'],
+    description="An interface layer for scripting the AMI-Reduce pipeline.",
+    author="Tim Staley",
+    author_email="timstaley337@gmail.com",
+    url="https://github.com/timstaley/drive-ami",
+    install_requires=requirements,
+)
