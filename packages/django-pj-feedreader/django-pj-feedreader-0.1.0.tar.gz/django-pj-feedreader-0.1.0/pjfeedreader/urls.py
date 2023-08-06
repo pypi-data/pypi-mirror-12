3ae703098c2c1565bfc2b44f167c23f08656df36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:34 +0000

    Add package django-pj-feedreader/0.1.0/django-pj-feedreader-0.1.0.tar.gz

---
+++ packages/django-pj-feedreader/django-pj-feedreader-0.1.0.tar.gz/django-pj-feedreader-0.1.0/pjfeedreader/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, url, include
+
+from rest_framework.routers import SimpleRouter
+from .views import FeedViewSet, CategoryViewSet, FeedIndexView
+
+router = SimpleRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'feeds', FeedViewSet)
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^api/v1/', include(router.urls)),
+    #url(r'^$', FeedIndexView.as_view(), name='feed-index'),
+    url(r'^.*', FeedIndexView.as_view(), name='feed-index'),
+    )
