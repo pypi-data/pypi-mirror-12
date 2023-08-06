8393c8feffd2aebabc654398226897416450f8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:14 +0000

    Add package cmsplugin-nvd3/0.4/cmsplugin-nvd3-0.4.tar.gz

---
+++ packages/cmsplugin-nvd3/cmsplugin-nvd3-0.4.tar.gz/cmsplugin-nvd3-0.4/cmsplugin_nvd3/settings.py
@@ -0,0 +1,31 @@
+# ------------------ Cmsplugin-nvd3 setting defaults --------------------
+
+# Value separator for x-data
+CMSNVD3_DATASEP = ','
+
+# Group separator for y-series
+CMSNVD3_YDATAGROUPSEP = ';'
+
+# Length of random part of id
+CMSNVD3_ID_RANDOM_LENGTH = 7
+
+# Container prefix; used only if container name is not specified by user
+CMSNVD3_CONT_ID_PREFIX = 'nvd3id_'
+
+# Default container width; used if not specified
+CMSNVD3_CONT_WIDTH = 600
+
+# Default container height; used if not specified
+CMSNVD3_CONT_HEIGHT = 400
+
+# Maximum allowed container size; all values of width or height will be
+# reduced to defaults if them exeed this value
+CMSNVD3_MAX_CONT_DIM = 3000
+
+# D3, NVD3 sources. You can define it as 'local' to use local copy from static dir
+CMSNVD3_D3JS_SOURCE = '//cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js'
+CMSNVD3_JS_SOURCE = '//cdn.rawgit.com/novus/nvd3/v1.8.1/build/nv.d3.min.js'
+CMSNVD3_CSS = '//cdn.rawgit.com/novus/nvd3/v1.8.1/build/nv.d3.css'
+
+#a path for d3,nvd3 static files, defined as: STATIC_URL+CMSNVD3_URL 
+CMSNVD3_URL = 'nvd3plugin/'
---
+++ packages/cmsplugin-nvd3/cmsplugin-nvd3-0.4.tar.gz/cmsplugin-nvd3-0.4/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+README_FILE = 'README.rst'
+
+setup(name='cmsplugin-nvd3',
+      packages=['cmsplugin_nvd3'],
+      version='0.4',
+      description='Include nvd3 charts into django-cms driven sites',
+      keywords='nvd3,plugin,charts,nvd3-charts,django,django-cms',
+      long_description=open(README_FILE).read(),
+      include_package_data=True,
+      author='Dmitry E. Kislov',
+      author_email='kislov@easydan.com',
+      url='https://github.com/scidam/cmsplugin-nvd3',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          'Topic :: Internet :: WWW/HTTP :: Site Management',
+          'Topic :: Multimedia :: Graphics :: Presentation'
+          ],
+      )
