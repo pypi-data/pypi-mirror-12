9027ccb8019fbad02707d518aa08d3704cca0e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:05 +0000

    Add package tsl/1.0.0/tsl-1.0.0.tar.gz

---
+++ packages/tsl/tsl-1.0.0.tar.gz/tsl-1.0.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+__author__ = 'idbord'
+from setuptools import setup
+
+setup(
+    name='tsl',
+    version="1.0.0",
+    keywords=('tsl', 'translate', 'baidu', 'dict', 'baidu api'),
+    description="tsl is ready. It is for translation work on linux platform and it can autoly translate words or article between Chinese and English. Also, it provides Baike category for extended need.",
+    license='MIT',
+    author='idbord',
+    author_email='byzone482@gmail.com',
+    url="https://github.com/idbord/trans",
+    packages=[
+        'trans'
+    ],
+    install_requires=[
+
+    ],
+    platforms="linux",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+	'Programming Language :: Python :: 3.4',
+        "Operating System :: POSIX :: Linux"
+    ],
+    entry_points={
+        'console_scripts': [
+            'tsl=trans.Trans:run'
+        ]
+    }
+)
