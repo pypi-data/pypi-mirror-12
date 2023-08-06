184ba5361a950f4ca7157a0446955587e2b6f2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:20 +0000

    Add package speech-processing/0.2/speech-processing-0.2.tar.gz

---
+++ packages/speech-processing/speech-processing-0.2.tar.gz/speech-processing-0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name='speech-processing',
+  version='0.2',
+  description='Python speech averaging and comparison',
+  author='Andrew Pedley',
+  url='https://github.com/vocalize/speechprocessing',
+  author_email='apedley@gmail.com',
+  keywords=['speech', 'processing'],
+  packages=['speechprocessing']
+)
