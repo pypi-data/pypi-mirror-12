28b0dc2153d3f96446fb83bdcc180b72acfb840b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:14 +0000

    Add package kevindongtest2/1.0.0/kevindongtest2-1.0.0.tar.gz

---
+++ packages/kevindongtest2/kevindongtest2-1.0.0.tar.gz/kevindongtest2-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'kevindongtest2',
+    version = '1.0.0',
+    py_modules =['kevindongtest2'],
+    author  ='kevin',
+    author_email ='kevin.dong.it@gmail.com',
+    url='www.abc.com',
+    description='kevindong',
+    )
