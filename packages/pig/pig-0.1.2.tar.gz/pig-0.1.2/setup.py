47377276f2fa7b0e3ac09c5a507561c322b29baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:27 +0000

    Add package pig/0.1.2/pig-0.1.2.tar.gz

---
+++ packages/pig/pig-0.1.2.tar.gz/pig-0.1.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import setuptools
+
+setuptools.setup(name="pig",
+                 version="0.1.2",
+                 description="Phoenix Interfaces for Gadgets",
+                 url="https://github.com/erikperillo/pig",
+                 author="Erik Perillo",
+                 author_email="erik.perillo@gmail.com",
+                 license="GPL",
+                 packages=["pig"],
+                 classifiers=["Programming Language :: Python :: 3"])
