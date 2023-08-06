ea66b65a5c63a1e32dd96d44cbd8267d5da7780f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:53 +0000

    Add package infi.projector/0.6.45/infi.projector-0.6.45.tar.gz

---
+++ packages/infi.projector/infi.projector-0.6.45.tar.gz/infi.projector-0.6.45/src/infi/projector/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.6.45"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.6.45)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'c86d816ca020cbdf28cb1b4714ac283a7cbc3bcd'
+__git_head_subject__ = 'add comment in doc of version upload'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2015-12-10 16:36:51'
