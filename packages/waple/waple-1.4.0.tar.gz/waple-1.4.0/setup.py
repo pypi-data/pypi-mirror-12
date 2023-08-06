4bf1b404ab09ea4cf3a0ce2a5f3578ada89b4d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:18 +0000

    Add package waple/1.4.0/waple-1.4.0.tar.gz

---
+++ packages/waple/waple-1.4.0.tar.gz/waple-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'waple',
+    version = '1.4.0',
+    py_modules = ['waple'],
+    author = 'waple',
+    author_email = 'waple0820@gmai.com',
+    url = 'www.headfirstlabs.com',
+    description = 'A sample',
+    )
