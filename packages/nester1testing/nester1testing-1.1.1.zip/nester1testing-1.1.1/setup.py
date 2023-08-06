0754464643b17bc4887fcdf762b39a958600163a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:17 +0000

    Add package nester1testing/1.1.1/nester1testing-1.1.1.zip

---
+++ packages/nester1testing/nester1testing-1.1.1.zip/nester1testing-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'nester1testing',
+version = '1.1.1',
+py_modules = ['nestertest'],
+author = 'amuklpython',
+author_email = '254304919@qq.com',
+url = 'http://www.qq.com',
+description = 'A simple printer of nested lists',
+)
