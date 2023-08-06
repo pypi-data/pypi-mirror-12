2a61f148b16a1b11843f1ae80fae404c59b093b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:47 +0000

    Add package nana/1.0.6/nana-1.0.6.tar.gz

---
+++ packages/nana/nana-1.0.6.tar.gz/nana-1.0.6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+try:
+    long_description = open('README.rst').read()
+except:
+    long_description = 'Nana keeps an eye on a directory and reacts when anything changes.'
+
+
+setup(
+    name='nana',
+    version='1.0.6',
+    author='Konstantin Molchanov',
+    description='Nana keeps an eye on a directory and reacts when anything changes',
+    long_description=long_description,
+    author_email='moigagoo@live.com',
+    url='https://bitbucket.org/moigagoo/nana',
+    py_modules=['nana'],
+    install_requires=['cliar'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix'],
+    entry_points={
+        'console_scripts': [
+            'nana=nana:main'
+        ]
+    }
+)
