a1ca0ea742c29082c3274623588337c03c873608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:02 +0000

    Add package pandas-td/0.8.6/pandas-td-0.8.6.tar.gz

---
+++ packages/pandas-td/pandas-td-0.8.6.tar.gz/pandas-td-0.8.6/pandas_td/__init__.py
@@ -0,0 +1,17 @@
+from .td import connect
+from .td import create_engine
+from .td import read_td
+from .td import read_td_query
+from .td import read_td_job
+from .td import read_td_table
+from .td import to_td
+
+__all__ = [
+    'connect',
+    'create_engine',
+    'read_td',
+    'read_td_query',
+    'read_td_job',
+    'read_td_table',
+    'to_td',
+]
