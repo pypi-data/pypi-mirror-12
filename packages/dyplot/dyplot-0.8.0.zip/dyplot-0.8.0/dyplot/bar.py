2c75253bf3a0af499ff48c99f336f3150b103859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:45 +0000

    Add package dyplot/0.8.0/dyplot-0.8.0.zip

---
+++ packages/dyplot/dyplot-0.8.0.zip/dyplot-0.8.0/dyplot/bar.py
@@ -0,0 +1,23 @@
+from dyplot.core import Core 
+class Bar(Core):
+    """
+    To plot a bar chart.
+        :param height: the list to plot.
+        :param label: A axis label.
+    """
+    def __init__(self, height, label):
+        Core.__init__(self)
+        self.option["data"]["type"] = "bar"
+        columns = []
+        columns.append(label)
+        columns.extend(height)
+        self.option["data"]["columns"].append(columns)
+        self.option["bar"] = {}
+        self.option["bar"]["width"] = {}
+        self.option["bar"]["width"]["ratio"] = 0.5
+    def __call__(self, height, label):
+        columns = []
+        columns.append(label)
+        columns.extend(height)
+        self.option["data"]["columns"].append(columns)
+
