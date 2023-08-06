592a1ce1f58408fa6e29c5fbe93cac9232163a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:34 +0000

    Add package sympy_recursive/0.1/sympy_recursive-0.1.tar.gz

---
+++ packages/sympy_recursive/sympy_recursive-0.1.tar.gz/sympy_recursive-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='sympy_recursive',
+      version='0.1',
+      description='Resolve recursive sequences to explicit nth item formulas with SymPy.',
+      url='http://github.com/hejmsdz/sympy_recursive',
+      author='Mikołaj Rozwadowski',
+      author_email='mikolaj.rozwadowski@outlook.com',
+      license='MIT',
+      packages=['sympy_recursive'],
+      zip_safe=False)
