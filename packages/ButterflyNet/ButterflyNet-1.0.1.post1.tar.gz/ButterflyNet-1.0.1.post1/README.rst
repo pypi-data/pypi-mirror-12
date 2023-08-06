1991ceb744563619b85f6fc5ed38b74c94af3652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:11 +0000

    Add package ButterflyNet/1.0.1.post1/ButterflyNet-1.0.1.post1.tar.gz

---
+++ packages/ButterflyNet/ButterflyNet-1.0.1.post1.tar.gz/ButterflyNet-1.0.1.post1/README.rst
@@ -0,0 +1,61 @@
+.. figure:: https://butterflynet.veriny.tf/bnet.png
+   :alt: ButterflyNet - Drawn by Tudor Iacobescu
+
+|Build Status| |PyPI version|
+|https://img.shields.io/pypi/l/ButterflyNet.svg|
+|https://img.shields.io/pypi/dw/ButterflyNet.svg|
+
+ButterflyNet is an server-side batteries-included secure networking
+framework built upon
+`asyncio <https://docs.python.org/3/library/asyncio.html>`__.
+
+| All code in ButterflyNet is designed to be asynchronous by default,
+  with special cases made for non-async code such as external libraries.
+| Because of the heavy usage of asyncio, this module does not officially
+  support Python versions before 3.4. It may be possible to run it with
+  a backported tulip library, but no official support will be given for
+  this.
+
+Why ButterflyNet?
+~~~~~~~~~~~~~~~~~
+
+ButterflyNet was designed for a few main reasons:
+
+-  Sockets are low-level; personally, I dislike them because they're not
+   the friendliest thing to use.
+-  Twisted has a steep learning curve, and has a lot of cruft.
+-  asyncio networking is good, but it's very hands-off - ButterflyNet
+   comes with everything needed for a full networking suite.
+
+All the existing libraries are not, by design, secure. You can work
+around this with SSLContexts - however, ButterflyNet forces TLS with
+secure settings on your server by default.
+
+Getting Started
+~~~~~~~~~~~~~~~
+
+ButterflyNet is available to download off of PyPI:
+``pip install ButterflyNet``
+
+| Documentation can be found `here <https://butterflynet.veriny.tf>`__.
+| Examples can be found `here </examples>`__.
+
+Limitations
+~~~~~~~~~~~
+
+You have a choice of TCP or UDP in most networking frameworks. However,
+ButterflyNet is designed upon persistent, TLS-authenticated and secured
+connections, of which UDP has:
+
+-  No persistent connections
+-  Unreliable TLS security
+
+This means ButterflyNet is locked to TCP for the future. OpenSSL *does*
+support DTLS, but no support for this is planned.
+
+.. |Build Status| image:: https://img.shields.io/circleci/project/SunDwarf/ButterflyNet.svg
+   :target: https://circleci.com/gh/SunDwarf/ConfigMaster/
+.. |PyPI version| image:: https://img.shields.io/pypi/v/ButterflyNet.svg
+   :target: https://pypi.python.org/pypi/ButterflyNet/
+.. |https://img.shields.io/pypi/l/ButterflyNet.svg| image:: https://img.shields.io/pypi/l/ButterflyNet.svg
+.. |https://img.shields.io/pypi/dw/ButterflyNet.svg| image:: https://img.shields.io/pypi/dw/ButterflyNet.svg
---
+++ packages/ButterflyNet/ButterflyNet-1.0.1.post1.tar.gz/ButterflyNet-1.0.1.post1/bfnet/packets/PacketButterfly.py
@@ -0,0 +1,86 @@
+import asyncio
+import struct
+from bfnet.Butterfly import AbstractButterfly
+
+
+class PacketButterfly(AbstractButterfly):
+    """
+    A packeted Butterfly uses a Queue of Packets instead of
+    a StreamReader/StreamWriter.
+    """
+    unpacker = struct.Struct("!2shh")
+
+    def __init__(self, handler, loop: asyncio.AbstractEventLoop, max_packets=0):
+        """
+        Create a new Packeted Butterfly.
+        """
+        # First, super call it.
+        super().__init__(handler, loop)
+
+        # Create a new Packet queue.
+        self.packet_queue = asyncio.Queue(loop=self._loop)
+
+    @property
+    def handler(self):
+        return self._handler
+
+    def data_received(self, data: bytes):
+        """
+        Parses out the Packet header, to create an appropriate new
+        Packet object.
+        :param data: The data to parse in.
+        """
+        self.logger.debug("Recieved new packet, deconstructing...")
+        if len(data) < 6:
+            self.logger.error("Invalid packet recieved, dropping client.")
+            self.stop()
+            return
+        # Decode the header.
+        header = data[0:6]
+        try:
+            magic, version, id = self.unpacker.unpack(header)
+        except struct.error as e:
+            self.logger.error("Failure unpacking packet header: {}".format(e.args))
+            self.stop()
+            return
+        # Check header.
+        if magic != b"BF":
+            self.logger.error("Recieved unknown packet with magic number {}".format(magic.decode()))
+            self.stop()
+            return
+        self.logger.debug("Packet version {}, id {}".format(version, id))
+        # Get the packet, if possible.
+        if id in self._handler.packet_types:
+            packet_type = self._handler.packet_types[id]
+            packet = packet_type(self)
+            created = packet.create(data[6:])
+            if created:
+                self._loop.create_task(self.packet_queue.put(packet))
+        else:
+            self.logger.warning("Recieved unknown packet ID: {}".format(id))
+
+    def connection_lost(self, exc):
+        class FakeQueue(object):
+            def get(self):
+                return None
+
+            @asyncio.coroutine
+            def put(self):
+                return
+
+        self.packet_queue = FakeQueue()
+        super().connection_lost(exc)
+
+    def read(self):
+        """
+        Get a new packet off the Queue.
+        """
+        return (yield from self.packet_queue.get())
+
+    def write(self, pack):
+        """
+        Write a packet to the client.
+        :param pack: The packet to write. This will automatically add a header.
+        """
+        header = self.unpacker.pack(b"BF", 1, pack.id)
+        self._transport.write(header + pack.gen())
