62164e2135d8eca58fbc31bb1f22730c4b015d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:51 +0000

    Add package nester_print_all/1.0.0/nester_print_all-1.0.0.zip

---
+++ packages/nester_print_all/nester_print_all-1.0.0.zip/nester_print_all-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name ='nester_print_all',
+        version='1.0.0',
+        py_modules=['nester'],
+        author='axi',
+        author_email='axi5278853@gmail.com',
+        url='http://www.baidu.com',
+        description='A simple printer of nested lists',
+        )
