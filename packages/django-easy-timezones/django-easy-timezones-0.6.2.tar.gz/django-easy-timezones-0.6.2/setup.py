4147ed359cc77994fece4c79b9baab4de8f6ddd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:27 +0000

    Add package django-easy-timezones/0.6.2/django-easy-timezones-0.6.2.tar.gz

---
+++ packages/django-easy-timezones/django-easy-timezones-0.6.2.tar.gz/django-easy-timezones-0.6.2/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup
+
+# Set external files
+try:
+    from pypandoc import convert
+    README = convert('README.md', 'rst')	 
+except ImportError:
+    README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    required = f.read().splitlines()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-easy-timezones',
+    version='0.6.2',
+    packages=['easy_timezones'],
+    install_requires=required,
+    include_package_data=True,
+    license='Apache License',
+    description='Easy timezones for Django (>=1.7) based on MaxMind GeoIP.',
+    long_description=README,
+    url='https://github.com/Miserlou/django-easy-timezones',
+    author='Rich Jones',
+    author_email='rich@openwatch.net',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
