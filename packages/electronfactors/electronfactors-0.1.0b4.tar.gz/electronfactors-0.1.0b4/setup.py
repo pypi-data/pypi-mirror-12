1ecbf051242807a7354054adfa4f2d2bf13eccb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:37 +0000

    Add package electronfactors/0.1.0b4/electronfactors-0.1.0b4.tar.gz

---
+++ packages/electronfactors/electronfactors-0.1.0b4.tar.gz/electronfactors-0.1.0b4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = "electronfactors",
+    version = "0.1.0-beta.4",
+    author = "Simon Biggs",
+    author_email = "mail@simonbiggs.net",
+    description = "Predict electron insert factors",
+    long_description = """Predict electron insert factors to an uncertainty approaching measurement using the measurements you have already taken.
+
+You can start on a given energy/applicator/SSD combination if you have at least eight data points.
+
+For more information visit the gihub repository, https://github.com/SimonBiggs/electronfactors
+    
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.""",
+    keywords = [],
+    packages = ["electronfactors"],
+    license='AGPL3+',
+    classifiers = [],
+)
