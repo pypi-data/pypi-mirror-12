185a7b81fe560462e902f610091dcac89f280014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:29 +0000

    Add package chineseregion/0.0.2/chineseregion-0.0.2.tar.gz

---
+++ packages/chineseregion/chineseregion-0.0.2.tar.gz/chineseregion-0.0.2/chineseregion/main.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+__author__ = 'xuanwo'
+
+'''
+chineseregion cli
+
+Usage:
+    chineseregion "your position"
+'''
+
+import os, sys
+import json
+import pkgutil
+import chineseregion
+
+data = json.loads(str(pkgutil.get_data(chineseregion.__package__, 'data.json'),'utf-8'))
+
+def find(str):
+    city = []
+    for i in sorted(data.keys()):
+        if str.find(data[i]) != -1:
+            city.append(data[i])
+    return city
+
+
+def main():
+    argv = sys.argv[1:]
+    for i in argv:
+        ans = find(i)
+        if len(ans) != 0:
+            print(ans)
+        else:
+            print("Not Found")
+
+
+if __name__ == "__main__":
+    main()
