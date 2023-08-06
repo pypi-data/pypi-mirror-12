8acee5adf100e115b91de31339cbb9e5116f307f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:22 +0000

    Add package mpip_nester/1.4.0/mpip_nester-1.4.0.tar.gz

---
+++ packages/mpip_nester/mpip_nester-1.4.0.tar.gz/mpip_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'mpip_nester',
+	version		= '1.4.0',
+	py_modules	= ['mpip_nester'],
+	author		= 'mpippins52',
+	author_email	= 'mpippins52@gmail.com',
+	url		= 'http://www.mpippins52.com',
+	description	= 'A simple printer of nested lists',
+     )
