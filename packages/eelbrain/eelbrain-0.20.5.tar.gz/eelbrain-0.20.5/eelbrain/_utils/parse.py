ecb4354834c08ea0a26256c21912aad9137277c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:34 +0000

    Add package eelbrain/0.20.5/eelbrain-0.20.5.tar.gz

---
+++ packages/eelbrain/eelbrain-0.20.5.tar.gz/eelbrain-0.20.5/eelbrain/_utils/parse.py
@@ -0,0 +1,25 @@
+# Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
+import parser
+
+
+def find_variables(expr):
+    """Find the variables participating in an expressions
+
+    Returns
+    -------
+    variables : tuple of str
+        Variables occurring in expr.
+    """
+    return _find_vars(parser.expr(expr).totuple())
+
+
+def _find_vars(st):
+    if isinstance(st, str):
+        return ()
+    elif st[0] == 318:
+        if st[1][0] == 1:
+            return st[1][1],
+        else:
+            return ()
+    else:
+        return sum((_find_vars(b) for b in st[1:]), ())
