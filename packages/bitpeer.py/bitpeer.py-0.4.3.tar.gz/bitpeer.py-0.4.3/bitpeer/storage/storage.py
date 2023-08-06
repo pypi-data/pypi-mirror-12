78f4436579389f6fa26f1b9067ea041afe8dcd95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:34 +0000

    Add package bitpeer.py/0.4.3/bitpeer.py-0.4.3.tar.gz

---
+++ packages/bitpeer.py/bitpeer.py-0.4.3.tar.gz/bitpeer.py-0.4.3/bitpeer/storage/storage.py
@@ -0,0 +1,14 @@
+class Storage:
+	def __init__ (self, f):
+		raise Exception ('This is an interface')
+
+	def __getitem__(self, key):
+		raise Exception ('This is an interface')
+		
+	def __setitem__(self, key, value):
+		raise Exception ('This is an interface')
+		
+	def __contains__(self, key):
+		raise Exception ('This is an interface')
+
+    
