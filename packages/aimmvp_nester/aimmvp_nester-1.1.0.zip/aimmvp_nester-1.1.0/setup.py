8994fcb1080d673d6148fcb6f94b8e60adca5294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:17 +0000

    Add package aimmvp_nester/1.1.0/aimmvp_nester-1.1.0.zip

---
+++ packages/aimmvp_nester/aimmvp_nester-1.1.0.zip/aimmvp_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'aimmvp_nester',
+    version = '1.1.0',
+    py_modules = ['aimmvp_nester'],
+    author = 'aimmvp',
+    author_email = 'abc@gmail.com',
+    url = 'http://www.domain.com',
+    description='a simple test',
+    )
