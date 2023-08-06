6236da0df265e0d6fb0876dd041ef0439f8f3ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:54 +0000

    Add package slackipycore/0.0.4/slackipycore-0.0.4.tar.gz

---
+++ packages/slackipycore/slackipycore-0.0.4.tar.gz/slackipycore-0.0.4/slackipycore/__init__.py
@@ -0,0 +1,4 @@
+from .main import invite, get_users, get_team_info
+from .slackipy_exceptions import (AlreadyInTeam, InvalidInviteeEmail,
+                                 InvalidAuthToken, AlreadyInvited,
+                                 APIRequestError)
