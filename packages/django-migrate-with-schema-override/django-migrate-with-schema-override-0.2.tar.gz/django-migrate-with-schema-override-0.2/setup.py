f2c1bec830824f2632ba1f4b7e02405d17bec797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:43 +0000

    Add package django-migrate-with-schema-override/0.2/django-migrate-with-schema-override-0.2.tar.gz

---
+++ packages/django-migrate-with-schema-override/django-migrate-with-schema-override-0.2.tar.gz/django-migrate-with-schema-override-0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+packages = [
+    'cursor_override',
+]
+
+setup(
+    name='django-migrate-with-schema-override',
+    version=0.2,
+    packages=packages,
+    include_package_data=True,
+    license='MIT',
+    description='Override migrate command not to trip on tables with schema name provided via db_tables.',
+    long_description=open('README.rst').read(),
+    author='Ariel Cercado',
+    author_email='ariel.cercado@gmail.com',
+    url='https://github.com/acercado/django-migrate-with-schema-override',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development',
+    ],
+)
