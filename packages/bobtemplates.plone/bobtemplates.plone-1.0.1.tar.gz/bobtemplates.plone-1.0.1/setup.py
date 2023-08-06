2dc3200dc13eb42088d1b1ed1e48c99022afce36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:46 +0000

    Add package bobtemplates.plone/1.0.1/bobtemplates.plone-1.0.1.tar.gz

---
+++ packages/bobtemplates.plone/bobtemplates.plone-1.0.1.tar.gz/bobtemplates.plone-1.0.1/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
+version = '1.0.1'
+
+long_description = (
+    open('README.rst').read() + '\n' +
+    'Contributors\n'
+    '============\n' + '\n' +
+    open('CONTRIBUTORS.rst').read() + '\n' +
+    open('CHANGES.rst').read() + '\n'
+)
+
+setup(
+    name='bobtemplates.plone',
+    version=version,
+    description="Templates for Plone projects.",
+    long_description=long_description,
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords='web plone zope skeleton project',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://github.com/plone/bobtemplates.plone',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'mr.bob',
+    ],
+    extras_require={
+        'test': [
+            'nose',
+            'nose-selecttests',
+            'scripttest',
+            'six',
+            'unittest2',
+        ]
+    },
+    entry_points={},
+)
