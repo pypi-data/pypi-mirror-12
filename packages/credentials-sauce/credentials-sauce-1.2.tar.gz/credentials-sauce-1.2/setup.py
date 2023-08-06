e04c0666452507a9e1745f70d0954b0e7817fa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:24 +0000

    Add package credentials-sauce/1.2/credentials-sauce-1.2.tar.gz

---
+++ packages/credentials-sauce/credentials-sauce-1.2.tar.gz/credentials-sauce-1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='credentials-sauce',
+    description='Credential Manager',
+    author='Mathieu Sabourin',
+    author_email='mathieu.c.sabourin@gmail.com',
+    url='https://github.com/OniOni/credentials',
+    packages=['credentials'],
+    version=1.2,
+    license='LGPLv3'
+)
