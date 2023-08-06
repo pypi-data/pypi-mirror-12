4650d45bd8808d00732af4f76ea59909ddf31e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:11 +0000

    Add package django-watchman/0.9.0a1/django_watchman-0.9.0a1-py2.py3-none-any.whl

---
+++ packages/django-watchman/django_watchman-0.9.0a1-py2.py3-none-any.whl/watchman/settings.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+
+# TODO: these should not be module level.
+WATCHMAN_ENABLE_PAID_CHECKS = getattr(settings, 'WATCHMAN_ENABLE_PAID_CHECKS', False)
+WATCHMAN_AUTH_DECORATOR = getattr(settings, 'WATCHMAN_AUTH_DECORATOR', 'watchman.decorators.token_required')
+WATCHMAN_TOKEN = getattr(settings, 'WATCHMAN_TOKEN', None)
+WATCHMAN_TOKEN_NAME = getattr(settings, 'WATCHMAN_TOKEN_NAME', 'watchman-token')
+WATCHMAN_ERROR_CODE = getattr(settings, 'WATCHMAN_ERROR_CODE', 500)
+
+DEFAULT_CHECKS = (
+    'watchman.checks.caches',
+    'watchman.checks.databases',
+    'watchman.checks.storage',
+)
+PAID_CHECKS = (
+    'watchman.checks.email',
+)
+
+if WATCHMAN_ENABLE_PAID_CHECKS:
+    DEFAULT_CHECKS = DEFAULT_CHECKS + PAID_CHECKS
+
+WATCHMAN_CHECKS = getattr(settings, 'WATCHMAN_CHECKS', DEFAULT_CHECKS)
