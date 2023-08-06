6481ca77d3532fe7869990751f7fec7bcfa1be1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:51 +0000

    Add package keepalive/0.1.1/keepalive-0.1.1.zip

---
+++ packages/keepalive/keepalive-0.1.1.zip/keepalive-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+      name = 'keepalive',
+      version = "0.1.1",
+      description = 'urllib keepalive support for python3',
+      long_description = 'An HTTP handler for `urllib2` that supports HTTP 1.1 and keepalive in Python 3.x.',
+      license = 'GNU GPL',
+      author = "Benjamin Woodruff",
+      author_email = "github@benjam.info",
+      maintainer = 'Sergio Fernandez',
+      maintainer_email = 'sergio@wikier.org',
+      url = 'https://github.com/wikier/keepalive',
+      download_url = 'https://github.com/wikier/keepalive/releases',
+      platforms = ['any'],
+      packages = ['keepalive'],
+      requires = [],
+      install_requires = [],
+      classifiers =  [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+      ],
+      keywords = 'python http urllib keepalive'
+)
