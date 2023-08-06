fce1953fd99ca79b17c278199ff14f5962cfd12c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:37 +0000

    Add package revscoring/0.7.3/revscoring-0.7.3-py2.py3-none-any.whl

---
+++ packages/revscoring/revscoring-0.7.3-py2.py3-none-any.whl/revscoring/languages/__init__.py
@@ -0,0 +1,44 @@
+"""
+This module implements a set of :class:`revscoring.Language`
+-- collections of features that are language specific.
+
+languages
++++++++++
+.. automodule:: revscoring.languages.dutch
+
+.. automodule:: revscoring.languages.english
+
+.. automodule:: revscoring.languages.estonian
+
+.. automodule:: revscoring.languages.french
+
+.. automodule:: revscoring.languages.german
+
+.. automodule:: revscoring.languages.hebrew
+
+.. automodule:: revscoring.languages.indonesian
+
+.. automodule:: revscoring.languages.italian
+
+.. automodule:: revscoring.languages.persian
+
+.. automodule:: revscoring.languages.portuguese
+
+.. automodule:: revscoring.languages.spanish
+
+.. automodule:: revscoring.languages.turkish
+
+.. automodule:: revscoring.languages.ukrainian
+
+.. automodule:: revscoring.languages.vietnamese
+
+Base classes
+++++++++++++
+.. automodule:: revscoring.languages.language
+
+.. automodule:: revscoring.languages.space_delimited
+
+"""
+from .language import Language
+
+__all__ = [Language]
