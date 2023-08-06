0cd7b0514563124558bc0f44be43cd0f07adb538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:07 +0000

    Add package honeyMilk/1.2.0/honeyMilk-1.2.0.zip

---
+++ packages/honeyMilk/honeyMilk-1.2.0.zip/honeyMilk-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'honeyMilk',
+    version      = '1.2.0',
+    py_modules   = ['nester'],
+    author       = 'joker',
+    author_email = 'leanjoker@126.com',
+    url          = 'http://www.headfirstlabs.com',
+    description  = 'A simple printer of nested lists',
+    )
