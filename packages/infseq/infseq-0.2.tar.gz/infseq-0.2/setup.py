aacb947400fb10fa59355b223d727b899e965740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:51 +0000

    Add package infseq/0.2/infseq-0.2.tar.gz

---
+++ packages/infseq/infseq-0.2.tar.gz/infseq-0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from os.path import dirname, join
+
+with open(join(dirname(__file__), 'README.rst')) as readme_file:
+    long_description = '\n' + readme_file.read()
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3 :: Only'
+]
+
+setup(name='infseq',
+      description='Lazy infinite cached sequences',
+      long_description=long_description,
+      author='Dmitry Shachnev',
+      author_email='mitya57@gmail.com',
+      version='0.2',
+      url='https://github.com/mitya57/infseq',
+      py_modules=['infseq'],
+      classifiers=classifiers,
+      platforms='any',
+      license='BSD')
