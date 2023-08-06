c3fa39191eb595087cbe171093c688fa520ec34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:03 +0000

    Add package cassiopeia/0.0.10/cassiopeia-0.0.10.tar.gz

---
+++ packages/cassiopeia/cassiopeia-0.0.10.tar.gz/cassiopeia-0.0.10/cassiopeia/type/api/exception.py
@@ -0,0 +1,13 @@
+class CassiopeiaException(Exception):
+    """Generic exception for a failure within Cassiopeia"""
+    pass
+
+class APIError(Exception):
+    """"
+    message       str    the error message
+    error_code    int    the HTTP error code that was received
+    """
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.message = message
+        self.error_code = error_code
