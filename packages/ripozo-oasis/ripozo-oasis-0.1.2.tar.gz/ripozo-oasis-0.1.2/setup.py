cdd16001c2ca22e8feb66b0f30afd8a34a89c2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:28 +0000

    Add package ripozo-oasis/0.1.2/ripozo-oasis-0.1.2.tar.gz

---
+++ packages/ripozo-oasis/ripozo-oasis-0.1.2.tar.gz/ripozo-oasis-0.1.2/setup.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+version = '0.1.2'
+
+setup(
+    author='Tim Martin',
+    author_email='tim.martin@vertical-knowledge.com',
+    entry_points={
+        'console_scripts': [
+            'ripozo-oasis = cli_commands:run_commands'
+        ]
+    },
+    extras_require={
+        'dev': [
+            'zest.releaser'
+        ]
+    },
+    install_requires=[
+        'click==5.1',
+        'Flask',
+        'ripozo>=1.2.0',
+        'flask-ripozo>=1.0.0',
+        'ripozo-sqlalchemy>=1.0.0',
+        'sqlalchemy>=1.0.0'
+    ],
+    name='ripozo-oasis',
+    packages=find_packages(include=['api_builder', 'cli_commands']),
+    version=version
+)
