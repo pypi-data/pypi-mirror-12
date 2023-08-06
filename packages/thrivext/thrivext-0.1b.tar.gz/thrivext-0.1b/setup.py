fb845ba93f8468952ca554855943fc719d4cee85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:14 +0000

    Add package thrivext/0.1b/thrivext-0.1b.tar.gz

---
+++ packages/thrivext/thrivext-0.1b.tar.gz/thrivext-0.1b/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = "thrivext",
+    packages = ["thrivext"],
+    version = "0.1b",
+    description = "Thrive extensions",
+    author = "Rohan Kekatpure",
+    author_email = "rohan_kekatpure@intuit.com",
+    url = "https://github.com/rohan-kekatpure/thrivext",
+    download_url = "https://github.com/rohan-kekatpure/thrivext/tarball/0.1",
+    keywords = ["pypi", "Thrive"],
+    classifiers = [],
+)
