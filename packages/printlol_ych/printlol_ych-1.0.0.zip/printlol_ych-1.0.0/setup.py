52eae1b1d38080c43c66002bb147dd7d1a05a8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:53 +0000

    Add package printlol_ych/1.0.0/printlol_ych-1.0.0.zip

---
+++ packages/printlol_ych/printlol_ych-1.0.0.zip/printlol_ych-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'printlol_ych',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'y_ch',
+    author_mail = 'ych01@163.com',
+    description = 'head first exercise chapter2 upload a simple printer',
+    )
