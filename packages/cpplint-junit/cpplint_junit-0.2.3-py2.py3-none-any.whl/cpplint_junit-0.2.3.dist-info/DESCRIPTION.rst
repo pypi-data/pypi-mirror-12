5c1e807ef8f3c2cc665c1abbb81bef6e43f4af90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:06 +0000

    Add package cpplint-junit/0.2.3/cpplint_junit-0.2.3-py2.py3-none-any.whl

---
+++ packages/cpplint-junit/cpplint_junit-0.2.3-py2.py3-none-any.whl/cpplint_junit-0.2.3.dist-info/DESCRIPTION.rst
@@ -0,0 +1,57 @@
+cpplint JUnit Converter
+=======================
+
+.. image:: https://badge.fury.io/py/cpplint-junit.png
+    :target: http://badge.fury.io/py/cpplint-junit
+
+Tool that converts ``cpplint`` output to JUnit XML format.  Use on your CI servers to get more
+helpful feedback.
+
+Installation
+------------
+
+You can install, upgrade, and uninstall ``cpplint-junit`` with these commands:
+
+.. code:: shell-session
+
+    $ pip install cpplint-junit
+    $ pip install --upgrade cpplint-junit
+    $ pip uninstall cpplint-junit
+
+Usage
+-----
+Redirect ``cpplint`` ``stderr`` to a file:
+
+.. code:: shell-session
+
+    $ cpplint main.cpp 2> cpplint.txt
+
+Convert it to JUnit XML format:
+
+.. code:: shell-session
+
+    $ cpplint_junit cpplint.txt cpplint_junit.xml
+
+Releases
+--------
+
+0.2.3 - 2015-11-14
+^^^^^^^^^^^^^^^^^^
+
+Use "error" consistantly rather than "failure" to refer to results from ``cpplint``.
+
+0.2.2 - 2015-11-14
+^^^^^^^^^^^^^^^^^^
+
+Mis-release.
+
+0.2.1 - 2015-10-24
+^^^^^^^^^^^^^^^^^^
+
+Added console entry point.
+
+0.1.0 - 2015-10-24
+^^^^^^^^^^^^^^^^^^
+
+First release.
+
---
+++ packages/cpplint-junit/cpplint_junit-0.2.3-py2.py3-none-any.whl/cpplint_junit.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python3
+
+"""Converts cpplint output to JUnit XML format."""
+
+import argparse
+import collections
+import os
+import re
+import sys
+from xml.etree import ElementTree
+
+__version__ = '0.2.3'
+
+EXIT_SUCCESS = 0
+EXIT_FAILURE = -1
+
+
+class CpplintError(object):
+    def __init__(self, file, line, message):
+        self.file = file
+        self.line = line
+        self.message = message
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Converts cpplint output to JUnit XML format.')
+    parser.add_argument('input_file', type=str, help='cpplint stdout text file.')
+    parser.add_argument('output_file', type=str, help='JUnit XML output file.')
+    return parser.parse_args()
+
+
+def parse_cpplint(file_name):
+    """Parses a cpplint output file.
+
+    Args:
+        file_name (str): cpplint output file.
+
+    Returns:
+        Dict[str, List[CpplintError]]: Parsed failures grouped by file name.
+
+    Raises:
+        FileNotFoundError: File does not exist.
+    """
+    lines = open(file_name, mode='rt').readlines()
+
+    errors = collections.defaultdict(list)
+    for line in lines:
+        line = line.rstrip()
+
+        match = re.search(r'(\S+):(\d+):\s+(.+)', line)
+        if match is not None:
+            error = CpplintError(file=match.group(1),
+                                 line=int(match.group(2)),
+                                 message=match.group(3))
+            errors[error.file].append(error)
+    return errors
+
+
+def generate_test_suite(errors, output_file):
+    """Writes a JUnit test file from parsed cpplint failures.
+
+    Args:
+        errors (Dict[str, List[CpplintError]]): Parsed cpplint failures.
+        output_file (str): File path to JUnit XML output.
+
+    Returns:
+        Nothing.
+    """
+    test_suite = ElementTree.Element('testsuite')
+    test_suite.attrib['errors'] = str(len(errors))
+    test_suite.attrib['failures'] = str(0)
+    test_suite.attrib['name'] = 'cpplint errors'
+    test_suite.attrib['tests'] = str(len(errors))
+    test_suite.attrib['time'] = str(1)
+
+    for file_name, errors in errors.items():
+        test_case = ElementTree.SubElement(test_suite,
+                                           'testcase',
+                                           name=os.path.relpath(file_name))
+        for error in errors:
+            ElementTree.SubElement(test_case,
+                                   'error',
+                                   file=os.path.relpath(error.file),
+                                   line=str(error.line),
+                                   message='{}: {}'.format(error.line, error.message))
+
+    tree = ElementTree.ElementTree(test_suite)
+    tree.write(output_file, encoding='utf-8', xml_declaration=True)
+
+
+def main():
+    """Main function.
+
+    Returns:
+        int: Exit code.
+    """
+    args = parse_arguments()
+
+    try:
+        errors = parse_cpplint(args.input_file)
+    except FileNotFoundError as e:
+        print(str(e))
+        return EXIT_FAILURE
+
+    if len(errors) > 0:
+        generate_test_suite(errors, args.output_file)
+
+    return EXIT_SUCCESS
+
+if __name__ == '__main__':
+    sys.exit(main())
