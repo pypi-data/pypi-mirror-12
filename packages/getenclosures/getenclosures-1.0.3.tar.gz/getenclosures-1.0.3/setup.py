ec2fac8c9f535f6aa2773e7ddef2dc97d5dcce98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:53 +0000

    Add package getenclosures/1.0.3/getenclosures-1.0.3.tar.gz

---
+++ packages/getenclosures/getenclosures-1.0.3.tar.gz/getenclosures-1.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.rst", "rb") as f:
+    long_descr = f.read().decode('utf-8')
+
+setup(
+    name = "getenclosures",
+    packages = ["getenclosures"],
+    install_requires = ['requests', 'beautifulsoup4', 'appdirs', 'lxml'],
+    entry_points = {
+        "console_scripts": ['getenclosures = getenclosures.getenclosures:main']
+        },
+    version = "1.0.3",
+    description = "Gets all enclosure urls from an RSS feed and pipes them to stdout",
+    long_description = long_descr,
+    author = "Steven Smith",
+    author_email = "stevensmith.ome@gmail.com",
+    license = "MIT",
+    url = "https://github.com/blha303/getenclosures/",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators"
+        ]
+    )
