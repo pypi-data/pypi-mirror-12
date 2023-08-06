dc5760e42933067f5a15ae6418e30447605956fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:19 +0000

    Add package django-pyfs/1.0.3/django-pyfs-1.0.3.tar.gz

---
+++ packages/django-pyfs/django-pyfs-1.0.3.tar.gz/django-pyfs-1.0.3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-pyfs',
+    version='1.0.3',
+    description='Django pyfilesystem integration',
+    author='Piotr Mitros',
+    author_email='pmitros@edx.org',
+    packages=['djpyfs'],
+    license = "AGPLv3",
+    url = "https://github.com/edx/django-pyfs",
+    long_description = read("README.md"),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+    ],
+)
