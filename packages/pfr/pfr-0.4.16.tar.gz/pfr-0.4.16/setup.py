98801dd363907628a5e67ce792f5b6b45a771aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:19 +0000

    Add package pfr/0.4.16/pfr-0.4.16.tar.gz

---
+++ packages/pfr/pfr-0.4.16.tar.gz/pfr-0.4.16/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='pfr',
+      version='0.4.16',
+      description='Scraping data from pro-football-reference.com',
+      url='https://github.com/mdgoldberg/pfr',
+      author='Matt Goldberg',
+      author_email='mgoldberg@college.harvard.edu',
+      license='Apache License 2.0',
+      packages=find_packages(),
+      install_requires=[
+          'pandas',
+          'numpy',
+          'requests',
+          'pyquery',
+          'appdirs',
+      ]
+      )
