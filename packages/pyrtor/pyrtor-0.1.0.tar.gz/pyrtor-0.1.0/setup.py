03e989cc0ff6bb329ef18d8900d92039b4ca7d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:48 +0000

    Add package pyrtor/0.1.0/pyrtor-0.1.0.tar.gz

---
+++ packages/pyrtor/pyrtor-0.1.0.tar.gz/pyrtor-0.1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name="pyrtor",
+      version="0.1.0",
+      description="Library for interfacing with the rtorrent XMLRPC interface",
+      author="Seth Curry",
+      author_email="seth@scurry.io",
+      py_modules=["pyrtor"]
+    )
