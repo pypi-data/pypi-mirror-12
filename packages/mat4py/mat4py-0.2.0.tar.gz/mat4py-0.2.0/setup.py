855ab2434127dbe6a1adf19752d11c7029c35fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:03 +0000

    Add package mat4py/0.2.0/mat4py-0.2.0.tar.gz

---
+++ packages/mat4py/mat4py-0.2.0.tar.gz/mat4py-0.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='mat4py',
+    version='0.2.0',
+    author='Jacob Sondergaard',
+    author_email='jacob@nephics.com',
+    packages=['mat4py'],
+    url='https://bitbucket.org/nephics/mat4py/',
+    license='MIT License',
+    description='Load and save data in the Matlab (TM) MAT-file format.',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering'
+    ]
+)
