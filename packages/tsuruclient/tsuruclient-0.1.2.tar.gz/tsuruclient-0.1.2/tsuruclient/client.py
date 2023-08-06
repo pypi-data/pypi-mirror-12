4d6e6cca1f9b60a1c656ce7b779d82aead27f3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:16 +0000

    Add package tsuruclient/0.1.2/tsuruclient-0.1.2.tar.gz

---
+++ packages/tsuruclient/tsuruclient-0.1.2.tar.gz/tsuruclient-0.1.2/tsuruclient/client.py
@@ -0,0 +1,8 @@
+from tsuruclient import apps, nodes, templates
+
+
+class Client(object):
+    def __init__(self, target, token):
+        self.apps = apps.Manager(target, token)
+        self.nodes = nodes.Manager(target, token)
+        self.templates = templates.Manager(target, token)
