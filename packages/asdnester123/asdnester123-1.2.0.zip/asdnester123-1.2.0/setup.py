ffdfed3066db3e0ffa2f65b959bb6f29ddb576ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:06 +0000

    Add package asdnester123/1.2.0/asdnester123-1.2.0.zip

---
+++ packages/asdnester123/asdnester123-1.2.0.zip/asdnester123-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+             name   = 'asdnester123',
+            version = '1.2.0',
+         py_modules = ['asdnester123'],
+         author     = 'hfpython',
+       author_email = 'hypython@headfirstlabs.com',
+             url    = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+     )
+             
