f58dffe8211d3c6b5d15563760c2786d1221f349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:11 +0000

    Add package pk-client-python/0.5/pk-client-python-0.5.tar.gz

---
+++ packages/pk-client-python/pk-client-python-0.5.tar.gz/pk-client-python-0.5/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Python client API for the pk port knocking server daemon',
+    'author': 'Anson Rosenthal',
+    'url': 'https://github.com/anrosent/pk-client-python',
+    'author_email': 'anson.rosenthal@gmail.com',
+    'version': '0.5',
+    'install_requires': ['nose'],
+    'packages': ['pk_client'],
+    'scripts': [],
+    'name': 'pk-client-python'
+}
+
+setup(**config)
