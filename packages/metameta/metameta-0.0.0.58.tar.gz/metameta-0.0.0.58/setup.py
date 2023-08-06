3d0a6480f8f92d607826d6fa6ba07f9316f7b01e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:24 +0000

    Add package metameta/0.0.0.58/metameta-0.0.0.58.tar.gz

---
+++ packages/metameta/metameta-0.0.0.58.tar.gz/metameta-0.0.0.58/setup.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name='metameta',
+      version='0.0.0.58',
+      description='Toolkit for analyzing '
+                  + 'meta-transcriptome/metagenome mapping data',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Scientific/Engineering :: Bio-Informatics'
+      ],
+      keywords='bioinformatics metadata metagenome metatranscriptome'
+               + 'short reads mapping alignment',
+      url='https://github.com/Brazelton-Lab/metameta/',
+      download_url='https://github.com/Brazelton-Lab/metameta/tarball/'
+                   + '0.0.0.58',
+      author='Alex Hyer',
+      author_email='theonehyer@gmail.com',
+      license='GPL',
+      packages=['metameta', 'metameta.bin', 'metameta.metameta_utils',
+                'metameta.data'],
+      include_package_data=True,
+      package_data={
+          'data': ['Documentation.txt'],
+      },
+      zip_safe=False,
+      install_requires=[
+          'bio_utils',
+          'pysam',
+          'screed',
+          'statistics'
+      ],
+      entry_points={
+          'console_scripts': ['metameta = metameta.bin.__main__:main']
+      },
+      )
