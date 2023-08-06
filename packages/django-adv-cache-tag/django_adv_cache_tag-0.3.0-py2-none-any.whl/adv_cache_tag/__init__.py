fc848ba2bdefed98e46f8b200d17f0fba2030671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:32 +0000

    Add package django-adv-cache-tag/0.3.0/django_adv_cache_tag-0.3.0-py2-none-any.whl

---
+++ packages/django-adv-cache-tag/django_adv_cache_tag-0.3.0-py2-none-any.whl/adv_cache_tag/__init__.py
@@ -0,0 +1,7 @@
+"""An advanced template tag for caching in django: versioning, compress, partial caching, easy inheritance"""
+VERSION = (0, 3, 0)
+
+__author__ = 'Stephane "Twidi" Angel'
+__contact__ = "s.angel@twidid.com"
+__homepage__ = "https://github.com/twidi/django-adv-cache-tag"
+__version__ = ".".join(map(str, VERSION))
