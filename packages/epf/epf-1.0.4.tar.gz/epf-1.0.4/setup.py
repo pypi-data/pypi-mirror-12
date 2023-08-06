088cd1003b05407cd051396557bce3876e031949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:55 +0000

    Add package epf/1.0.4/epf-1.0.4.tar.gz

---
+++ packages/epf/epf-1.0.4.tar.gz/epf-1.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+v = '1.0.4'
+
+setup(
+    name='epf',
+    version=v,
+    description='Apple EPF crawler, downloader and parser',
+    author='Andrei Fokau',
+    author_email='andrei@5monkeys.se',
+    url='https://github.com/andreif/epf',
+    download_url='https://github.com/andreif/epf/tarball/' + v,
+    license='MIT',
+    packages=['epf'],
+    install_requires=['requests'],
+)
