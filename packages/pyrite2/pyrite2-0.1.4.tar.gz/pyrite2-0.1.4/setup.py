93e1b7de7cacb1d27309c78dcab7b2232d9ec518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:03 +0000

    Add package pyrite2/0.1.4/pyrite2-0.1.4.tar.gz

---
+++ packages/pyrite2/pyrite2-0.1.4.tar.gz/pyrite2-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='pyrite2',
+    version='0.1.4',
+    author='StartupML',
+    author_email='team@startup.ml',
+    license='LICENSE.txt',
+    url='http://pyrite.startup.ml',
+    install_requires=['scikit-learn','numpy','scipy','pandas','astroML','wget','astroML_addons','matplotlib'],
+    description='Anomaly Detection in High Dimensional Heterogeneous Datasets',
+    packages=find_packages())
