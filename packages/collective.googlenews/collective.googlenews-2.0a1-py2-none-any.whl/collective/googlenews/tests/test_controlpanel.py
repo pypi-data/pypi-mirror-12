4c87c74aa7063216969821341d70a0594585db80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:24 +0000

    Add package collective.googlenews/2.0a1/collective.googlenews-2.0a1-py2-none-any.whl

---
+++ packages/collective.googlenews/collective.googlenews-2.0a1-py2-none-any.whl/collective/googlenews/tests/test_controlpanel.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+from plone import api
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import logout
+from plone.app.testing import setRoles
+from plone.registry.interfaces import IRegistry
+
+from collective.googlenews.config import PROJECTNAME
+from collective.googlenews.interfaces import GoogleNewsSettings
+from collective.googlenews.testing import INTEGRATION_TESTING
+
+
+class ControlPanelTestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.controlpanel = self.portal['portal_controlpanel']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_controlpanel_view(self):
+        view = getMultiAdapter((self.portal, self.portal.REQUEST),
+                               name='googlenews-controlpanel')
+        view = view.__of__(self.portal)
+        self.assertTrue(view())
+
+    def test_controlpanel_view_protected(self):
+        from AccessControl import Unauthorized
+        logout()
+        self.assertRaises(Unauthorized, self.portal.restrictedTraverse,
+                          'googlenews-controlpanel')
+
+    def test_controlpanel_installed(self):
+        actions = [a.getAction(self)['id']
+                   for a in self.controlpanel.listActions()]
+        self.assertTrue('collective.googlenews.settings' in actions)
+
+    def test_controlpanel_removed_on_uninstall(self):
+        qi = self.portal['portal_quickinstaller']
+        qi.uninstallProducts(products=[PROJECTNAME])
+        actions = [a.getAction(self)['id']
+                   for a in self.controlpanel.listActions()]
+        self.assertTrue('collective.googlenews.settings' not in actions)
+
+
+class RegistryTestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.registry = getUtility(IRegistry)
+        self.settings = self.registry.forInterface(GoogleNewsSettings)
+
+    def test_record_portal_types(self):
+        self.assertTrue(hasattr(self.settings, 'portal_types'))
+        self.assertListEqual(self.settings.portal_types, ['News Item'])
+
+    def test_logo_record_in_registry(self):
+        self.assertTrue(hasattr(self.settings, 'logo'))
+        self.assertIsNone(self.settings.logo)
+
+    def test_records_removed_on_uninstall(self):
+        qi = self.portal['portal_quickinstaller']
+        with api.env.adopt_roles(['Manager']):
+            qi.uninstallProducts(products=[PROJECTNAME])
+
+        BASE_REGISTRY = 'collective.googlenews.interfaces.GoogleNewsSettings.'
+        records = [
+            BASE_REGISTRY + 'portal_types',
+            BASE_REGISTRY + 'logo',
+        ]
+
+        for r in records:
+            self.assertNotIn(r, self.registry)
---
+++ packages/collective.googlenews/collective.googlenews-2.0a1-py2-none-any.whl/collective/googlenews/tests/test_sitemap.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from collective.googlenews.testing import INTEGRATION_TESTING
+from DateTime import DateTime
+from plone import api
+
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        request = self.layer['request']
+        self.view = api.content.get_view(
+            name='googlenews-sitemap.xml', context=self.portal, request=request)
+
+        with api.env.adopt_roles(['Manager']):
+            self.n1 = api.content.create(
+                type='News Item',
+                container=self.portal,
+                id='n1',
+                title='News Item 1',
+            )
+            self.n1.setEffectiveDate(DateTime())
+            self.n1.reindexObject()
+
+            self.n2 = api.content.create(
+                type='News Item',
+                container=self.portal,
+                id='n2',
+                title='News Item 2',
+            )
+            self.n2.setEffectiveDate(DateTime())
+            self.n2.reindexObject()
+
+            self.n3 = api.content.create(
+                type='News Item',
+                container=self.portal,
+                id='n3',
+                title='News Item 3',
+            )
+            self.n3.setEffectiveDate(DateTime() - 3)
+            self.n3.reindexObject()
+
+    def test_news(self):
+        expected = [
+            dict(
+                loc='http://nohost/plone/n2',
+                publication_date=self.n2.effective_date.ISO(),
+                title='News Item 2',
+            ),
+            dict(
+                loc='http://nohost/plone/n1',
+                publication_date=self.n1.effective_date.ISO(),
+                title='News Item 1',
+            ),
+        ]
+        self.assertEqual(self.view.news(), expected)
+
+    def test_portaltitle(self):
+        self.assertEqual(self.view.portal_title(), u'Plone site')
+
+    def test_portallanguage(self):
+        self.assertEqual(self.view.portal_language(), 'en')
+
+    def test_view(self):
+        render = self.view()
+        self.assertIn(u'<news:title>News Item 1</news:title>', render)
+        self.assertIn(u'<news:title>News Item 2</news:title>', render)
+        self.assertNotIn(u'<news:title>News Item 3</news:title>', render)
