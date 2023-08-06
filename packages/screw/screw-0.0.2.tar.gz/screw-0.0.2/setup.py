8da4dc65e4c73dc63860fe0e2c51fb63c2bcf740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:30 +0000

    Add package screw/0.0.2/screw-0.0.2.tar.gz

---
+++ packages/screw/screw-0.0.2.tar.gz/screw-0.0.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from setuptools import setup, find_packages
+
+setup(name="screw",
+      version="0.0.2",
+      keywords=('screw', 'egg'),
+      description='screw egg',
+      license='MIT License',
+
+      url="http://yinwentao.com/",
+      author="yoon",
+      author_email="yinwentao312@gmail.com",
+
+      packages=find_packages(),
+      include_package_data=True,
+      platforms='any',
+      install_requires=[],
+      entry_points={'console_scripts': ['screw=screw.shell:main']}
+      )
+
