0034315353ab5a1f7fdf699bb09da594ff6d770a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:25 +0000

    Add package v_learn_nester/1.4.0/v_learn_nester-1.4.0.zip

---
+++ packages/v_learn_nester/v_learn_nester-1.4.0.zip/v_learn_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'v_learn_nester',
+		version = '1.4.0',
+		py_modules = ['nester'],
+		author = 'v',
+		author_email = 'v@v.com',
+		url = 'http://www.v.com',
+		description = 'A simple printer of nested lists',
+)
