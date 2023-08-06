c02a6ceea34d6217fde0046c702c9fe5d02da4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:03 +0000

    Add package cassiopeia/0.0.8/cassiopeia-0.0.8.tar.gz

---
+++ packages/cassiopeia/cassiopeia-0.0.8.tar.gz/cassiopeia-0.0.8/cassiopeia/baseriotapi.py
@@ -0,0 +1,77 @@
+"""
+Accesses the LoL REST API (https://developer.riotgames.com/), returning Python objects matching the exact API spec.
+"""
+
+import urllib.request
+
+import cassiopeia.dto.requests
+import cassiopeia.type.api.rates
+import cassiopeia.dto.staticdataapi
+from cassiopeia.dto.championapi import *
+from cassiopeia.dto.currentgameapi import *
+from cassiopeia.dto.featuredgamesapi import *
+from cassiopeia.dto.gameapi import *
+from cassiopeia.dto.leagueapi import *
+from cassiopeia.dto.staticdataapi import *
+from cassiopeia.dto.statusapi import *
+from cassiopeia.dto.matchapi import *
+from cassiopeia.dto.matchlistapi import *
+from cassiopeia.dto.statsapi import *
+from cassiopeia.dto.summonerapi import *
+from cassiopeia.dto.teamapi import *
+
+def set_api_key(key):
+    """Set your API key
+
+    key    str    the key to use
+    """
+    cassiopeia.dto.requests.api_key = key
+
+def set_region(region):
+    """Set the region to run API queries against
+
+    region    str    the region to query against
+    """
+    cassiopeia.dto.requests.region = region
+
+def print_calls(on):
+    """Sets whether to print calls to stdout as they are made
+
+    on    bool    the region to query against
+    """
+    cassiopeia.dto.requests.print_calls = on
+
+def set_rate_limit(calls_per_epoch, seconds_per_epoch):
+    """Sets the rate limit for cassiopeia to manage internally
+
+    calls_per_epoch      int    the number of calls allowed in each epoch
+    seconds_per_epoch    int    the number of seconds per epoch
+    """
+    cassiopeia.dto.requests.rate_limiter = cassiopeia.type.api.rates.SingleRateLimiter(calls_per_epoch, seconds_per_epoch)
+
+def set_rate_limits(*limits):
+    """Sets the rate limits for cassiopeia to manage internally
+
+    *limits    tuple...    the rate limits to apply. Rate limits are of the form (calls_per_epoch, seconds_per_epoch)
+    """
+    cassiopeia.dto.requests.rate_limiter = cassiopeia.type.api.rates.MultiRateLimiter(*limits)
+
+def set_proxy(url, port=80):
+    """Sets a proxy server to tunnel requests to the Riot API through
+
+    url     str    the URL of the proxy server, without port number or protocol
+    port    int    the port number to conntect to (default 80)
+    """
+    if(url):
+        cassiopeia.dto.requests.proxy = urllib.request.ProxyHandler({"https": "https://{url}:{port}".format(url=url, port=port)})
+        urllib.request.install_opener(urllib.request.build_opener(cassiopeia.dto.requests.proxy))
+    else:
+        cassiopeia.dto.requests.proxy = urllib.request.ProxyHandler({})
+        urllib.request.install_opener(urllib.request.build_opener(cassiopeia.dto.requests.proxy))
+
+def set_locale(locale):
+    """Sets the locale (language) to use for calls to the Riot API. Use get_languages() to find valid locales.
+
+    locale    str    the locale to use for calls to the API
+    """
+    cassiopeia.dto.staticdataapi._locale = locale
---
+++ packages/cassiopeia/cassiopeia-0.0.8.tar.gz/cassiopeia-0.0.8/cassiopeia/dto/requests.py
@@ -0,0 +1,94 @@
+"""
+Handles making HTTP requests to the REST API and converting the results into a usable format
+"""
+
+import urllib.parse
+import urllib.request
+import urllib.error
+import json
+
+import cassiopeia.type.api.exception
+import cassiopeia.type.api.rates
+
+api_versions = {
+    "champion": "v1.2",
+    "currentgame": "v1.0",
+    "featuredgames": "v1.0",
+    "game": "v1.3",
+    "league": "v2.5",
+    "staticdata": "v1.2",
+    "status": "v1.0",
+    "match": "v2.2",
+    "matchlist": "v2.2",
+    "stats": "v1.3",
+    "summoner": "v1.4",
+    "team": "v2.4"
+}
+
+api_key = ""
+region = ""
+print_calls = False
+rate_limiter = cassiopeia.type.api.rates.MultiRateLimiter((10, 10), (500, 600))
+
+def get(request, params={}, static=False, include_base=True):
+    """Makes a rate-limited HTTP request to the Riot API and returns the result
+
+    request         str               the request string
+    params          dict<str, any>    the parameters to send with the request (default {})
+    static          bool              whether this is a call to a static (non-rate-limited) API
+    include_base    bool              whether to prepend https://{server}.api.pvp.net/api/lol/{region}/ to the request
+
+    return          dict              the JSON response from the Riot API as a dict
+    """
+    if(not api_key):
+        raise cassiopeia.type.api.exception.CassiopeiaException("API Key must be set before the API can be queried.")
+    if(not region):
+        raise cassiopeia.type.api.exception.CassiopeiaException("Region must be set before the API can be queried.")
+
+    # Set server and rgn
+    server = "global" if static else region.lower()
+    rgn = ("static-data/{region}" if static else "{region}").format(region=region.lower())
+
+    # Encode params
+    params["api_key"] = api_key
+    encoded_params = urllib.parse.urlencode(params)
+
+    # Build and execute request
+    if(include_base):
+        url = "https://{server}.api.pvp.net/api/lol/{region}/{request}?{params}".format(server=server, region=rgn, request=request, params=encoded_params)
+    else:
+        url = "{request}?{params}".format(request=request, params=encoded_params)
+
+    try:
+        content = rate_limiter.call(executeRequest, url) if rate_limiter else executeRequest(url)
+        return json.loads(content)
+    except urllib.error.HTTPError as e:
+        # Reset rate limiter and retry on 429 (rate limit exceeded)
+        if(e.code == 429 and rate_limiter):
+            retry_after = 1
+            if(e.headers["Retry-After"]):
+                retry_after += int(e.headers["Retry-After"])
+                
+            rate_limiter.reset_in(retry_after)
+            return get(request, params, static)
+        else:
+            raise cassiopeia.type.api.exception.APIError("Server returned error {code} on call: {url}".format(code=e.code, url=url), e.code)
+
+def executeRequest(url):
+    """Executes an HTTP GET request and returns the result in a string
+
+    url       str    the full URL to send a GET request to
+
+    return    str    the content returned by the server
+    """
+    if(print_calls):
+        print(url)
+
+    response = None
+    try:
+        response = urllib.request.urlopen(url)
+        content = response.read().decode(encoding="UTF-8")
+        return content
+    finally:
+        if(response): 
+            response.close()
