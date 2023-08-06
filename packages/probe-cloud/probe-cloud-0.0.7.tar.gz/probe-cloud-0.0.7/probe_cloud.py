b0937a1167df0b76b0d3799efccc0ab05728bcac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:57 +0000

    Add package probe-cloud/0.0.7/probe-cloud-0.0.7.tar.gz

---
+++ packages/probe-cloud/probe-cloud-0.0.7.tar.gz/probe-cloud-0.0.7/probe_cloud.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+from cleo import Command, InputArgument, InputOption
+from cleo import Application
+from probe.cloud import Cloud
+import logging
+import probe.log
+import probe.rdirwatcher
+import probe.cloud
+import pkg_resources
+import shutil
+import os
+
+class CheckCommand(Command):
+    name = 'cloud:check'
+
+    description = 'returns all compures where is AD running right now'
+
+    options = [
+        {
+            'name': 'config',
+            'description': 'path to config file',
+            'value_required': True,
+            'default': 'cloud.cfg',
+        }
+    ]
+
+    @staticmethod
+    def execute(i, o):
+
+
+        rdirwatcher_cfg = i.get_option('config')
+        cloud = Cloud(rdirwatcher_cfg)
+        cloud.check()
+
+class RunSimulationCommand(Command):
+    name = 'cloud:run_simulation'
+    description = 'run simulation on cloud'
+
+    options = [
+        {
+            'name': 'config',
+            'description': 'path to config file',
+            'value_required': True,
+            'default': 'cloud.cfg',
+        }
+    ]
+
+    @staticmethod
+    def execute(i, o):
+        rdirwatcher_cfg = i.get_option('config')
+        cloud = Cloud(rdirwatcher_cfg)
+        cloud.run_simulation()
+
+class GetCfgCommand(Command):
+    name = 'cloud:get_cfg'
+    description = 'get cloud config file'
+
+    arguments = [
+        {
+            'name': 'dest',
+            'description': 'relative path where to put config file',
+            'required': False,
+            'default': '.',
+        }
+    ]
+
+    @staticmethod
+    def execute(i, o):
+        dest = i.get_argument('dest')
+        path_cfg = pkg_resources.resource_filename('probe.cloud', 'cfg/cloud.cfg')
+        shutil.copyfile(path_cfg, os.path.join(dest, 'cloud.cfg'))
+
+class GetFabfileCommand(Command):
+    name = 'cloud:get_fabfile'
+    description = 'get cloud fabfile'
+
+    arguments = [
+        {
+            'name': 'dest',
+            'description': 'relative path where to put fabfile',
+            'required': False,
+            'default': '.',
+        }
+    ]
+
+    @staticmethod
+    def execute(i, o):
+        dest = i.get_argument('dest')
+        path_cfg = pkg_resources.resource_filename('probe.cloud', 'fabfile.py')
+        shutil.copyfile(path_cfg, os.path.join(dest, 'fabfile.py'))
+
+if __name__ == '__main__':
+
+    logging.getLogger('paramiko').setLevel(logging.WARNING)
+    logger = probe.log.get_logger('rdirwatcher')
+    logger.setLevel(logging.DEBUG)
+    probe.rdirwatcher.rdirwatcher.logger = logger
+    probe.cloud.cloud.logger = logger
+
+    application = Application()
+    application.add(CheckCommand())
+    application.add(RunSimulationCommand())
+    application.add(GetCfgCommand())
+    application.add(GetFabfileCommand())
+    application.run()
