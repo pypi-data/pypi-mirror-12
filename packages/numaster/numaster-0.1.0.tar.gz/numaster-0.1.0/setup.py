65f561695273ebde3d38f2740249cf2ee08db695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:48 +0000

    Add package numaster/0.1.0/numaster-0.1.0.tar.gz

---
+++ packages/numaster/numaster-0.1.0.tar.gz/numaster-0.1.0/setup.py
@@ -0,0 +1,52 @@
+import setuptools
+
+
+install_requires = [
+    'mypy-lang',
+]
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('CHANGES.rst') as f:
+    changes = f.read()
+
+
+setuptools.setup(
+    name='numaster',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    description="Number normalizer",
+    long_description='\n'.join([readme, changes]),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+    ],
+    license='GPLv3',
+    author='Motoki Naruse',
+    author_email='motoki@naru.se',
+    url='https://github.com/yparrot/numaster',
+    keywords='number normalize extract convert',
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'test': [
+            'pytest>=2.8.2',
+            'pytest-xdist>=1.13.1',
+            'pytest-cov>=2.2.0',
+            'flake8>=2.5.0',
+        ],
+    }
+)
