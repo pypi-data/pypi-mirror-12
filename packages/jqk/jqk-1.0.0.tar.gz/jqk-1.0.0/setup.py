aca90609035c936efd8b8c88b7f6e2c1f695d904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:16 +0000

    Add package jqk/1.0.0/jqk-1.0.0.tar.gz

---
+++ packages/jqk/jqk-1.0.0.tar.gz/jqk-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name       ='jqk',
+        version    ='1.0.0',
+        py_modules = ['jqk'],
+        author    ='jqk',
+        author_email='kangqingjie01@gmail.com',
+        url         ='http://wwww.headfirstlabs.com',
+        description ='A simple printer of nested lists',
+        )
