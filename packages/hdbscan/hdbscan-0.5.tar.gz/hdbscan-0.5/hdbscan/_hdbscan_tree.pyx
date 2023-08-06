b1c2133a36c16340679a7b53a280c1f3dd55e442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:55 +0000

    Add package hdbscan/0.5/hdbscan-0.5.tar.gz

---
+++ packages/hdbscan/hdbscan-0.5.tar.gz/hdbscan-0.5/hdbscan/_hdbscan_tree.pyx
@@ -0,0 +1,385 @@
+#cython: boundscheck=False, nonecheck=False, initializedcheck=False
+# Tree handling (condensing, finding stable clusters) for hdbscan
+# Authors: Leland McInnes
+# License: 3-clause BSD
+
+import numpy as np
+cimport numpy as np
+
+cdef list bfs_from_hierarchy(np.ndarray[np.double_t, ndim=2] hierarchy, long long bfs_root):
+
+    """
+    Perform a breadth first search on a tree in scipy hclust format.
+    """
+    
+    cdef list to_process
+    cdef long long max_node
+    cdef long long num_points
+    cdef long long dim
+
+    dim = hierarchy.shape[0]
+    max_node = 2 * dim
+    num_points = max_node - dim + 1
+
+    to_process = [bfs_root]
+    result = []
+
+    while to_process:
+        result.extend(to_process)
+        to_process = [x - num_points for x in 
+                          to_process if x >= num_points]
+        if to_process:
+            to_process = hierarchy[to_process,:2].flatten().astype(np.int64).tolist()
+
+    return result
+        
+cpdef np.ndarray condense_tree(np.ndarray[np.double_t, ndim=2] hierarchy,
+                          long long min_cluster_size=10):
+
+    cdef long long root
+    cdef long long num_points
+    cdef long long next_label
+    cdef list node_list
+    cdef list result_list
+
+    cdef np.ndarray[np.int64_t, ndim=1] relabel
+    cdef np.ndarray[np.int_t, ndim=1] ignore
+    cdef np.ndarray[np.double_t, ndim=1] children
+
+    cdef long long node
+    cdef long long sub_node
+    cdef long long left
+    cdef long long right
+    cdef double lambda_value
+    cdef long long left_count
+    cdef long long right_count
+    
+    root = 2 * hierarchy.shape[0]
+    num_points = root // 2 + 1
+    next_label = num_points + 1
+    
+    node_list = bfs_from_hierarchy(hierarchy, root)
+    
+    relabel = np.empty(len(node_list), dtype=np.int64)
+    relabel[root] = num_points
+    result_list = []
+    ignore = np.zeros(len(node_list), dtype=np.int)
+    
+    for node in node_list:
+        if ignore[node] or node < num_points:
+            continue
+            
+        children = hierarchy[node - num_points]
+        left = <long long> children[0]
+        right = <long long> children[1]
+        lambda_value = 1.0 / children[2] if children[2] > 0.0 else np.inf
+        left_count = <long long> (hierarchy[left - num_points][3] 
+                            if left >= num_points else 1)
+        right_count = <long long> (hierarchy[right - num_points][3] 
+                             if right >= num_points else 1)
+        
+        if left_count >= min_cluster_size and right_count >= min_cluster_size:
+            relabel[left] = next_label
+            next_label += 1
+            result_list.append((relabel[node], relabel[left], lambda_value, left_count))
+            
+            relabel[right] = next_label
+            next_label += 1
+            result_list.append((relabel[node], relabel[right], lambda_value, right_count))
+            
+        elif left_count < min_cluster_size and right_count < min_cluster_size:
+            for sub_node in bfs_from_hierarchy(hierarchy, left):
+                if sub_node < num_points:
+                    result_list.append((relabel[node], sub_node, lambda_value, 1))
+                ignore[sub_node] = True
+                
+            for sub_node in bfs_from_hierarchy(hierarchy, right):
+                if sub_node < num_points:
+                    result_list.append((relabel[node], sub_node, lambda_value, 1))
+                ignore[sub_node] = True
+                
+        elif left_count < min_cluster_size:
+            relabel[right] = relabel[node]
+            for sub_node in bfs_from_hierarchy(hierarchy, left):
+                if sub_node < num_points:
+                    result_list.append((relabel[node], sub_node, lambda_value, 1))
+                ignore[sub_node] = True
+                
+        else:
+            relabel[left] = relabel[node]
+            for sub_node in bfs_from_hierarchy(hierarchy, right):
+                if sub_node < num_points:
+                    result_list.append((relabel[node], sub_node, lambda_value, 1))
+                ignore[sub_node] = True
+                
+    return np.array(result_list, dtype=[
+                                        ('parent', np.int64),
+                                        ('child', np.int64),
+                                        ('lambda', float),
+                                        ('child_size', np.int64)
+                                       ])
+
+cpdef dict compute_stability(np.ndarray condensed_tree):
+
+    cdef np.ndarray[np.double_t, ndim=1] result_arr
+    cdef np.ndarray sorted_child_data
+    cdef np.ndarray[np.int64_t, ndim=1] sorted_children
+    cdef np.ndarray[np.double_t, ndim=1] sorted_lambdas
+
+    cdef np.ndarray[np.int64_t, ndim=1] parents
+    cdef np.ndarray[np.int64_t, ndim=1] sizes
+    cdef np.ndarray[np.double_t, ndim=1] lambdas
+
+    cdef np.int64_t child
+    cdef np.int64_t current_child
+    cdef np.float64_t lambda_
+    cdef np.float64_t min_lambda
+
+    cdef np.ndarray[np.double_t, ndim=1] births_arr
+    cdef np.double_t *births
+
+    cdef np.int64_t largest_child = condensed_tree['child'].max()
+    cdef np.int64_t smallest_cluster = condensed_tree['parent'].min()
+    cdef np.int64_t num_clusters = condensed_tree['parent'].max() - smallest_cluster + 1
+
+    sorted_child_data = np.sort(condensed_tree[['child', 'lambda']], axis=0)
+    births_arr = np.nan * np.ones(largest_child + 1, dtype=np.double)
+    births = (<np.double_t *> births_arr.data)
+    sorted_children = sorted_child_data['child']
+    sorted_lambdas = sorted_child_data['lambda']
+
+    parents = condensed_tree['parent']
+    sizes = condensed_tree['child_size']
+    lambdas = condensed_tree['lambda']
+
+    current_child = -1
+    min_lambda = 0
+
+    for row in range(sorted_child_data.shape[0]):
+        child = <np.int64_t> sorted_children[row]
+        lambda_ = sorted_lambdas[row]
+
+        if child == current_child:
+            min_lambda = min(min_lambda, lambda_)
+        elif current_child != -1:
+            births[current_child] = min_lambda
+            current_child = child
+            min_lambda = lambda_
+        else:
+            # Initialize
+            current_child = child
+            min_lambda = lambda_
+
+    result_arr = np.zeros(num_clusters, dtype=np.double)
+
+    for i in range(condensed_tree.shape[0]):
+        parent = parents[i]
+        lambda_ = lambdas[i]
+        child_size = sizes[i]
+        result_index = parent - smallest_cluster
+
+        result_arr[result_index] += (lambda_ - births[parent]) * child_size
+
+    return dict(np.vstack((np.arange(smallest_cluster, condensed_tree['parent'].max() + 1), result_arr)).T)
+
+cdef list bfs_from_cluster_tree(np.ndarray tree, long long bfs_root):
+
+    cdef list result
+    cdef np.ndarray[np.int64_t, ndim=1] to_process
+
+    result = []
+    to_process = np.array([bfs_root], dtype=np.int64)
+    
+    while to_process.shape[0] > 0:
+        result.extend(to_process.tolist())
+        to_process = tree['child'][np.in1d(tree['parent'], to_process)]
+
+    return result
+
+cdef max_lambdas(np.ndarray tree):
+
+    cdef np.ndarray sorted_parent_data
+    cdef np.ndarray[np.int64_t, ndim=1] sorted_parents
+    cdef np.ndarray[np.double_t, ndim=1] sorted_lambdas
+
+    cdef np.int64_t parent
+    cdef np.int64_t current_parent
+    cdef np.float64_t lambda_
+    cdef np.float64_t max_lambda
+
+    cdef np.ndarray[np.double_t, ndim=1] deaths_arr
+    cdef np.double_t *deaths
+
+    cdef np.int64_t largest_parent = tree['parent'].max()
+
+    sorted_parent_data = np.sort(tree[['parent', 'lambda']], axis=0)
+    deaths_arr = np.zeros(largest_parent + 1, dtype=np.double)
+    deaths = (<np.double_t *> deaths_arr.data)
+    sorted_parents = sorted_parent_data['parent']
+    sorted_lambdas = sorted_parent_data['lambda']
+
+    current_parent = -1
+    max_lambda = 0
+
+    for row in range(sorted_parent_data.shape[0]):
+        parent = <np.int64_t> sorted_parents[row]
+        lambda_ = sorted_lambdas[row]
+
+        if parent == current_parent:
+            max_lambda = max(max_lambda, lambda_)
+        elif current_parent != -1:
+            deaths[current_parent] = max_lambda
+            current_parent = parent
+            max_lambda = lambda_
+        else:
+            # Initialize
+            current_parent = parent
+            max_lambda = lambda_
+
+    return deaths_arr
+
+cdef class TreeUnionFind (object):
+
+    cdef np.ndarray _data_arr
+    cdef np.int64_t[:,::1] _data
+    cdef np.ndarray is_component
+
+    def __init__(self, size):
+        self._data_arr = np.zeros((size, 2), dtype=np.int64)
+        self._data_arr.T[0] = np.arange(size)
+        self._data = (<np.int64_t[:size, :2:1]> (<np.int64_t *> self._data_arr.data))
+        self.is_component = np.ones(size, dtype=np.bool)
+
+    cdef union_(self, np.int64_t x, np.int64_t y):
+        cdef np.int64_t x_root = self.find(x)
+        cdef np.int64_t y_root = self.find(y)
+
+        if self._data[x_root, 1] < self._data[y_root, 1]:
+            self._data[x_root, 0] = y_root
+        elif self._data[x_root, 1] > self._data[y_root, 1]:
+            self._data[y_root, 0] = x_root
+        else:
+            self._data[y_root, 0] = x_root
+            self._data[x_root, 1] += 1
+
+        return
+
+    cdef find(self, np.int64_t x):
+        if self._data[x, 0] != x:
+            self._data[x, 0] = self.find(self._data[x, 0])
+            self.is_component[x] = False
+        return self._data[x, 0]
+
+    cdef np.ndarray[np.int64_t, ndim=1] components(self):
+        return self.is_component.nonzero()[0]
+
+cdef np.ndarray[np.int64_t, ndim=1] do_labelling(np.ndarray tree, set clusters, dict cluster_label_map):
+
+    cdef np.int64_t root_cluster
+    cdef np.ndarray[np.int64_t, ndim=1] result_arr
+    cdef np.int64_t *result
+    cdef TreeUnionFind union_find
+    cdef np.int64_t n
+    cdef np.int64_t cluster
+
+    root_cluster = tree['parent'].min()
+    result_arr = np.empty(root_cluster, dtype=np.int64)
+    result = (<np.int64_t *> result_arr.data)
+
+    union_find = TreeUnionFind(tree['parent'].max() + 1)
+
+    for row in tree:
+        if row['child'] not in clusters:
+            union_find.union_(row['parent'], row['child'])
+
+    for n in range(root_cluster):
+        cluster = union_find.find(n)
+        if cluster <= root_cluster:
+            result[n] = -1
+        else:
+            result[n] = cluster_label_map[cluster]
+
+    return result_arr
+
+cdef get_probabilities(np.ndarray tree, dict cluster_map, np.ndarray labels):
+
+    cdef np.ndarray[np.double_t, ndim=1] result
+    cdef np.ndarray[np.double_t, ndim=1] deaths
+    cdef np.int64_t root_cluster
+    cdef np.int64_t point
+    cdef np.int64_t cluster_num
+    cdef np.int64_t cluster
+    cdef np.double_t max_lambda
+    cdef np.double_t lambda_
+
+    result = np.zeros(labels.shape[0])
+    deaths = max_lambdas(tree)
+    root_cluster = tree['parent'].min()
+
+    for row in tree:
+        point = row['child']
+        if point >= root_cluster:
+            continue
+
+        cluster_num = labels[point]
+
+        if cluster_num == -1:
+            continue
+
+        cluster = cluster_map[cluster_num]
+        max_lambda = deaths[cluster]
+        lambda_ = min(row['lambda'], max_lambda)
+        result[point] = lambda_ / max_lambda
+
+    return result
+
+
+cpdef tuple get_clusters(np.ndarray tree, dict stability):
+    """
+    The tree is assumed to have numeric node ids such that a reverse numeric
+    sort is equivalent to a topological sort.
+    """
+    cdef list node_list
+    cdef np.ndarray cluster_tree
+    cdef np.ndarray child_selection
+    cdef dict is_cluster
+    cdef float subtree_stability
+    cdef long long node
+    cdef long long sub_node
+    cdef long long cluster
+    cdef long long num_points
+    cdef np.ndarray labels
+
+    # Assume clusters are ordered by numeric id equivalent to
+    # a topological sort of the tree; This is valid given the
+    # current implementation above, so don't change that ... or
+    # if you do, change this accordingly!
+    node_list = sorted(stability.keys(), reverse=True)[:-1] # (exclude root)
+    cluster_tree = tree[tree['child_size'] > 1]
+    is_cluster = {cluster:True for cluster in node_list}
+    num_points = np.max(tree[tree['child_size'] == 1]['child']) + 1
+
+    for node in node_list:
+        child_selection = (cluster_tree['parent'] == node)
+        subtree_stability = np.sum([stability[child] for 
+                                    child in cluster_tree['child'][child_selection]])
+        if subtree_stability > stability[node]:
+            is_cluster[node] = False
+            stability[node] = subtree_stability
+        else:
+            for sub_node in bfs_from_cluster_tree(cluster_tree, node):
+                if sub_node != node:
+                    is_cluster[sub_node] = False
+
+    clusters = set([c for c in is_cluster if is_cluster[c]])
+    cluster_map = {c:n for n, c in enumerate(clusters)}
+    reverse_cluster_map = {n:c for n, c in enumerate(clusters)}
+
+    labels = do_labelling(tree, clusters, cluster_map)
+    probs = get_probabilities(tree, reverse_cluster_map, labels)
+
+    return (labels, probs)
+    
+    
+    
+       
---
+++ packages/hdbscan/hdbscan-0.5.tar.gz/hdbscan-0.5/hdbscan/robust_single_linkage_.py
@@ -0,0 +1,382 @@
+# -*- coding: utf-8 -*-
+"""
+Robust Single Linkage: Density based single linkage clustering.
+"""
+# Author: Leland McInnes <leland.mcinnes@gmail.com>
+#
+# License: BSD 3 clause
+
+import numpy as np
+
+from sklearn.base import BaseEstimator, ClusterMixin
+from sklearn.metrics import pairwise_distances
+from scipy.sparse import issparse
+
+try:
+    from sklearn.utils import check_array
+except ImportError:
+    from sklearn.utils import check_arrays
+
+    check_array = check_arrays
+
+from ._hdbscan_linkage import single_linkage, mst_linkage_core_cdist, label
+from ._hdbscan_boruvka import KDTreeBoruvkaAlgorithm, BallTreeBoruvkaAlgorithm
+from dist_metrics import DistanceMetric
+from ._hdbscan_reachability import mutual_reachability
+from .plots import SingleLinkageTree
+from sklearn.neighbors import KDTree, BallTree
+
+from warnings import warn
+
+FAST_METRICS = KDTree.valid_metrics + BallTree.valid_metrics
+
+def _rsl_generic(X, cut, k=5, alpha=1.4142135623730951, gamma=5, metric='minkowski', p=2):
+
+    if metric == 'minkowski':
+        if p is None:
+            raise TypeError('Minkowski metric given but no p value supplied!')
+        if p < 0:
+            raise ValueError('Minkowski metric with negative p value is not defined!')
+
+        distance_matrix = pairwise_distances(X, metric=metric, p=p)
+    else:
+        distance_matrix = pairwise_distances(X, metric=metric)
+
+    mutual_reachability_ = mutual_reachability(distance_matrix, k)
+
+    min_spanning_tree = mst_linkage_core(mutual_reachability_)
+    min_spanning_tree = min_spanning_tree[np.argsort(min_spanning_tree.T[2]), :]
+
+    single_linkage_tree = label(min_spanning_tree)
+    single_linkage_tree = SingleLinkageTree(single_linkage_tree)
+
+    labels = single_linkage_tree.get_clusters(cut, gamma)
+
+    return labels, single_linkage_tree
+
+def _rsl_prims_kdtree(X, cut, k=5, alpha=1.4142135623730951, gamma=5, metric='minkowski', p=2):
+
+    if metric == 'minkowski':
+        if p is None:
+            raise TypeError('Minkowski metric given but no p value supplied!')
+        if p < 0:
+            raise ValueError('Minkowski metric with negative p value is not defined!')
+    elif p is None:
+        p = 2 # Unused, but needs to be integer; assume euclidean
+
+    dim = X.shape[0]
+    k = min(dim - 1, k)
+
+    tree = KDTree(X, metric=metric)
+
+    dist_metric = DistanceMetric.get_metric(metric)
+
+    core_distances = tree.query(X, k=k)[0][:,-1]
+    min_spanning_tree = mst_linkage_core_cdist(X, core_distances, dist_metric)
+
+    single_linkage_tree = label(min_spanning_tree)
+    single_linkage_tree = SingleLinkageTree(single_linkage_tree)
+
+    labels = single_linkage_tree.get_clusters(cut, gamma)
+
+    return labels, single_linkage_tree
+
+def _rsl_prims_balltree(X, cut, k=5, alpha=1.4142135623730951, gamma=5, metric='minkowski', p=2):
+
+    if metric == 'minkowski':
+        if p is None:
+            raise TypeError('Minkowski metric given but no p value supplied!')
+        if p < 0:
+            raise ValueError('Minkowski metric with negative p value is not defined!')
+    elif p is None:
+        p = 2 # Unused, but needs to be integer; assume euclidean
+
+    dim = X.shape[0]
+    k = min(dim - 1, k)
+
+    tree = BallTree(X, metric=metric)
+
+    dist_metric = DistanceMetric.get_metric(metric)
+
+    core_distances = tree.query(X, k=k)[0][:,-1]
+    min_spanning_tree = mst_linkage_core_cdist(X, core_distances, dist_metric)
+
+    single_linkage_tree = label(min_spanning_tree)
+    single_linkage_tree = SingleLinkageTree(single_linkage_tree)
+
+    labels = single_linkage_tree.get_clusters(cut, gamma)
+
+    return labels, single_linkage_tree
+
+def _rsl_boruvka_kdtree(X, min_cluster_size=5, min_samples=None, alpha=1.0,
+                            metric='minkowski', p=2, leaf_size=40,
+                            algorithm='best', gen_min_span_tree=False):
+
+    dim = X.shape[0]
+    min_samples = min(dim - 1, min_samples)
+
+    tree = KDTree(X, metric=metric, leaf_size=leaf_size)
+    alg = KDTreeBoruvkaAlgorithm(tree, min_samples, metric=metric,
+                                 alpha=alpha, leaf_size=leaf_size)
+    min_spanning_tree = alg.spanning_tree()
+
+    single_linkage_tree = label(min_spanning_tree)
+    single_linkage_tree = SingleLinkageTree(single_linkage_tree)
+
+    labels = single_linkage_tree.get_clusters(cut, gamma)
+
+    return labels, single_linkage_tree
+
+def _rsl_boruvka_balltree(X, min_cluster_size=5, min_samples=None, alpha=1.0,
+                              metric='minkowski', p=2, leaf_size=40,
+                              algorithm='best', gen_min_span_tree=False):
+
+    dim = X.shape[0]
+    min_samples = min(dim - 1, min_samples)
+
+    tree = BallTree(X, metric=metric, leaf_size=leaf_size)
+    alg = BallTreeBoruvkaAlgorithm(tree, min_samples, metric=metric,
+                                   alpha=alpha, leaf_size=leaf_size)
+    min_spanning_tree = alg.spanning_tree()
+
+    single_linkage_tree = label(min_spanning_tree)
+    single_linkage_tree = SingleLinkageTree(single_linkage_tree)
+
+    labels = single_linkage_tree.get_clusters(cut, gamma)
+
+    return labels, single_linkage_tree
+
+def robust_single_linkage(X, cut, k=5, alpha=1.4142135623730951, gamma=5, metric='minkowski', p=2, algorithm=None):
+    """Perform robust single linkage clustering from a vector array
+    or distance matrix.
+
+    Parameters
+    ----------
+    X : array or sparse (CSR) matrix of shape (n_samples, n_features), or \
+            array of shape (n_samples, n_samples)
+        A feature array, or array of distances between samples if
+        ``metric='precomputed'``.
+
+    cut : float
+        The reachability distance value to cut the cluster heirarchy at
+        to derive a flat cluster labelling.
+
+    k : int, optional
+        Reachability distances will be computed with regard to the `k`
+        nearest neighbors. (default 5)
+
+    alpha : float, optional
+        Distance scaling for reachability distance computation. Reachability
+        distance is computed as $max \{ core_k(a), core_k(b), 1/\alpha d(a,b) \}$.
+        (default sqrt(2))
+
+    gamma : int, optional
+        Ignore any clusters in the flat clustering with size less than gamma,
+        and declare points in such clusters as noise points. (default 5)
+
+    metric : string, or callable, optional
+        The metric to use when calculating distance between instances in a
+        feature array. If metric is a string or callable, it must be one of
+        the options allowed by metrics.pairwise.pairwise_distances for its
+        metric parameter.
+        If metric is "precomputed", X is assumed to be a distance matrix and
+        must be square.
+
+    algorithm : string, optional
+        Exactly which algorithm to use; hdbscan has variants specialised
+        for different characteristics of the data. By default this is set
+        to ``best`` which chooses the "best" algorithm given the nature of
+        the data. You can force other options if you believe you know
+        better. Options are:
+            * ``generic``
+            * ``best``
+            * ``prims_kdtree``
+            * ``prims_balltree``
+            * ``boruvka_kdtree``
+            * ``boruvka_balltree``
+
+    Returns
+    -------
+    labels : array [n_samples]
+        Cluster labels for each point.  Noisy samples are given the label -1.
+
+    single_linkage_tree : array [n_samples - 1, 4]
+        The single linkage tree produced during clustering in scipy
+        hierarchical clustering format
+        (see http://docs.scipy.org/doc/scipy/reference/cluster.hierarchy.html).
+
+    References
+    ----------
+    K. Chaudhuri and S. Dasgupta.
+    "Rates of convergence for the cluster tree."
+    In Advances in Neural Information Processing Systems, 2010.
+
+    """
+
+    if type(k) is not int or k < 1:
+        raise ValueError('k must be an integer greater than zero!')
+
+    if type(alpha) is not float or alpha < 1.0:
+        raise ValueError('alpha must be a float greater than or equal to 1.0!')
+
+    if type(gamma) is not int or gamma < 1:
+        raise ValueError('gamma must be an integer greater than zero!')
+
+    X = check_array(X, accept_sparse='csr')
+
+    if algorithm is not None:
+        if algorithm == 'generic':
+            return _rsl_generic(X, cut, k, alpha, gamma, metric, p)
+        elif algorithm == 'prims_kdtree':
+            return _rsl_prims_kdtree(X, cut, k, alpha, gamma, metric, p)
+        elif algorithm == 'prims_balltree':
+            return _rsl_prims_balltree(X, cut, k, alpha, gamma, metric, p)
+        elif algorithm == 'boruvka_kdtree':
+            return _rsl_boruvka_kdtree(X, cut, k, alpha, gamma, metric, p)
+        elif algorithm == 'boruvka_balltree':
+            return _rsl_boruvka_balltree(X, cut, k, alpha, gamma, metric, p)
+        else:
+            raise TypeError('Unknown algorithm type %s specified' % algorithm)
+
+    if issparse(X) or metric not in FAST_METRICS:  # We can't do much with sparse matrices ...
+        return _rsl_generic(X, cut, k, alpha, gamma, metric, p)
+    elif metric in KDTree.valid_metrics:
+        # Need heuristic to decide when to go to boruvka; still debugging for now
+        if True:
+            return _rsl_prims_kdtree(X, cut, k, alpha, gamma, metric, p)
+        else:
+            return _rsl_boruvka_kdtree(X, cut, k, alpha, gamma, metric, p)
+    else: # Metric is a valid BallTree metric
+        # Need heuristic to decide when to go to boruvka; still debugging for now
+        if True:
+            return _rsl_prims_kdtree(X, cut, k, alpha, gamma, metric, p)
+        else:
+            return _rsl_boruvka_balltree(X, cut, k, alpha, gamma, metric, p)
+
+
+
+class RobustSingleLinkage (BaseEstimator, ClusterMixin):
+    """Perform robust single linkage clustering from a vector array
+    or distance matrix.
+
+    Roust single linkage is a modified version of single linkage that
+    attempts to be more robust to noise. Specifically the goal is to
+    more accurately approximate the level set tree of the unknown
+    probability density function from which the sample data has
+    been drawn.
+
+    Parameters
+    ----------
+    X : array or sparse (CSR) matrix of shape (n_samples, n_features), or \
+            array of shape (n_samples, n_samples)
+        A feature array, or array of distances between samples if
+        ``metric='precomputed'``.
+
+    cut : float
+        The reachability distance value to cut the cluster heirarchy at
+        to derive a flat cluster labelling.
+
+    k : int, optional
+        Reachability distances will be computed with regard to the `k`
+        nearest neighbors. (default 5)
+
+    alpha : float, optional
+        Distance scaling for reachability distance computation. Reachability
+        distance is computed as $max \{ core_k(a), core_k(b), 1/\alpha d(a,b) \}$.
+        (default sqrt(2))
+
+    gamma : int, optional
+        Ignore any clusters in the flat clustering with size less than gamma,
+        and declare points in such clusters as noise points. (default 5)
+
+    metric : string, or callable, optional
+        The metric to use when calculating distance between instances in a
+        feature array. If metric is a string or callable, it must be one of
+        the options allowed by metrics.pairwise.pairwise_distances for its
+        metric parameter.
+        If metric is "precomputed", X is assumed to be a distance matrix and
+        must be square.
+
+    algorithm : string, optional
+        Exactly which algorithm to use; hdbscan has variants specialised
+        for different characteristics of the data. By default this is set
+        to ``best`` which chooses the "best" algorithm given the nature of
+        the data. You can force other options if you believe you know
+        better. Options are:
+            * ``small``
+            * ``small_kdtree``
+            * ``large_kdtree``
+            * ``large_kdtree_fastcluster``
+
+    Attributes
+    -------
+    labels_ : array [n_samples]
+        Cluster labels for each point.  Noisy samples are given the label -1.
+
+    cluster_hierarchy_ : SingleLinkageTree object
+        The single linkage tree produced during clustering. This object provides
+        several methods for:
+            * Plotting
+            * Generating a flat clustering
+            * Exporting to NetworkX
+            * Exporting to Pandas
+
+    References
+    ----------
+    K. Chaudhuri and S. Dasgupta.
+    "Rates of convergence for the cluster tree."
+    In Advances in Neural Information Processing Systems, 2010.
+
+    """
+
+    def __init__(self, cut=0.25, k=5, alpha=1.4142135623730951, gamma=5, metric='euclidean', p=None):
+
+        self.cut = cut
+        self.k = k
+        self.alpha = alpha
+        self.gamma = gamma
+        self.metric = metric
+        self.p = p
+
+        self._cluster_hierarchy_ = None
+
+    def fit(self, X, y=None):
+        """Perform robust single linkage clustering from features or distance matrix.
+
+        Parameters
+        ----------
+        X : array or sparse (CSR) matrix of shape (n_samples, n_features), or \
+                array of shape (n_samples, n_samples)
+            A feature array, or array of distances between samples if
+            ``metric='precomputed'``.
+        """
+        X = check_array(X, accept_sparse='csr')
+        self.labels_, self._cluster_hierarchy = robust_single_linkage(X, **self.get_params())
+        return self
+
+    def fit_predict(self, X, y=None):
+        """Performs clustering on X and returns cluster labels.
+
+        Parameters
+        ----------
+        X : array or sparse (CSR) matrix of shape (n_samples, n_features), or \
+                array of shape (n_samples, n_samples)
+            A feature array, or array of distances between samples if
+            ``metric='precomputed'``.
+
+        Returns
+        -------
+        y : ndarray, shape (n_samples,)
+            cluster labels
+        """
+
+        self.fit(X)
+        return self.labels_
+
+    @property
+    def cluster_hierarchy_(self):
+        if self._cluster_hierarchy_ is not None:
+            return SingleLinkageTree(self._cluster_hierarchy_)
+        else:
+            warn('No single linkage tree was generated; try running fit first.')
+            return None
