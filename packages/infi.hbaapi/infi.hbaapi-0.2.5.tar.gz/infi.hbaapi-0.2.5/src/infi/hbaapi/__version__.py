d716c34aa0924040f29e7001d0075063d06a83bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:55 +0000

    Add package infi.hbaapi/0.2.5/infi.hbaapi-0.2.5.tar.gz

---
+++ packages/infi.hbaapi/infi.hbaapi-0.2.5.tar.gz/infi.hbaapi-0.2.5/src/infi/hbaapi/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.5"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.2.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '423a44f1ac746c946b0a181a8396908a91246c78'
+__git_head_subject__ = 'Finished release v0.2.5.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
