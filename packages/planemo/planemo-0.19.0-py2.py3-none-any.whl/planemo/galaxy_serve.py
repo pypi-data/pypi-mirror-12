a6f30825c2c51de7eb63ce0f6c9f671087d25404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:00 +0000

    Add package planemo/0.19.0/planemo-0.19.0-py2.py3-none-any.whl

---
+++ packages/planemo/planemo-0.19.0-py2.py3-none-any.whl/planemo/galaxy_serve.py
@@ -0,0 +1,66 @@
+import contextlib
+import os
+
+from planemo import galaxy_config
+from planemo import galaxy_run
+from planemo import io
+
+
+def serve(ctx, paths, **kwds):
+    # TODO: Preceate a user.
+    # TODO: Setup an admin user.
+    # TODO: Pass through more parameters.
+    # TODO: Populate test-data directory as FTP directory.
+    daemon = kwds.get("daemon", False)
+    if daemon:
+        kwds["no_cleanup"] = True
+
+    with galaxy_config.galaxy_config(ctx, paths, **kwds) as config:
+        # TODO: Allow running dockerized Galaxy here instead.
+        run_script = os.path.join(config.galaxy_root, "run.sh")
+        if daemon:
+            run_script += " --daemon --wait"
+            config.env["GALAXY_RUN_ALL"] = "1"
+        else:
+            run_script += " --server-name '%s' --reload" % config.server_name
+        server_ini = os.path.join(config.config_directory, "galaxy.ini")
+        config.env["GALAXY_CONFIG_FILE"] = server_ini
+        cmds = [
+            run_script,
+        ]
+        cmd = "; ".join(cmds)
+        action = "Starting galaxy"
+        galaxy_run.run_galaxy_command(
+            ctx,
+            cmd,
+            config.env,
+            action,
+        )
+        return config
+
+
+@contextlib.contextmanager
+def shed_serve(ctx, install_args_list, **kwds):
+    with serve_daemon(ctx, **kwds) as config:
+        install_deps = not kwds.get("skip_dependencies", False)
+        io.info("Installing repositories - this may take some time...")
+        for install_args in install_args_list:
+            install_args["install_tool_dependencies"] = install_deps
+            install_args["install_repository_dependencies"] = True
+            install_args["new_tool_panel_section_label"] = "Shed Installs"
+            config.install_repo(
+                **install_args
+            )
+        config.wait_for_all_installed()
+        yield config
+
+
+@contextlib.contextmanager
+def serve_daemon(ctx, paths=[], **kwds):
+    try:
+        config = serve(ctx, paths, daemon=True, **kwds)
+        yield config
+    finally:
+        config.kill()
+        if not kwds.get("no_cleanup", False):
+            config.cleanup()
