182d1e39a34d78fb381bf3eff53f765faf61b850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:57 +0000

    Add package PyLC/0.1.0a8/PyLC-0.1.0a8.tar.gz

---
+++ packages/PyLC/PyLC-0.1.0a8.tar.gz/PyLC-0.1.0a8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='PyLC',
+  version='0.1.0a8',
+  author='Benjamin Morrise',
+  author_email='bmorrise@gmail.com',
+  description='A python wrapper for the Lending Club API',
+  url='https://github.com/bmorrise/pylc',
+  license='MIT',
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',    
+  ],
+  packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"])
+)
