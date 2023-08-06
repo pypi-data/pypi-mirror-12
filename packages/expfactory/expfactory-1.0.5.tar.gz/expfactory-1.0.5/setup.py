6ee07dcaa0125cb9c80c00946cf7ffa302102149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:15 +0000

    Add package expfactory/1.0.5/expfactory-1.0.5.tar.gz

---
+++ packages/expfactory/expfactory-1.0.5.tar.gz/expfactory-1.0.5/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the relevant file
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    # Application name:
+    name="expfactory",
+
+    # Version number (initial):
+    version="1.0.5",
+
+    # Application author details:
+    author="poldracklab",
+    author_email="vsochat@stanford.edu",
+
+    # Packages
+    packages=find_packages(),
+
+    # Data files
+    include_package_data=True,
+    zip_safe=False,
+
+    # Details
+    url="http://www.github.com/expfactory",
+
+    license="LICENSE.txt",
+    description="Python module for managing experiment factory javascript experiment files, a psiturk battery, and virtual machines to host the compilation of these things.",
+    long_description=long_description,
+    keywords='psiturk behavior neuroscience experiment factory',
+
+    install_requires = ['numpy','Flask','gitpython','flask-restful'],
+
+    entry_points = {
+        'console_scripts': [
+            'expfactory=expfactory.scripts:main',
+        ],
+    },
+
+)
