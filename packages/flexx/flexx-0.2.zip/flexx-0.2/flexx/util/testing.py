ea817ed9b773cf52b0fc5b4c4d4f07862b5acd44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:56 +0000

    Add package flexx/0.2/flexx-0.2.zip

---
+++ packages/flexx/flexx-0.2.zip/flexx-0.2/flexx/util/testing.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, Almar Klein
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+
+""" Functionality used for testing. This code itself is not covered in tests.
+"""
+
+from __future__ import absolute_import, print_function, division
+
+import os
+import sys
+import inspect
+
+import pytest
+
+# Get root dir
+PACKAGE_NAME = __name__.split('.')[0]
+
+# Get root dir
+THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+ROOT_DIR = THIS_DIR
+for i in range(9):
+    ROOT_DIR = os.path.dirname(ROOT_DIR)
+    if os.path.isfile(os.path.join(ROOT_DIR, '.gitignore')):
+        break
+
+
+STYLE_IGNORES = ['E226', 
+                 'E241', 
+                 'E265', 
+                 'E266',  # too many leading '#' for block comment
+                 'E402',  # module level import not at top of file
+                 'E731',  # do not assign a lambda expression, use a def
+                 'W291', 
+                 'W293',
+                 ]
+
+
+## Functions to use in tests
+
+def run_tests_if_main(show_coverage=False):
+    """ Run tests in a given file if it is run as a script
+    
+    Coverage is reported for running this single test. Set show_coverage to
+    launch the report in the web browser.
+    """
+    local_vars = inspect.currentframe().f_back.f_locals
+    if not local_vars.get('__name__', '') == '__main__':
+        return
+    # we are in a "__main__"
+    os.chdir(ROOT_DIR)
+    fname = str(local_vars['__file__'])
+    _clear_our_modules()
+    _enable_faulthandler()
+    pytest.main('-v -x --color=yes --cov flexx '
+                '--cov-config .coveragerc --cov-report html %s' % repr(fname))
+    if show_coverage:
+        import webbrowser
+        fname = os.path.join(ROOT_DIR, 'htmlcov', 'index.html')
+        webbrowser.open_new_tab(fname)
+
+
+## Requirements
+
+def _enable_faulthandler():
+    """ Enable faulthandler (if we can), so that we get tracebacks
+    on segfaults.
+    """
+    try:
+        import faulthandler
+        faulthandler.enable()
+        print('Faulthandler enabled')
+    except Exception:
+        print('Could not enable faulthandler')
+
+
+def _clear_our_modules():
+    # Remove ourselves from sys.modules to force an import
+    for key in list(sys.modules.keys()):
+        if key.startswith(PACKAGE_NAME) and 'testing' not in key:
+            del sys.modules[key]
+
+
+class FileForTesting(object):
+    """ Alternative to stdout that makes path relative to ROOT_DIR
+    """
+    def __init__(self, original):
+        self._original = original
+    
+    def write(self, msg):
+        if msg.startswith(ROOT_DIR):
+            msg = os.path.relpath(msg, ROOT_DIR)
+        self._original.write(msg)
+        self._original.flush()
+    
+    def flush(self):
+        self._original.flush()
+    
+    def revert(self):
+        sys.stdout = self._original
---
+++ packages/flexx/flexx-0.2.zip/flexx-0.2/setup.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+""" Flexx setup script.
+
+Release:
+
+* bump __version__
+* python setup.py register
+* python setup.py sdist bdist_wheel --universal upload
+* build conda packages?
+
+"""
+
+import os
+
+try:
+    # use setuptools namespace, allows for "develop"
+    import setuptools  # noqa, analysis:ignore
+except ImportError:
+    pass  # it's not essential for installation
+from distutils.core import setup
+
+name = 'flexx'
+description = "Pure Python toolkit for creating GUI's using web technology."
+
+
+# Get version and docstring
+__version__ = None
+__doc__ = ''
+docStatus = 0  # Not started, in progress, done
+initFile = os.path.join(os.path.dirname(__file__), name, '__init__.py')
+for line in open(initFile, 'rt', encoding='utf-8').readlines():
+    if (line.startswith('version_info') or line.startswith('__version__')):
+        exec(line.strip())
+    elif line.startswith('"""'):
+        if docStatus == 0:
+            docStatus = 1
+            line = line.lstrip('"')
+        elif docStatus == 1:
+            docStatus = 2
+    if docStatus == 1:
+        __doc__ += line
+
+
+def package_tree(pkgroot):
+    path = os.path.dirname(__file__)
+    subdirs = [os.path.relpath(i[0], path).replace(os.path.sep, '.')
+               for i in os.walk(os.path.join(path, pkgroot))
+               if '__init__.py' in i[2]]
+    return subdirs
+
+
+# Define dependencies per subpackage
+extras_require = {'app': ['tornado']}
+extras_require['ui'] = extras_require['app']
+extras_require['all'] = [i for ii in extras_require.values() for i in ii]
+
+
+setup(
+    name=name,
+    version=__version__,
+    author='Flexx contributors',
+    author_email='almar.klein@gmail.com',
+    license='(new) BSD',
+    url='http://flexx.readthedocs.org',
+    download_url='https://pypi.python.org/pypi/flexx',
+    keywords="ui design, web runtime, pyscript, reactive programming, FRP",
+    description=description,
+    long_description=__doc__,
+    platforms='any',
+    provides=[name],
+    install_requires=[],  # react, pyscript and webruntime require nothing
+    extras_require=extras_require,
+    packages=package_tree(name),
+    package_dir={name: name},
+    package_data={'flexx': ['resources/*']},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        #'Programming Language :: Python :: 2.7',  # not yet supported
+        'Programming Language :: Python :: 3.4',
+    ],
+)
