b877a6d16fff2a07e7c06346d5937cd6396d8ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:36 +0000

    Add package outlier_utils/0.0.1/outlier_utils-0.0.1-py3-none-any.whl

---
+++ packages/outlier_utils/outlier_utils-0.0.1-py3-none-any.whl/outliers/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+from . import smirnov_grubbs
+
+__version__ = '0.0.1'
+__author__ = 'Masashi Shibata <contact@c-bata.link>'
+__author_email__ = 'contact@c-bata.link'
+__license__ = 'MIT License'
+
