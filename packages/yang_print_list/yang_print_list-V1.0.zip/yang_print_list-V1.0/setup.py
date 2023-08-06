440135484e75c06762fd7e1662b72d2d9e10a4e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:09 +0000

    Add package yang_print_list/V1.0/yang_print_list-V1.0.zip

---
+++ packages/yang_print_list/yang_print_list-V1.0.zip/yang_print_list-V1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+        name         = 'yang_print_list',
+        version      = 'V1.0',
+        py_modules   = ['print_list'],
+        author       = 'yanggang',
+        author_email = 'yanggangfly@163.com',
+        description  = 'A simple printer of lists',
+    )
