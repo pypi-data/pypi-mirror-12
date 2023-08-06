c00988ae9c3aba4dd6531a52a867d0960c85338b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:09 +0000

    Add package name_suggestor/0.1.3/name_suggestor-0.1.3.tar.gz

---
+++ packages/name_suggestor/name_suggestor-0.1.3.tar.gz/name_suggestor-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='name_suggestor',
+    packages=['name_suggestor'],
+    version='0.1.3',
+    description='Suggests a name based on other names found in the US census',
+    author='Alex Kahan',
+    author_email='alexk307@gmail.com',
+    url='https://github.com/alexk307/last_name',
+    download_url='https://github.com/alexk307/last_name',
+    install_requires=['metaphone'],
+    include_package_data=True,
+    package_data={'': ['*.json']},
+    keywords=['name', 'census', 'recommendation'],
+    classifiers=[],
+)
