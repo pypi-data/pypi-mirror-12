4d7706d93d4a400cf41d0fa1c20d1fe563bcb6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:34 +0000

    Add package miniconfig/0.1.4/miniconfig-0.1.4-py2.py3-none-any.whl

---
+++ packages/miniconfig/miniconfig-0.1.4-py2.py3-none-any.whl/miniconfig/__init__.py
@@ -0,0 +1,96 @@
+# -*- coding:utf-8 -*-
+import logging
+logger = logging.getLogger(__name__)
+from functools import partial
+import pkg_resources
+import sys
+
+
+PHASE1_CONFIG = -20
+PHASE2_CONFIG = -10
+
+
+class ConfigurationError(Exception):
+    pass
+
+
+def import_symbol(symbol):
+    return pkg_resources.EntryPoint.parse("x=%s" % symbol).load(False)
+
+
+def caller_module(level=2):
+    module_globals = sys._getframe(level).f_globals
+    name = module_globals.get("__name__", "__main__")
+    return sys.modules[name]
+
+
+class Control(object):
+    def __init__(self, queue=None):
+        self.queue = queue or []
+
+
+class ConfiguratorCore(object):
+    def __init__(self, settings=None, module=None, control=None):
+        self.settings = settings or {}
+        self.module = module or caller_module()
+        self.control = control or Control()
+
+    def build_import_symbol_string(self, fn_or_string):
+        if not fn_or_string.startswith("."):
+            return fn_or_string
+
+        nodes = self.module.__name__.split(".")
+        if fn_or_string.endswith("."):
+            fn_or_string = fn_or_string[1:]
+
+        poped = []
+        for i, c in enumerate(fn_or_string):
+            if c != ".":
+                break
+            poped.append(nodes.pop())
+        if fn_or_string == "" or fn_or_string.endswith("."):
+            return ".".join(nodes)
+        nodes.append(poped[-1])
+        return ".".join(nodes) + "." + fn_or_string[i:]
+
+    def include(self, fn_or_string):
+        if callable(fn_or_string):
+            includeme = fn_or_string
+            module = getattr(fn_or_string, "__module__", None)
+        else:
+            symbol_string = self.build_import_symbol_string(fn_or_string)
+            if ":" in symbol_string:
+                module_string, fn_name = symbol_string.split(":", 1)
+            else:
+                module_string, fn_name = symbol_string, "includeme"
+            module = import_symbol(module_string)
+            includeme = getattr(module, fn_name)
+
+        config = self.__class__(self.settings,
+                                module=module,
+                                control=self.control)
+        return includeme(config)
+
+    def action(self, callback, order=0):
+        self.queue.append((order, callback))
+
+    def commit(self):
+        for o, callback in sorted(self.queue, key=lambda xs: xs[0]):
+            callback()
+        self.queue = []
+
+    def maybe_dotted(self, fn_or_string):
+        if callable(fn_or_string):
+            return fn_or_string
+        symbol_string = self.build_import_symbol_string(fn_or_string)
+        return import_symbol(symbol_string)
+
+    def add_directive(self, name, fn_or_string):
+        fn = self.maybe_dotted(fn_or_string)
+        setattr(self.control, name, fn)
+
+    def __getattr__(self, name):
+        attr = getattr(self.control, name)
+        if callable(attr):
+            return partial(attr, self)
+        return attr
---
+++ packages/miniconfig/miniconfig-0.1.4-py2.py3-none-any.whl/miniconfig/tests/test_directive.py
@@ -0,0 +1,104 @@
+# -*- coding:utf-8 -*-
+def _getTarget():
+    from miniconfig import ConfiguratorCore
+    return ConfiguratorCore
+
+
+def test_directive__action_is_added():
+    def do_action(config):
+        pass
+
+    config = _getTarget()()
+
+    assert hasattr(config, "do_action") is False
+    config.add_directive("do_action", do_action)
+    assert hasattr(config, "do_action") is True
+
+
+def test_directive__define_by_dottedname():
+    import imp
+    import sys
+
+    finished = [False]
+
+    def do_action_another_module(config):
+        finished[0] = True
+
+    module = imp.new_module("miniconfig.moo")
+    module.do_action = do_action_another_module
+    sys.modules["miniconfig.moo"] = module
+
+    config = _getTarget()()
+    config.add_directive("do_action", "miniconfig.moo:do_action")
+    assert hasattr(config, "do_action") is True
+
+    config.do_action()
+    assert finished[0] is True
+
+
+def test_directive__action_with_config():
+    m = object()
+    finished = [False]
+
+    def with_me(config):
+        assert config.settings["m"] == m
+        finished[0] = True
+
+    config = _getTarget()(settings={"m": m})
+    config.add_directive("with_me", with_me)
+
+    config.with_me()
+
+    assert finished[0] is True
+
+
+def test_directive__action_with_arguments():
+    collection = []
+    finished = [False]
+
+    def add_item(config, x):
+        collection.append(x)
+        finished[0] = True
+
+    config = _getTarget()()
+    config.add_directive("add_item", add_item)
+
+    config.add_item(10)
+    config.add_item(200)
+
+    assert finished[0] is True
+    assert collection == [10, 200]
+
+
+def test_directive__sub_configurator_also_same_control():
+    config = _getTarget()()
+    finished = [False]
+
+    def includeme(config2):
+        assert config2.hello() == "hello"
+        finished[0] = True
+
+    def hello(config):
+        return "hello"
+    config.add_directive("hello", hello)
+    config.include(includeme)
+
+    assert finished[0] is True
+    assert config.hello() == "hello"
+
+
+def test_directive__sub_configurator_also_same_control2():
+    class WithHelloControl(object):
+        def hello(self, config):
+            return "hello"
+
+    config = _getTarget()(control=WithHelloControl())
+    finished = [False]
+
+    def includeme(config2):
+        assert config2.hello() == "hello"
+        finished[0] = True
+
+    config.include(includeme)
+    assert finished[0] is True
+    assert config.hello() == "hello"
