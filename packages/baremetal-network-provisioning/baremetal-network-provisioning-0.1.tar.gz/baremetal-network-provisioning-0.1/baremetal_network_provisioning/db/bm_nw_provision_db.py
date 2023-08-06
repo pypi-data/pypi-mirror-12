8b6dc5e58f319c041c88e2605af37189d720aa6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:50 +0000

    Add package baremetal-network-provisioning/0.1/baremetal-network-provisioning-0.1.tar.gz

---
+++ packages/baremetal-network-provisioning/baremetal-network-provisioning-0.1.tar.gz/baremetal-network-provisioning-0.1/baremetal_network_provisioning/db/bm_nw_provision_db.py
@@ -0,0 +1,252 @@
+# Copyright (c) 2015 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from oslo_log import log as logging
+from sqlalchemy.orm import exc
+
+from baremetal_network_provisioning.db import bm_nw_provision_models as models
+
+from neutron.db import models_v2
+
+
+LOG = logging.getLogger(__name__)
+
+
+def add_hp_switch_port(context, record_dict):
+    """Insert a new switch port to hp_switch_ports."""
+    session = context.session
+    with session.begin(subtransactions=True):
+        switch_port = models.HPSwitchPort(
+            id=record_dict['id'],
+            switch_id=record_dict['switch_id'],
+            port_name=record_dict['port_name'],
+            lag_id=record_dict['lag_id'])
+        session.add(switch_port)
+
+
+def delete_hp_switch_port(context, record_dict):
+    """Delete switch port that matches the supplied id."""
+    session = context.session
+    with session.begin(subtransactions=True):
+        if(record_dict['id']):
+            session.query(models.HPSwitchPort).filter_by(
+                id=record_dict['id']).delete()
+
+
+def get_all_hp_sw_port_by_swchid_portname(context, record_dict):
+    """Get hp_switch_port that matches the supplied switch id and port name."""
+    try:
+        query = context.session.query(models.HPSwitchPort)
+        switch_port = query.filter_by(
+            switch_id=record_dict['switch_id'],
+            port_name=record_dict['port_name']).all()
+    except exc.NoResultFound:
+        LOG.debug('no hp switch port found for %s and %s',
+                  record_dict['switch_id'],
+                  record_dict['port_name'])
+        return
+    return switch_port
+
+
+def get_hp_ironic_swport_map_by_sw_id(context, record_dict):
+    """Get ironic_switch_port_mapping that matches the supplied switch id."""
+    try:
+        query = context.session.query(models.HPIronicSwitchPortMapping)
+        port_mapping = query.filter_by(
+            switch_port_id=record_dict['id']).one()
+    except exc.NoResultFound:
+        LOG.debug('no hp ironic switch port mapping found for switch id %s',
+                  record_dict['id'])
+        return
+    return port_mapping
+
+
+def add_hp_switch_lag_port(context, record_dict):
+    """Insert a new lag port to hp_switch_lag_ports."""
+    session = context.session
+    with session.begin(subtransactions=True):
+        lag_port = models.HPSwitchLAGPort(
+            id=record_dict['id'],
+            external_lag_id=record_dict['external_lag_id'])
+        session.add(lag_port)
+
+
+def delete_hp_switch_lag_port(context, record_dict):
+    """Delete lag port that matches the supplied id."""
+    session = context.session
+    with session.begin(subtransactions=True):
+        if(record_dict['id']):
+            session.query(models.HPSwitchLAGPort).filter_by(
+                id=record_dict['id']).delete()
+
+
+def get_hp_switch_lag_port_by_id(context, record_dict):
+    """Get hp_switch_lag_port that matches the supplied id."""
+    try:
+        query = context.session.query(models.HPSwitchLAGPort)
+        switch_lag_port = query.filter_by(
+            id=record_dict['id']).one()
+    except exc.NoResultFound:
+        LOG.debug('no hp switch lag port found for %s',
+                  record_dict['id'])
+        return
+    return switch_lag_port
+
+
+def add_hp_ironic_switch_port_mapping(context, record_dict):
+    """Insert a new ironic switch port mapping."""
+    session = context.session
+    with session.begin(subtransactions=True):
+        mapping = models.HPIronicSwitchPortMapping(
+            neutron_port_id=record_dict['neutron_port_id'],
+            switch_port_id=record_dict['switch_port_id'],
+            lag_id=record_dict['lag_id'],
+            access_type=record_dict['access_type'],
+            segmentation_id=record_dict['segmentation_id'],
+            host_id=record_dict['host_id'])
+        session.add(mapping)
+
+
+def delete_hp_ironic_switch_port_mapping(context, record_dict):
+    """Delete ironic switch port mapping that matches neutron_port_id."""
+    session = context.session
+    with session.begin(subtransactions=True):
+        if(record_dict['neutron_port_id']):
+            session.query(models.HPIronicSwitchPortMapping).filter_by(
+                neutron_port_id=record_dict['neutron_port_id']).delete()
+
+
+def get_hp_ironic_swport_map_by_id(context, record_dict):
+    """Get ironic_switch_port_mapping that matches the supplied id."""
+    try:
+        query = context.session.query(models.HPIronicSwitchPortMapping)
+        port_mapping = query.filter_by(
+            neutron_port_id=record_dict['neutron_port_id']).all()
+    except exc.NoResultFound:
+        LOG.debug('no hp ironic switch port mapping found for neutron port%s',
+                  record_dict['neutron_port_id'])
+        return
+    return port_mapping
+
+
+def update_hp_switch_lag_port(context, rec_dict):
+    """Update hp_switch_lag_port."""
+    try:
+        with context.session.begin(subtransactions=True):
+            (context.session.query(models.HPSwitchLAGPort).filter_by(
+                id=rec_dict['id']).update(
+                    {'external_lag_id': rec_dict['external_lag_id']},
+                    synchronize_session=False))
+    except exc.NoResultFound:
+        LOG.debug('no lag port found for id %s',
+                  rec_dict['id'])
+
+
+def update_hp_ironic_swport_map_with_seg_id(context, rec_dict):
+    """Update hp_ironic_switch_port_mapping."""
+    try:
+        with context.session.begin(subtransactions=True):
+            (context.session.query(models.HPIronicSwitchPortMapping).filter_by(
+                neutron_port_id=rec_dict['neutron_port_id']).update(
+                    {'access_type': rec_dict['access_type'],
+                     'host_id': rec_dict['host_id'],
+                     'segmentation_id': rec_dict['segmentation_id']},
+                    synchronize_session=False))
+    except exc.NoResultFound:
+        LOG.debug('no ironic switch port mapping found for id %s',
+                  rec_dict['neutron_port_id'])
+
+
+def update_hp_ironic_swport_map_with_lag_id(context, rec_dict):
+    """Update hp_ironic_switch_port_mapping."""
+    try:
+        with context.session.begin(subtransactions=True):
+            (context.session.query(models.HPIronicSwitchPortMapping).filter_by(
+                neutron_port_id=rec_dict['neutron_port_id']).update(
+                    {'lag_id': rec_dict['id']},
+                    synchronize_session=False))
+    except exc.NoResultFound:
+        LOG.debug('no ironic switch port mapping found for id %s',
+                  rec_dict['neutron_port_id'])
+
+
+def update_hp_ironic_swport_map_with_host_id(context, rec_dict):
+    """Update hp_ironic_switch_port_mapping."""
+    try:
+        with context.session.begin(subtransactions=True):
+            (context.session.query(models.HPIronicSwitchPortMapping).filter_by(
+                neutron_port_id=rec_dict['neutron_port_id']).update(
+                    {'host_id': rec_dict['host_id']},
+                    synchronize_session=False))
+    except exc.NoResultFound:
+        LOG.debug('no ironic switch port mapping found for id %s',
+                  rec_dict['neutron_port_id'])
+
+
+def get_hp_switch_port_by_id(context, record_dict):
+    """Get hp_switch_port that matches the supplied switch id."""
+    try:
+        query = context.session.query(models.HPSwitchPort)
+        switch_port = query.filter_by(
+            id=record_dict['id']).one()
+    except exc.NoResultFound:
+        LOG.debug('no hp switch port found for %s',
+                  record_dict['id'])
+        return
+    return switch_port
+
+
+def update_hp_switch_ports_with_lag_id(context, rec_dict):
+    """Update hp switch ports with lag_id."""
+    try:
+        with context.session.begin(subtransactions=True):
+            (context.session.query(models.HPSwitchPort).filter_by(
+                id=rec_dict['id']).update(
+                    {'lag_id': rec_dict['lag_id']},
+                    synchronize_session=False))
+    except exc.NoResultFound:
+        LOG.debug('no hp port found for lag_id %s',
+                  rec_dict['lag_id'])
+
+
+def get_lag_id_by_neutron_port_id(context, record_dict):
+    """Get lag_id that matches the supplied port id."""
+    try:
+        query = context.session.query(models.HPIronicSwitchPortMapping)
+        switch_port = query.filter_by(
+            neutron_port_id=record_dict['neutron_port_id']).all()
+    except exc.NoResultFound:
+        LOG.debug('no hp lag_id  found for %s',
+                  record_dict['neutron_port_id'])
+        return
+    return switch_port
+
+
+def get_ext_lag_id_by_lag_id(context, record_dict):
+    """Get ext_lag_id  that matches the supplied neutron lag id."""
+    try:
+        query = context.session.query(models.HPSwitchLAGPort)
+        switch_port = query.filter_by(
+            id=record_dict['id']).one()
+    except exc.NoResultFound:
+        LOG.debug('no hp lag_id  found for %s',
+                  record_dict['id'])
+        return
+    return switch_port
+
+
+def get_subnets_by_network(context, network_id):
+        subnet_qry = context.session.query(models_v2.Subnet)
+        return subnet_qry.filter_by(network_id=network_id).all()
---
+++ packages/baremetal-network-provisioning/baremetal-network-provisioning-0.1.tar.gz/baremetal-network-provisioning-0.1/baremetal_network_provisioning/tests/unit/db/test_bm_nw_provision_db.py
@@ -0,0 +1,194 @@
+# Copyright (c) 2015 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from neutron import context
+from neutron.tests.unit import testlib_api
+
+from baremetal_network_provisioning.db import bm_nw_provision_db as db
+from baremetal_network_provisioning.db import bm_nw_provision_models as models
+
+
+class NetworkProvisionDBTestCase(testlib_api.SqlTestCase):
+    """Test all network provisioning db helper methods."""
+
+    def setUp(self):
+        super(NetworkProvisionDBTestCase, self).setUp()
+        self.ctx = context.get_admin_context()
+
+    def _get_switch_port_dict(self):
+        """Get a switch port dict."""
+        rec_dict = {'id': "1234",
+                    'switch_id': "test_switch1",
+                    'port_name': "Tengig0/1",
+                    'lag_id': "lag1234"}
+        return rec_dict
+
+    def _get_switch_lag_port_dict(self):
+        """Get a switch lag port dict."""
+        rec_dict = {'id': "lag1234",
+                    'external_lag_id': "extlag123"}
+        return rec_dict
+
+    def _get_ironic_switch_port_map_dict(self):
+        """Get a ironic switch port map dict."""
+        rec_dict = {'neutron_port_id': "n1234",
+                    'switch_port_id': "1234",
+                    'lag_id': "lag1234",
+                    'access_type': "access",
+                    'segmentation_id': 100,
+                    'host_id': 'ironic'}
+        return rec_dict
+
+    def test_add_hp_switch_lag_port(self):
+        """Test add_hp_switch_lag_port method."""
+        rec_dict = self._get_switch_lag_port_dict()
+        db.add_hp_switch_lag_port(self.ctx, rec_dict)
+        count = self.ctx.session.query(models.HPSwitchLAGPort).count()
+        self.assertEqual(1, count)
+
+    def test_add_hp_switch_port(self):
+        """Test add_hp_switch_port method."""
+        self._add_switch_and_lag_port()
+        count = self.ctx.session.query(models.HPSwitchPort).count()
+        self.assertEqual(1, count)
+
+    def test_add_hp_ironic_switch_port_mapping(self):
+        """Test hp_ironic_switch_port_mapping."""
+        self._add_switch_and_lag_port()
+        rec_dict = self._get_ironic_switch_port_map_dict()
+        db.add_hp_ironic_switch_port_mapping(self.ctx, rec_dict)
+        count = self.ctx.session.query(
+            models.HPIronicSwitchPortMapping).count()
+        self.assertEqual(1, count)
+
+    def test_get_hp_switch_port_by_switchid_portname(self):
+        """Test get_hp_switch_port_by_switchid_portname method."""
+        self._add_switch_and_lag_port()
+        result = db.get_all_hp_sw_port_by_swchid_portname(
+            self.ctx,
+            {'switch_id': "test_switch1", 'port_name': "Tengig0/1"})
+        self.assertEqual('test_switch1', result[0].switch_id)
+
+    def test_get_hp_switch_lag_port_by_id(self):
+        """Test get_hp_switch_lag_port_by_id method."""
+        self._add_switch_and_lag_port()
+        result = db.get_hp_switch_lag_port_by_id(
+            self.ctx, {'id': "lag1234"})
+        self.assertEqual('lag1234', result.id)
+
+    def test_get_hp_ironic_swport_map_by_id(self):
+        """Test get_hp_ironic_swport_map_by_id method."""
+        self._add_switch_and_lag_port()
+        rec_dict = self._get_ironic_switch_port_map_dict()
+        db.add_hp_ironic_switch_port_mapping(self.ctx, rec_dict)
+        result = db.get_hp_ironic_swport_map_by_id(
+            self.ctx, {'neutron_port_id': "n1234"})
+        self.assertEqual("n1234", result[0].neutron_port_id)
+
+    def test_update_hp_switch_lag_port(self):
+        """Test update_hp_switch_lag_port method."""
+        rec_dict = self._get_switch_lag_port_dict()
+        db.add_hp_switch_lag_port(self.ctx, rec_dict)
+        db.update_hp_switch_lag_port(
+            self.ctx, {'id': "lag1234", 'external_lag_id': "extlag456"})
+        result = db.get_hp_switch_lag_port_by_id(
+            self.ctx, {'id': "lag1234"})
+        self.assertEqual("extlag456", result.external_lag_id)
+
+    def test_update_hp_ironic_swport_map_with_seg_id(self):
+        """Test update_hp_ironic_swport_map_with_seg_id method."""
+        self._add_switch_and_lag_port()
+        rec_dict = self._get_ironic_switch_port_map_dict()
+        db.add_hp_ironic_switch_port_mapping(self.ctx, rec_dict)
+        db.update_hp_ironic_swport_map_with_seg_id(
+            self.ctx, {'neutron_port_id': "n1234",
+                       'access_type': "access",
+                       'segmentation_id': 200,
+                       'host_id': 'ironic'})
+        result = db.get_hp_ironic_swport_map_by_id(
+            self.ctx, {'neutron_port_id': "n1234"})
+        self.assertEqual(200, result[0].segmentation_id)
+
+    def test_get_hp_switch_port_by_id(self):
+        """Test get_hp_switch_port_by_switchid_portname method."""
+        self._add_switch_and_lag_port()
+        result = db.get_hp_switch_port_by_id(
+            self.ctx,
+            {'id': "1234"})
+        self.assertEqual("1234", result.id)
+
+    def test_delete_hp_switch_port(self):
+        """Test delete_hp_switch_port method."""
+        self._add_switch_and_lag_port()
+        sw_rec_dict = self._get_switch_port_dict()
+        db.delete_hp_switch_port(self.ctx, sw_rec_dict)
+        count = self.ctx.session.query(models.HPSwitchPort).count()
+        self.assertEqual(count, 0)
+
+    def test_delete_hp_ironic_switch_port_mapping(self):
+        """Test delete_hp_ironic_switch_port_mapping."""
+        self._add_switch_and_lag_port()
+        rec_dict = self._get_ironic_switch_port_map_dict()
+        db.add_hp_ironic_switch_port_mapping(self.ctx, rec_dict)
+        db.delete_hp_ironic_switch_port_mapping(self.ctx, rec_dict)
+        count = self.ctx.session.query(
+            models.HPIronicSwitchPortMapping).count()
+        self.assertEqual(count, 0)
+
+    def test_delete_hp_switch_lag_port(self):
+        """Test delete_hp_switch_lag_port method."""
+        self._add_switch_and_lag_port()
+        lag_dict = self._get_switch_lag_port_dict()
+        db.delete_hp_switch_lag_port(self.ctx, lag_dict)
+        count = self.ctx.session.query(models.HPSwitchLAGPort).count()
+        self.assertEqual(count, 0)
+
+    def _add_switch_and_lag_port(self):
+        """Add entries to hpswitchports and hpswitchlagports."""
+        sw_rec_dict = self._get_switch_port_dict()
+        lag_dict = self._get_switch_lag_port_dict()
+        db.add_hp_switch_lag_port(self.ctx, lag_dict)
+        db.add_hp_switch_port(self.ctx, sw_rec_dict)
+
+    def test_update_hp_ironic_swport_map_with_lag_id(self):
+        """Test update_hp_ironic_swport_map_with_lag_id method."""
+        self._add_switch_and_lag_port()
+        rec_dict = self._get_ironic_switch_port_map_dict()
+        lag_dict = {'id': "lag1234",
+                    'external_lag_id': "1234",
+                    'neutron_port_id': "n1234"}
+        db.add_hp_ironic_switch_port_mapping(self.ctx, rec_dict)
+        db.update_hp_ironic_swport_map_with_lag_id(
+            self.ctx, lag_dict)
+        result = db.get_hp_ironic_swport_map_by_id(
+            self.ctx, {'neutron_port_id': "n1234"})
+        self.assertEqual("lag1234", result[0].lag_id)
+
+    def test_update_hp_swport_with_lag_id(self):
+        """Test update_hp_swport_with_lag_id method."""
+        self._add_switch_and_lag_port()
+        lag_dict = {'id': "lag1234", 'lag_id': "1234"}
+        db.update_hp_switch_ports_with_lag_id(self.ctx, lag_dict)
+        result = db.get_hp_switch_port_by_id(
+            self.ctx,
+            {'id': "1234"})
+        self.assertEqual("lag1234", result.lag_id)
+
+    def test_get_ext_lag_id_by_id(self):
+        """Test get_ext_lag_id_by_id method."""
+        self._add_switch_and_lag_port()
+        result = db.get_ext_lag_id_by_lag_id(
+            self.ctx, {'id': "lag1234"})
+        self.assertEqual('extlag123', result.external_lag_id)
