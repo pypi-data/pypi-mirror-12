9c35fcdf6195d3edada7c6064bad6dfd58716d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:15 +0000

    Add package address_extractor/0.1.0.post1/address_extractor-0.1.0.post1.tar.gz

---
+++ packages/address_extractor/address_extractor-0.1.0.post1.tar.gz/address_extractor-0.1.0.post1/src/address_extractor/__init__.py
@@ -0,0 +1,19 @@
+from .__main__ import main
+from .__main__ import parsed_address_to_human
+
+__version__ = '0.1.0post1'
+
+__title__ = 'address_extractor'
+__description__ = 'A script to extract US-style street addresses from a text file.'
+__url__ = 'https://github.com/scolby33/address_extractor'
+
+__author__ = 'Scott Colby'
+__email__ = 'scolby33@gmail.com'
+
+__license__ = 'MIT License'
+__copyright__ = 'Copyright (c) 2015 Scott Colby'
+
+__all__ = [
+    'main',
+    'parsed_address_to_human'
+]
