5e0d5377f2b648f41b6abdb483e9fe2cce1baa6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:18 +0000

    Add package catest1/1.0.2/catest1-1.0.2.zip

---
+++ packages/catest1/catest1-1.0.2.zip/catest1-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'catest1',
+	version = '1.0.2',
+	py_modules = ['catest1'],
+	author = 'python_CAT007',
+	author_email = 'jingyan_qiu@hotmail.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of lists for cat\'s tests',
+      )
