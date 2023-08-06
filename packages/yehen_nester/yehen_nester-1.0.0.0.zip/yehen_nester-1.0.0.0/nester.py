3cd4bbf6fdef8d5516637b3dc6cbb0a48b79a2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:41 +0000

    Add package yehen_nester/1.0.0.0/yehen_nester-1.0.0.0.zip

---
+++ packages/yehen_nester/yehen_nester-1.0.0.0.zip/yehen_nester-1.0.0.0/nester.py
@@ -0,0 +1,12 @@
+"""这是"nester.py"模块，提供了一个名为print_lol()的函数，这个函数
+的作用是打印列表，其中有可能包含（也肯能不包含）嵌套列表"""
+def print_lol(the_list):
+        """This function takes one positional argument called "the_list",which
+is any Python list(of-possibly-nested lsits).Each data item in the provided
+list is (recursivley printed to the screen on it's onw line"""
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item)
+                else:
+                        print(each_item)
+                        
