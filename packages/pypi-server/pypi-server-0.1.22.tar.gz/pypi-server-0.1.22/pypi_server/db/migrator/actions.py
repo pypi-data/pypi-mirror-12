ba693284db692abcd596bc310db660b7e8698f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:25 +0000

    Add package pypi-server/0.1.22/pypi-server-0.1.22.tar.gz

---
+++ packages/pypi-server/pypi-server-0.1.22.tar.gz/pypi-server-0.1.22/pypi_server/db/migrator/actions.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+from pypi_server.db.migrator import MIGRATIONS, log
+
+
+def migrate_db(DB, Migrations, migrator):
+    migrations = sorted(MIGRATIONS, key=lambda x: x[0])
+
+    q = Migrations.select(Migrations.id).order_by(Migrations.id.desc())
+
+    if q.count():
+        last_applied_migration = q[0].id
+    else:
+        last_applied_migration = -1
+
+    for migration_id, name, migration in filter(lambda x: x[0] > last_applied_migration, migrations):
+        try:
+            with DB.transaction():
+                log.info('Applying migration: "%s"', name)
+                migration(migrator, DB)
+                Migrations.create(
+                    id=migration_id,
+                    name=name
+                )
+        except Exception as e:
+            log.error("Migration failed.")
+            log.exception(e)
+            raise
+        finally:
+            DB.commit()
+
---
+++ packages/pypi-server/pypi-server-0.1.22.tar.gz/pypi-server-0.1.22/pypi_server/handlers/pypi/proxy/client.py
@@ -0,0 +1,106 @@
+# encoding: utf-8
+import logging
+from copy import copy
+from tornado.gen import coroutine, Return
+from tornado.httpclient import AsyncHTTPClient
+from tornado.ioloop import IOLoop
+from tornado.locks import Lock
+from tornado_xmlrpc.client import ServerProxy
+from pypi_server.cache import Cache, HOUR
+from pypi_server.hash_version import HashVersion
+
+
+log = logging.getLogger(__name__)
+
+
+def normalize_package_name(name):
+    return name.lower().replace("_", "-")
+
+
+class PYPIClient(object):
+    CLIENT = None
+    BACKEND = None
+    THREAD_POOL = None
+    INDEX = None
+    XMLRPC = None
+    LOCK = None
+
+    @classmethod
+    def configure(cls, backend, thread_pool):
+        cls.CLIENT = AsyncHTTPClient(io_loop=IOLoop.current())
+        cls.BACKEND = backend
+        cls.THREAD_POOL = thread_pool
+        cls.XMLRPC = ServerProxy(
+            str(copy(backend)(path="/pypi")),
+        )
+        cls.LOCK = Lock()
+
+    @classmethod
+    @coroutine
+    @Cache(24 * HOUR)
+    def packages(cls):
+        with (yield cls.LOCK.acquire()):
+            index = dict(
+                map(
+                    lambda x: (x.lower().replace("_", "-"), x),
+                    (yield cls.XMLRPC.list_packages())
+                )
+            )
+
+            log.info("Remote PYPI index updated: %d packages", len(index))
+            raise Return(index)
+
+    @classmethod
+    @coroutine
+    @Cache(HOUR)
+    def search(cls, names, descriptions, operator="or"):
+        assert operator in ('or', 'and')
+        result = yield cls.XMLRPC.search({'name': names, 'description': descriptions}, operator)
+        raise Return(result)
+
+    @classmethod
+    @coroutine
+    def exists(cls, name):
+        try:
+            real_name = yield cls.find_real_name(name)
+        except LookupError:
+            raise Return(False)
+
+        releases = yield cls.releases(real_name)
+        if not releases:
+            raise Return(False)
+
+        raise Return(True)
+
+    @classmethod
+    @coroutine
+    def find_real_name(cls, name):
+        name = normalize_package_name(name)
+
+        packages = yield cls.packages()
+        real_name = packages.get(name.lower())
+
+        if real_name is None:
+            raise LookupError("Package not found")
+
+        raise Return(real_name)
+
+    @classmethod
+    @coroutine
+    @Cache(HOUR)
+    def releases(cls, name):
+        raise Return(set(map(HashVersion, (yield cls.XMLRPC.package_releases(name, True)))))
+
+    @classmethod
+    @coroutine
+    @Cache(4 * HOUR)
+    def release_data(cls, name, version):
+        info = yield cls.XMLRPC.release_data(str(name), str(version))
+        raise Return(info)
+
+    @classmethod
+    @coroutine
+    @Cache(4 * HOUR)
+    def release_files(cls, name, version):
+        info = yield cls.XMLRPC.release_urls(str(name), str(version))
+        raise Return(sorted(info, key=lambda x: x['filename']))
