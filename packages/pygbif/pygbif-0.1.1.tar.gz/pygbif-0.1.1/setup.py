c3ad112b40bf616127ca6d466947f40f45de24f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:40 +0000

    Add package pygbif/0.1.1/pygbif-0.1.1.tar.gz

---
+++ packages/pygbif/pygbif-0.1.1.tar.gz/pygbif-0.1.1/setup.py
@@ -0,0 +1,34 @@
+import codecs
+from setuptools import setup
+from setuptools import find_packages
+
+with codecs.open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
+with codecs.open('CHANGES.txt', 'r', 'utf-8') as f:
+    changes = f.read()
+
+long_description = readme + '\n\n' + changes
+
+setup(
+  name             = 'pygbif',
+	version          = '0.1.1',
+	description      = 'Python client for GBIF',
+  long_description = long_description,
+  author           = 'Scott Chamberlain',
+  author_email     = 'myrmecocystus@gmail.com',
+  url              = 'http://github.com/sckott/pygbif',
+  license          = "MIT",
+  packages         = find_packages(exclude=['test-*']),
+  install_requires = ['requests>2.7'],
+  classifiers      = (
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Natural Language :: English',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7'
+	)
+)
