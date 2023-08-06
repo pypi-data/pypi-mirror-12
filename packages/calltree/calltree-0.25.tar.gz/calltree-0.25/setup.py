0eda4fef581423440e74703589fb2e15905d393c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:56 +0000

    Add package calltree/0.25/calltree-0.25.tar.gz

---
+++ packages/calltree/calltree-0.25.tar.gz/calltree-0.25/setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+  name = 'calltree',
+  packages_dir = {'calltree' : ' '},
+  packages = ['calltree'],
+  version = '0.25',
+  description = 'Show Callgraph from cscope database',
+  author = 'Vadivel',
+  author_email = 'vadivelmurugank@gmail.com',
+  url = 'https://github.com/vadivelmurugank/calltree',
+  license = 'MIT',
+)
