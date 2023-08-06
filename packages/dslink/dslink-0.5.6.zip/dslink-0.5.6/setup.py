cafddd101ec2a0eb2a0012a3b0dd4286e17cfcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:07 +0000

    Add package dslink/0.5.6/dslink-0.5.6.zip

---
+++ packages/dslink/dslink-0.5.6.zip/dslink-0.5.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import sys
+
+requirements = [
+    "autobahn",
+    "pyelliptic",
+    "requests",
+    "twisted",
+    "zope.interface"
+]
+
+if sys.version_info < (3, 3):
+    requirements.append("enum34")
+
+setup(
+    name="dslink",
+    version="0.5.6",
+    description="DSLink SDK for Python",
+    url="http://github.com/IOT-DSA/sdk-dslink-python",
+    author="Logan Gorence",
+    author_email="l.gorence@dglogik.com",
+    license="Apache 2.0",
+    packages=[
+        "dslink"
+    ],
+    install_requires=requirements,
+    use_2to3=True
+)
