fa9fcaa0b6259b921e0dafc7cad36423fe52fd35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:01 +0000

    Add package nester_lcy/1.0.0/nester_lcy-1.0.0.zip

---
+++ packages/nester_lcy/nester_lcy-1.0.0.zip/nester_lcy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name = "nester_lcy",
+    version = "1.0.0",
+    py_modules = ['nester'],
+    author = "lcy_python",
+    author_email = "leecoyo@sina.cn",
+    url = "http://www/leecoyo.com",
+    description = "a simple printer of nested lists",
+
+)
