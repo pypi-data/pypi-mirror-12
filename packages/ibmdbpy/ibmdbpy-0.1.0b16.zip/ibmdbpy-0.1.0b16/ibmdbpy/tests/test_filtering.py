c467d8c0f38784bd50811aab1ca869e52e5bfe32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:33 +0000

    Add package ibmdbpy/0.1.0b16/ibmdbpy-0.1.0b16.zip

---
+++ packages/ibmdbpy/ibmdbpy-0.1.0b16.zip/ibmdbpy-0.1.0b16/ibmdbpy/tests/test_filtering.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------------------------
+# Copyright (c) 2015, IBM Corp.
+# All rights reserved.
+#
+# Distributed under the terms of the BSD Simplified License.
+#
+# The full license is in the LICENSE file, distributed with this software.
+#-----------------------------------------------------------------------------
+
+"""
+Test module for IdaDataFrameObjects
+"""
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+
+class Test_Filtering(object):
+    # Test combinations
+    def test_Filtering_lt(self, idadf):
+        pass
+
+    def test_Filtering_le(self, idadf):
+        pass
+
+    def test_Filtering_eq(self, idadf):
+        pass
+
+    def test_Filtering_ge(self, idadf):
+        pass
+
+    def test_Filtering_gt(self, idadf):
+        pass
+
+
+class Test_FilterQuery(object):
+    # Test combinations
+    def test_FilterQuery_and(self, idadf):
+        pass
+
+    def test_FilterQuery_or(self, idadf):
+        pass
+
+    def test_FilterQuery_xor(self, idadf):
+        pass
+
+    def test_FilterQuery_error(self, idadf):
+        pass
