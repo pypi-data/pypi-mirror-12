66abf7f94963bfbd40f6f0501fc12c376227f7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:28 +0000

    Add package python-evoqueclient/0.0.1.dev3/python-evoqueclient-0.0.1.dev3.tar.gz

---
+++ packages/python-evoqueclient/python-evoqueclient-0.0.1.dev3.tar.gz/python-evoqueclient-0.0.1.dev3/evoqueclient/v1/shell.py
@@ -0,0 +1,28 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from evoqueclient.common import utils
+
+
+def do_ticket_list(ec, args={}):
+    """List all available tickets."""
+    tickets = ec.ticket.list()
+    field_labels = ["ID", "Name"]
+    fields = ["id", "name"]
+    utils.print_list(tickets, fields, field_labels)
+
+
+@utils.arg("name", metavar="<TICKET_NAME>",
+           help="Ticket name.")
+def do_ticket_create(ec, args):
+    """Create a ticket."""
+    ec.tickets.add({"name": args.name})
