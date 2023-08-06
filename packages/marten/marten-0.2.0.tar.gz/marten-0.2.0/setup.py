604d3fa0fd09fb62eb0b389127434a8a77612f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:15 +0000

    Add package marten/0.2.0/marten-0.2.0.tar.gz

---
+++ packages/marten/marten-0.2.0.tar.gz/marten-0.2.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+from marten import __version__
+
+def parse_requirements(requirements):
+	with open(requirements) as f:
+		return [l.strip('\n') for l in f if l.strip('\n') and not l.startswith('#')]
+
+with open('README.md') as f:
+	long_description = f.read().strip()
+
+test_requirements = parse_requirements('test-requirements.txt')
+
+setup(
+	name='marten',
+	version=__version__,
+	packages=find_packages(exclude=('tests.*', 'tests',)),
+	url='https://github.com/nick-allen/marten',
+	license='MIT',
+	author='Nick Allen',
+	author_email='nick.allen.cse@gmail.com',
+	description='Simple Python configuration management',
+	long_description=long_description,
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=parse_requirements('requirements.txt'),
+	extras_require={
+		'test': test_requirements
+	},
+	entry_points={
+		'console_scripts': [
+			'marten = marten.cli:marten_cli'
+		]
+	},
+	test_suite='nose.collector',
+	tests_require=test_requirements
+)
