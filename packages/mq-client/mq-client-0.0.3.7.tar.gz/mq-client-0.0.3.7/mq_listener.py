b4c27830139e9f262bb52525d4228d1d8978fffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:33 +0000

    Add package mq-client/0.0.3.7/mq-client-0.0.3.7.tar.gz

---
+++ packages/mq-client/mq-client-0.0.3.7.tar.gz/mq-client-0.0.3.7/mq_listener.py
@@ -0,0 +1,46 @@
+import argparse
+import pika
+from mq_client import AsyncMQConsumer
+
+import logging
+
+logging.basicConfig(filename='listener.log',
+                    filemode='a',
+                    level=logging.DEBUG)
+
+logger = logging.getLogger('mq_listener')
+
+
+def on_message(channel, method, header, body):
+    global terminate
+    # Acknowledge message receipt
+    channel.basic_ack(method.delivery_tag)
+    print body
+    logger.info(body)
+
+
+def listen_and_echo(args):
+    consumer = AsyncMQConsumer(args.url,
+                               on_message=on_message,
+                               queue=args.queue,
+                               routing_key=args.routing_key,
+                               logger=logger)
+
+    consumer.run()
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', default="amqp://guest:guest@localhost:5672/%2F")
+    parser.add_argument('--exchange', default="test_exchange")
+    parser.add_argument('--exchange-type', default="direct")
+    parser.add_argument('--queue', default="test_queue")
+    parser.add_argument('--routing-key', default="mq.text")
+    args = parser.parse_args()
+
+    listen_and_echo(args)
+
+
+if __name__ == "__main__":
+    run()
+
---
+++ packages/mq-client/mq-client-0.0.3.7.tar.gz/mq-client-0.0.3.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='mq-client',
+    version='0.0.3.7',
+    url='https://github.com/deginner/mq-client',
+    py_modules=['mq_client', 'mq_listener', 'mq_publisher'],
+    author='deginner',
+    author_email='support@deginner.com',
+    description='A simple pika client implementation based on the examples.',
+    install_requires=['amqp', 'pika', 'tornado'],
+    entry_points = """
+    [console_scripts]
+    mqlisten = mq_listener:run
+    mqpublish = mq_publisher:run
+    """
+)
