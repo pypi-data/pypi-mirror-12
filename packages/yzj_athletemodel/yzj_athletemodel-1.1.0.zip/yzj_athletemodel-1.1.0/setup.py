1091f2490923b7c39380d9b1ace7d7bf3aac2ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:14 +0000

    Add package yzj_athletemodel/1.1.0/yzj_athletemodel-1.1.0.zip

---
+++ packages/yzj_athletemodel/yzj_athletemodel-1.1.0.zip/yzj_athletemodel-1.1.0/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup    #从发布工具导入setup函数
+
+setup(
+    name= 'yzj_athletemodel',
+    version='1.1.0',
+    py_modules=['yzj_athletemodel'],
+    author='yzj',
+    author_email='359618717@qq.com',
+    url='http://www.headfirstlabs.com',
+    description='athletemodel',
+ )
