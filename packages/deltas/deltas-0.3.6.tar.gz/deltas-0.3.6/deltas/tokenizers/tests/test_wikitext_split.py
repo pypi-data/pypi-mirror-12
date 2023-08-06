4dd6c63a1d94ee1f7af3be045f81fe4166388c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:15 +0000

    Add package deltas/0.3.6/deltas-0.3.6.tar.gz

---
+++ packages/deltas/deltas-0.3.6.tar.gz/deltas-0.3.6/deltas/tokenizers/tests/test_wikitext_split.py
@@ -0,0 +1,30 @@
+from nose.tools import eq_
+
+from ..wikitext_split import wikitext_split
+
+
+def test_wikitext_split():
+
+    input = "As a sentence, this includes punctuation. \n" + \
+            "\n" + \
+            "==Header!==\n" + \
+            "克·科伊尔 しメfoobar!" + \
+            "And then we have another sentence here!\n" + \
+            "https://website.gov?param=value\n" + \
+            "peoples' ain't d’encyclopédie\n" + \
+            "mailto:email@email.mail"
+
+    expecteds = ['As', ' ', 'a', ' ', 'sentence', ',', ' ', 'this', ' ',
+                 'includes', ' ', 'punctuation', '.', ' \n\n',
+                 '==', 'Header', '!', '==', '\n',
+                 '克', '·', '科', '伊', '尔', ' ', 'し', 'メ', 'foobar', '!',
+                 'And', ' ', 'then', ' ', 'we', ' ', 'have', ' ', 'another',
+                 ' ', 'sentence',
+                 ' ', 'here', '!', '\n', 'https://website.gov?param=value', '\n',
+                 'peoples\'', ' ', 'ain\'t', ' ', 'd’encyclopédie', '\n',
+                 'mailto:email@email.mail']
+
+    tokens = list(wikitext_split.tokenize(input))
+
+    for token, expected in zip(tokens, expecteds):
+        eq_(token, expected)
