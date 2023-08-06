988f01111ca578ffa2fd3d758c82adbd3eddffdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:47 +0000

    Add package pfr/0.2.1/pfr-0.2.1.tar.gz

---
+++ packages/pfr/pfr-0.2.1.tar.gz/pfr-0.2.1/pfr/__init__.py
@@ -0,0 +1,19 @@
+BASE_URL = 'http://www.pro-football-reference.com'
+
+import finders
+import decorators
+import utils
+import boxscores
+import players
+import teams
+
+# modules/variables to expose
+__all__ = [
+    'BASE_URL',
+    'finders',
+    'decorators',
+    'utils',
+    'boxscores'
+    'players',
+    'teams',
+]
