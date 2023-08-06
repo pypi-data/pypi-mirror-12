c1e4270ea8a221d0bac58a846afd1201fa391763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:14 +0000

    Add package intercom_python/1.0.4/intercom_python-1.0.4.tar.gz

---
+++ packages/intercom_python/intercom_python-1.0.4.tar.gz/intercom_python-1.0.4/intercom/client.py
@@ -0,0 +1,19 @@
+from models import *
+
+INTERCOM_API_BASE_URL = "https://api.intercom.io/"
+
+class IntercomClient(object):
+
+    def __init__(self, app_id, app_api_key):
+        self.app_id = app_id
+        self.app_api_key = app_api_key
+        self.users = IntercomUser(INTERCOM_API_BASE_URL, app_id, app_api_key)
+        self.companies = IntercomCompany(INTERCOM_API_BASE_URL, app_id, app_api_key)
+        self.contacts = IntercomContact(INTERCOM_API_BASE_URL, app_id, app_api_key)
+        self.admins = IntercomAdmin(INTERCOM_API_BASE_URL, app_id, app_api_key)
+        self.tags = IntercomTag(INTERCOM_API_BASE_URL, app_id, app_api_key)
+        self.segments = IntercomSegment(INTERCOM_API_BASE_URL, app_id, app_api_key)
+        self.notes = IntercomNote(INTERCOM_API_BASE_URL, app_id, app_api_key)
+        self.events = IntercomEvent(INTERCOM_API_BASE_URL, app_id, app_api_key)
+        self.counts = IntercomCount(INTERCOM_API_BASE_URL, app_id, app_api_key)
+        self.conversations = IntercomConversation(INTERCOM_API_BASE_URL, app_id, app_api_key)
---
+++ packages/intercom_python/intercom_python-1.0.4.tar.gz/intercom_python-1.0.4/intercom/models.py
@@ -0,0 +1,81 @@
+from .base import IntercomBaseModel
+
+class IntercomUser(IntercomBaseModel):
+
+    def __init__(self, base_request_url, app_id, app_api_key):
+        self.base_request_url = base_request_url
+        self.app_id = app_id
+        self.app_api_key = app_api_key
+        self.name = "users"
+
+class IntercomCompany(IntercomBaseModel):
+
+    def __init__(self, base_request_url, app_id, app_api_key):
+        self.base_request_url = base_request_url
+        self.app_id = app_id
+        self.app_api_key = app_api_key
+        self.name = "companies"
+
+class IntercomContact(IntercomBaseModel):
+
+    def __init__(self, base_request_url, app_id, app_api_key):
+        self.base_request_url = base_request_url
+        self.app_id = app_id
+        self.app_api_key = app_api_key
+        self.name = "contacts"
+
+class IntercomAdmin(IntercomBaseModel):
+
+    def __init__(self, base_request_url, app_id, app_api_key):
+        self.base_request_url = base_request_url
+        self.app_id = app_id
+        self.app_api_key = app_api_key
+        self.name = "admins"
+
+class IntercomTag(IntercomBaseModel):
+
+    def __init__(self, base_request_url, app_id, app_api_key):
+        self.base_request_url = base_request_url
+        self.app_id = app_id
+        self.app_api_key = app_api_key
+        self.name = "tags"
+
+class IntercomSegment(IntercomBaseModel):
+
+    def __init__(self, base_request_url, app_id, app_api_key):
+        self.base_request_url = base_request_url
+        self.app_id = app_id
+        self.app_api_key = app_api_key
+        self.name = "segments"
+
+class IntercomNote(IntercomBaseModel):
+
+    def __init__(self, base_request_url, app_id, app_api_key):
+        self.base_request_url = base_request_url
+        self.app_id = app_id
+        self.app_api_key = app_api_key
+        self.name = "notes"
+
+class IntercomEvent(IntercomBaseModel):
+
+    def __init__(self, base_request_url, app_id, app_api_key):
+        self.base_request_url = base_request_url
+        self.app_id = app_id
+        self.app_api_key = app_api_key
+        self.name = "events"
+
+class IntercomCount(IntercomBaseModel):
+
+    def __init__(self, base_request_url, app_id, app_api_key):
+        self.base_request_url = base_request_url
+        self.app_id = app_id
+        self.app_api_key = app_api_key
+        self.name = "count"
+
+class IntercomConversation(IntercomBaseModel):
+
+    def __init__(self, base_request_url, app_id, app_api_key):
+        self.base_request_url = base_request_url
+        self.app_id = app_id
+        self.app_api_key = app_api_key
+        self.name = "conversations"
