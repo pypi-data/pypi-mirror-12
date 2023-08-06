47dc154f77324c826136237609903902a96bd664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:12 +0000

    Add package djangorestversioning/0.2.1/djangorestversioning-0.2.1.tar.gz

---
+++ packages/djangorestversioning/djangorestversioning-0.2.1.tar.gz/djangorestversioning-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'djangorestversioning',
+  packages = ['djangorestversioning'],
+  version = '0.2.1',
+  description = 'A lightweight mixin to allow routing Django Rest Framework endpoints to different classes depending on version.',
+  author = 'Craig Tweedy',
+  author_email = 'ahoy@craigtweedy.co.uk',
+  url = 'https://github.com/craigtweedy/django-rest-versioning',
+  download_url = 'https://github.com/craigtweedy/django-rest-versioning/tarball/0.2.1',
+  keywords = ['django', 'django-rest-framework', 'version', 'versioning', 'api'],
+  classifiers = [],
+)
