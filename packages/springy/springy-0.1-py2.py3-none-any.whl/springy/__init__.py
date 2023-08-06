c17b13d9941c6fff971ee19fd661d28929098a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:24 +0000

    Add package springy/0.1/springy-0.1-py2.py3-none-any.whl

---
+++ packages/springy/springy-0.1-py2.py3-none-any.whl/springy/__init__.py
@@ -0,0 +1,6 @@
+from .indices import Index, registry
+from .helpers import query, parse, index, model_indices
+from .utils import autodiscover
+
+default_app_config = 'springy.apps.SpringyAppConfig'
+
