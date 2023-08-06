f237700dc254fb6aafd6a9206aa23ee5623e3df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:23 +0000

    Add package schachtlerypkon/1.1.0/schachtlerypkon-1.1.0.zip

---
+++ packages/schachtlerypkon/schachtlerypkon-1.1.0.zip/schachtlerypkon-1.1.0/schachtlerypkon.py
@@ -0,0 +1,15 @@
+"""Ein Modul um alle Einzeldaten einer Liste anzuzeigen"""
+def print_lvl(liste, ebene):
+    """Mit dieser Funktion wird sichergestellt, dass auch in Listen eingebettete Listen
+    einzeln dargestellt werden (die normale Tiefe beträgt bis zu 1000 Ebenen.
+    Die Funktion erwartet ein positionelles Argument namens "liste", das eine beliebige Python-Liste ist.
+    Mit Hilfe des zweiten Arguments wird angezeigt wenn eine liste in einer liste eingebettet ist."""
+
+    for element in liste:
+        if isinstance(element, list):
+            print_lvl(element, ebene+1)
+        else:
+            for tab in range(ebene):
+                print("\t", end='')
+            print(element)
+            
