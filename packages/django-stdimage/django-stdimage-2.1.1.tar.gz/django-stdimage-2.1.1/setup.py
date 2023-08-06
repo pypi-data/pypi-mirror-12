8b7fe1694e391dde1b27296b7f2ce1adfa58a9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:40 +0000

    Add package django-stdimage/2.1.1/django-stdimage-2.1.1.tar.gz

---
+++ packages/django-stdimage/django-stdimage-2.1.1.tar.gz/django-stdimage-2.1.1/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import Command, find_packages, setup
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys
+        import subprocess
+
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+
+setup(
+    name='django-stdimage',
+    version='2.1.1',
+    description='Django Standarized Image Field',
+    author='codingjoe',
+    url='https://github.com/codingjoe/django-stdimage',
+    author_email='info@johanneshoppe.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    packages=find_packages(exclude=[
+        "*.tests", "*.tests.*", "tests.*", "tests", ".egg-info"
+    ]),
+    include_package_data=True,
+    install_requires=[
+        'pillow>=2.5',
+        'progressbar2>=3.0.0',
+    ],
+    cmdclass={'test': PyTest},
+)
