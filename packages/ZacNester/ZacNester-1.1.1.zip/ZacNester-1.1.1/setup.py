1ac8966ba8c9b5f702cf6248673702cded8a634f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:21 +0000

    Add package ZacNester/1.1.1/ZacNester-1.1.1.zip

---
+++ packages/ZacNester/ZacNester-1.1.1.zip/ZacNester-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'ZacNester',
+	version = '1.1.1',
+	py_modules = ['nester'],
+	author = 'Zacland',
+	author_email = 'zac@zacland.nester',
+	url = 'http://www.zacland.net',
+	description = 'A simple Zac version of printer of nested lists',
+	)
