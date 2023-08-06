efeded7289d3daaab8d83876009f52cceffff94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:28 +0000

    Add package django-model-history2/0.9.0/django-model-history2-0.9.0_0.zip

---
+++ packages/django-model-history2/django-model-history2-0.9.0_0.zip/django-model-history2-0.9.0/setup.py
@@ -0,0 +1,35 @@
+import os
+from codecs import open
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django-model-history2',
+    version='0.9.0',
+    description='Utility to track changes in object models',
+    long_description=long_description,
+    license='MIT',
+    url='https://github.com/shamanis/django-model-history2',
+    author='Petr Bondarenko',
+    author_email='mdma.zone@gmail.com',
+    packages=find_packages(),
+    keywords='models history revert objects',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
