3232539352d0253e186197bd31a698eb8298bd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:55 +0000

    Add package ucnumber/0.1.0/ucnumber-0.1.0.tar.gz

---
+++ packages/ucnumber/ucnumber-0.1.0.tar.gz/ucnumber-0.1.0/setup.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+
+from setuptools import setup
+
+setup(name='ucnumber',
+      version='0.1.0',
+      description='Valida un número de alumno de la UC ',
+      url='https://github.com/mrpatiwi/uc-numero-alumno-python',
+      author='Patricio López',
+      author_email='patricio@lopezjuri.com',
+      license='MIT',
+      packages=['ucnumber'],
+      keywords=['uc', 'puc', 'chile', 'numero', 'alumno'],
+      zip_safe=False)
