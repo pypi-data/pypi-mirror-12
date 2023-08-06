ed9669dacc25c9042de323c78b586262a3c2bafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:19 +0000

    Add package onespacemedia-cms/1.9.14/onespacemedia-cms-1.9.14.tar.gz

---
+++ packages/onespacemedia-cms/onespacemedia-cms-1.9.14.tar.gz/onespacemedia-cms-1.9.14/cms/project_template/Gulpfile.babel.js
@@ -0,0 +1,42 @@
+/*****************************************************************************/
+/* Imports */
+/*****************************************************************************/
+// - Main
+import gulp from 'gulp';
+import runSequence from 'run-sequence';
+import del from 'del';
+
+// - Gulp modules
+import gulpLoadPlugins from 'gulp-load-plugins';
+const $ = gulpLoadPlugins();
+
+// - Gulp tasks
+import images from './{{ project_name }}/assets/gulp/tasks/images';
+import scripts from './{{ project_name }}/assets/gulp/tasks/scripts';
+import serve from './{{ project_name }}/assets/gulp/tasks/serve';
+import styles from './{{ project_name }}/assets/gulp/tasks/styles';
+
+// - Browser sync
+import browserSync from 'browser-sync';
+export const bs = browserSync.create();
+const reload = bs.reload;
+
+/*****************************************************************************/
+/* Tasks */
+/*****************************************************************************/
+// - Images
+gulp.task('images', images);
+
+// - SCSS/CSS Styles
+gulp.task('styles', styles);
+
+// - JS Scripts
+gulp.task('scripts', scripts);
+
+// - Browser sync
+gulp.task('serve', serve);
+
+// - Default task to use when deving
+gulp.task('default', (cb) => {
+  return runSequence('styles', 'scripts', 'serve', cb);
+});
---
+++ packages/onespacemedia-cms/onespacemedia-cms-1.9.14.tar.gz/onespacemedia-cms-1.9.14/cms/project_template/project_name/assets/gulp/tasks/styles.js
@@ -0,0 +1,58 @@
+/*****************************************************************************/
+/* Imports */
+/*****************************************************************************/
+// - Main
+import gulp from 'gulp';
+
+// - Gulp modules
+import gulpLoadPlugins from 'gulp-load-plugins';
+const $ = gulpLoadPlugins();
+
+// - Browser Sync
+import browserSync from 'browser-sync';
+const reload = browserSync.reload;
+
+// - Project config
+import config from './_config';
+
+export default () => {
+  // Browsers we support
+  const autoprefixerBrowsers = [
+    'last 2 versions',
+    'ie >= 9'
+  ];
+
+  return gulp.src(config.sass.src)
+    // Initialise source maps
+    .pipe($.sourcemaps.init())
+
+    // Process our SCSS to CSS
+    .pipe($.sass({
+      precision: 10,
+      stats: true,
+      includePaths: ['node_modules/normalize.css/']
+    }).on('error', $.sass.logError))
+
+    // PostCSS our vendor prefixes
+    .pipe($.autoprefixer(autoprefixerBrowsers))
+
+    // Convert viable px units to REM
+    .pipe($.pxtorem())
+
+    // Write our source map, the root is needed for Django funnyness
+    .pipe($.sourcemaps.write('./', {
+      includeContent: false,
+      sourceRoot: () => {
+        return '../../static'
+      }
+    }))
+
+    // Place our CSS in the location we link to
+    .pipe(gulp.dest(config.css.dist))
+
+    // Stream the changes to Browser Sync
+    .pipe(browserSync.stream({match: '**/*.css'}))
+
+    // Spit out the size to the console
+    .pipe($.size({title: 'styles'}));
+}
