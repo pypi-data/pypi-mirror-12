bfb305ba6505328262cff2c7c3a35c88a558962c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:29 +0000

    Add package ionize/0.16.2/ionize-0.16.2.tar.gz

---
+++ packages/ionize/ionize-0.16.2.tar.gz/ionize-0.16.2/ionize/Ion/BaseIon.py
@@ -0,0 +1,149 @@
+"""Module containing the BaseIon class."""
+import json
+import numpy as np
+import contextlib
+import operator
+from math import sqrt
+import warnings
+
+from .fixed_state import fixed_state
+from ..Solvent import Aqueous
+from ..constants import reference_temperature
+from ..serialize import _serialize
+
+
+@fixed_state
+class BaseIon(object):
+
+    """BaseIon class describing basic ion properties.
+
+    All ions on ionize should be subclassed from BaseIon.
+    """
+
+    _solvent = Aqueous
+
+    _state = ('name', 'reference_temperature')
+    _name = 'BaseIon'
+    _reference_temperature = reference_temperature
+
+    _context = None
+
+    def __repr__(self):
+        inner = []
+        for prop in self._state:
+            prop = str(prop)  # convert unicode to string
+            value = getattr(self, prop)
+            if isinstance(value, np.ndarray):
+                value = value.tolist()
+            elif isinstance(value, str):
+                value = str(value)
+            inner.append('{}={}'.format(str(prop), repr(value)))
+        return '{}({})'.format(type(self).__name__, ', '.join(inner))
+
+    def __str__(self):
+        return "{}('{}')".format(type(self).__name__, self.name)
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __eq__(self, other):
+        try:
+            assert self._state == other._state
+            for prop in self._state:
+                if isinstance(getattr(self, prop), np.ndarray):
+                    assert(np.all(getattr(self, prop) == getattr(other, prop)))
+                else:
+                    assert getattr(self, prop) == getattr(other, prop)
+            return True
+        except Exception as e:
+            return False
+
+    def serialize(self, nested=False, compact=False):
+        """Return a serialized representation of the ion.
+
+        If "nested" is true, returns a dictionary. Otherwise, returns
+        a json-formatted string. This string can be returned to an Ion object
+        using deserialize.
+        """
+        serial = {key: getattr(self, key) for key in self._state}
+        serial['__ion__'] = type(self).__name__
+
+        return _serialize(serial, nested, compact)
+
+    # TODO: figure out why this dumps text
+    def save(self, filename):
+        """Save a serialized Ion to a file."""
+        with open(filename, 'w') as file:
+            file.write(self.serialize())
+
+    def mobility(self):
+        """The effective mobility of the ion.
+
+        Mobility must be overridden by subclasses."""
+        raise NotImplementedError
+
+    def diffusivity(self):
+        """The diffusivity of the ion.
+
+        Diffusivity must be overridden by subclasses."""
+        raise NotImplementedError
+
+    def molar_conductivity(self):
+        """The molar conductivity of the ion, in S/m/M
+
+        molar_conductivity must be overridden by subclasses."""
+        raise NotImplementedError
+
+    def charge(self, moment=1):
+        """The charge of the ion.
+
+        molar_conductivity must be overridden by subclasses."""
+        raise NotImplementedError
+
+    def context(self, context=False):
+        """Control the context of the ion.
+
+        The context is used as a convenience to set pH, temperature,
+        ionic_strength, and other information used in calculations about
+        the ion. If no
+
+        """
+        if context is False:
+            return self._context
+
+        # Update to new context, save old_context
+        old_context, self._context = self._context, context
+
+        # Return a context manager to revert to old_context
+        @contextlib.contextmanager
+        def manager():
+            yield
+            self._context = old_context
+        return manager()
+
+    def _resolve_context(self, pH, ionic_strength, temperature):
+        if pH is None:
+            try:
+                pH = self.context().pH
+            except AttributeError:
+                pH = None
+
+        if temperature is None:
+            try:
+                temperature = self.context().temperature() or \
+                              self.reference_temperature
+            except AttributeError:
+                temperature = self.reference_temperature
+
+        if pH is not None:
+            lower_limit = self._solvent.ionic_strength(pH, temperature)
+        else:
+            lower_limit = sqrt(self._solvent.dissociation(0., temperature))
+
+        if ionic_strength is None:
+            try:
+                ionic_strength = self.context().ionic_strength
+            except AttributeError:
+                ionic_strength = lower_limit
+
+        return pH, ionic_strength, temperature
---
+++ packages/ionize/ionize-0.16.2.tar.gz/ionize-0.16.2/ionize/Ion/__init__.py
@@ -0,0 +1,102 @@
+"""Module containing the Ion class."""
+from __future__ import division
+import warnings
+from math import copysign
+import json
+import numpy as np
+
+from .BaseIon import BaseIon
+from ..constants import reference_temperature, boltzmann, kelvin, \
+                        elementary_charge
+from .fixed_state import fixed_state
+
+
+@fixed_state
+class Ion(BaseIon):
+
+    """Describe an ion dissolved in aqueous solution."""
+
+    _state = ('name',
+              'valence',
+              'reference_pKa',
+              'reference_mobility',
+              'reference_temperature',
+              'enthalpy',
+              'heat_capacity',
+              'nightingale_data',
+              'molecular_weight')
+
+    # The reference properties of the ion are stored.
+    _valence = None
+    _reference_pKa = None
+    _reference_mobility = None
+    _reference_temperature = reference_temperature
+    _pKa = None
+    _absolute_mobility = None
+    _enthalpy = None
+    _heat_capacity = None
+    _nightingale_data = None
+    _molecular_weight = None
+
+    # Polynomial function is a derived parameter.
+    _nightingale_function = None
+
+    def __init__(self, name, valence, reference_pKa, reference_mobility,
+                 reference_temperature=None, enthalpy=None, heat_capacity=None,
+                 nightingale_data=None, molecular_weight=None):
+        """Initialize an Ion object."""
+
+        self._name = str(name)
+        self._valence = np.int_(valence)
+
+        if len(self.valence) > 1:
+            assert np.all(np.diff(self.valence) > 0), \
+                'Valences must be sorted.'
+
+        self._reference_pKa = np.float_(reference_pKa)
+        self._reference_mobility = np.float_(reference_mobility)
+
+        if reference_temperature is not None:
+            self._reference_temperature = float(reference_temperature)
+
+        if enthalpy is not None:
+            self._enthalpy = np.float_(enthalpy)
+            assert len(enthalpy) == len(self.reference_pKa)
+
+        if heat_capacity is not None:
+            self._heat_capacity = np.float_(heat_capacity)
+
+        self._molecular_weight = molecular_weight
+
+        for prop in ('reference_pKa',
+                     'reference_mobility',
+                     'enthalpy',
+                     'heat_capacity'):
+            if getattr(self, prop) is not None:
+                assert getattr(self, prop).shape == self.valence.shape, \
+                    '{} must have the same shape as valence.'.format(prop)
+
+        assert np.all((self.reference_mobility / self.valence) > 0.), \
+            'Mobilities must be signed. {}, {}'.format(self.reference_mobility,
+                                                       self.valence)
+
+        if nightingale_data is not None:
+            self._nightingale_data = nightingale_data
+            self._nightingale_function = \
+                np.poly1d(self.nightingale_data['fit'])
+
+    def _valence_zero(self):
+        """Create a list of charge states with 0 inserted."""
+        return np.sort(np.append(self.valence, [0]))
+
+    from .acidity import pKa, acidity, _clark_glew_pKa, \
+        _clark_glew_acidity, _vant_hoff_acidity, _vant_hoff_pKa
+
+    from .ionization import acidity_product, ionization_fraction, charge
+
+    from .mobility import absolute_mobility, actual_mobility, \
+        mobility, robinson_stokes_mobility, onsager_fuoss_mobility
+
+    from .transport import molar_conductivity, diffusivity
+
+    from .separation import separability
