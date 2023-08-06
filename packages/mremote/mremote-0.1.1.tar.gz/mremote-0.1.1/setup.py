9d0ec65deb85dde65f401fe686777ad1fe646d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:38 +0000

    Add package mremote/0.1.1/mremote-0.1.1.tar.gz

---
+++ packages/mremote/mremote-0.1.1.tar.gz/mremote-0.1.1/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    'flask',
+    'numpy',
+    'scipy',
+    'flask-bootstrap'
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='mremote',
+    version='0.1.1',
+    description="Generic LIRC web controller",
+    long_description=readme + '\n\n' + history,
+    author="David Francos Cuartero",
+    author_email='me@davidfrancos.net',
+    url='https://github.com/XayOn/mremote',
+    packages=[
+        'mremote',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mremote = mremote.mremote:main'
+        ]
+    },
+    package_dir={'mremote':
+                 'mremote'},
+    include_package_data=True,
+    package_data={
+        'mremote': ['*.json', 'templates/*.html']
+    },
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='mremote',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
