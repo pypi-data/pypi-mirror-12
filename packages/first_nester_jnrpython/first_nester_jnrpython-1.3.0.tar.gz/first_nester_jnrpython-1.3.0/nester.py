2156ecce997439d5f5e1583b6568b0e7365d50b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:33 +0000

    Add package first_nester_jnrpython/1.3.0/first_nester_jnrpython-1.3.0.tar.gz

---
+++ packages/first_nester_jnrpython/first_nester_jnrpython-1.3.0.tar.gz/first_nester_jnrpython-1.3.0/nester.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Esta é o modulo "nester.py" e ele fornece uma função chamada print_lol() que imprime listas que 
+podem ou não incluir listas aninhadas."""
+def print_lol(the_list, indent=False, level=0):
+    """Essa funcao tem um argumento posicional chamado "the_list" que é uma lista Python qualquer (talvez aninhada). 
+    Cada item   na lista fornecida é (recursivamente) imprimida na tela na sua própria linha. Um segundo argumento 
+    chamado "level" é usado para inserir tab-stops quando uma lista aninhada é encontrada."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end=" ")
+            print(each_item)
---
+++ packages/first_nester_jnrpython/first_nester_jnrpython-1.3.0.tar.gz/first_nester_jnrpython-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name          = 'first_nester_jnrpython',
+        version       = '1.3.0',
+        py_modules    = ['nester'],
+        author        = 'Jean Nunes',
+        author_email  = 'jean.to@gmail.com',
+        url           = 'www.google.com.br',
+        description   = 'A simple printer of nested lists with recursion',
+    )
