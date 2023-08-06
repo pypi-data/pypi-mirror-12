1da7eaa3073f4d302580d01966a271398ec54d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:49 +0000

    Add package jaraco.mongodb/3.9.3/jaraco.mongodb-3.9.3.tar.gz

---
+++ packages/jaraco.mongodb/jaraco.mongodb-3.9.3.tar.gz/jaraco.mongodb-3.9.3/jaraco/mongodb/monitor-index-creation.py
@@ -0,0 +1,21 @@
+import sys
+import time
+import re
+from jaraco.mongodb import helper
+
+def is_index_op(op):
+	return op.get('query', {}).get('createIndexes')
+
+def run():
+	db = helper.connect_db(sys.argv[1])
+
+	while True:
+		ops = db.current_op()['inprog']
+		index_op = next(filter(is_index_op, ops))
+		msg = index_op['msg']
+		name = index_op['query']['indexes'][0]['name']
+		msg = re.sub('Index Build( \(background\))?', name, msg)
+		print(msg, end='\r')
+		time.sleep(5)
+
+__name__ == '__main__' and run()
