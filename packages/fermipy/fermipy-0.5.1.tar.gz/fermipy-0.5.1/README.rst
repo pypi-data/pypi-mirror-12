366dfa0b6f4815bebb4317583ce77d82bc621438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:45 +0000

    Add package fermipy/0.5.1/fermipy-0.5.1.tar.gz

---
+++ packages/fermipy/fermipy-0.5.1.tar.gz/fermipy-0.5.1/README.rst
@@ -0,0 +1,15 @@
+FermiPy
+=======
+
+Fermi Python Analysis Framework
+
+* Documentation: http://fermipy.readthedocs.org/
+* Installation: http://fermipy.readthedocs.org/en/latest/install.html
+
+License
+-------
+This project is licensed under a 3-clause BSD style license - see the
+``licenses/LICENSE.rst`` file.
+
+
+
---
+++ packages/fermipy/fermipy-0.5.1.tar.gz/fermipy-0.5.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import re
+
+from fermipy.version import get_git_version
+
+setup(name='fermipy',
+      version=get_git_version(),
+      author='The Fermipy developers',
+      license='BSD',
+      packages=['fermipy','fermipy.config','fermipy.catalogs'],
+      package_data = {
+        '' : ['*yaml','*xml','*fit'],
+        'fermipy.catalogs' : ['Extended_archive_v14/*fits',
+                              'Extended_archive_v14/*xml',
+                              'Extended_archive_v14/*/*fits',
+                              'Extended_archive_v14/*/*xml']},
+      include_package_data = True, 
+      url = "https://github.com/fermiPy/fermipy",
+      scripts = [],
+      data_files=[('fermipy',['fermipy/_version.py'])],
+      install_requires=['numpy >= 1.6.1',
+                        'matplotlib >= 1.1.0',
+                        'astropy >= 0.4',
+                        'pyyaml',
+                        'healpy',
+                        'ez_setup',
+                        'pywcsgrid2 >= 1.0-b3',
+                        'scipy >= 0.13'])
