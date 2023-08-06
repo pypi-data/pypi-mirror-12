06fb0f163d67caf4c7dc0b12af29b9133db27d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:28 +0000

    Add package pyyawt/0.1.1/pyyawt-0.1.1-cp27-none-win32.whl

---
+++ packages/pyyawt/pyyawt-0.1.1-cp27-none-win32.whl/pyyawt/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyyawt SETUP.PY
+short_version = '0.1.1'
+version = '0.1.1'
+full_version = '0.1.1'
+git_revision = 'f087f14c562eab09324d3fa4cb91e7588e85d30a'
+release = True
+
+if not release:
+    version = full_version
