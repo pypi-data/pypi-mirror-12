561b437c85fca767d38292723a175816555d04b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:28 +0000

    Add package findex_common/0.2.3/findex_common-0.2.3-py2-none-any.whl

---
+++ packages/findex_common/findex_common-0.2.3-py2-none-any.whl/findex_common/resourcetypes.py
@@ -0,0 +1,16 @@
+def get_protocol_from_int(id):
+    return {
+        "0": "ftp",
+        "1": "fs",
+        "2": "smb",
+        "3": "sftp",
+        "4": "http"}[id]
+
+def get_protocol_id_from_name(name):
+    return {
+        "ftp": 0,
+        "fs": 1,
+        "smb": 2,
+        "sftp": 3,
+        "http": 4
+    }[name.lower()]
