d76a4e49fe752f9b3d96ca0e91d8be97c5d6f66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:19 +0000

    Add package urequests/0.1.1/urequests-0.1.1.tar.gz

---
+++ packages/urequests/urequests-0.1.1.tar.gz/urequests-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='urequests',
+      version='0.1.1',
+      description="""HTTP Library with a similar interface to python-requests
+Particularly well suited for usage with MicroPython.org""",
+      url='https://github.com/lucien2k/wipy-urllib',
+      author='Alex Cowan',
+      author_email='acowan@gmail.com',
+      license='MIT',
+      keywords=['requests','http','https','micropython'],
+      packages=['urequests'])
