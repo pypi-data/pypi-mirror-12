6948e2f28f22c29cd3d54b54f4b885d75666e986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:22 +0000

    Add package todoman/1.5.0/todoman-1.5.0.tar.gz

---
+++ packages/todoman/todoman-1.5.0.tar.gz/todoman-1.5.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='todoman',
+    description='A simple CalDav-based todo manager.',
+    author='Hugo Osvaldo Barrera',
+    author_email='hugo@barrera.io',
+    url='https://gitlab.com/hobarrera/todoman',
+    license='MIT',
+    packages=['todoman'],
+    entry_points={
+        'console_scripts': [
+            'todo = todoman.cli:run',
+        ]
+    },
+    install_requires=[
+        open('requirements.txt').readlines()
+    ],
+    long_description=open('README.rst').read(),
+    use_scm_version={'version_scheme': 'post-release'},
+    setup_requires=['setuptools_scm'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Console :: Curses',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Office/Business :: Scheduling',
+        'Topic :: Utilities',
+    ]
+)
