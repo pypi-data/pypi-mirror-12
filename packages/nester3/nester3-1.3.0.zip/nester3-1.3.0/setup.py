26fec7d85cf6a2d384ac5b8634a9acb671ab597c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:53 +0000

    Add package nester3/1.3.0/nester3-1.3.0.zip

---
+++ packages/nester3/nester3-1.3.0.zip/nester3-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester3',
+    version = '1.3.0',
+    py_modules = ['nester'],
+    author = 'lcy',
+    author_email = 'leecoyo@qq.com',
+    url = 'http://www.lcy.com',
+    description = 'a simple printer of nested lists',
+)
