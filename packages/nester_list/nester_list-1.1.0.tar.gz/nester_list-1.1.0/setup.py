3e487545a7b6b89c31c0db26d31dc27676582aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:07 +0000

    Add package nester_list/1.1.0/nester_list-1.1.0.tar.gz

---
+++ packages/nester_list/nester_list-1.1.0.tar.gz/nester_list-1.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+		name 			='nester_list',
+		version			='1.1.0',
+		py_modules		=['nester_list'],
+		author			='Jasmin Softic',
+		author_email	='jasoftic@gmail.com',
+		url				='http://www.sacgc.org',
+		description		='A simple printer of nester lists',
+	)
+
+
+
+
+	
