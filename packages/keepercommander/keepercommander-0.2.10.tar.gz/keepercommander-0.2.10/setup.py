f00b66e7f27764740d6b13f10c755f0386325f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:59 +0000

    Add package keepercommander/0.2.10/keepercommander-0.2.10.tar.gz

---
+++ packages/keepercommander/keepercommander-0.2.10.tar.gz/keepercommander-0.2.10/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from pypandoc import convert
+
+from os import path
+
+# strip links from the descripton on the PyPI
+# here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#     long_description = convert(f.read(), 'rst')
+
+install_requires = [
+    'colorama',
+    'pycrypto',
+    'requests',
+    'tabulate'
+]
+
+setup(name='keepercommander',
+      version='0.2.10',
+      description='Keeper Commander for Python 3',
+      long_description=convert('README.md', 'rst'),
+      author='Craig Lurey',
+      author_email='craig@keepersecurity.com',
+      url='https://github.com/Keeper-Security/Commander',
+      license='MIT',
+      classifiers=["Development Status :: 4 - Beta",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python :: 3.4",
+                   "Topic :: Security"],
+      keywords='security password',
+
+      packages=['keepercommander'],
+
+      entry_points={
+          "console_scripts": [
+              "keeper=keepercommander:main",
+          ],
+      },
+      install_requires=install_requires
+      )
