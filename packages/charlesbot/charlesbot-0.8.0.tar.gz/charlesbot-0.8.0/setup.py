9f7abd3404e9bf3781092c1a6638471e3f307e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:18 +0000

    Add package charlesbot/0.8.0/charlesbot-0.8.0.tar.gz

---
+++ packages/charlesbot/charlesbot-0.8.0.tar.gz/charlesbot-0.8.0/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+long_description = """
+Full details available at https://github.com/marvinpinto/charlesbot
+"""
+
+setup(
+    name='charlesbot',
+    version='0.8.0',
+    description='Slack Real Time Messaging robot written in Python 3!',
+    long_description=long_description,
+    author='Marvin Pinto',
+    author_email='marvin@pinto.im',
+    license='MIT',
+
+   # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='slack robot chatops automation',
+
+    url="https://github.com/marvinpinto/charlesbot",
+    packages=find_packages(
+        exclude=[
+            'Makefile',
+            'docker',
+            'images',
+            'requirements-dev.txt',
+            'requirements.txt',
+            'tests'
+        ]
+    ),
+
+    entry_points={
+        'console_scripts': ['charlesbot = charlesbot.__main__:main']
+    },
+
+    install_requires=[
+        "slackclient",
+        "websocket-client",
+        "cchardet",
+        "aiohttp",
+        "PyYAML",
+        "aiocron",
+        "croniter",
+        "python-dateutil",
+    ],
+
+    test_suite='nose.collector'
+)
