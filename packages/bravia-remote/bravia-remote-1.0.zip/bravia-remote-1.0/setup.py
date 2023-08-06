85ed44833b582c4dc1064d0bf87c219dc9b81fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:40 +0000

    Add package bravia-remote/1.0/bravia-remote-1.0.zip

---
+++ packages/bravia-remote/bravia-remote-1.0.zip/bravia-remote-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='bravia-remote',
+    version='1.0',
+    packages=['api'],
+    package_dir={'': 'src'},
+    url='',
+    license='MIT',
+    author='Petro Zdeb',
+    author_email='peterzdeb@gmail.com',
+    description=''
+)
