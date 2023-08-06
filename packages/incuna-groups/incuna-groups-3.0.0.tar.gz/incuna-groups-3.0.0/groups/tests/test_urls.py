8976827c6a45072a5ef9526a01117bbe89cb0afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:57 +0000

    Add package incuna-groups/3.0.0/incuna-groups-3.0.0.tar.gz

---
+++ packages/incuna-groups/incuna-groups-3.0.0.tar.gz/incuna-groups-3.0.0/groups/tests/test_urls.py
@@ -0,0 +1,70 @@
+from incuna_test_utils.testcases.urls import URLTestCase
+
+from .. import views
+
+
+class TestGroupUrls(URLTestCase):
+    pk = 42
+
+    def test_group_list(self):
+        self.assert_url_matches_view(
+            views.groups.GroupList,
+            '/groups/',
+            'group-list',
+        )
+
+    def test_group_detail(self):
+        self.assert_url_matches_view(
+            views.groups.GroupDetail,
+            '/groups/{}/'.format(self.pk),
+            'group-detail',
+            url_kwargs={'pk': self.pk}
+        )
+
+    def test_group_subscribe(self):
+        self.assert_url_matches_view(
+            views.subscriptions.GroupSubscribe,
+            '/groups/{}/subscribe/'.format(self.pk),
+            'group-subscribe',
+            url_kwargs={'pk': self.pk}
+        )
+
+    def test_discussion_create(self):
+        self.assert_url_matches_view(
+            views.discussions.DiscussionCreate,
+            '/groups/{}/new-discussion/'.format(self.pk),
+            'discussion-create',
+            url_kwargs={'pk': self.pk}
+        )
+
+    def test_discussion_detail(self):
+        self.assert_url_matches_view(
+            views.discussions.DiscussionThread,
+            '/groups/discussions/{}/'.format(self.pk),
+            'discussion-thread',
+            url_kwargs={'pk': self.pk}
+        )
+
+    def test_discussion_subscribe(self):
+        self.assert_url_matches_view(
+            views.subscriptions.DiscussionSubscribe,
+            '/groups/discussions/{}/subscribe/'.format(self.pk),
+            'discussion-subscribe',
+            url_kwargs={'pk': self.pk}
+        )
+
+    def test_comment_upload_file(self):
+        self.assert_url_matches_view(
+            views.comments.CommentUploadFile,
+            '/groups/discussions/{}/upload-file/'.format(self.pk),
+            'comment-upload-file',
+            url_kwargs={'pk': self.pk}
+        )
+
+    def test_comment_delete(self):
+        self.assert_url_matches_view(
+            views.comments.CommentDelete,
+            '/groups/comments/{}/delete/'.format(self.pk),
+            'comment-delete',
+            url_kwargs={'pk': self.pk}
+        )
