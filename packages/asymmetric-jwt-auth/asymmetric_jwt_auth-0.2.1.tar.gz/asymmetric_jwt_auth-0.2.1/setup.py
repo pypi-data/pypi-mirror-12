10c68042949eaa1566afaa028a5eec65a6486a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:20 +0000

    Add package asymmetric-jwt-auth/0.2.1/asymmetric_jwt_auth-0.2.1.tar.gz

---
+++ packages/asymmetric-jwt-auth/asymmetric_jwt_auth-0.2.1.tar.gz/asymmetric_jwt_auth-0.2.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+packages = [
+    'asymmetric_jwt_auth',
+    'asymmetric_jwt_auth.test',
+    'asymmetric_jwt_auth.management',
+    'asymmetric_jwt_auth.management.commands',
+    'asymmetric_jwt_auth.migrations',
+]
+
+requires = [
+    'PyJWT>=1.4.0',
+    'cryptography>=1.0',
+]
+
+setup(
+    name='asymmetric_jwt_auth',
+    version='0.2.1',
+    description='Asymmetric key based authentication for HTTP APIs',
+    long_description=open('README.rst').read(),
+    author='Craig Weber',
+    author_email='crgwbr@gmail.com',
+    url='https://github.com/crgwbr/asymmetric_jwt_auth',
+    packages=packages,
+    install_requires=requires,
+    license='LICENSE.md'
+)
