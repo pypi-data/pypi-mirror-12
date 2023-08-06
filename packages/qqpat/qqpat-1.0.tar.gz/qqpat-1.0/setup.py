919ba748514a51aeb1b6ab5ebade4dd92bbde937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:07 +0000

    Add package qqpat/1.0/qqpat-1.0.tar.gz

---
+++ packages/qqpat/qqpat-1.0.tar.gz/qqpat-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name='qqpat',
+      version='1.0',
+      description='Python Financial Performance Analysys Tool',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Office/Business :: Financial :: Investment',
+      ],
+      keywords='financial time series statistics performance analytics',
+      url='http://github.com/quriquant/qq-pat',
+      author='QuriQuant',
+      author_email='suport@quriquant.com',
+      license='MIT',
+      packages=['qqpat'],
+      install_requires=[
+          'pandas',
+          'seaborn',
+          'matplotlib'
+      ],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
