9cf27bd3e23158cc7975fb28c39fb270f20f45ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:53 +0000

    Add package infi.multipathtools/0.1.32/infi.multipathtools-0.1.32-py2.7.egg

---
+++ packages/infi.multipathtools/infi.multipathtools-0.1.32-py2.7.egg/infi/multipathtools/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.32"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.1.32)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2f95b7dfccce0490274624f5c1cf1b25d58baaf5'
+__git_head_subject__ = "HPT-1667 parse aliases with '-' in them"
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
