da4740745a4fedb96b7789a3ae5786c2950b6797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:21 +0000

    Add package pyplotwrap/0.1beta/pyplotwrap-0.1beta.tar.gz

---
+++ packages/pyplotwrap/pyplotwrap-0.1beta.tar.gz/pyplotwrap-0.1beta/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyplotwrap',
+    version='0.1beta',
+    packages=['src',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    url='https://github.com/nenanth/python_plot_wrappers',
+    author='Ananth Sridharan',
+    author_email='sridharan.ananth@gmail.com',
+)
