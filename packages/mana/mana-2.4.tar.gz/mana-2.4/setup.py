2822638c17b7831f5185c9d59790b7d3bb8c46e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:09 +0000

    Add package mana/2.4/mana-2.4.tar.gz

---
+++ packages/mana/mana-2.4.tar.gz/mana-2.4/setup.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+"""
+    mana
+    ~~~~
+
+    happy generate flask project
+"""
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mana',
+    version='2.4',
+    packages=find_packages(),
+    url='https://github.com/neo1218/mana',
+    license='MIT',
+    author='neo1218',
+    author_email='neo1218@yeah.net',
+    description='happy generate flask project',
+    long_description=__doc__,
+    # if you would be using a package instead use packages instead
+    # of py_modules:
+    # packages=['flask_sqlite3'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'click'
+    ],
+    # /mana/mana.py/click::mana
+    entry_points='''
+        [console_scripts]
+        mana=mana.mana:cli
+    ''',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
