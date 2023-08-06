6a1385dd40cc9ca561f35e19a216f254b22ef371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:55 +0000

    Add package MuseParse/1.1.8/MuseParse-1.1.8.tar.gz

---
+++ packages/MuseParse/MuseParse-1.1.8.tar.gz/MuseParse-1.1.8/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='MuseParse',
+    version='1.1.8',
+    packages=['MuseParse', 'MuseParse.demo', 'MuseParse.classes',
+              'MuseParse.classes.Input', 'MuseParse.classes.Output', 'MuseParse.classes.ObjectHierarchy',
+              'MuseParse.classes.ObjectHierarchy.ItemClasses', 'MuseParse.classes.ObjectHierarchy.TreeClasses'],
+    url='http://github.com/godley/MuseParse',
+    license='MIT',
+    author='charlottegodley',
+    author_email='me@charlottegodley.co.uk',
+    description='A package which takes an inputted music file (at the moment only MusicXML) and generates an object hierarchy in memory. This version has a reorganised structure and passes all tests on the build server.'
+
+)
