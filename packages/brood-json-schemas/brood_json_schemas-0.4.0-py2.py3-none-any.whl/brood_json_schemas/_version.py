4518faa2991761014acf7eb329f974844edb2bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:54 +0000

    Add package brood-json-schemas/0.4.0/brood_json_schemas-0.4.0-py2.py3-none-any.whl

---
+++ packages/brood-json-schemas/brood_json_schemas-0.4.0-py2.py3-none-any.whl/brood_json_schemas/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM SETUP.PY
+version = '0.4.0'
+full_version = '0.4.0'
+git_revision = '5e7b4d35405e39788831fd732b600bc2e3b8d5b8'
+is_released = True
+
+if not is_released:
+    version = full_version
