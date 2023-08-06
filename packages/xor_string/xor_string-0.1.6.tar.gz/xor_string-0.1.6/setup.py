8e009de2fab07b7ab0724a8eda1ccd75d88af67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:10 +0000

    Add package xor_string/0.1.6/xor_string-0.1.6.tar.gz

---
+++ packages/xor_string/xor_string-0.1.6.tar.gz/xor_string-0.1.6/setup.py
@@ -0,0 +1,26 @@
+
+# import os
+
+from setuptools import setup
+
+# here = os.path.abspath(os.path.dirname(__file__))
+# README = open(os.path.join(here, 'README.md')).read()
+
+requires = [
+    'itertools',
+]
+
+setup(
+    name='xor_string',
+    packages=['xor_string',],
+    version='0.1.6',
+    url='https://github.com/Akagi201/xor_string',
+    download_url='https://github.com/Akagi201/xor_string/tarball/0.1.6',
+    description='Elegant xor encryption in Python',
+    # long_description=README,
+    license='MIT',
+    author='Akagi201',
+    author_email='akagi201@gmail.com',
+    keywords=['xor', 'string', 'itertools', 'python'],
+    classifiers=[],
+)
