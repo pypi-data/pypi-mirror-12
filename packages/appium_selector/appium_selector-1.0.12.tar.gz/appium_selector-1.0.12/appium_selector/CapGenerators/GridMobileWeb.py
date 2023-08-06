1a9b72078e04e1c5fa6bd9f5111813476ada510b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:22 +0000

    Add package appium_selector/1.0.12/appium_selector-1.0.12.tar.gz

---
+++ packages/appium_selector/appium_selector-1.0.12.tar.gz/appium_selector-1.0.12/appium_selector/CapGenerators/GridMobileWeb.py
@@ -0,0 +1,36 @@
+from appium_selector.CapGenerators.Caps import Caps, propertyFromString
+from appium_selector.Helpers.GetDeviceInfo import getDeviceInfo
+
+
+class GridMobileWeb(Caps):
+
+    options = {}
+    caps = {}
+
+    def __init__(self, environment):
+        self.env = environment
+        self.udid = propertyFromString('deviceName', self.env)
+        self.platform = propertyFromString('platform', self.env)
+        self.device = getDeviceInfo(self.udid, self.platform)
+
+    def displayString(self):
+        platform = self.device['platform']
+        udid = self.device['udid']
+        name = self.device['name']
+        browser = propertyFromString('browserName', self.env)
+        return "%s -- %s -- %s -- %s" % (platform, udid, name, browser)
+    
+    def desiredCaps(self, mustard=True):
+        self.options['provider'] = 'gridMobileWeb'
+        self.options['manufacturer'] = self.device['platform']
+        self.options['model'] = propertyFromString('browserName', self.env)
+        self.options['osv'] = self.device['osv']
+        self.options['mustard'] = mustard
+
+        self.caps['platform'] = propertyFromString('platform', self.env)
+        self.caps['platformName'] = propertyFromString('platform', self.env)
+        self.caps['browserName'] = propertyFromString('browserName', self.env)
+        self.caps['udid'] = self.device['udid']
+        self.caps['deviceName'] = self.device['udid']
+
+        return {'desiredCaps': self.caps, 'options': self.options}
