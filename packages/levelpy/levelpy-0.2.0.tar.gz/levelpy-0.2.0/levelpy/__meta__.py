3572929c2a54debc444a9f12e65e8d4a7c5df66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:54 +0000

    Add package levelpy/0.2.0/levelpy-0.2.0.tar.gz

---
+++ packages/levelpy/levelpy-0.2.0.tar.gz/levelpy-0.2.0/levelpy/__meta__.py
@@ -0,0 +1,14 @@
+#
+# levelpy/__meta__.py
+#
+#
+"""
+LevelPy metadata file.
+"""
+
+version = '0.2.0'
+date = 'Nov 13, 2015'
+author = 'Andrew Kubera'
+author_email = 'andrewkubera@gmail.com'
+license = 'MIT'
+url = 'https://github.com/akubera/levelpy'
---
+++ packages/levelpy/levelpy-0.2.0.tar.gz/levelpy-0.2.0/levelpy/view.py
@@ -0,0 +1,68 @@
+#
+# levelpy/view.py
+#
+"""
+Provides a read-only object accessing a database or sublevel
+"""
+
+from .db_accessors import LevelReader
+
+
+class View(LevelReader):
+    """
+    This is an immutable object providing read-only access to the database
+    (or a sublevel)
+    """
+
+    def __init__(self, db, prefix='', delim='!', value_encoding='utf-8'):
+
+        super().__init__(prefix, delim)
+
+        self._db = db
+        self.prefix = prefix
+        self.delim = delim
+        # self.RangeIter = db.RangeIter
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            if key.step is not None:
+                raise ValueError("Step values are not available for "
+                                 "slices in levelpy")
+            start, stop = self.subkey(key.start), self.subkey(key.stop)
+            return self._db[start:stop]
+
+        elif isinstance(key, (tuple, list, set)):
+            t = type(key)
+            return self._db[t(self.subkey(k) for k in key)]
+
+        else:
+            return self._db[self.subkey(key)]
+
+    def __copy__(self):
+        """
+        Simple copy of sublevel - same db, prefix, and delimeter
+        """
+        return type(self)(self._db, self.prefix, self.delim)
+
+    def items(self, key_from='', key_to='~', *args, **kwargs):
+        """iterate over all items in the sublevel"""
+        key_from = self.subkey(key_from)
+        key_to = self.subkey(key_to)
+        yield from self._db.items(key_from=key_from,
+                                  key_to=key_to,
+                                  *args,
+                                  **kwargs)
+
+    def view(self, key, delim=None, value_encoding=None):
+        """
+        Return a subview of this view
+        """
+        prefix = self.key_transform(key)
+        delim = self.delim if (delim is None) else delim
+        enc = self._get_encoding(value_encoding)
+
+        return View(self._db,
+                    prefix,
+                    delim=delim,
+                    value_encoding=enc,
+                    )
