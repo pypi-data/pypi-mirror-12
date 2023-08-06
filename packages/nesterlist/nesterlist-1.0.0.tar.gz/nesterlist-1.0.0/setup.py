6ab06e743b704b71cffa2405518c570d76779a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:51 +0000

    Add package nesterlist/1.0.0/nesterlist-1.0.0.tar.gz

---
+++ packages/nesterlist/nesterlist-1.0.0.tar.gz/nesterlist-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+		name="nesterlist",
+		version="1.0.0",
+		py_modules=["nesterlist"],
+		author="Fred",
+		author_email="shuishuiwawa@gmail.com",
+		url="https//localhost",
+		description="It is a function to print nested list.",
+		license="Apache"
+		)
