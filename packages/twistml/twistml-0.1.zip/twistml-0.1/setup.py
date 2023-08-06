3468cb2be11e4dde9f92563e60b2932c043062c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:30 +0000

    Add package twistml/0.1/twistml-0.1.zip

---
+++ packages/twistml/twistml-0.1.zip/twistml-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='twistml',
+      version='0.1',
+      description='TWItter STock market Machine Learning package',
+      long_description=readme(),
+      keywords='twitter stock market machine learning',
+      url='https://bitbucket.org/madmat3001/twistml.git',
+      author='Matthias Manhertz',
+      author_email='m@nhertz.de',
+      license='MIT',
+      packages=['twistml'],
+      zip_safe=False)
