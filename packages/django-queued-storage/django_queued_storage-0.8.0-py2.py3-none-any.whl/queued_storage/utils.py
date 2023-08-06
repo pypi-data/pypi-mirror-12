7e31723e9b524b4ac11abe1d2cb4b53bef87de77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:42 +0000

    Add package django-queued-storage/0.8.0/django_queued_storage-0.8.0-py2.py3-none-any.whl

---
+++ packages/django-queued-storage/django_queued_storage-0.8.0-py2.py3-none-any.whl/queued_storage/utils.py
@@ -0,0 +1,28 @@
+from importlib import import_module
+
+import django
+from django.core.exceptions import ImproperlyConfigured
+
+
+def import_attribute(import_path=None, options=None):
+    if import_path is None:
+        raise ImproperlyConfigured("No import path was given.")
+    try:
+        dot = import_path.rindex('.')
+    except ValueError:
+        raise ImproperlyConfigured("%s isn't a module." % import_path)
+    module, classname = import_path[:dot], import_path[dot + 1:]
+    try:
+        mod = import_module(module)
+    except ImportError as e:
+        raise ImproperlyConfigured('Error importing module %s: "%s"' %
+                                   (module, e))
+    try:
+        return getattr(mod, classname)
+    except AttributeError:
+        raise ImproperlyConfigured(
+            'Module "%s" does not define a "%s" class.' % (module, classname))
+
+
+def django_version():
+    return [int(x) for x in django.get_version().split('.')]
