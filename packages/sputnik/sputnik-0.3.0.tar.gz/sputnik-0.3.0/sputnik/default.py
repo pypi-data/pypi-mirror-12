e96221f03ee1fe80df63188661f1aedf6fa118e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:02 +0000

    Add package sputnik/0.3.0/sputnik-0.3.0.tar.gz

---
+++ packages/sputnik/sputnik-0.3.0.tar.gz/sputnik-0.3.0/sputnik/default.py
@@ -0,0 +1,13 @@
+# cli/param defaults
+list_meta = False
+list_package_string = ''
+search_string = ''
+build_package_path = '.'
+
+# misc
+CHUNK_SIZE = 1024 * 16
+ARCHIVE_FILENAME = 'archive.gz'
+META_FILENAME = 'meta.json'
+COMPRESSLEVEL = 9
+COOKIES_FILENAME = 'cookies.txt'
+CACHE_DIRNAME = '__cache__'
