7d8e67d0f2394a47e3959757a71a0a2c51f409c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:42 +0000

    Add package v_nester/1.1.0/v_nester-1.1.0.zip

---
+++ packages/v_nester/v_nester-1.1.0.zip/v_nester-1.1.0/v_nester.py
@@ -0,0 +1,20 @@
+""" This is my first module: nester.py for python,
+	and it provides one function called print_lol.
+	The following code is learned from book: <Head First Python>. """
+def print_lol(the_list, level=0):
+
+	""" The function checks wether the 'the_list' has a nest list or not.
+		if it has a nest list, then call itself again(recursive). if not,
+		print the whole list.
+		A sceond argument called "level" is used to insert tab-stops when
+		 a nested list is ecountered."""
+
+# comments
+
+	for ei in the_list:
+		if isinstance(ei, list):
+			print_lol(ei, level+1)
+		else:
+			for tab_stops in range(level):
+				print('\t', end='')
+			print(ei)
