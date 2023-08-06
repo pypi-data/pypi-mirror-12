8ccfc18606e579101ca3cc3fa022decb4122912a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:23 +0000

    Add package clnester/1.1.0/clnester-1.1.0.zip

---
+++ packages/clnester/clnester-1.1.0.zip/clnester-1.1.0/clnester.py
@@ -0,0 +1,22 @@
+"""This is the "clnester.py" module and it provides one function called print_lal()
+    which prints lists that may or may not include nested lists."""
+
+def print_lal (a, tab):
+    """This function takes one positional argument "a", which
+       is any Phython list (of - possibly - nested lists). Each data item in the
+       provided list is (recursively) printed to the screen on it's own line."""
+    for a1 in a:
+        if isinstance(a1, list):
+            for a2 in a1:
+                if isinstnce(a2, list):
+                    for a3 in a2:
+                        print(a2)
+                 else:
+                     print(a2)
+         else:
+             for bb in range(tab):
+                 print("\t",end='')
+             print(a1)
+
+			
+
---
+++ packages/clnester/clnester-1.1.0.zip/clnester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name            =       "clnester",
+        version         =       "1.1.0",
+        py_modules      =       ["clnester"],
+        author          =        "Chloe_Hwang",
+        author_email    =       "yuraree_90@hotmail.com",
+        url             =       "http://www.facebook.com/yuraree",
+        description     =       "A simple printer of nested lists and tab ket application",
+
+        )
