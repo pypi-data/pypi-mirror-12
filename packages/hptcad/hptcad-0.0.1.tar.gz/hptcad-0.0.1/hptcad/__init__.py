1d5942937435e7d89b9cc3fd65b8748c1f088315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:36 +0000

    Add package hptcad/0.0.1/hptcad-0.0.1.tar.gz

---
+++ packages/hptcad/hptcad-0.0.1.tar.gz/hptcad-0.0.1/hptcad/__init__.py
@@ -0,0 +1,3 @@
+import hptcad.radiosity
+from hptcad.radiosity import number
+print('initializing hptcad package')
---
+++ packages/hptcad/hptcad-0.0.1.tar.gz/hptcad-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hptcad',
+      version='0.0.1',
+      description='HPTCAD',
+      url='http://github.com/manstetten/hptcad',
+      author='Paul Manstetten',
+      author_email='manstetten@iue.tuwien.ac.at',
+      license='MIT',
+      packages=['hptcad'],
+      zip_safe=False)
