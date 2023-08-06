fd390579a65d001cc66189c711a9442cc68b8bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:36 +0000

    Add package nesterwang/1.0.1/nesterwang-1.0.1.zip

---
+++ packages/nesterwang/nesterwang-1.0.1.zip/nesterwang-1.0.1/nesterwang.py
@@ -0,0 +1,14 @@
+"""Hi!!! 나야!!!"""
+
+def print_lol(the_list,level):
+       for each_item in the_list:
+               if isinstance(each_item, list):
+                         print_lol(each_item,level+1)
+               else:
+                         for tab_stop in range(level):
+                              print("\t",end='')
+                         print(each_item)
+
+
+
+                         
