ac027507c030adab6929267295432328fb634157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:07 +0000

    Add package python-perforce/0.3.3/python-perforce-0.3.3.zip

---
+++ packages/python-perforce/python-perforce-0.3.3.zip/python-perforce-0.3.3/perforce/errors.py
@@ -0,0 +1,28 @@
+"""Errors"""
+
+class Error(Exception):
+    pass
+
+
+class CommandError(Exception):
+    """Errors that occur while running a command"""
+
+
+class ChangelistError(Exception):
+    """Errors that occur in a Changelist"""
+
+
+class ShelveError(Exception):
+    """Errors that occur when shelving/unshelving a file revision"""
+
+
+class RevisionError(Exception):
+    """Errors that occur on a file revision"""
+
+
+class ConnectionError(Exception):
+    """Errors that occurred with the connection"""
+
+
+class AuthenticationError(Exception):
+    """An error occurred while trying to authenticate to the server"""
