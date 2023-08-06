471fbbb6e8461adeabb5d8e24c654eeb5c51eb7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:58 +0000

    Add package backlog_puller/1.0.0/backlog_puller-1.0.0-py2-none-any.whl

---
+++ packages/backlog_puller/backlog_puller-1.0.0-py2-none-any.whl/backlog_puller/__main__.py
@@ -0,0 +1,15 @@
+import sys
+import backlog_puller
+
+
+def main():
+    if len(sys.argv) == 2:
+        request = backlog_puller.GamesRequest(sys.argv[1])
+        parser = backlog_puller.BacklogHTMLParser()
+        parser.feed(request.get_raw_page())
+        print parser.backlog
+    else:
+        print("Incorrect number of arguments. Please pass just your username and try again")
+
+if __name__ == 'main':
+    main()
