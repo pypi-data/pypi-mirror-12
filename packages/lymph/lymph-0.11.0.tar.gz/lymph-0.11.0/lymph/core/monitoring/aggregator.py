ea2d3499b2594f55bcb6633071a2087d3b80bd72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:42 +0000

    Add package lymph/0.11.0/lymph-0.11.0.tar.gz

---
+++ packages/lymph/lymph-0.11.0.tar.gz/lymph-0.11.0/lymph/core/monitoring/aggregator.py
@@ -0,0 +1,14 @@
+from lymph.core.monitoring.metrics import Aggregate
+from lymph.core.monitoring.global_metrics import RUsageMetrics, GeventMetrics, GarbageCollectionMetrics
+
+
+class Aggregator(Aggregate):
+    @classmethod
+    def from_config(cls, config):
+        tags = config.get_raw('tags', {})
+        # FIXME: move default metrics out
+        return cls([
+            RUsageMetrics(),
+            GarbageCollectionMetrics(),
+            GeventMetrics(),
+        ], **tags)
