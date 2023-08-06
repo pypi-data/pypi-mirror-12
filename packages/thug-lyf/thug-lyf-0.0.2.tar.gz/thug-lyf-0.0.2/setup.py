b36cec7fb10ebb7379da6dc25c011e12023c4e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:24 +0000

    Add package thug-lyf/0.0.2/thug-lyf-0.0.2.tar.gz

---
+++ packages/thug-lyf/thug-lyf-0.0.2.tar.gz/thug-lyf-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='thug-lyf',
+    packages=['thug_lyf'],
+    version='0.0.2',
+    description='A Python Webframework',
+    author='Mihir',
+    author_email='khatwanimihir@gmail.com',
+    url='https://github.com/barbieworld/thuf-lyf',
+    download_url='https://github.com/barbieworld/thuf-lyf/archive/master.zip',
+    keywords=['web', 'framework', 'database'],
+    classifiers=[],
+)
