de37ec26c048a7a9f833836c5beab62defdcb1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:22 +0000

    Add package platereader/0.2.0/platereader-0.2.0.tar.gz

---
+++ packages/platereader/platereader-0.2.0.tar.gz/platereader-0.2.0/platereader/__init__.py
@@ -0,0 +1,1 @@
+__all__=["plateimage", "engine","gui","my"]
---
+++ packages/platereader/platereader-0.2.0.tar.gz/platereader-0.2.0/platereader/__main__.py
@@ -0,0 +1,4 @@
+from platereader.engine import PlateReader
+
+#Start the program if called from the command line
+PlateReader().showGUI()
