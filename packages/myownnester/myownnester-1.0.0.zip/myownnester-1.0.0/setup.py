01f6a36c00adba1fdd56666ef3e6f56731d713b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:46 +0000

    Add package myownnester/1.0.0/myownnester-1.0.0.zip

---
+++ packages/myownnester/myownnester-1.0.0.zip/myownnester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name 			='myownnester',
+		version 		='1.0.0',
+		py_modules 		=['myownnester'],
+		author 			='silence',
+		author_email 	='1538084963@qq.com',
+		url 			='http://www.headfirstlabs.com',
+		description		='A simple printer of nested lists',
+	)
