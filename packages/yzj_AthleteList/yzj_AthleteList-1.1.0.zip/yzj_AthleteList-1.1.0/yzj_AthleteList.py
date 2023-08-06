4097c79afe4b146c91d65ec8133faf1928bc4b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:54 +0000

    Add package yzj_AthleteList/1.1.0/yzj_AthleteList-1.1.0.zip

---
+++ packages/yzj_AthleteList/yzj_AthleteList-1.1.0.zip/yzj_AthleteList-1.1.0/yzj_AthleteList.py
@@ -0,0 +1,11 @@
+__author__ = 'Administrator'
+#coding=utf-8
+from yzj_sanitize import sanitize
+class AthleteList(list):
+    def __init__(self,a_name,a_dob=None,a_times=[]):
+        list.__init__([])
+        self.name=a_name
+        self.dob=a_dob
+        self.extend(a_times)
+    def top3(self):
+        return(sorted(set([sanitize(t) for t in self]))[0:3])
