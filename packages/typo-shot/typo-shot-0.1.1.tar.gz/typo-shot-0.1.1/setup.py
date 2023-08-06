aa0821dc41a5fd5981908d161a65db3159c19b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:48 +0000

    Add package typo-shot/0.1.1/typo-shot-0.1.1.tar.gz

---
+++ packages/typo-shot/typo-shot-0.1.1.tar.gz/typo-shot-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import typoshot
+import os
+
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+setup(
+    name="typo-shot",
+    version=typoshot.__version__,
+    author="Flaverton Rodrigues Rosa",
+    author_email="contato@flaverton.com",
+    description=("Check your website/blog typography simple, with screenshots!"),
+    license="MIT",
+    keywords="splinter screenshot typo typography",
+    url="https://github.com/flavertonrr/typo-shot",
+    scripts=["typo"],
+    packages=["typoshot"],
+    long_description=read('README'),
+    install_requires=[
+        "splinter>=0.7.3",
+        "requests>=2.4.3"
+    ],
+    classifiers=[
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7"
+    ]
+)
