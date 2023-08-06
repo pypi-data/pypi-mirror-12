d0951b6d77fc6d3c001e98e5a278c82f5a5b1ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:59 +0000

    Add package hfcca/1.9.2/hfcca-1.9.2.tar.gz

---
+++ packages/hfcca/hfcca-1.9.2.tar.gz/hfcca-1.9.2/lizard_ext/__init__.py
@@ -0,0 +1,7 @@
+''' extensions of lizard '''
+
+from .javascript import JavaScriptReader
+from .python import PythonReader
+from .objc import ObjCReader
+from .ttcn import TTCNReader
+from .xmloutput import xml_output
