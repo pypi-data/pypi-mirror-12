7f9c7d7203cac112fe18353f36bcdc00cdad9175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:16 +0000

    Add package python-jose/0.5.3/python-jose-0.5.3.tar.gz

---
+++ packages/python-jose/python-jose-0.5.3.tar.gz/python-jose-0.5.3/jose/exceptions.py
@@ -0,0 +1,32 @@
+
+
+class JOSEError(Exception):
+    pass
+
+
+class JWSError(JOSEError):
+    pass
+
+
+class JWSAlgorithmError(JWSError):
+    pass
+
+
+class JWTError(JOSEError):
+    pass
+
+
+class JWTClaimsError(JWTError):
+    pass
+
+
+class JWTSignatureError(JWTError):
+    pass
+
+
+class ExpiredSignatureError(JWTError):
+    pass
+
+
+class JWKError(JOSEError):
+    pass
