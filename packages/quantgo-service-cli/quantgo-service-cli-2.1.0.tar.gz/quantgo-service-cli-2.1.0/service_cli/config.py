bbfb171ab8bc2f1edb6dcca376f09aa832276e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:37 +0000

    Add package quantgo-service-cli/2.1.0/quantgo-service-cli-2.1.0.tar.gz

---
+++ packages/quantgo-service-cli/quantgo-service-cli-2.1.0.tar.gz/quantgo-service-cli-2.1.0/service_cli/config.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
+# ======================= QuantGo Copyright Notice ============================
+# Copyright 2013 QuantGo, LLC.  All rights reserved.
+# Permission to use solely inside a QuantGo Virtual Quant Lab
+# Written By: Nikolay
+# Date: 12-12.2013
+# ======================= QuantGo Copyright Notice ============================
+
+HOST = 'service.quantgo.com'
+PORT = 4476
