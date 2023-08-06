479abe2a32a0cd592db3ea77cb5cf896b3a3e059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:05 +0000

    Add package historist/0.1.0.dev1/historist-0.1.0.dev1.tar.gz

---
+++ packages/historist/historist-0.1.0.dev1.tar.gz/historist-0.1.0.dev1/setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='historist',
+      version='0.1.0.dev1',
+      description='activity logger',
+      url='http://github.com/sina33/historist',
+      author='Sina Saeedi',
+      author_email='sina.saeedi@outlook.com',
+      license='MIT',
+      keywords="archive events logs".split(),
+      packages=['historist'],
+      install_requires=[
+          "sqlalchemy>=1.0.9"
+      ],
+      entry_points={
+          "console_scripts": [
+              "histopush = historist:histopush",
+              "histoplay = historist:histoplay",
+              "historist = historist:historist"
+          ]
+      },
+      zip_safe=False)
