0cc3a19edb299cccac45b555d957bbdcc72dc75e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:15 +0000

    Add package django-security-knaperek/0.9.4/django-security-knaperek-0.9.4.tar.gz

---
+++ packages/django-security-knaperek/django-security-knaperek-0.9.4.tar.gz/django-security-knaperek-0.9.4/security/password_expiry.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2011, SD Elements. See LICENSE.txt for details.
+
+from .models import PasswordExpiry
+
+
+def password_is_expired(user):
+    password_expiry, _ = PasswordExpiry.objects.get_or_create(user=user)
+    return not user.is_superuser and password_expiry.is_expired()
+
+
+def never_expire_password(user):
+    password_expiry, _ = PasswordExpiry.objects.get_or_create(user=user)
+    password_expiry.never_expire()
