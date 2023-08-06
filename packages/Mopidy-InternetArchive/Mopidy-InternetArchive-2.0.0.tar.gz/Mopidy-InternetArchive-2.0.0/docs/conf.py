53592d87e9bdff585b5d7017787bf890bca29cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:41 +0000

    Add package Mopidy-InternetArchive/2.0.0/Mopidy-InternetArchive-2.0.0.tar.gz

---
+++ packages/Mopidy-InternetArchive/Mopidy-InternetArchive-2.0.0.tar.gz/Mopidy-InternetArchive-2.0.0/docs/conf.py
@@ -0,0 +1,33 @@
+def setup(app):
+    app.add_object_type(
+        'confval', 'confval',
+        objname='configuration value',
+        indextemplate='pair: %s; configuration value'
+    )
+
+
+def get_version(filename):
+    from re import findall
+    with open(filename) as fh:
+        metadata = dict(findall(r"__([a-z]+)__ = '([^']+)'", fh.read()))
+    return metadata['version']
+
+project = 'Mopidy-InternetArchive'
+copyright = '2014, 2015 Thomas Kemmer'
+version = get_version(b'../mopidy_internetarchive/__init__.py')
+release = version
+
+exclude_patterns = ['_build']
+master_doc = 'index'
+html_theme = 'default'
+
+latex_documents = [(
+    'index', 'Mopidy-InternetArchive.tex',
+    'Mopidy-InternetArchive Documentation',
+    'Thomas Kemmer', 'manual'
+)]
+
+man_pages = [(
+    'index', 'mopidy-internetarchive', 'Mopidy-InternetArchive Documentation',
+    ['Thomas Kemmer'], 1
+)]
---
+++ packages/Mopidy-InternetArchive/Mopidy-InternetArchive-2.0.0.tar.gz/Mopidy-InternetArchive-2.0.0/docs/install.rst
@@ -0,0 +1,9 @@
+Installation
+========================================================================
+
+Mopidy-InternetArchive can be installed using pip_ by running::
+
+    pip install Mopidy-InternetArchive
+
+
+.. _pip: https://pip.pypa.io/en/latest/
