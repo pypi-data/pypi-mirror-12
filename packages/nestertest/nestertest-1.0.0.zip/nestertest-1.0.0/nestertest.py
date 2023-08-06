71e7552bd24eac678aa548d86ebfeb39a8ef5811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:14 +0000

    Add package nestertest/1.0.0/nestertest-1.0.0.zip

---
+++ packages/nestertest/nestertest-1.0.0.zip/nestertest-1.0.0/nestertest.py
@@ -0,0 +1,19 @@
+﻿"""This is the standard way to include a multiple-line comment in
+your code."""
+
+def allitem(the_list):
+    """This function takes a positional argument called “the_list", which is any
+    Python list (of, possibly, nested lists). Each data item in the provided list
+    is (recursively) printed to the screen on its own line."""
+    for i in the_list:
+        if isinstance(i,list):
+            allitem(i)
+        else:
+            print(i)
+            
+        
+
+
+
+  
+            
---
+++ packages/nestertest/nestertest-1.0.0.zip/nestertest-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'nestertest',
+version = '1.0.0',
+py_modules = ['nestertest'],
+author = 'amuklpython',
+author_email = '254304919@qq.com',
+url = 'http://www.qq.com',
+description = 'A simple printer of nested lists',
+)
