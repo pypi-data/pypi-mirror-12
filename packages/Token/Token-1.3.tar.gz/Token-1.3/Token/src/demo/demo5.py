60928c129b0bd9341dcfdbea5c9a857da11133cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:01 +0000

    Add package Token/1.3/Token-1.3.tar.gz

---
+++ packages/Token/Token-1.3.tar.gz/Token-1.3/Token/src/demo/demo5.py
@@ -0,0 +1,34 @@
+# Demo 5: Basic inter bank transfer through a push payment
+
+import Token
+
+bofa = Token.BankNoteService('bofa')
+citi = Token.BankNoteService('citi')
+IS = Token.IdentityService()
+
+# Generate a private and public key for each user
+aliceKeys = Token.generate_keys()
+bobKeys = Token.generate_keys()
+
+# Create the members at the identity service
+Token.switchContext('token')
+alice = IS.create_member(aliceKeys)
+bob = IS.create_member(bobKeys)
+
+# Add a bank account for each member, at their respective banks
+Token.switchContext('bofa')
+aliceAcc = bofa.create_account(alice, 'dec7')
+
+Token.switchContext('citi')
+bobAcc = citi.create_account(bob, 'a63a')
+
+Token.switchContext('bob')
+citi.get_balance(bobAcc)
+
+Token.switchContext('alice')
+bofa.get_balance(aliceAcc)
+# Now create a push payment of $10 to bob
+t1 = bofa.create_payment(aliceAcc, bob, 10, 'USD', "push")
+
+Token.switchContext('bob')
+citi.get_balance(bobAcc)
