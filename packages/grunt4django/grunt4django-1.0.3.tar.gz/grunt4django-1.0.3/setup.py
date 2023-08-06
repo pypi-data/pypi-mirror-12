84397036754b90d704199c8e228d1373fd5a2d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:08 +0000

    Add package grunt4django/1.0.3/grunt4django-1.0.3.tar.gz

---
+++ packages/grunt4django/grunt4django-1.0.3.tar.gz/grunt4django-1.0.3/setup.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+ 
+setup (
+    name = 'grunt4django',
+    version = '1.0.3',
+    py_modules = ['grunt4django'],
+    author = 'lihao',
+    author_email = 'lihao12518@163.com',
+    description = '包含django的runserver命令，还有针对html的中间件(添加livereload)',
+    packages = find_packages(),
+)
