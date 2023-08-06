d4f30ce44db4aa1f501e4068c8b3c82943e0bc01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:58 +0000

    Add package suppressor/0.0.8/suppressor-0.0.8.tar.gz

---
+++ packages/suppressor/suppressor-0.0.8.tar.gz/suppressor-0.0.8/suppressor.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Usage: suppressor [-h] pass COMMAND
+          suppressor [-h] fail COMMAND
+
+Arguments:
+  EXPECTED_STATUS        expect pass or fails comand status
+  COMMAND                Command to execute
+Options:
+  -h --help
+
+"""
+
+import sys
+from subprocess import Popen, PIPE
+from docopt import docopt
+
+def main():
+    docopt_args = docopt(__doc__)
+    p = Popen(docopt_args["COMMAND"], shell=True, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate()
+    if docopt_args["pass"]:
+        if p.returncode != 0:
+            print(err.decode(), file=sys.stderr)
+            exit(1)
+
+    if docopt_args["fail"]:
+        if p.returncode == 0:
+            print(output.decode(), file=sys.stderr)
+            exit(1)
+
