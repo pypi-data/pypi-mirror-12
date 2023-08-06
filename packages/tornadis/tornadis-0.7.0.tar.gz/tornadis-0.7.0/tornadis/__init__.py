8f36f04cbeb80f3f16b711ecd056e3da6c486474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:00 +0000

    Add package tornadis/0.7.0/tornadis-0.7.0.tar.gz

---
+++ packages/tornadis/tornadis-0.7.0.tar.gz/tornadis-0.7.0/tornadis/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of tornadis library released under the MIT license.
+# See the LICENSE file for more information.
+
+DEFAULT_HOST = '127.0.0.1'
+DEFAULT_PORT = 6379
+DEFAULT_CONNECT_TIMEOUT = 20
+DEFAULT_READ_PAGE_SIZE = 65536
+DEFAULT_WRITE_PAGE_SIZE = 65536
+
+from tornadis.utils import WriteBuffer
+from tornadis.client import Client
+from tornadis.pubsub import PubSubClient
+from tornadis.pool import ClientPool
+from tornadis.pipeline import Pipeline
+from tornadis.connection import Connection
+from tornadis.exceptions import ConnectionError, ClientError, TornadisException
+
+__all__ = ['Client', 'ClientPool', 'Pipeline',
+           'ConnectionError', 'ClientError', 'TornadisException',
+           'PubSubClient', 'WriteBuffer', 'Connection']
