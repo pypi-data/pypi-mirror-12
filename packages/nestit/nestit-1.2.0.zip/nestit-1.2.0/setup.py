b739ce26618efac37b411e831502b5b0a78910d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:42 +0000

    Add package nestit/1.2.0/nestit-1.2.0.zip

---
+++ packages/nestit/nestit-1.2.0.zip/nestit-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name = 'nestit',
+		version = '1.2.0',
+		py_modules = ['nestit'],
+		author = 'Tom',
+		author_email = 'junfeng142857@gmail.com',
+		url = 'http://Intranet',
+		description = 'A simple printer of nested lists',
+	)
