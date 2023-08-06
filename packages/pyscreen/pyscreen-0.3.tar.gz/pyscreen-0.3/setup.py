0bb128a71ae572ef9c42b627decfbb692a2534c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:34 +0000

    Add package pyscreen/0.3/pyscreen-0.3.tar.gz

---
+++ packages/pyscreen/pyscreen-0.3.tar.gz/pyscreen-0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'pyscreen',
+  packages = ['pyscreen'],
+  version = '0.3',
+  description = 'A light-weight library for easy interaction between Python and GNU screen.',
+  author = 'Wessel Stoop',
+  author_email = 'stoopwessel@gmail.com',
+  url = 'https://github.com/woseseltops/pyscreen',
+  download_url = 'https://github.com/woseseltops/pyscreen/tarball/0.3',
+  keywords = ['GNU screen', 'screen'],
+  classifiers = [],
+)
