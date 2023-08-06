51b2b75f602bfcb3d3160b084af1d720d65725cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:02 +0000

    Add package docker-parse/0.2.0/docker-parse-0.2.0.tar.gz

---
+++ packages/docker-parse/docker-parse-0.2.0.tar.gz/docker-parse-0.2.0/docker_parse/__init__.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import json
+import subprocess
+import sys
+import pipes
+import getopt
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Please specific container name!")
+        sys.exit()
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "p", ["pretty"])
+    except getopt.GetoptError as e:
+        print("Usage: docker-parse [--pretty|-p] <container>")
+        sys.exit(2)
+
+    if len(args) > 0:
+        container = args[0]
+    else:
+        print("Please specific container name!")
+        sys.exit()
+
+    sep = ' '
+    for o, a in opts:
+        if o == "-p" or o == "--pretty":
+            sep = ' \\\n    '
+
+    try:
+        cmd = ["docker", "inspect"]
+        cmd.extend([container])
+        output = subprocess.check_output(cmd, universal_newlines=True)
+    except subprocess.CalledProcessError as e:
+        sys.exit()
+
+    infos = json.loads(output)
+    for info in infos:
+
+        short_options = ''
+        options = []
+
+        conf = info['Config']
+        hconf = info['HostConfig']
+
+        options.append("--name {name}".format(name=info['Name'][1:]))
+
+        if not conf['AttachStdout']:
+            short_options += 'd'
+
+        if conf['OpenStdin']:
+            short_options += 'i'
+
+        if conf['Tty']:
+            short_options += 't'
+
+        if (len(short_options) > 0):
+            options.append('-' + short_options)
+
+        options.append("-h {hostname}".format(hostname=conf['Hostname']))
+
+        if 'Binds' in hconf and isinstance(hconf['Binds'], list):
+            for v in hconf['Binds']:
+                options.append("-v {binds}".format(binds=v))
+
+        if 'PortBindings' in hconf and isinstance(hconf['PortBindings'], dict):
+            for k, v in hconf['PortBindings'].items():
+                for hv in v:
+                    portbinding = ''
+                    if 'HostIp' in hv and hv['HostIp']:
+                        portbinding += hv['HostIp'] + ':'
+                    if 'HostPort' in hv and hv['HostPort']:
+                        portbinding += hv['HostPort'] + ':'
+                    portbinding += k
+                    options.append("-p {portbinding}".format(portbinding=portbinding))
+
+        if 'RestartPolicy' in hconf and hconf['RestartPolicy']['Name']:
+            policy = hconf['RestartPolicy']['Name']
+            if hconf['RestartPolicy']['MaximumRetryCount'] > 0:
+                policy += ':' + str(hconf['RestartPolicy']['MaximumRetryCount'])
+            options.append("--restart={policy}".format(policy=policy))
+
+        if hconf['Privileged']:
+            options.append('--privileged')
+
+        if type(conf['Env']) is list :
+            for v in conf['Env']:
+                options.append("-e={env}".format(env=pipes.quote(v)))
+
+        entry = []
+        if type(conf['Entrypoint']) is list :
+            for v in conf['Entrypoint']:
+                entry.append(pipes.quote(v))
+        elif type(conf['Entrypoint']) is str :
+            entry.append(pipes.quote(conf['Entrypoint']))
+        
+        if (len(entry) > 0):
+            options.append("--entrypoint={entry}".format(entry=' '.join(entry)))
+
+        cmd = []
+        if type(conf['Cmd']) is list :
+            for v in conf['Cmd']:
+                cmd.append(pipes.quote(v))
+        elif type(conf['Cmd']) is str:
+            cmd.append(pipes.quote(conf['Cmd']))
+
+        print('docker run{sep}{options}{sep}{image}{sep}{cmd}'.format(
+            options=sep.join(options), sep=sep, image=conf['Image'], cmd=' '.join(cmd)))
+
+if __name__ == "__main__":
+    main()
