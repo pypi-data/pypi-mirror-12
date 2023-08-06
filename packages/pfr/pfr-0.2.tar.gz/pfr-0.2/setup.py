db2294558924569d9385d588c0172cfa297ec2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:47 +0000

    Add package pfr/0.2/pfr-0.2.tar.gz

---
+++ packages/pfr/pfr-0.2.tar.gz/pfr-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='pfr',
+      version='0.2',
+      description='Scraping data from pro-football-reference.com',
+      url='https://github.com/mdgoldberg/pfr',
+      author='Matt Goldberg',
+      author_email='mgoldberg@college.harvard.edu',
+      packages=find_packages()
+      )
