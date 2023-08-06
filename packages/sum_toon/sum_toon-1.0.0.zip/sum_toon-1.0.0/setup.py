8fb51821d8170989bbee94228926d0eb7ab70f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:36 +0000

    Add package sum_toon/1.0.0/sum_toon-1.0.0.zip

---
+++ packages/sum_toon/sum_toon-1.0.0.zip/sum_toon-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name       = 'sum_toon',
+    version    = '1.0.0',
+    py_modules = ['sum_toon'],
+    author     = 'toonyang',
+    author_email = 'toontoon_yang@163.com',
+    url          = 'http://www.lovebabyy.com',
+    description  = 'A simple sum of two numbers'
+    )
---
+++ packages/sum_toon/sum_toon-1.0.0.zip/sum_toon-1.0.0/sum_toon.py
@@ -0,0 +1,2 @@
+def sum_toon(sum1,sum2):
+    return(sum1+sum2)
