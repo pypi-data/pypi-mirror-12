c0facd509f1c20f10a1a8bafaa4b597e4f541cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:09 +0000

    Add package platformio/2.3.5/platformio-2.3.5.tar.gz

---
+++ packages/platformio/platformio-2.3.5.tar.gz/platformio-2.3.5/platformio/__init__.py
@@ -0,0 +1,32 @@
+# Copyright 2014-2015 Ivan Kravets <me@ikravets.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+VERSION = (2, 3, 5)
+__version__ = ".".join([str(s) for s in VERSION])
+
+__title__ = "platformio"
+__description__ = (
+    "An open-source cross-platform code builder and the missing library "
+    "manager (Ready for embedded development, IDE and Continuous integration, "
+    "Arduino and MBED compatible)"
+)
+__url__ = "http://platformio.org"
+
+__author__ = "Ivan Kravets"
+__email__ = "me@ikravets.com"
+
+__license__ = "Apache Software License"
+__copyright__ = "Copyright 2014-2015 Ivan Kravets"
+
+__apiurl__ = "http://api.platformio.org"
