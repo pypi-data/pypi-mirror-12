1e6af67dc506bcbbe49376352188ef4736c4dc30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:11 +0000

    Add package django-classy-settings/1.1.5/django-classy-settings-1.1.5.tar.gz

---
+++ packages/django-classy-settings/django-classy-settings-1.1.5.tar.gz/django-classy-settings-1.1.5/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup
+
+setup(
+    name='django-classy-settings',
+    version='1.1.5',
+    description='Simple class-based settings for Django',
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    packages=[
+        'cbs',
+        'cbs.base',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+    ],
+    install_requires=[
+        'Django>=1.6',
+    ],
+)
