ce03fc2ec0888e0521b34f366f48634bfa156c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:35 +0000

    Add package trident_nester/1.0.0/trident_nester-1.0.0.zip

---
+++ packages/trident_nester/trident_nester-1.0.0.zip/trident_nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = 'trident_nester',
+        version = '1.0.0',
+        py_modules = ['trident_nester'],
+        author = 'trident_ashish',
+        author_email = 'bhatia.ash@gmail.com',
+        description = 'Experimental list printer'
+     )
+        
+    
