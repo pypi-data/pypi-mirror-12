84714891b5a415c6935a524e0f905069e33d6e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:00 +0000

    Add package pymay/1.0.0/pymay-1.0.0.tar.gz

---
+++ packages/pymay/pymay-1.0.0.tar.gz/pymay-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+name='pymay',
+version='1.0.0',
+description='A micro framework Google AppEngine Based',
+url='https://github.com/jesse1983/pymay',
+author='Jesse Pinheiro',
+author_email='jessebpinheiro@gmail.com',
+license='MIT',
+packages=['pymay'],
+zip_safe=False
+)
