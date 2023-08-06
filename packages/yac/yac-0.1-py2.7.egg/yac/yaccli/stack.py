d8d94bbea17dd2ea868d43157fb27ca54288d78f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:54 +0000

    Add package yac/0.1/yac-0.1-py2.7.egg

---
+++ packages/yac/yac-0.1-py2.7.egg/yac/yaccli/stack.py
@@ -0,0 +1,21 @@
+import argparse
+from yaccli.lib.stack import create_stack
+
+def main():
+
+    parser = argparse.ArgumentParser('Print a stack for an Atlassian application via cloudformation')
+    # required args
+    parser.add_argument('app',  help='name of the app to build stack for', 
+                                choices=['jira','crowd','confluence','bamboo','stash','hipchat'])
+    parser.add_argument('env',  help='the env', 
+                                choices=['dev', 'stage', 'prod','archive'])
+
+    # optional
+    # store_true allows user to not pass a value (default to true, false, etc.)
+    parser.add_argument('-c','--create',  
+                        help='create a new stack rather than updating existing (defaults to false)', 
+                        action='store_true')
+
+    args = parser.parse_args()
+
+    return create_stack()
