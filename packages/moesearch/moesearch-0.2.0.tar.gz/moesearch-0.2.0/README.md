091ec8367110ee2d8e37778de44658e28fbc471f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:57 +0000

    Add package moesearch/0.2.0/moesearch-0.2.0.tar.gz

---
+++ packages/moesearch/moesearch-0.2.0.tar.gz/moesearch-0.2.0/README.md
@@ -0,0 +1,33 @@
+MoeSearch
+=========
+
+A stupid Python library for [desustorage.org]http://desustorage.org/) (dump retrived from archive.moe, that was previously known as FoolzArchive) REST API.  
+To be fair, actually it should work with every installation of FoolFuuka, but yve have to try that.
+
+Useless binaries included.
+
+BEWARE
+------
+
+This software is experimental, use it at your own risk.
+For those who want to try this...  
+[PyPI archive](https://pypi.python.org/pypi/moesearch/)
+
+Quickstart
+--------
+
+(You can check the executables in /bin folder).
+FoolFuuka docs are available [at foolz.us](https://web.archive.org/web/20140728111047/http://www.foolz.us/docs/foolfuuka/documentation.html).  
+```py
+>>> import moesearch
+>>> print(moesearch.search(board="a", text="woxxy")[2].comment)
+>>112732805
+Fuck Woxxy.
+>>> print(moesearch.index("a", 1)["112834776"].op.media.media_link)
+http://data.archive.moe/board/a/image/1366/74/1366741186747.jpg
+>>> print(moesearch.post("a", 112766871).board.short_name)
+a
+>>> print(moesearch.thread("a", 112800651).posts[0].comment)
+>>112800651
+I fucking went grocery shopping all the time when I was a kid. I didn't have qt lesbian friends to go with though.
+```
---
+++ packages/moesearch/moesearch-0.2.0.tar.gz/moesearch-0.2.0/moesearch/api.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import requests
+from .parser import *
+from .exceptions import ArchiveException
+from pprint import pprint
+
+#disabling warnings about unverified SSL certificates
+#as urllib3 is embedded, we don't have to worry about
+#urllib3 being missing.
+requests.packages.urllib3.disable_warnings()
+
+DESUSTORAGE_API_URL = "https://desustorage.org/_/api/chan"
+
+def index(board, page=1):
+  req = requests.get("{}/index".format(DESUSTORAGE_API_URL),
+          stream=False, verify=False,
+          params = {"board": str(board), "page": int(page)})
+  #print(req.content)
+  res = req.json()
+  if ArchiveException.is_error(res):
+    raise ArchiveException(res)
+  for thread_num in res:
+    res[thread_num] = IndexResult(res[thread_num])
+  return res 
+
+def search(board, **kwargs):
+  url = "{}/search".format(DESUSTORAGE_API_URL)
+  try:
+    kwargs["boards"] = board.lower() #it's a string?
+  except AttributeError:
+    try: 
+    #https://github.com/FoolCode/FoolFuuka/blob/master/src/Controller/Api/Chan.php#L328
+      kwargs["boards"] = ".".join([str(b) for b in board]) #we got a list of boards!
+    except TypeError: #it's not an iterable (probably a Board object): try to convert it
+      kwargs["boards"] = str(board)
+  req = requests.get(url, stream=False, verify=True, params=kwargs)
+  res = req.json()
+  if ArchiveException.is_error(res):
+    raise ArchiveException(res)
+  res = res[0]
+  return [Post(post_obj) for post_obj in res["posts"]]
+
+def thread(board, thread_num, latest_doc_id=-1, last_limit=-1):
+  url = "{}/thread".format(DESUSTORAGE_API_URL)
+  payload = {"board": str(board), "num": thread_num}
+  if(latest_doc_id != -1):
+    payload["latest_doc_id"] = (int(latest_doc_id))
+  if(last_limit != -1):
+    payload["last_limit"] = (int(last_limit))
+  req = requests.get(url, stream=False, verify=True, params=payload)
+  res = req.json()
+  if ArchiveException.is_error(res):
+    raise ArchiveException(res)
+  return Thread(res)
+
+def post(board, post_num):
+  req = requests.get("{}/post".format(DESUSTORAGE_API_URL),
+                  verify=True,  stream=False,
+                  params={"board":str(board), "num":post_num})
+  res = req.json()
+  if ArchiveException.is_error(res):
+    raise ArchiveException(res)
+  return Post(res)
