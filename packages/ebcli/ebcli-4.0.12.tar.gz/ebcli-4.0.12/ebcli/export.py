b08c114ce7a2edfb88bdb79e644098377939e1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:32 +0000

    Add package ebcli/4.0.12/ebcli-4.0.12.tar.gz

---
+++ packages/ebcli/ebcli-4.0.12.tar.gz/ebcli-4.0.12/ebcli/export.py
@@ -0,0 +1,30 @@
+"""
+ElasticBox Confidential
+Copyright (c) 2014 All Right Reserved, ElasticBox Inc.
+
+NOTICE:  All information contained herein is, and remains the property
+of ElasticBox. The intellectual and technical concepts contained herein are
+proprietary and may be covered by U.S. and Foreign Patents, patents in process,
+and are protected by trade secret or copyright law. Dissemination of this
+information or reproduction of this material is strictly forbidden unless prior
+written permission is obtained from ElasticBox
+"""
+
+import os
+
+from ebcli import Boxes, AuthenticatedSession
+from ebcli import RECURSIVE_ARG, BOXES_PATH_ARG, add_standard_argument
+
+
+def initialize_parser(subparsers):
+    parser = subparsers.add_parser('export', help='Export Box to a folder')
+
+    add_standard_argument(parser, BOXES_PATH_ARG)
+    add_standard_argument(parser, RECURSIVE_ARG)
+    parser.add_argument(dest='box_id', metavar='box-id', help='Box ID')
+    parser.set_defaults(func=execute_command)
+
+
+def execute_command(args, _unknown):
+    session = AuthenticatedSession(args.url, args.token)
+    Boxes(args.path if 'path' in args else os.getcwd(), session).export_box(args.box_id, args.recursive)
