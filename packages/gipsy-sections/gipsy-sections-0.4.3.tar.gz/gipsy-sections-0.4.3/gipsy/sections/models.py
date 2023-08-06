d50bae5f5b5b61072a8a670600b613fefd88f479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:52 +0000

    Add package gipsy-sections/0.4.3/gipsy-sections-0.4.3.tar.gz

---
+++ packages/gipsy-sections/gipsy-sections-0.4.3.tar.gz/gipsy-sections-0.4.3/gipsy/sections/models.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = u"Rafał Selewońko <rafal@selewonko.com>"
+
+
+from gipsy.sections.models_base import SectionBase
+from gipsy.sections.managers import SectionManager, SectionsManager
+
+
+class Section(SectionBase):
+
+    objects = SectionManager()
+    sections = SectionsManager()
