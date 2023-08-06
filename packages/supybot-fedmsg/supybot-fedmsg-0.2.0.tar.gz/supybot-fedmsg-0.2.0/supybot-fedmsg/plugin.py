526fce7c1d95e69d925dab12800448b9f6c269b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:49 +0000

    Add package supybot-fedmsg/0.2.0/supybot-fedmsg-0.2.0.tar.gz

---
+++ packages/supybot-fedmsg/supybot-fedmsg-0.2.0.tar.gz/supybot-fedmsg-0.2.0/supybot-fedmsg/plugin.py
@@ -0,0 +1,141 @@
+""" supybot-fedmsg - augment other supybot plugins to emit fedmsg messages.
+
+:Author: Ralph Bean <rbean@redhat.com>
+"""
+
+import fedmsg
+import socket
+import supybot.callbacks
+import threading
+import time
+
+# A flag placed on wrapped methods to note we have already wrapped them once.
+SENTINEL = '_sentinel_flag'
+
+# When we start up, we try to wrap meetbot.  But if it's not there, we die and
+# create a new thread that wakes up in 60s and tries again.
+INTERVAL = 60
+
+
+def already_wrapped(method):
+    """ Return true if it looks like we have already wrapped a target. """
+    return hasattr(method, SENTINEL) or hasattr(method.__func__, SENTINEL)
+
+
+class Fedmsg(supybot.callbacks.Plugin):
+    """ Use this plugin to fedmsg-enable various other supybot plugins.
+
+    It modifies other plugins at startup and does nothing else.
+
+    Supported plugins are:
+        - supybot-meetbot
+
+    """
+
+    def __init__(self, irc):
+        super(Fedmsg, self).__init__(irc)
+
+        # If fedmsg was already initialized, let's not re-do that.
+        if getattr(getattr(fedmsg, '__local', None), '__context', None):
+            print "Not reinitializing fedmsg."
+        else:
+            # Initialize fedmsg resources.
+            hostname = socket.gethostname().split('.', 1)[0]
+            fedmsg.init(name="supybot." + hostname)
+
+        # Launch in a thread to duckpunch *after* the other plugins
+        # have been set up.
+        thread = Injector()
+        thread.start()
+
+
+class Injector(threading.Thread):
+    """ Injector our code into other supybot plugins.
+
+    Wait 2 seconds before doing so to help make sure they've loaded
+    before we try.
+    """
+
+    def run(shmelf):
+        """ Replace some of meetbot's methods with our own which simply call
+        meetbot's original method, and then emit a fedmsg message before
+        returning.
+        """
+
+        time.sleep(INTERVAL)
+
+        try:
+            import sys
+            target_cls = sys.modules['MeetBot.meeting'].Meeting
+        except KeyError:
+            # Start another thread to finish our work later.
+            another_thread = Injector()
+            another_thread.start()
+            print "MeetBot not yet enabled."
+            print "  Will try to wrap fedmsg again in %is" % INTERVAL
+            return  # Bail out early.
+
+        tap_points = {
+            'do_startmeeting': 'meeting.start',
+            'do_endmeeting': 'meeting.complete',
+            'do_topic': 'meeting.topic.update',
+
+            'do_agreed':    'meeting.item.agreed',
+            'do_accepted':  'meeting.item.accepted',
+            'do_rejected':  'meeting.item.rejected',
+
+            'do_action':    'meeting.item.action',
+            'do_info':      'meeting.item.info',
+            'do_idea':      'meeting.item.idea',
+            'do_help':      'meeting.item.help',  # We map #help and #halp
+            'do_halp':      'meeting.item.help',  # to the same topic...
+            'do_link':      'meeting.item.link',
+        }
+        for target_method, topic in tap_points.items():
+
+            def wrapper_factory(topic):
+                old_method = getattr(target_cls, target_method)
+
+                def wrapper(self, *args, **kw):
+                    # Call the target plugin's original code first and save the
+                    # result.
+                    result = old_method.__func__(self, *args, **kw)
+
+                    # Include the owner of the meeting in the chairs dict just
+                    # in case they never explicitly #chair'd themselves.
+                    chairs = self.chairs
+                    chairs[self.owner] = chairs.get(self.owner, True)
+
+                    # Emit on "org.fedoraproject.prod.meetbot.meeting.start"
+                    fedmsg.publish(
+                        modname="meetbot",
+                        topic=topic,
+                        msg=dict(
+                            owner=self.owner,
+                            chairs=chairs,
+                            attendees=self.attendees,
+                            url=self.config.filename(url=True),
+                            meeting_topic=self._meetingTopic,
+                            topic=self.currenttopic,
+                            channel=self.channel,
+                            details=kw,  # This includes the 'who' and 'what'
+                        ),
+                    )
+
+                    # Return the original result from the target plugin.
+                    return result
+
+                # Set a flag indicating that we are wrapping the other plugin
+                setattr(wrapper, SENTINEL, True)
+
+                if already_wrapped(old_method):
+                    return old_method
+                else:
+                    return wrapper
+
+            # Build the new method and attach it to the target class.
+            new_method = wrapper_factory(topic=topic)
+            setattr(target_cls, target_method, new_method)
+
+
+Class = Fedmsg
