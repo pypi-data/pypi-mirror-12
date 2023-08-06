f04a8c80a266df052dd9b41e5ee4cd09f2f75bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:55 +0000

    Add package djinn_forms/1.3.5/djinn_forms-1.3.5.tar.gz

---
+++ packages/djinn_forms/djinn_forms-1.3.5.tar.gz/djinn_forms-1.3.5/djinn_forms/fields/keyword.py
@@ -0,0 +1,16 @@
+from django.forms.fields import Field
+from djinn_forms.widgets.keyword import KeywordWidget
+
+
+class KeywordField(Field):
+
+    """ Field for adding keywords to content """
+
+    widget = KeywordWidget
+
+    def prepare_value(self, data):
+
+        if not data:
+            return []
+
+        return data.split(',')
