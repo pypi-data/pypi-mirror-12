8d277f7175735d2e38459a51b0def77c0dc8d587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:25 +0000

    Add package pytsite/0.33.6/PytSite-0.33.6.tar.gz

---
+++ packages/pytsite/PytSite-0.33.6.tar.gz/PytSite-0.33.6/pytsite/browser/res/assets/js/common.js
@@ -0,0 +1,19 @@
+pytsite = {};
+
+pytsite.js = {
+    request: function(method, endpoint, data, success, error) {
+        return $.ajax({
+            url: '/pytsite/browser/' + endpoint,
+            method: method,
+            data: data,
+            success: success,
+            error: error
+        });
+    },
+    get: function(endpoint, data, success, error) {
+        return pytsite.js.request('GET', endpoint, data, success, error)
+    },
+    post: function(endpoint, data, success, error) {
+        return pytsite.js.request('POST', endpoint, data, success, error)
+    }
+};
