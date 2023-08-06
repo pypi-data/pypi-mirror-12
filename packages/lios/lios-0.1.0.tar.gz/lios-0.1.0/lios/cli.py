8839b68651737d681e98c06de9ef1a31c0dfb577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:06 +0000

    Add package lios/0.1.0/lios-0.1.0.tar.gz

---
+++ packages/lios/lios-0.1.0.tar.gz/lios-0.1.0/lios/cli.py
@@ -0,0 +1,69 @@
+import click
+import subprocess
+
+
+last_media = None
+
+
+def download_media(media_name):
+    """
+    Download media in url if it has content type.
+    """
+    url = 'http://192.168.1.45:15555/0/{}'.format(media_name)
+    cmd = 'curl -I {}'.format(url)
+    result = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+    if 'Content-Type' in result:
+        print('Downloading {}'.format(media_name))
+        cmd = 'wget {} -q'.format(url)
+        subprocess.call(cmd, shell=True)
+
+
+def sync_media(f_type):
+    """
+    Download media of given f_type
+    :f_type: str
+    """
+    if f_type == 'photos':
+        media = 'fr_{}.jpg'
+    if f_type == 'videos':
+        media = 'fs_{}.mov'
+
+    for i in range(last_media + 1):
+        media_name = media.format(i)
+        download_media(media_name)
+
+
+@click.command()
+@click.option('--photos', '-p', is_flag=True, help='Sync photos only')
+@click.option('--videos', '-v', is_flag=True, help='Sync videos only')
+@click.option('--media', '-m', is_flag=True, help='Sync photos and videos')
+@click.argument('address', required=True)
+def main(address, media, photos, videos):
+    print('Detecting device at {}'.format(address))
+    cmd = 'wget {}/0/ -O /tmp/lios.html'.format(address)
+    try:
+        result = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+        if '200 OK' in result:
+            print('Device detected.')
+        else:
+            print('Unable to detect device.')
+
+        with open("/tmp/lios.html", "r") as fh:
+            data = fh.read().replace('\n', '')
+
+            # to get number from html like this
+            # <img class="thumbnail" src="/0/tn_55.jpg"
+            global last_media
+            last_media = int(data.split('<img class="thumbnail" src="/0/tn_')[1].split('.jpg')[0])
+            
+    except subprocess.CalledProcessError:
+        print('Unable to detect address.')
+
+    if media:
+        photos = videos = True
+        
+    if photos:
+        sync_media('photos')
+
+    if videos:
+        sync_media('videos')
---
+++ packages/lios/lios-0.1.0.tar.gz/lios-0.1.0/setup.py
@@ -0,0 +1,46 @@
+"""
+Auto sync photos/videos/music/files between iOS & Linux.
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='lios',
+    version='0.1.0',
+    url='https://github.com/chillaranand/lios',
+    license='BSD',
+    author='anand reddy pandikunta',
+    author_email='anand21nanda@gmail.com',
+    description='Auto sync photos/videos/music/files between iOS & Linux.',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'lios = lios.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
