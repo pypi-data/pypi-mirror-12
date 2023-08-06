cc7da6662db31b2c4e1ed5d1b7402615df0f79ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:44 +0000

    Add package listPrinterAGP/1.0.0/listPrinterAGP-1.0.0.tar.gz

---
+++ packages/listPrinterAGP/listPrinterAGP-1.0.0.tar.gz/listPrinterAGP-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name = 'listPrinterAGP',
+	version = '1.0.0',
+	py_modules=['listPrinterAGP'],
+	author='akash',
+	author_email='akash.abcd@gmail.com',
+	url='www.google.com',
+	description='A simple print list function')
+
