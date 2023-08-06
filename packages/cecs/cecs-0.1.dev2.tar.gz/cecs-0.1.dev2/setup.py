01458281d07e298e08ab7ac46fba603b469fd016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:35 +0000

    Add package cecs/0.1.dev2/cecs-0.1.dev2.tar.gz

---
+++ packages/cecs/cecs-0.1.dev2.tar.gz/cecs-0.1.dev2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='cecs',
+      version='0.1.dev2',
+      description='Cisco Enterprise Cloud Suite',
+      long_description='long_description',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+      ],
+      keywords='cecs ucsd icfb cisco',
+      url='https://github.com/clijockey/CECS',
+      author='clijockey',
+      author_email='rob.j.edwards@gmail.com',
+      license='MIT',
+      packages=['cecs'],
+      install_requires=[
+          'markdown',
+          'requests',
+          'json',
+          'colorama',
+      ],
+      include_package_data=True,
+      zip_safe=False)
