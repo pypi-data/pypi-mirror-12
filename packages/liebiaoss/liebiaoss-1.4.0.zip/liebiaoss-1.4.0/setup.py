d187cbc9df3edfab4fac9ce7b68417ac073f6ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:01 +0000

    Add package liebiaoss/1.4.0/liebiaoss-1.4.0.zip

---
+++ packages/liebiaoss/liebiaoss-1.4.0.zip/liebiaoss-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name              = 'liebiaoss',
+    version           = '1.4.0',
+    py_modules        = ['liebiao'],
+    author            = 'szw',
+    author_email      = '980092413@qq.com',
+    url               = '123456789',
+    description       = 'example',
+    )
