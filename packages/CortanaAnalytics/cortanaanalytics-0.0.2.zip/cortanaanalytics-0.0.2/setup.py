2b5ad41f043a254f19995a3c5146ebab21665814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:12 +0000

    Add package CortanaAnalytics/0.0.2/cortanaanalytics-0.0.2.zip

---
+++ packages/CortanaAnalytics/cortanaanalytics-0.0.2.zip/cortanaanalytics-0.0.2/setup.py
@@ -0,0 +1,40 @@
+﻿#!/usr/bin/env python
+
+from setuptools import setup
+
+# To build:
+# python setup.py sdist
+# python setup.py bdist_wheel
+#
+# To install:
+# python setup.py install
+#
+# To register (only needed once):
+# python setup.py register
+#
+# To upload:
+# python setup.py sdist upload
+# python setup.py bdist_wheel upload
+
+setup(
+    name='cortanaanalytics',
+    version='0.0.2',
+    description='Wrappers of Cortana Analytics services',
+    license='Apache License 2.0',
+    author='Microsoft Corporation',
+    author_email='ptvshelp@microsoft.com',
+    url='https://github.com/crwilcox/cortanaanalytics',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    packages=['cortanaanalytics'],
+    install_requires=[
+        'requests',
+    ],
+    zip_safe = False,
+)
