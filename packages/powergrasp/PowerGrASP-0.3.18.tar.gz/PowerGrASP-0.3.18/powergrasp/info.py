bf90fd2913e73d2fb040c358ca6e55b503d9ec6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:25 +0000

    Add package powergrasp/0.3.18/PowerGrASP-0.3.18.tar.gz

---
+++ packages/powergrasp/PowerGrASP-0.3.18.tar.gz/PowerGrASP-0.3.18/powergrasp/info.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__name__    = 'PowerGrASP'
+__fix       = '18'
+__minor     = '3'
+__major     = '0'
+__version__ = '.'.join((__major, __minor, __fix))
+
+
+# link between filenames and studies
+from collections import defaultdict
+STUDYNAMES = defaultdict(lambda: 'No related study')
+STUDYNAMES.update({
+    'proteome_yeast_1'   : 'Casein Kinase II Complex',
+    'proteome_yeast_2'   : 'Histone interaction',
+    'structural_binding' : 'Interactions of SH3 Carrying Proteins',
+    'phosphatase'        : 'Human Protein Tyrosine Phosphatase Homology Network',
+})
+
+
+
