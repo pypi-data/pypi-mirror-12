d47a74e2c927829312a2613f33030f604b00aa0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:27 +0000

    Add package humboldt.cmfbibliographyat/0.2.5.3/humboldt.cmfbibliographyat-0.2.5.3-py2-none-any.whl

---
+++ packages/humboldt.cmfbibliographyat/humboldt.cmfbibliographyat-0.2.5.3-py2-none-any.whl/humboldt/cmfbibliographyat/browser/__init__.py
@@ -0,0 +1,4 @@
+##########################################################################
+# humboldt.cmfbibliographyat - published under the GNU Public License V 2
+# Written by Andreas Jung, ZOPYX Ltd. & Co. KG, D-72070 Tuebingen, Germany
+##########################################################################
---
+++ packages/humboldt.cmfbibliographyat/humboldt.cmfbibliographyat-0.2.5.3-py2-none-any.whl/humboldt/cmfbibliographyat/interfaces.py
@@ -0,0 +1,4 @@
+from zope.interface import Interface
+
+class IBibLayer(Interface):
+    """Marker interface for browser layer"""
