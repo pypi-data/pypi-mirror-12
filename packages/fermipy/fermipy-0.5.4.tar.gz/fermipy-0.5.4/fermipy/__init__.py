b590acf6d0cfe54645b84c245799b043f6cbd8b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:06 +0000

    Add package fermipy/0.5.4/fermipy-0.5.4.tar.gz

---
+++ packages/fermipy/fermipy-0.5.4.tar.gz/fermipy-0.5.4/fermipy/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+
+__version__ = "unknown"
+
+try:
+    from version import get_git_version
+    __version__ = get_git_version()
+except Exception as message:
+    print(message)
+
+__author__ = "Matthew Wood"
+
+PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
+os.environ['FERMIPY_ROOT'] = PACKAGE_ROOT
