b96d7ee26cba5b4ebe534d3f82aa02637dc168a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:39 +0000

    Add package eventsourcing/0.9.3/eventsourcing-0.9.3.tar.gz

---
+++ packages/eventsourcing/eventsourcing-0.9.3.tar.gz/eventsourcing-0.9.3/eventsourcing/infrastructure/event_sourced_repo.py
@@ -0,0 +1,52 @@
+from abc import ABCMeta, abstractproperty
+from functools import reduce
+from six import with_metaclass
+from eventsourcing.domain.model.entity import EntityRepository
+from eventsourcing.infrastructure.event_store import EventStore
+
+
+class EventPlayer(object):
+
+    def __init__(self, event_store, mutator, id_prefix=''):
+        assert isinstance(event_store, EventStore), event_store
+        self.event_store = event_store
+        self.mutator = mutator
+        self.id_prefix = id_prefix
+
+    def __getitem__(self, entity_id):
+        # Get the entity's domain events from the event store.
+        stored_entity_id = self.id_prefix + '::' + entity_id
+        domain_events = self.event_store.get_entity_events(stored_entity_id)
+
+        # Mutate entity state according to the sequence of domain events.
+        entity = reduce(self.mutator, domain_events, None)
+        if entity is None:
+            # Entity already discarded, or it was never created.
+            raise KeyError(entity_id)
+        else:
+            return entity
+
+
+class EventSourcedRepository(EntityRepository):
+
+    def __init__(self, event_store):
+        self.event_player = self.construct_event_player(event_store)
+
+    def construct_event_player(self, event_store):
+        return EventPlayer(event_store=event_store, mutator=self.domain_class.mutator, id_prefix=self.domain_class.__name__)
+
+    @abstractproperty
+    def domain_class(self):
+        """Defines the type of entity available in this repo.
+        """
+
+    def __getitem__(self, entity_id):
+        return self.event_player[entity_id]
+
+    def __contains__(self, entity_id):
+        try:
+            self[entity_id]
+        except KeyError:
+            return False
+        else:
+            return True
---
+++ packages/eventsourcing/eventsourcing-0.9.3.tar.gz/eventsourcing-0.9.3/eventsourcing/infrastructure/event_store.py
@@ -0,0 +1,27 @@
+from eventsourcing.domain.model.events import DomainEvent
+from eventsourcing.infrastructure.stored_events.base import StoredEventRepository, serialize_domain_event, \
+    recreate_domain_event
+
+
+class EventStore(object):
+
+    def __init__(self, stored_event_repo):
+        assert isinstance(stored_event_repo, StoredEventRepository), stored_event_repo
+        self.stored_event_repo = stored_event_repo
+
+    def append(self, domain_event):
+        assert isinstance(domain_event, DomainEvent)
+
+        # Serialize the domain event.
+        stored_event = serialize_domain_event(domain_event)
+
+        # Append the stored event to the stored event repo.
+        self.stored_event_repo.append(stored_event)
+
+    def get_entity_events(self, stored_entity_id):
+
+        # Get all the stored events for the entity.
+        stored_events = self.stored_event_repo.get_entity_events(stored_entity_id=stored_entity_id)
+
+        # Recreate the entity's domain events from the stored events.
+        return map(recreate_domain_event, stored_events)
