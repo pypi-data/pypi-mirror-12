3620e5269162ac1a227d7debdf72af41bc99fcdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:36 +0000

    Add package nesterwang/1.0.2/nesterwang-1.0.2.zip

---
+++ packages/nesterwang/nesterwang-1.0.2.zip/nesterwang-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+       name  = 'nesterwang',
+       version = '1.0.2',
+       py_modules = ['nesterwang'],
+       author = 'bohyunwang',
+       author_email = 'bhwang99@hanmail.net',
+       url = 'http://www.headfirstlabs.com',
+       description = 'A simple printer of nested lists',
+     )
