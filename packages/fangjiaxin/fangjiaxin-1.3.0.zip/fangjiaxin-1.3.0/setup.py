c9138c71f7efedf1e710e84af3fa802f49cf07ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:51 +0000

    Add package fangjiaxin/1.3.0/fangjiaxin-1.3.0.zip

---
+++ packages/fangjiaxin/fangjiaxin-1.3.0.zip/fangjiaxin-1.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='fangjiaxin',
+        version='1.3.0',
+        py_modules=['fangjiaxin'],
+        author='fang',
+        author_email='shirley__fang@163.com',
+        url='http://www.headfirstlabs.com',
+        description='a simple printer of fang\'s list.',
+
+      )
