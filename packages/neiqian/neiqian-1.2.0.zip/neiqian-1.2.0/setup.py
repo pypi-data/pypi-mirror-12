a7cb091e864c9eee15162569f387719b507b7550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:17 +0000

    Add package neiqian/1.2.0/neiqian-1.2.0.zip

---
+++ packages/neiqian/neiqian-1.2.0.zip/neiqian-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'neiqian',
+		version = '1.2.0',
+		py_modules = ['neiqian'],
+		author = 'chris_lei',
+		author_email = '374301183@qq.com',
+		url = 'http://www.eighthoursenglish.com',
+		description = 'A simple printer of nested lists.',
+		)
