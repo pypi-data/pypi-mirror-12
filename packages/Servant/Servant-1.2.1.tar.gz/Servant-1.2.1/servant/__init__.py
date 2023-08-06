0ee3c605e6ebab5fcb871a737666d55033fc0db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:21 +0000

    Add package Servant/1.2.1/Servant-1.2.1.tar.gz

---
+++ packages/Servant/Servant-1.2.1.tar.gz/Servant-1.2.1/servant/__init__.py
@@ -0,0 +1,12 @@
+
+# Import the most common items to simplify URL handlers code.
+
+from .connection import HttpProtocol
+from .staticfiles import File
+from .routing import route
+from .middleware import middleware
+from .middleware import register as register_middleware
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
