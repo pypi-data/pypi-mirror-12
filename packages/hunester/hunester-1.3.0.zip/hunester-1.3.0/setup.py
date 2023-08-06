0a7aa0cfb9bf7c8ba97c592a107997220a278faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:14 +0000

    Add package hunester/1.3.0/hunester-1.3.0.zip

---
+++ packages/hunester/hunester-1.3.0.zip/hunester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'hunester',
+        version = '1.3.0',
+        py_modules = ['hunester'],
+        author = 'hughlin',
+        author_email = 'hughlin2003@hotmail.com',
+        url = 'http://hughes.zapto.org',
+        description = 'A simple printer of nested lists',
+     )
