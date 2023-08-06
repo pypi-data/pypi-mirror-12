e21685c248673f2b987f5db1501e03ea6cafa734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:47 +0000

    Add package rebulk/0.6.0/rebulk-0.6.0.tar.gz

---
+++ packages/rebulk/rebulk-0.6.0.tar.gz/rebulk-0.6.0/rebulk/test/default_rules_module.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=no-self-use, pointless-statement, missing-docstring, invalid-name
+from ..match import Match
+from ..rules import Rule, RemoveMatch, AppendMatch, RenameMatch
+
+
+class RuleRemove0(Rule):
+    consequence = RemoveMatch
+    def when(self, matches, context):
+        return matches[0]
+
+
+class RuleAppend0(Rule):
+    consequence = AppendMatch()
+    def when(self, matches, context):
+        return Match(5, 10)
+
+class RuleRename0(Rule):
+    consequence = [RenameMatch('renamed')]
+    def when(self, matches, context):
+        return [Match(5, 10, name="original")]
+
+class RuleRemove1(Rule):
+    consequence = [RemoveMatch()]
+    def when(self, matches, context):
+        return [matches[0]]
+
+class RuleAppend1(Rule):
+    consequence = [AppendMatch]
+    def when(self, matches, context):
+        return [Match(5, 10)]
+
+class RuleRename1(Rule):
+    consequence = RenameMatch('renamed')
+    def when(self, matches, context):
+        return [Match(5, 10, name="original")]
+
+class RuleAppend2(Rule):
+    consequence = [AppendMatch('renamed')]
+    properties = {'renamed': []}
+    def when(self, matches, context):
+        return [Match(5, 10)]
+
+class RuleRename2(Rule):
+    consequence = RenameMatch('renamed')
+    def when(self, matches, context):
+        return Match(5, 10, name="original")
+
+class RuleAppend3(Rule):
+    consequence = AppendMatch('renamed')
+    def when(self, matches, context):
+        return [Match(5, 10)]
+
+class RuleRename3(Rule):
+    consequence = [RenameMatch('renamed')]
+    def when(self, matches, context):
+        return Match(5, 10, name="original")
+
