55607e53d5c4d5513dc52642b3ffa46d9c024240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:03 +0000

    Add package nester_rickyzhao/1.6.0/nester_rickyzhao-1.6.0.zip

---
+++ packages/nester_rickyzhao/nester_rickyzhao-1.6.0.zip/nester_rickyzhao-1.6.0/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup
+
+setup(
+		name		= 'nester_rickyzhao',
+		version		= '1.6.0',
+		py_modules	= ['nester_rickyzhao'],
+		author		= 'hfpython',
+		author_email= 'rickyzhao_425@',
+		url			= 'http://www.headfirstable.com',
+		description = 'A simple printer of nester lists',
+		)
