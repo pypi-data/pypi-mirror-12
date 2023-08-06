0df6f38bfcf85f9debd9906e3c4df300796b065c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:29 +0000

    Add package Menus/0.1.7/Menus-0.1.7.tar.gz

---
+++ packages/Menus/Menus-0.1.7.tar.gz/Menus-0.1.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+import versioneer
+
+cmd_class = versioneer.get_cmdclass()
+
+setup(name='Menus',
+      version=versioneer.get_version(),
+      description='Create cli menus with ease',
+      url='https://github.com/JMSwag/Menus',
+      author='JMSwag',
+      author_email='johnymoswag@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=True,
+      install_requires=['six'],
+      cmdclass=cmd_class,
+      )
