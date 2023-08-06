b30e24b3049eb63f6e90a9e456f398b3f8ae7e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:01 +0000

    Add package behave-pytest/0.1.0/behave-pytest-0.1.0.tar.gz

---
+++ packages/behave-pytest/behave-pytest-0.1.0.tar.gz/behave-pytest-0.1.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+    name="behave-pytest",
+    version="0.1.0",
+    url="https://github.com/ribozz/behave-pytest",
+
+    author="Alex Rudakov",
+    author_email="ribozz@gmail.com",
+
+    description="Integrates pytest asserts with behave",
+    long_description=open('README.md').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=['pytest', 'behave'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
