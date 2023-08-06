c80006b452641296e25424f8f02a48fcc2d6d24e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:52 +0000

    Add package pybedtools/0.7.4/pybedtools-0.7.4.tar.gz

---
+++ packages/pybedtools/pybedtools-0.7.4.tar.gz/pybedtools-0.7.4/pybedtools/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.7.4'
+version = '0.7.4'
+full_version = '0.7.4'
+git_revision = '36d400077d61f777a1c51402580ace955c26cf64'
+release = True
+
+__version__ = version
+if not release:
+    version = full_version
