2f1a5dc05b2eb245ce135f1d33d2b3ced12b5dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:14 +0000

    Add package ghostly/0.2.0/ghostly-0.2.0.tar.gz

---
+++ packages/ghostly/ghostly-0.2.0.tar.gz/ghostly-0.2.0/CHANGELOG.md
@@ -0,0 +1,8 @@
+# Release 0.2.0 - Tue Dec  1 12:13:28 AEDT 2015
+
+- Added ability for xpath_wait to call xpath_click on completion.
+
+# Release 0.1.0 - Tue Dec  1 09:26:27 AEDT 2015
+
+- Initial release
+
---
+++ packages/ghostly/ghostly-0.2.0.tar.gz/ghostly-0.2.0/ghostly/errors.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+    module.name
+    ~~~~~~~~~~~~~~~
+    Preamble...
+"""
+from __future__ import absolute_import, print_function, unicode_literals
+
+
+__all__ = ['GhostlyError', 'DriverDoesNotExistError', 'GhostlyTestFailed']
+
+
+class GhostlyError(Exception):
+    """
+    Ghostly error, which all Ghostly errors inherit.
+    """
+    pass
+
+
+class DriverDoesNotExistError(GhostlyError):
+    """
+    Raised when :py:meth:`Ghostly.__init__` can't load a specific driver.
+    """
+    pass
+
+
+class GhostlyTestFailed(GhostlyError):
+    pass
+
+
+class GhostlyTimeoutError(GhostlyError):
+    """
+    Raised when a timeout occurs within Ghostly.
+    """
+    pass
