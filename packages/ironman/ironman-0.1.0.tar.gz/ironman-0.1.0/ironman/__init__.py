fb4b95b81f7e1d641380b88d69c620ac8431087f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:22 +0000

    Add package ironman/0.1.0/ironman-0.1.0.tar.gz

---
+++ packages/ironman/ironman-0.1.0.tar.gz/ironman-0.1.0/ironman/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.1.0'
+__all__ = ['communicator',
+           'hardware',
+           'interfaces',
+           'packet',
+           'server',
+           'utilities']
+
+def engage():
+    ''' Fire thrusters.
+    '''
+    from ironman.server import IPBusServerProtocol
+    from twisted.internet import reactor
+    from twisted.internet.defer import Deferred
+    reactor.listenUDP(8888, IPBusServerProtocol(Deferred))
+    reactor.run()
