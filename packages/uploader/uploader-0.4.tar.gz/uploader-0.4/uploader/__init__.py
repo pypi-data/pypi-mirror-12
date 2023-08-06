7ac0cbea94450ab45d0b605e739f473030f4ba9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:50 +0000

    Add package uploader/0.4/uploader-0.4.tar.gz

---
+++ packages/uploader/uploader-0.4.tar.gz/uploader-0.4/uploader/__init__.py
@@ -0,0 +1,300 @@
+# coding=utf-8
+"""
+ftp_upload.py
+Загружает файлы из home директории проекта по FTP на сервер. Загружает файл сначала в темповое имя, а потом переименовывает его - чтобы не он не обнулялся во время записи.
+Передавать имена файлов по argv, поддерживаются несколько файлов сразу. Можно передавать файлы пачками через буфер обмена - он их съест.
+Управление загрузкой происходит, если запустить скрипт без аргументов.
+Каталоги обрабатывать пока не умеет.
+
+Пример:
+ftp_upload.py Z:\home\rosrabota\www\404.htm Z:\home\rosrabota\www\off\ip.php
+    Грузит 2 файла себе в список.
+
+ftp_upload.py
+    Выдает список загруженных файлов и запрос на команду.
+"""
+import os
+import tempfile
+import time
+
+from structure import *
+
+stackPath = tempfile.gettempdir() + '/.uploader.stack'
+
+
+def split_conf_path(abspath):
+    """Разбиваем полный путь к файлу на сервер и относительный путь.
+    Если разбиение не получилось (сервер неизвестен), то возвращаем false
+    """
+    # Попытаться найти .upload.conf.py файл
+    path = abspath
+    while path != '/':
+        path = os.path.dirname(path)
+        if os.path.exists(path + '/.upload.conf.py'):
+            return path, abspath[len(path) + 1:]
+    return False
+
+
+def append_file(stack, confs, abs_path):
+    """Просто добавляем файл в конец списка
+
+    @param stack                Структура стека с подготовленными для загрузки файлами
+    @param confs                Dict конфигураций, создаётся динамически. Нужен, чтобы получить ignore файлов.
+    @param abs_path             Абсолютный путь добавляемого файла или каталога
+    """
+    assert isinstance(stack, Stack)
+    conf_and_path = split_conf_path(abs_path)
+    if not conf_and_path:
+        raise ConfError('Config file .upload.conf.py not found in this directory or parent tree')
+    conf_path, sub_path = conf_and_path
+
+    if conf_path not in confs:
+        confs[conf_path] = Conf(conf_path)
+    conf = confs[conf_path]
+    assert isinstance(conf, Conf)
+
+    stack_item = StackItem.from_sub_path(sub_path)
+    basename = os.path.basename(stack_item.pure_path)
+    if conf.is_ignore(basename):
+        return False
+    stack_item.server_filter.validate(conf)
+
+    if not os.path.exists(abs_path) and not os.path.islink(abs_path):  # We allow to add broken symlinks
+        raise ConfError('File ' + abs_path + ' does not exist')
+
+    if conf.is_exec(basename) and not os.access(abs_path, os.X_OK):
+        raise ConfError('Exec ' + abs_path + ' not executable')
+
+    # Если попалась директория - включить каждый файл в ней
+    if os.path.isdir(abs_path):
+        for sub_path in os.listdir(abs_path):
+            append_file(stack, confs, abs_path + '/' + sub_path)
+    else:
+        stack.append(conf_path, stack_item)
+    return True
+
+
+def append_files(files):
+    """Добавить пачку файлов из массива files.
+
+    Это обертка для append_file()
+    """
+    assert isinstance(files, list)
+    stack = Stack(stackPath)
+    confs = {}
+    for filename in files:
+        print filename + ': ',
+        if append_file(stack, confs, os.path.abspath(filename)):
+            print('OK')
+        else:
+            print('ignored')
+    stack.save()
+
+
+def receive_command():
+    """Ожидание и разбор команды от юзера."""
+
+    stack = Stack(stackPath)
+    if not stack:
+        print('Download stack is empty')
+        return False
+    # Load configs
+    confs = stack.load_confs()
+
+    # Выводим список файлов для загрузки
+    stack.print_data(confs)
+
+    command = raw_input("Enter: upload, 'c': clear list, 'w': watch for changes, ^C: stop > ")
+
+    if command == 'c':
+        # Чистим этот список файлов
+        stack.clear_and_save()
+        return True
+
+    if command == 'w':
+        # Мониторим файлы и заливаем их на сервер по мере изменения
+        watch_command(stack, confs)
+        return True
+
+    # Upload files to server/servers in parallel threads
+    was_error = [False]
+    def safe_run(fn):
+        try:
+            fn()
+        except:
+            # Код ниже стопорит скрипт, если случилась ошибка. Чтобы можно было посмотреть что там такое.
+            was_error[0] = True
+            import traceback
+            traceback.print_exc()
+
+    import threading
+    threads = []
+    for conf_path, stack_items in stack.data.iteritems():
+        conf = confs[conf_path]
+        assert isinstance(conf, Conf)
+        for server in conf.servers:
+            assert isinstance(server, Server)
+            items = filter(lambda v: server in v.server_filter, stack_items)
+
+            if items:
+                if conf.protocol == 'ssh':
+                    thread = threading.Thread(target=safe_run, args=(lambda: upload_ssh(conf, server, items),))
+                elif conf.protocol == 'ftp':
+                    thread = threading.Thread(target=safe_run, args=(lambda: upload_ftp(conf, server, items),))
+                else:
+                    raise Exception("Cannot process protocol: " + conf.protocol)
+                thread.start()
+                threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+    if was_error[0]:
+        return False
+    else:
+        if len(threads) > 1:
+            print('--- all uploads finished ---')
+        stack.clear_and_save()
+        return True
+
+
+def upload_ssh(conf, server, stack_items):
+    """Поехали аплоадить файлы на сервак по SSH
+
+    @param conf             Конфигурация аплоада
+    @param server           Сервер аплоада
+    @param stack_items      List of StackItem for upload
+    """
+    import io
+    import tarfile
+    import subprocess
+    assert isinstance(conf, Conf)
+    assert isinstance(server, Server)
+
+    ssh_args = ['/usr/bin/ssh']
+    if server.ssh_args:
+        ssh_args += server.ssh_args
+    ssh_args += [server.user_host, 'tar', '-C', server.rootdir, '-xzf', '-']
+
+    # Check for execs and add them to ssh_args
+    execs = []
+    for stack_item in stack_items:
+        if conf.is_exec(os.path.basename(stack_item.pure_path)):
+            path = server.remote_path(stack_item.pure_path)
+            execs += [';', path, ';', 'rm ' + path]
+    if execs:
+        execs[0] = '&&'
+        ssh_args += execs
+
+    if execs:
+        print 'Uploading as ' + server.user_host + ' and executing ' + ' '.join(execs)
+    else:
+        print 'Uploading as ' + server.user_host
+
+    ssh_process = subprocess.Popen(ssh_args, stdin=subprocess.PIPE)
+
+    with tarfile.open(mode="w|gz",
+                      fileobj=ssh_process.stdin,
+                      encoding='utf-8') as tar:
+        assert isinstance(tar, tarfile.TarFile)
+        for stack_item in stack_items:
+            abspath = conf.local_path(stack_item.sub_path)
+            tarinfo = tar.gettarinfo(abspath, arcname=stack_item.pure_path)
+            assert isinstance(tarinfo, tarfile.TarInfo)
+            if server.owner:
+                if isinstance(server.owner, int):
+                    tarinfo.uid = server.owner
+                else:
+                    tarinfo.uname = server.owner
+            if server.group:
+                if isinstance(server.group, int):
+                    tarinfo.gid = server.group
+                else:
+                    tarinfo.gname = server.group
+            tar.addfile(tarinfo, io.FileIO(abspath))
+        tar.close()
+    ssh_process.stdin.close()
+    ret_code = ssh_process.wait()
+    if ret_code != 0:
+        raw_input()
+
+    print '--- finished ' + server.user_host + ' ---'
+
+
+def upload_ftp(conf, server, sub_paths):
+    """Поехали аплоадить файлы на сервак по FTP
+
+    @param conf         Конфигурация аплоада
+    @param server       Сервер аплоада
+    @param sub_paths    Список файлов для аплоада относительно пути conf.path
+    """
+    assert isinstance(conf, Conf)
+    assert isinstance(server, Server)
+    from ftplib import FTP
+    print('Connecting')
+    ftp = FTP(server.host, server.user, server.srv['passwd'])
+    ftp.cwd(server.rootdir)
+
+    print 'Uploading'
+    for sub_path in sub_paths:
+        sub_path_tmp = sub_path + '~'
+        print sub_path,
+        ftp.storbinary('STOR ' + sub_path_tmp, open(conf.local_path(sub_path), 'rb'))
+        ftp.rename(sub_path_tmp, sub_path)
+        print '... OK'
+
+    ftp.quit()
+    print '--- finished ---'
+
+
+def watch_command(stack, confs):
+    """Наблюдаем за изменениями файлов и заливаем их при изменении.
+    Выход из этого режима - ctrl+c
+
+    TODO: настройки owner, group не поддерживаются
+
+    @param stack    Структура стека
+    @param confs    Dict конфигураций: {path: Conf}
+    """
+    from os.path import getmtime
+
+    def upload(conf, item):
+        assert isinstance(item, StackItem)
+        for server in conf.servers:
+            assert isinstance(server, Server)
+            if server in item.server_filter:
+                cmd_ssh = ('scp ' + conf.local_path(item.sub_path) + ' ' +
+                           server.user_host + ':' + server.remote_path(item.pure_path))
+                os.system(cmd_ssh)
+
+    for conf in confs.itervalues():
+        assert isinstance(conf, Conf)
+        if conf.protocol != 'ssh':
+            raise Exception("server protocol must be ssh")
+
+    files = []
+    for conf_path, stack_items in stack.data.iteritems():
+        conf = confs[conf_path]
+        assert isinstance(conf, Conf)
+        for item in stack_items:
+            assert isinstance(item, StackItem)
+            files.append((conf, item, getmtime(conf.local_path(item.sub_path))))
+            upload(conf, item)
+
+    print 'Watching for changes'
+
+    while True:
+        uploaded = False
+        for idx, (conf, item, mtime) in enumerate(files):
+            assert isinstance(item, StackItem)
+            local_path = conf.local_path(item.sub_path)
+            new_mtime = getmtime(local_path)
+            if new_mtime != mtime:
+                print local_path
+                # Make commands
+                upload(conf, item)
+
+                files[idx] = (conf, item, new_mtime)
+                uploaded = True
+
+        if not uploaded:
+            time.sleep(1)
