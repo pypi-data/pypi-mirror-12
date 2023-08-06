7844c9446eb61a32ab8e869f8bb62c823bca192f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:00 +0000

    Add package zipline/0.8.3/zipline-0.8.3.tar.gz

---
+++ packages/zipline/zipline-0.8.3.tar.gz/zipline-0.8.3/setup.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+#
+# Copyright 2014 Quantopian, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import print_function
+
+import re
+import sys
+from operator import lt, gt, eq, le, ge
+from os.path import (
+    abspath,
+    dirname,
+    join,
+)
+from distutils.version import StrictVersion
+from setuptools import (
+    Extension,
+    find_packages,
+    setup,
+)
+
+
+class LazyCythonizingList(list):
+    cythonized = False
+
+    def lazy_cythonize(self):
+        if self.cythonized:
+            return
+        self.cythonized = True
+
+        from Cython.Build import cythonize
+        from numpy import get_include
+
+        self[:] = cythonize(
+            [
+                Extension(*ext_args, include_dirs=[get_include()])
+                for ext_args in self
+            ]
+        )
+
+    def __iter__(self):
+        self.lazy_cythonize()
+        return super(LazyCythonizingList, self).__iter__()
+
+    def __getitem__(self, num):
+        self.lazy_cythonize()
+        return super(LazyCythonizingList, self).__getitem__(num)
+
+
+ext_modules = LazyCythonizingList([
+    ('zipline.assets._assets', ['zipline/assets/_assets.pyx']),
+    ('zipline.lib.adjusted_array', ['zipline/lib/adjusted_array.pyx']),
+    ('zipline.lib.adjustment', ['zipline/lib/adjustment.pyx']),
+    ('zipline.lib.rank', ['zipline/lib/rank.pyx']),
+    (
+        'zipline.data._equities',
+        ['zipline/data/_equities.pyx'],
+    ),
+    (
+        'zipline.data._adjustments',
+        ['zipline/data/_adjustments.pyx'],
+    ),
+])
+
+
+STR_TO_CMP = {
+    '<': lt,
+    '<=': le,
+    '=': eq,
+    '==': eq,
+    '>': gt,
+    '>=': ge,
+}
+
+
+def _filter_requirements(lines_iter):
+    for line in lines_iter:
+        line = line.strip()
+        if not line or line.startswith('#'):
+            continue
+
+        # pip install -r understands line with ;python_version<'3.0', but
+        # whatever happens inside extras_requires doesn't.  Parse the line
+        # manually and conditionally add it if needed.
+        if ';' not in line:
+            yield line
+            continue
+
+        requirement, version_spec = line.split(';')
+        try:
+            groups = re.match(
+                "(python_version)([<>=]{1,2})(')([0-9\.]+)(')(.*)",
+                version_spec,
+            ).groups()
+            comp = STR_TO_CMP[groups[1]]
+            version_spec = StrictVersion(groups[3])
+        except Exception as e:
+            # My kingdom for a 'raise from'!
+            raise AssertionError(
+                "Couldn't parse requirement line; '%s'\n"
+                "Error was:\n"
+                "%r" % (line, e)
+            )
+
+        sys_version = '.'.join(list(map(str, sys.version_info[:3])))
+        if comp(sys_version, version_spec):
+            yield requirement
+
+
+def read_requirements(path):
+    """
+    Read a requirements.txt file, expressed as a path relative to Zipline root.
+    """
+    real_path = join(dirname(abspath(__file__)), path)
+    with open(real_path) as f:
+        return list(_filter_requirements(f.readlines()))
+
+
+def install_requires():
+    return read_requirements('etc/requirements.txt')
+
+
+def extras_requires():
+    dev_reqs = read_requirements('etc/requirements_dev.txt')
+    talib_reqs = ['TA-Lib==0.4.9']
+    return {
+        'dev': dev_reqs,
+        'talib': talib_reqs,
+        'all': dev_reqs + talib_reqs,
+    }
+
+
+def module_requirements(requirements_path, module_names):
+    module_names = set(module_names)
+    found = set()
+    module_lines = []
+    parser = re.compile("([^=<>]+)([<=>]{1,2})(.*)")
+    for line in read_requirements(requirements_path):
+        match = parser.match(line)
+        if match is None:
+            raise AssertionError("Could not parse requirement: '%s'" % line)
+
+        groups = match.groups()
+        name = groups[0]
+        if name in module_names:
+            found.add(name)
+            module_lines.append(line)
+
+    if found != module_names:
+        raise AssertionError(
+            "No requirements found for %s." % module_names - found
+        )
+    return module_lines
+
+
+def pre_setup():
+    if not set(sys.argv) & {'install', 'develop', 'egg_info', 'bdist_wheel'}:
+        return
+
+    try:
+        import pip
+        if StrictVersion(pip.__version__) < StrictVersion('7.1.0'):
+            raise AssertionError(
+                "Zipline installation requires pip>=7.1.0, but your pip "
+                "version is {version}. \n"
+                "You can upgrade your pip with "
+                "'pip install --upgrade pip'.".format(
+                    version=pip.__version__,
+                )
+            )
+    except ImportError:
+        raise AssertionError("Zipline installation requires pip")
+
+    required = ('Cython', 'numpy')
+    for line in module_requirements('etc/requirements.txt', required):
+        pip.main(['install', line])
+
+
+pre_setup()
+
+
+setup(
+    name='zipline',
+    version='0.8.3',
+    description='A backtester for financial algorithms.',
+    author='Quantopian Inc.',
+    author_email='opensource@quantopian.com',
+    packages=find_packages('.', include=['zipline', 'zipline.*']),
+    ext_modules=ext_modules,
+    scripts=['scripts/run_algo.py'],
+    include_package_data=True,
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: System :: Distributed Computing',
+    ],
+    install_requires=install_requires(),
+    extras_require=extras_requires(),
+    url="http://zipline.io",
+)
---
+++ packages/zipline/zipline-0.8.3.tar.gz/zipline-0.8.3/zipline/utils/test_utils.py
@@ -0,0 +1,404 @@
+from contextlib import contextmanager
+from itertools import (
+    product,
+)
+import operator
+import os
+import shutil
+from string import ascii_uppercase
+import tempfile
+
+from logbook import FileHandler
+from mock import patch
+from numpy.testing import assert_allclose, assert_array_equal
+import pandas as pd
+from six import (
+    itervalues,
+)
+from six.moves import filter
+from sqlalchemy import create_engine
+
+from zipline.assets import AssetFinder
+from zipline.assets.asset_writer import AssetDBWriterFromDataFrame
+from zipline.finance.blotter import ORDER_STATUS
+from zipline.utils import security_list
+
+
+EPOCH = pd.Timestamp(0, tz='UTC')
+
+
+def seconds_to_timestamp(seconds):
+    return pd.Timestamp(seconds, unit='s', tz='UTC')
+
+
+def to_utc(time_str):
+    """Convert a string in US/Eastern time to UTC"""
+    return pd.Timestamp(time_str, tz='US/Eastern').tz_convert('UTC')
+
+
+def str_to_seconds(s):
+    """
+    Convert a pandas-intelligible string to (integer) seconds since UTC.
+
+    >>> from pandas import Timestamp
+    >>> (Timestamp('2014-01-01') - Timestamp(0)).total_seconds()
+    1388534400.0
+    >>> str_to_seconds('2014-01-01')
+    1388534400
+    """
+    return int((pd.Timestamp(s, tz='UTC') - EPOCH).total_seconds())
+
+
+def setup_logger(test, path='test.log'):
+    test.log_handler = FileHandler(path)
+    test.log_handler.push_application()
+
+
+def teardown_logger(test):
+    test.log_handler.pop_application()
+    test.log_handler.close()
+
+
+def drain_zipline(test, zipline):
+    output = []
+    transaction_count = 0
+    msg_counter = 0
+    # start the simulation
+    for update in zipline:
+        msg_counter += 1
+        output.append(update)
+        if 'daily_perf' in update:
+            transaction_count += \
+                len(update['daily_perf']['transactions'])
+
+    return output, transaction_count
+
+
+def assert_single_position(test, zipline):
+
+    output, transaction_count = drain_zipline(test, zipline)
+
+    if 'expected_transactions' in test.zipline_test_config:
+        test.assertEqual(
+            test.zipline_test_config['expected_transactions'],
+            transaction_count
+        )
+    else:
+        test.assertEqual(
+            test.zipline_test_config['order_count'],
+            transaction_count
+        )
+
+    # the final message is the risk report, the second to
+    # last is the final day's results. Positions is a list of
+    # dicts.
+    closing_positions = output[-2]['daily_perf']['positions']
+
+    # confirm that all orders were filled.
+    # iterate over the output updates, overwriting
+    # orders when they are updated. Then check the status on all.
+    orders_by_id = {}
+    for update in output:
+        if 'daily_perf' in update:
+            if 'orders' in update['daily_perf']:
+                for order in update['daily_perf']['orders']:
+                    orders_by_id[order['id']] = order
+
+    for order in itervalues(orders_by_id):
+        test.assertEqual(
+            order['status'],
+            ORDER_STATUS.FILLED,
+            "")
+
+    test.assertEqual(
+        len(closing_positions),
+        1,
+        "Portfolio should have one position."
+    )
+
+    sid = test.zipline_test_config['sid']
+    test.assertEqual(
+        closing_positions[0]['sid'],
+        sid,
+        "Portfolio should have one position in " + str(sid)
+    )
+
+    return output, transaction_count
+
+
+class ExceptionSource(object):
+
+    def __init__(self):
+        pass
+
+    def get_hash(self):
+        return "ExceptionSource"
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        5 / 0
+
+    def __next__(self):
+        5 / 0
+
+
+@contextmanager
+def security_list_copy():
+    old_dir = security_list.SECURITY_LISTS_DIR
+    new_dir = tempfile.mkdtemp()
+    try:
+        for subdir in os.listdir(old_dir):
+            shutil.copytree(os.path.join(old_dir, subdir),
+                            os.path.join(new_dir, subdir))
+            with patch.object(security_list, 'SECURITY_LISTS_DIR', new_dir), \
+                    patch.object(security_list, 'using_copy', True,
+                                 create=True):
+                yield
+    finally:
+        shutil.rmtree(new_dir, True)
+
+
+def add_security_data(adds, deletes):
+    if not hasattr(security_list, 'using_copy'):
+        raise Exception('add_security_data must be used within '
+                        'security_list_copy context')
+    directory = os.path.join(
+        security_list.SECURITY_LISTS_DIR,
+        "leveraged_etf_list/20150127/20150125"
+    )
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    del_path = os.path.join(directory, "delete")
+    with open(del_path, 'w') as f:
+        for sym in deletes:
+            f.write(sym)
+            f.write('\n')
+    add_path = os.path.join(directory, "add")
+    with open(add_path, 'w') as f:
+        for sym in adds:
+            f.write(sym)
+            f.write('\n')
+
+
+def all_pairs_matching_predicate(values, pred):
+    """
+    Return an iterator of all pairs, (v0, v1) from values such that
+
+    `pred(v0, v1) == True`
+
+    Parameters
+    ----------
+    values : iterable
+    pred : function
+
+    Returns
+    -------
+    pairs_iterator : generator
+       Generator yielding pairs matching `pred`.
+
+    Examples
+    --------
+    >>> from zipline.utils.test_utils import all_pairs_matching_predicate
+    >>> from operator import eq, lt
+    >>> list(all_pairs_matching_predicate(range(5), eq))
+    [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)]
+    >>> list(all_pairs_matching_predicate("abcd", lt))
+    [('a', 'b'), ('a', 'c'), ('a', 'd'), ('b', 'c'), ('b', 'd'), ('c', 'd')]
+    """
+    return filter(lambda pair: pred(*pair), product(values, repeat=2))
+
+
+def product_upper_triangle(values, include_diagonal=False):
+    """
+    Return an iterator over pairs, (v0, v1), drawn from values.
+
+    If `include_diagonal` is True, returns all pairs such that v0 <= v1.
+    If `include_diagonal` is False, returns all pairs such that v0 < v1.
+    """
+    return all_pairs_matching_predicate(
+        values,
+        operator.le if include_diagonal else operator.lt,
+    )
+
+
+def all_subindices(index):
+    """
+    Return all valid sub-indices of a pandas Index.
+    """
+    return (
+        index[start:stop]
+        for start, stop in product_upper_triangle(range(len(index) + 1))
+    )
+
+
+def make_rotating_asset_info(num_assets,
+                             first_start,
+                             frequency,
+                             periods_between_starts,
+                             asset_lifetime):
+    """
+    Create a DataFrame representing lifetimes of assets that are constantly
+    rotating in and out of existence.
+
+    Parameters
+    ----------
+    num_assets : int
+        How many assets to create.
+    first_start : pd.Timestamp
+        The start date for the first asset.
+    frequency : str or pd.tseries.offsets.Offset (e.g. trading_day)
+        Frequency used to interpret next two arguments.
+    periods_between_starts : int
+        Create a new asset every `frequency` * `periods_between_new`
+    asset_lifetime : int
+        Each asset exists for `frequency` * `asset_lifetime` days.
+
+    Returns
+    -------
+    info : pd.DataFrame
+        DataFrame representing newly-created assets.
+    """
+    return pd.DataFrame(
+        {
+            'sid': range(num_assets),
+            'symbol': [chr(ord('A') + i) for i in range(num_assets)],
+            # Start a new asset every `periods_between_starts` days.
+            'start_date': pd.date_range(
+                first_start,
+                freq=(periods_between_starts * frequency),
+                periods=num_assets,
+            ),
+            # Each asset lasts for `asset_lifetime` days.
+            'end_date': pd.date_range(
+                first_start + (asset_lifetime * frequency),
+                freq=(periods_between_starts * frequency),
+                periods=num_assets,
+            ),
+            'exchange': 'TEST',
+        }
+    )
+
+
+def make_simple_asset_info(assets, start_date, end_date, symbols=None):
+    """
+    Create a DataFrame representing assets that exist for the full duration
+    between `start_date` and `end_date`.
+
+    Parameters
+    ----------
+    assets : array-like
+    start_date : pd.Timestamp
+    end_date : pd.Timestamp
+    symbols : list, optional
+        Symbols to use for the assets.
+        If not provided, symbols are generated from the sequence 'A', 'B', ...
+
+    Returns
+    -------
+    info : pd.DataFrame
+        DataFrame representing newly-created assets.
+    """
+    num_assets = len(assets)
+    if symbols is None:
+        symbols = list(ascii_uppercase[:num_assets])
+    return pd.DataFrame(
+        {
+            'sid': assets,
+            'symbol': symbols,
+            'start_date': [start_date] * num_assets,
+            'end_date': [end_date] * num_assets,
+            'exchange': 'TEST',
+        }
+    )
+
+
+def check_allclose(actual,
+                   desired,
+                   rtol=1e-07,
+                   atol=0,
+                   err_msg='',
+                   verbose=True):
+    """
+    Wrapper around np.testing.assert_allclose that also verifies that inputs
+    are ndarrays.
+
+    See Also
+    --------
+    np.assert_allclose
+    """
+    if type(actual) != type(desired):
+        raise AssertionError("%s != %s" % (type(actual), type(desired)))
+    return assert_allclose(actual, desired, err_msg=err_msg, verbose=True)
+
+
+def check_arrays(x, y, err_msg='', verbose=True):
+    """
+    Wrapper around np.testing.assert_array_equal that also verifies that inputs
+    are ndarrays.
+
+    See Also
+    --------
+    np.assert_array_equal
+    """
+    if type(x) != type(y):
+        raise AssertionError("%s != %s" % (type(x), type(y)))
+    return assert_array_equal(x, y, err_msg=err_msg, verbose=True)
+
+
+class UnexpectedAttributeAccess(Exception):
+    pass
+
+
+class ExplodingObject(object):
+    """
+    Object that will raise an exception on any attribute access.
+
+    Useful for verifying that an object is never touched during a
+    function/method call.
+    """
+    def __getattribute__(self, name):
+        raise UnexpectedAttributeAccess(name)
+
+
+class tmp_assets_db(object):
+    """Create a temporary assets sqlite database.
+    This is meant to be used as a context manager.
+
+    Paramaters
+    ----------
+    data : pd.DataFrame, optional
+        The data to feed to the writer. By default this maps:
+        ('A', 'B', 'C') -> map(ord, 'ABC')
+    """
+    def __init__(self, data=None):
+        self._eng = None
+        self._data = AssetDBWriterFromDataFrame(
+            data if data is not None else make_simple_asset_info(
+                list(map(ord, 'ABC')),
+                pd.Timestamp(0),
+                pd.Timestamp('2015'),
+            )
+        )
+
+    def __enter__(self):
+        self._eng = eng = create_engine('sqlite://')
+        self._data.write_all(eng)
+        return eng
+
+    def __exit__(self, *excinfo):
+        assert self._eng is not None, '_eng was not set in __enter__'
+        self._eng.dispose()
+
+
+class tmp_asset_finder(tmp_assets_db):
+    """Create a temporary asset finder using an in memory sqlite db.
+
+    Paramaters
+    ----------
+    data : dict, optional
+        The data to feed to the writer
+    """
+    def __enter__(self):
+        return AssetFinder(super(tmp_asset_finder, self).__enter__())
