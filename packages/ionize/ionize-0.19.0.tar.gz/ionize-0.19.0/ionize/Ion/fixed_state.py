1572f00cc15cdfded6dd225ecb258353cf6a618f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:52 +0000

    Add package ionize/0.19.0/ionize-0.19.0.tar.gz

---
+++ packages/ionize/ionize-0.19.0.tar.gz/ionize-0.19.0/ionize/Ion/fixed_state.py
@@ -0,0 +1,12 @@
+import operator
+
+
+# TODO: Is this a good way of doing this?
+def fixed_state(cls):
+    for state_property in cls._state:
+        setattr(cls,
+                state_property,
+                property(operator.attrgetter("_{}".format(state_property))
+                         )
+                )
+    return cls
