9e27ee94cc95ffd65182457ae37d9c43e2838e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:53 +0000

    Add package sphinx_molview_theme/0.0.1/sphinx_molview_theme-0.0.1.tar.gz

---
+++ packages/sphinx_molview_theme/sphinx_molview_theme-0.0.1.tar.gz/sphinx_molview_theme-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+setup(
+    name = 'sphinx_molview_theme',
+    packages = ['sphinx_molview_theme'],
+    version = '0.0.1',
+    description = 'Sphinx theme for MolView',
+    author = 'Herman Bergwerf',
+    author_email = 'hermanbergwerf@gmail.com',
+    license = 'MIT',
+    url = 'https://github.com/molview/sphinx-molview-theme',
+    keywords = ['sphinx', 'molview', 'theme'],
+    classifiers = [],
+    install_requires = [
+        'setuptools',
+        'sphinx_bootstrap_theme'
+    ],
+)
