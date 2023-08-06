c1200fd4fd4f2a9ae59b242fde3656863c520768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:39 +0000

    Add package shub/1.4.0/shub-1.4.0-py2-none-any.whl

---
+++ packages/shub/shub-1.4.0-py2-none-any.whl/shub-1.4.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,31 @@
+Scrapinghub command line client
+===============================
+
+``shub`` is a tool used to manage Scrapinghub services from the command line.
+
+Requirements
+------------
+
+* Python 2.7
+
+Installation
+------------
+
+Install with::
+
+    pip install shub
+
+Usage
+-----
+
+Get help with::
+
+    shub
+
+Documentation
+-------------
+
+Documentation is available here:
+http://doc.scrapinghub.com/shub.html
+
+
---
+++ packages/shub/shub-1.4.0-py2-none-any.whl/shub/deploy_reqs.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import click
+import os
+import tempfile
+import shutil
+
+from shub.utils import run, decompress_egg_files
+from shub.click_utils import log
+from shub import utils
+from shub.auth import find_api_key
+
+
+@click.command(help="Build and deploy eggs from requirements.txt")
+@click.argument("project_id", required=True)
+@click.argument("requirements_file", required=True)
+def cli(project_id, requirements_file):
+    """Just a wrapper around the deploy_egg module"""
+    main(project_id, requirements_file)
+
+
+def main(project_id, requirements_file):
+    apikey = find_api_key()
+    requirements_full_path = os.path.abspath(requirements_file)
+    eggs_tmp_dir = _mk_and_cd_eggs_tmpdir()
+    _download_egg_files(eggs_tmp_dir, requirements_full_path)
+    decompress_egg_files()
+    utils.build_and_deploy_eggs(project_id, apikey)
+
+
+def _mk_and_cd_eggs_tmpdir():
+    tmpdir = tempfile.mkdtemp(prefix="eggs")
+    os.chdir(tmpdir)
+    os.mkdir('eggs')
+    os.chdir('eggs')
+    return os.path.join(tmpdir, 'eggs')
+
+
+def _download_egg_files(eggs_dir, requirements_file):
+    editable_src_dir = tempfile.mkdtemp(prefix='pipsrc')
+
+    log('Downloading eggs...')
+    try:
+        pip_cmd = ("pip install -d {eggs_dir} -r {requirements_file}"
+                   " --src {editable_src_dir} --no-deps --no-use-wheel")
+        log(run(pip_cmd.format(eggs_dir=eggs_dir,
+                                 editable_src_dir=editable_src_dir,
+                                 requirements_file=requirements_file)))
+    finally:
+        shutil.rmtree(editable_src_dir, ignore_errors=True)
