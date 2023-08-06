9ba0f821f5a4431e4abbf8cbead0ed4ebbd4f739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:53 +0000

    Add package lya/2.0.0/lya-2.0.0.zip

---
+++ packages/lya/lya-2.0.0.zip/lya-2.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+ 
+setup(
+        name        ='lya',
+	version     ='2.0.0',
+	py_modules  =['lya'],
+	auther      ='tonysti',
+	auther_email='z.j19910903@163.com',
+	url         ='http://www.headfirstlabs.com',
+	description ='A simple printer of nested lists',
+      )
