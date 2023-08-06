24d6441d750cdc606c3d7987d95c433b865a9a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:23 +0000

    Add package pluginmanager/0.1.7/pluginmanager-0.1.7-py2.py3-none-any.whl

---
+++ packages/pluginmanager/pluginmanager-0.1.7-py2.py3-none-any.whl/pluginmanager/plugin_filters/by_name.py
@@ -0,0 +1,13 @@
+from pluginmanager import util
+
+
+class NameFilter(object):
+    def __init__(self, names=None):
+        self.names = util.return_list(names)
+
+    def __call__(self, plugins):
+        approved_plugins = []
+        for plugin in plugins:
+            if hasattr(plugin, 'name') and plugin.name in self.names:
+                approved_plugins.append(plugin)
+        return approved_plugins
