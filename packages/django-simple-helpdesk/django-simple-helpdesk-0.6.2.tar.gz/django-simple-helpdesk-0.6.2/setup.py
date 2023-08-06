4da566babed481409e4cd7c9b0c45c9669633fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:46 +0000

    Add package django-simple-helpdesk/0.6.2/django-simple-helpdesk-0.6.2.tar.gz

---
+++ packages/django-simple-helpdesk/django-simple-helpdesk-0.6.2.tar.gz/django-simple-helpdesk-0.6.2/setup.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+version = '0.6.2'
+
+setup(
+    name='django-simple-helpdesk',
+    version=version,
+    author='Kirill Bubochkin',
+    author_email='ookami.kb@gmail.com',
+    description='Simple HelpDesk for Django',
+    packages=find_packages(),
+    install_requires=['django-bootstrap3', 'django-widget-tweaks'],
+    include_package_data=True,
+)
