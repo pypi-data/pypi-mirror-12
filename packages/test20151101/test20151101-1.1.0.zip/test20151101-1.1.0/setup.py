d6bf6d05e69f8a6a42ebc3976f14bbcee5a15550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:29 +0000

    Add package test20151101/1.1.0/test20151101-1.1.0.zip

---
+++ packages/test20151101/test20151101-1.1.0.zip/test20151101-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'test20151101',
+    version = '1.1.0',
+    py_modules = ['test'],
+    author = 'me',
+    author_email = 'yiqiedoushi123@163.com',
+    url = 'NULL',
+    description = 'a simple modlues for test'
+)
---
+++ packages/test20151101/test20151101-1.1.0.zip/test20151101-1.1.0/test.py
@@ -0,0 +1,6 @@
+import numpy
+
+cast=['hello','world!']
+print cast,
+def printf():
+    print 'My name is ,i could not tell you'
