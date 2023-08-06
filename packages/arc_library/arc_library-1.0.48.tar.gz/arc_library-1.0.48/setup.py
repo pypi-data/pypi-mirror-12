a54a4dcde0c75ac55b1c151a742c4332b4d7cc92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:26 +0000

    Add package arc_library/1.0.48/arc_library-1.0.48.tar.gz

---
+++ packages/arc_library/arc_library-1.0.48.tar.gz/arc_library-1.0.48/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+ 
+setup(
+    name = 'arc_library',
+    version = '1.0.48',
+    # py_modules = ['arc_django_models', 'arc_functions', 'arc_jsonobject', 'arc_storage', 'arc_tag'],
+	#py_modules = ['arc_library'],
+    packages = ['arclib', 'arclib/django', 'arclib/gae', 'arclib/http'],
+    author = 'Arcanelux',
+    author_email = 'Arcanelux@gmail.com',
+    url = 'iiii.so',
+    description = 'Bug fix of apply local timezone to datetime.date instance',
+)
+
+
