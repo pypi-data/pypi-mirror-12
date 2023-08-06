3ccd6993198b187ff52b4dcd045ea2cc4dfb08fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package singlEpoClass/1.0/singlEpoClass-1.0.tar.gz

---
+++ packages/singlEpoClass/singlEpoClass-1.0.tar.gz/singlEpoClass-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='singlEpoClass',
+      version='1.0',
+      description='Single Epoch Transient Classifier',
+      url='http://github.com/sofiatti/SingleEpochPhotometricClassifier',
+      author='Caroline Sofiatti',
+      author_email='sofiatti@berkeley.edu',
+      
+      zip_safe=False)
