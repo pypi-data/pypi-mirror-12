aaa6a5d2a7e4c2fe65766882987b2d10d009388c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:11 +0000

    Add package spx/0.2.0/spx-0.2.0.tar.gz

---
+++ packages/spx/spx-0.2.0.tar.gz/spx-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='spx',
+    version='0.2.0',
+    install_requires=[
+        'requests',
+        'apscheduler',
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'spx-tool = spx.tool:SPXTool.run',
+        ]
+    },
+    url='https://github.com/tback/spx',
+    license='MIT',
+    author='Till Backhaus',
+    author_email='till@backha.us',
+    description='Unofficial Python Library for Edimax Smartplugs SP-2101W and SP-1101W'
+)
---
+++ packages/spx/spx-0.2.0.tar.gz/spx-0.2.0/spx/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from .smartplug import (Smartplug, \
+    get_state, \
+    get_usage, \
+    set_state, switch, on, off,
+)
