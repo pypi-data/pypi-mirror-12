1b3d8a19c5bb9066c260145bd102c60e4355da4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:04 +0000

    Add package SDH-Curator/0.1.1a5/SDH-Curator-0.1.1a5.tar.gz

---
+++ packages/SDH-Curator/SDH-Curator-0.1.1a5.tar.gz/SDH-Curator-0.1.1a5/sdh/curator/server/config.py
@@ -0,0 +1,82 @@
+"""
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  This file is part of the Smart Developer Hub Project:
+    http://www.smartdeveloperhub.org
+
+  Center for Open Middleware
+        http://www.centeropenmiddleware.com/
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  Copyright (C) 2015 Center for Open Middleware.
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+"""
+
+__author__ = 'Fernando Serena'
+
+import logging
+
+
+class Config(object):
+    STORE_PATHS = {
+        'graph': 'graph_store'
+    }
+    PORT = 5007
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    LOG = logging.DEBUG
+    REDIS = {
+        'host': 'localhost',
+        'db': 4,
+        'port': 6379
+    }
+    STORE = 'persist'
+    AGORA = 'http://138.4.249.224:9009'
+    RABBIT = {
+        'host': 'localhost',
+        'port': 5672
+    }
+
+
+class TestingConfig(Config):
+    DEBUG = False
+    LOG = logging.DEBUG
+    REDIS = {
+        'host': 'localhost',
+        'db': 5,
+        'port': 6379
+    }
+    TESTING = True
+    STORE = 'memory'
+    RABBIT = {
+        'host': 'localhost',
+        'port': 5672
+    }
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    LOG = logging.DEBUG
+    REDIS = {
+        'host': 'redis',
+        'db': 4,
+        'port': 6379
+    }
+    STORE = 'persist'
+    AGORA = 'http://138.4.249.224:9009'
+    RABBIT = {
+        'host': 'rabbit',
+        'port': 5672
+    }
