58a7cb952dac7aa00cbbb811ce2f9170c0b5b209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:12 +0000

    Add package publib/0.1/publib-0.1.zip

---
+++ packages/publib/publib-0.1.zip/publib-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='publib',
+      version='0.1',
+      description='Produce publication-level quality images on top of Matplotlib',
+      url='github.com/rainwear/publib',
+      author='Erwan Pannier',
+      author_email='erwan.pannier@gmail.com',
+      license='BSL',
+      packages=['publib'],
+      zip_safe=False)
