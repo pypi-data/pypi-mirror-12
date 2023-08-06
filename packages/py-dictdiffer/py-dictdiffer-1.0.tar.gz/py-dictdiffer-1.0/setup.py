1d7cf25fd5d7df9f907617e8c065b46dd1fdffc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:01 +0000

    Add package py-dictdiffer/1.0/py-dictdiffer-1.0.tar.gz

---
+++ packages/py-dictdiffer/py-dictdiffer-1.0.tar.gz/py-dictdiffer-1.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Author: Hugh Brown <github.com/hughdbrown>
+# Maintainer: Disruptive Labs <pypi+dictdiffer@comanage.com>
+
+from setuptools import setup
+
+setup(name='py-dictdiffer',
+      version='1.0',
+      description='Python class to calculate the difference between two python dictionaries',
+      author='Disruptive Labs',
+      author_email='pypi+dictdiffer@comanage.com',
+      url='https://github.com/DisruptiveLabs/dictdiffer',
+      py_modules=['dictdiffer'],
+      )
