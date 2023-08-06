46cdcd621597b44f464f7d3c819b423dcc0c29de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:11 +0000

    Add package surfdist/0.13.10/surfdist-0.13.10.tar.gz

---
+++ packages/surfdist/surfdist-0.13.10.tar.gz/surfdist-0.13.10/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'surfdist',
+  packages = ['surfdist'],
+  install_requires = ['numpy',
+                      'mayavi',
+                      'gdist',
+                      'nibabel',
+                      'scipy'],
+  version = '0.13.10',
+  description = 'For calculating exact geodesic distances on cortical surface meshes',
+  author = 'Daniel Margulies',
+  author_email = 'margulies@cbs.mpg.de',
+  url = 'https://github.com/margulies/surfdist',
+  download_url = 'https://github.com/margulies/surfdist/tarball/0.13.10',
+  keywords = ['geodesic', 'distance', 'brain', 'cortex'],
+  classifiers = [],
+)
