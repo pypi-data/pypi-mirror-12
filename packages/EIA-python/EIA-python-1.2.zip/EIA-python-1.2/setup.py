f61f0e462feff4e62d234936acc68d5a2b9ca92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:01 +0000

    Add package EIA-python/1.2/EIA-python-1.2.zip

---
+++ packages/EIA-python/EIA-python-1.2.zip/EIA-python-1.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='EIA-python',
+    version='1.2',
+    packages=['EIA'],
+    url='https://github.com/mra1385/EIA-python',
+    license='BSD',
+    author='Mike Azar',
+    author_email='mikeazar@gmail.com',
+    description='Python Wrapper for EIA API'
+)
