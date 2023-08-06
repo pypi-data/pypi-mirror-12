aa21f0d12a28e6933f8f5254ab575ab9a9ce62da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:17 +0000

    Add package nester_zj/1.1.0/nester_zj-1.1.0.tar.gz

---
+++ packages/nester_zj/nester_zj-1.1.0.tar.gz/nester_zj-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester_zj',
+        version = '1.1.0',
+        py_modules = ['nester'],
+        author = 'zhang',
+        author_email = '623239185@qq.com',
+        url = 'www.baidu.com',
+        description = 'A foolish printter',
+    )
