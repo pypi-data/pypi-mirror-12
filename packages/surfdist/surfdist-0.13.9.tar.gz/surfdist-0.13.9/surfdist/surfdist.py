ece3d73a33bf270028501dbc177c63f9d26b6310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:10 +0000

    Add package surfdist/0.13.9/surfdist-0.13.9.tar.gz

---
+++ packages/surfdist/surfdist-0.13.9.tar.gz/surfdist-0.13.9/surfdist/surfdist.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+def dist_calc(surf, cortex, src):
+    """
+    Calculate exact geodesic distance along cortical surface from set of source nodes.
+    """
+    import gdist
+    from utils import surf_keep_cortex, translate_src, recort
+
+    vertices, triangles = surf_keep_cortex(surf, cortex)
+    src_new = translate_src(src, cortex)
+    data = gdist.compute_gdist(vertices, triangles, source_indices=src_new)
+    dist = recort(data, surf, cortex)
+    del data
+
+    return dist
+
+
+def zone_calc(surf, cortex, src):
+    """
+    Calculate closest nodes to each source node using exact geodesic distance along the cortical surface.
+    """
+    import gdist
+    from utils import surf_keep_cortex, recort
+
+    vertices, triangles = surf_keep_cortex(surf, cortex)
+
+    src_new = translate_src(src, cortex)
+
+    dist_vals = np.zeros((len(src_new), len(vertices)))
+    for x in range(len(src_new)):
+        dist_vals[x, :] = gdist.compute_gdist(vertices, triangles,
+                                              source_indices=np.array([src_new[x]], dtype=np.int32))
+
+    data = np.argsort(dist_vals, axis=0)[-1, :] + 1
+
+    zone = recort(data, surf, cortex)
+
+    del data
+
+    return zone
