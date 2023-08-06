6ec470a8861c5a62e29f1960ad95fa1fe5a2bcb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:09 +0000

    Add package stackctl/0.0.1/stackctl-0.0.1.tar.gz

---
+++ packages/stackctl/stackctl-0.0.1.tar.gz/stackctl-0.0.1/stackctl/errors.py
@@ -0,0 +1,14 @@
+class CommandExecutionError(Exception):
+    pass
+
+
+class InvalidCommandArgs(Exception):
+    pass
+
+
+class WrapperFailure(Exception):
+    pass
+
+
+class ConnectionFailure(Exception):
+    pass
