413423d58fd5bc50f6b2e64352b440f256b2633b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:30 +0000

    Add package sqlalchemy-bigquery/0.0.7/sqlalchemy_bigquery-0.0.7.tar.gz

---
+++ packages/sqlalchemy-bigquery/sqlalchemy_bigquery-0.0.7.tar.gz/sqlalchemy_bigquery-0.0.7/sqlalchemy_bigquery/base.py
@@ -0,0 +1,96 @@
+"""
+Support for Google BigQuery.
+
+Does not support actually connecting to BQ.
+Directly derived from the mssql dialect with minor modifications
+
+"""
+import sqlalchemy.dialects.mssql.base as mssql_base
+
+from sqlalchemy.dialects.mssql.base import MSDialect
+from sqlalchemy.sql import compiler
+from sqlalchemy.sql import sqltypes
+
+
+class BQString(sqltypes.String):
+    def __init__(
+        self,
+        length=None,
+        collation=None,
+        convert_unicode=False,
+        unicode_error=None,
+        _warn_on_bytestring=False
+    ):
+        return super(BQString, self).__init__(
+            length=length,
+            collation=collation,
+            convert_unicode=convert_unicode,
+            unicode_error=unicode_error,
+            _warn_on_bytestring=_warn_on_bytestring
+        )
+
+    def literal_processor(self, dialect):
+        def process(value):
+            value = value.replace("'", "\\'")
+            return "'%s'" % value
+        return process
+
+
+class BQSQLCompiler(mssql_base.MSSQLCompiler):
+
+    def get_select_precolumns(self,select, **kw):
+        """BQ uses TOP differently from MS-SQL"""
+        s = ""
+        if select._distinct:
+            s += "DISTINCT "
+
+        if s:
+            return s
+        else:
+            return compiler.SQLCompiler.get_select_precolumns(
+                    self, select, **kw)
+
+    def limit_clause(self, select, **kw):
+        """Only supports simple (integer) LIMIT clause"""
+        s = ""
+        if select._simple_int_limit and not select._offset:
+            s += "\nLIMIT %d " % select._limit
+        
+        return s
+
+
+    def visit_column(self, column, add_to_result_map=None, **kwargs):
+        # TODO: figure out how to do this immutably
+        # force column rendering to not use quotes by declaring every col literal
+        column.is_literal = True
+        return super(BQSQLCompiler, self).visit_column(
+            column,
+            add_to_result_map=add_to_result_map,
+            **kwargs
+        )
+
+    def visit_match_op_binary(self, binary, operator, **kw):
+        return "%s CONTAINS %s" % (
+            self.process(binary.left, **kw),
+            self.process(binary.right, **kw))
+
+
+class BQIdentifierPreparer(compiler.IdentifierPreparer):
+    def __init__(self, dialect):
+        super(BQIdentifierPreparer, self).__init__(
+            dialect,
+            initial_quote='[',
+            final_quote=']',
+        )
+
+    def format_label(self, label, name=None):
+        """ bq can't handle quoting labels """
+        return name or label.name
+
+
+class BQDialect(MSDialect):
+    statement_compiler = BQSQLCompiler
+    preparer = BQIdentifierPreparer
+    colspecs = {
+        sqltypes.String: BQString
+    }
---
+++ packages/sqlalchemy-bigquery/sqlalchemy_bigquery-0.0.7.tar.gz/sqlalchemy_bigquery-0.0.7/test/test_suite.py
@@ -0,0 +1,112 @@
+# from sqlalchemy.testing.suite import *
+
+import sqlalchemy.sql as sql
+from sqlalchemy import func
+
+import sqlalchemy_bigquery
+import sqlalchemy_bigquery.base as bq
+
+
+def test_assert_correct_version():
+    """
+    yep. update it every time.
+
+    This is a dumb test, but I want it here to remind future committers that if
+    you make a change to the package, your change should probably include a
+    test which verifies why the change was made.
+
+    If you want your changes to be seen in PyPI, you have to rev the version.
+    If you rev the version without updating the test suite, you will break the
+    tests.
+    """
+
+    assert "0.0.6" == sqlalchemy_bigquery.__version__
+
+
+def test_mixed_case_function_label_not_quoted():
+    s = sql.select([
+        func.sum().label("Pork")
+    ]).compile(
+        dialect=bq.BQDialect()
+    )
+    assert "[Pork]" not in str(s)
+    assert "Pork" in str(s)
+
+
+def test_table_is_quoted_with_square_brackets():
+    s = sql.select([
+        func.sum().label("Pork")
+    ]).select_from(
+        sql.table("with.dot")
+    ).compile(
+        dialect=bq.BQDialect()
+    )
+    assert "[with.dot]" in str(s)
+
+
+def test_mixed_case_column_not_quoted():
+    s = sql.select([
+        sql.column("Pork")
+    ]).compile(
+        dialect=bq.BQDialect()
+    )
+    assert "[Pork]" not in str(s)
+    assert "Pork" in str(s)
+
+
+def test_string_literal_does_not_use_old_sql_quote_escaping():
+    s = sql.select([
+        func.look_at(sql.column("something") == "here's a single-quote")
+    ]).select_from(
+        sql.table("something_else")
+    ).compile(
+        compile_kwargs={"literal_binds": True},
+        dialect=bq.BQDialect()
+    )
+    assert "here''s" not in str(s)
+
+
+def test_string_literals_correctly_quoted():
+    s = sql.select([
+        func.look_at(sql.column("something") == "here's a single-quote")
+    ]).select_from(
+        sql.table("something_else")
+    ).compile(
+        compile_kwargs={"literal_binds": True},
+        dialect=bq.BQDialect()
+    )
+    assert "here\\'s" in str(s)
+
+
+def test_string_literals_not_oldstyle_quoted_in_func():
+    s = str(
+        sql.case(
+            [(sql.column("WHOA") == "Jason's", 1)],
+            else_=0
+        ).compile(
+            compile_kwargs={"literal_binds": True},
+            dialect=bq.BQDialect()
+        )
+    )
+    assert "Jason's" not in s
+
+
+def test_limit_compiles_correctly():
+    s = sql.select([sql.column('col1'),sql.column('col2')
+        ]).select_from(
+                sql.table('table_name')
+                ).limit(5
+                        ).compile(
+                                dialect=bq.BQDialect())
+    assert ('LIMIT 5' in str(s)) and ('TOP 5' not in str(s)) 
+
+def test_matches_correct_order():
+    s = sql.select([
+        sql.column("beef")
+    ]).where(
+        sql.column("beef").match("cow")
+    ).compile(
+        compile_kwargs={"literal_binds": True},
+        dialect=bq.BQDialect()
+    )
+    assert "WHERE beef CONTAINS 'cow'" in str(s)
