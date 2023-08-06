f23a9948a09911107496600fac17777bd7bfa7fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:18 +0000

    Add package thrift_http_tornado/0.0.6/thrift_http_tornado-0.0.6.tar.gz

---
+++ packages/thrift_http_tornado/thrift_http_tornado-0.0.6.tar.gz/thrift_http_tornado-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='thrift_http_tornado',
+      version='0.0.6',
+      description='Thirft transport implementation over the HTTP protocol',
+      author='Alexis Montagne',
+      author_email='alexis.montagne@upfluence.co',
+      long_description=open('README', 'r').read(),
+      url='https://github.com/upfluence/thrift-http-tornado',
+      packages=['thrift_http_tornado'],
+      install_requires=['thrift', 'tornado', 'futures', 'toro'])
