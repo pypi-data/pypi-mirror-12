344779fbd065082e48efc66c0988f99febbb2e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:56 +0000

    Add package django-resumator/1.1.6/django-resumator-1.1.6.tar.gz

---
+++ packages/django-resumator/django-resumator-1.1.6.tar.gz/django-resumator-1.1.6/resumator/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.db.utils import OperationalError
+from solo.admin import SingletonModelAdmin
+
+from models import BasicInformation
+from models import Project
+from models import Experience
+from models import Language
+from models import Publication
+from models import Education
+
+
+admin.site.register(BasicInformation, SingletonModelAdmin)
+admin.site.register(Project)
+admin.site.register(Experience)
+admin.site.register(Language)
+admin.site.register(Publication)
+admin.site.register(Education)
