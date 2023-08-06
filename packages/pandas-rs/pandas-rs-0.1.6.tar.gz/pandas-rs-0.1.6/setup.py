725173e7dd1d037b98113d1fc4037332660a9dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:12 +0000

    Add package pandas-rs/0.1.6/pandas-rs-0.1.6.tar.gz

---
+++ packages/pandas-rs/pandas-rs-0.1.6.tar.gz/pandas-rs-0.1.6/setup.py
@@ -0,0 +1,48 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import re
+import os
+
+def pandas_rs_version():
+    version_regex  = re.compile(r'__version__ = "([^\"]*)"')
+    return version_regex.match(
+        open('pandas_rs/version.py').read()
+    ).group(1)
+
+def read_file(filename):
+    filepath = os.path.join(
+        os.path.dirname(
+            os.path.dirname(__file__)
+        ),
+        filename
+    )
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    else:
+        return ''
+
+setup(
+    name = "pandas-rs",
+    packages = ["pandas_rs"],
+    install_requires = ["pandas", "psycopg2"],
+    version = pandas_rs_version(),
+    description = "pandas extension for PostgreSQL and AWS RedShift (Not Officail library)",
+    author = "Tatsuro Yasukawa",
+    author_email = "t.yasukawa01@gmail.com",
+    url = "https://github.com/SamuraiT/pandas-rs",
+    download_url = "",
+    keywords = ["pandas", "PostgreSQL","RedShift", "sql"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description = read_file('README.md')
+)
