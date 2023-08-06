de131c2207fdcd81f17adb0ce697287b1264a794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:39 +0000

    Add package mciutil/0.4.0/mciutil-0.4.0.tar.gz

---
+++ packages/mciutil/mciutil-0.4.0.tar.gz/mciutil-0.4.0/mciutil/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+mciutil
+=======
+Shared functions for working with MasterCard files
+"""
+from .mciutil import (
+    block, unblock, vbs_pack, vbs_unpack, get_message_elements,
+    flip_message_encoding, b,
+)
+
+__author__ = 'Anthony Delosa'
+__email__ = 'adelosa@gmail.com'
+__version__ = '0.4.0'
