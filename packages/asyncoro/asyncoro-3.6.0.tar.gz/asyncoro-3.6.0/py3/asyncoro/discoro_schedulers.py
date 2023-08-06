b00d2070a508f82753a8debc09b4f1cbc89c904a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:04 +0000

    Add package asyncoro/3.6.0/asyncoro-3.6.0.tar.gz

---
+++ packages/asyncoro/asyncoro-3.6.0.tar.gz/asyncoro-3.6.0/py3/asyncoro/discoro_schedulers.py
@@ -0,0 +1,116 @@
+"""This file is part of asyncoro; see http://asyncoro.sourceforge.net
+for details.
+
+'discoro' implements generic coroutine scheduler that creates remote
+coroutines at server processes. This scheduler is used by 'run'
+methods of computations. This file implements special purpose
+schedulers using the same 'run' method.
+"""
+
+__author__ = "Giridhar Pemmasani (pgiri@yahoo.com)"
+__copyright__ = "Copyright (c) 2015 Giridhar Pemmasani"
+__license__ = "MIT"
+__url__ = "http://asyncoro.sourceforge.net"
+
+__all__ = ['ProcScheduler']
+
+import asyncoro.disasyncoro as asyncoro
+import asyncoro.discoro as discoro
+from asyncoro.discoro import DiscoroStatus
+
+
+class ProcScheduler(object):
+    """Scheduler for submitting computation jobs.
+
+    When coroutines are created with 'run' methods of Computation
+    instances, they are created with a load-balancing algorithm on
+    available servers with no limit on how many coroutines are run at
+    a server. This works when coroutines are not CPU bound always. If,
+    however, coroutines are computations (CPU bound always/mostly),
+    then it may be more appropriate to schedule one coroutine at a
+    server so creating a new coroutine waits until a server becomes
+    available.
+
+    ProcScheduler schedules at most one computation (coroutine) at a
+    server process at any time (so a node may execute as many
+    computation coroutines as there are server processes running on
+    that node, but not more).
+
+    See 'discomp*.py' files in 'examples' directory for some use
+    cases.
+
+    NB: When using this scheduler, 'run' method of computation
+    shouldn't be used to create (remote) coroutines (unless those
+    don't take up CPU), as this scheduler is not aware of those.
+    """
+
+    _Servers = 0
+    __ServerAvail = asyncoro.Event()
+
+    def __init__(self, computation):
+        self.computation = computation
+        self.status_coro = asyncoro.Coro(self.status_proc)
+        if not computation.status_coro:
+            computation.status_coro = self.status_coro
+        self._rcoros = set()
+        self._rcoros_done = asyncoro.Event()
+
+    def schedule(self, gen, *args, **kwargs):
+        """Similar to 'run' method of computation, except as noted
+        above: This method will block until a server process is
+        available (i.e., not running another computation).
+
+        Must be used with 'yield', similar to 'run' method of Compute
+        instance.
+        """
+        while not ProcScheduler._Servers:
+            ProcScheduler.__ServerAvail.clear()
+            yield ProcScheduler.__ServerAvail.wait()
+        ProcScheduler._Servers -= 1
+        rcoro = yield self.computation.run(gen, *args, **kwargs)
+        if isinstance(rcoro, asyncoro.Coro):
+            self._rcoros.add(str(rcoro))
+        else:
+            ProcScheduler._Servers += 1
+            ProcScheduler.__ServerAvail.set()
+        raise StopIteration(rcoro)
+
+    def finish(self, close=False):
+        """Wait until all scheduled coroutines finish. If 'close' is
+        True, the computation is closed as well.
+
+        Must be used with 'yield' as 'yield proc_scheduler.finish()'.
+        """
+        if self._rcoros:
+            self._rcoros_done.clear()
+            yield self._rcoros_done.wait()
+        if close:
+            yield self.computation.close()
+            # TODO: status messages may arrive (e.g., ServerClosed)
+            # after computation is closed; when to terminate
+            # status_coro?
+            # self.status_coro.terminate()
+
+    def status_proc(self, coro=None):
+        """Coroutine to process discoro scheduler messages.
+        """
+        coro.set_daemon()
+        while True:
+            msg = yield coro.receive()
+            if isinstance(msg, asyncoro.MonitorException):
+                if msg.args[1][0] != discoro.Scheduler.ServerClosed:
+                    try:
+                        self._rcoros.remove(str(msg.args[0]))
+                    except KeyError:
+                        pass
+                    else:
+                        ProcScheduler._Servers += 1
+                        ProcScheduler.__ServerAvail.set()
+                    if not self._rcoros:
+                        self._rcoros_done.set()
+            elif isinstance(msg, DiscoroStatus):
+                if msg.status == discoro.Scheduler.ServerInitialized:
+                    ProcScheduler._Servers += 1
+                    ProcScheduler.__ServerAvail.set()
+                elif msg.status == discoro.Scheduler.ServerClosed:
+                    ProcScheduler._Servers -= 1
---
+++ packages/asyncoro/asyncoro-3.6.0.tar.gz/asyncoro-3.6.0/py3/asyncoro/examples/discomp2.py
@@ -0,0 +1,71 @@
+# Run 'discoronode.py' program to start processes to execute
+# computations sent by this client, along with this program.
+
+# Distributed computing example where this client sends computation to
+# remote discoro process to run as remote coroutines. At any time at
+# most one computation coroutine is scheduled at a process. This
+# implementation handles server processes terminating abruptly.
+
+import logging, random
+import asyncoro.discoro as discoro
+import asyncoro.disasyncoro as asyncoro
+from asyncoro.discoro_schedulers import ProcScheduler
+import asyncoro.httpd
+
+
+# this generator function is sent to remote discoro servers to run
+# coroutines there
+def compute(n, coro=None):
+    import time
+    print('process at %s received: %s' % (coro.location, n))
+    yield coro.sleep(n)
+    raise StopIteration(time.asctime()) # result of 'compute' is current time
+
+
+def client_proc(computation, coro=None):
+
+    def status_proc(coro=None):
+        coro.set_daemon()
+        while True:
+            msg = yield coro.receive()
+            # send message to ProcScheduler's status_proc:
+            proc_scheduler_status_coro.send(msg)
+            # and to httpd's status_coro:
+            httpd.status_coro.send(msg)
+            if isinstance(msg, asyncoro.MonitorException):
+                if msg.args[1][0] == StopIteration:
+                    print('result from %s: %s' % (msg.args[0].location, msg.args[1][1]))
+                else:
+                    # if computation is reentrant, resubmit this job
+                    # (keep track of submitted rcoro, args and kwargs)
+                    print('%s failed: %s' % (msg.args[0], msg.args[1][1]))
+
+    proc_scheduler = ProcScheduler(computation)
+    # ProcScheduler sets 'status_coro'; save it and set to status_proc
+    # above, which relays status messages to ProcScheduler's coro
+    proc_scheduler_status_coro = computation.status_coro
+    computation.status_coro = asyncoro.Coro(status_proc)
+
+    if (yield computation.schedule()):
+        raise Exception('schedule failed')
+
+    # submit jobs
+    for i in range(5):
+        rcoro = yield proc_scheduler.schedule(compute, random.uniform(3, 10))
+
+    # wait for all jobs to be done and close computation
+    yield proc_scheduler.finish(close=True)
+
+
+if __name__ == '__main__':
+    asyncoro.logger.setLevel(logging.DEBUG)
+    # if scheduler is not already running (on a node as a program),
+    # start private scheduler:
+    discoro.Scheduler()
+    # to illustrate passing status messages to multiple coroutines,
+    # httpd is also used in this example:
+    httpd = asyncoro.httpd.HTTPServer()
+    # send 'compute' generator function; 'depends' can include files, functions, objets
+    computation = discoro.Computation([compute], timeout=5)
+    asyncoro.Coro(client_proc, computation).value()
+    httpd.shutdown()
