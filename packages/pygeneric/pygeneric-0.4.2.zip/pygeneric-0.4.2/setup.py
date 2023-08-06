66bea643fecba7a5da02d5a0af226d733d0d061c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:29 +0000

    Add package pygeneric/0.4.2/pygeneric-0.4.2.zip

---
+++ packages/pygeneric/pygeneric-0.4.2.zip/pygeneric-0.4.2/setup.py
@@ -0,0 +1,100 @@
+#-*- coding: utf8 -*-
+import os
+import sys
+import setuptools
+from setuptools import setup
+
+AUTHOR = 'Fábio Macêdo Mendes'
+BASE, _ = os.path.split(__file__)
+SRC = os.path.join(BASE, 'src')
+setup_kwds = dict(cmdclass={})
+
+#
+# Update VERSION and meta.py with meta information
+#
+with open(os.path.join(BASE, 'VERSION')) as F:
+    VERSION = F.read().strip()
+
+with open(os.path.join(SRC, 'generic', 'meta.py'), 'w') as F:
+    F.write(
+        '# Auto-generated file. Please do not edit\n'
+        '__version__ = %r\n' % VERSION +
+        '__author__ = %r\n' % AUTHOR)
+
+
+#
+# Choose the default Python3 branch or the code converted by 3to2
+#
+PYSRC = 'src' if sys.version.startswith('3') else 'py2src'
+
+#
+# Test integration
+#
+from setuptools.command.test import test as TestCommand
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+setup_kwds['cmdclass']['test'] = PyTest
+
+#
+# Cython stuff
+#
+if 'PyPy' not in sys.version:
+    try:
+        from Cython.Build import cythonize
+        from Cython.Distutils import build_ext
+    except ImportError:
+        import warnings
+        warnings.warn('Please install Cython to compile faster versions of FGAme modules')
+    else:
+        try:
+            setup_kwds.update(ext_modules=cythonize('src/generic/*.pyx'))
+            setup_kwds['cmdclass']['build_ext'] = build_ext
+        except ValueError:
+            pass
+
+#
+# Main configuration script
+#
+setup(
+    name='pygeneric',
+    version=VERSION,
+    description='Generic functions for Python',
+    author=AUTHOR,
+    author_email='fabiomacedomendes@gmail.com',
+    url='https://github.com/fabiommendes/pygeneric',
+    long_description=open(os.path.join(BASE, 'README.txt')).read(),
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+    ],
+
+    package_dir={'': PYSRC},
+    packages=setuptools.find_packages(PYSRC),
+    license='GPL',
+    install_requires=[
+        'six',
+    ],
+    zip_safe=False,
+    tests_require=['pytest', 'psutil'],
+    **setup_kwds
+)
---
+++ packages/pygeneric/pygeneric-0.4.2.zip/pygeneric-0.4.2/src/generic/tests/__main__.py
@@ -0,0 +1,6 @@
+'''
+Loads all tests in module and run
+'''
+
+from pytest import main 
+main('../ -q --doctest-modules')
