f48c039aa44e1a4389984706157b16498f4ce5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:11 +0000

    Add package enstaller/4.8.10/enstaller-4.8.10.tar.gz

---
+++ packages/enstaller/enstaller-4.8.10.tar.gz/enstaller-4.8.10/enstaller/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM ENSTALLER SETUP.PY
+version = '4.8.10'
+full_version = '4.8.10'
+git_revision = 'fb5bcb18c686f019dca33b2a1a6e8c8043c3c651'
+is_released = True
+
+if not is_released:
+    version = full_version
