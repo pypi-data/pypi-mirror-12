5a71c6902bd0010fa1a58b4e77bf18ae91c6f531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:46 +0000

    Add package nester_rickyzhao/1.0.0/nester_rickyzhao-1.0.0.zip

---
+++ packages/nester_rickyzhao/nester_rickyzhao-1.0.0.zip/nester_rickyzhao-1.0.0/nester_rickyzhao.py
@@ -0,0 +1,8 @@
+﻿""" This is the standrad way to include a multiple-line comment in your code."""
+def print_lol(the_list):
+	""" 这个函数去一个位置参数，名为“the list",则可以是任何python列表（也可以是包含嵌套列表的列表）。所指定的列表中的每个数据项会（递归地）输出到屏幕上，各数据项各占一行。"""
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item)
+		else:
+			print(each_item)
---
+++ packages/nester_rickyzhao/nester_rickyzhao-1.0.0.zip/nester_rickyzhao-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name		= 'nester_rickyzhao',
+		version		= '1.0.0',
+		py_modules	= ['nester_rickyzhao'],
+		author		= 'hfpython',
+		author_email= 'rickyzhao_425@',
+		url			= 'http://www.headfirstable.com',
+		description = 'A simple printer of nester lists',
+		)
