82f0392459b4222dcd428bf21219ffbc2121bffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:49 +0000

    Add package ping-me/0.2.1/ping_me-0.2.1-py2.py3-none-any.whl

---
+++ packages/ping-me/ping_me-0.2.1-py2.py3-none-any.whl/ping_me/release.py
@@ -0,0 +1,14 @@
+"""Release file for ping-me"""
+
+name = 'ping-me'
+major = '0'
+minor = '2.1'
+
+__version__ = major + '.' + minor
+
+__date__ = '2015-12-08'  ## YYYY-MM-DD
+
+__description__ = 'Cross platform personalized ping'
+
+__author__ = 'Himanshu Mishra'
+__email__ = 'himanshu2014iit@gmail.com'
