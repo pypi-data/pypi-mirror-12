576500c55c3ee858e33f43cd530818f783c7e7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:53 +0000

    Add package pkglts/0.1.5/pkglts-0.1.5-py2.py3-none-any.whl

---
+++ packages/pkglts/pkglts-0.1.5-py2.py3-none-any.whl/pkglts/option/readthedocs/handlers.py
@@ -0,0 +1,12 @@
+from pkglts.option.doc import fmt_badge
+
+
+def badge(txt, env):
+    project = env['github']['project']
+
+    url = "%s.readthedocs.org/en/latest/?badge=latest" % project
+    img = "readthedocs.org/projects/%s/badge/?version=latest" % project
+    return fmt_badge(img, url, "Documentation status")
+
+
+mapping = {'readthedocs.badge': badge}
