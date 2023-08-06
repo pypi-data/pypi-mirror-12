64def1280c6743b9412e0e6b2acbc1fd77faee0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:02 +0000

    Add package etl2osm/0.0.3/etl2osm-0.0.3-py2.py3-none-any.whl

---
+++ packages/etl2osm/etl2osm-0.0.3-py2.py3-none-any.whl/etl2osm/extract.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import logging
+import os
+import fiona
+import json
+from etl2osm.transform import reproject, transform_columns, read_config, extract_epsg, config_to_properties
+from etl2osm.load import Load
+from osgeo import osr
+
+
+class Extract(Load):
+    def __init__(self, infile, **kwargs):
+        # Reset Values
+        self.features = []
+        self.wkt = ''
+        self.epsg = ''
+        extension = os.path.splitext(infile)[1][1:]
+
+        read_file = {
+            'osm': self.read_osm,
+            'geojson': self.read_geojson,
+            'json': self.read_geojson,
+            'shp': self.read_shp,
+            'kml': self.read_kml,
+            'topojson': self.read_topojson
+        }
+
+        # Error detection
+        if not os.path.exists(infile):
+            raise ValueError('File path does not exist: %s' % infile)
+
+        if extension not in read_file:
+            raise ValueError('etl2osm cannot read file extension: %s' % extension)
+
+        read_file[extension](infile, **kwargs)
+
+    def __len__(self):
+        return len(self.features)
+
+    def __iter__(self):
+        for feature in self.features:
+            yield feature
+
+    def __add__(self, data):
+        for feature in data:
+            self.features.append(feature)
+        return self
+
+    def __repr__(self):
+        return '<Data [%i]>' % len(self)
+
+    def __setitem__(self, key, value):
+        self.features[key] = value
+        return value
+
+    def __getitem__(self, key):
+        return self.features[key]
+
+    @property
+    def crs(self):
+        if self.epsg:
+            return {"type": "name", "properties": {"name": self.epsg}}
+
+        elif self.wkt:
+            return {"type": "name", "properties": {"name": self.wkt}}
+
+    @property
+    def geojson(self):
+        return {"type": "FeatureCollection", "crs": self.crs, "features": self.features}
+
+    def read_shp(self, infile, **kwargs):
+        """Reads a Shapefile and gives the results in GeoJSON format"""
+
+        logging.info('Reading Shapefile: %s' % infile)
+
+        with fiona.drivers():
+            with fiona.open(infile) as source:
+                self.geometry = set([source.meta['schema']['geometry']])
+                self.properties = source.meta['schema']['properties']
+                self.wkt = source.meta['crs_wkt']
+
+                # Read EPSG
+                crs = source.meta['crs']
+                if 'init' in crs:
+                    self.epsg = crs['init'].upper()
+
+                for feature in source:
+                    if feature:
+                        if feature['geometry']:
+                            self.features.append(feature)
+                        else:
+                            logging.warning('Could not find [geometry] in feature.')
+
+    def read_geojson(self, infile, **kwargs):
+        """Reads a GeoJSON and gives the results in GeoJSON format"""
+
+        logging.info('Reading GeoJSON: %s' % infile)
+        with open(infile) as f:
+            geojson = json.load(f)
+
+            if 'type' not in geojson:
+                raise ValueError('GeoJSON must contain a [type] "FeatureCollection" or "Feature"')
+            if 'crs' not in geojson:
+                logging.warning('Coordinate Reference System was not detected (default=EPSG:4326)')
+                self.epsg = 'EPSG:4326'
+            else:
+                self.epsg = 'EPSG:{0}'.format(extract_epsg(geojson['crs']))
+
+            # Read Feature Collection
+            if geojson['type'] == 'FeatureCollection':
+                if not geojson['features']:
+                    raise ValueError('FeatureCollection has [0] features.')
+
+                # --------->>>>>>>---------------------------
+                # GeoJSON properties NEEDS IMPROVEMENTS:
+                # - Add appropriate datatype (int/float,str)
+                # - Scan geojson for all available attributes
+                # - Data may contain multiple geometries
+                # --------->>>>>>>---------------------------
+
+                """
+                if 'geometry' in geojson['features'][0]:
+                    self.geometry = geojson['features'][0]['geometry']['type']
+                else:
+                    self.geometry = "Unknown"
+                    logging.warning('Could not find [geometry] in feature.')
+                """
+                properties = set()
+                self.geometry = set()
+
+                for feature in geojson['features']:
+                    # Add unique attribute keys to properties
+                    if 'properties' in feature:
+                        properties.update(feature['properties'].keys())
+
+                    # Only add features with geometry
+                    if feature.get('geometry'):
+                        self.features.append(feature)
+                        self.geometry.update([feature['geometry']['type']])
+                    else:
+                        logging.warning('Could not find [geometry] in feature.')
+
+                # Creating basic properties for attribute table when building a shapefile.
+                self.properties = dict((key, 'str') for key in properties)
+
+    def read_topojson(self, infile, **kwargs):
+        """Reads a TopoJSON and gives the results in GeoJSON format"""
+
+        logging.info('Reading TopoJSON: %s' % infile)
+        raise ValueError('Reading TopoJSON not implemented')
+
+    def read_kml(self, infile, **kwargs):
+        """Reads a KML and gives the results in GeoJSON format"""
+
+        logging.info('Reading KML: %s' % infile)
+        raise ValueError('Reading KML not implemented')
+
+    def read_osm(self, infile, **kwargs):
+        """Reads a OSM and gives the results in GeoJSON format"""
+
+        logging.info('Reading OSM: %s' % infile)
+        raise ValueError('Reading OSM not implemented')
+
+    def transform(self, config={}, **kwargs):
+        """ Transform the data using the config file """
+
+        self.config = read_config(config)
+
+        if config:
+            self.properties = config_to_properties(self.config)
+
+        for x, feature in enumerate(self.features):
+            # Reproject data to WGS84
+            if not self.epsg == 'ESPG:4326':
+                feature = reproject(feature, self.crs, osr.SRS_WKT_WGS84, **kwargs)
+
+            # Transform Columns
+            if self.config:
+                feature = transform_columns(feature, self.config, **kwargs)
+
+            # Save feature to self
+            self[x] = feature
+
+        self.epsg = 'EPSG:4326'
+        self.wkt = osr.SRS_WKT_WGS84
+
+
+if __name__ == '__main__':
+    pass
---
+++ packages/etl2osm/etl2osm-0.0.3-py2.py3-none-any.whl/etl2osm/transform.py
@@ -0,0 +1,357 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import os
+import re
+import json
+import logging
+import etl2osm
+from six import string_types, binary_type
+from collections import OrderedDict
+from osgeo import osr, ogr
+
+
+true_list = ['True', 'true', '1', True, 1]
+
+
+def load_json(model, **kwargs):
+    if model in kwargs:
+        model = kwargs[model]
+
+    # Model is already in JSON format
+    if isinstance(model, (dict, list, tuple)):
+        return model
+    else:
+        # Find user defined file path
+        if os.path.exists(model):
+            with open(model) as f:
+                return json.load(f)
+
+        # Look inside etl2osm [models] folder for .json files
+        root = os.path.dirname(etl2osm.__file__)[:-len('etl2osm')]
+        path = os.path.join(root, 'etl2osm', 'models', model)
+
+        if os.path.exists(path):
+            with open(path) as f:
+                return json.load(f)
+
+
+def regex_strip(value):
+    # ESRI Shapfiles have fields [SUB] blank fields at the end
+    # Regex will search all characters but must END with a letter or number
+    # Also acts as an .strip() function
+    if isinstance(value, (string_types, binary_type)):
+        match = re.search(r'^([a-z,A-Z,0-9].+[a-z,A-Z,0-9])', value)
+        if match:
+            value = match.group()
+    return str(value)
+
+
+def config_to_properties(config):
+    properties = OrderedDict()
+    config = read_config(config)
+
+    if 'conform' not in config:
+        raise ValueError('Config file missing [conform] to format attributes.')
+
+    for key, value in config['conform'].items():
+        datatype = 'str'
+
+        # If value is a dictionary, looking for {'int': True}
+        if isinstance(value, (dict)):
+            if value.get('int') in true_list:
+                datatype = 'int'
+            elif value.get('float') in true_list:
+                datatype = 'float'
+        properties[key] = datatype
+
+    return properties
+
+
+def get_coordinate_rerefence_system(crs):
+    projection = osr.SpatialReference()
+
+    # Read Projection from EPSG or WKT
+    # For all EPSG projections
+    # http://spatialreference.org/ref/epsg/wgs-84/
+    if isinstance(crs, int):
+        valid = projection.ImportFromEPSG(crs)
+
+    elif isinstance(crs, (string_types, binary_type)):
+        valid = projection.ImportFromWkt(crs)
+    else:
+        raise ValueError('Cannot detect the type Coordinate Reference System (CRS)')
+
+    # Check if results are valid (0 == Valid projection)
+    if not valid == 0:
+        raise ValueError('EPSG provided was invalid for CRS: {0}'.format(crs))
+
+    logging.info('Get CRS: %s' % projection)
+    return projection
+
+
+def extract_epsg(crs):
+    if isinstance(crs, (string_types, binary_type)):
+        crs = crs.lower()
+        if 'epsg:' in crs:
+            return int(crs.replace('epsg:', ''))
+    if isinstance(crs, dict):
+        if crs['type'] == 'name':
+            if 'properties' in crs:
+                return extract_epsg(crs['properties']['name'])
+            else:
+                return extract_epsg(crs['name'])
+    return crs
+
+
+def reproject(feature, crs_source, crs_target=4326, **kwargs):
+    # Source Projection
+    p1 = get_coordinate_rerefence_system(extract_epsg(crs_source))
+
+    # Output Projection (WGS84)
+    p2 = get_coordinate_rerefence_system(extract_epsg(crs_target))
+
+    geom = feature['geometry']
+    coord = feature['geometry']['coordinates']
+
+    convert = {
+        'Point': convert_point,
+        'LineString': convert_linestring,
+        'Polygon': convert_polygon,
+        'MultiLineString': convert_multi_linestring,
+        'MultiPoint': convert_multi_point,
+    }
+
+    if geom['type'] not in convert:
+        raise ValueError('Reproject geometry type not implemented: %s' % geom['type'])
+    feature['geometry']['coordinates'] = convert[geom['type']](p1, p2, coord)
+
+    return feature
+
+
+def convert_point(p1, p2, coord):
+    coord_trans = osr.CoordinateTransformation(p1, p2)
+    point = ogr.Geometry(ogr.wkbPoint)
+    point.AddPoint(coord[0], coord[1])
+    point.Transform(coord_trans)
+
+    return point.GetPoint_2D()
+
+
+def convert_multi_point(p1, p2, coord):
+    multi_point = []
+    for point in coord:
+        multi_point.append(convert_point(p1, p2, point))
+
+    return multi_point
+
+
+def convert_multi_linestring(p1, p2, coord):
+    multi_line = []
+    for line in coord:
+        multi_line.append(convert_linestring(p1, p2, line))
+
+    return multi_line
+
+
+def convert_linestring(p1, p2, coord):
+    line = []
+    for point in coord:
+        line.append(convert_point(p1, p2, point))
+
+    return line
+
+
+def convert_polygon(p1, p2, coord):
+    polygon = []
+    for line in coord:
+        polygon.append(convert_linestring(p1, p2, line))
+
+    return polygon
+
+
+def read_config(config):
+    if isinstance(config, dict):
+        return config
+
+    if not os.path.exists(config):
+        raise ValueError('Config file path does not exist: %s' % config)
+
+    with open(config) as f:
+        return json.load(f, object_pairs_hook=OrderedDict)
+
+
+def titlecase_except(value, **kwargs):
+    if isinstance(value, (string_types, binary_type)):
+        word_list = re.split(' ', value)
+        final = []
+
+        for word in word_list:
+            if word in load_json('title_except.json', **kwargs):
+                final.append(word)
+            else:
+                final.append(word.capitalize())
+
+        return ' '.join(final)
+    return value
+
+
+def clean_field(properties, conform, **kwargs):
+    if 'field' in conform:
+        # STRING
+        if isinstance(conform, dict):
+            field = conform['field']
+            if field not in properties:
+                raise ValueError('Cannot find attribute [%s] using the Attribute Function.' % field)
+            value = properties[field]
+
+    # DICT
+    elif isinstance(conform, (string_types, binary_type)):
+        field = conform
+        if field not in properties:
+            raise ValueError('Cannot find attribute [%s] using the Attribute Function.' % field)
+        value = properties[field]
+
+    # LIST
+    elif isinstance(conform, (list, tuple)):
+        values = []
+        for field in conform:
+            value = clean_field(properties, field)
+            if value:
+                values.append(value)
+        value = ' '.join(values)
+
+    # LIST using Fields
+    elif 'fields' in conform:
+        value = []
+        for field in conform['fields']:
+            if field not in properties:
+                raise ValueError('Cannot find attribute [%s] using the Attribute Function.' % field)
+            value.append(properties[field])
+
+    # Attribute Functions
+    if 'function' in conform:
+
+        # Applies a Regex function match or replace from pattern.
+        if 'regexp' in conform['function']:
+            if 'field' not in conform:
+                raise ValueError('[field] is missing using the Regex Attribute Function.')
+            if 'pattern' not in conform:
+                raise ValueError('[pattern] is missing using the Regex Attribute Function.')
+            match = re.search(conform['pattern'], properties[field])
+            value = match.group()
+            if 'replace' in conform:
+                value = properties[field].replace(value, conform['replace'])
+
+        # Applies the Join attribute function
+        elif 'join' in conform['function']:
+            if 'fields' not in conform:
+                raise ValueError('[fields] are missing using the Join Attribute Function.')
+            if 'separator' not in conform:
+                logging.warning('[separator] is missing using the Join Attribute Function.')
+            separator = conform.get('separator', ' ')
+            value = separator.join(value)
+
+        # Replaces the abreviated suffix (AVE=Avenue)
+        elif 'suffix' in conform['function']:
+            suffix = load_json('suffix.json', **kwargs)
+
+            if 'field' not in conform:
+                raise ValueError('[field] is missing using the Suffix Attribute Function.')
+            if properties[field]:
+                if properties[field] not in suffix:
+                    logging.warning('Suffix cannot be found [%s] in ETL2OSM models.' % properties[field])
+                else:
+                    value = suffix[str(properties[field])]
+
+        # Replaces the abreviated directions (NE=Northeast)
+        elif 'direction' in conform['function']:
+            direction = load_json('direction.json', **kwargs)
+
+            if 'field' not in conform:
+                raise ValueError('[field] is missing using the Direction Attribute Function.')
+            if properties[field]:
+                if properties[field] not in direction:
+                    logging.warning('Direction cannot be found [%s] in ETL2OSM models.' % properties[field])
+                else:
+                    value = direction[str(properties[field])]
+
+        # Converts string to a nice Titlecase (3RD AVENUE=3rd Avenue)
+        elif 'title' in conform['function']:
+            if 'field' not in conform:
+                raise ValueError('[field] is missing using the Title Attribute Function.')
+            value = titlecase_except(properties[field])
+
+        # Adds mph at the end of the integer field.
+        elif 'mph' in conform['function']:
+            if 'field' not in conform:
+                raise ValueError('[field] is missing using the Mph Attribute Function.')
+            value = '{0} mph'.format(properties[field])
+
+    # Remove any white spaces [True/False]
+    if 'strip' in conform:
+        if conform['strip'] in true_list:
+            if not isinstance(value, (string_types, binary_type)):
+                raise ValueError('Can only [strip] Attribute Function to strings or binary types.')
+            value = value.strip()
+
+    # Converts String to Integer [True/False]
+    if 'int' in conform:
+        if conform['int'] in true_list:
+            try:
+                value = int(value)
+            except:
+                logging.warning('Cannot convert [%s] to integer.' % value)
+
+    # Converts String to Integer [True/False]
+    if 'float' in conform:
+        if conform['float'] in true_list:
+            try:
+                value = float(value)
+            except:
+                logging.warning('Cannot convert [%s] to float.' % value)
+
+    # Converts String to Integer [True/False]
+    if 'text' in conform:
+        value = conform['text']
+
+    return value
+
+
+def transform_fields(properties, conform, **kwargs):
+    fields = OrderedDict()
+    for key in conform.keys():
+        value = None
+
+        # STRING
+        # Replace only a single field
+        if isinstance(conform[key], (string_types, binary_type)):
+            if conform[key] in properties:
+                value = properties[conform[key]]
+                value = clean_field(properties, conform[key], **kwargs)
+            fields.update(dict([(key, value)]))
+
+        # DICT
+        # Replace & join multiple fields together
+        elif isinstance(conform[key], (OrderedDict, dict)):
+            value = clean_field(properties, conform[key], **kwargs)
+            fields.update(dict([(key, value)]))
+
+        # LIST
+        # Join a values from a list
+        elif isinstance(conform[key], (list, tuple)):
+            value = clean_field(properties, conform[key], **kwargs)
+            fields.update(dict([(key, value)]))
+
+    return fields
+
+
+def transform_columns(feature, config, **kwargs):
+    config = read_config(config)
+    conform = config['conform']
+    feature['properties'] = transform_fields(feature['properties'], conform, **kwargs)
+
+    return feature
+
+
+if __name__ == "__main__":
+    pass
