7bb7ed9214141acf1857a2e755ddb3b06ab0916e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:53 +0000

    Add package revscoring/0.7.10/revscoring-0.7.10-py2.py3-none-any.whl

---
+++ packages/revscoring/revscoring-0.7.10-py2.py3-none-any.whl/revscoring/dependencies/__init__.py
@@ -0,0 +1,20 @@
+"""
+This module provides a general set of utilities for implementing a set of
+dependencies, solving them and injecting context and cache.
+
+.. automodule:: revscoring.dependencies.dependent
+
+functions
++++++++++
+.. automodule:: revscoring.dependencies.functions
+
+context
++++++++
+.. automodule:: revscoring.dependencies.context
+"""
+
+from .functions import solve, expand, dig, draw, normalize_context
+from .context import Context
+from .dependent import Dependent
+
+__all__ = [solve, expand, dig, draw, normalize_context, Context, Dependent]
